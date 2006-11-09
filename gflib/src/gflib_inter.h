a47b8c4b6d18c5a8decd316686e37b5382311bc7
Satoshi Mitsuhara <mitsuhara@gamefreak.co.jp>
2006-11-09 07:43:24 +0000

    gflib_inter.hを追加

---
+++ gflib/src/gflib_inter.h
@@ -0,0 +1,19 @@
+//===================================================================
+/**
+ * @file	gflib_inter.h
+ * @brief	ì‡ïîå¸ÇØëççáincludeÉtÉ@ÉCÉã
+ * @author	GAME FREAK Inc.
+ * @date	06.11.09
+ *
+ * $LastChangedDate$
+ * $Id$
+ */
+//===================================================================
+#ifndef __GFLIB_INTER_H__
+#define __GFLIB_INTER_H__
+
+#include "filesystem_inter.h"
+
+
+#endif	// __GFLIB_INTER_H__
+/*  */
