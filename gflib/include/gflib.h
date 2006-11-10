6f2d32af8fdb3b87bc59ce0a140d1efb28cf60cf
Satoshi Mitsuhara <mitsuhara@gamefreak.co.jp>
2006-11-10 09:24:41 +0000

    filesystem.cに関数追加

--- gflib/include/gflib.h
+++ gflib/include/gflib.h
@@ -1,11 +1,12 @@
 #include <nitro.h>
 #include <nnsys.h>
 
-	#define GF_ASSERT_Printf( ... )		((void)0);
-	#define GF_ASSERT(exp)				((void)0);
-	#define GF_ASSERT_MSG(exp, ...)		((void)0);
+#define GF_ASSERT_Printf( ... )		((void)0);
+#define GF_ASSERT(exp)				((void)0);
+#define GF_ASSERT_MSG(exp, ...)		((void)0);
 
 #include "initialize.h"
 #include "heapsys.h"
+#include "filesystem.h"
 
 /*  */
