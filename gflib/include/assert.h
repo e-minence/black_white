2f051649badb91596a297a965c8c0ebc30a490ca
Sosuke Tamada <tamada@gamefreak.co.jp>
2007-01-19 06:31:26 +0000

    定義無効状態でアサートの実装を追加した

--- gflib/include/assert.h
+++ gflib/include/assert.h
@@ -9,8 +9,31 @@
 #ifndef	__ASSERT_H__
 #define	__ASSERT_H__
 
-#define GF_ASSERT_Printf( ... )		((void)0);
+#define	GF_ASSERT_ENABLE
+
+
+#ifndef	GF_ASSERT_ENABLE
+
+//ASSERTñ≥å¯ÇÃèÍçá
+extern void GFL_Assert(const char * filename, unsigned int line_no, const char * exp);
+extern void GFL_AssertMsg(const char * filename, unsigned int line_no, const char * fmt, ... );
+extern void GFL_AssertPrintf(const char * fmt, ... );
+
+#define GF_ASSERT(exp)		\
+	(exp)? ((void)0): GFL_Assert(__FILE__, __LINE__, #exp);
+#define GF_ASSERT_MSG(exp, ... )		\
+	(exp)? ((void)0): GFL_AssertMsg(__FILE__, __LINE__, __VA_ARGS__ );
+//#define GF_ASSERT_Printf( ... )		((exp)? ((void)0);
+
+
+#else
+//ASSERTóLå¯ÇÃèÍçá
+
 #define GF_ASSERT(exp)				((void)0);
 #define GF_ASSERT_MSG(exp, ...)		((void)0);
+#define GF_ASSERT_Printf( ... )		((void)0);
+
+#endif
+
 
 #endif	/* __ASSERT_H__ */
---
+++ gflib/src/assert.c
@@ -0,0 +1,78 @@
+//==============================================================================================
+/**
+ * @file	assert.c
+ * @date	2007.01.19
+ * @author	tamada GAME FREAK inc.
+ */
+//==============================================================================================
+
+#include "gflib.h"
+
+//==============================================================================================
+//==============================================================================================
+//------------------------------------------------------------------
+/**
+ * @brief	ASSERT処理用表示メイン
+ * @param	fmt
+ */
+//------------------------------------------------------------------
+static void AssertPrintf(const char * fmt, ... )
+{
+    va_list vlist;
+
+    va_start(vlist, fmt);
+    OS_TVPrintf(fmt, vlist);
+    va_end(vlist);
+}
+
+//------------------------------------------------------------------
+/**
+ * @brief	アサート失敗箇所のソース情報を出力
+ */
+//------------------------------------------------------------------
+static void AssertPrintTitle(const char * filename, unsigned int line_no)
+{
+	AssertPrintf("  **** ASSERTION FAILED ! **** \n");
+	AssertPrintf("%s(%d)\n", filename, line_no);
+}
+
+//------------------------------------------------------------------
+/**
+ * @brief	ASSERT処理簡易版（条件式と発生箇所の表示）
+ */
+//------------------------------------------------------------------
+void GFL_Assert(const char * filename, unsigned int line_no, const char * exp)
+{
+	AssertPrintTitle(filename, line_no);
+
+	AssertPrintf("%s\n", exp);
+
+	OS_Terminate();
+}
+
+//------------------------------------------------------------------
+/**
+ * @brief	ASSERT処理（条件式とユーザー指定の文字列表示）
+ */
+//------------------------------------------------------------------
+void GFL_AssertMsg(const char * filename, unsigned int line_no, const char * fmt, ... )
+{
+	va_list vlist;
+	AssertPrintTitle(filename, line_no);
+	va_start(vlist, fmt);
+	AssertPrintf(fmt, vlist);
+	va_end(vlist);
+
+	OS_Terminate();
+}
+
+#if 0
+//------------------------------------------------------------------
+/**
+ */
+//------------------------------------------------------------------
+void GFL_AssertPrintf(const char * fmt, ... )
+{
+}
+#endif
+
