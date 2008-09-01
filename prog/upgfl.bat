669f0f82626fe8c4dbc83ddc4c0ae6602fcfe3de
Masao Taya <taya@gamefreak.co.jp>
2008-09-01 07:06:09 +0000

    GFLibの更新用バッチ処理

---
+++ prog/upgfl.bat
@@ -0,0 +1,13 @@
+@ECHO OFF
+cd %GFL_MAKE_DIR:/=\%
+make
+if ERRORLEVEL 1 goto MAKE_ERR
+xcopy /Y /D /I include\*.h %GFL_PROJECT_DIR:/=\%\include
+xcopy /Y /D /I install\ARM9-TS\Release\*.a %GFL_PROJECT_DIR:/=\%\install\ARM9-TS\Release
+goto END
+
+:MAKE_ERR
+echo GFLibÇÃmakeÇ…é∏îsÇµÇ‹ÇµÇΩÅI
+
+:END
+cd %PROJECT_PROGDIR:/=\%
