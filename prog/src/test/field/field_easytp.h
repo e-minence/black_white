50e2d75af7e10bc7474bcdeff327f27903a8f9ea
Keita Kagaya <kagaya@gamefreak.co.jp>
2008-09-30 06:48:52 +0000

    field_easytp.c,h 簡易タッチパネル処理

---
+++ prog/src/test/field/field_easytp.h
@@ -0,0 +1,46 @@
+//======================================================================
+/**
+ *
+ * @file	field_easytp.h
+ * @brief	フィールド簡易タッチパネル
+ * @author	kagaya
+ * @data	08.09.30
+ */
+//======================================================================
+#ifndef __FIELD_EASYTP_H__
+#define __FIELD_EASYTP_H__
+#include <gflib.h>
+
+//======================================================================
+//	define
+//======================================================================
+//--------------------------------------------------------------
+///	FLDEASYTP_TCHDIR	押された方向を表す
+//--------------------------------------------------------------
+typedef enum
+{
+	FLDEASYTP_TCHDIR_UP = 0,	//上
+	FLDEASYTP_TCHDIR_DOWN,		//下 
+	FLDEASYTP_TCHDIR_LEFT,		//左
+	FLDEASYTP_TCHDIR_RIGHT,		//右
+	FLDEASYTP_TCHDIR_LEFTUP,	//左上
+	FLDEASYTP_TCHDIR_LEFTDOWN,	//左下
+	FLDEASYTP_TCHDIR_RIGHTUP,	//右上
+	FLDEASYTP_TCHDIR_RIGHTDOWN,	//右下
+	FLDEASYTP_TCHDIR_CENTER,	//中心
+	FLDEASYTP_TCHDIR_NON,		//押されていない
+	
+	FLDEASYTP_TCHDIR_MAX = FLDEASYTP_TCHDIR_NON,	//最大
+}FLDEASYTP_TCHDIR;
+
+//======================================================================
+//	typedef struct
+//======================================================================
+//typedef struct _FLD_EASYTP FLD_EASYTP;
+
+//======================================================================
+//	extern
+//======================================================================
+extern FLDEASYTP_TCHDIR FieldEasyTP_TouchDirGet( void );
+
+#endif //__FIELD_EASYTP_H__
