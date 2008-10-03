d8edd49ae79aa9a7fc3b54af1d58a56276b77727
Keita Kagaya <kagaya@gamefreak.co.jp>
2008-10-03 08:17:25 +0000

    グリッド移動テスト更新 testgridmap_a マップデータがおかしかった

--- prog/src/test/field/field_sub_grid.c
+++ prog/src/test/field/field_sub_grid.c
@@ -8,6 +8,7 @@
  * しばらくはこの形式です。
  */
 //======================================================================
+#include "arc/test_graphic/testgridmap_a.naix"
 
 //======================================================================
 //	define
@@ -30,10 +31,10 @@ enum
 #define GRID_VALUE	(16)
 #define GRID_VALUE_FX32 (GRID_VALUE*FX32_ONE)
 
-#define NUM_GRID(a)	((a)/GRID_VALUE)
-#define GRID_NUM(a) ((a)*GRID_VALUE)
-#define NUM_GRID_FX32(a) (NUM_FX32(NUM_GRID(a)))
-#define GRID_NUM_FX32(a) (FX32_NUM(GRID_NUM(a)))
+#define SIZE_GRID(a)((a)/GRID_VALUE)
+#define GRID_SIZE(a) ((a)*GRID_VALUE)
+#define SIZE_GRID_FX32(a) (FX32_NUM(SIZE_GRID(a)))
+#define GRID_SIZE_FX32(a) (NUM_FX32(GRID_SIZE(a)))
 
 //======================================================================
 //	typedef struct
@@ -50,6 +51,10 @@ struct _FGRID_CONT
 	FIELD_WORK *pFieldWork;
 	
 	FGRID_PLAYER *pGridPlayer;
+	
+	u16 *pMapAttr[4];
+	
+	int debug_camera_num;
 };
 
 //--------------------------------------------------------------
@@ -72,6 +77,15 @@ struct _FGRID_PLAYER
 	PC_ACTCONT *pActCont;
 };
 
+//--------------------------------------------------------------
+///	camera
+//--------------------------------------------------------------
+typedef struct
+{
+	u16 len;
+	fx32 hi;
+}GRID_CAMERA_DATA;
+
 //======================================================================
 //	proto
 //======================================================================
@@ -87,6 +101,21 @@ static BOOL GridVecPosMove(
 	const FLD_G3D_MAPPER *g3Dmapper,
 	FLD_G3D_MAPPER_INFODATA *gridInfoData,
 	VecFx32 *pos, VecFx32 *vecMove );
+static BOOL MapHitCheck( const FGRID_CONT *pGridCont, fx32 x, fx32 z );
+
+//--------------------------------------------------------------
+///	data
+//--------------------------------------------------------------
+static const GRID_CAMERA_DATA DATA_CameraTbl[] =
+{
+	{ 0x78, 0xd8000 },	//DPぽい
+	{ 0x78, 0xb8000 },	//ちょっと近い
+	{ 0x78, 0x80000 },	//だいぶ近い
+	{ 0x50, 0x40000 },	//凄い近い
+	{ 0x108, 0xd8000 },	//遠くを見渡せる
+};
+
+#define GRIDCAMERA_MAX (NELEMS(DATA_CameraTbl))
 
 //======================================================================
 //	フィールドグリッドタイプ　メイン
@@ -114,8 +143,19 @@ static void GridMoveCreate( FIELD_WORK * fieldWork, VecFx32 * pos, u16 dir)
 	fieldWork->pGridCont = FGridCont_Init( fieldWork );
 	
 	//カメラ設定
-	FLD_SetCameraLength( fieldWork->camera_control, 0x58 );
-	FLD_SetCameraHeight( fieldWork->camera_control, 0x48000 );
+	FLD_SetCameraLength( fieldWork->camera_control, DATA_CameraTbl[0].len );
+	FLD_SetCameraHeight( fieldWork->camera_control, DATA_CameraTbl[0].hi );
+	
+	//ビルボード設定
+	{
+		VecFx32 scale = {
+			FX32_ONE+(FX32_ONE/2)+(FX32_ONE/4),
+			FX32_ONE+(FX32_ONE/2)+(FX32_ONE/4),
+			FX32_ONE+(FX32_ONE/2)+(FX32_ONE/4),
+		};
+		GFL_BBD_SetScale(
+			GFL_BBDACT_GetBBDSystem(fieldWork->gs->bbdActSys), &scale );
+	}
 }
 
 //------------------------------------------------------------------
@@ -126,25 +166,32 @@ static void GridMoveCreate( FIELD_WORK * fieldWork, VecFx32 * pos, u16 dir)
 static void GridMoveMain( FIELD_WORK* fieldWork, VecFx32 * pos )
 {
 //	MainPlayerAct( fieldWork->pcActCont, fieldWork->key_cont );
-	{
-		FGRID_CONT *pGridCont = fieldWork->pGridCont;
-		FGRID_PLAYER *pGridPlayer = pGridCont->pGridPlayer;
-		FGridPlayer_Move( pGridPlayer, 0, fieldWork->key_cont );
-	}
+	FGRID_CONT *pGridCont = fieldWork->pGridCont;
+	FGRID_PLAYER *pGridPlayer = pGridCont->pGridPlayer;
 	
-	if( (fieldWork->key_cont & PAD_BUTTON_L) ){
-		FLD_SetCameraLength( fieldWork->camera_control, 0x58 );
-		FLD_SetCameraHeight( fieldWork->camera_control, 0x48000 );
-	}else if( (fieldWork->key_cont & PAD_BUTTON_R) ){
-		FLD_SetCameraLength( fieldWork->camera_control, 0x68 );
-		FLD_SetCameraHeight( fieldWork->camera_control, 0x64000 );
+	FGridPlayer_Move( pGridPlayer, 0, fieldWork->key_cont );
+	
+	if( (GFL_UI_KEY_GetTrg()&PAD_BUTTON_L) ){
+		pGridCont->debug_camera_num++;
+		pGridCont->debug_camera_num %= GRIDCAMERA_MAX;
+		FLD_SetCameraLength( fieldWork->camera_control,
+				DATA_CameraTbl[pGridCont->debug_camera_num].len );
+		FLD_SetCameraHeight( fieldWork->camera_control,
+				DATA_CameraTbl[pGridCont->debug_camera_num].hi );
+		OS_Printf( "テストカメラ　No.%d\n", pGridCont->debug_camera_num );
 	}
 	
 	FLD_MainFieldActSys( fieldWork->fldActCont );
 	GetPlayerActTrans( fieldWork->pcActCont, pos );
 	FLD_SetCameraTrans( fieldWork->camera_control, pos );
 //	FLD_SetCameraDirection( fieldWork->camera_control, &dir );
-	FLD_MainCamera( fieldWork->camera_control, 0 );
+	
+	{
+		int key = fieldWork->key_cont;
+//		key &= ~(PAD_BUTTON_L|PAD_BUTTON_R);
+		key = 0;
+		FLD_MainCamera( fieldWork->camera_control, key );
+	}
 }
 
 //------------------------------------------------------------------
@@ -183,6 +230,24 @@ static FGRID_CONT * FGridCont_Init( FIELD_WORK *fieldWork )
 	pGridCont->pFieldWork = fieldWork;
 	pGridCont->pGridPlayer = FGridPlayer_Init( pGridCont );
 	
+	{
+		u32 size;
+		ARCHANDLE *arcH;
+		arcH = GFL_ARC_OpenDataHandle(
+			ARCID_FLDMAP_TESTGRIDMAP_ATTR, pGridCont->heapID );
+		
+		pGridCont->pMapAttr[0] = GFL_ARC_LoadDataAllocByHandle( arcH,
+			NARC_testgridmap_a_map14_07a_dat, pGridCont->heapID );
+		pGridCont->pMapAttr[1] = GFL_ARC_LoadDataAllocByHandle( arcH,
+			NARC_testgridmap_a_map15_07a_dat, pGridCont->heapID );
+		pGridCont->pMapAttr[2] = GFL_ARC_LoadDataAllocByHandle( arcH,
+			NARC_testgridmap_a_map14_08a_dat, pGridCont->heapID );
+		pGridCont->pMapAttr[3] = GFL_ARC_LoadDataAllocByHandle( arcH,
+			NARC_testgridmap_a_map15_08a_dat, pGridCont->heapID );
+		
+		GFL_ARC_CloseDataHandle( arcH );
+	}
+
 	return( pGridCont );
 }
 
@@ -197,6 +262,11 @@ static void FGridCont_Delete( FIELD_WORK *fieldWork )
 {
 	FGRID_CONT *pGridCont;
 	pGridCont = fieldWork->pGridCont;
+	
+	GFL_HEAP_FreeMemory( pGridCont->pMapAttr[0] );
+	GFL_HEAP_FreeMemory( pGridCont->pMapAttr[1] );
+	GFL_HEAP_FreeMemory( pGridCont->pMapAttr[2] );
+	GFL_HEAP_FreeMemory( pGridCont->pMapAttr[3] );
 	
 	FGridPlayer_Delete( pGridCont );
 	
@@ -216,8 +286,8 @@ static void FGridCont_Delete( FIELD_WORK *fieldWork )
 //--------------------------------------------------------------
 static FGRID_PLAYER * FGridPlayer_Init( FGRID_CONT *pGridCont )
 {
-	VecFx32 pos;
 	FGRID_PLAYER *pJiki;
+	VecFx32 pos = { GRID_SIZE_FX32(32), 0x30000, GRID_SIZE_FX32(37) };
 
 	pJiki = GFL_HEAP_AllocClearMemory(
 			pGridCont->heapID, sizeof(FGRID_PLAYER) );
@@ -225,10 +295,10 @@ static FGRID_PLAYER * FGridPlayer_Init( FGRID_CONT *pGridCont )
 	
 	pJiki->pGridCont = pGridCont;
 	pJiki->pActCont = pGridCont->pFieldWork->pcActCont;
-	
-	GetPlayerActTrans( pJiki->pActCont, &pos );
-	pJiki->vec_pos = pos;
 
+	pJiki->vec_pos = pos;
+	
+	SetPlayerActTrans( pJiki->pActCont, &pJiki->vec_pos );
 	return( pJiki );
 }
 
@@ -267,21 +337,38 @@ static void FGridPlayer_Move(
 		else if( (key_cont&PAD_KEY_RIGHT) ){ dir = DIR_RIGHT; }
 		
 		if( dir != DIR_NOT ){
-			pJiki->move_flag = TRUE;
-			pJiki->dir = dir;
-			pJiki->move_dir = dir;
-			
+			fx32 nx = pJiki->vec_pos.x;
+			fx32 nz = pJiki->vec_pos.z;
+
 			switch( dir ){
-			case DIR_UP:	pJiki->move_val.z = NUM_FX32( -1 ); break;
-			case DIR_DOWN:	pJiki->move_val.z = NUM_FX32( 1 ); break;
-			case DIR_LEFT:	pJiki->move_val.x = NUM_FX32( -1 ); break;
-			case DIR_RIGHT: pJiki->move_val.x = NUM_FX32( 1 ); break;
+			case DIR_UP:	nz -= GRID_VALUE_FX32; break;
+			case DIR_DOWN:	nz += GRID_VALUE_FX32; break;
+			case DIR_LEFT:	nx -= GRID_VALUE_FX32; break;
+			case DIR_RIGHT:	nx += GRID_VALUE_FX32; break;
 			}
 			
-			if( (key_cont & PAD_BUTTON_B) ){
-				pJiki->move_val.x <<= 1;
-				pJiki->move_val.y <<= 1;
-				pJiki->move_val.z <<= 1;
+			if( (key_cont&PAD_BUTTON_R) ||
+				MapHitCheck(pJiki->pGridCont,nx,nz) == FALSE ){
+				pJiki->move_flag = TRUE;
+				pJiki->dir = dir;
+				pJiki->move_dir = dir;
+				
+				switch( dir ){
+				case DIR_UP:	pJiki->move_val.z = NUM_FX32( -2 ); break;
+				case DIR_DOWN:	pJiki->move_val.z = NUM_FX32( 2 ); break;
+				case DIR_LEFT:	pJiki->move_val.x = NUM_FX32( -2 ); break;
+				case DIR_RIGHT: pJiki->move_val.x = NUM_FX32( 2 ); break;
+				}
+				
+				if( (key_cont & PAD_BUTTON_B) ){
+					pJiki->move_val.x <<= 1;
+					pJiki->move_val.y <<= 1;
+					pJiki->move_val.z <<= 1;
+				}else if( (key_cont & PAD_BUTTON_R) ){
+					pJiki->move_val.x <<= 2;
+					pJiki->move_val.y <<= 2;
+					pJiki->move_val.z <<= 2;
+				}
 			}
 		}
 	}
@@ -297,11 +384,6 @@ static void FGridPlayer_Move(
 			GetFieldG3Dmapper(pJiki->pGridCont->pFieldWork->gs),
 			PlayerActGrid_GridInfoGet(pcActCont),
 			&pJiki->vec_pos, &pJiki->move_val );
-		
-		OS_Printf( "x=%d,z=%d\n",
-			FX32_NUM(pJiki->vec_pos.x), FX32_NUM(pJiki->vec_pos.y) );
-		OS_Printf( "val_x=%x,val_z=%x\n",
-			pJiki->move_val.x, pJiki->move_val.y );
 #endif
 		pJiki->move_count.x += pJiki->move_val.x;
 		pJiki->move_count.y += pJiki->move_val.y;
@@ -344,6 +426,23 @@ static void FGridPlayer_Move(
 		case DIR_RIGHT:	dir = 2; break;
 		}
 		PlayerActGrid_AnimeSet( pcActCont, dir, pJiki->move_flag );
+	}
+	
+	if( (GFL_UI_KEY_GetTrg()&PAD_BUTTON_X) ){
+		fx32 x = pJiki->vec_pos.x;
+		fx32 z = pJiki->vec_pos.z;
+
+		OS_Printf(
+			"自機情報 GridX %d(%d,%xH), GridZ %d(%d,%xH), Y=%xH",
+			SIZE_GRID_FX32(x), x, x,
+			SIZE_GRID_FX32(z), z, z,
+			pJiki->vec_pos.y );
+		
+		if( MapHitCheck(pJiki->pGridCont,x,z) == TRUE ){
+			OS_Printf( "位置障害物\n" );
+		}else{
+			OS_Printf( "\n" );
+		}
 	}
 }
 
@@ -406,6 +505,49 @@ static BOOL GridVecPosMove(
 	}
 	
 	return( TRUE );
+}
+
+//--------------------------------------------------------------
+/**
+ * アトリビュート当り判定
+ * @param
+ * @retval
+ */
+//--------------------------------------------------------------
+static BOOL MapHitCheck( const FGRID_CONT *pGridCont, fx32 x, fx32 z )
+{
+	u16 attr;
+	const u16 *buf = NULL;
+	int gx = SIZE_GRID_FX32(x), gz = SIZE_GRID_FX32(z);
+	
+	if( gx < 0x20 ){
+		if( gz < 0x20 ){
+			buf = pGridCont->pMapAttr[0];
+		}else if( gz < 0x40 ){
+			gz -= 0x20;
+			buf = pGridCont->pMapAttr[2];
+		}
+	}else if( gx < 0x40 ){
+		gx -= 0x20;
+		if( gz < 0x20 ){
+			buf = pGridCont->pMapAttr[1];
+		}else if( gz < 0x40 ){
+			gz -= 0x20;
+			buf = pGridCont->pMapAttr[3];
+		}
+	}
+	
+	if( buf == NULL ){
+		return( FALSE );
+	}
+	
+	attr = buf[gx+(gz*0x20)];
+	
+	if( ((attr&0x8000)>>15) ){
+		return( TRUE );
+	}
+	
+	return( FALSE );
 }
 
 //======================================================================
