050ec2ccfbca8875dad0a059bb5bf166c535734b
Sosuke Tamada <tamada@gamefreak.co.jp>
2008-10-23 08:33:26 +0000

    自機状態保持のためのワークとその関連関数を追加

--- prog/src/gamesystem/gamesystem.c
+++ prog/src/gamesystem/gamesystem.c
@@ -12,56 +12,62 @@
 #include "system/main.h"		//HEAPIDの参照用
 
 #include "gamesystem/gamesystem.h"
+#include "gamesystem/playerwork.h"
 
+//============================================================================================
+//============================================================================================
 enum {
 	HEAPID_GAMESYS = HEAPID_TAMADA_DEBUG,
-};
-//------------------------------------------------------------------
-//------------------------------------------------------------------
-struct _GAMESYS_WORK {
-	HEAPID heapID;
-	void * parent_work;
 
-	GFL_PROCSYS * procsys;
+	HEAPSIZE_GAMESYS = 0x2000,
+	//x200000
 };
 
 //------------------------------------------------------------------
+/**
+ * @brief	ゲーム制御システム用ワーク保持変数
+ *
+ * ※たぶんデバッグ用か限定時のグローバル参照用にしか使わないはず
+ */
 //------------------------------------------------------------------
 static GAMESYS_WORK * GameSysWork;
 
-extern const GFL_PROC_DATA DebugFieldProcData;
 
 //============================================================================================
 //============================================================================================
-static void GameSystem_Init(GAMESYS_WORK * gsys);
+extern const GFL_PROC_DATA DebugFieldProcData;
+
+static void GameSystem_Init(GAMESYS_WORK * gsys, HEAPID heapID, void * pwk);
 static BOOL GameSystem_Main(GAMESYS_WORK * gsys);
 static void GameSystem_End(GAMESYS_WORK * gsys);
+static u32 GameSystem_GetGameSysWorkSize(void);
+
+
+//============================================================================================
+/**
+ * @brief	ゲームシステム用プロセス
+ *
+ * NitroMain->GFLUser_Main->GFL_PROC_Main()に登録される。
+ * 基本的にはゲーム本編開始後はこのプロセスが動作し続けるはず
+ */
+//============================================================================================
 static GFL_PROC_RESULT GameMainProcInit(GFL_PROC * proc, int * seq, void * pwk, void * mywk);
 static GFL_PROC_RESULT GameMainProcMain(GFL_PROC * proc, int * seq, void * pwk, void * mywk);
 static GFL_PROC_RESULT GameMainProcEnd(GFL_PROC * proc, int * seq, void * pwk, void * mywk);
 
 //------------------------------------------------------------------
-/**
- * @brief		プロセス関数テーブル
- */
-//------------------------------------------------------------------
-const GFL_PROC_DATA GameMainProcData = {
-	GameMainProcInit,
-	GameMainProcMain,
-	GameMainProcEnd,
-};
-
-
-//------------------------------------------------------------------
 //------------------------------------------------------------------
 static GFL_PROC_RESULT GameMainProcInit(GFL_PROC * proc, int * seq, void * pwk, void * mywk)
 {
-	GFL_HEAP_CreateHeap( GFL_HEAPID_APP, HEAPID_GAMESYS, 0x2000 );
-	//GFL_HEAP_CreateHeap( GFL_HEAPID_APP, HEAPID_GAMESYS, 0x200000 );
-	GameSysWork = GFL_PROC_AllocWork(proc, sizeof(GAMESYS_WORK), HEAPID_GAMESYS);
-	GameSysWork->heapID = HEAPID_GAMESYS;
-	GameSysWork->parent_work = pwk;
-	GameSystem_Init(GameSysWork);
+	GAMESYS_WORK * gsys;
+	u32 work_size = GameSystem_GetGameSysWorkSize();
+	GFL_HEAP_CreateHeap( GFL_HEAPID_APP, HEAPID_GAMESYS, HEAPSIZE_GAMESYS );
+	gsys = GFL_PROC_AllocWork(proc, work_size, HEAPID_GAMESYS);
+	GameSysWork = gsys;
+	GameSystem_Init(gsys, HEAPID_GAMESYS, pwk);
+#if 1		/* 暫定的にプロセス登録 */
+	GameSystem_CallProc(gsys, NO_OVERLAY_ID, &DebugFieldProcData, gsys);
+#endif
 	return GFL_PROC_RES_FINISH;
 }
 //------------------------------------------------------------------
@@ -79,26 +85,69 @@ static GFL_PROC_RESULT GameMainProcMain(GFL_PROC * proc, int * seq, void * pwk, 
 //------------------------------------------------------------------
 static GFL_PROC_RESULT GameMainProcEnd(GFL_PROC * proc, int * seq, void * pwk, void * mywk)
 {
-	GameSystem_End(GameSysWork);
+	GAMESYS_WORK * gsys = mywk;
+	GameSystem_End(gsys);
 	GFL_PROC_FreeWork(proc);
 	GFL_HEAP_DeleteHeap( HEAPID_GAMESYS );
 	return GFL_PROC_RES_FINISH;
 }
+//------------------------------------------------------------------
+/**
+ * @brief		プロセス関数テーブル
+ */
+//------------------------------------------------------------------
+const GFL_PROC_DATA GameMainProcData = {
+	GameMainProcInit,
+	GameMainProcMain,
+	GameMainProcEnd,
+};
+
+
 
 //============================================================================================
 //============================================================================================
+enum {
+	PLAYER_MAX = 5,
+};
+//------------------------------------------------------------------
+/**
+ * @brief	ゲーム制御システム用ワーク定義
+ */
+//------------------------------------------------------------------
+struct _GAMESYS_WORK {
+	HEAPID heapID;			///<使用するヒープ指定ID
+	void * parent_work;		///<親（呼び出し元）から引き継いだワークへのポインタ
+
+	GFL_PROCSYS * procsys;	///<使用しているPROCシステムへのポインタ
+	PLAYER_WORK playerWork[PLAYER_MAX];
+};
+
 //------------------------------------------------------------------
 //------------------------------------------------------------------
-static void GameSystem_Init(GAMESYS_WORK * gsys)
+static u32 GameSystem_GetGameSysWorkSize(void)
 {
+	return sizeof(GAMESYS_WORK);
+}
+//------------------------------------------------------------------
+//------------------------------------------------------------------
+static void GameSystem_Init(GAMESYS_WORK * gsys, HEAPID heapID, void * pwk)
+{
+	int i;
+	gsys->heapID = heapID;
+	gsys->parent_work = pwk;
 	gsys->procsys = GFL_PROC_LOCAL_boot(gsys->heapID);
-	GameSystem_CallProc(gsys, NO_OVERLAY_ID, &DebugFieldProcData, gsys);
+
+	for (i = 0; i < PLAYER_MAX; i++) {
+		PLAYERWORK_init(&gsys->playerWork[i]);
+	}
 }
 
 //------------------------------------------------------------------
 //------------------------------------------------------------------
 static BOOL GameSystem_Main(GAMESYS_WORK * gsys)
 {
+	//Game Server Proccess
+	//	PlayerController/Event
 	GFL_PROC_LOCAL_Main(gsys->procsys);
 	return FALSE;
 }
@@ -117,7 +166,7 @@ static void GameSystem_End(GAMESYS_WORK * gsys)
 void GameSystem_SetNextProc(GAMESYS_WORK * gsys,
 		FSOverlayID ov_id, const GFL_PROC_DATA *procdata, void * pwk)
 {
-	GFL_PROC_LOCAL_SetNextProc(GameSysWork->procsys, ov_id, procdata, pwk);
+	GFL_PROC_LOCAL_SetNextProc(gsys->procsys, ov_id, procdata, pwk);
 }
 
 //------------------------------------------------------------------
@@ -125,10 +174,62 @@ void GameSystem_SetNextProc(GAMESYS_WORK * gsys,
 void GameSystem_CallProc(GAMESYS_WORK * gsys,
 		FSOverlayID ov_id, const GFL_PROC_DATA *procdata, void * pwk)
 {
-	GFL_PROC_LOCAL_CallProc(GameSysWork->procsys, ov_id, procdata, pwk);
+	GFL_PROC_LOCAL_CallProc(gsys->procsys, ov_id, procdata, pwk);
 }
 
 
+//============================================================================================
+//============================================================================================
+//------------------------------------------------------------------
+/**
+ * @brief	PLAYER_WORK初期化
+ */
+//------------------------------------------------------------------
+void PLAYERWORK_init(PLAYER_WORK * player)
+{
+	player->zoneID = 0;
+	player->position.x = 0;
+	player->position.y = 0;
+	player->position.z = 0;
+	player->direction = 0;
+	player->mystatus.id = 0;
+	player->mystatus.sex = 0;
+	player->mystatus.region_code = 0;
+}
 
+//------------------------------------------------------------------
+//------------------------------------------------------------------
+void PLAYERWORK_setPosition(PLAYER_WORK * player, const VecFx32 * pos)
+{
+	player->position = *pos;
+}
 
+const VecFx32 * PLAYERWORK_getPosition(const PLAYER_WORK * player)
+{
+	return &player->position;
+}
+
+//------------------------------------------------------------------
+//------------------------------------------------------------------
+void PLAYERWORK_setZoneID(PLAYER_WORK * player, ZONEID zoneid)
+{
+	player->zoneID = zoneid;
+}
+
+ZONEID PLAYERWORK_getZoneID(const PLAYER_WORK * player)
+{
+	return player->zoneID;
+}
+
+//------------------------------------------------------------------
+//------------------------------------------------------------------
+void PLAYERWORK_setDirection(PLAYER_WORK * player, u16 direction)
+{
+	player->direction = direction;
+}
+
+u16 PLAYERWORK_getDirection(const PLAYER_WORK * player)
+{
+	return player->direction;
+}
 
