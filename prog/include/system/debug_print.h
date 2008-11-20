813b002056b2ef166e091f633f734782a214b989
Masao Taya <taya@gamefreak.co.jp>
2008-11-20 08:36:33 +0000

    新規

---
+++ prog/include/system/debug_print.h
@@ -0,0 +1,93 @@
+//===================================================================
+/**
+ * @file	debug_print.h
+ * @brief	特定ユーザの環境でコンパイルした場合のみ動作するPrintf関数
+ * @author	taya
+ * @date	07.02.13
+ */
+//===================================================================
+#ifndef __DEBUG_PRINT_H__
+#define __DEBUG_PRINT_H__
+
+
+typedef enum {
+	AUTHOR_FREE = 0,	///< これを指定すると全環境で出力される
+
+	AUTHOR_WATANABE,
+	AUTHOR_TAMADA,
+	AUTHOR_SOGABE,
+	AUTHOR_OHNO,
+	AUTHOR_MATSUDA,
+	AUTHOR_KAGAYA,
+	AUTHOR_ARIIZUMI,
+	AUTHOR_TAYA,
+
+	AUTHOR_IGNORE,		///< これを指定すると出力されない
+}AUTHOR_ID;
+
+
+#if defined DEBUG_ONLY_FOR_taya
+#define DEFINED_AUTHOR_ID	(AUTHOR_TAYA)
+#elif defined DEBUG_ONLY_FOR_hiro_nakamura
+#define DEFINED_AUTHOR_ID	(AUTHOR_NAKAMURA)
+#elif defined DEBUG_ONLY_FOR_genya_hosaka
+#define DEFINED_AUTHOR_ID	(AUTHOR_HOSAKA)
+#elif defined DEBUG_ONLY_FOR_toru_nagihashi
+#define DEFINED_AUTHOR_ID	(AUTHOR_NAGI)
+#elif defined DEBUG_ONLY_FOR_watanabe
+#define DEFINED_AUTHOR_ID	(AUTHOR_WATANABE)
+#elif defined DEBUG_ONLY_FOR_tamada
+#define DEFINED_AUTHOR_ID	(AUTHOR_TAMADA)
+#elif defined DEBUG_ONLY_FOR_sogabe
+#define DEFINED_AUTHOR_ID	(AUTHOR_SOGABE)
+#elif defined DEBUG_ONLY_FOR_ohno
+#define DEFINED_AUTHOR_ID	(AUTHOR_OHNO)
+#elif defined DEBUG_ONLY_FOR_matsuda
+#define DEFINED_AUTHOR_ID	(AUTHOR_MATSUDA)
+#elif defined DEBUG_ONLY_FOR_kagaya
+#define DEFINED_AUTHOR_ID	(AUTHOR_KAGAYA)
+#elif defined DEBUG_ONLY_FOR_ariizumi_nobuhiko
+#define DEFINED_AUTHOR_ID	(AUTHOR_ARIIZUMI)
+#else
+#define DEFINED_AUTHOR_ID	(AUTHOR_FREE)
+#endif
+
+
+
+#ifdef PM_DEBUG
+
+//==============================================================================================
+/**
+ * 特定ユーザの環境でコンパイルした場合のみ動作するPrintf関数
+ *
+ * @param   author_id		ユーザID（debug_print.hに記述）
+ * @param   fmt				OS_TPrintfに渡す書式付き文字列
+ *
+ */
+//==============================================================================================
+extern void DEBUG_Printf( AUTHOR_ID author_id, const char * fmt, ... );
+
+#define		DEBUG_PrintfEx( flg, id, ... )		{  if(flg){ DEBUG_Printf(id, __VA_ARGS__); } }
+
+#else
+
+#define		DEBUG_Printf( id, ... )				((void)0)
+#define		DEBUG_PrintfEx( flg, id, ... )		((void)0)
+
+#endif
+
+
+//-----------------------------------------------------------
+// かんたん記述用マクロ
+//-----------------------------------------------------------
+#define		TAYA_Printf( ... )		DEBUG_Printf( AUTHOR_TAYA, __VA_ARGS__ )
+#define		WATANABE_Printf( ... )	DEBUG_Printf( AUTHOR_WATANABE, __VA_ARGS__ )
+#define		TAMADA_Printf( ...)		DEBUG_Printf( AUTHOR_TAMADA, __VA_ARGS__ )
+#define		SOGABE_Printf( ...)		DEBUG_Printf( AUTHOR_SOGABE, __VA_ARGS__ )
+#define		OHNO_Printf( ...)		DEBUG_Printf( AUTHOR_OHNO, __VA_ARGS__ )
+#define		MATSUDA_Printf( ...)	DEBUG_Printf( AUTHOR_MATSUDA, __VA_ARGS__ )
+#define		KAGAYA_Printf( ...)		DEBUG_Printf( AUTHOR_KAGAYA, __VA_ARGS__ )
+#define		ARIIZUMI_Printf( ...)	DEBUG_Printf( AUTHOR_ARIIZUMI, __VA_ARGS__ )
+
+
+#endif
---
+++ prog/src/system/debug_print.c
@@ -0,0 +1,38 @@
+//==============================================================================================
+/**
+ * @file	debug_print.c
+ * @date	2007.02.13
+ * @author	taya GAME FREAK inc.
+ */
+//==============================================================================================
+
+#include "debug_print.h"
+
+
+// 一時的に別環境でも出力したい情報がある場合、そのPrintf記述者のIDを定義する
+#define REGULAR_AUTHOR_ID	(AUTHOR_FREE)
+
+
+//==============================================================================================
+/**
+ * 特定ユーザの環境でコンパイルした場合のみ動作するPrintf関数
+ *
+ * @param   author_id		ユーザID（debug_print.hに記述）
+ * @param   fmt				OS_TPrintfに渡す書式付き文字列
+ *
+ */
+//==============================================================================================
+void DEBUG_Printf( AUTHOR_ID author_id, const char * fmt, ... )
+{
+	if(	(author_id==AUTHOR_FREE)
+	||	(author_id==DEFINED_AUTHOR_ID)
+	||	(author_id==REGULAR_AUTHOR_ID)
+	){
+		va_list vlist;
+
+		va_start(vlist, fmt);
+		OS_TVPrintf(fmt, vlist);
+		va_end(vlist);
+	}
+}
+
