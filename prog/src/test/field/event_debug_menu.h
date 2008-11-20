252325b69b626950b842ac2183d89e27e91e7343
Keita Kagaya <kagaya@gamefreak.co.jp>
2008-11-20 04:18:55 +0000

    デバッグメニューソースヘッダー

---
+++ prog/src/test/field/event_debug_menu.h
@@ -0,0 +1,37 @@
+//======================================================================
+/**
+ * @file	event_debug_menu.c
+ * @brief	フィールドデバッグメニューの制御イベント
+ * @author	kagaya
+ * @date	2008.11.13
+ */
+//======================================================================
+#ifndef __FIELD_DEBUG_H__
+#define __FIELD_DEBUG_H__
+
+#include "gamesystem/gamesystem.h"
+#include "gamesystem/game_event.h"
+#include "field_main.h"
+
+//======================================================================
+//	define
+//======================================================================
+
+//======================================================================
+//	struct
+//======================================================================
+//--------------------------------------------------------------
+///	DEBUG_FLDMENU
+//--------------------------------------------------------------
+typedef struct _TAG_DEBUG_FLDMENU DEBUG_FLDMENU;
+
+//======================================================================
+//	extern
+//======================================================================
+extern GMEVENT * DEBUG_EVENT_DebugMenu( GAMESYS_WORK * gsys,
+		FIELD_MAIN_WORK * fieldWork, HEAPID heapID, u16 page_id );
+
+extern void FldDebugMenu_SetCommSystem(
+	void *commSys , DEBUG_FLDMENU *d_menu );
+
+#endif //__FIELD_DEBUG_H__
