52898fb38f48035a284294f280b5a7b3e7219a7b
Sosuke Tamada <tamada@gamefreak.co.jp>
2008-11-21 07:07:49 +0000

    春夏秋冬のマップを追加した

--- resource/test_graphic/fldmapdata/zonetable/zone_id.h
+++ resource/test_graphic/fldmapdata/zonetable/zone_id.h
@@ -1,13 +1,16 @@
 //zone id define table
 #ifndef __ZONE_ID_H__
 #define __ZONE_ID_H__
-#define ZONE_ID_LOOPBRIDGE       (  0)
-#define ZONE_ID_PLANNERTEST      (  1)
-#define ZONE_ID_BRIDGE           (  2)
-#define ZONE_ID_GSSAMPLE         (  3)
-#define ZONE_ID_SAMPLE01         (  4)
-#define ZONE_ID_SAMPLE02         (  5)
-#define ZONE_ID_SAMPLE03         (  6)
-#define ZONE_ID_MAX	(7)
+#define ZONE_ID_MAPSPRING        (  0)
+#define ZONE_ID_MAPSUMMER        (  1)
+#define ZONE_ID_MAPAUTUMN        (  2)
+#define ZONE_ID_MAPWINTER        (  3)
+#define ZONE_ID_LOOPBRIDGE       (  4)
+#define ZONE_ID_PLANNERTEST      (  5)
+#define ZONE_ID_BRIDGE           (  6)
+#define ZONE_ID_GSSAMPLE         (  7)
+#define ZONE_ID_LOOPBRIDGE2      (  8)
+#define ZONE_ID_LOOPBRIDGE3      (  9)
+#define ZONE_ID_MAX	(10)
 #endif // __ZONE_ID_H__
 
