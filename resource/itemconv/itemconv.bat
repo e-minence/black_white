e7149ba505d6ca022c2f5e3112aba60486f514f5
Junichi Masuda <matsuda@gamefreak.co.jp>
2008-12-01 08:54:30 +0000

    アイテム移植中

---
+++ resource/itemconv/itemconv.bat
@@ -0,0 +1,27 @@
+del itemequip.h
+del itemsym.h
+del itemindex.dat
+del itemname.gmm
+del iteminfo.gmm
+del itemdata /q
+del itemtype.dat
+del item_data.naix
+del item_data.narc
+
+md itemdata
+
+itemconv_dp.exe wb_item.csv >itemconv_dp.err
+itemcnv_gmm.exe wb_item.csv >itemcnv_gmm.err
+rem notepad itemconv_dp.err
+rem notepad itemcnv_gmm.err
+
+nnsarc -c -l -n -i item_data itemdata
+del itemdata /q
+
+move itemequip.h %1include/item
+move itemsym.h %1include/item
+move itemindex.dat %1src/item
+move itemname.gmm %2resource/message/src
+move iteminfo.gmm %2resource/message/src
+move itemtype.dat %1src/item
+
