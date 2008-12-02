51bf3b444ced6bcdcf995a3c157062525704d058
Sosuke Tamada <tamada@gamefreak.co.jp>
2008-12-02 14:12:43 +0000

    個人用開始スクリプトのパス追加、オーバーレイ解析結果を更新

--- tools/personal/tamada_prompt.bat
+++ tools/personal/tamada_prompt.bat
@@ -4,5 +4,5 @@ REM		玉田の個人用設定（prompt.batから呼び出される）
 REM
 REM ##################################################
 doskey arc=cd c:\home\pokemon_wb\prog\arc
-PATH=C:\tools\Subversion\bin;%PATH%;c:\tools\mytools
+PATH=C:\tools\Subversion\bin;%PATH%c:\tools\mytools;c:\tools\glo561wb\bin;
 
