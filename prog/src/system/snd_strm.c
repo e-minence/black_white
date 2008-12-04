d2772f3260ce48b28742897137a89abc6888fd65
Katsumi Ohno <ohno@gamefreak.co.jp>
2008-12-04 02:02:42 +0000

    ストリーミング再生用プログラム追加

---
+++ prog/src/system/snd_strm.c
@@ -0,0 +1,347 @@
+//[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[
+/**
+ *	GAME FREAK inc.
+ *
+ *	@file		snd_strm.c
+ *	@brief		サウンド	ストリーミング再生
+ *	@author		tomoya takahashi
+ *	@data		2008.12.02
+ *
+ */
+//]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]
+
+#include "common.h"
+
+#include "system/snd_strm.h"
+
+//-----------------------------------------------------------------------------
+/**
+ *					コーディング規約
+ *		●関数名
+ *				１文字目は大文字それ以降は小文字にする
+ *		●変数名
+ *				・変数共通
+ *						constには c_ を付ける
+ *						staticには s_ を付ける
+ *						ポインタには p_ を付ける
+ *						全て合わさると csp_ となる
+ *				・グローバル変数
+ *						１文字目は大文字
+ *				・関数内変数
+ *						小文字と”＿”と数字を使用する 関数の引数もこれと同じ
+*/
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+/**
+ *					定数宣言
+*/
+//-----------------------------------------------------------------------------
+
+//-------------------------------------
+///	チャンネル
+//=====================================
+#define SND_STRM_CHANNEL_MAX		(1)
+static const u8 sc_SND_STRM_CHANNEL_TBL[ SND_STRM_CHANNEL_MAX ] = {
+	0,
+};
+
+//-------------------------------------
+///	周波数
+//=====================================
+static const int sc_SND_STRM_HZ_TBL[ SND_STRM_HZMAX ] = {
+	NNS_SND_STRM_TIMER_CLOCK/8000,
+	NNS_SND_STRM_TIMER_CLOCK/11025,
+	NNS_SND_STRM_TIMER_CLOCK/22050,
+};
+
+//-------------------------------------
+/// サウンドデータタイプ
+//=====================================
+static const u32 sc_SND_STRM_DATATYPE[ SND_STRM_TYPE_MAX ] = {
+	NNS_SND_STRM_FORMAT_PCM8,
+	NNS_SND_STRM_FORMAT_PCM16,
+};
+
+//-------------------------------------
+///	ストリーミング読み込み１サイズ
+//=====================================
+#define SND_STRM_ONELOAD_DATASIZ	(0)
+
+
+
+//-------------------------------------
+///	その他情報
+//=====================================
+#define	INTERVAL		(16)
+#define	STRM_BUF_SIZE	( INTERVAL * 2 * SND_STRM_CHANNEL_MAX * 32 )
+#define	SWAV_HEAD_SIZE	(18)
+
+//-----------------------------------------------------------------------------
+/**
+ *					構造体宣言
+*/
+//-----------------------------------------------------------------------------
+typedef struct
+{
+	NNSSndStrm			strm;
+	u8					FS_strmBuffer[STRM_BUF_SIZE];
+	int					FSReadPos;
+	u32					strmReadPos;
+	u32					strmWritePos;
+
+	// 音楽情報
+	BOOL				snddata_in;
+	u32					arcid;
+	u32					dataid;
+	FSFile				file;
+}STRM_WORK;
+
+//ワーク
+static STRM_WORK*	sp_STRM_WORK = NULL;
+
+	
+//32バイトアライメントでヒープからの取り方がわからないので、とりあえず静的に
+static	u8				strmBuffer[STRM_BUF_SIZE] ATTRIBUTE_ALIGN(32);
+
+//-----------------------------------------------------------------------------
+/**
+ *					プロトタイプ宣言
+*/
+//-----------------------------------------------------------------------------
+static void SND_STRM_StockWaveData( NNSSndStrmCallbackStatus status, int numChannels, void *buffer[], u32	len, NNSSndStrmFormat format, void *arg );
+static void SND_STRM_CopyBuffer( STRM_WORK* p_wk,int size );
+
+
+//----------------------------------------------------------------------------
+/**
+ *	@brief	ストリーミング再生システム	初期化
+ *	
+ *	@param	heapID		ヒープID
+ */
+//-----------------------------------------------------------------------------
+void SND_STRM_Init( u32 heapID )
+{
+	GF_ASSERT( sp_STRM_WORK == NULL );
+
+	sp_STRM_WORK = GFL_HEAP_AllocMemory( heapID, sizeof(STRM_WORK) );
+	GFL_STD_MemClear( sp_STRM_WORK, sizeof(STRM_WORK) );
+
+	// ストリーミングチャンネル設定
+    NNS_SndInit();
+    NNS_SndStrmInit( &sp_STRM_WORK->strm );
+	NNS_SndStrmAllocChannel( &sp_STRM_WORK->strm, SND_STRM_CHANNEL_MAX, sc_SND_STRM_CHANNEL_TBL);
+}
+
+//----------------------------------------------------------------------------
+/**
+ *	@brief	ストリーミング再生システム	破棄
+ */
+//-----------------------------------------------------------------------------
+void SND_STRM_Exit( void )
+{
+	GF_ASSERT( sp_STRM_WORK );
+
+	if(sp_STRM_WORK){
+		// ストリーミングチャンネル破棄
+		NNS_SndStrmFreeChannel( &sp_STRM_WORK->strm );
+
+		// メモリ破棄
+		GFL_HEAP_FreeMemory( sp_STRM_WORK );
+		sp_STRM_WORK = NULL;
+	}
+}
+
+//----------------------------------------------------------------------------
+/**
+ *	@brief	ストリーミング再生システム	メイン
+ */
+//-----------------------------------------------------------------------------
+void SND_STRM_Main( void )
+{
+	GF_ASSERT( sp_STRM_WORK );
+	
+	if( sp_STRM_WORK->snddata_in ){
+		SND_STRM_CopyBuffer( sp_STRM_WORK, SND_STRM_ONELOAD_DATASIZ );
+	}
+}
+
+
+//----------------------------------------------------------------------------
+/**
+ *	@brief	サウンドデータ	設定
+ *
+ *	@param	arcid		アークID
+ *	@param	dataid		データID
+ *	@param	type		データタイプ
+ *	@param	hz			サンプリング周期
+ */
+//-----------------------------------------------------------------------------
+void SND_STRM_SetUp( u32 arcid, u32 dataid, SND_STRM_TYPE type, SND_STRM_HZ hz )
+{
+	GF_ASSERT( sp_STRM_WORK );
+
+	sp_STRM_WORK->FSReadPos=SWAV_HEAD_SIZE;
+	sp_STRM_WORK->strmReadPos=0;
+	sp_STRM_WORK->strmWritePos=0;
+
+	MI_CpuClearFast( &strmBuffer[0], STRM_BUF_SIZE );
+    
+	NNS_SndStrmSetup( &sp_STRM_WORK->strm,
+					  sc_SND_STRM_DATATYPE[type],
+					  &strmBuffer[0],
+					  STRM_BUF_SIZE,
+					  sc_SND_STRM_HZ_TBL[hz],
+					  INTERVAL,
+					  SND_STRM_StockWaveData,
+					  sp_STRM_WORK);
+
+	sp_STRM_WORK->snddata_in = TRUE;
+	sp_STRM_WORK->arcid		= arcid;
+	sp_STRM_WORK->dataid	= dataid;
+	GFL_ARC_OpenFileTopPosWrite( sp_STRM_WORK->arcid, sp_STRM_WORK->dataid, &sp_STRM_WORK->file );
+
+	
+	SND_STRM_CopyBuffer( sp_STRM_WORK, STRM_BUF_SIZE );
+}
+
+//----------------------------------------------------------------------------
+/**
+ *	@brief	サウンドデータの破棄
+ */
+//-----------------------------------------------------------------------------
+void SND_STRM_Release( void )
+{
+	GF_ASSERT( sp_STRM_WORK );
+	GF_ASSERT( sp_STRM_WORK->snddata_in );
+
+	// 停止
+	NNS_SndStrmStop( &sp_STRM_WORK->strm );
+
+	// 破棄
+	FS_CloseFile(&sp_STRM_WORK->file);
+
+	sp_STRM_WORK->snddata_in = FALSE;
+}
+
+
+//----------------------------------------------------------------------------
+/**
+ *	@brief		サウンドデータの有無を取得
+ *	
+ *	@retval	TRUE	ある
+ *	@retval	FALSE	ない
+ */
+//-----------------------------------------------------------------------------
+BOOL SND_STRM_CheckSetUp( void )
+{
+	if( !sp_STRM_WORK ){
+		return FALSE;
+	}
+	return sp_STRM_WORK->snddata_in;
+
+}
+
+
+//----------------------------------------------------------------------------
+/**
+ *	@brief	再生
+ */
+//-----------------------------------------------------------------------------
+void SND_STRM_Play( void )
+{
+	GF_ASSERT( sp_STRM_WORK );
+	GF_ASSERT( sp_STRM_WORK->snddata_in );
+	NNS_SndStrmStart( &sp_STRM_WORK->strm );
+}
+
+//----------------------------------------------------------------------------
+/**
+ *	@brief	停止
+ */
+//-----------------------------------------------------------------------------
+void SND_STRM_Stop( void )
+{
+	GF_ASSERT( sp_STRM_WORK );
+	GF_ASSERT( sp_STRM_WORK->snddata_in );
+	NNS_SndStrmStop( &sp_STRM_WORK->strm );
+}
+
+
+
+
+
+
+//-----------------------------------------------------------------------------
+/**
+ *			プライベート関数
+ */
+//-----------------------------------------------------------------------------
+//----------------------------------------------------------------------------
+/**
+ *	@brief	波形データを補充するコールバック
+ *
+ *	@param	status
+ *	@param	num_channels
+ *	@param	p_buffer[]
+ *	@param	len
+ *	@param	format
+ *	@param	p_arg 
+ */
+//-----------------------------------------------------------------------------
+static void SND_STRM_StockWaveData( NNSSndStrmCallbackStatus status, int num_channels, void* p_buffer[], u32 len, NNSSndStrmFormat format, void* p_arg )
+{
+	STRM_WORK *sw=(STRM_WORK *)p_arg;
+	int	i;
+	u8	*strBuf;
+
+	strBuf=p_buffer[0];
+	
+	for(i=0;i<len;i++){
+		strBuf[i]=sw->FS_strmBuffer[i+sw->strmReadPos];
+	}
+
+	sw->strmReadPos+=len;
+	if(sw->strmReadPos>=STRM_BUF_SIZE){
+		sw->strmReadPos=0;
+	}
+	DC_FlushRange(strBuf,len);
+}
+
+
+
+
+//----------------------------------------------------------------------------
+/**
+ *	@brief	波形データを補充するコールバックが割り込み禁止で呼ばれるので、この関数でリードしておく
+ *
+ *	@param	p_wk		ワーク
+ *	@param	size		サイズ
+ */
+//-----------------------------------------------------------------------------
+static void SND_STRM_CopyBuffer( STRM_WORK* p_wk,int size )
+{
+	FSFile	file;
+	s32		ret;
+	
+	GF_ASSERT( p_wk->snddata_in );
+
+	if(size){
+		ret=FS_ReadFile(&p_wk->file,&p_wk->FS_strmBuffer[0],size);
+		p_wk->FSReadPos+=size;
+	}
+	else{
+		while(p_wk->strmReadPos!=p_wk->strmWritePos){
+			ret=FS_ReadFile(&p_wk->file,&p_wk->FS_strmBuffer[p_wk->strmWritePos],32);
+			if(ret==0){
+				p_wk->FSReadPos=SWAV_HEAD_SIZE;
+				FS_SeekFile(&p_wk->file,p_wk->FSReadPos,FS_SEEK_SET);
+				continue;
+			}
+			p_wk->FSReadPos+=32;
+			p_wk->strmWritePos+=32;
+			if(p_wk->strmWritePos>=STRM_BUF_SIZE){
+				p_wk->strmWritePos=0;
+			}
+		}
+	}
+}
