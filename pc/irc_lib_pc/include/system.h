ba1c8681d7bd20624f67e5f038b1ab5377cd71f8
Katsumi Ohno <ohno@gamefreak.co.jp>
2008-12-05 03:21:07 +0000

    プレゼン用のPC-赤外線ソフトを完成させた

---
+++ pc/irc_lib_pc/include/system.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+/******************************************************************************
+******************************************************************************/
+#include <tchar.h>
+
+#include "irc_internal.h"
+#include "irc.h"
+#include "com.h"
