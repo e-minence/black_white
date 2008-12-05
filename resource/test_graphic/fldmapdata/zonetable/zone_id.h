4f6f55721c2690ccb2971aa2d2d576cac5486f8c
Sosuke Tamada <tamada@gamefreak.co.jp>
2008-12-05 13:29:08 +0000

    テーブルを並び替えた

--- resource/test_graphic/fldmapdata/zonetable/zone_id.h
+++ resource/test_graphic/fldmapdata/zonetable/zone_id.h
@@ -7,12 +7,12 @@
 #define ZONE_ID_MAPWINTER        (  3)
 #define ZONE_ID_TESTPC           (  4)
 #define ZONE_ID_TESTROOM         (  5)
-#define ZONE_ID_LOOPBRIDGE       (  6)
-#define ZONE_ID_PLANNERTEST      (  7)
-#define ZONE_ID_BRIDGE           (  8)
-#define ZONE_ID_GSSAMPLE         (  9)
-#define ZONE_ID_LOOPBRIDGE2      ( 10)
-#define ZONE_ID_LOOPBRIDGE3      ( 11)
+#define ZONE_ID_PLANNERTEST      (  6)
+#define ZONE_ID_BRIDGE           (  7)
+#define ZONE_ID_LOOPBRIDGE       (  8)
+#define ZONE_ID_LOOPBRIDGE2      (  9)
+#define ZONE_ID_LOOPBRIDGE3      ( 10)
+#define ZONE_ID_GSSAMPLE         ( 11)
 #define ZONE_ID_MAX	(12)
 #endif // __ZONE_ID_H__
 
