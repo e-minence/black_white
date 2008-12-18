3d34622fdb002f8e4b7f20a44fe7b39f5170cd77
Junichi Masuda <matsuda@gamefreak.co.jp>
2008-12-18 06:53:12 +0000

    WIFI広場ミニゲームツールグラフィック環境コミット

---
+++ resource/wifi_lobby_minigame/conv.bat
@@ -0,0 +1,21 @@
+g2dcvtr font_boad.nce -br
+g2dcvtr minigame_win_oam.nce -br
+g2dcvtr result_fusen.nce -br
+g2dcvtr minigame_win_bg0.nsc
+g2dcvtr minigame_win_bg1.nsc
+g2dcvtr minigame_win_bg2.nsc
+g2dcvtr minigame_win.ncl
+g2dcvtr minigame_win_ani.ncl
+g2dcvtr touchpen.nce -br
+g2dcvtr mini_fusen_score.nsc
+
+nnsarc -c -i -n wlmngm_tool -S list.txt
+
+copy *.naix ..\..\pokemon_pl\src\graphic\
+copy *.narc ..\..\pokemon_pl\src\graphic\
+
+del *.NSCR
+del *.NCLR
+del *.NCGR
+del *.NCER
+del *.NANR
