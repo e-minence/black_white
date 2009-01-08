d507045e6c86f72174288f24d5ce9594707c03ae
Junichi Masuda <matsuda@gamefreak.co.jp>
2009-01-08 02:37:39 +0000

    game_start.c追加

---
+++ prog/src/title/game_start.c
@@ -0,0 +1,38 @@
+//==============================================================================
+/**
+ * @file	game_start.c
+ * @brief	「さいしょから」or「つづきから」を選択後の処理
+ * @author	matsuda
+ * @date	2009.01.07(水)
+ */
+//==============================================================================
+#include <gflib.h>
+#include <procsys.h>
+#include "system\main.h"
+#include "gamesystem/gamesystem.h"
+#include "gamesystem/game_init.h"
+#include "title/game_start.h"
+
+
+//--------------------------------------------------------------
+/**
+ * @brief   「最初から始める」を選択
+ */
+//--------------------------------------------------------------
+void GameStart_Beginning(void)
+{
+	GAME_INIT_WORK * init_param = DEBUG_GetGameInitWork(GAMEINIT_MODE_DEBUG, 0);
+	GFL_PROC_SysSetNextProc(NO_OVERLAY_ID, &GameMainProcData, init_param);
+}
+
+//--------------------------------------------------------------
+/**
+ * @brief   「続きから始める」を選択
+ */
+//--------------------------------------------------------------
+void GameStart_Continue(void)
+{
+	GAME_INIT_WORK * init_param = DEBUG_GetGameInitWork(GAMEINIT_MODE_DEBUG, 0);
+	GFL_PROC_SysSetNextProc(NO_OVERLAY_ID, &GameMainProcData, init_param);
+}
+
