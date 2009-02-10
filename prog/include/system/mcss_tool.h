3d698b580eb09358a7c92c2d00719057837d6c95
Hisashi Sogabe <sogabe@gamefreak.co.jp>
2009-02-10 07:43:19 +0000

    MAW構造体生成関数を作成

---
+++ prog/include/system/mcss_tool.h
@@ -0,0 +1,24 @@
+
+//============================================================================================
+/**
+ * @file	mcss_tool.h
+ * @brief	MCSSÉcÅ[Éã
+ * @author	soga
+ * @date	2009.02.10
+ */
+//============================================================================================
+
+#include "system/mcss.h"
+#include "poke_tool/poke_tool.h"
+
+#ifndef __MCSS_TOOL_H_
+#define __MCSS_TOOL_H_
+
+#define	MCSS_DIR_FRONT	( 0 )	//ê≥ñ 
+#define	MCSS_DIR_BACK	( 1 )	//îwñ 
+
+extern	void	MCSS_TOOL_MakeMAWPP( const POKEMON_PARAM *pp, MCSS_ADD_WORK *maw, int dir );
+extern	void	MCSS_TOOL_MakeMAWPPP( const POKEMON_PASO_PARAM *ppp, MCSS_ADD_WORK *maw, int dir );
+extern	void	MCSS_TOOL_MakeMAWParam( int mons_no, int form_no, int sex, int rare, MCSS_ADD_WORK *maw, int dir );
+
+#endif //__MCSS_TOOL_H_
