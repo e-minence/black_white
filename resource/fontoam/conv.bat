475e5b42098f1a6b0fbfcc91bb725b1f96a25114
Junichi Masuda <matsuda@gamefreak.co.jp>
2009-02-25 08:45:47 +0000

    フォントOAMリソース登録

---
+++ resource/fontoam/conv.bat
@@ -0,0 +1,23 @@
+del *.NCLR
+del *.NCGR
+del *.NSCR
+del *.NCER
+del *.NANR
+del *.naix
+del *.narc
+del *.nsbtx
+del *.nsbmd
+del *.nsbca
+
+echo g2dコンバート
+g2dcvtr fontoam_dummy32x16.nce
+
+
+echo narc作成
+nnsarc -c -l -n -i fontoam -S fontoam.lst
+
+
+del *.N??R
+del *.nsbtx
+del *.nsbmd
+del *.nsbca
