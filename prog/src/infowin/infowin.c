2f0b767557fe4ce5f370c6c9bec1514950d25acb
Ariizumi Nobuhiko <ariizumi_nobuhiko@gamefreak.co.jp>
2009-02-26 01:55:45 +0000

    InfoWinに電池アイコン追加

--- prog/src/infowin/infowin.c
+++ prog/src/infowin/infowin.c
@@ -13,7 +13,7 @@
 #include "net/network_define.h"
 
 #include "arc_def.h"
-#include "infobar.naix"
+#include "infowin.naix"
 
 #include "infowin/infowin.h"
 #include "test/ariizumi/ari_debug.h"
@@ -38,41 +38,75 @@ static const u8 INFOWIN_TIME_DRAW_WIDTH = 5;
 static const u8 INFOWIN_AMPM_DRAW_X = 7;
 static const u8 INFOWIN_AMPM_DRAW_WIDTH = 2;
 
-static const u8 INFOWIN_GPFSYNC_DRAW_X = 14;
+static const u8 INFOWIN_GPFSYNC_DRAW_X = 10;
 static const u8 INFOWIN_GPFSYNC_DRAW_WIDTH = 6;
 
-static const u8 INFOWIN_IR_DRAW_X = 20;
-static const u8 INFOWIN_IR_DRAW_WIDTH = 2;
+static const u8 INFOWIN_IR_DRAW_X = 16;
+static const u8 INFOWIN_IR_DRAW_WIDTH = 4;
 
-static const u8 INFOWIN_WIFI_DRAW_X = 22;
+static const u8 INFOWIN_WIFI_DRAW_X = 20;
 static const u8 INFOWIN_WIFI_DRAW_WIDTH = 4;
 
-static const u8 INFOWIN_BEACON_DRAW_X = 26;
+static const u8 INFOWIN_BEACON_DRAW_X = 24;
 static const u8 INFOWIN_BEACON_DRAW_WIDTH = 4;
 
 static const u8 INFOWIN_WMI_DRAW_X = 30;
 static const u8 INFOWIN_WMI_DRAW_WIDTH = 2;
 
+static const u8 INFOWIN_BATTERY_DRAW_X = 28;
+static const u8 INFOWIN_BATTERY_DRAW_WIDTH = 2;
+
+static const u16 INFOWIN_CHR_FLIP_X = 0x400;
+static const u16 INFOWIN_CHR_FLIP_Y = 0x800;
 #define INFOWIN_CALC_NUM_SCR(num,pal) (num+INFOWIN_TIMECHR_START+(pal<<12))
 #define INFOWIN_CHECK_BIT(value,bit) (value&bit?TRUE:FALSE)
+
+static const u16 INFOWIN_COLOR_BLACK = 0x0000;
+static const u16 INFOWIN_COLOR_WHITE = 0x7FFF;
+static const u16 INFOWIN_COLOR_GRAY = 0x318c;
+static const u16 INFOWIN_COLOR_RED = 0x187b;
+static const u16 INFOWIN_COLOR_YERROW = 0x1e7f;
+static const u16 INFOWIN_COLOR_GREEN = 0x0667;
 //======================================================================
 //	enum
 //======================================================================
 enum
 {
-	INFOWIN_REFRESH_TIME		= 0x01,
-	INFOWIN_REFRESH_GPF_SYNC	= 0x02,
-	INFOWIN_REFRESH_IR			= 0x04,
-	INFOWIN_REFRESH_WIFI		= 0x08,
-	INFOWIN_REFRESH_BEACON		= 0x10,
+	INFOWIN_REFRESH_TIME		= 0x0001,
+	INFOWIN_REFRESH_GPF_SYNC	= 0x0002,
+	INFOWIN_REFRESH_IR			= 0x0004,
+	INFOWIN_REFRESH_WIFI		= 0x0008,
+	INFOWIN_REFRESH_BEACON		= 0x0010,
+	INFOWIN_REFRESH_BATTERY		= 0x0020,
 
-	INFOWIN_DISP_GPF_SYNC	= 0x20,
-	INFOWIN_DISP_IR			= 0x40,
-	INFOWIN_DISP_BEACON		= 0x80,
+	INFOWIN_DISP_GPF_SYNC	= 0x0100,
+	INFOWIN_DISP_IR			= 0x0200,
+	INFOWIN_DISP_BEACON		= 0x0400,
 	
-	INFOWIN_REFRESH_MASK = 0x1F,
-	INFOWIN_DISP_MASK = 0xE0,
-	INFOWIN_FLG_NULL = 0x00,
+	INFOWIN_REFRESH_MASK = 0x003F,
+	INFOWIN_DISP_MASK = 0x0700,
+	INFOWIN_FLG_NULL = 0x0000,
+};
+
+//パレット番号定義
+enum
+{
+	INFOWIN_PLT_NULL,
+	INFOWIN_PLT_COMMON_BLACK,
+	INFOWIN_PLT_COMMON_WHITE,
+	INFOWIN_PLT_TIME_STR,
+	INFOWIN_PLT_GPF_SYNC_BACK,
+	INFOWIN_PLT_GPF_SYNC_STR,
+	INFOWIN_PLT_IR_BACK,
+	INFOWIN_PLT_IR_STR,
+	INFOWIN_PLT_WIFI_BACK,
+	INFOWIN_PLT_WIFI_STR,
+	INFOWIN_PLT_BEACON_BACK,
+	INFOWIN_PLT_BEACON_STR,
+	INFOWIN_PLT_BATTERY_BACK,
+	INFOWIN_PLT_BATTERY_STR,
+	INFOWIN_PLT_NOTUSE1,
+	INFOWIN_PLT_NOTUSE2,
 };
 
 
@@ -103,7 +137,7 @@ typedef enum
 typedef struct
 {
 	u32	ncgPos;
-	u8	isRefresh;
+	u16	isRefresh;
 	u8	bgplane;
 	u8	pltNo;
 	
@@ -112,10 +146,14 @@ typedef struct
 	u8		hour;
 	BOOL	isDispColon;
 	BOOL	isPm;
+	u8		batteryCnt;
 	
 	//Wi-Fi
 	INFOWIN_WIFI_STATE	wifiState;
 	
+	//BATTERY
+	u8	batteryVol;		//0~3
+
 	GFL_TCB *vblankFunc;
 }INFOWIN_WORK;
 
@@ -125,6 +163,7 @@ typedef struct
 
 static	void	INFOWIN_VBlankFunc( GFL_TCB* tcb , void* work );
 static	void	INFOWIN_SetScrFunc( const u16 topChar , const u8 pltNo , const u8 posX , const u8 posY , const u8 sizeX , const u8 sizeY );
+static	void	INFOWIN_SetPltFunc( const u8 pltPos , const u16 *col , const u8 num );
 static	void	INFOWIN_InitBg( u8 bgplane , u8 pltNo, HEAPID heapId );
 static	void	INFOWIN_UpdateTime(void);
 
@@ -153,6 +192,7 @@ void	INFOWIN_Init( u8 bgplane , u8 pltNo ,HEAPID heapId )
 	infoWk->vblankFunc = GFUser_VIntr_CreateTCB( INFOWIN_VBlankFunc , (void*)infoWk , 1 );
 
 	infoWk->wifiState = IWS_DISABLE;
+	infoWk->batteryVol = 3;
 /*
 	//ダミー用通信アイコン
 	INFOWIN_SetScrFunc( 0x6e,infoWk->pltNo,
@@ -167,6 +207,23 @@ void	INFOWIN_Init( u8 bgplane , u8 pltNo ,HEAPID heapId )
 void	INFOWIN_Update( void )
 {
 	INFOWIN_UpdateTime();
+	//バッテリー
+	infoWk->batteryCnt++;
+	if(	infoWk->batteryCnt > 200 )
+
+	{
+		PMBattery buf;
+		if( PM_GetBattery(&buf) == PM_RESULT_SUCCESS )
+		{
+			const u8 nowVol = (buf==PM_BATTERY_HIGH?3:0);
+			if( infoWk->batteryVol != nowVol )
+			{
+				infoWk->batteryVol = nowVol;
+				infoWk->batteryVol |= INFOWIN_REFRESH_BATTERY;
+			}
+			infoWk->batteryCnt = 0;
+		}
+	}
 	if( INFOWIN_IsStartComm() == TRUE )
 	{
 		INFOWIN_UpdateSearchBeacon();
@@ -231,11 +288,18 @@ static	void	INFOWIN_VBlankFunc( GFL_TCB* tcb , void* work )
 	
 	if( INFOWIN_CHECK_BIT(infoWk->isRefresh,INFOWIN_REFRESH_GPF_SYNC) )
 	{
-		static const u16 GPFSYNC_TOP_CHAR[2] = {0x20,0x40};
+		static const u16 GPFSYNC_TOP_CHAR[2] = {0x20,0x20};
 		const u8	flg = INFOWIN_CHECK_BIT(infoWk->isRefresh,INFOWIN_DISP_GPF_SYNC);
 		INFOWIN_SetScrFunc( GPFSYNC_TOP_CHAR[flg],infoWk->pltNo,
 							INFOWIN_GPFSYNC_DRAW_X,0,
 							INFOWIN_GPFSYNC_DRAW_WIDTH,2 );
+		{//Pallet
+			static const u16 GPFSYNC_BACK_COLOR[2][2] = {
+						{INFOWIN_COLOR_GRAY,INFOWIN_COLOR_BLACK},
+						{INFOWIN_COLOR_WHITE,INFOWIN_COLOR_BLACK} };
+						
+			INFOWIN_SetPltFunc( INFOWIN_PLT_GPF_SYNC_BACK , GPFSYNC_BACK_COLOR[flg] , 2 );
+		}
 		
 		infoWk->isRefresh -= INFOWIN_REFRESH_GPF_SYNC;
 		transReq = TRUE;
@@ -244,12 +308,19 @@ static	void	INFOWIN_VBlankFunc( GFL_TCB* tcb , void* work )
 	
 	if( INFOWIN_CHECK_BIT(infoWk->isRefresh,INFOWIN_REFRESH_IR) )
 	{
-		static const u16 IR_TOP_CHAR[2] = {0x26,0x46};
+		static const u16 IR_TOP_CHAR[2] = {0x48,0x48};
 		const u8	flg = INFOWIN_CHECK_BIT(infoWk->isRefresh,INFOWIN_DISP_IR);
 		INFOWIN_SetScrFunc( IR_TOP_CHAR[flg],infoWk->pltNo,
 							INFOWIN_IR_DRAW_X,0,
 							INFOWIN_IR_DRAW_WIDTH,2 );
 		
+		{//Pallet
+			static const u16 IR_BACK_COLOR[2][2] = {
+						{INFOWIN_COLOR_GRAY,INFOWIN_COLOR_BLACK},
+						{INFOWIN_COLOR_WHITE,INFOWIN_COLOR_BLACK} };
+						
+			INFOWIN_SetPltFunc( INFOWIN_PLT_IR_BACK , IR_BACK_COLOR[flg] , 2 );
+		}
 		infoWk->isRefresh -= INFOWIN_REFRESH_IR;
 		transReq = TRUE;
 		
@@ -257,11 +328,21 @@ static	void	INFOWIN_VBlankFunc( GFL_TCB* tcb , void* work )
 	
 	if( INFOWIN_CHECK_BIT(infoWk->isRefresh,INFOWIN_REFRESH_WIFI) )
 	{
-		static const u16 WIFI_TOP_CHAR[IWS_MAX] = {0x28,0x48,0x60,0x64,0x68};
+		static const u16 WIFI_TOP_CHAR[IWS_MAX] = {0x28,0x28,0x28,0x28,0x28};
 		INFOWIN_SetScrFunc( WIFI_TOP_CHAR[infoWk->wifiState],infoWk->pltNo,
 							INFOWIN_WIFI_DRAW_X,0,
 							INFOWIN_WIFI_DRAW_WIDTH,2 );
 		
+		{//Pallet
+			static const u16 WIFI_BACK_COLOR[IWS_MAX][2] = {
+						{INFOWIN_COLOR_GRAY,INFOWIN_COLOR_BLACK},
+						{INFOWIN_COLOR_WHITE,INFOWIN_COLOR_BLACK},
+						{INFOWIN_COLOR_RED,INFOWIN_COLOR_BLACK},
+						{INFOWIN_COLOR_YERROW,INFOWIN_COLOR_BLACK},
+						{INFOWIN_COLOR_GREEN,INFOWIN_COLOR_BLACK} };
+						
+			INFOWIN_SetPltFunc( INFOWIN_PLT_WIFI_BACK , WIFI_BACK_COLOR[infoWk->wifiState] , 2 );
+		}
 		infoWk->isRefresh -= INFOWIN_REFRESH_WIFI;
 		transReq = TRUE;
 		
@@ -269,13 +350,48 @@ static	void	INFOWIN_VBlankFunc( GFL_TCB* tcb , void* work )
 	
 	if( INFOWIN_CHECK_BIT(infoWk->isRefresh,INFOWIN_REFRESH_BEACON) )
 	{
-		static const u16 BEACON_TOP_CHAR[2] = {0x2C,0x4C};
+		static const u16 BEACON_TOP_CHAR[2] = {0x2C,0x2C};
 		const u8	flg = INFOWIN_CHECK_BIT(infoWk->isRefresh,INFOWIN_DISP_BEACON);
 		INFOWIN_SetScrFunc( BEACON_TOP_CHAR[flg],infoWk->pltNo,
 							INFOWIN_BEACON_DRAW_X,0,
 							INFOWIN_BEACON_DRAW_WIDTH,2 );
 		
+		{//Pallet
+			static const u16 BEACON_BACK_COLOR[2][2] = {
+						{INFOWIN_COLOR_GRAY,INFOWIN_COLOR_BLACK},
+						{INFOWIN_COLOR_WHITE,INFOWIN_COLOR_BLACK} };
+						
+			INFOWIN_SetPltFunc( INFOWIN_PLT_BEACON_BACK , BEACON_BACK_COLOR[flg] , 2 );
+		}
 		infoWk->isRefresh -= INFOWIN_REFRESH_BEACON;
+		transReq = TRUE;
+		
+	}
+	
+	if( INFOWIN_CHECK_BIT(infoWk->isRefresh,INFOWIN_REFRESH_BATTERY) )
+	{
+		static const u16 BATTERY_TOP_CHAR[4] = {0x40,0x41,0x42,0x43};
+		const u8	flg = INFOWIN_CHECK_BIT(infoWk->isRefresh,INFOWIN_DISP_BEACON);
+		u16	scrData[2][2];
+		scrData[0][0] = BATTERY_TOP_CHAR[infoWk->batteryVol] + (infoWk->pltNo<<12) + infoWk->ncgPos;
+		scrData[1][0] = scrData[0][0] + INFOWIN_LINE_CHRNUM;
+		scrData[0][1] = scrData[0][0] + INFOWIN_CHR_FLIP_X;
+		scrData[1][1] = scrData[1][0] + INFOWIN_CHR_FLIP_X;
+		
+		GFL_BG_WriteScreenExpand( infoWk->bgplane , 
+							INFOWIN_BATTERY_DRAW_X , 0, 
+							INFOWIN_BATTERY_DRAW_WIDTH , INFOWIN_HEIGHT ,
+							scrData,0,0,INFOWIN_BATTERY_DRAW_WIDTH,INFOWIN_HEIGHT);
+		{//Pallet
+			static const u16 BEACON_BACK_COLOR[4][2] = {
+						{INFOWIN_COLOR_RED,INFOWIN_COLOR_WHITE},
+						{INFOWIN_COLOR_BLACK,INFOWIN_COLOR_RED},
+						{INFOWIN_COLOR_BLACK,INFOWIN_COLOR_WHITE},
+						{INFOWIN_COLOR_BLACK,INFOWIN_COLOR_WHITE}};
+						
+			INFOWIN_SetPltFunc( INFOWIN_PLT_BATTERY_BACK , BEACON_BACK_COLOR[infoWk->batteryVol] , 2 );
+		}
+		infoWk->isRefresh -= INFOWIN_REFRESH_BATTERY;
 		transReq = TRUE;
 		
 	}
@@ -304,6 +420,12 @@ static	void	INFOWIN_SetScrFunc( const u16 topChar , const u8 pltNo , const u8 po
 						scrData,0,0,sizeX,sizeY);
 }
 
+static	void	INFOWIN_SetPltFunc( const u8 pltPos , const u16 *col , const u8 num )
+{
+	GFL_BG_LoadPalette( infoWk->bgplane , (void*)col , sizeof(u16)*num , sizeof(u16)*(infoWk->pltNo*16 + pltPos) );
+}
+
+
 static	void	INFOWIN_InitBg( u8 bgplane , u8 pltNo, HEAPID heapId )
 {
 	const PALTYPE palType = (bgplane<=GFL_BG_FRAME3_M ? PALTYPE_MAIN_BG : PALTYPE_SUB_BG );
@@ -314,8 +436,8 @@ static	void	INFOWIN_InitBg( u8 bgplane , u8 pltNo, HEAPID heapId )
 	GF_ASSERT_MSG( infoWk->ncgPos != AREAMAN_POS_NOTFOUND ,"Infobar生成に必要なキャラＶＲＡＭ領域が足りない\n" );
 
 	//データの読み込み
-	GFL_ARCHDL_UTIL_TransVramPalette( arcHdl,NARC_infobar_infobar_NCLR,palType,pltNo*2*0x10,2*0x10,heapId );
-	GFL_ARCHDL_UTIL_TransVramBgCharacter( arcHdl,NARC_infobar_infobar_NCGR,bgplane,infoWk->ncgPos,0,FALSE,heapId );
+	GFL_ARCHDL_UTIL_TransVramPalette( arcHdl,NARC_infowin_infobar_NCLR,palType,pltNo*2*0x10,2*0x10,heapId );
+	GFL_ARCHDL_UTIL_TransVramBgCharacter( arcHdl,NARC_infowin_infobar_NCGR,bgplane,infoWk->ncgPos,0,FALSE,heapId );
 	GFL_ARC_CloseDataHandle(arcHdl);
 	
 	//初期状態BGの設定
