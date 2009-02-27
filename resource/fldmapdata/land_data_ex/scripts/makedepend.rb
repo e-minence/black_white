bac0ccd3227ddae8e029c574e6088a8c67609f06
Sosuke Tamada <tamada@gamefreak.co.jp>
2009-02-27 06:21:47 +0000

    resource/test3dp2が内包していたデータを取り込んだ。 画面出力を抑止したり、make clean時の削除対象を増やしたり環境を修正した。

--- resource/fldmapdata/land_data_ex/scripts/makedepend.rb
+++ resource/fldmapdata/land_data_ex/scripts/makedepend.rb
@@ -17,30 +17,30 @@ File.open(ARGV[0]){|file|
 		if name == '#END' then break end
 				#データ連結ルール
 		dependfile.puts "#{OUTDIR}/#{name}.#{TARGETEXT}: #{OUTDIR}/#{name}.nsbmd #{OUTDIR}/#{name}.bhc #{SRCDIR}/#{name}.3dmd"
-		dependfile.puts "	$(BINLINKER) $*.nsbmd $*.bhc #{OUTDIR}/#{name}.3dmd $*.#{TARGETEXT} BR"
+		dependfile.puts "	@$(BINLINKER) $*.nsbmd $*.bhc #{OUTDIR}/#{name}.3dmd $*.#{TARGETEXT} BR"
 		dependfile.puts ""
 				#imd-->nsbmd生成ルール
 		dependfile.puts "#{OUTDIR}/#{name}.nsbmd: #{SRCDIR}/#{name}.imd"
-		dependfile.puts "	g3dcvtr #{SRCDIR}/#{name}.imd -emdl -o #{OUTDIR}/#{name}.nsbmd"
+		dependfile.puts "	@g3dcvtr #{SRCDIR}/#{name}.imd -emdl -o #{OUTDIR}/#{name}.nsbmd"
 		dependfile.puts ""
 				#ダミー高さデータ生成ルール
 		dependfile.puts "#{SRCDIR}/#{name}h.imd:"
 		dependfile.puts "	@echo #{name}.imdが存在しないためダミーデータを生成します"
-		dependfile.puts "	cp dummy_file/dummy_h.imd #{SRCDIR}/#{name}h.imd"
+		dependfile.puts "	@cp dummy_file/dummy_h.imd #{SRCDIR}/#{name}h.imd"
 		dependfile.puts ""
 				#高さデータコンバートルール
 		dependfile.puts "#{OUTDIR}/#{name}.bhc: #{SRCDIR}/#{name}h.imd"
 		dependfile.puts "	@echo #{SRCDIR}/#{name}h > heightlist.lst"
 		dependfile.puts "	@echo \\\#END >> heightlist.lst"
-		dependfile.puts "	-$(HEIGHTCNV) heightlist.lst < $(HEIGHTRETFILE)"
-		dependfile.puts "	ruby scripts/pad4byte.rb #{SRCDIR}/#{name}h.bhc #{OUTDIR}/#{name}.bhc"
-		dependfile.puts "	rm #{SRCDIR}/#{name}h.bhc"
+		dependfile.puts "	-@$(HEIGHTCNV) heightlist.lst < $(HEIGHTRETFILE) > convresult"
+		dependfile.puts "	@ruby scripts/pad4byte.rb #{SRCDIR}/#{name}h.bhc #{OUTDIR}/#{name}.bhc"
+		dependfile.puts "	@rm #{SRCDIR}/#{name}h.bhc"
 		#dependfile.puts "	mv #{SRCDIR}/#{name}.bhc ./#{OUTDIR}"
 		dependfile.puts ""
 				#配置データ生成ルール（今はダミー）
 		dependfile.puts "#{SRCDIR}/#{name}.3dmd:"
 		dependfile.puts "	@echo #{name}.3dmdが存在しないためダミーデータを生成します"
-		dependfile.puts "	cp dummy_file/dummy.3dmd #{SRCDIR}/#{name}.3dmd"
+		dependfile.puts "	@cp dummy_file/dummy.3dmd #{SRCDIR}/#{name}.3dmd"
 
 		depends += "	#{OUTDIR}/#{name}.#{TARGETEXT} \\\n"
 
