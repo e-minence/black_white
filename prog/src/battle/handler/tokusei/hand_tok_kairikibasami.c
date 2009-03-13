c73b33072ebcb40537af281b79c7746fc9509814
Masao Taya <taya@gamefreak.co.jp>
2009-03-13 10:35:48 +0000

    かいりきバサミの発動メッセージを差し替え

--- prog/src/battle/handler/tokusei/hand_tok_kairikibasami.c
+++ prog/src/battle/handler/tokusei/hand_tok_kairikibasami.c
@@ -43,7 +43,7 @@ static void handler_BeforeRankDown( BTL_EVENT_FACTOR* myHandle, BTL_SVFLOW_WORK*
 
 		BTL_EVENTVAR_SetValue( BTL_EVAR_FAIL_FLAG, TRUE );
 		BTL_SERVER_RECEPT_TokuseiWinIn( flowWk, myPos );
-		BTL_SERVER_RECTPT_SetMessage( flowWk, BTL_STRID_SET_RankdownFail, pokeID );
+		BTL_SERVER_RECTPT_SetMessage( flowWk, BTL_STRID_SET_RankdownFail_ATK, pokeID );
 		BTL_SERVER_RECEPT_TokuseiWinOut( flowWk, myPos );
 	}
 }
