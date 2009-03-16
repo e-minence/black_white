28c9d84b053675d647eac3a4c38812e3cc2ce95c
Hisashi Sogabe <sogabe@gamefreak.co.jp>
2009-03-16 08:55:03 +0000

    パーティクルデファインヘッダー追加

---
+++ prog/arc/spa_def.h
@@ -0,0 +1,13 @@
+#ifndef	_SPA_H_
+#define	_SPA_H_
+
+#define	DUMMY_SPA					(0)
+#define	SWORD_ROLL_SPA				(1)
+#define	TORNADO_SPA					(2)
+#define	W_014_SPA					(3)
+#define	W_055_SPA					(4)
+#define	W_157_SPA					(5)
+#define	W_467_SPA					(6)
+#define	WAZA_126_SPA				(7)
+
+#endif
