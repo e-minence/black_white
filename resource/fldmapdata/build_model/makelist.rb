7f09fd40296d8542a3f2cdf8dc3feb9384a8dce7
Sosuke Tamada <tamada@gamefreak.co.jp>
2009-04-26 12:00:55 +0000

    現在のbuildmodel_outdoor.xlsを元に配置モデルデータアーカイブを生成するように修正

--- resource/fldmapdata/build_model/makelist.rb
+++ resource/fldmapdata/build_model/makelist.rb
@@ -1,3 +1,5 @@
+require "fileutils"
+
 INPUT_FILE	=	ARGV[0]
 DEP_FILE	=	ARGV[1]
 ARC_FILE	=	ARGV[2]
@@ -8,15 +10,26 @@ arc_file = File.open(ARC_FILE, "w")
 
 dep_file.puts "DEPEND_FILES	=	\\\n"
 
+check_list = Array.new
+
 input_file.gets
 
 while line = input_file.gets
 	column = line.split
-	name = "#{column[1]}.nsbmd"
-	dep_file.puts "\t$(RESDIR)/#{name} \\\n"
-	arc_file.puts "\"nsbmdfiles/#{name}\""
+	if column.length < 2 then break end
+	nsbmd_name = "#{column[1]}.nsbmd"
+	dep_file.puts "\t$(RESDIR)/#{nsbmd_name} \\\n"
+	arc_file.puts "\"nsbmdfiles/#{nsbmd_name}\""
+	imd_name = "imdfiles/#{column[1]}.imd"
+	check_list << imd_name unless FileTest.exists? imd_name
 end
 
+dep_file.printf("\n")
+check_list.each{|item|
+	puts "\t@echo #{item}が存在しないため、ダミーファイルをコピーします"
+	FileUtils.cp "imdfiles/dummy_buildmodel.imd",item
+}
 input_file.close
 dep_file.close
 arc_file.close
+puts "#{check_list}"
