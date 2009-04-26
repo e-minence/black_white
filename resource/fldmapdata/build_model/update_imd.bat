de4d366171d5856d5a4789340969a2dc3733da2a
Sosuke Tamada <tamada@gamefreak.co.jp>
2009-04-26 12:02:39 +0000

    作業用バッチファイルを追加

---
+++ resource/fldmapdata/build_model/update_imd.bat
@@ -0,0 +1,2 @@
+cp c:/home/wb_data3d/build_model/outdoor/*/*.imd imdfiles
+cp c:/home/wb_data3d/build_model/indoor/*/*.imd imdfiles
---
+++ resource/fldmapdata/build_model/update_list.bat
@@ -0,0 +1,2 @@
+copy c:\home\wb_data3d\list\buildmodel_outdoor.xls .
+copy c:\home\wb_data3d\list\buildmodel_indoor.xls .
