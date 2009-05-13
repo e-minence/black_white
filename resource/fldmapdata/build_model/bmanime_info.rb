8f7d495e30e4acd05953790b77f14b37928340e0
Sosuke Tamada <tamada@gamefreak.co.jp>
2009-05-13 10:50:23 +0000

    配置モデルアニメコンバート環境とりあえず完成

---
+++ resource/fldmapdata/build_model/bmanime_info.rb
@@ -0,0 +1,158 @@
+#------------------------------------------------------------------------------
+#
+#   配置モデルアニメデータコンバート用スクリプト
+#
+#   2009.05.11  tamada
+#
+#------------------------------------------------------------------------------
+
+INPUT_FILE = ARGV[0]
+OUTPUT_FILE = ARGV[1]
+OUTPUT_HEADER = OUTPUT_FILE.sub(/\.bin/,"\.h")
+
+lib_path_naixread = ENV["PROJECT_ROOT"] + "tools/naixread"
+require lib_path_naixread
+
+class InputFileError < Exception; end
+
+#------------------------------------------------------------------------------
+#
+#   配置モデルアニメデータクラス
+#
+#------------------------------------------------------------------------------
+class BmAnime
+
+  @@MAX_FILE = 4
+
+  @@COL_SYMBOL  = 1
+  @@COL_ANMTYPE = 2
+  @@COL_PROGTYPE = 3
+  @@COL_ANMCOUNT = 4
+  @@COL_SETCOUNT = 5
+  @@COL_FILES = 6
+
+  #アニメタイプ指定(ならびに注意！！）
+  @@anime_type = ["NO_ANIME", "ETERNAL", "EVENT"]
+
+  #拡張子変換テーブル
+  @@ext_tbl = {
+    ".ita" => ".nsbta",
+    ".itp" => ".nsbtp",
+    ".ica" => ".nsbca",
+    ".iva" => ".nsbva",
+    ".ima" => ".nsbma"
+  }
+
+	attr :symbol
+	attr :anm_type
+  attr :prog_type
+  attr :anime_count
+  attr :set_count
+  attr :files
+
+  #初期化
+	def initialize(line)
+    column = line.split(",")
+
+    @symbol = column[@@COL_SYMBOL]
+
+    @anm_type = @@anime_type.index(column[@@COL_ANMTYPE])
+    if @anm_type == nil
+      raise InputFileError, "アニメID #{@symbol} のアニメタイプ #{column[@COL_ANMTYPE]} は非対応です"
+    end
+
+    @prog_type = Integer(column[@@COL_PROGTYPE])
+
+    @anime_count = Integer(column[@@COL_ANMCOUNT])
+
+    @set_count = Integer(column[@@COL_SETCOUNT])
+
+    @files = column[@@COL_FILES .. (@@COL_FILES + @@MAX_FILE - 1)].find_all{|item|item != ""}.map{|item|
+      ext = File.extname(item)
+      unless @@ext_tbl.has_key?(ext) then
+        raise InputFileError, "#{item} の拡張子 #{ext} には非対応です"
+      end
+      item.sub(ext, @@ext_tbl[ext])   #拡張子をコンバート後のものに変換
+    }
+	end
+
+
+  #以下はクラスメソッド定義
+  #------------------------------------------------------------------------------
+
+  #アニメ指定ファイルの最大数
+  def BmAnime.MAX_FILE
+    @@MAX_FILE
+  end
+
+  #ファイルから読み込んでBmAnimeの配列を返す
+  def BmAnime.readFile(filename)
+    bmarray = Array.new
+    names = Hash.new
+
+    # ファイルから読み込み
+    File.open(filename){|file|
+      file.gets #1行目削除
+      file.each{|line|
+        anm = BmAnime.new(line.chomp!)
+        if names.has_key?(anm.symbol) then
+          raise InputFileError, "アニメID #{anm.symbol}が2回定義されています"
+        else
+          bmarray << anm
+        end
+      }
+    }
+    return bmarray
+  end
+
+end
+
+
+#------------------------------------------------------------------------------
+#
+#     メイン処理
+#
+#------------------------------------------------------------------------------
+begin
+  #アニメーションファイルインデックス読み込み
+  anime_naix = NAIXReader.read("anime_files/buildmodel_anime.naix")
+
+  #配置モデルアニメーションテーブル読み込み
+  bmarray = BmAnime.readFile(INPUT_FILE)
+
+  #参照用ファイル生成：シンボルを定義順で出力
+  File.open(OUTPUT_HEADER,"w"){|file|
+    bmarray.each_index{|index|
+      file.printf("%-20s %3d\n", bmarray[index].symbol, index)
+    }
+  }
+
+  #バイナリファイル生成：出力データ作成
+  str = ""
+  bmarray.each{|anm|
+    #アニメタイプ、動作指定、アニメカウント、セットカウント
+    arr = [anm.anm_type, anm.prog_type, anm.anime_count, anm.set_count]
+    #アニメ指定ファイル
+    anm.files.each{|file|
+      index = anime_naix.getIndex(file.sub(/\./,"_"))
+      if index == nil then
+          raise InputFileError, "#{anm.symbol} のファイル #{file} が存在しません"
+      else
+        arr << index
+      end
+    }
+    #アニメ指定ファイルが足りない部分をダミーで追加
+    (BmAnime.MAX_FILE - anm.files.length).times do |i|
+      arr << 0xffff
+    end
+  
+    str += arr.pack("C C C C S4")
+  }
+  #バイナリファイル生成：出力
+  File.open(OUTPUT_FILE,"wb"){|file| file.write(str)}
+
+end
+
+
+
+
--- resource/fldmapdata/build_model/makelist.rb
+++ resource/fldmapdata/build_model/makelist.rb
@@ -1,22 +1,66 @@
 #------------------------------------------------------------------------------
 #
+# 配置モデルアーカイブ生成
 #		
 #
 #------------------------------------------------------------------------------
+KCODE = "SJIS"
+
 require "fileutils"
 
+class AnimeIndexError < Exception; end
+
+ANIME_INDEX_FILE  ="wkdir/buildmodel_anime_info.h"
+#------------------------------------------------------------------------------
+# 各配置モデルごとのアニメ指定IDのテーブルを生成する
+# IN: outputfile  出力先オブジェクト
+#     arr         配置モデルごとのアニメ指定ID（配置モデルと同じ並び順であることを想定）
+#------------------------------------------------------------------------------
+def makeAnimeIndex(outputfile, arr)
+  anm_dic = Array.new
+  File.open(ANIME_INDEX_FILE, "r"){|file|
+    file.each{|line|
+      if line =~/^#/ then
+        next
+      end
+      column = line.split
+      anm_dic << column[0].upcase
+    }
+  }
+  #p anm_dic
+  arr.each{|id|
+    index = anm_dic.index(id.upcase)
+    if id == "×" then
+      index = 0xffff
+    elsif index == nil then
+      raise AnimeIndexError, "アニメ指定 #{id} が見つかりませんでした"
+    end
+    outputfile.write [index].pack("S")
+  }
+end
+
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+#入力ファイル（Excelファイルをタブ区切りテキストに変換したもの）
 INPUT_FILE	=	ARGV[0]
+#依存ファイルを定義するMakefile変数のシンボル名
 DEP_SYMBOL	= ARGV[1]
+#依存ファイル定義を記述するファイルの名前
 DEP_FILE	=	ARGV[2]
+#アーカイブ対象ファイルを記述するファイルの名前
 ARC_FILE	=	ARGV[3]
+
+BIN_FILE  = ARGV[4]
 
 input_file = File.open(INPUT_FILE, "r")
 dep_file = File.open(DEP_FILE, "w")
 arc_file = File.open(ARC_FILE, "w")
+bin_file = File.open(BIN_FILE, "wb")
 
 dep_file.puts "#{DEP_SYMBOL}	=	\\\n"
 
 check_list = Array.new
+animes = Array.new
 
 input_file.gets
 
@@ -28,7 +72,10 @@ while line = input_file.gets
 	arc_file.puts "\"nsbmdfiles/#{nsbmd_name}\""
 	imd_name = "imdfiles/#{column[1]}.imd"
 	check_list << imd_name unless FileTest.exists? imd_name
+  animes << column[8]
 end
+
+makeAnimeIndex(bin_file, animes)
 
 dep_file.printf("\n")
 check_list.each{|item|
@@ -38,3 +85,5 @@ check_list.each{|item|
 input_file.close
 dep_file.close
 arc_file.close
+bin_file.close
+
