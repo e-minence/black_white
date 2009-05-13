f59db7af56830f44e3fbdb1534f3c1fc72c12450
Sosuke Tamada <tamada@gamefreak.co.jp>
2009-05-13 02:37:55 +0000

    チェック用の表示部分をフックした

--- resource/fldmapdata/build_model/makeindex.rb
+++ resource/fldmapdata/build_model/makeindex.rb
@@ -78,7 +78,7 @@ arc_list = File.open("#{OUTPUT_DIR}/#{OUTPUT_SYM}.list","w")
 book.each_index{|sheet_index|
 	filename = sprintf("%s/%s%03d.bin", OUTPUT_DIR, OUTPUT_SYM, sheet_index+1)
 	arc_list.puts "\"#{filename}\""
-	puts "#{filename}"
+	#puts "#{filename}"
 	File.open(filename , "wb"){|outfile|
 		entrys = Hash.new
 		sheet = book[sheet_index]
@@ -92,7 +92,7 @@ book.each_index{|sheet_index|
 			bmodel = bmodel_list[index]
 			if bmodel.flag == true || entrys.has_key?(bmodel.name) then
 				outfile.write([index].pack("S"))
-				printf("%3d %3d %-5s %-20s\n", count, index, bmodel.flag, bmodel.name)
+				#printf("%3d %3d %-5s %-20s\n", count, index, bmodel.flag, bmodel.name)
 				count += 1
 			end
 		}
