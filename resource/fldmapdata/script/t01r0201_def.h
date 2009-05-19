7b1330ccd4ed3b1ca4f8be61ff7d37c84f864d4d
Keita Kagaya <kagaya@gamefreak.co.jp>
2009-05-19 02:25:21 +0000

    t01r0101 t01r0201 t01r0301 t01r0401 t02 t02pc0101のスクリプトファイルを追加

---
+++ resource/fldmapdata/script/t01r0201_def.h
@@ -0,0 +1,8 @@
+#ifndef _T01R0201_DEF_H_
+#define _T01R0201_DEF_H_
+
+//スクリプトデータID定義 
+#define	SCRID_T01R0201_OLDMAN1_1		(1)
+#define	SCRID_T01R0201_DATA_MAX		(1)		//最大数
+
+#endif //_T01R0201_DEF_H_
---
+++ resource/fldmapdata/script/t02pc0101_def.h
@@ -0,0 +1,9 @@
+#ifndef _T02PC0101_DEF_H_
+#define _T02PC0101_DEF_H_
+
+//スクリプトデータID定義 
+#define	SCRID_T02PC0101_WOMAN1_1		(1)
+#define	SCRID_T02PC0101_MIDDLEWOMAN1_1		(2)
+#define	SCRID_T02PC0101_DATA_MAX		(2)		//最大数
+
+#endif //_T02PC0101_DEF_H_
