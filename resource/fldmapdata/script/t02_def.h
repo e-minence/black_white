904e1d8ecf0e6198204288b1b3257c233062ebde
Toshinobu Matsumiya <matsumiya@gamefreak.co.jp>
2009-05-20 02:10:33 +0000

    5月ROM用

--- resource/fldmapdata/script/t02_def.h
+++ resource/fldmapdata/script/t02_def.h
@@ -2,10 +2,7 @@
 #define _T02_DEF_H_
 
 //スクリプトデータID定義 
-#define	SCRID_T02_BABYGIRL1_1		(1)
-#define	SCRID_T02_WOMAN1_1		(2)
-#define	SCRID_T02_OLDMAN1_1		(3)
-#define	SCRID_T02_OLDWOMAN1_1		(4)
-#define	SCRID_T02_DATA_MAX		(4)		//最大数
+#define	SCRID_T02		(1)
+#define	SCRID_T02_DATA_MAX		(1)		//最大数
 
 #endif //_T02_DEF_H_
