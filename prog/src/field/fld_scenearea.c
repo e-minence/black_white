cdc244c7f59110bf9cbcf472c47cf250122f6531
Tomoya Takahashi <tomoya_takahashi@gamefreak.co.jp>
2009-05-22 05:13:28 +0000

    フィールドシーンエリア システム作成

---
+++ prog/src/field/fld_scenearea.c
@@ -0,0 +1,184 @@
+//[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[
+/**
+ *	GAME FREAK inc.
+ *
+ *	@file		fld_scenearea.c
+ *	@brief  フィールド　特殊シーン領域管理
+ *	@author	tomoya takahashi
+ *	@data		2009.05.22
+ *
+ *	モジュール名：FLD_SCENEAREA
+ */
+//]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]
+
+#include "gflib.h"
+
+#include "fld_scenearea.h"
+
+//-----------------------------------------------------------------------------
+/**
+ *					定数宣言
+*/
+//-----------------------------------------------------------------------------
+#define FLD_SCENEAREA_ACTIVE_NONE (0xffffffff)
+
+//-----------------------------------------------------------------------------
+/**
+ *					構造体宣言
+*/
+//-----------------------------------------------------------------------------
+//-------------------------------------
+///	シーンエリア
+//=====================================
+struct _FLD_SCENEAREA {
+  // カメラ
+  FIELD_CAMERA* p_camera;
+  
+  // データ
+  const FLD_SCENEAREA_DATA* cp_data;
+  u32 datanum;
+
+  // 起動フラグ
+  u32 active_area;
+};
+
+//-----------------------------------------------------------------------------
+/**
+ *					プロトタイプ宣言
+*/
+//-----------------------------------------------------------------------------
+
+
+//----------------------------------------------------------------------------
+/**
+ *	@brief  シーンエリア　作成
+ *
+ *	@param	heapID      ヒープID
+ *	@param	p_camera    カメラワーク
+ *
+ *	@return シーンエリア管理システム
+ */
+//-----------------------------------------------------------------------------
+FLD_SCENEAREA* FLD_SCENEAREA_Create( u32 heapID, FIELD_CAMERA * p_camera )
+{
+  FLD_SCENEAREA* p_sys;
+
+  p_sys = GFL_HEAP_AllocClearMemory( heapID, sizeof(FLD_SCENEAREA) );
+
+  p_sys->p_camera = p_camera;
+
+  // アクティブ領域なし
+  p_sys->active_area = FLD_SCENEAREA_ACTIVE_NONE;
+
+  return p_sys;
+}
+
+//----------------------------------------------------------------------------
+/**
+ *	@brief  情報の破棄
+ *
+ *	@param	p_sys   システムワーク
+ */
+//-----------------------------------------------------------------------------
+void FLD_SCENEAREA_Delete( FLD_SCENEAREA* p_sys )
+{
+  FLD_SCENEAREA_Release( p_sys );
+  GFL_HEAP_FreeMemory( p_sys );
+}
+
+
+//----------------------------------------------------------------------------
+/**
+ *	@brief  シーンエリア  情報設定
+ *
+ *	@param	p_sys     システムワーク
+ *	@param	cp_data   シーンエリア情報
+ *	@param	datanum   エリア数
+ *	@param	heapID    ヒープID
+ */
+//-----------------------------------------------------------------------------
+void FLD_SCENEAREA_Load( FLD_SCENEAREA* p_sys, const FLD_SCENEAREA_DATA* cp_data, u32 datanum )
+{
+  // 情報があるかもしれないのでRelease
+  FLD_SCENEAREA_Release( p_sys );
+
+  p_sys->cp_data = cp_data;
+  p_sys->datanum = datanum;
+
+  // アクティブ領域なし
+  p_sys->active_area = FLD_SCENEAREA_ACTIVE_NONE;
+}
+
+//----------------------------------------------------------------------------
+/**
+ *	@brief  シーンエリア  情報破棄
+ *
+ *	@param	p_sys システムワーク
+ */
+//-----------------------------------------------------------------------------
+void FLD_SCENEAREA_Release( FLD_SCENEAREA* p_sys )
+{
+  p_sys->cp_data      = NULL;
+  p_sys->datanum      = 0;
+  p_sys->active_area  = FLD_SCENEAREA_ACTIVE_NONE;
+}
+
+//----------------------------------------------------------------------------
+/**
+ *	@brief  シーンエリア　更新
+ *
+ *	@param	p_sys     システム
+ *	@param	cp_pos    自機位置
+ */
+//-----------------------------------------------------------------------------
+void FLD_SCENEAREA_Updata( FLD_SCENEAREA* p_sys, const VecFx32* cp_pos )
+{
+  int i;
+  u32 now_active;
+  
+  // エリアチェック
+  now_active = FLD_SCENEAREA_ACTIVE_NONE;
+  for( i=0; i<p_sys->datanum; i++ ){
+    
+    // チェック
+    GF_ASSERT( p_sys->cp_data[i].p_checkArea );
+    if( p_sys->cp_data[i].p_checkArea( p_sys, &p_sys->cp_data[i], cp_pos ) ){
+
+      // エリアがかぶっています。
+      GF_ASSERT_MSG( now_active == FLD_SCENEAREA_ACTIVE_NONE, "エリア範囲がかぶっています。インデックス%d,%d\n", now_active, i );
+      now_active = i;
+    }
+  }
+
+  // エリアインデックスの変更をチェック
+  if( now_active != p_sys->active_area )
+  {
+    if( now_active != FLD_SCENEAREA_ACTIVE_NONE ){
+      p_sys->cp_data[now_active].p_inside( p_sys, &p_sys->cp_data[now_active], cp_pos );
+    }
+
+    if( p_sys->active_area != FLD_SCENEAREA_ACTIVE_NONE ){
+      p_sys->cp_data[now_active].p_outside( p_sys, &p_sys->cp_data[now_active], cp_pos );
+    }
+  }else{
+
+    // 更新処理
+    p_sys->cp_data[now_active].p_updata( p_sys, &p_sys->cp_data[now_active], cp_pos );
+  }
+}
+
+
+
+//----------------------------------------------------------------------------
+/**
+ *	@brief  フィールドカメラの取得
+ *
+ *	@param	cp_sys  システム
+ *
+ *	@return フィールドカメラ
+ */
+//-----------------------------------------------------------------------------
+FIELD_CAMERA* FLD_SCENEAREA_GetFieldCamera( const FLD_SCENEAREA* cp_sys )
+{
+  return cp_sys->p_camera;
+}
---
+++ prog/src/field/fld_scenearea.h
@@ -0,0 +1,89 @@
+//[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[
+/**
+ *	GAME FREAK inc.
+ *
+ *	@file		fld_scenearea.h
+ *	@brief  フィールド　特殊シーン領域管理
+ *	@author	tomoya takahashi
+ *	@data		2009.05.22
+ *
+ *	モジュール名：FLD_SCENEAREA
+ */
+//]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]
+#pragma once
+
+#ifdef _cplusplus
+extern "C"{
+#endif
+
+
+#include "gflib.h"
+#include "field/fieldmap_proc.h"
+#include "field_camera.h"
+
+//-----------------------------------------------------------------------------
+/**
+ *					定数宣言
+*/
+//-----------------------------------------------------------------------------
+// シーンに持たせるパラメータサイズの最大値
+#define FLD_SCENEAREA_SCENEPARAM_SIZE  ( 32 )
+
+//-----------------------------------------------------------------------------
+/**
+ *					構造体宣言
+*/
+//-----------------------------------------------------------------------------
+//-------------------------------------
+///	シーンエリア
+//=====================================
+typedef struct _FLD_SCENEAREA FLD_SCENEAREA;
+
+
+
+//-------------------------------------
+///	シーンエリア情報
+//=====================================
+typedef struct _FLD_SCENEAREA_DATA FLD_SCENEAREA_DATA;
+
+// コールバック関数の型
+typedef BOOL (FLD_SCENEAREA_CHECK_AREA_FUNC)( const FLD_SCENEAREA* cp_sys, const FLD_SCENEAREA_DATA* cp_data, const VecFx32* cp_pos );
+typedef void (FLD_SCENEAREA_UPDATA_FUNC)( const FLD_SCENEAREA* cp_sys, const FLD_SCENEAREA_DATA* cp_data, const VecFx32* cp_pos );
+
+// 構造体定義
+struct _FLD_SCENEAREA_DATA{
+
+  // シーンパラメータ
+  u8 area[FLD_SCENEAREA_SCENEPARAM_SIZE];
+
+  // コールバック関数
+  FLD_SCENEAREA_CHECK_AREA_FUNC* p_checkArea;   // 範囲内チェック
+  FLD_SCENEAREA_CHECK_AREA_FUNC* p_updata;      // 情報の更新（エリア内の場合マイフレーム行う処理）
+  FLD_SCENEAREA_CHECK_AREA_FUNC* p_inside;      // 範囲に入った瞬間の処理
+  FLD_SCENEAREA_CHECK_AREA_FUNC* p_outside;     // 範囲を抜けた瞬間の処理
+};
+
+
+//-----------------------------------------------------------------------------
+/**
+ *					プロトタイプ宣言
+*/
+//-----------------------------------------------------------------------------
+
+extern FLD_SCENEAREA* FLD_SCENEAREA_Create( u32 heapID, FIELD_CAMERA * p_camera );
+extern void FLD_SCENEAREA_Delete( FLD_SCENEAREA* p_sys );
+
+extern void FLD_SCENEAREA_Load( FLD_SCENEAREA* p_sys, const FLD_SCENEAREA_DATA* cp_data, u32 datanum );
+extern void FLD_SCENEAREA_Release( FLD_SCENEAREA* p_sys );
+extern void FLD_SCENEAREA_Updata( FLD_SCENEAREA* p_sys, const VecFx32* cp_pos );
+
+
+// 各種情報の受け渡し
+extern FIELD_CAMERA* FLD_SCENEAREA_GetFieldCamera( const FLD_SCENEAREA* cp_sys );
+
+#ifdef _cplusplus
+}	// extern "C"{
+#endif
+
+
+
