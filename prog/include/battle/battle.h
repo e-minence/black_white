bb84545b6b2e11babf0e1ed6a0a518be9e7e6e39
Masao Taya <taya@gamefreak.co.jp>
2009-05-27 07:01:59 +0000

    メインモジュールからプレイヤー情報を取得できるようにした。

--- prog/include/battle/battle.h
+++ prog/include/battle/battle.h
@@ -1,10 +1,10 @@
 //=============================================================================================
 /**
- * @file	battle.h
- * @brief	ポケモンWB バトルシステム 外部システムとのインターフェイス
- * @author	taya
+ * @file  battle.h
+ * @brief ポケモンWB バトルシステム 外部システムとのインターフェイス
+ * @author  taya
  *
- * @date	2008.09.22	作成
+ * @date  2008.09.22  作成
  */
 //=============================================================================================
 #ifndef __BATTLE_H__
@@ -18,107 +18,109 @@
 #include <procsys.h>
 
 #include "poke_tool/pokeparty.h"
+#include "savedata/mystatus.h"
 
 //--------------------------------------------------------------
 /**
- *	駆動タイプ
+ *  駆動タイプ
  */
 //--------------------------------------------------------------
 typedef enum {
 
-	BTL_ENGINE_ALONE,					///< スタンドアロン
-	BTL_ENGINE_COMM_PARENT,		///< 通信（親）
-	BTL_ENGINE_COMM_CHILD,		///< 通信（子）
+  BTL_ENGINE_ALONE,         ///< スタンドアロン
+  BTL_ENGINE_COMM_PARENT,   ///< 通信（親）
+  BTL_ENGINE_COMM_CHILD,    ///< 通信（子）
 
 }BtlEngineType;
 
 //--------------------------------------------------------------
 /**
- *	対戦ルール
+ *  対戦ルール
  */
 //--------------------------------------------------------------
 typedef enum {
 
-	BTL_RULE_SINGLE,		///< シングル
-	BTL_RULE_DOUBLE,		///< ダブル
-	BTL_RULE_TRIPLE,		///< トリプル
+  BTL_RULE_SINGLE,    ///< シングル
+  BTL_RULE_DOUBLE,    ///< ダブル
+  BTL_RULE_TRIPLE,    ///< トリプル
 
 }BtlRule;
 
 //--------------------------------------------------------------
 /**
- *	対戦相手
+ *  対戦相手
  */
 //--------------------------------------------------------------
 typedef enum {
 
-	BTL_COMPETITOR_WILD,		///< 野生
-	BTL_COMPETITOR_TRAINER,		///< ゲーム内トレーナー
-	BTL_COMPETITOR_COMM,		///< 通信対戦
+  BTL_COMPETITOR_WILD,    ///< 野生
+  BTL_COMPETITOR_TRAINER,   ///< ゲーム内トレーナー
+  BTL_COMPETITOR_COMM,    ///< 通信対戦
 
 }BtlCompetitor;
 
 //--------------------------------------------------------------
 /**
- *	通信タイプ
+ *  通信タイプ
  */
 //--------------------------------------------------------------
 typedef enum {
 
-	BTL_COMM_NONE,	///< 通信しない
-	BTL_COMM_DS,		///< DS無線通信
-	BTL_COMM_WIFI,	///< Wi-Fi通信
+  BTL_COMM_NONE,  ///< 通信しない
+  BTL_COMM_DS,    ///< DS無線通信
+  BTL_COMM_WIFI,  ///< Wi-Fi通信
 
 }BtlCommMode;
 
 
 //--------------------------------------------------------------
 /**
- *	天候
+ *  天候
  */
 //--------------------------------------------------------------
 typedef enum {
 
-	BTL_WEATHER_NONE = 0,		///< 天候なし
+  BTL_WEATHER_NONE = 0,   ///< 天候なし
 
-	BTL_WEATHER_SHINE,		///< はれ
-	BTL_WEATHER_RAIN,			///< あめ
-	BTL_WEATHER_SNOW,			///< あられ
-	BTL_WEATHER_SAND,			///< すなあらし
-	BTL_WEATHER_MIST,			///< きり
+  BTL_WEATHER_SHINE,    ///< はれ
+  BTL_WEATHER_RAIN,     ///< あめ
+  BTL_WEATHER_SNOW,     ///< あられ
+  BTL_WEATHER_SAND,     ///< すなあらし
+  BTL_WEATHER_MIST,     ///< きり
 
-	BTL_WEATHER_MAX,
+  BTL_WEATHER_MAX,
 
 }BtlWeather;
 
 
 typedef struct {
 
-	BtlEngineType		engine;
-	BtlCompetitor		competitor;
-	BtlRule					rule;
+  BtlEngineType   engine;
+  BtlCompetitor   competitor;
+  BtlRule         rule;
 
-	GFL_NETHANDLE*	netHandle;
-	BtlCommMode			commMode;
-	u8							commPos;		///< 通信対戦なら自分の立ち位置（非通信時は無視）
-	u8							netID;			///< NetID
-	u8							multiMode;	///< ダブルの時、１だとマルチバトル。
+  GFL_NETHANDLE*  netHandle;
+  BtlCommMode     commMode;
+  u8              commPos;    ///< 通信対戦なら自分の立ち位置（非通信時は無視）
+  u8              netID;      ///< NetID
+  u8              multiMode;  ///< ダブルの時、１だとマルチバトル。
 
-	POKEPARTY*			partyPlayer;	///< プレイヤーのパーティ
-	POKEPARTY*			partyPartner;	///< 2vs2時の味方AI（不要ならnull）
-	POKEPARTY*			partyEnemy1;	///< 1vs1時の敵AI, 2vs2時の１番目敵AI用
-	POKEPARTY*			partyEnemy2;	///< 2vs2時の２番目敵AI用（不要ならnull）
+  POKEPARTY*      partyPlayer;  ///< プレイヤーのパーティ
+  POKEPARTY*      partyPartner; ///< 2vs2時の味方AI（不要ならnull）
+  POKEPARTY*      partyEnemy1;  ///< 1vs1時の敵AI, 2vs2時の１番目敵AI用
+  POKEPARTY*      partyEnemy2;  ///< 2vs2時の２番目敵AI用（不要ならnull）
 
+  const MYSTATUS*   statusPlayer; ///< プレイヤーのステータス
 
-	u16				musicDefault;		///< デフォルト時のBGMナンバー
-	u16				musicPinch;			///< ピンチ時のBGMナンバー
+  u16       musicDefault;   ///< デフォルト時のBGMナンバー
+  u16       musicPinch;     ///< ピンチ時のBGMナンバー
 
 }BATTLE_SETUP_PARAM;
 
 
 //--------------------------------------------------------------
 /**
- *	Proc Data
+ *  Proc Data
  */
 //--------------------------------------------------------------
 extern const GFL_PROC_DATA BtlProcData;
