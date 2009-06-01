1777d1c3e75a7781cf7227fd001e4dd4859b64db
Katsumi Ohno <ohno@gamefreak.co.jp>
2009-06-01 03:04:48 +0000

    古い通信システムを追加開始

---
+++ prog/src/net_old/dwc_raputility.c
@@ -0,0 +1,50 @@
+//=============================================================================
+/**
+ * @file	dwc_raputility.c
+ * @bfief	DWCラッパー。バトルオーバーレイ２層に並列  06.03.25
+ * @author	kazuki yoshihara  -> k.ohno
+ * @date	06/03/25
+ */
+//=============================================================================
+
+#include <gflib.h>
+/*↑[GS_CONVERT_TAG]*/
+#include "net_old/dwc_rap.h"
+#include "net_old/communication.h"
+
+//==============================================================================
+/**
+ * WiFi接続ユーティリティを呼び出す
+ * @param   none
+ * @retval  none
+ */
+//==============================================================================
+void mydwc_callUtility( int HeapID )
+{
+	OSIntrMode old;
+	void *work;
+
+	CommVRAMDInitialize(); // 
+	// アラームシステムを初期化しておかないとDWC_StartUtility呼出し後にPanicが発生する
+	OS_InitTick();
+	OS_InitAlarm();
+
+
+	// 割り込み処理を一旦禁止へ
+	old = OS_DisableInterrupts();
+
+	// WiFi設定メニュー呼び出し（終わるまで帰ってこない)
+	work = GFL_HEAP_AllocMemory( HeapID, DWC_UTILITY_WORK_SIZE );
+/*↑[GS_CONVERT_TAG]*/
+	(void)DWC_StartUtility( work, DWC_LANGUAGE_JAPANESE, DWC_UTILITY_TOP_MENU_FOR_JPN );
+	GFL_HEAP_FreeMemory( work );
+/*↑[GS_CONVERT_TAG]*/
+
+	// 禁止した割り込み処理を復帰
+	OS_RestoreInterrupts( old );
+
+	//マスター割り込み処理を有効に
+	OS_EnableIrq( ); 
+
+
+}
