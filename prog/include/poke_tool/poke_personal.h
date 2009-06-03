a0ab9689cc2fc04e76fa9548e49fa488ebb46764
Masao Taya <taya@gamefreak.co.jp>
2009-06-03 03:35:30 +0000

    poke_tool内のローカル定義だった「性別ベクトル」を、外部公開した。

--- prog/include/poke_tool/poke_personal.h
+++ prog/include/poke_tool/poke_personal.h
@@ -1,10 +1,10 @@
 //=============================================================================================
 /**
- * @file	poke_personal.h
- * @brief	ポケモンパーソナルデータアクセス関数群
- * @author	taya
+ * @file  poke_personal.h
+ * @brief ポケモンパーソナルデータアクセス関数群
+ * @author  taya
  *
- * @date	2008.11.06	作成
+ * @date  2008.11.06  作成
  */
 //=============================================================================================
 #ifndef __POKE_PERSONAL_H__
@@ -12,69 +12,81 @@
 
 //--------------------------------------------------------------
 /**
- *	ポケモンパーソナルデータ構造体ハンドラ型の宣言
+ *  ポケモンパーソナルデータ構造体ハンドラ型の宣言
  */
 //--------------------------------------------------------------
-typedef struct pokemon_personal_data	POKEMON_PERSONAL_DATA;
+typedef struct pokemon_personal_data  POKEMON_PERSONAL_DATA;
 
 //--------------------------------------------------------------
 /**
- *	ポケモンパーソナルデータ取得のためのインデックス
+ *  ポケモンパーソナルデータ取得のためのインデックス
  */
 //--------------------------------------------------------------
 typedef enum {
 
-	POKEPER_ID_basic_hp=0,			//基本ＨＰ
-	POKEPER_ID_basic_pow,			//基本攻撃力
-	POKEPER_ID_basic_def,			//基本防御力
-	POKEPER_ID_basic_agi,			//基本素早さ
-	POKEPER_ID_basic_spepow,		//基本特殊攻撃力
-	POKEPER_ID_basic_spedef,		//基本特殊防御力
-	POKEPER_ID_type1,				//属性１
-	POKEPER_ID_type2,				//属性２
-	POKEPER_ID_get_rate,			//捕獲率
-	POKEPER_ID_give_exp,			//贈与経験値
-	POKEPER_ID_pains_hp,			//贈与努力値ＨＰ
-	POKEPER_ID_pains_pow,			//贈与努力値攻撃力
-	POKEPER_ID_pains_def,			//贈与努力値防御力
-	POKEPER_ID_pains_agi,			//贈与努力値素早さ
-	POKEPER_ID_pains_spepow,		//贈与努力値特殊攻撃力
-	POKEPER_ID_pains_spedef,		//贈与努力値特殊防御力
-	POKEPER_ID_item1,				//アイテム１
-	POKEPER_ID_item2,				//アイテム２
-	POKEPER_ID_sex,					//性別ベクトル
-	POKEPER_ID_egg_birth,			//タマゴの孵化歩数
-	POKEPER_ID_friend,				//なつき度初期値
-	POKEPER_ID_grow,				//成長曲線識別
-	POKEPER_ID_egg_group1,			//こづくりグループ1
-	POKEPER_ID_egg_group2,			//こづくりグループ2
-	POKEPER_ID_speabi1,				//特殊能力１
-	POKEPER_ID_speabi2,				//特殊能力２
-	POKEPER_ID_escape,				//逃げる率
-	POKEPER_ID_color,				//色（図鑑で使用）
-	POKEPER_ID_reverse,				//反転フラグ
-	POKEPER_ID_machine1,			//技マシンフラグ１
-	POKEPER_ID_machine2,			//技マシンフラグ２
-	POKEPER_ID_machine3,			//技マシンフラグ３
-	POKEPER_ID_machine4				//技マシンフラグ４
+  POKEPER_ID_basic_hp=0,      //基本ＨＰ
+  POKEPER_ID_basic_pow,     //基本攻撃力
+  POKEPER_ID_basic_def,     //基本防御力
+  POKEPER_ID_basic_agi,     //基本素早さ
+  POKEPER_ID_basic_spepow,    //基本特殊攻撃力
+  POKEPER_ID_basic_spedef,    //基本特殊防御力
+  POKEPER_ID_type1,       //属性１
+  POKEPER_ID_type2,       //属性２
+  POKEPER_ID_get_rate,      //捕獲率
+  POKEPER_ID_give_exp,      //贈与経験値
+  POKEPER_ID_pains_hp,      //贈与努力値ＨＰ
+  POKEPER_ID_pains_pow,     //贈与努力値攻撃力
+  POKEPER_ID_pains_def,     //贈与努力値防御力
+  POKEPER_ID_pains_agi,     //贈与努力値素早さ
+  POKEPER_ID_pains_spepow,    //贈与努力値特殊攻撃力
+  POKEPER_ID_pains_spedef,    //贈与努力値特殊防御力
+  POKEPER_ID_item1,       //アイテム１
+  POKEPER_ID_item2,       //アイテム２
+  POKEPER_ID_sex,         //性別ベクトル
+  POKEPER_ID_egg_birth,     //タマゴの孵化歩数
+  POKEPER_ID_friend,        //なつき度初期値
+  POKEPER_ID_grow,        //成長曲線識別
+  POKEPER_ID_egg_group1,      //こづくりグループ1
+  POKEPER_ID_egg_group2,      //こづくりグループ2
+  POKEPER_ID_speabi1,       //特殊能力１
+  POKEPER_ID_speabi2,       //特殊能力２
+  POKEPER_ID_escape,        //逃げる率
+  POKEPER_ID_color,       //色（図鑑で使用）
+  POKEPER_ID_reverse,       //反転フラグ
+  POKEPER_ID_machine1,      //技マシンフラグ１
+  POKEPER_ID_machine2,      //技マシンフラグ２
+  POKEPER_ID_machine3,      //技マシンフラグ３
+  POKEPER_ID_machine4       //技マシンフラグ４
 
 }PokePersonalParamID;
 
 
 //パーソナルデータの色定義
 typedef enum {
-	POKEPER_COLOR_RED = 0,		//赤
-	POKEPER_COLOR_BLUE,			//青
-	POKEPER_COLOR_YELLOW,		//黄
-	POKEPER_COLOR_GREEN,		//緑
-	POKEPER_COLOR_BLACK,		//黒
-	POKEPER_COLOR_BROWN,		//茶
-	POKEPER_COLOR_PERPLE,		//紫
-	POKEPER_COLOR_GRAY,			//灰
-	POKEPER_COLOR_WHITE,		//白
-	POKEPER_COLOR_PINK,			//桃
+  POKEPER_COLOR_RED = 0,    //赤
+  POKEPER_COLOR_BLUE,     //青
+  POKEPER_COLOR_YELLOW,   //黄
+  POKEPER_COLOR_GREEN,    //緑
+  POKEPER_COLOR_BLACK,    //黒
+  POKEPER_COLOR_BROWN,    //茶
+  POKEPER_COLOR_PERPLE,   //紫
+  POKEPER_COLOR_GRAY,     //灰
+  POKEPER_COLOR_WHITE,    //白
+  POKEPER_COLOR_PINK,     //桃
 }PokePersonal_Color;
 
+
+//--------------------------------------------------------------
+/**
+ * ポケモンパーソナルデータの関連定数
+ */
+//--------------------------------------------------------------
+enum {
+  //  性別ベクトル（パーソナル上のオスメス比率）(1～253でオスメスの比率が変わる）
+  POKEPER_SEX_MALE = 0,     ///<オスのみ
+  POKEPER_SEX_FEMALE = 254,   ///<メスのみ
+  POKEPER_SEX_UNKNOWN = 255,    ///<性別なし
+};
 
 //ポケモンパーソナル操作関数系
 extern POKEMON_PERSONAL_DATA*  POKE_PERSONAL_OpenHandle( u16 mons_no, u16 form_no, HEAPID heapID );
@@ -82,7 +94,7 @@ extern void POKE_PERSONAL_CloseHandle( POKEMON_PERSONAL_DATA* handle );
 extern u32 POKE_PERSONAL_GetParam( POKEMON_PERSONAL_DATA *ppd, PokePersonalParamID paramID );
 
 
-extern	u8		PokePersonal_SexGet( POKEMON_PERSONAL_DATA* personalData, u16 monsno, u32 rnd );
+extern  u8    PokePersonal_SexGet( POKEMON_PERSONAL_DATA* personalData, u16 monsno, u32 rnd );
 
 
 
--- prog/src/poke_tool/poke_personal_local.h
+++ prog/src/poke_tool/poke_personal_local.h
@@ -1,10 +1,10 @@
 //=============================================================================================
 /**
- * @file	poke_personal.h
- * @brief	ポケモンパーソナルデータアクセス関数群（poke_toolグループ内）
- * @author	taya
+ * @file  poke_personal.h
+ * @brief ポケモンパーソナルデータアクセス関数群（poke_toolグループ内）
+ * @author  taya
  *
- * @date	2008.11.06	作成
+ * @date  2008.11.06  作成
  */
 //=============================================================================================
 #ifndef __POKE_PERSONAL_LOCAL_H__
@@ -19,68 +19,57 @@
  * ポケモンパーソナルデータの構造体定義
  */
 //--------------------------------------------------------------
-struct	pokemon_personal_data
+struct  pokemon_personal_data
 {
-	u8		basic_hp;			//基本ＨＰ
-	u8		basic_pow;			//基本攻撃力
-	u8		basic_def;			//基本防御力
-	u8		basic_agi;			//基本素早さ
+  u8    basic_hp;     //基本ＨＰ
+  u8    basic_pow;      //基本攻撃力
+  u8    basic_def;      //基本防御力
+  u8    basic_agi;      //基本素早さ
 
-	u8		basic_spepow;		//基本特殊攻撃力
-	u8		basic_spedef;		//基本特殊防御力
-	u8		type1;				//属性１
-	u8		type2;				//属性２
+  u8    basic_spepow;   //基本特殊攻撃力
+  u8    basic_spedef;   //基本特殊防御力
+  u8    type1;        //属性１
+  u8    type2;        //属性２
 
-	u8		get_rate;			//捕獲率
-	u8		give_exp;			//贈与経験値
+  u8    get_rate;     //捕獲率
+  u8    give_exp;     //贈与経験値
 
-	u16		pains_hp	:2;		//贈与努力値ＨＰ
-	u16		pains_pow	:2;		//贈与努力値攻撃力
-	u16		pains_def	:2;		//贈与努力値防御力
-	u16		pains_agi	:2;		//贈与努力値素早さ
-	u16		pains_spepow:2;		//贈与努力値特殊攻撃力
-	u16		pains_spedef:2;		//贈与努力値特殊防御力
-	u16					:4;		//贈与努力値予備
+  u16   pains_hp  :2;   //贈与努力値ＨＰ
+  u16   pains_pow :2;   //贈与努力値攻撃力
+  u16   pains_def :2;   //贈与努力値防御力
+  u16   pains_agi :2;   //贈与努力値素早さ
+  u16   pains_spepow:2;   //贈与努力値特殊攻撃力
+  u16   pains_spedef:2;   //贈与努力値特殊防御力
+  u16         :4;   //贈与努力値予備
 
-	u16		item1;				//アイテム１
-	u16		item2;				//アイテム２
+  u16   item1;        //アイテム１
+  u16   item2;        //アイテム２
 
-	u8		sex;				//性別ベクトル
-	u8		egg_birth;			//タマゴの孵化歩数
-	u8		friend;				//なつき度初期値
-	u8		grow;				//成長曲線識別
+  u8    sex;        //性別ベクトル
+  u8    egg_birth;      //タマゴの孵化歩数
+  u8    friend;       //なつき度初期値
+  u8    grow;       //成長曲線識別
 
-	u8		egg_group1;			//こづくりグループ1
-	u8		egg_group2;			//こづくりグループ2
-	u8		speabi1;			//特殊能力１
-	u8		speabi2;			//特殊能力２
+  u8    egg_group1;     //こづくりグループ1
+  u8    egg_group2;     //こづくりグループ2
+  u8    speabi1;      //特殊能力１
+  u8    speabi2;      //特殊能力２
 
-	u8		escape;				//逃げる率
-	u8		color	:7;			//色（図鑑で使用）
-	u8		reverse	:1;			//反転フラグ
-	u32		machine1;			//技マシンフラグ１
-	u32		machine2;			//技マシンフラグ２
-	u32		machine3;			//技マシンフラグ２
-	u32		machine4;			//技マシンフラグ２
+  u8    escape;       //逃げる率
+  u8    color :7;     //色（図鑑で使用）
+  u8    reverse :1;     //反転フラグ
+  u32   machine1;     //技マシンフラグ１
+  u32   machine2;     //技マシンフラグ２
+  u32   machine3;     //技マシンフラグ２
+  u32   machine4;     //技マシンフラグ２
 };
 
-//--------------------------------------------------------------
-/**
- * ポケモンパーソナルデータの関連定数
- */
-//--------------------------------------------------------------
-enum {
-	//	性別ベクトル（パーソナル上のオスメス比率）(1～253でオスメスの比率が変わる）
-	POKEPER_SEX_MALE = 0,			///<オスのみ
-	POKEPER_SEX_FEMALE = 254,		///<メスのみ
-	POKEPER_SEX_UNKNOWN = 255,		///<性別なし
-};
 
 //=============================================================================================
 /**
  * システム初期化（プログラム起動後に１度だけ呼び出す）
  *
- * @param   heapID		システム初期化用ヒープID（アーカイブハンドル用に数十バイト必要）
+ * @param   heapID    システム初期化用ヒープID（アーカイブハンドル用に数十バイト必要）
  */
 //=============================================================================================
 extern void POKE_PERSONAL_InitSystem( HEAPID heapID );
