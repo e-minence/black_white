1c8f0e8cdc990af6876c6e7b86926fae51242a2c
Keita Kagaya <kagaya@gamefreak.co.jp>
2009-06-05 11:39:53 +0000

    ラベル名にゾーン名を追加

--- resource/fldmapdata/script/t01_def.h
+++ resource/fldmapdata/script/t01_def.h
@@ -2,11 +2,11 @@
 #define _T01_DEF_H_
 
 //スクリプトデータID定義 
-#define	SCRID_TALKOBJ_MAN01		(1)
-#define	SCRID_TALKOBJ_WOMAN01		(2)
-#define	SCRID_TALKOBJ_BABYGIRL1		(3)
-#define	SCRID_POSEVENT_TEST1		(4)
-#define	SCRID_BGEVENT_TEST1		(5)
+#define	SCRID_T01_TALKOBJ_MAN01		(1)
+#define	SCRID_T01_TALKOBJ_WOMAN01		(2)
+#define	SCRID_T01_TALKOBJ_BABYGIRL1		(3)
+#define	SCRID_T01_POSEVENT_TEST1		(4)
+#define	SCRID_T01_BGEVENT_TEST1		(5)
 #define	SCRID_T01_DATA_MAX		(5)		//最大数
 
 #endif //_T01_DEF_H_
