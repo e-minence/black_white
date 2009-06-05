6f08a36ad000ae599a219bb81a008d7b034f658a
Keita Kagaya <kagaya@gamefreak.co.jp>
2009-06-05 04:49:46 +0000

    BGイベント、POSイベントチェック関数を定義

--- prog/include/field/eventdata_system.h
+++ prog/include/field/eventdata_system.h
@@ -8,6 +8,8 @@
 //============================================================================================
 #pragma once
 
+#define EVENTDATA_ID_NONE (0xffff) ///<イベントデータなし
+
 //------------------------------------------------------------------
 //------------------------------------------------------------------
 typedef struct _EVDATA_SYS EVENTDATA_SYSTEM;
@@ -19,3 +21,11 @@ extern void EVENTDATA_SYS_Delete(EVENTDATA_SYSTEM * evdata);
 extern void EVENTDATA_SYS_Clear(EVENTDATA_SYSTEM * evdata);
 extern void EVENTDATA_SYS_Load(EVENTDATA_SYSTEM * evdata, u16 mapid);
 
+#include "field/eventwork.h"
+
+extern u16 EVENTDATA_CheckPosEvent(
+  const EVENTDATA_SYSTEM *evdata, EVENTWORK *evwork, const VecFx32 *pos );
+
+extern u16 EVENTDATA_CheckTalkBGEvent(
+    const EVENTDATA_SYSTEM *evdata, EVENTWORK *evwork,
+    const VecFx32 *pos, u16 talk_dir );
