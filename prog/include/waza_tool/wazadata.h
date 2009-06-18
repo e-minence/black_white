0a7b18e5d1aad6e73f98a4e149d3b3b17234fd74
Masao Taya <taya@gamefreak.co.jp>
2009-06-18 17:48:07 +0000

    ワザ状態異常新規追加

--- prog/include/waza_tool/wazadata.h
+++ prog/include/waza_tool/wazadata.h
@@ -87,7 +87,8 @@ typedef enum {
   WAZASICK_KAIHUKUHUUJI,
   WAZASICK_KANASIBARI,
   WAZASICK_HOROBINOUTA,
-  WAZASICK_MUSTHIT,
+  WAZASICK_MUSTHIT,         ///< ワザ必中状態
+  WAZASICK_MUSTHIT_TARGET,  ///< 特定相手に対してのみ必中状態
   WAZASICK_WAZALOCK,      ///< 直前に使ったワザしか出せない
   WAZASICK_WAZALOCK_HARD, ///< 直前に使ったワザしか出せない上、行動不可（行動を選択できない）
   WAZASICK_CANT_ESCAPE,   ///< にげる・いれかえが出来ない
