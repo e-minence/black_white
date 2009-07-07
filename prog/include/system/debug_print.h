a11d1f415eacd17e6e88d6e0d9000886aa6cdaca
Masao Taya <taya@gamefreak.co.jp>
2009-07-07 01:55:18 +0000

    全プログラマ向けに自分のマシンでコンパイルしたもののみ出力するPrintf機構を用意した。

--- prog/include/system/debug_print.h
+++ prog/include/system/debug_print.h
@@ -1,9 +1,9 @@
 //===================================================================
 /**
- * @file	debug_print.h
- * @brief	特定ユーザの環境でコンパイルした場合のみ動作するPrintf関数
- * @author	taya
- * @date	07.02.13
+ * @file  debug_print.h
+ * @brief 特定ユーザの環境でコンパイルした場合のみ動作するPrintf関数
+ * @author  taya
+ * @date  07.02.13
  */
 //===================================================================
 #ifndef __DEBUG_PRINT_H__
@@ -11,46 +11,62 @@
 
 
 typedef enum {
-	AUTHOR_FREE = 0,	///< これを指定すると全環境で出力される
+  AUTHOR_FREE = 0,  ///< これを指定すると全環境で出力される
 
-	AUTHOR_WATANABE,
-	AUTHOR_TAMADA,
-	AUTHOR_SOGABE,
-	AUTHOR_OHNO,
-	AUTHOR_MATSUDA,
-	AUTHOR_KAGAYA,
-	AUTHOR_ARIIZUMI,
-	AUTHOR_TAYA,
-	AUTHOR_NAGI,
+  AUTHOR_WATANABE,
+  AUTHOR_TAMADA,
+  AUTHOR_SOGABE,
+  AUTHOR_OHNO,
+  AUTHOR_MATSUDA,
+  AUTHOR_KAGAYA,
+  AUTHOR_ARIIZUMI,
+  AUTHOR_TAYA,
+  AUTHOR_NAGI,
+  AUTHOR_TOMOYA,
+  AUTHOR_OBATA,
+  AUTHOR_MORI,
+  AUTHOR_NAKAHIRO,
+  AUTHOR_IWASAWA,
+  AUTHOR_NOZOMU,
 
-	AUTHOR_IGNORE,		///< これを指定すると出力されない
+  AUTHOR_IGNORE,    ///< これを指定すると出力されない
 }AUTHOR_ID;
 
 
 #if defined DEBUG_ONLY_FOR_taya
-#define DEFINED_AUTHOR_ID	(AUTHOR_TAYA)
+#define DEFINED_AUTHOR_ID (AUTHOR_TAYA)
 #elif defined DEBUG_ONLY_FOR_hiro_nakamura
-#define DEFINED_AUTHOR_ID	(AUTHOR_NAKAMURA)
+#define DEFINED_AUTHOR_ID (AUTHOR_NAKAHIRO)
 #elif defined DEBUG_ONLY_FOR_genya_hosaka
-#define DEFINED_AUTHOR_ID	(AUTHOR_HOSAKA)
+#define DEFINED_AUTHOR_ID (AUTHOR_HOSAKA)
 #elif defined DEBUG_ONLY_FOR_toru_nagihashi
-#define DEFINED_AUTHOR_ID	(AUTHOR_NAGI)
+#define DEFINED_AUTHOR_ID (AUTHOR_NAGI)
 #elif defined DEBUG_ONLY_FOR_watanabe
-#define DEFINED_AUTHOR_ID	(AUTHOR_WATANABE)
+#define DEFINED_AUTHOR_ID (AUTHOR_WATANABE)
 #elif defined DEBUG_ONLY_FOR_tamada
-#define DEFINED_AUTHOR_ID	(AUTHOR_TAMADA)
+#define DEFINED_AUTHOR_ID (AUTHOR_TAMADA)
 #elif defined DEBUG_ONLY_FOR_sogabe
-#define DEFINED_AUTHOR_ID	(AUTHOR_SOGABE)
+#define DEFINED_AUTHOR_ID (AUTHOR_SOGABE)
 #elif defined DEBUG_ONLY_FOR_ohno
-#define DEFINED_AUTHOR_ID	(AUTHOR_OHNO)
+#define DEFINED_AUTHOR_ID (AUTHOR_OHNO)
 #elif defined DEBUG_ONLY_FOR_matsuda
-#define DEFINED_AUTHOR_ID	(AUTHOR_MATSUDA)
+#define DEFINED_AUTHOR_ID (AUTHOR_MATSUDA)
 #elif defined DEBUG_ONLY_FOR_kagaya
-#define DEFINED_AUTHOR_ID	(AUTHOR_KAGAYA)
+#define DEFINED_AUTHOR_ID (AUTHOR_KAGAYA)
 #elif defined DEBUG_ONLY_FOR_ariizumi_nobuhiko
-#define DEFINED_AUTHOR_ID	(AUTHOR_ARIIZUMI)
+#define DEFINED_AUTHOR_ID (AUTHOR_ARIIZUMI)
+#elif defined DEBUG_ONLY_FOR_tomoya_takahashi
+#define DEFINED_AUTHOR_ID (AUTHOR_TOMOYA)
+#elif defined DEBUG_ONLY_FOR_toshihiro_obata
+#define DEFINED_AUTHOR_ID (AUTHOR_OBATA)
+#elif defined DEBUG_ONLY_FOR_mori
+#define DEFINED_AUTHOR_ID (AUTHOR_MORI)
+#elif defined DEBUG_ONLY_FOR_iwasawa
+#define DEFINED_AUTHOR_ID (AUTHOR_IWASAWA)
+#elif defined DEBUG_ONLY_FOR_saitou
+#define DEFINED_AUTHOR_ID (AUTHOR_NOZOMU)
 #else
-#define DEFINED_AUTHOR_ID	(AUTHOR_FREE)
+#define DEFINED_AUTHOR_ID (AUTHOR_FREE)
 #endif
 
 
@@ -66,19 +82,19 @@ typedef enum {
 /**
  * 特定ユーザの環境でコンパイルした場合のみ動作するPrintf関数
  *
- * @param   author_id		ユーザID（debug_print.hに記述）
- * @param   fmt				OS_TPrintfに渡す書式付き文字列
+ * @param   author_id   ユーザID（debug_print.hに記述）
+ * @param   fmt       OS_TPrintfに渡す書式付き文字列
  *
  */
 //==============================================================================================
 extern void DEBUG_Printf( AUTHOR_ID author_id, const char * fmt, ... );
 
-#define		DEBUG_PrintfEx( flg, id, ... )		{  if(flg){ DEBUG_Printf(id, __VA_ARGS__); } }
+#define   DEBUG_PrintfEx( flg, id, ... )    {  if(flg){ DEBUG_Printf(id, __VA_ARGS__); } }
 
 #else
 
-#define		DEBUG_Printf( id, ... )				((void)0)
-#define		DEBUG_PrintfEx( flg, id, ... )		((void)0)
+#define   DEBUG_Printf( id, ... )       ((void)0)
+#define   DEBUG_PrintfEx( flg, id, ... )    ((void)0)
 
 #endif
 
@@ -86,14 +102,20 @@ extern void DEBUG_Printf( AUTHOR_ID author_id, const char * fmt, ... );
 //-----------------------------------------------------------
 // かんたん記述用マクロ
 //-----------------------------------------------------------
-#define		TAYA_Printf( ... )			DEBUG_Printf( AUTHOR_TAYA, __VA_ARGS__ )
-#define		WATANABE_Printf( ... )	DEBUG_Printf( AUTHOR_WATANABE, __VA_ARGS__ )
-#define		TAMADA_Printf( ...)			DEBUG_Printf( AUTHOR_TAMADA, __VA_ARGS__ )
-#define		SOGABE_Printf( ...)			DEBUG_Printf( AUTHOR_SOGABE, __VA_ARGS__ )
-#define		OHNO_Printf( ...)				DEBUG_Printf( AUTHOR_OHNO, __VA_ARGS__ )
-#define		MATSUDA_Printf( ...)		DEBUG_Printf( AUTHOR_MATSUDA, __VA_ARGS__ )
-#define		KAGAYA_Printf( ...)			DEBUG_Printf( AUTHOR_KAGAYA, __VA_ARGS__ )
-#define		ARIIZUMI_Printf( ...)		DEBUG_Printf( AUTHOR_ARIIZUMI, __VA_ARGS__ )
-#define		NAGI_Printf( ... )			DEBUG_Printf( AUTHOR_NAGI, __VA_ARGS__ )
+#define   TAYA_Printf( ... )      DEBUG_Printf( AUTHOR_TAYA, __VA_ARGS__ )
+#define   WATANABE_Printf( ... )  DEBUG_Printf( AUTHOR_WATANABE, __VA_ARGS__ )
+#define   TAMADA_Printf( ...)     DEBUG_Printf( AUTHOR_TAMADA, __VA_ARGS__ )
+#define   SOGABE_Printf( ...)     DEBUG_Printf( AUTHOR_SOGABE, __VA_ARGS__ )
+#define   OHNO_Printf( ...)       DEBUG_Printf( AUTHOR_OHNO, __VA_ARGS__ )
+#define   MATSUDA_Printf( ...)    DEBUG_Printf( AUTHOR_MATSUDA, __VA_ARGS__ )
+#define   KAGAYA_Printf( ...)     DEBUG_Printf( AUTHOR_KAGAYA, __VA_ARGS__ )
+#define   ARIIZUMI_Printf( ...)   DEBUG_Printf( AUTHOR_ARIIZUMI, __VA_ARGS__ )
+#define   NAGI_Printf( ... )      DEBUG_Printf( AUTHOR_NAGI, __VA_ARGS__ )
+#define   TOMOYA_Printf( ... )    DEBUG_Printf( AUTHOR_TOMOYA, __VA_ARGS__ )
+#define   OBATA_Printf( ... )     DEBUG_Printf( AUTHOR_OBATA, __VA_ARGS__ )
+#define   MORI_Printf( ... )      DEBUG_Printf( AUTHOR_MORI, __VA_ARGS__ )
+#define   IWASAWA_Printf( ... )   DEBUG_Printf( AUTHOR_IWASAWA, __VA_ARGS__ )
+#define   NOZOMU_Printf( ... )    DEBUG_Printf( AUTHOR_NOZOMU, __VA_ARGS__ )
+
 
 #endif
