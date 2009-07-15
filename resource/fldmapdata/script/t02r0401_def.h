2fa4f45d99bdebf7e6a01880e122fb4e82dc02d7
Toshinobu Matsumiya <matsumiya@gamefreak.co.jp>
2009-07-15 02:21:33 +0000

    スクリプトテーブル修正

--- resource/fldmapdata/script/t02r0401_def.h
+++ resource/fldmapdata/script/t02r0401_def.h
@@ -2,9 +2,9 @@
 #define _T02R0401_DEF_H_
 
 //スクリプトデータID定義 
-#define	SCRID_T02R0401_GIRL4_1		(1)
-#define	SCRID_T02R0401_MAN2_1		(2)
-#define	SCRID_T02R0401_POKE_1		(3)
+#define	SCRID_T02R0401_TALKOBJ_GIRL4		(1)
+#define	SCRID_T02R0401_TALKOBJ_MAN2		(2)
+#define	SCRID_T02R0401_TALKOBJ_POKE		(3)
 #define	SCRID_T02R0401_DATA_MAX		(3)		//最大数
 
 #endif //_T02R0401_DEF_H_
