ed7d626a3fd2a4bda1c6f776ce6c06e2e23190a4
Ariizumi Nobuhiko <ariizumi_nobuhiko@gamefreak.co.jp>
2009-07-15 04:26:10 +0000

    共通素材フォルダ:タイプ系整理・バーボタン用定義追加

---
+++ prog/include/app/app_menu_common.h
@@ -0,0 +1,68 @@
+//======================================================================
+/**
+ * @file	app_menu_common.h
+ * @brief	共通素材関係ソース
+ * @author	ariizumi
+ * @data	09/07/14
+ *
+ * モジュール名：APP_COMMON
+ */
+//======================================================================
+
+#pragma once
+
+#include "poke_tool/poketype.h"
+
+//==============================================================================
+//	定数定義
+//==============================================================================
+//バーのボタンのアニメインデックス
+enum
+{
+  APP_COMMON_BARICON_CLOSE,
+  APP_COMMON_BARICON_RETURN,
+  APP_COMMON_BARICON_CURSOR_DOWN,
+  APP_COMMON_BARICON_CURSOR_UP,
+  APP_COMMON_BARICON_CURSOR_LEFT,
+  APP_COMMON_BARICON_CURSOR_RIGHT,
+  APP_COMMON_BARICON_CHECK_ON,
+  APP_COMMON_BARICON_CHECK_OFF,
+  
+  APP_COMMON_BARICON_MAX,
+};
+
+
+///アイコンタイプ(技タイプ以外のタイプ)
+enum{
+  ICONTYPE_STYLE = POKETYPE_MAX,  ///<格好よさ
+  ICONTYPE_BEAUTIFUL,             ///<美しさ
+  ICONTYPE_CUTE,                  ///<可愛さ
+  ICONTYPE_INTELLI,               ///<賢さ
+  ICONTYPE_STRONG,                ///<逞しさ
+};
+
+//ポケモンタイプのパレットの本数
+#define APP_COMMON_POKETYPE_PLT_NUM (3)
+
+//セルの読みわけに使うマッピングモード定義
+typedef enum
+{
+  APP_COMMON_MAPPING_32K,
+  APP_COMMON_MAPPING_64K,
+  APP_COMMON_MAPPING_128K,
+}APP_COMMON_MAPPING;
+
+//ARCIDの取得
+extern const u32 APP_COMMON_GetArcId(void);
+
+//タイプ系素材取得
+extern const u32 APP_COMMON_GetPokeTypePltArcIdx( void );
+extern const u8  APP_COMMON_GetPokeTypePltOffset( const PokeType type );
+extern const u32 APP_COMMON_GetPokeTypeCharArcIdx( const PokeType type );
+extern const u32 APP_COMMON_GetPokeTypeCellArcIdx( const APP_COMMON_MAPPING mapping );
+extern const u32 APP_COMMON_GetPokeTypeAnimeArcIdx( const APP_COMMON_MAPPING mapping );
+
+//技分類系素材取得(パレット・セル・アニメはタイプと共通
+extern const u8  APP_COMMON_GetWazaKindPltOffset( const PokeType type );
+extern const u32 APP_COMMON_GetWazaKindCharArcIdx( const PokeType type );
+
---
+++ prog/src/app/app_menu_common.c
@@ -0,0 +1,100 @@
+//======================================================================
+/**
+ * @file	app_menu_common.c
+ * @brief	共通素材関係ソース
+ * @author	ariizumi
+ * @data	09/07/14
+ *
+ * モジュール名：APP_COMMON
+ */
+//======================================================================
+#include <gflib.h>
+
+#include "arc_def.h"
+#include "app_menu_common.naix"
+
+#include "app/app_menu_common.h"
+
+///技タイプ毎のパレットオフセット番号
+ALIGN4 static const u8 PokeTypePlttOffset[] = {
+	0,	//POKETYPE_NORMAL
+	0,	//POKETYPE_BATTLE
+	1,	//POKETYPE_HIKOU
+	1,	//POKETYPE_POISON
+	0,	//POKETYPE_JIMEN
+	0,	//POKETYPE_IWA
+	2,	//POKETYPE_MUSHI
+	1,	//POKETYPE_GHOST
+	0,	//POKETYPE_METAL
+	2,	//POKETYPE_HATE
+	0,	//POKETYPE_FIRE
+	1,	//POKETYPE_WATER
+	2,	//POKETYPE_KUSA
+	0,	//POKETYPE_ELECTRIC
+	1,	//POKETYPE_SP
+	1,	//POKETYPE_KOORI
+	2,	//POKETYPE_DRAGON
+	0,	//POKETYPE_AKU
+	0,	//ICONTYPE_STYLE,			///<格好よさ
+	1,	//ICONTYPE_BEAUTIFUL,		///<美しさ
+	1,	//ICONTYPE_CUTE,			///<可愛さ
+	2,	//ICONTYPE_INTELLI,		///<賢さ
+	0,	//ICONTYPE_STRONG,		///<逞しさ
+};
+
+///分類毎のパレットオフセット番号
+ALIGN4 static const u8 WazaKindPlttOffset[] = {
+  0,    //KIND_HENNKA
+  0,    //KIND_BUTSURI
+  1,    //KIND_TOKUSYU
+};
+
+//--------------------------------------------------------------
+//	ARCIDの取得
+//--------------------------------------------------------------
+const u32 APP_COMMON_GetArcId(void)
+{
+  return ARCID_APP_MENU_COMMON;
+}
+
+//--------------------------------------------------------------
+//	タイプ系素材取得
+//--------------------------------------------------------------
+const u32 APP_COMMON_GetPokeTypePltArcIdx( void )
+{
+  return NARC_app_menu_common_p_st_type_NCLR;
+}
+
+const u8  APP_COMMON_GetPokeTypePltOffset( const PokeType type )
+{
+  return PokeTypePlttOffset[type];
+}
+
+const u32 APP_COMMON_GetPokeTypeCharArcIdx( const PokeType type )
+{
+  return NARC_app_menu_common_p_st_type_normal_NCGR + type;
+}
+
+const u32 APP_COMMON_GetPokeTypeCellArcIdx( const APP_COMMON_MAPPING mapping )
+{
+  return NARC_app_menu_common_p_st_type_32k_NCER + mapping;
+}
+
+const u32 APP_COMMON_GetPokeTypeAnimeArcIdx( const APP_COMMON_MAPPING mapping )
+{
+  return NARC_app_menu_common_p_st_type_32k_NANR + mapping;
+}
+
+//--------------------------------------------------------------
+//技分類系素材取得(パレット・セル・アニメはタイプと共通
+//--------------------------------------------------------------
+const u8  APP_COMMON_GetWazaKindPltOffset( const PokeType type )
+{
+  return WazaKindPlttOffset[type];
+}
+
+const u32 APP_COMMON_GetWazaKindCharArcIdx( const PokeType type )
+{
+  return NARC_app_menu_common_p_st_bunrui_henka_NCGR + type;
+}
+
