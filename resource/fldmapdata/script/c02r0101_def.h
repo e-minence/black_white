005253422d5cd311ba8998e8690aded99b4b298f
Toshinobu Matsumiya <matsumiya@gamefreak.co.jp>
2009-07-16 08:28:33 +0000

    スクリプトテーブル修正

--- resource/fldmapdata/script/c02r0101_def.h
+++ resource/fldmapdata/script/c02r0101_def.h
@@ -2,7 +2,7 @@
 #define _C02R0101_DEF_H_
 
 //スクリプトデータID定義 
-#define	SCRID_C02R0101		(1)
+#define	SCRID_C02R0101_TALKOBJ_BADMAN		(1)
 #define	SCRID_C02R0101_DATA_MAX		(1)		//最大数
 
 #endif //_C02R0101_DEF_H_
