157c2d90004c7adcb23c92a951581d682fc125a4
Toshinobu Matsumiya <matsumiya@gamefreak.co.jp>
2009-07-16 09:03:34 +0000

    スクリプトテーブル追加・修正

--- resource/fldmapdata/script/c02r0201_def.h
+++ resource/fldmapdata/script/c02r0201_def.h
@@ -2,7 +2,8 @@
 #define _C02R0201_DEF_H_
 
 //スクリプトデータID定義 
-#define	SCRID_C02R0201		(1)
-#define	SCRID_C02R0201_DATA_MAX		(1)		//最大数
+#define	SCRID_C02R0201_TALKOBJ_MUSICIAN		(1)
+#define	SCRID_C02R0201_TALKOBJ_BABYBOY1		(2)
+#define	SCRID_C02R0201_DATA_MAX		(2)		//最大数
 
 #endif //_C02R0201_DEF_H_
--- resource/fldmapdata/script/c02r0501_def.h
+++ resource/fldmapdata/script/c02r0501_def.h
@@ -2,7 +2,8 @@
 #define _C02R0501_DEF_H_
 
 //スクリプトデータID定義 
-#define	SCRID_C02R0501		(1)
-#define	SCRID_C02R0501_DATA_MAX		(1)		//最大数
+#define	SCRID_C02R0501_TALKOBJ_WOMAN2		(1)
+#define	SCRID_C02R0501_TALKOBJ_MAN2		(2)
+#define	SCRID_C02R0501_DATA_MAX		(2)		//最大数
 
 #endif //_C02R0501_DEF_H_
