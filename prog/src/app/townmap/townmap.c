52197a19393f8267b79578b28ee91eeab62829b8
Toru Nagihashi <nagihashi@gamefreak.co.jp>
2009-07-17 03:49:46 +0000

    タウンマップ新規アップ

---
+++ prog/src/app/townmap/townmap.c
@@ -0,0 +1,150 @@
+//[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[
+/**
+ *
+ *	@file		map_main.c
+ *	@brief	タウンマップメイン
+ *	@author	Toru=Nagihashi
+ *	@data		2009.07.17
+ *
+ */
+//]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]
+//lib
+#include <gflib.h>
+
+//	constant
+#include "system/main.h"	//HEAPID
+#include "system/gfl_use.h"
+
+//	archive
+#include "arc_def.h"
+#include "message.naix"
+//#include "msg/msg_townmap.h"
+#include "townmap_gra.naix"
+#include "font/font.naix"
+
+//mine
+#include "app/townmap.h"
+
+//=============================================================================
+/**
+ *					定数宣言
+*/
+//=============================================================================
+
+//=============================================================================
+/**
+ *					構造体宣言
+*/
+//=============================================================================
+//-------------------------------------
+///	メインワーク
+//=====================================
+typedef struct 
+{
+	int dummy;
+} TOWNMAP_WORK;
+
+//=============================================================================
+/**
+ *					プロトタイプ宣言
+*/
+//=============================================================================
+//-------------------------------------
+///	PROC
+//=====================================
+static GFL_PROC_RESULT TOWNMAP_PROC_Init(
+		GFL_PROC *p_proc, int *p_seq, void *p_param_adrs, void *p_wk_adrs );
+static GFL_PROC_RESULT TOWNMAP_PROC_Exit(
+		GFL_PROC *p_proc, int *p_seq, void *p_param_adrs, void *p_wk_adrs );
+static GFL_PROC_RESULT TOWNMAP_PROC_Main(
+		GFL_PROC *p_proc, int *p_seq, void *p_param_adrs, void *p_wk_adrs );
+
+//=============================================================================
+/**
+ *						GLOBAL
+ */
+//=============================================================================
+//-------------------------------------
+///	PROCデータ外部公開
+//=====================================
+const GFL_PROC_DATA	TownMap_ProcData	=
+{
+	TOWNMAP_PROC_Init,
+	TOWNMAP_PROC_Main,
+	TOWNMAP_PROC_Exit,
+};
+//=============================================================================
+/**
+ *						PROC
+ */
+//=============================================================================
+//----------------------------------------------------------------------------
+/**
+ *	@brief	PROC初期化
+ *
+ *	@param	GFL_PROC *p_proc	プロセス
+ *	@param	*p_seq						シーケンス
+ *	@param	*p_param_adrs					引数
+ *	@param	*p_wk_adrs						ワーク
+ *
+ *	@return	終了コード
+ */
+//-----------------------------------------------------------------------------
+static GFL_PROC_RESULT TOWNMAP_PROC_Init( GFL_PROC *p_proc, int *p_seq, void *p_param_adrs, void *p_wk_adrs )
+{	
+	TOWNMAP_WORK	*p_wk;
+	TOWNMAP_PARAM	*p_rank_param;
+	u16	data_len;
+
+	//ヒープ作成
+	GFL_HEAP_CreateHeap( GFL_HEAPID_APP, HEAPID_TOWNMAP, 0x40000 );
+
+	//ワーク作成
+	p_wk	= GFL_PROC_AllocWork( p_proc, sizeof(TOWNMAP_WORK), HEAPID_TOWNMAP );
+	GFL_STD_MemClear( p_wk, sizeof(TOWNMAP_WORK) );
+
+	return GFL_PROC_RES_FINISH;
+}
+//----------------------------------------------------------------------------
+/**
+ *	@brief	PROC破棄
+ *
+ *	@param	GFL_PROC *p_proc	プロセス
+ *	@param	*p_seq						シーケンス
+ *	@param	*p_param_adrs					引数
+ *	@param	*p_wk_adrs						ワーク
+ *
+ *	@return	終了コード
+ */
+//-----------------------------------------------------------------------------
+static GFL_PROC_RESULT TOWNMAP_PROC_Exit( GFL_PROC *p_proc, int *p_seq, void *p_param_adrs, void *p_wk_adrs )
+{	
+	TOWNMAP_WORK	*p_wk	= p_wk_adrs;
+
+	//ワーク破棄
+	GFL_PROC_FreeWork( p_proc );
+
+	//ヒープ破棄
+	GFL_HEAP_DeleteHeap(HEAPID_TOWNMAP );
+
+	return GFL_PROC_RES_FINISH;
+}
+//----------------------------------------------------------------------------
+/**
+ *	@brief	PROCメイン処理
+ *
+ *	@param	GFL_PROC *p_proc	プロセス
+ *	@param	*p_seq						シーケンス
+ *	@param	*p_param_adrs					引数
+ *	@param	*p_wk_adrs						ワーク
+ *
+ *	@return	終了コード
+ */
+//-----------------------------------------------------------------------------
+static GFL_PROC_RESULT TOWNMAP_PROC_Main( GFL_PROC *p_proc, int *p_seq, void *p_param_adrs, void *p_wk_adrs )
+{	
+	TOWNMAP_WORK	*p_wk	= p_wk_adrs;
+
+	return GFL_PROC_RES_CONTINUE;
+}
+
