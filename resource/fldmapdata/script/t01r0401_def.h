42a9777210bedceac877c5043546bfcdb71a2e86
Toshinobu Matsumiya <matsumiya@gamefreak.co.jp>
2009-07-23 10:41:46 +0000

    スクリプト追加・修正

--- resource/fldmapdata/script/t01r0401_def.h
+++ resource/fldmapdata/script/t01r0401_def.h
@@ -2,7 +2,8 @@
 #define _T01R0401_DEF_H_
 
 //スクリプトデータID定義 
-#define	SCRID_T01R0401_KABI32_1		(1)
-#define	SCRID_T01R0401_DATA_MAX		(1)		//最大数
+#define	SCRID_T01R0401_HAKASE2_ZUKAN		(1)
+#define	SCRID_T01R0401_KABI32_1		(2)
+#define	SCRID_T01R0401_DATA_MAX		(2)		//最大数
 
 #endif //_T01R0401_DEF_H_
