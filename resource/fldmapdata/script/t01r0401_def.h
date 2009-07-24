f619086f0789afcb67dc079cd057d3504edf6303
Toshinobu Matsumiya <matsumiya@gamefreak.co.jp>
2009-07-24 12:52:08 +0000

    図勧入手イベントを仮作成

--- resource/fldmapdata/script/t01r0401_def.h
+++ resource/fldmapdata/script/t01r0401_def.h
@@ -3,7 +3,9 @@
 
 //スクリプトデータID定義 
 #define	SCRID_T01R0401_HAKASE2_ZUKAN		(1)
-#define	SCRID_T01R0401_KABI32_1		(2)
-#define	SCRID_T01R0401_DATA_MAX		(2)		//最大数
+#define	SCRID_T01R0401_BLACK_01		(2)
+#define	SCRID_T01R0401_WHITE_01		(3)
+#define	SCRID_T01R0401_KABI32_1		(4)
+#define	SCRID_T01R0401_DATA_MAX		(4)		//最大数
 
 #endif //_T01R0401_DEF_H_
