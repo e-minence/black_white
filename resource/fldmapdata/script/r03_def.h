0d955d6ff548c03900b44cdf4500f62db56dc4b2
Toshinobu Matsumiya <matsumiya@gamefreak.co.jp>
2009-07-25 07:31:17 +0000

    3番道路に動作モデルを配置

--- resource/fldmapdata/script/r03_def.h
+++ resource/fldmapdata/script/r03_def.h
@@ -2,7 +2,10 @@
 #define _R03_DEF_H_
 
 //スクリプトデータID定義 
-#define	SCRID_R03		(1)
-#define	SCRID_R03_DATA_MAX		(1)		//最大数
+#define	SCRID_R03_TALKOBJ_OLDWOMAN1		(1)
+#define	SCRID_R03_TALKOBJ_WOMAN3		(2)
+#define	SCRID_R03_TALKOBJ_BABYGIRL1A		(3)
+#define	SCRID_R03_TALKOBJ_BABYGIRL1B		(4)
+#define	SCRID_R03_DATA_MAX		(4)		//最大数
 
 #endif //_R03_DEF_H_
