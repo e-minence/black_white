5c117960b8a538d28b6f89900c0608f0ac1a4e77
Toshinobu Matsumiya <matsumiya@gamefreak.co.jp>
2009-07-25 08:32:12 +0000

    C02PC0101に動作モデルを配置

--- resource/fldmapdata/script/c02pc0101_def.h
+++ resource/fldmapdata/script/c02pc0101_def.h
@@ -2,7 +2,8 @@
 #define _C02PC0101_DEF_H_
 
 //スクリプトデータID定義 
-#define	SCRID_C02PC0101		(1)
-#define	SCRID_C02PC0101_DATA_MAX		(1)		//最大数
+#define	SCRID_C02PC0101_TALKOBJ_GIRL2		(1)
+#define	SCRID_C02PC0101_TALKOBJ_BACKPACKERM		(2)
+#define	SCRID_C02PC0101_DATA_MAX		(2)		//最大数
 
 #endif //_C02PC0101_DEF_H_
