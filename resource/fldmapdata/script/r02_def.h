3fd710a0c35e8075fff518d50dc78e6af1548d19
Toshinobu Matsumiya <matsumiya@gamefreak.co.jp>
2009-07-25 06:41:52 +0000

    2番道路に動作モデルを配置

--- resource/fldmapdata/script/r02_def.h
+++ resource/fldmapdata/script/r02_def.h
@@ -4,6 +4,8 @@
 //スクリプトデータID定義 
 #define	SCRID_R02_TALKOBJ_BLACK		(1)
 #define	SCRID_R02_TALKOBJ_MAMA		(2)
-#define	SCRID_R02_DATA_MAX		(2)		//最大数
+#define	SCRID_R02_TALKOBJ_BOY2		(3)
+#define	SCRID_R02_TALKOBJ_GIRL2		(4)
+#define	SCRID_R02_DATA_MAX		(4)		//最大数
 
 #endif //_R02_DEF_H_
