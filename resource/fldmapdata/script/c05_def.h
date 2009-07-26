29f42165a074f4e11c4a458abf89abf17c5d2617
Toshinobu Matsumiya <matsumiya@gamefreak.co.jp>
2009-07-26 05:24:08 +0000

    c05用にスクリプト作成

--- resource/fldmapdata/script/c05_def.h
+++ resource/fldmapdata/script/c05_def.h
@@ -2,7 +2,13 @@
 #define _C05_DEF_H_
 
 //スクリプトデータID定義 
-#define	SCRID_C05		(1)
-#define	SCRID_C05_DATA_MAX		(1)		//最大数
+#define	SCRID_C05_TALKOBJ_WOMAN2		(1)
+#define	SCRID_C05_TALKOBJ_BABYGIRL1		(2)
+#define	SCRID_C05_TALKOBJ_BACKPACKERW		(3)
+#define	SCRID_C05_TALKOBJ_TRAINERM		(4)
+#define	SCRID_C05_TALKOBJ_DRIVER		(5)
+#define	SCRID_C05_TALKOBJ_BADMAN		(6)
+#define	SCRID_C05_TALKOBJ_WORKMAN		(7)
+#define	SCRID_C05_DATA_MAX		(7)		//最大数
 
 #endif //_C05_DEF_H_
