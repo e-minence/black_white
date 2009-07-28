3d9e8f0bf4f444d6f86ca049f2090a9c0c686c18
Toshinobu Matsumiya <matsumiya@gamefreak.co.jp>
2009-07-28 10:03:30 +0000

    ミュージカル用スクリプト仮配置

--- resource/fldmapdata/script/c01_def.h
+++ resource/fldmapdata/script/c01_def.h
@@ -2,14 +2,15 @@
 #define _C01_DEF_H_
 
 //スクリプトデータID定義 
-#define	SCRID_C01_TALKOBJ_BADMAN		(1)
-#define	SCRID_C01_TALKOBJ_BOY2		(2)
-#define	SCRID_C01_TALKOBJ_CLEANINGM		(3)
-#define	SCRID_C01_TALKOBJ_BREEDERW		(4)
-#define	SCRID_C01_TALKOBJ_WOMAN2		(5)
-#define	SCRID_C01_SIGN1		(6)
-#define	SCRID_C01_SIGN2		(7)
-#define	SCRID_C01_SIGN3		(8)
-#define	SCRID_C01_DATA_MAX		(8)		//最大数
+#define	SCRID_C01_MUSICAL		(1)
+#define	SCRID_C01_TALKOBJ_BADMAN		(2)
+#define	SCRID_C01_TALKOBJ_BOY2		(3)
+#define	SCRID_C01_TALKOBJ_CLEANINGM		(4)
+#define	SCRID_C01_TALKOBJ_BREEDERW		(5)
+#define	SCRID_C01_TALKOBJ_WOMAN2		(6)
+#define	SCRID_C01_SIGN1		(7)
+#define	SCRID_C01_SIGN2		(8)
+#define	SCRID_C01_SIGN3		(9)
+#define	SCRID_C01_DATA_MAX		(9)		//最大数
 
 #endif //_C01_DEF_H_
