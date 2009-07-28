6554e3e8ac31c41b0aa306244f1178f143b806a6
Hisashi Sogabe <sogabe@gamefreak.co.jp>
2009-07-28 11:39:00 +0000

    トレーナーデータ追加

---
+++ resource/trdata/trdata.h
@@ -0,0 +1,15 @@
+#define		__ASM_NO_DEF_
+#include	"..\..\prog\include\poke_tool\monsno_def.h"
+#include	"..\..\prog\include\tr_tool\trtype_def.h"
+#include	"..\..\prog\include\waza_tool\wazano_def.h"
+#include	"..\..\prog\include\item\itemsym.h"
+
+#define		DATATYPE_NORMAL	(0)
+#define		DATATYPE_WAZA	(1)
+#define		DATATYPE_ITEM	(2)
+#define		DATATYPE_MULTI	(3)
+
+//現状DPのコンバータを使用しているのでMAKEを通すために定義
+//のとほど削除する
+#define   FIGHT_TYPE_1vs1 ( 0 )
+#define   FIGHT_TYPE_2vs2 ( 1 )
