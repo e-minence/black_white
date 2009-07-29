d8f7c8cdd283213afce8e74a20bd5f718fea7811
Toshinobu Matsumiya <matsumiya@gamefreak.co.jp>
2009-07-29 03:57:44 +0000

    1番道路捕獲デモのイベントスクリプト

--- resource/fldmapdata/script/r01_def.h
+++ resource/fldmapdata/script/r01_def.h
@@ -2,9 +2,10 @@
 #define _R01_DEF_H_
 
 //スクリプトデータID定義 
-#define	SCRID_R01_TALKOBJ_BOY1		(1)
-#define	SCRID_R01_TALKOBJ_GIRL1		(2)
-#define	SCRID_R01_TALKOBJ_MAN1		(3)
-#define	SCRID_R01_DATA_MAX		(3)		//最大数
+#define	SCRID_R01_POKE_GETDEMO		(1)
+#define	SCRID_R01_TALKOBJ_BOY1		(2)
+#define	SCRID_R01_TALKOBJ_GIRL1		(3)
+#define	SCRID_R01_TALKOBJ_MAN1		(4)
+#define	SCRID_R01_DATA_MAX		(4)		//最大数
 
 #endif //_R01_DEF_H_
