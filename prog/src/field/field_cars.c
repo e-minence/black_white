e20c0afbeab5ef2229390aad04ec2f269ee677fb
Toshihiro Obata <obata_toshihiro@gamefreak.co.jp>
2009-07-29 08:28:43 +0000

    船を追加

--- prog/src/field/field_cars.c
+++ prog/src/field/field_cars.c
@@ -43,6 +43,29 @@
 #define START_MAX_INTERVAL  (150)
 
 
+// 船の数
+#define SHIP_NUM (1)
+
+// 船の移動範囲
+#define SHIP_RAIL_MIN_X (600)
+#define SHIP_RAIL_MAX_X (1500)
+#define SHIP_RAIL_Y     (-35)
+#define SHIP_RAIL_Z     (1300)
+
+// 船の発車範囲(プレイヤー位置)
+#define SHIP_START_MIN_Z   (750)
+#define SHIP_START_MAX_Z  (2000)
+
+// 船の移動速度
+#define SHIP_SPEED_MAX (3)
+#define SHIP_SPEED_MIN (1)
+
+// 船の発車間隔(単位:フレーム)
+#define SHIP_START_MIN_INTERVAL   (60)
+#define SHIP_START_MAX_INTERVAL  (150)
+
+
+
 //============================================================================== 
 /**
  * @brief インデックス
@@ -53,6 +76,7 @@
 enum RES_INDEX
 {
 	RES_INDEX_TRAILER_BMD,	// トレーラーのモデル
+	RES_INDEX_SHIP_BMD,		// 船のモデル
 	RES_INDEX_MAX
 };
 
@@ -60,6 +84,7 @@ enum RES_INDEX
 enum RND_INDEX
 {
 	RND_INDEX_TRAILER,		// トレーラー
+	RND_INDEX_SHIP,			// 船
 	RND_INDEX_MAX
 };
 
@@ -67,6 +92,7 @@ enum RND_INDEX
 enum OBJ_INDEX
 {
 	OBJ_INDEX_TRAILER,		// トレーラー
+	OBJ_INDEX_SHIP,			// 船
 	OBJ_INDEX_MAX
 };
 
@@ -87,6 +113,17 @@ typedef struct
 	fx32              speed;	// 移動速度
 }
 TRAILER; 
+
+//-----
+// 船
+//-----
+typedef struct 
+{
+	BOOL              active;	// 動いているかどうか
+	GFL_G3D_OBJSTATUS status;	// ステータス
+	fx32              speed;	// 移動速度
+}
+SHIP; 
 
 //------------
 // タイマー
@@ -110,8 +147,11 @@ struct _FIELD_CARS
 	GFL_G3D_RND* pRnd[ RND_INDEX_MAX ];	// 3Dレンダー
 	GFL_G3D_OBJ* pObj[ OBJ_INDEX_MAX ]; // 3Dオブジェクト
 
-	TRAILER trailer[ TRAILER_NUM ];	// トレーラー
-	TIMER   timer[ TRAILER_NUM ];	// タイマー
+	TRAILER trailer[ TRAILER_NUM ];			// トレーラー
+	TIMER   timerOfTrailer[ TRAILER_NUM ];	// タイマー
+
+	SHIP    ship[ SHIP_NUM ];			// 船
+	TIMER   timerOfShip[ TRAILER_NUM ];	// タイマー
 
 	BOOL active;	// 動作フラグ
 };
@@ -127,15 +167,20 @@ struct _FIELD_CARS
 // トレーラー動作
 static void MoveTrailer( TRAILER* p_trailer );
 
+// 船動作
+static void MoveShip( SHIP* p_ship );
+
 // システム初期化
 static void LoadResource( FIELD_CARS* p_sys );
 static void CreateRender( FIELD_CARS* p_sys );
 static void CreateObject( FIELD_CARS* p_sys );
 static void InitTrailers( FIELD_CARS* p_sys );
+static void InitShips( FIELD_CARS* p_sys );
 static void InitTimers( FIELD_CARS* p_sys );
 
 // システム進行
 static void ActTrailers( FIELD_CARS* p_sys );
+static void ActShips( FIELD_CARS* p_sys );
 
 
 //============================================================================== 
@@ -169,6 +214,9 @@ FIELD_CARS* FIELD_CARS_Create( FIELD_PLAYER* p_player, u16 zone_id, HEAPID heap_
 
 	// トレーラー初期化
 	InitTrailers( p_sys );
+
+	// 船の初期化
+	InitShips( p_sys );
 
 	// タイマー初期化
 	InitTimers( p_sys );
@@ -236,6 +284,9 @@ void FIELD_CARS_Process( FIELD_CARS* p_sys )
 
 	// トレーラーの動作
 	ActTrailers( p_sys ); 
+
+	// 船の動作
+	ActShips( p_sys );
 }
 
 
@@ -259,6 +310,15 @@ void FIELD_CARS_Draw( FIELD_CARS* p_sys )
 		if( p_sys->trailer[i].active == TRUE )
 		{ 
 			GFL_G3D_DRAW_DrawObject( p_sys->pObj[ OBJ_INDEX_TRAILER ], &p_sys->trailer[i].status );
+		}
+	}
+
+	// 動作中の船のみを描画する
+	for( i=0; i<SHIP_NUM; i++ )
+	{ 
+		if( p_sys->ship[i].active == TRUE )
+		{ 
+			GFL_G3D_DRAW_DrawObject( p_sys->pObj[ OBJ_INDEX_SHIP ], &p_sys->ship[i].status );
 		}
 	}
 }
@@ -292,6 +352,27 @@ static void MoveTrailer( TRAILER* p_trailer )
 	}
 }
 
+//------------------------------------------------------------------------------- 
+/**
+ * @brief 船を動かす
+ *
+ * @param p_ship 動かすトレーラー
+ */
+//------------------------------------------------------------------------------- 
+static void MoveShip( SHIP* p_ship )
+{ 
+	// アクティブでなければ, 動かない
+	if( p_ship->active != TRUE ) return;
+
+	// 移動
+	p_ship->status.trans.x += p_ship->speed;
+
+	// 移動範囲を超えたら, 動作終了
+	if( ( FX_Whole( p_ship->status.trans.x ) < SHIP_RAIL_MIN_X ) || ( SHIP_RAIL_MAX_X < FX_Whole( p_ship->status.trans.x ) ) )
+	{
+		p_ship->active = FALSE;
+	}
+}
 
 //------------------------------------------------------------------------------- 
 /**
@@ -307,8 +388,14 @@ static void LoadResource( FIELD_CARS* p_sys )
 		GFL_G3D_CreateResourceArc( 
 				ARCID_BMODEL_OUTDOOR, NARC_output_buildmodel_outdoor_trailer_01_nsbmd );
 
+	// 船のモデル
+	p_sys->pRes[ RES_INDEX_SHIP_BMD ] = 
+		GFL_G3D_CreateResourceArc( 
+				ARCID_BMODEL_OUTDOOR, NARC_output_buildmodel_outdoor_h01_ship_nsbmd );
+
 	// テクスチャ転送
 	GFL_G3D_TransVramTexture( p_sys->pRes[ RES_INDEX_TRAILER_BMD ] );
+	GFL_G3D_TransVramTexture( p_sys->pRes[ RES_INDEX_SHIP_BMD ] );
 
 	// DEBUG:
 	{
@@ -336,6 +423,13 @@ static void CreateRender( FIELD_CARS* p_sys )
 				0,
 				p_sys->pRes[ RES_INDEX_TRAILER_BMD ] ); 
 
+	// 船
+	p_sys->pRnd[ RND_INDEX_SHIP ] = 
+		GFL_G3D_RENDER_Create(
+				p_sys->pRes[ RES_INDEX_SHIP_BMD ],
+				0,
+				p_sys->pRes[ RES_INDEX_SHIP_BMD ] ); 
+
 	// DEBUG:
 	{
 		int i; 
@@ -358,6 +452,10 @@ static void CreateObject( FIELD_CARS* p_sys )
 	// トレーラー
 	p_sys->pObj[ OBJ_INDEX_TRAILER ]
 		= GFL_G3D_OBJECT_Create( p_sys->pRnd[ RND_INDEX_TRAILER ], NULL, 0 );
+
+	// 船
+	p_sys->pObj[ OBJ_INDEX_SHIP ]
+		= GFL_G3D_OBJECT_Create( p_sys->pRnd[ RND_INDEX_SHIP ], NULL, 0 );
 
 	// DEBUG:
 	{
@@ -389,6 +487,20 @@ static void InitTrailers( FIELD_CARS* p_sys )
 
 //------------------------------------------------------------------------------- 
 /**
+ * @brief 船を初期化する
+ *
+ * @param p_sys 初期化対象のシステム
+ */
+//------------------------------------------------------------------------------- 
+static void InitShips( FIELD_CARS* p_sys )
+{
+	p_sys->ship[0].active = FALSE;
+	VEC_Set( &p_sys->ship[0].status.scale, FX32_ONE, FX32_ONE, FX32_ONE );
+	MTX_Identity33( &p_sys->ship[0].status.rotate ); 
+}
+
+//------------------------------------------------------------------------------- 
+/**
  * @brief タイマーを初期化する
  *
  * @param p_sys 初期化対象のシステム
@@ -398,9 +510,16 @@ static void InitTimers( FIELD_CARS* p_sys )
 {
 	int i;
 
+	// トレーラー用タイマー
 	for( i=0; i<TRAILER_NUM; i++ )
 	{
-		p_sys->timer[i].count = 0;
+		p_sys->timerOfTrailer[i].count = 0;
+	}
+
+	// 船用タイマー
+	for( i=0; i<SHIP_NUM; i++ )
+	{
+		p_sys->timerOfShip[i].count = 0;
 	}
 }
 
@@ -436,14 +555,14 @@ static void ActTrailers( FIELD_CARS* p_sys )
 			// 移動が終わったら, タイマーセット
 			if( p_sys->trailer[i].active == FALSE )
 			{
-				p_sys->timer[i].count
+				p_sys->timerOfTrailer[i].count
 					= START_MIN_INTERVAL + GFL_STD_MtRand( START_MAX_INTERVAL - START_MIN_INTERVAL ); 
 
 				// 念のため
-				if( START_MAX_INTERVAL < p_sys->timer[i].count ) p_sys->timer[i].count = START_MAX_INTERVAL;
+				if( START_MAX_INTERVAL < p_sys->timerOfTrailer[i].count ) p_sys->timerOfTrailer[i].count = START_MAX_INTERVAL;
 
 				// DEBUG:
-				//OBATA_Printf("[%d]SetTimer : count = %d\n", i, p_sys->timer[i].count );
+				//OBATA_Printf("[%d]SetTimer : count = %d\n", i, p_sys->timerOfTrailer[i].count );
 			}
 		}
 		// 動いていない場合
@@ -453,7 +572,7 @@ static void ActTrailers( FIELD_CARS* p_sys )
 			if( player_is_in_area )
 			{
 				// 時間経過で発車
-				if( --p_sys->timer[i].count < 0 )
+				if( --p_sys->timerOfTrailer[i].count < 0 )
 				{
 					fx32 x = 0;
 					fx32 z = 0;
@@ -487,3 +606,80 @@ static void ActTrailers( FIELD_CARS* p_sys )
 	}
 }
 
+//------------------------------------------------------------------------------- 
+/**
+ * @brief 船を動かす
+ *
+ * @param p_sys 処理対象のシステム
+ */
+//------------------------------------------------------------------------------- 
+static void ActShips( FIELD_CARS* p_sys )
+{
+	int i;
+	VecFx32 pos;
+	BOOL player_is_in_area = FALSE;	// プレイヤーが発車領域内にいるかどうか
+
+	// プレイヤーの位置判定
+	FIELD_PLAYER_GetPos( p_sys->pFieldPlayer, &pos );
+	if( ( SHIP_START_MIN_Z < FX_Whole( pos.z ) ) && ( FX_Whole( pos.z ) < SHIP_START_MAX_Z ) )
+	{
+		player_is_in_area = TRUE;
+	}
+
+	// 全ての船をチェックする
+	for( i=0; i<SHIP_NUM; i++ )
+	{
+		// 動いている場合
+		if( p_sys->ship[i].active == TRUE )
+		{
+			// 移動させる
+			MoveShip( &p_sys->ship[i] );
+
+			// 移動が終わったら, タイマーセット
+			if( p_sys->ship[i].active == FALSE )
+			{
+				p_sys->timerOfShip[i].count
+					= SHIP_START_MIN_INTERVAL + GFL_STD_MtRand( SHIP_START_MAX_INTERVAL - SHIP_START_MIN_INTERVAL ); 
+
+				// 念のため
+				if( START_MAX_INTERVAL < p_sys->timerOfShip[i].count ) p_sys->timerOfShip[i].count = START_MAX_INTERVAL;
+
+				// DEBUG:
+				//OBATA_Printf("[%d]SetTimer : count = %d\n", i, p_sys->timerOfShip[i].count );
+			}
+		}
+		// 動いていない場合
+		else 
+		{ 
+			// 発車領域内にいれば, 発車判定を行う
+			if( player_is_in_area )
+			{
+				// 時間経過で発車
+				if( --p_sys->timerOfShip[i].count < 0 )
+				{
+					fx32 x = 0;
+					fx32 z = 0;
+					fx32 s = 0;
+
+					switch( i )
+					{
+						case 0:	
+							x = (SHIP_RAIL_MIN_X) << FX32_SHIFT;
+							z = (SHIP_RAIL_Z) << FX32_SHIFT; 
+							s = ( GFL_STD_MtRand( SHIP_SPEED_MAX - SHIP_SPEED_MIN ) + SHIP_SPEED_MIN ) << FX32_SHIFT;	
+							break;
+					}
+
+					p_sys->ship[i].active = TRUE;
+					p_sys->ship[i].status.trans.x  = x;
+					p_sys->ship[i].status.trans.y  = (SHIP_RAIL_Y) << FX32_SHIFT;
+					p_sys->ship[i].status.trans.z  = z;
+					p_sys->ship[i].speed  = s;
+
+					// DEBUG:
+					//OBATA_Printf( "[%d]ShipStart : speed = %d\n", i, FX_Whole( p_sys->ship[i].speed ) );
+				}
+			} 
+		}
+	}
+}
