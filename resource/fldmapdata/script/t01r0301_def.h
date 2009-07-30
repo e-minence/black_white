5a2de91baad10456c72bc27db07a7ab55200cb7d
Toshinobu Matsumiya <matsumiya@gamefreak.co.jp>
2009-07-30 05:43:28 +0000

    t01r0301、動作モデル変更調整

--- resource/fldmapdata/script/t01r0301_def.h
+++ resource/fldmapdata/script/t01r0301_def.h
@@ -2,7 +2,8 @@
 #define _T01R0301_DEF_H_
 
 //スクリプトデータID定義 
-#define	SCRID_T01R0301_OLDWOMAN1_1		(1)
-#define	SCRID_T01R0301_DATA_MAX		(1)		//最大数
+#define	SCRID_T01R0301_MIDDLEMAN2		(1)
+#define	SCRID_T01R0301_MIDDLEWOMAN2		(2)
+#define	SCRID_T01R0301_DATA_MAX		(2)		//最大数
 
 #endif //_T01R0301_DEF_H_
