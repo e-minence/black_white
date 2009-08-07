92b41a2685eabc2349cc0e2d87f58da3d5c409c4
Keita Kagaya <kagaya@gamefreak.co.jp>
2009-08-07 08:26:56 +0000

    land_data　narcコミットをやめ、中間ファイルをコミットする形に。

--- resource/fldmapdata/land_data/land_list.rb
+++ resource/fldmapdata/land_data/land_list.rb
@@ -8,30 +8,33 @@
 #=======================================================================
 # íËêî
 #=======================================================================
-FNAME_LAND_LIST = "land_list"
-FNAME_ARC_LIST = "land_arc_list"
-FNAME_MDL_LIST = "land_model_list"
-FNAME_3DPPACK_LIST = "land_depend_list"
+FNAME_OUTPUT_LIST = "land_output_list"
+FNAME_MODEL_LIST = "land_model_list"
+FNAME_DEPEND_LIST = "land_depend_list"
 FNAME_MAKE_DEPEND = "land_make_depend"
 
-LNAME_LAND_LIST = "CONCAT_TARGET"
-LNAME_MDL_LIST = "G3D_IMD"
-LNAME_3DPPACK_LIST = "LAND_3DPPACK_LIST"
+DIR_OUTPUT = "land_output"
 
-DIR_LANDRES = "DIR_LANDRES"
+DIRSTR_OUTPUT = "$(DIR_OUTPUT)"
+DIRSTR_TEMP = "$(DIR_TEMP)"
+DIRSTR_RES = "$(DIR_RES)"
+
+LNAME_MODEL_LIST = "LAND_MODEL_LIST"
+LNAME_DEPEND_LIST = "LAND_DEPEND_LIST"
+
+STR_BINLINKER = "$(BINLINKER)"
+STR_G3DCVTR = "$(G3DCVTR)"
 
 #=======================================================================
 # 
 #=======================================================================
-file_land_list = File.open( FNAME_LAND_LIST, "w" )
-file_arc_list = File.open( FNAME_ARC_LIST, "w" )
-file_model_list = File.open( FNAME_MDL_LIST, "w" )
-file_3dppack_list = File.open( FNAME_3DPPACK_LIST, "w" )
+file_output_list = File.open( FNAME_OUTPUT_LIST, "w" )
+file_model_list = File.open( FNAME_MODEL_LIST, "w" )
+file_depend_list = File.open( FNAME_DEPEND_LIST, "w" )
 file_make_depend = File.open( FNAME_MAKE_DEPEND, "w" )
 
-file_land_list.printf( "%s = \\\n", LNAME_LAND_LIST )
-file_model_list.printf( "%s = \\\n", LNAME_MDL_LIST )
-file_3dppack_list.printf( "%s = \\\n", LNAME_3DPPACK_LIST )
+file_model_list.printf( "%s = \\\n", LNAME_MODEL_LIST )
+file_depend_list.printf( "%s = \\\n", LNAME_DEPEND_LIST )
 
 yen_flg = 0
 
@@ -48,44 +51,56 @@ while file_name = ARGV.shift
 		end
     
 		if( yen_flg != 0 )
-			file_land_list.printf( " \\\n" )
 			file_model_list.printf( " \\\n" )
-			file_3dppack_list.printf( " \\\n" )
+			file_depend_list.printf( " \\\n" )
 		end
     
 		yen_flg = 1
 		column = line.split ","
 		name = column[1]
     
-    #land_list
-		file_land_list.printf( "\t$(%s)/%s.bin", DIR_LANDRES, name )
-		file_land_list.printf( " $(%s)/%s.imd", DIR_LANDRES, name )
-    
-    #land_arc_list
-		file_arc_list.printf( "\"land_3dppack/%s.3dppack\"\n", name )
+    #land_output_list
+		file_output_list.printf( "\"%s/%s.3dppack\"\n", DIR_OUTPUT, name )
     
     #land_model_list
-		file_model_list.printf( "\t$(%s)/%s.imd", DIR_LANDRES, name )
+		file_model_list.printf( "\t%s/%s.imd", DIRSTR_RES, name )
     
     #land_3dppack_list
-		file_3dppack_list.printf( "\tland_3dppack/%s.3dppack", name )
-
+		file_depend_list.printf( "\t%s/%s.3dppack", DIRSTR_OUTPUT, name )
+    
     #land_make_depend
     file_make_depend.printf( "#%s\n", name )
     
-    file_make_depend.printf( "land_3dppack/%s.3dppack: land_g3dcvtr/%s.nsbmd land_res/%s.bin land_res/%s.3dmd\n", name, name, name, name )
+    file_make_depend.printf(
+      "%s/%s.3dppack: %s/%s.nsbmd %s/%s.bin %s/%s.3dmd\n",
+      DIRSTR_OUTPUT, name,
+      DIRSTR_TEMP, name,
+      DIRSTR_RES, name,
+      DIRSTR_RES, name )
+    
     file_make_depend.printf( "\t@echo create 3dppack %s\n", name )
-    file_make_depend.printf( "\t@$(BINLINKER) land_g3dcvtr/%s.nsbmd land_res/%s.bin land_res/%s.3dmd land_3dppack/%s.3dppack WB\n\n", name, name, name, name )
-
-    file_make_depend.printf( "land_g3dcvtr/%s.nsbmd: land_res/%s.imd\n", name, name )
+    
+    file_make_depend.printf(
+      "\t@%s %s/%s.nsbmd %s/%s.bin %s/%s.3dmd %s/%s.3dppack WB\n\n",
+      STR_BINLINKER,
+      DIRSTR_TEMP, name,
+      DIRSTR_RES, name,
+      DIRSTR_RES, name,
+      DIRSTR_OUTPUT, name )
+    
+    file_make_depend.printf( "%s/%s.nsbmd: %s/%s.imd\n",
+      DIRSTR_TEMP, name,
+      DIRSTR_RES, name )
+    
     file_make_depend.printf( "\t@echo create_nsbmd %s\n", name )
-    file_make_depend.printf( "\t@$(G3DCVTR) land_res/%s.imd -o land_g3dcvtr/%s.nsbmd\n\n", name, name )
+    
+    file_make_depend.printf( "\t@%s %s/%s.imd -o %s/%s.nsbmd\n\n",
+      STR_G3DCVTR, DIRSTR_RES, name, DIRSTR_TEMP, name )
 	end
 	file_csv.close
 end
 
 file_make_depend.close
-file_3dppack_list.close
+file_depend_list.close
 file_model_list.close
-file_arc_list.close
-file_land_list.close
+file_output_list.close
