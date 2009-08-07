76f0a310ca80aeb2186c7c94ace20123cbaa2629
Masao Taya <taya@gamefreak.co.jp>
2009-08-07 07:00:32 +0000

    ROM焼き時、アサート情報をＬＣＤ表示するように。

--- prog/src/system/gfl_use.c
+++ prog/src/system/gfl_use.c
@@ -1,9 +1,9 @@
 //=============================================================================================
 /**
- * @file	gfl_use.c
- * @date	2006.12.06
- * @author	GAME FREAK inc.
- * @brief	ユーザーのGFライブラリ使用のためのスケルトンソース
+ * @file  gfl_use.c
+ * @date  2006.12.06
+ * @author  GAME FREAK inc.
+ * @brief ユーザーのGFライブラリ使用のためのスケルトンソース
  *
  */
 //=============================================================================================
@@ -11,6 +11,8 @@
 #include <nitro.h>
 
 #include <gflib.h>
+#include <assert_default.h>
+
 #include <procsys.h>
 #include <gf_overlay.h>
 #include <tcb.h>
@@ -37,41 +39,41 @@
 //------------------------------------------------------------------
 //------------------------------------------------------------------
 enum {
-	TCB_HINTR_MAX = 4,
-	TCB_VINTR_MAX = 16,
+  TCB_HINTR_MAX = 4,
+  TCB_VINTR_MAX = 16,
 };
 
 //------------------------------------------------------------------
 //------------------------------------------------------------------
 typedef struct {
     GFL_STD_RandContext publicRandContext;
-	GFL_TCBSYS *	TCBSysHintr;
-	void *		TCBMemHintr;
-	GFL_TCBSYS *	TCBSysVintr;
-	void *		TCBMemVintr;
+  GFL_TCBSYS *  TCBSysHintr;
+  void *    TCBMemHintr;
+  GFL_TCBSYS *  TCBSysVintr;
+  void *    TCBMemVintr;
 }GFL_USE_WORK;
 
 //------------------------------------------------------------------
 //------------------------------------------------------------------
 static const HEAP_INIT_HEADER hih[]={
-	{ HEAPSIZE_SYSTEM,	OS_ARENA_MAIN },
-	{ HEAPSIZE_APP,		OS_ARENA_MAIN },
+  { HEAPSIZE_SYSTEM,  OS_ARENA_MAIN },
+  { HEAPSIZE_APP,   OS_ARENA_MAIN },
 };
 
-static GFL_USE_WORK *	gfl_work = NULL;
-static int				GFL_USE_VintrCounter;
+static GFL_USE_WORK * gfl_work = NULL;
+static int        GFL_USE_VintrCounter;
 
 static void GFUser_PublicRandInit(void);
 static void gfluse_AssertFinish( void );
 
 //=============================================================================================
 //
-//			関数
+//      関数
 //
 //=============================================================================================
 //------------------------------------------------------------------
 /**
- * @brief	GFライブラリ利用部分：初期化
+ * @brief GFライブラリ利用部分：初期化
  *
  * InitSystemの内部で呼んでいるInitFileSystemはgflibの外では
  * 呼べないため、InitSystemを呼んでいる。
@@ -79,53 +81,59 @@ static void gfluse_AssertFinish( void );
 //------------------------------------------------------------------
 void GFLUser_Init(void)
 {
-	//InitSystem();
+  //InitSystem();
 #if 0
 #ifdef USE_MAINEXMEMORY
   OS_EnableMainExArena();
 #endif
 
-	// システム初期化
-	OS_Init();
-	OS_InitTick();
-	OS_InitAlarm();
-	// RTC初期化
-	RTC_Init();
-	// GX初期化
-	GX_Init();
-	// 固定小数点API初期化
-	FX_Init();
-	// ファイルシステム初期化
-	InitFileSystem();
+  // システム初期化
+  OS_Init();
+  OS_InitTick();
+  OS_InitAlarm();
+  // RTC初期化
+  RTC_Init();
+  // GX初期化
+  GX_Init();
+  // 固定小数点API初期化
+  FX_Init();
+  // ファイルシステム初期化
+  InitFileSystem();
 #endif
 
-	//ヒープシステム初期化
-	GFL_HEAP_Init(&hih[0],GFL_HEAPID_MAX,HEAPID_CHILD_MAX,0);	//メインアリーナ
-	GFL_HEAP_DTCM_Init( 0x2000 );				//ＤＴＣＭアリーナ
+  //ヒープシステム初期化
+  GFL_HEAP_Init(&hih[0],GFL_HEAPID_MAX,HEAPID_CHILD_MAX,0); //メインアリーナ
+  GFL_HEAP_DTCM_Init( 0x2000 );       //ＤＴＣＭアリーナ
 
     //STD 標準ライブラリ初期化（乱数やCRC）
     GFL_STD_Init(GFL_HEAPID_SYSTEM);
-	//アーカイブシステム初期化
-	GFL_ARC_Init(&ArchiveFileTable[0],ARCID_TABLE_MAX);
+  //アーカイブシステム初期化
+  GFL_ARC_Init(&ArchiveFileTable[0],ARCID_TABLE_MAX);
 
-	//UIシステム初期化
-	GFL_UI_Boot(GFL_HEAPID_SYSTEM);
+  //UIシステム初期化
+  GFL_UI_Boot(GFL_HEAPID_SYSTEM);
 
-	// アサート停止関数の設定（コールスタック表示＆可能なら処理続行のため）
-	GFL_ASSERT_SetDisplayFunc( NULL, NULL, gfluse_AssertFinish );
+  // アサート停止関数の設定
+  if( OS_GetConsoleType() & OS_CONSOLE_NITRO ){
+    // ROM実行用（可能なら情報をLCD表示）
+    GFL_ASSERT_SetLCDMode();
+  }else{
+    // NITROデバッガ用（コールスタック表示＆可能なら処理続行のため）
+    GFL_ASSERT_SetDisplayFunc( NULL, NULL, gfluse_AssertFinish );
+  }
 
-	//OVERLAYシステム初期化
-	GFL_OVERLAY_boot(GFL_HEAPID_SYSTEM, 8, 4, 4);
+  //OVERLAYシステム初期化
+  GFL_OVERLAY_boot(GFL_HEAPID_SYSTEM, 8, 4, 4);
 
-	//PROCシステム初期化
-	GFL_PROC_boot(GFL_HEAPID_SYSTEM);
-	gfl_work = GFL_HEAP_AllocMemory(GFL_HEAPID_SYSTEM, sizeof(GFL_USE_WORK));
-	gfl_work->TCBMemHintr = GFL_HEAP_AllocMemory(
-		  GFL_HEAPID_SYSTEM, GFL_TCB_CalcSystemWorkSize(TCB_HINTR_MAX));
-	gfl_work->TCBSysHintr = GFL_TCB_Init(TCB_HINTR_MAX, gfl_work->TCBMemHintr);
-	gfl_work->TCBMemVintr = GFL_HEAP_AllocMemory(
-		  GFL_HEAPID_SYSTEM, GFL_TCB_CalcSystemWorkSize(TCB_VINTR_MAX));
-	gfl_work->TCBSysVintr = GFL_TCB_Init(TCB_VINTR_MAX, gfl_work->TCBMemVintr);
+  //PROCシステム初期化
+  GFL_PROC_boot(GFL_HEAPID_SYSTEM);
+  gfl_work = GFL_HEAP_AllocMemory(GFL_HEAPID_SYSTEM, sizeof(GFL_USE_WORK));
+  gfl_work->TCBMemHintr = GFL_HEAP_AllocMemory(
+      GFL_HEAPID_SYSTEM, GFL_TCB_CalcSystemWorkSize(TCB_HINTR_MAX));
+  gfl_work->TCBSysHintr = GFL_TCB_Init(TCB_HINTR_MAX, gfl_work->TCBMemHintr);
+  gfl_work->TCBMemVintr = GFL_HEAP_AllocMemory(
+      GFL_HEAPID_SYSTEM, GFL_TCB_CalcSystemWorkSize(TCB_VINTR_MAX));
+  gfl_work->TCBSysVintr = GFL_TCB_Init(TCB_VINTR_MAX, gfl_work->TCBMemVintr);
 
    // 汎用乱数初期化
     GFUser_PublicRandInit();
@@ -136,146 +144,146 @@ void GFLUser_Init(void)
     //サウンドシステム初期化
     //GFL_SOUND_Init(&sndHeap[0],SOUND_HEAP_SIZE);
 
-	//バックアップシステム初期化
-	GFL_BACKUP_Init(GFL_HEAPID_APP, GFL_HEAPID_APP);
+  //バックアップシステム初期化
+  GFL_BACKUP_Init(GFL_HEAPID_APP, GFL_HEAPID_APP);
 
-	//システムフォント初期化
-	GFL_TEXT_CreateSystem( NULL );
+  //システムフォント初期化
+  GFL_TEXT_CreateSystem( NULL );
 
-	//RTC初期化
-	GFL_RTC_Init();
-	
-	GFL_USE_VintrCounter = 0;
+  //RTC初期化
+  GFL_RTC_Init();
+
+  GFL_USE_VintrCounter = 0;
 }
 
 
 //------------------------------------------------------------------
 /**
- * @brief	GFライブラリ利用部分：メイン呼び出し
+ * @brief GFライブラリ利用部分：メイン呼び出し
  *
  * 毎フレーム呼び出される必要があるメイン処理を記述する
  */
 //------------------------------------------------------------------
 void GFLUser_Main(void)
 {
-	GFL_UI_Main();
+  GFL_UI_Main();
   GFL_NET_Main();  //キーの処理の後すぐに通信を処理したい為ここに配置
-	NetOld_Update();
-	
+  NetOld_Update();
+
 #ifdef PM_DEBUG
   //デバッグシステム
   DEBUGWIN_UpdateSystem();
-	//Lを押している間停止。L押しながらRで1コマ送る
-	if( DEBUG_PAUSE_Update() == TRUE &&
-	    DEBUGWIN_IsActive() == FALSE )
+  //Lを押している間停止。L押しながらRで1コマ送る
+  if( DEBUG_PAUSE_Update() == TRUE &&
+      DEBUGWIN_IsActive() == FALSE )
 #endif
-	{
-		GFL_PROC_Main();
-		GFL_FADE_Main();
-	}
-	GFL_RTC_Main();
-	//GFL_SOUND_Main();
+  {
+    GFL_PROC_Main();
+    GFL_FADE_Main();
+  }
+  GFL_RTC_Main();
+  //GFL_SOUND_Main();
 }
 
 //------------------------------------------------------------------
 /**
- * @brief	GFライブラリ利用部分：描画処理
+ * @brief GFライブラリ利用部分：描画処理
  *
  * 毎フレーム呼び出される必要がある描画処理を記述する
  */
 //------------------------------------------------------------------
 void GFLUser_Display(void)
 {
-	//GFL_OAM_sysMain(...);
-	//GFL_BG_sysMain(...);
+  //GFL_OAM_sysMain(...);
+  //GFL_BG_sysMain(...);
 }
 
 //------------------------------------------------------------------
 /**
- * @brief	GFライブラリ利用部分：終了処理
+ * @brief GFライブラリ利用部分：終了処理
  */
 //------------------------------------------------------------------
 void GFLUser_Exit(void)
 {
-	GFL_TEXT_DeleteSystem();
-	GFL_BACKUP_Exit();
-	//GFL_SOUND_Exit();
-	GFL_FADE_Exit();
-	GFL_PROC_Exit();
-	GFL_TCB_Exit(gfl_work->TCBSysVintr);
-	GFL_HEAP_FreeMemory(gfl_work->TCBSysVintr);
-	GFL_HEAP_FreeMemory(gfl_work->TCBMemVintr);
-	GFL_OVERLAY_Exit();
-	GFL_UI_Exit();
-	GFL_HEAP_DTCM_Exit();
-	GFL_HEAP_Exit();
+  GFL_TEXT_DeleteSystem();
+  GFL_BACKUP_Exit();
+  //GFL_SOUND_Exit();
+  GFL_FADE_Exit();
+  GFL_PROC_Exit();
+  GFL_TCB_Exit(gfl_work->TCBSysVintr);
+  GFL_HEAP_FreeMemory(gfl_work->TCBSysVintr);
+  GFL_HEAP_FreeMemory(gfl_work->TCBMemVintr);
+  GFL_OVERLAY_Exit();
+  GFL_UI_Exit();
+  GFL_HEAP_DTCM_Exit();
+  GFL_HEAP_Exit();
 }
 
 //------------------------------------------------------------------
 /**
- * @brief	GFライブラリ利用部分：HBlank割り込み処理
+ * @brief GFライブラリ利用部分：HBlank割り込み処理
  */
 //------------------------------------------------------------------
 void GFLUser_HIntr(void)
 {
-	GFL_TCB_Main(gfl_work->TCBSysHintr);
+  GFL_TCB_Main(gfl_work->TCBSysHintr);
 }
 
 //------------------------------------------------------------------
 /**
- * @brief	GFライブラリ利用部分：HBlank中TCBの登録処理
+ * @brief GFライブラリ利用部分：HBlank中TCBの登録処理
  */
 //------------------------------------------------------------------
 GFL_TCB * GFUser_HIntr_CreateTCB(GFL_TCB_FUNC * func, void * work, u32 pri)
 {
-	return GFL_TCB_AddTask(gfl_work->TCBSysHintr, func, work, pri);
+  return GFL_TCB_AddTask(gfl_work->TCBSysHintr, func, work, pri);
 }
 
 //------------------------------------------------------------------
 /**
- * @brief	GFライブラリ利用部分：VBlank割り込み処理
+ * @brief GFライブラリ利用部分：VBlank割り込み処理
  */
 //------------------------------------------------------------------
 void GFLUser_VIntr(void)
 {
-	GFL_TCB_Main(gfl_work->TCBSysVintr);
-	GFL_BG_VBlankFunc();
-	// Vブランク期間で実行します。
-	// ただ、ユニットの描画が行われていないのに
-	// この関数を実行すると、描画しているOBJが消えてしまうため
-	// 割り込みないで呼ばないほうが良いかもしれません。
-		//開放しました Ariizumi1031
-	//GFL_CLACT_VBlankFunc();
+  GFL_TCB_Main(gfl_work->TCBSysVintr);
+  GFL_BG_VBlankFunc();
+  // Vブランク期間で実行します。
+  // ただ、ユニットの描画が行われていないのに
+  // この関数を実行すると、描画しているOBJが消えてしまうため
+  // 割り込みないで呼ばないほうが良いかもしれません。
+    //開放しました Ariizumi1031
+  //GFL_CLACT_VBlankFunc();
     // 通信アイコンの描画のためにあります。通信自体は行っていません
     //GFL_NET_VBlankFunc();
 
-	GFL_DMA_Main();
-	GFL_USE_VintrCounter++;
+  GFL_DMA_Main();
+  GFL_USE_VintrCounter++;
 }
 
 //------------------------------------------------------------------
 /**
- * @brief	GFライブラリ利用部分：VBlank中TCBの登録処理
+ * @brief GFライブラリ利用部分：VBlank中TCBの登録処理
  */
 //------------------------------------------------------------------
 GFL_TCB * GFUser_VIntr_CreateTCB(GFL_TCB_FUNC * func, void * work, u32 pri)
 {
-	return GFL_TCB_AddTask(gfl_work->TCBSysVintr, func, work, pri);
+  return GFL_TCB_AddTask(gfl_work->TCBSysVintr, func, work, pri);
 }
 
 //------------------------------------------------------------------
 /**
- * @brief	GFライブラリ利用部分：VBlank中TCBSYSの取得
+ * @brief GFライブラリ利用部分：VBlank中TCBSYSの取得
  */
 //------------------------------------------------------------------
 GFL_TCBSYS * GFUser_VIntr_GetTCBSYS( void )
 {
-	return gfl_work->TCBSysVintr;
+  return gfl_work->TCBSysVintr;
 }
 
 //------------------------------------------------------------------
 /**
- * @brief	GFUser 汎用乱数作成
+ * @brief GFUser 汎用乱数作成
  */
 //------------------------------------------------------------------
 static void GFUser_PublicRandInit(void)
@@ -285,7 +293,7 @@ static void GFUser_PublicRandInit(void)
 
 //------------------------------------------------------------------
 /**
- * @brief	GFUser 汎用乱数を得る
+ * @brief GFUser 汎用乱数を得る
  */
 //------------------------------------------------------------------
 u32 GFUser_GetPublicRand(u32 range)
@@ -295,28 +303,28 @@ u32 GFUser_GetPublicRand(u32 range)
 
 //------------------------------------------------------------------
 /**
- * @brief	VBlankカウンター取得とリセット
+ * @brief VBlankカウンター取得とリセット
  */
 //------------------------------------------------------------------
 void GFUser_VIntr_ResetVblankCounter( void )
 {
-	GFL_USE_VintrCounter = 0;
+  GFL_USE_VintrCounter = 0;
 }
 
 int GFUser_VIntr_GetVblankCounter( void )
 {
-	return GFL_USE_VintrCounter;
+  return GFL_USE_VintrCounter;
 }
 
 //--------------------------------------------------------------------------
 /**
- * @brief	ArchiveIDでファイル名を取得する
+ * @brief ArchiveIDでファイル名を取得する
  */
 //--------------------------------------------------------------------------
 const char * GFUser_GetFileNameByArcID(ARCID arcID)
 {
-	GF_ASSERT(arcID < ARCID_TABLE_MAX);
-	return ArchiveFileTable[arcID];
+  GF_ASSERT(arcID < ARCID_TABLE_MAX);
+  return ArchiveFileTable[arcID];
 }
 
 //--------------------------------------------------------------------------
@@ -330,22 +338,22 @@ const char * GFUser_GetFileNameByArcID(ARCID arcID)
 //--------------------------------------------------------------------------
 static void gfluse_AssertFinish( void )
 {
-	int key_cont;
+  int key_cont;
 
-	while(1){
+  while(1){
 
-		GFL_UI_Main();
-		key_cont = GFL_UI_KEY_GetCont();
+    GFL_UI_Main();
+    key_cont = GFL_UI_KEY_GetCont();
 
-		// キーが押されるまでループ
-		if( (key_cont & PAD_BUTTON_L) && (key_cont & PAD_BUTTON_R) &&
-			(key_cont & PAD_BUTTON_X) && (key_cont & PAD_BUTTON_Y) ){
-			return ;
-		}
+    // キーが押されるまでループ
+    if( (key_cont & PAD_BUTTON_L) && (key_cont & PAD_BUTTON_R) &&
+      (key_cont & PAD_BUTTON_X) && (key_cont & PAD_BUTTON_Y) ){
+      return ;
+    }
 
-		// VBlank待ち
-		// (これを有効にすると、デバッカで停止させたときにコールスタックが表示されない)
-//		OS_WaitIrq(TRUE,OS_IE_V_BLANK);
-	}
+    // VBlank待ち
+    // (これを有効にすると、デバッカで停止させたときにコールスタックが表示されない)
+//    OS_WaitIrq(TRUE,OS_IE_V_BLANK);
+  }
 }
 
