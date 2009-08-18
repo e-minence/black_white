25bf1cc5d6d135e11a709679ea639c6476a65e1a
Sosuke Tamada <tamada@gamefreak.co.jp>
2009-08-18 09:18:28 +0000

    subversion(command line)のインストール箇所を変更した

--- tools/personal/tamada_prompt.bat
+++ tools/personal/tamada_prompt.bat
@@ -4,5 +4,6 @@ REM		玉田の個人用設定（prompt.batから呼び出される）
 REM
 REM ##################################################
 doskey arc=cd c:\home\pokemon_wb\prog\arc
-PATH=C:\tools\Subversion\bin;%PATH%c:\tools\mytools;c:\tools\glo561wb\bin;
+REM PATH=C:\tools\Subversion\bin;%PATH%c:\tools\mytools;%PROJECT_ROOT%tools/maketools;c:\tools\glo561wb\bin;
+PATH="C:\Program Files\Subversion\bin";%PATH%c:\tools\mytools;%PROJECT_ROOT%tools/maketools;c:\tools\glo561wb\bin;
 
