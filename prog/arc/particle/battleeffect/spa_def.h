5beb80bebf913da487ab84bbfec6067651ccada0
Hisashi Sogabe <sogabe@gamefreak.co.jp>
2009-08-20 02:27:10 +0000

    使用しなくなったファイルをフォルダごと削除

--- prog/arc/particle/battleeffect/spa_def.h
+++
@@ -1,9 +0,0 @@
-#ifndef	_SPA_H_
-#define	_SPA_H_
-
-#define	BE_000_EC001_1A_SPA			(0)
-#define	BE_BALL_NORMAL_1_SPA		(1)
-#define	BE_M_BALL_00_KAITEN_SPA		(2)
-#define	BE_M_BALL_00_OPEN1_SPA		(3)
-
-#endif
