9318445239ba46d5332634d1c6f63407af10f69c
Toshihiro Obata <obata_toshihiro@gamefreak.co.jp>
2009-08-27 12:55:15 +0000

    場面転換時にトレーラーと船の座標を記憶するように修正

--- prog/src/field/field_gimmick_h01.c
+++ prog/src/field/field_gimmick_h01.c
@@ -2,6 +2,8 @@
 #include "fieldmap.h"
 #include "field_gimmick_h01.h"
 #include "arc/fieldmap/buildmodel_outdoor.naix"
+#include "savedata/gimmickwork.h"
+#include "field_gimmick_def.h"
 
 
 //======================================================================
@@ -118,12 +120,26 @@ GFL_G3D_UTIL_SETUP setup2 = {
 //--------------------------------------------------------------------
 void H01_GIMMICK_Setup( FIELDMAP_WORK* fieldmap )
 {
-  FLD_EXP_OBJ_CNT_PTR ptr = FIELDMAP_GetExpObjCntPtr( fieldmap );
+  FLD_EXP_OBJ_CNT_PTR ptr = FIELDMAP_GetExpObjCntPtr( fieldmap ); 
+  GAMESYS_WORK*      gsys = FIELDMAP_GetGameSysWork( fieldmap );
+  GAMEDATA*      gamedata = GAMESYSTEM_GetGameData( gsys );
+  SAVE_CONTROL_WORK*   sv = GAMEDATA_GetSaveControlWork( gamedata );
+  GIMMICKWORK*    gmkwork = SaveData_GetGimmickWork( sv );
+  fx32              pos[] = { TRAILER_RAIL_Z_MIN, TRAILER_RAIL_Z_MAX, SHIP_RAIL_X_MIN };
+
+  // セーブデータをチェック
+  if( GIMMICKWORK_GetAssignID( gmkwork ) == FLD_GIMMICK_H01 )
+  {
+    int work_adrs = (int)GIMMICKWORK_Get( gmkwork, FLD_GIMMICK_H01 );
+    pos[0] = *( (fx32*)( work_adrs + 0 ) );
+    pos[1] = *( (fx32*)( work_adrs + 4 ) );
+    pos[2] = *( (fx32*)( work_adrs + 8 ) );
+  }
 
   // トレーラー01
   {
     GFL_G3D_OBJSTATUS status;
-    VEC_Set( &status.trans, TRAILER_RAIL_X_01, TRAILER_RAIL_Y, TRAILER_RAIL_Z_MIN );
+    VEC_Set( &status.trans, TRAILER_RAIL_X_01, TRAILER_RAIL_Y, pos[0] );
     VEC_Set( &status.scale, FX32_ONE, FX32_ONE, FX32_ONE );
     MTX_RotY33( &status.rotate, FX_SinIdx( 32768 ), FX_CosIdx( 32768 ) );	  // y軸180度回転
     FLD_EXP_OBJ_AddUnit( ptr, &setup0, &status, UNIT_IDX_TRAILER_01 );
@@ -131,7 +147,7 @@ void H01_GIMMICK_Setup( FIELDMAP_WORK* fieldmap )
   // トレーラー02
   {
     GFL_G3D_OBJSTATUS status;
-    VEC_Set( &status.trans, TRAILER_RAIL_X_02, TRAILER_RAIL_Y, TRAILER_RAIL_Z_MAX );
+    VEC_Set( &status.trans, TRAILER_RAIL_X_02, TRAILER_RAIL_Y, pos[1] );
     VEC_Set( &status.scale, FX32_ONE, FX32_ONE, FX32_ONE );
     MTX_Identity33( &status.rotate );
     FLD_EXP_OBJ_AddUnit( ptr, &setup1, &status, UNIT_IDX_TRAILER_02 );
@@ -139,7 +155,7 @@ void H01_GIMMICK_Setup( FIELDMAP_WORK* fieldmap )
   // 船
   {
     GFL_G3D_OBJSTATUS status;
-    VEC_Set( &status.trans, SHIP_RAIL_X_MIN, SHIP_RAIL_Y, SHIP_RAIL_Z );
+    VEC_Set( &status.trans, pos[2], SHIP_RAIL_Y, SHIP_RAIL_Z );
     VEC_Set( &status.scale, FX32_ONE, FX32_ONE, FX32_ONE );
     MTX_Identity33( &status.rotate );
     FLD_EXP_OBJ_AddUnit( ptr, &setup2, &status, UNIT_IDX_SHIP );
@@ -153,16 +169,34 @@ void H01_GIMMICK_Setup( FIELDMAP_WORK* fieldmap )
 //--------------------------------------------------------------------
 void H01_GIMMICK_End( FIELDMAP_WORK* fieldmap )
 {
-  int i;
-  FLD_EXP_OBJ_CNT_PTR ptr = FIELDMAP_GetExpObjCntPtr( fieldmap ); 
 
-  // 全ユニットを破棄
-  /*
-  for(i=0; i<UNIT_IDX_MAX; i++)
+  FLD_EXP_OBJ_CNT_PTR ptr = FIELDMAP_GetExpObjCntPtr( fieldmap ); 
+  GAMESYS_WORK*      gsys = FIELDMAP_GetGameSysWork( fieldmap );
+  GAMEDATA*      gamedata = GAMESYSTEM_GetGameData( gsys );
+  SAVE_CONTROL_WORK*   sv = GAMEDATA_GetSaveControlWork( gamedata );
+  GIMMICKWORK*    gmkwork = SaveData_GetGimmickWork( sv );
+  int           work_adrs = 0;
+  int           data_size = 0;
+
+  // 仕掛け用ワークを初期化
+  work_adrs = (int)GIMMICKWORK_Assign( gmkwork, FLD_GIMMICK_H01 );
+
+  // データを保存
   {
-    FLD_EXP_OBJ_DelUnit( ptr, UNIT_IDX_TRAILER_01 );
+    GFL_G3D_OBJSTATUS* status            = FLD_EXP_OBJ_GetUnitObjStatus( ptr, UNIT_IDX_TRAILER_01 );
+    *( (fx32*)(work_adrs + data_size) )  = status->trans.z;
+    data_size                           += sizeof( fx32 );
   }
-  */
+  {
+    GFL_G3D_OBJSTATUS* status            = FLD_EXP_OBJ_GetUnitObjStatus( ptr, UNIT_IDX_TRAILER_02 );
+    *( (fx32*)(work_adrs + data_size) )  = status->trans.z;
+    data_size                           += sizeof( fx32 );
+  }
+  {
+    GFL_G3D_OBJSTATUS* status            = FLD_EXP_OBJ_GetUnitObjStatus( ptr, UNIT_IDX_SHIP );
+    *( (fx32*)(work_adrs + data_size) )  = status->trans.x;
+    data_size                           += sizeof( fx32 );
+  } 
 } 
 
 //--------------------------------------------------------------------
