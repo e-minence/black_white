2ec333333cb21e2bb035d5838c07d297d7a2a192
Keita Kagaya <kagaya@gamefreak.co.jp>
2009-08-28 06:15:26 +0000

    land_data_exを統合。

--- resource/fldmapdata/land_data/land_list.rb
+++ resource/fldmapdata/land_data/land_list.rb
@@ -4,13 +4,14 @@
 # land_list.rb land.csv..
 # land.csv テキスト変換済みの地形データ管理表 複数指定可能
 #=======================================================================
+$KCODE = "SJIS"
 
 #=======================================================================
 # 定数
 #=======================================================================
 #作成するファイル名
 FNAME_OUTPUT_LIST = "land_output_list"
-FNAME_MODEL_LIST = "land_model_list"
+FNAME_TEMP_LIST = "land_model_list"
 FNAME_DEPEND_LIST = "land_depend_list"
 FNAME_MAKE_DEPEND = "land_make_depend"
 
@@ -24,7 +25,7 @@ DIRSTR_TEMP = "$(DIR_TEMP)"
 DIRSTR_RES = "$(DIR_RES)"
 
 #makefileで使用するラベル名
-LNAME_MODEL_LIST = "LAND_MODEL_LIST"
+LNAME_TEMP_LIST = "LAND_TEMP_LIST"
 LNAME_DEPEND_LIST = "LAND_DEPEND_LIST"
 
 #makefileで定義されている文字列
@@ -35,6 +36,13 @@ STR_G3DCVTR = "$(G3DCVTR)"
 FNAME_DMYFILE_GRID_3DMD = "dmygmap.3dmd"
 FNAME_DMYFILE_GRID_BIN = "dmygmap.bin"
 FNAME_DMYFILE_GRID_IMD = "dmygmap.imd"
+FNAME_DMYFILE_NOGRID_3DMD = "dummy_ng.3dmd"
+FNAME_DMYFILE_NOGRID_IMD = "dummy_ng.imd"
+FNAME_DMYFILE_NOGRID_H_IMD = "dummy_h_ng.imd"
+
+#マップタイプ
+MAPTYPESTR_GRID = "MAPTYPE_GRID"
+MAPTYPESTR_NOGRID = "MAPTYPE_NOGRID"
 
 #=======================================================================
 #	ファイルコピー
@@ -48,14 +56,159 @@ def file_copy( srcpath, copypath )
 end
 
 #=======================================================================
+# マップタイプ　グリッド用一覧書き込み
+#=======================================================================
+def file_write_grid( name,
+  file_output_list, file_temp_list, file_depend_list, file_make_depend )
+  #land_output_list
+	file_output_list.printf( "\"%s/%s.3dppack\"\n", DIR_OUTPUT, name )
+  
+  #land_temp_list
+	file_temp_list.printf( "\t%s/%s.nsbmd", DIRSTR_TEMP, name )
+  
+  #land_depend_list
+	file_depend_list.printf( "\t%s/%s.3dppack", DIRSTR_OUTPUT, name )
+  
+  #land_make_depend 3dppack
+  file_make_depend.printf( "#%s\n", name )
+   
+  file_make_depend.printf(
+    "%s/%s.3dppack: %s/%s.nsbmd %s/%s.bin %s/%s.3dmd\n",
+    DIRSTR_OUTPUT, name,
+    DIRSTR_TEMP, name,
+    DIRSTR_RES, name,
+    DIRSTR_RES, name )
+  
+  file_make_depend.printf( "\t@echo create 3dppack %s\n", name )
+  
+  file_make_depend.printf(
+    "\t@%s %s/%s.nsbmd %s/%s.bin %s/%s.3dmd %s/%s.3dppack WB\n\n",
+    STR_BINLINKER,
+    DIRSTR_TEMP, name,
+    DIRSTR_RES, name,
+    DIRSTR_RES, name,
+    DIRSTR_OUTPUT, name )
+  
+  #land_make_depend nsbmd
+  file_make_depend.printf( "%s/%s.nsbmd: %s/%s.imd\n",
+    DIRSTR_TEMP, name,
+    DIRSTR_RES, name )
+
+  file_make_depend.printf( "\t@echo create_nsbmd %s\n", name )
+  
+  file_make_depend.printf( "\t@%s %s/%s.imd -o %s/%s.nsbmd\n\n",
+    STR_G3DCVTR, DIRSTR_RES, name, DIRSTR_TEMP, name )
+  
+  #land_make_depend exist file check
+  check = DIR_RES + "/" + name + ".imd"
+  if( FileTest.exist?(check) != true )
+    printf( "%s.imdをダミーファイルから生成します\n", name )
+    file_copy( DIR_RES+"/"+FNAME_DMYFILE_GRID_IMD, check )
+  end
+ 
+  check = DIR_RES + "/" + name + ".bin"
+  if( FileTest.exist?(check) != true )
+    printf( "%s.binをダミーファイルから生成します\n", name )
+    file_copy( DIR_RES+"/"+FNAME_DMYFILE_GRID_BIN, check )
+  end
+  
+  check = DIR_RES + "/" + name + ".3dmd"
+  if( FileTest.exist?(check) != true )
+    printf( "%s.3dmdをダミーファイルから生成します\n", name )
+    file_copy( DIR_RES+"/"+FNAME_DMYFILE_GRID_3DMD, check )
+  end
+end
+
+#=======================================================================
+# マップタイプ　非グリッド用一覧書き込み
+#=======================================================================
+def file_write_nogrid( name,
+  file_output_list, file_temp_list, file_depend_list, file_make_depend )
+  #land_output_list
+	file_output_list.printf( "\"%s/%s.ngpack\"\n", DIR_OUTPUT, name )
+  
+  #land_temp_list
+	file_temp_list.printf( "\t%s/%s.nsbmd", DIRSTR_TEMP, name )
+	file_temp_list.printf( "\t%s/%s.bhc", DIRSTR_TEMP, name )
+  
+  #land_depend_list
+	file_depend_list.printf( "\t%s/%s.ngpack", DIRSTR_OUTPUT, name )
+  
+  #land_make_depend ngpack
+  file_make_depend.printf( "#%s\n", name )
+  
+  file_make_depend.printf(
+    "%s/%s.ngpack: %s/%s.nsbmd %s/%s.bhc %s/%s.3dmd\n",
+    DIRSTR_OUTPUT, name,
+    DIRSTR_TEMP, name,
+    DIRSTR_TEMP, name,
+    DIRSTR_RES, name )
+  
+  file_make_depend.printf( "\t@echo create ngpack %s\n", name )
+  
+  file_make_depend.printf(
+    "\t@%s %s/%s.nsbmd %s/%s.bhc %s/%s.3dmd %s/%s.ngpack BR\n\n",
+    STR_BINLINKER,
+    DIRSTR_TEMP, name,
+    DIRSTR_TEMP, name,
+    DIRSTR_RES, name,
+    DIRSTR_OUTPUT, name )
+  
+  #land_make_depend nsbmd
+  file_make_depend.printf( "%s/%s.nsbmd: %s/%s.imd\n",
+    DIRSTR_TEMP, name,
+    DIRSTR_RES, name )
+  
+  file_make_depend.printf( "\t@echo create_nsbmd %s\n", name )
+   
+  file_make_depend.printf( "\t@%s %s/%s.imd -emdl -o %s/%s.nsbmd\n\n",
+    STR_G3DCVTR, DIRSTR_RES, name, DIRSTR_TEMP, name )
+
+  #land_make_depend bhc
+  file_make_depend.printf( "%s/%s.bhc: %s/%sh.imd\n",
+    DIRSTR_TEMP, name, DIRSTR_RES, name )
+  
+  file_make_depend.printf( "\t@echo create_bhc %s\n", name )
+  file_make_depend.printf(
+    "\t@echo ./%s/%sh > heightlist.lst\n", DIRSTR_RES, name )
+  file_make_depend.printf( "\t@echo \\#END >> heightlist.lst\n" )
+  
+  file_make_depend.printf(
+   "\t@$(HEIGHTCNV) heightlist.lst < $(HEIGHTRETFILE) > convresult\n" )
+  file_make_depend.printf(
+    "\t@ruby pad4byte.rb %s/%sh.bhc %s/%s.bhc\n",
+    DIRSTR_RES, name, DIRSTR_TEMP, name )
+  file_make_depend.printf( "\t@rm %s/%sh.bhc\n\n", DIRSTR_RES, name )
+  
+  #land_make_depend exist file check
+  check = DIR_RES + "/" + name + ".imd"
+  if( FileTest.exist?(check) != true )
+    printf( "%s.imdをダミーファイルから生成します\n", name )
+    file_copy( DIR_RES+"/"+FNAME_DMYFILE_NOGRID_IMD, check )
+  end
+  
+  check = DIR_RES + "/" + name + "h.imd"
+  if( FileTest.exist?(check) != true )
+    printf( "%sh.imdをダミーファイルから生成します\n", name )
+    file_copy( DIR_RES+"/"+FNAME_DMYFILE_NOGRID_H_IMD, check )
+  end
+  
+  check = DIR_RES + "/" + name + ".3dmd"
+  if( FileTest.exist?(check) != true )
+    printf( "%s.3dmdをダミーファイルから生成します\n", name )
+    file_copy( DIR_RES+"/"+FNAME_DMYFILE_NOGRID_3DMD, check )
+  end
+end
+
+#=======================================================================
 # 一覧作成
 #=======================================================================
 file_output_list = File.open( FNAME_OUTPUT_LIST, "w" )
-file_model_list = File.open( FNAME_MODEL_LIST, "w" )
+file_temp_list = File.open( FNAME_TEMP_LIST, "w" )
 file_depend_list = File.open( FNAME_DEPEND_LIST, "w" )
 file_make_depend = File.open( FNAME_MAKE_DEPEND, "w" )
 
-file_model_list.printf( "%s = \\\n", LNAME_MODEL_LIST )
+file_temp_list.printf( "%s = \\\n", LNAME_TEMP_LIST )
 file_depend_list.printf( "%s = \\\n", LNAME_DEPEND_LIST )
 
 yen_flg = 0
@@ -73,7 +226,7 @@ while file_name = ARGV.shift
 		end
     
 		if( yen_flg != 0 )
-			file_model_list.printf( " \\\n" )
+			file_temp_list.printf( " \\\n" )
 			file_depend_list.printf( " \\\n" )
 		end
     
@@ -81,61 +234,15 @@ while file_name = ARGV.shift
 		column = line.split ","
 		name = column[1]
     type = column[2]
-
-    #land_output_list
-		file_output_list.printf( "\"%s/%s.3dppack\"\n", DIR_OUTPUT, name )
     
-    #land_model_list
-		file_model_list.printf( "\t%s/%s.imd", DIRSTR_RES, name )
-    
-    #land_depend_list
-		file_depend_list.printf( "\t%s/%s.3dppack", DIRSTR_OUTPUT, name )
-    
-    #land_make_depend
-    file_make_depend.printf( "#%s\n", name )
-    
-    file_make_depend.printf(
-      "%s/%s.3dppack: %s/%s.nsbmd %s/%s.bin %s/%s.3dmd\n",
-      DIRSTR_OUTPUT, name,
-      DIRSTR_TEMP, name,
-      DIRSTR_RES, name,
-      DIRSTR_RES, name )
-    
-    file_make_depend.printf( "\t@echo create 3dppack %s\n", name )
-    
-    file_make_depend.printf(
-      "\t@%s %s/%s.nsbmd %s/%s.bin %s/%s.3dmd %s/%s.3dppack WB\n\n",
-      STR_BINLINKER,
-      DIRSTR_TEMP, name,
-      DIRSTR_RES, name,
-      DIRSTR_RES, name,
-      DIRSTR_OUTPUT, name )
-    
-    file_make_depend.printf( "%s/%s.nsbmd: %s/%s.imd\n",
-      DIRSTR_TEMP, name,
-      DIRSTR_RES, name )
-    
-    file_make_depend.printf( "\t@echo create_nsbmd %s\n", name )
-    
-    file_make_depend.printf( "\t@%s %s/%s.imd -o %s/%s.nsbmd\n\n",
-      STR_G3DCVTR, DIRSTR_RES, name, DIRSTR_TEMP, name )
-    
-    check = DIR_RES + "/" + name + ".imd"
-    if( FileTest.exist?(check) != true )
-      printf( "%s.imdをダミーファイルから生成します\n", name )
-      file_copy( DIR_RES+"/"+FNAME_DMYFILE_GRID_IMD, check )
-    end
-   
-    check = DIR_RES + "/" + name + ".bin"
-    if( FileTest.exist?(check) != true )
-      printf( "%s.binをダミーファイルから生成します\n", name )
-      file_copy( DIR_RES+"/"+FNAME_DMYFILE_GRID_BIN, check )
-    end
-    
-    check = DIR_RES + "/" + name + ".3dmd"
-    if( FileTest.exist?(check) != true )
-      printf( "%s.3dmdをダミーファイルから生成します\n", name )
-      file_copy( DIR_RES+"/"+FNAME_DMYFILE_GRID_3DMD, check )
+    if( type == MAPTYPESTR_GRID )
+      file_write_grid( name,
+        file_output_list, file_temp_list,
+        file_depend_list, file_make_depend )
+    else
+      file_write_nogrid( name,
+        file_output_list, file_temp_list,
+        file_depend_list, file_make_depend )
     end
 	end
 	file_csv.close
@@ -143,5 +250,5 @@ end
 
 file_make_depend.close
 file_depend_list.close
-file_model_list.close
+file_temp_list.close
 file_output_list.close
