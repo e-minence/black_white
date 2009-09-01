9d20879ec6242d83ca4a01fadcd0eb428f761bd3
Hisashi Sogabe <sogabe@gamefreak.co.jp>
2009-09-01 04:43:39 +0000

    ポケモンの高さを少し下げた

--- prog/src/battle/btlv/btlv_mcss.c
+++ prog/src/battle/btlv/btlv_mcss.c
@@ -113,16 +113,17 @@ static  const VecFx32 poke_pos_table[]={
 };
 */
 
+#define OFS_Y ( 0.3f )
 
 static  const VecFx32 poke_pos_table[]={
-  { FX_F32_TO_FX32( -2.5f + 3.000f ), FX_F32_TO_FX32( 0.7f ), FX_F32_TO_FX32(   7.5f - 0.5f ) },    //POS_AA
-  { FX_F32_TO_FX32(  4.5f - 4.200f ), FX_F32_TO_FX32( 0.7f ), FX_F32_TO_FX32( -10.0f ) },   //POS_BB
-  { FX_F32_TO_FX32( -3.5f + 3.500f ), FX_F32_TO_FX32( 0.7f ), FX_F32_TO_FX32(   8.5f ) },   //POS_A
-  { FX_F32_TO_FX32(  6.0f - 4.200f ), FX_F32_TO_FX32( 0.7f ), FX_F32_TO_FX32(  -9.0f ) },   //POS_B
-  { FX_F32_TO_FX32( -0.5f + 3.845f ), FX_F32_TO_FX32( 0.7f ), FX_F32_TO_FX32(   9.0f ) },   //POS_C
-  { FX_F32_TO_FX32(  2.0f - 4.964f ), FX_F32_TO_FX32( 0.7f ), FX_F32_TO_FX32( -11.0f ) },   //POS_D
-  { FX_F32_TO_FX32( -2.5f + 3.845f ), FX_F32_TO_FX32( 0.7f ), FX_F32_TO_FX32(  10.0f ) },   //POS_E
-  { FX_F32_TO_FX32(  4.5f - 4.964f ), FX_F32_TO_FX32( 0.7f ), FX_F32_TO_FX32( -10.0f ) },   //POS_F
+  { FX_F32_TO_FX32( -2.5f + 3.000f ), FX_F32_TO_FX32( 0.7f - OFS_Y ), FX_F32_TO_FX32(   7.5f - 0.5f ) },    //POS_AA
+  { FX_F32_TO_FX32(  4.5f - 4.200f ), FX_F32_TO_FX32( 0.7f - OFS_Y ), FX_F32_TO_FX32( -10.0f ) },   //POS_BB
+  { FX_F32_TO_FX32( -3.5f + 3.500f ), FX_F32_TO_FX32( 0.7f - OFS_Y ), FX_F32_TO_FX32(   8.5f ) },   //POS_A
+  { FX_F32_TO_FX32(  6.0f - 4.200f ), FX_F32_TO_FX32( 0.7f - OFS_Y ), FX_F32_TO_FX32(  -9.0f ) },   //POS_B
+  { FX_F32_TO_FX32( -0.5f + 3.845f ), FX_F32_TO_FX32( 0.7f - OFS_Y ), FX_F32_TO_FX32(   9.0f ) },   //POS_C
+  { FX_F32_TO_FX32(  2.0f - 4.964f ), FX_F32_TO_FX32( 0.7f - OFS_Y ), FX_F32_TO_FX32( -11.0f ) },   //POS_D
+  { FX_F32_TO_FX32( -2.5f + 3.845f ), FX_F32_TO_FX32( 0.7f - OFS_Y ), FX_F32_TO_FX32(  10.0f ) },   //POS_E
+  { FX_F32_TO_FX32(  4.5f - 4.964f ), FX_F32_TO_FX32( 0.7f - OFS_Y ), FX_F32_TO_FX32( -10.0f ) },   //POS_F
   { FX_F32_TO_FX32( -2.5f + 3.000f ), FX_F32_TO_FX32( 0.0f ), FX_F32_TO_FX32(   7.5f - 0.5f ) },    //POS_TR_AA
   { FX_F32_TO_FX32(  4.5f - 4.200f ), FX_F32_TO_FX32( 0.0f ), FX_F32_TO_FX32( -25.0f ) },   //POS_TR_BB
   { FX_F32_TO_FX32( -3.5f + 3.500f ), FX_F32_TO_FX32( 0.0f ), FX_F32_TO_FX32(   8.5f ) },   //POS_TR_A
