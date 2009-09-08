1f73270adc2c0e6a5601867cea4124f70d934f79
Keita Kagaya <kagaya@gamefreak.co.jp>
2009-09-08 07:26:59 +0000

    field_player更新時にPLAYERWORKへ座標の反映を行う。

--- prog/src/field/field_player.c
+++ prog/src/field/field_player.c
@@ -164,6 +164,10 @@ void FIELD_PLAYER_Delete( FIELD_PLAYER *fld_player )
 //--------------------------------------------------------------
 void FIELD_PLAYER_Update( FIELD_PLAYER *fld_player )
 {
+  //プレイヤー座標をPLAYERWORKへ反映
+  VecFx32 pos;
+  FIELD_PLAYER_GetPos( fld_player, &pos );
+	PLAYERWORK_setPosition( fld_player->playerWork, &pos );
 }
 
 //======================================================================
