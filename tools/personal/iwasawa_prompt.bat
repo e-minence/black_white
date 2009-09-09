443f0664790dd1b6277c604438bfe749919a1869
Miyuki Iwasawa <iwasawa@gamefreak.co.jp>
2009-09-09 04:06:22 +0000

    DOSプロンプト　岩澤用個人設定ファイル更新

--- tools/personal/iwasawa_prompt.bat
+++ tools/personal/iwasawa_prompt.bat
@@ -10,3 +10,7 @@ if exist "tools\personal\script_prompt.bat" call tools\personal\script_prompt.ba
 REM ##################################################
 REM ツールパス系
 doskey vi=c:\tools\vi\gvim.exe $*
+doskey golib=cd c:\home\gflib\$*
+
+doskey chs=cd %PROJECT_ROOT:/=\%prog\src\$*
+doskey che=cd %PROJECT_ROOT:/=\%prog\src\fielddata\$*
