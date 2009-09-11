3c06c15937d600a6b9baf892e03c8a54045acdc3
Sosuke Tamada <tamada@gamefreak.co.jp>
2009-09-11 07:55:25 +0000

    無理やり実装を入れてみた

--- prog/src/field/scrcmd_mapchange.c
+++ prog/src/field/scrcmd_mapchange.c
@@ -1,7 +1,7 @@
 //======================================================================
 /**
  * @file  scrcmd_mapchange.c
- * @bfief  スクリプトコマンド：アイテム関連
+ * @bfief  スクリプトコマンド：マップ遷移関連
  * @date  2009.09.11
  * @author	tamada GAMEFREAK inc.
  */
@@ -22,11 +22,11 @@
 #include "scrcmd_mapchange.h"
 
 #include "event_mapchange.h"
-
+#include "field/field_const.h"  //FIELD_CONST_GRID_SIZE
 //======================================================================
 //  define
 //======================================================================
-
+#define GRID_TO_FX32(gvalue)  ( (gvalue * FIELD_CONST_GRID_SIZE) << FX32_SHIFT )
 //======================================================================
 //  struct
 //======================================================================
@@ -36,7 +36,7 @@
 //======================================================================
 
 //======================================================================
-//  アイテム関連
+//  スクリプトコマンド：
 //======================================================================
 //--------------------------------------------------------------
 /**
@@ -48,24 +48,39 @@
 VMCMD_RESULT EvCmdMapChangeBySandStream( VMHANDLE *core, void *wk )
 {
   SCRCMD_WORK *work = wk;
-  HEAPID heapID = SCRCMD_WORK_GetHeapID( work );
-  GAMEDATA *gdata = SCRCMD_WORK_GetGameData( work );
   SCRIPT_WORK *sc = SCRCMD_WORK_GetScriptWork( work );
+  GAMESYS_WORK * gsys = SCRIPT_GetGameSysWork( sc );
+  FIELDMAP_WORK * fieldmap = GAMESYSTEM_GetFieldMapWork( gsys );
+  //HEAPID heapID = SCRCMD_WORK_GetHeapID( work );
+  //GAMEDATA *gdata = SCRCMD_WORK_GetGameData( work );
+  VecFx32 disappear_pos;
+  VecFx32 appear_pos;
 
-  u16 zone_id = VMGetU16(core);
-  u16 next_x = VMGetU16(core);
-  u16 next_z = VMGetU16(core);
-  u16 dir;
+  u16 zone_id = VMGetU16(core); //2byte read  ZONE指定
+  u16 next_x = VMGetU16(core);  //2byte read  X位置グリッド単位
+  u16 next_z = VMGetU16(core);  //2byte read  Z位置グリッド単位
   
   {
-    GAMESYS_WORK * gsys = SCRIPT_GetGameSysWork( sc );
-    FIELDMAP_WORK * fieldmap = GAMESYSTEM_GetFieldMapWork( gsys );
-    GMEVENT * parent_event = GAMESYSTEM_GetEvent( gsys );
+    FIELD_PLAYER *fld_player = FIELDMAP_GetFieldPlayer( fieldMap );
+    MMDL *mmdl = FIELD_PLAYER_GetMMdl( fld_player );
+    u16 x = MMDL_GetGridPosX( mmdl );
+    u16 y = MMDL_GetGridPosY( mmdl );
+    u16 z = MMDL_GetGridPosZ( mmdl );
+    VEC_Set(&disappear_pos, GRID_TO_FX32(x), GRID_TO_FX32(y), GRID_TO_FX32(z)); 
+  }
+  
+  {
     GMEVENT * mapchange_event;
-    VecFx32 disappear_pos;
-    VecFx32 appear_pos;
+    GMEVENT * parent_event;
+    u16 dir = 0;                  //仮  仕様としてはどうするべきなのか？
 
+    VEC_Set(&appear_pos,
+        GRID_TO_FX32(next_x),
+        GRID_TO_FX32(0),      //todo 高さ（Y）も指定するべきかどうか？
+        GRID_TO_FX32(next_z)
+        ); 
     
+    parent_event = GAMESYSTEM_GetEvent( gsys ); //現在のイベント
     mapchange_event = DEBUG_EVENT_ChangeMapBySandStream( gsys, fieldmap,
         &disappear_pos, zone_id, &appear_pos, dir );
     GMEVENT_CallEvent( parent_event, mapchange_event );
