1ccac41f22d16b43a8fdee39064240db61090984
Sosuke Tamada <tamada@gamefreak.co.jp>
2009-09-13 10:21:49 +0000

    スクリプトIDのオフセット生成環境を追加

---
+++ resource/fldmapdata/script/scrid_offset/make_offset_table.rb
@@ -0,0 +1,241 @@
+#----------------------------------------------------------------------------
+#
+#
+#   スクリプトIDオフセット自動生成
+#
+#
+#     2009.08.07  tamada GAMEFREAK inc.
+#
+#----------------------------------------------------------------------------
+
+$KCODE= "SJIS"
+
+
+SCRIPT_GMM_PATH = ENV["PROJECT_RSCDIR"] + "message/src/script_message/"
+SCRIPT_PATH = ENV["PROJECT_RSCDIR"] + "fldmapdata/script/"
+
+class ScrOfsConverterError < Exception; end
+
+XLS2TAB = "ruby #{ENV["PROJECT_ROOT"]}tools/exceltool/xls2xml/tab_out.rb -c "
+
+#----------------------------------------------------------------------------
+#----------------------------------------------------------------------------
+class ScrOfs
+  attr_reader :name, :startno, :max, :scriptfile, :msgfile, :comment
+
+  def initialize(name, startno, max, scriptfile, msgfile, comment)
+    @name = name
+    @startno = startno
+    @max = max
+    @scriptfile = scriptfile
+    @msgfile = msgfile
+    @comment = comment
+
+    STDERR.puts "MAKE SCRIPT OFFSET \"#{name}\", #{startno}-#{endno}"
+  end
+
+  def endno
+    return @startno+@max-1
+  end
+end
+
+#----------------------------------------------------------------------------
+#----------------------------------------------------------------------------
+class ScrOfsData
+
+  COL_AREANAME  = 0
+  COL_STARTNO   = 1
+  COL_MAX       = 2
+  COL_SCRFILE   = 3
+  COL_MSGFILE   = 4
+  COL_COMMENT   = 5
+
+  attr_reader :manager_filename, :ofsdatas, :define_count
+
+  def initialize(filename)
+    @manager_filename = filename
+
+    command = "#{XLS2TAB} #{@manager_filename}"
+    @tmpfile = `#{command}`.split("\n")
+
+    @define_count = 0
+    @ofsdatas = Array.new
+
+    read_list(@tmpfile)
+  end
+
+  def raiseError(string, count)
+    raise ScrOfsConverterError, "#{@manager_filename}:#{count}:\n #{string}"
+  end
+
+  def read_list(lines)
+    linecount = 1
+    column = lines.shift.split(",")
+    #ファイルフォーマットのチェック（カラムの説明が想定しているものと一致しているか？）
+    errorMsg = "ファイルフォーマット異常："
+    raiseError(errorMsg,lincount) if column[COL_AREANAME] != "管理領域名"
+    raiseError(errorMsg,lincount) if column[COL_STARTNO] != "開始ナンバー"
+    raiseError(errorMsg,lincount) if column[COL_MAX] != "最大値"
+    raiseError(errorMsg,lincount) if column[COL_SCRFILE] != "対象スクリプトファイル"
+    raiseError(errorMsg,lincount) if column[COL_MSGFILE] != "対象メッセージファイル"
+    raiseError(errorMsg,lincount) if column[COL_COMMENT] != "コメント"
+
+    names = Hash.new
+
+    before_max = -1
+    lines.each{|line|
+      linecount += 1
+      column = line.split(",")
+      if column[COL_AREANAME] == "#END" || column.size < 5 then
+          break
+      end
+      areaname = column[COL_AREANAME]
+      startno = column[COL_STARTNO]
+      if startno == "auto" then
+        startno = before_max + 1
+      else
+        startno = Integer(startno)
+      end
+      max = Integer(column[COL_MAX])
+      scrfile = column[COL_SCRFILE]
+      msgfile = column[COL_MSGFILE]
+
+      raiseError("領域名が重なっています.#{areaname}",linecount) if names.has_key?(areaname)
+      names[areaname] = nil
+
+      raiseError("使用領域が前の部分に重なっています\n",linecount) if startno <= before_max
+      before_max = startno + max - 1
+
+      if msgfile != "×"
+        unless FileTest.exist?(SCRIPT_GMM_PATH + msgfile) then
+          raiseError("指定されたgmmファイル#{msgfile}が存在しません", linecount)
+        end
+      else
+        msgfile = nil
+      end
+      if scrfile != "×"
+        unless FileTest.exist?(SCRIPT_PATH + scrfile) then
+          raiseError("指定されたscriptファイル#{scrfile}が存在しません", linecount)
+        end
+      else
+        scrfile = nil
+      end
+
+      comment = column[COL_COMMENT]
+      @ofsdatas << ScrOfs.new(areaname, startno, max, scrfile, msgfile, comment)
+      @define_count += 1
+    }
+  end
+
+  def make_ofs_header(file)
+    file.puts "//このファイルは#{@manager_filename}から自動生成されたものです"
+  end
+end
+
+#----------------------------------------------------------------------------
+#----------------------------------------------------------------------------
+def make_offset_header(scrData)
+  head_string = <<END_OF_STRING
+//======================================================================
+/**
+ * @file  scr_offset_id.h
+ * @bfief  スクリプトIDのオフセット定義
+ * @author  tamada GAMEFREAK inc.
+ * @date  DATE_STRING
+ *
+ * このファイルはコンバータにより自動生成されています
+ */
+//======================================================================
+#ifndef SCR_OFFSET_ID_H
+#define SCR_OFFSET_ID_H
+
+//======================================================================
+//======================================================================
+END_OF_STRING
+
+  tail_string = <<END_OF_STRING
+
+#endif  /* SCR_OFFSET_ID_H */
+END_OF_STRING
+
+date_string = Time.new.localtime.strftime("%Y %m/%d (%a) %H:%M")
+output = String.new
+output = head_string.sub(/DATE_STRING/,date_string)
+
+scrData.ofsdatas.each{|data|
+  start_id = "ID_#{data.name.upcase}_OFFSET"
+  end_id = "ID_#{data.name.upcase}_OFFSET_END"
+  output += sprintf("#define %-32s (%5d) //%s\n",start_id, data.startno, data.comment)
+  output += sprintf("#define %-32s (%5d)\n",  end_id, data.endno)
+  output += "\n"
+}
+
+output += tail_string
+return output
+end
+
+#----------------------------------------------------------------------------
+#----------------------------------------------------------------------------
+def make_cdat_table(scrData)
+  head_string =<<END_OF_STRING
+//======================================================================
+/**
+ * @file  scr_offset.cdat
+ * @bfief  スクリプトIDのオフセットとそれに対応するアーカイブの定義テーブル
+ * @author  tamada GAMEFREAK inc.
+ * @date  DATE_STRING
+ *
+ * このファイルはコンバータにより自動生成されています
+ */
+//======================================================================
+
+//※スクリプトIDが大きい順に定義されているため、元データと並びが逆です
+static const SCRIPT_ARC_TABLE ScriptArcTable[] = {
+
+END_OF_STRING
+  tail_string =<<END_OF_STRING
+};
+END_OF_STRING
+
+date_string = Time.new.localtime.strftime("%Y %m/%d (%a) %H:%M")
+output = String.new
+output = head_string.sub(/DATE_STRING/,date_string)
+
+scrData.ofsdatas.reverse_each{|data|
+  start_id = "ID_#{data.name.upcase}_OFFSET"
+  data.scriptfile
+  data.msgfile
+  if data.scriptfile != nil && data.msgfile != nil
+    scridx = "NARC_script_seq_#{data.scriptfile.sub(/\.ev$/,"_bin")}"
+    msgidx = "NARC_script_message_#{data.msgfile.sub(/\.gmm/,"_dat")}"
+    output += sprintf("    {//#{data.comment}\n")
+    output += sprintf("      %s,\n", start_id)
+    output += sprintf("      %s,\n", scridx)
+    output += sprintf("      %s\n", msgidx)
+    output += sprintf("    },\n")
+  end
+}
+output += tail_string
+return output
+end
+
+#----------------------------------------------------------------------------
+#----------------------------------------------------------------------------
+
+scrData =ScrOfsData.new(ARGV[0])
+
+
+
+=begin
+File.open("scr_offset_id.h","w"){|file|
+  header = make_offset_header(scrData)
+  file.puts header
+}
+=end
+
+File.open("scr_offset.cdat","w"){|file|
+  table = make_cdat_table(scrData)
+  file.puts table
+}
+
+
---
+++ resource/fldmapdata/script/scrid_offset/script_offset_id.h
@@ -0,0 +1,43 @@
+//======================================================================
+/**
+ * @file  scr_offset.h
+ * @bfief  スクリプトIDのオフセット定義
+ * @author  Satoshi Nohara
+ * @date  06.05.26
+ */
+//======================================================================
+#ifndef SCR_OFFSET_H
+#define SCR_OFFSET_H
+
+//======================================================================
+//  スクリプトIDオフセット
+//
+//  定義追加ルール
+//  #define ID_"スクリプトファイル名"_OFFSET
+//  #define ID_"スクリプトファイル名"_OFFSET_END
+//
+//  script.cの読み込むスクリプトファイル、
+//  メッセージファイルを対応させて下さい。
+//======================================================================
+//#define SCRID_NULL                 (0) //ダミースクリプトID
+#define ID_START_SCR_OFFSET          (1) //開始スクリプトIDのオフセット
+
+#define ID_COMMON_SCR_OFFSET         (2000) //共通スクリプトIDのオフセット
+#define ID_COMMON_SCR_OFFSET_END     (2499) //共通スクリプトIDのオフセット終了
+
+#define ID_BG_ATTR_OFFSET            (2500) //BGスクリプトIDのオフセット
+#define ID_BG_ATTR_OFFSET_END        (2799) //BGスクリプトIDのオフセット終了
+
+#define ID_TRAINER_OFFSET            (3000) //トレーナースクリプトIDのオフセット
+#define ID_TRAINER_OFFSET_END        (4999) //トレーナースクリプトIDのオフセット終了
+
+#define ID_TRAINER_2VS2_OFFSET       (5000) //2vs2トレーナースクリプトIDのオフセット
+#define ID_TRAINER_2VS2_OFFSET_END   (6999) //2vs2トレーナースクリプトIDのオフセット終了
+
+#define ID_HIDE_ITEM_OFFSET          (8000) //隠しアイテムスクリプトIDのオフセット
+#define ID_HIDE_ITEM_OFFSET_END      (8799) //隠しアイテムスクリプトIDのオフセット終了
+
+#define	ID_INIT_SCR_OFFSET           (9600) //ゲーム開始スクリプトIDのオフセット
+#define	ID_INIT_SCR_OFFSET_END (9699) //ゲーム開始スクリプトIDのオフセット終了
+
+#endif  /* SCR_OFFSET_H */
