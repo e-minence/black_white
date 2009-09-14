1d0d5f45836aa85e4a4eeb3d00667e3da41a7350
Hisashi Sogabe <sogabe@gamefreak.co.jp>
2009-09-14 05:15:18 +0000

    トレーナーデータ更新

---
+++ resource/trdata/trtype_def.h
@@ -0,0 +1,22 @@
+//============================================================================================
+/**
+ * @file	trtype_def.h
+ * @bfief	トレーナータイプ定義
+ * @author	TrainerDataConverter
+ * @date	2009-09-14
+ * @author	このファイルは、コンバータが吐き出したファイルです
+*/
+//============================================================================================
+
+#pragma once
+
+
+#define	TRTYPE_HERO	    ( 0 )
+#define	TRTYPE_HEROINE	( 1 )
+#define	TRTYPE_TANPAN    ( 2 )    //たんぱんこぞう
+#define	TRTYPE_MINI    ( 3 )    //ミニスカート
+#define	TRTYPE_SCHOOLB    ( 4 )    //じゅくがえり
+#define	TRTYPE_SCHOOLG    ( 5 )    //じゅくがえり
+#define	TRTYPE_TENNIS    ( 6 )    //テニスプレイヤー
+#define	TRTYPE_FOOTBALL    ( 7 )    //フットボーラー
+#define	TRTYPE_MAX	( 8 )
---
+++ resource/trdata/trtype_sex.h
@@ -0,0 +1,24 @@
+//============================================================================================
+/**
+ * @file	trtype_sex.h
+ * @bfief	トレーナー性別定義
+ * @author	TrainerDataConverter
+ * @date	2009-09-14
+ * @author	このファイルは、コンバータが吐き出したファイルです
+*/
+//============================================================================================
+
+#pragma once
+
+
+static	const	u8	TrTypeSexTable[]={
+	PTL_SEX_MALE,		//男主人公
+	PTL_SEX_FEMALE,	//女主人公
+	PTL_SEX_MALE,		//たんぱんこぞう
+	PTL_SEX_FEMALE,		//ミニスカート
+	PTL_SEX_MALE,		//じゅくがえり
+	PTL_SEX_FEMALE,		//じゅくがえり
+	PTL_SEX_FEMALE,		//テニスプレイヤー
+	PTL_SEX_MALE,		//フットボーラー
+};
+
