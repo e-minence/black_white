3298919c02e58b5f20573d1c3b60788b7b6b516d
Hiroyuki Nakamura <nakamura@gamefreak.co.jp>
2009-09-14 08:25:24 +0000

    デバッグ処理を入れてしまったので修正

--- prog/src/battle/app/b_bag_main.c
+++ prog/src/battle/app/b_bag_main.c
@@ -266,7 +266,7 @@ void BattleBag_TaskAdd( BBAG_DATA * dat )
 //	GFL_HEAP_CreateHeap( GFL_HEAPID_APP, HEAPID_BATTLE_APP_TEST, 0x70000 );
 //	dat->heap = HEAPID_BATTLE_APP_TEST;
 	// シューターモードテスト
-	dat->mode = BBAG_MODE_SHOOTER;
+//	dat->mode = BBAG_MODE_SHOOTER;
 
 	wk = GFL_HEAP_AllocClearMemory( dat->heap, sizeof(BBAG_WORK) );
 
@@ -315,8 +315,8 @@ void BattleBag_TaskAdd( BBAG_DATA * dat )
 //	wk->dat->used_item = 2;
 //	wk->dat->used_poke = 1;
 
-	wk->dat->energy = 7;
-	wk->dat->reserved_energy = 3;
+//	wk->dat->energy = 7;
+//	wk->dat->reserved_energy = 3;
 
 /*
 	if( BattleWorkFightTypeGet(wk->dat->bw) & FIGHT_TYPE_GET_DEMO ){
