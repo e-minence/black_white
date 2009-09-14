32c75d868142d425ad4b3db8dca95be77db54e88
Hisashi Sogabe <sogabe@gamefreak.co.jp>
2009-09-14 04:58:18 +0000

    WB向けトレーナーデータコンバート環境作成

---
+++ resource/trdata/convcsv.mk
@@ -0,0 +1,92 @@
+#------------------------------------------------------------------------------
+#
+#	ポケモンパーソナル用Makefile
+#
+#	2008.08.19	tamada@gamefreak
+#------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------
+#※コンバート作業が必要なユーザーの名前を記述する
+#------------------------------------------------------------------------------
+
+USERS	=	sogabe
+
+#------------------------------------------------------------------------------
+#※ここに作成するnarc名を書く
+#------------------------------------------------------------------------------
+
+NO_HEADER = trno_def.h
+TYPE_HEADER = trtype_def.h
+GENDER_HEADER = trtype_sex.h
+NAME_GMMFILE = trname.gmm
+TYPE_GMMFILE = trtype.gmm
+SCRFILE = trfgra_wb.scr
+
+#------------------------------------------------------------------------------
+#※コピー先へのパスを書く（通常はPROJECT_ARCDIRでよい）
+#------------------------------------------------------------------------------
+TARGETDIR	= ../../prog/include/tr_tool/
+GMMDIR	= ../../resource/message/src/
+
+#------------------------------------------------------------------------------
+#※サブディレクトリでもMakeしたい場合、ここにディレクトリ名を書く
+#------------------------------------------------------------------------------
+SUBDIRS	=
+
+#共通ルールファイルのインクルード
+include $(PROJECT_RSCDIR)\macro_define
+
+include	$(NITROSDK_ROOT)/build/buildtools/commondefs.cctype.CW
+
+.PHONY:	do-build clean
+
+#------------------------------------------------------------------------------
+#	make do-build ルール
+#------------------------------------------------------------------------------
+ifeq	($(CONVERTUSER),true)	#コンバート対象者のみ、コンバートのルールを有効にする
+
+#パーソナルデータ生成
+
+do-build: out_end
+
+out_end: trainer_wb.csv
+	ruby ../../tools/trdata_conv/trdata_conv.rb $< ../message/template.gmm
+endif
+
+do-build: $(TARGETDIR)$(NO_HEADER) $(TARGETDIR)$(TYPE_HEADER) $(TARGETDIR)$(GENDER_HEADER) $(GMMDIR)$(NAME_GMMFILE) $(GMMDIR)$(TYPE_GMMFILE)
+
+$(TARGETDIR)$(NO_HEADER):	$(NO_HEADER)
+	$(COPY)	$(NO_HEADER) $(TARGETDIR)
+
+$(TARGETDIR)$(TYPE_HEADER):	$(TYPE_HEADER)
+	$(COPY)	$(TYPE_HEADER) $(TARGETDIR)
+
+$(TARGETDIR)$(GENDER_HEADER):	$(GENDER_HEADER)
+	$(COPY)	$(GENDER_HEADER) $(TARGETDIR)
+
+$(GMMDIR)$(NAME_GMMFILE):	$(NAME_GMMFILE)
+	$(COPY)	$(NAME_GMMFILE) $(GMMDIR)
+
+$(GMMDIR)$(TYPE_GMMFILE):	$(TYPE_GMMFILE)
+	$(COPY)	$(TYPE_GMMFILE) $(GMMDIR)
+
+#------------------------------------------------------------------------------
+#	make cleanルール
+#------------------------------------------------------------------------------
+clean:
+ifeq	($(CONVERTUSER),true)	#コンバート対象者のみ、コンバートのルールを有効にする
+	-rm -f *.s
+	-rm -f $(NO_HEADER)
+	-rm -f $(TYPE_HEADER)
+	-rm -f $(GENDER_HEADER)
+	-rm -f $(NAME_GMMFILE)
+	-rm -f $(TYPE_GMMFILE)
+	-rm -f *.scr
+	-rm -f out_end
+endif
+	-rm -f $(TARGETDIR)$(NO_HEADER)
+	-rm -f $(TARGETDIR)$(TYPE_HEADER)
+	-rm -f $(TARGETDIR)$(GENDER_HEADER)
+	-rm -f $(GMMDIR)$(NAME_GMMFILE)
+	-rm -f $(GMMDIR)$(TYPE_GMMFILE)
+
--- resource/trdata/mknarc.mk
+++ resource/trdata/mknarc.mk
@@ -1,8 +1,8 @@
 #------------------------------------------------------------------------------
 #
-#	トレーナーデータコンバート用Makefile
+#	トレーナーデータ用Makefile
 #
-#	2009.02.13	sogabe@gamefreak
+#	2009.07.27	sogabe@gamefreak
 #------------------------------------------------------------------------------
 
 #------------------------------------------------------------------------------
@@ -13,12 +13,15 @@ USERS	=	sogabe
 #------------------------------------------------------------------------------
 #※ここに作成するnarc名を書く
 #------------------------------------------------------------------------------
-targetname	= 
+targetname1	= trdata
+NARCNAME1	= $(targetname1).narc
+targetname2	= trpoke
+NARCNAME2	= $(targetname2).narc
 
 #------------------------------------------------------------------------------
 #※コピー先へのパスを書く（通常はPROJECT_ARCDIRでよい）
 #------------------------------------------------------------------------------
-TARGETDIR	=
+TARGETDIR	= $(PROJECT_ARCDIR)
 
 #------------------------------------------------------------------------------
 #※サブディレクトリでもMakeしたい場合、ここにディレクトリ名を書く
@@ -27,20 +30,69 @@ SUBDIRS	=
 
 #共通ルールファイルのインクルード
 include $(PROJECT_RSCDIR)/macro_define
+
 include	$(NITROSDK_ROOT)/build/buildtools/commondefs.cctype.CW
 
-.PHONY:	do-build clean subdir
+#コンバート対象のファイルツリーを生成
+DATAFILES:=$(wildcard trdata_*.s)
+POKEFILES:=$(wildcard trpoke_*.s)
+ELF2BINDIR = ../../tools/
+
+.PHONY:	do-build clean
+
+ifeq	($(CONVERTUSER),true)	#コンバート対象者のみ、コンバートのルールを有効にする
+
+.SUFFIXES: .s .bin
+
+do-build: trdata.narc
+trdata.narc: $(DATAFILES:.s=.bin)
+	nnsarc -c -l -n trdata.narc trdata_*.bin
+
+do-build: trpoke.narc
+trpoke.narc: $(POKEFILES:.s=.bin)
+	nnsarc -c -l -n trpoke.narc trpoke_*.bin
+
+do-build: $(DATAFILES:.s=.bin)
+%.bin:%.s
+	$(MWAS) $< -o $*.o
+	$(MWLD) -dis -o $*.elf $*.o
+	$(ELF2BINDIR)elf2bin $*.elf
+	rm $*.o
+	rm $*.elf
+
+do-build: $(POKEFILES:.s=.bin)
+%.bin:%.s
+	$(MWAS) $< -o $*.o
+	$(MWLD) -dis -o $*.elf $*.o
+	$(ELF2BINDIR)elf2bin $*.elf
+	rm $*.o
+	rm $*.elf
+
+endif
 
 #------------------------------------------------------------------------------
 #	make do-build ルール
 #------------------------------------------------------------------------------
-do-build:
-	$(MAKE) -f convcsv.mk
-	$(MAKE) -f mknarc.mk
+do-build:	$(TARGETDIR)/$(NARCNAME1)	$(TARGETDIR)/$(NARCNAME2)
+
+$(TARGETDIR)/$(NARCNAME1):	$(NARCNAME1)
+	$(COPY)	$(NARCNAME1) $(TARGETDIR)
+
+$(TARGETDIR)/$(NARCNAME2):	$(NARCNAME2)
+	$(COPY)	$(NARCNAME2) $(TARGETDIR)
 
 #------------------------------------------------------------------------------
 #	make cleanルール
 #------------------------------------------------------------------------------
 clean:
-	$(MAKE) -f convcsv.mk clean
-	$(MAKE) -f mknarc.mk clean
+ifeq	($(CONVERTUSER),true)	#コンバート対象者のみ、コンバートのルールを有効にする
+	-rm -f *.o
+	-rm -f *.elf
+	-rm -f *.bin
+	-rm -f $(NARCNAME1)
+	-rm -f $(NARCNAME2)
+endif
+	-rm -f $(TARGETDIR)/$(NARCNAME1)
+	-rm -f $(TARGETDIR)/$(NARCNAME2)
+
+
