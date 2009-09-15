8539cf44c602bcd197cbe866e734b1ed90f50848
Nozomu Saito <saito_nozomu@gamefreak.co.jp>
2009-09-15 09:05:21 +0000

    //ヘッダー追加

---
+++ prog/src/field/gym_elec.h
@@ -0,0 +1,22 @@
+//======================================================================
+/**
+ * @file  gym_elec.h
+ * @bfief  “d‹CƒWƒ€
+ * @author  Saito
+ * @date  09.09.02
+ */
+//======================================================================
+
+#ifndef __GYM_ELEC_H__
+#define __GYM_ELEC_H__
+
+#include "field/fieldmap_proc.h"
+
+extern void GYM_ELEC_Setup(FIELDMAP_WORK *fieldWork);
+extern void GYM_ELEC_End(FIELDMAP_WORK *fieldWork);
+extern void GYM_ELEC_Move(FIELDMAP_WORK *fieldWork);
+
+extern GMEVENT *GYM_ELEC_ChangePoint(GAMESYS_WORK *gsys, const u8 inLeverIdx);
+
+
+#endif  //__GYM_ELEC_H__
