e19bde227833084c8494496ef5fcf7a6044fd0bf
Junichi Masuda <matsuda@gamefreak.co.jp>
2009-09-16 04:29:53 +0000

    通信エラー対応処理を色々と実装

---
+++ prog/src/field/event_comm_error.c
@@ -0,0 +1,84 @@
+//==============================================================================
+/**
+ * @file    event_comm_error.c
+ * @brief   フィールドでの通信エラー画面呼び出し
+ * @author  matsuda
+ * @date    2009.09.14(月)
+ */
+//==============================================================================
+#include <gflib.h>
+
+#include "gamesystem/gamesystem.h"
+#include "gamesystem/game_init.h"
+#include "gamesystem/game_event.h"
+#include "gamesystem/game_data.h"
+
+#include "field/fieldmap.h"
+#include "gamesystem\gamesystem.h"
+
+#include "./event_fieldmap_control.h"
+#include "event_comm_error.h"
+#include "system/net_err.h"
+
+
+//==============================================================================
+//  
+//==============================================================================
+FS_EXTERN_OVERLAY(fieldmap);
+
+//==============================================================================
+//  プロトタイプ宣言
+//==============================================================================
+static GFL_PROC_RESULT FieldCommErrorProc_Init(GFL_PROC * proc, int * seq , void *pwk, void *mywk);
+static GFL_PROC_RESULT FieldCommErrorProc_Main(GFL_PROC * proc, int * seq , void *pwk, void *mywk);
+static GFL_PROC_RESULT FieldCommErrorProc_Exit(GFL_PROC * proc, int * seq , void *pwk, void *mywk);
+
+//==============================================================================
+//  データ
+//==============================================================================
+static const GFL_PROC_DATA FieldCommErrorProc = {
+  FieldCommErrorProc_Init,
+  FieldCommErrorProc_Main,
+  FieldCommErrorProc_Exit,
+};
+
+
+
+//==================================================================
+/**
+ * フィールドでの通信エラー画面呼び出しイベント
+ *
+ * @param   gsys		
+ * @param   fieldmap		
+ *
+ * @retval  GMEVENT *		
+ */
+//==================================================================
+GMEVENT * EVENT_FieldCommErrorProc(GAMESYS_WORK * gsys, FIELD_MAIN_WORK * fieldmap)
+{
+  GAMESYSTEM_SetFieldCommErrorReq(gsys, FALSE);
+  NetErr_App_ReqErrorDisp();
+  return EVENT_FieldSubProc(gsys, fieldmap, FS_OVERLAY_ID(fieldmap), &FieldCommErrorProc, NULL);
+}
+
+
+//==============================================================================
+//
+//  ダミーPROC
+//      PROC遷移でエラー画面を表示させる為だけのダミーPROC
+//
+//==============================================================================
+static GFL_PROC_RESULT FieldCommErrorProc_Init(GFL_PROC * proc, int * seq , void *pwk, void *mywk)
+{
+  return GFL_PROC_RES_FINISH;
+}
+
+static GFL_PROC_RESULT FieldCommErrorProc_Main(GFL_PROC * proc, int * seq , void *pwk, void *mywk)
+{
+  return GFL_PROC_RES_FINISH;
+}
+
+static GFL_PROC_RESULT FieldCommErrorProc_Exit(GFL_PROC * proc, int * seq , void *pwk, void *mywk)
+{
+  return GFL_PROC_RES_FINISH;
+}
