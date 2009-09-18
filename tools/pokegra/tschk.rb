e8b2806cb8b99dec256303c5f5e03b528e9a60c6
Hisashi Sogabe <sogabe@gamefreak.co.jp>
2009-09-18 13:32:53 +0000

    コンバータ更新

---
+++ tools/pokegra/tschk.rb
@@ -0,0 +1,51 @@
+#! ruby -Ks
+
+#=======================================================================================
+#
+#	タイムスタンプ比較して別のrubyを実行
+#
+#=======================================================================================
+
+#クラス定義
+
+#サブルーチン
+
+#メインルーチン
+	if ARGV.size < 3
+		print "error: ruby tschk.rb list_file res_dir copy_dir\n"
+		print "list_file:タイムスタンプチェックするファイルリスト\n"
+		print "res_dir:タイムスタンプチェックするファイルが存在するディレクトリ\n"
+		print "copy_dir:コピー先ディレクトリ\n"
+		exit( 1 )
+	end
+
+  ARGV_LIST_FILE  = 0
+  ARGV_RES_DIR    = 1
+  ARGV_COPY_DIR   = 2
+
+  begin
+    fp_list = open( ARGV[ ARGV_LIST_FILE ] )
+  rescue
+    printf( "FileOpenError:%s\n", ARGV[ ARGV_LIST_FILE ] )
+    exit( 1 )
+  end
+
+  read_data = []
+  while str = fp_list.gets
+    read_data << ARGV[ ARGV_RES_DIR ] + str.tr( "\r\n", "" )
+  end
+  fp_list.close
+
+  read_data.size.times {|i|
+    if File.exist?( read_data[ i ] ) == TRUE
+      com = "ruby " +  File::dirname(__FILE__) + "/nclinkrw.rb " + read_data[ i ] + " " + ARGV[ ARGV_COPY_DIR ]
+      if File.exist?( File::basename( read_data[ i ] ) ) == FALSE
+        printf("copy %s\n", File::basename( read_data[ i ] ) )
+        system( com )
+      elsif File::stat( File::basename( read_data[ i ] ) ).mtime < File::stat( read_data[ i ] ).mtime
+        printf("copy %s\n", File::basename( read_data[ i ] ) )
+        system( com )
+      end
+    end
+  }
+
