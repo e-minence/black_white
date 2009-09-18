d8738e5d50d6b37670a96654a40f8adbde25e29b
Tomoya Takahashi <tomoya_takahashi@gamefreak.co.jp>
2009-09-18 07:22:43 +0000

    レール情報のコンバート環境を作成

---
+++ resource/fldmapdata/rail_data/conv.pl
@@ -0,0 +1,145 @@
+#################################################
+#
+# レール構成データのコンバート
+# 
+#   conv.pl   レールデータ rail_dataのnaix  sceneareaのnaix
+#
+#################################################
+
+$NONE_NUM = 0xffff; 
+$DATA_OUT_SIZE = "S";  #packに渡す文字
+
+if( @ARGV != 3 )
+{
+  print( "conv.pl   レールデータ rail_dataのnaix  sceneareaのnaix\n" );
+  exit(1);
+}
+
+@RAIL_DATA_NAIX = undef;
+@AREA_DATA_NAIX = undef;
+
+
+#読み込み
+open( FILEIN, $ARGV[0] );
+@input_file = <FILEIN>;
+close( FILEIN );
+
+#rail_dataのNAIX読み込み
+&loadNaix( $ARGV[1], \@RAIL_DATA_NAIX );
+
+#sceneareaのNAIX読み込み
+&loadNaix( $ARGV[2], \@AREA_DATA_NAIX );
+
+
+$TAG = 0;
+$RAIL_ID = 0;
+$SCENE_ID = 0;
+
+$data_in = 0;
+foreach $one ( @input_file )
+{
+  @data_one = split( /\t/, $one );
+
+  if( $data_in == 0 )
+  {
+    if( $data_one[0] =~ /#START/ )
+    {
+      $data_in = 1;
+    }
+  }
+  elsif( $data_in == 1 )
+  {
+    if( $data_one[0] =~ /#END/ )
+    {
+      $data_in = 2;
+    }
+    else
+    {
+    
+      $TAG        = $data_one[0];
+      $RAIL_ID    = &getNaixIndex( \@RAIL_DATA_NAIX, $data_one[1] );
+      $SCENE_ID   = &getNaixIndex( \@AREA_DATA_NAIX, $data_one[2] );
+
+
+      #出力
+      open( FILEOUT, ">".$TAG.".dat" );
+      binmode( FILEOUT );
+
+      print( FILEOUT pack( $DATA_OUT_SIZE, $RAIL_ID ) );
+      print( FILEOUT pack( $DATA_OUT_SIZE, $SCENE_ID ) );
+      
+      close( FILEOUT );
+    }
+  }
+}
+
+
+exit(0);
+
+
+
+
+
+
+
+
+## naixの読み込み
+sub loadNaix
+{
+  my( $filename, $buff ) = @_;
+  my( $load_one, @naix_data, $naix_data_in, $naix_count );
+  
+  open( NAIXFILE, $filename );
+  @naix_data = <NAIXFILE>;
+  close( NAIXFILE );
+
+  $naix_data_in = 0;
+  $naix_count = 0;
+  foreach $load_one ( @naix_data )
+  {
+    if( $naix_data_in == 0 )
+    {
+      if( $load_one =~ /{/ )
+      {
+        $naix_data_in = 1;
+      }
+    }
+    elsif( $naix_data_in == 1 )
+    {
+      if( $load_one =~ /}/ )
+      {
+        $naix_data_in = 2;
+      }
+      else
+      {
+        if( $load_one =~ /[\s\t]*([^\s]*) = / )
+        {
+          $$buff[ $naix_count ] = $1;
+          $naix_count ++;
+        }
+      }
+    }
+  }
+}
+
+
+sub getNaixIndex
+{
+  my( $buff, $sarch ) = @_;
+  my( $sarch_one, $sarch_count );
+
+  $sarch_count = 0;
+  foreach $sarch_one ( @$buff )
+  {
+    if( $sarch_one =~ /$sarch/ )
+    {
+      return $sarch_count;
+    }
+
+    $sarch_count++;
+  }
+}
+
+
+
+
