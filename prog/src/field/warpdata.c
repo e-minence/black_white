7884616f8cd5a9261b325b24b2ca137d95604cfd
Sosuke Tamada <tamada@gamefreak.co.jp>
2009-09-20 09:33:08 +0000

    そらをとぶ・全滅などの遷移先データベースを過去作から移植

---
+++ prog/src/field/warpdata.c
@@ -0,0 +1,265 @@
+//============================================================================================
+/**
+ * @file	warpdata.c
+ * @brief	ワープ用データ関連ソース
+ * @since	2005.12.19
+ * 
+ * 2009.09.20 HGSSから移植開始
+ */
+//============================================================================================
+
+#include <gflib.h>
+
+#include "gamesystem/game_data.h"
+#include "warpdata.h"
+
+#include "field/location.h"
+
+#include "arc/fieldmap/zone_id.h"
+
+#include "field/field_const.h"  //GRID_TO_FX32
+
+//#include "fieldobj_code.h"		//DIR_UP DIR_DOWN
+//#include "ev_mapchange.h"		//DOOR_ID_JUMP_CODE
+
+#include "../../../resource/fldmapdata/flagwork/flag_define.h"
+//#include "sysflag.h"			//SysFlag_ArriveSet
+
+//自動生成されるはずのシンボル定義
+#include "../../../resource/fldmapdata/warpdata/warpdata.h"
+
+//============================================================================================
+//============================================================================================
+//------------------------------------------------------------------
+//------------------------------------------------------------------
+#if 0
+typedef struct {
+	u16 room_id, room_gx, room_gz;
+	u16 fld_id, fld_gx, fld_gz;
+	u8 IsTeleportPos;
+	u8 AutoSetArriveFlag;
+	u16 arrive_id;
+}WARPDATA;
+#else
+typedef struct {
+	u16	arrive_id:8;	//到着フラグ
+	u16	IsTeleportPos:1;
+	u16	AutoSetArriveFlag:1;
+	u16	:6;
+
+	u16	room_id;	//全滅時の戻り先ZoneID
+	u16	room_gx:8;	//全滅時の戻り先X
+	u16	room_gz:8;	//全滅時の戻り先Z
+	
+	u16	fld_id;		//ワープ座標ZoneID
+	u16 fld_gx;		//ワープ座標X
+	u16	fld_gz;		//ワープ座標Z
+
+	u16	escape_id;	//出口座標ZoneID
+	u16 escape_gx;	//出口座標X
+	u16	escape_gz;	//出口座標Z
+}WARPDATA;
+
+SDK_COMPILER_ASSERT(sizeof(WARPDATA) == 18);
+
+#endif
+//自動生成されるはずのデータ定義
+#include "../../../resource/fldmapdata/warpdata/warpdata.cdat"
+
+
+static void setLocation(LOCATION * loc, u16 zone_id, u16 dir, u16 gx, u16 gz);
+//============================================================================================
+//============================================================================================
+//------------------------------------------------------------------
+/**
+ * @brief	ワープIDの正規化
+ * @param	warp_id		ワープID
+ * @return	int			正規化したワープID
+ *
+ * 不正な値は変換する。1オリジンを0オリジンに変換する。
+ */
+//------------------------------------------------------------------
+static int RegulateWarpID(int warp_id)
+{
+	if(warp_id <= 0 || warp_id > NELEMS(WarpData)) {
+		GF_ASSERT_MSG(0, "不正なワープID（%d）です。\n", warp_id);
+		warp_id = 1;		//製品版では不正な値でも動作するように補正する
+	}
+	warp_id --;		//1 origin --> 0 origin
+	return warp_id;
+}
+
+//------------------------------------------------------------------
+/**
+ * @brief	ワープIDの初期値を取得する
+ */
+//------------------------------------------------------------------
+int WARPDATA_GetInitializeID(void)
+{
+	return 1;
+}
+
+//------------------------------------------------------------------
+/**
+ * @brief	ワープ場所の取得
+ * @param	warp_id		ワープID
+ * @param	loc			場所を受け取るLOCATIONへのポインタ
+ */
+//------------------------------------------------------------------
+void WARPDATA_GetWarpLocation(int warp_id, LOCATION * loc)
+{
+	warp_id = RegulateWarpID(warp_id);
+
+  setLocation( loc,
+      WarpData[warp_id].fld_id,
+      DIR_DOWN, 
+      WarpData[warp_id].fld_gx,
+      WarpData[warp_id].fld_gz );
+}
+
+//------------------------------------------------------------------
+/**
+ * @brief	復活場所の取得
+ * @param	warp_id		ワープID
+ * @param	loc			場所を受け取るLOCATIONへのポインタ
+ */
+//------------------------------------------------------------------
+void WARPDATA_GetRevivalLocation(int warp_id, LOCATION * loc)
+{
+	warp_id = RegulateWarpID(warp_id);
+
+  setLocation( loc,
+      WarpData[warp_id].room_id,
+      DIR_UP, 
+      WarpData[warp_id].room_gx,
+      WarpData[warp_id].room_gz );
+}
+
+//------------------------------------------------------------------
+/**
+ * @brief	出口座標の取得(Escapeフラグのないポイントなら空飛ぶ座標を返す)
+ * @param	warp_id		ワープID
+ * @param	loc			場所を受け取るLOCATIONへのポインタ
+ */
+//------------------------------------------------------------------
+void WARPDATA_GetEscapeLocation(int warp_id, LOCATION * loc)
+{
+	int id = RegulateWarpID(warp_id);
+
+  setLocation( loc,
+      WarpData[warp_id].escape_id,
+      DIR_DOWN, 
+      WarpData[warp_id].escape_gx,
+      WarpData[warp_id].escape_gz );
+}
+
+//------------------------------------------------------------------
+/**
+ * @brief	ワープIDの取得
+ * @param	zone_id		検索する屋内のゾーンID
+ * @retval	int			ワープID（１オリジン）
+ * @retval	0			見つからなかった
+ */
+//------------------------------------------------------------------
+int WARPDATA_SearchByRoomID(int zone_id)
+{
+	int i;
+	for (i = 0; i < NELEMS(WarpData); i++) {
+		if (WarpData[i].room_id == zone_id
+				&& WarpData[i].IsTeleportPos) {
+			return i + 1;
+		}
+	}
+	return 0;
+}
+
+//------------------------------------------------------------------
+/**
+ * @brief	ワープIDの取得
+ * @param	zone_id		検索するフィールドのゾーンID
+ * @retval	int			ワープID（１オリジン）
+ * @retval	0			見つからなかった
+ */
+//------------------------------------------------------------------
+int WARPDATA_SearchByFieldID(int zone_id)
+{
+	int i;
+	for (i = 0; i < NELEMS(WarpData); i++) {
+		if (WarpData[i].fld_id == zone_id
+				&& WarpData[i].IsTeleportPos) {
+			return i + 1;
+		}
+	}
+	return 0;
+}
+
+//------------------------------------------------------------------
+/**
+ * @brief	ワープIDの取得（「そらをとぶ」用）
+ * @param	zone_id		検索するフィールドのゾーンID
+ * @retval	int			ワープID（１オリジン）
+ * @retval	0			見つからなかった
+ */
+//------------------------------------------------------------------
+int WARPDATA_SearchForFly(int zone_id)
+{
+	int i;
+	int id = 0;
+	for (i = 0; i < NELEMS(WarpData); i++) {
+		if (WarpData[i].fld_id == zone_id) {
+			return (i+1);
+		}
+	}
+	return id;
+}
+
+//============================================================================================
+//============================================================================================
+//------------------------------------------------------------------
+/**
+ * @brief	到着フラグのセット
+ * @param	fsys		フィールド制御ワークへのポインタ
+ * @param	zone_id		セットする場所のゾーンID
+ */
+//------------------------------------------------------------------
+void ARRIVEDATA_SetArriveFlag( GAMEDATA * gamedata, int zone_id)
+{
+	int i;
+	for (i = 0; i < NELEMS(WarpData); i++) {
+		OS_Printf("zone = %d, WarpZone = %d\n",zone_id,WarpData[i].fld_id);
+		if (WarpData[i].fld_id == zone_id && WarpData[i].AutoSetArriveFlag) {
+			EVENTWORK_SetEventFlag(GAMEDATA_GetEventWork( gamedata ), WarpData[i].arrive_id);
+			return;
+		}
+	}
+}
+
+//------------------------------------------------------------------
+/**
+ * @brief	到着フラグを参照する
+ * @param	fsys		フィールド制御ワークへのポインタ
+ * @param	warp_id		対象となるマップのワープID
+ * @retval	BOOL		TRUEのとき、来たことがある
+ */
+//------------------------------------------------------------------
+BOOL ARRIVEDATA_GetArriveFlag(GAMEDATA * gamedata, int warp_id)
+{
+	int reg_id = RegulateWarpID(warp_id);
+	return EVENTWORK_CheckEventFlag(GAMEDATA_GetEventWork( gamedata ), WarpData[reg_id].arrive_id);
+}
+
+//============================================================================================
+//============================================================================================
+//------------------------------------------------------------------
+//------------------------------------------------------------------
+static void setLocation(LOCATION * loc, u16 zone_id, u16 dir, u16 gx, u16 gz)
+{
+  fx32 x, y, z;
+  x = GRID_TO_FX32( gx );
+  y = 0;
+  z = GRID_TO_FX32( gz );
+
+  LOCATION_SetDirect(loc, zone_id, DIR_DOWN, x, y, z);
+}
+
+
---
+++ prog/src/field/warpdata.h
@@ -0,0 +1,33 @@
+//============================================================================================
+/**
+ * @file	warpdata.h
+ * @brief	ワープ用データ関連ヘッダ
+ * @date	2005.12.19
+ *
+ * 2009.09.20 HGSSから移植開始
+ */
+//============================================================================================
+
+#pragma once
+
+
+#include "field/location.h"
+
+//--------------------------------------------------------------------------------------------
+//	ワープとび先
+//--------------------------------------------------------------------------------------------
+extern void WARPDATA_GetWarpLocation(int warp_id, LOCATION * loc);
+extern void WARPDATA_GetRevivalLocation(int warp_id, LOCATION * loc);
+extern void WARPDATA_GetEscapeLocation(int warp_id, LOCATION * loc);
+extern int WARPDATA_SearchByRoomID(int zone_id);
+extern int WARPDATA_SearchByFieldID(int zone_id);
+extern int WARPDATA_GetInitializeID(void);
+extern int WARPDATA_SearchForFly(int zone_id);
+
+//--------------------------------------------------------------------------------------------
+//	到着フラグ
+//--------------------------------------------------------------------------------------------
+extern void ARRIVEDATA_SetArriveFlag( GAMEDATA * gamedata, int zone_id );
+extern BOOL ARRIVEDATA_GetArriveFlag( GAMEDATA * gamedata, int warp_id );
+
+
