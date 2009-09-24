0a6d471e9e78ae93753b7d03810abc07eb4c0c2b
Nozomu Saito <saito_nozomu@gamefreak.co.jp>
2009-09-24 10:36:03 +0000

    //ノーマルジムセーブデータ型追加

---
+++ prog/src/field/gym_normal_sv.h
@@ -0,0 +1,18 @@
+//======================================================================
+/**
+ * @file  gym_normal_sv.h
+ * @bfief  ノーマルジムセーブデータ
+ * @author  Saito
+ */
+//======================================================================
+#ifndef __GYM_NORMAL_SV_H__
+
+#define __GYM_NORMAL_SV_H__
+
+//ノーマルジムセーブワーク
+typedef struct GYM_NORMAL_SV_WORK_tag
+{
+  u8 Wall[4];
+}GYM_NORMAL_SV_WORK;
+
+#endif  //__GYM_NORMAL_SV_H__
