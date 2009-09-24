8b85b3f9b1e640467f10c8b95c957f2e009200fd
Sosuke Tamada <tamada@gamefreak.co.jp>
2009-09-24 08:44:54 +0000

    育て屋用スクリプトファイルを追加

---
+++ resource/fldmapdata/script/sodateya_scr_def.h
@@ -0,0 +1,10 @@
+//sodateya_scr_def.h
+//このファイルはコンバータによって作成されました
+
+#ifndef _SODATEYA_SCR_DEF_H_
+#define _SODATEYA_SCR_DEF_H_
+
+//スクリプトデータID定義
+#define SCRID_SODATEYA_SCR (1)
+
+#endif //_SODATEYA_SCR_DEF_H_
