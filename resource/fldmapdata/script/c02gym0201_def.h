8be67adaa540f4d7cd6510c1073cc7d520c4414b
Sosuke Tamada <tamada@gamefreak.co.jp>
2009-09-25 03:39:58 +0000

    C02GYM0201のスクリプトファイル（ダミー）を追加

---
+++ resource/fldmapdata/script/c02gym0201_def.h
@@ -0,0 +1,10 @@
+//c02gym0201_def.h
+//このファイルはコンバータによって作成されました
+
+#ifndef _C02GYM0201_DEF_H_
+#define _C02GYM0201_DEF_H_
+
+//スクリプトデータID定義
+#define SCRID_C02GYM0201 (1)
+
+#endif //_C02GYM0201_DEF_H_
