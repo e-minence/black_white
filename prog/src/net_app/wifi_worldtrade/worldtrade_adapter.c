9c78b9903efbf68f0332f173bd8f7f684eab81e5
Toru Nagihashi <nagihashi@gamefreak.co.jp>
2009-09-28 08:46:39 +0000

    フォントデータフォーマット変更に対応

--- prog/src/net_app/wifi_worldtrade/worldtrade_adapter.c
+++ prog/src/net_app/wifi_worldtrade/worldtrade_adapter.c
@@ -234,7 +234,7 @@ void WT_PRINT_Init( WT_PRINT *wk, const CONFIG *cfg )
 
 	wk->tcbsys	= GFL_TCBL_Init( HEAPID_WORLDTRADE, HEAPID_WORLDTRADE, 32, 32 );
 	wk->cfg			= cfg;
-	wk->font		= GFL_FONT_Create( ARCID_FONT, NARC_font_large_nftr, GFL_FONT_LOADTYPE_FILE, FALSE, HEAPID_WORLDTRADE );
+	wk->font		= GFL_FONT_Create( ARCID_FONT, NARC_font_large_gftr, GFL_FONT_LOADTYPE_FILE, FALSE, HEAPID_WORLDTRADE );
 
 
 	{	
