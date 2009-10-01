16f851009b6642ff65fcce36f6e597527344ed5e
Masao Taya <taya@gamefreak.co.jp>
2009-10-01 06:09:19 +0000

    追加

---
+++ prog/include/gamesystem/btl_setup.h
@@ -0,0 +1,23 @@
+//=============================================================================================
+/**
+ * @file  btl_setup.h
+ * @brief ポケモンWB バトルパラメータ簡易セットアップ処理
+ * @author  taya
+ *
+ * @date  2009.10.01 作成
+ */
+//=============================================================================================
+#pragma once
+
+#include "battle/battle.h"
+#include "gamesystem/game_data.h"
+
+extern void BTL_SETUP_Single_Wild( BATTLE_SETUP_PARAM* dst, GAMEDATA* gameData,
+  POKEPARTY* partyEnemy, BtlLandForm landForm, BtlWeather weather );
+
+extern void BTL_SETUP_Single_Trainer( BATTLE_SETUP_PARAM* dst, GAMEDATA* gameData,
+  POKEPARTY* partyEnemy, BtlLandForm landForm, BtlWeather weather, TrainerID trID );
+
+extern void BTL_SETUP_Single_Comm( BATTLE_SETUP_PARAM* dst, GAMEDATA* gameData,
+  GFL_NETHANDLE* netHandle, BtlCommMode commMode );
+
