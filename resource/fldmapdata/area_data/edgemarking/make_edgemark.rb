7aa32a10a86f1f54774cc292ca2f48528a07d7f8
Sosuke Tamada <tamada@gamefreak.co.jp>
2009-10-01 02:05:44 +0000

    RGB生成の並びが間違っていたので修正

--- resource/fldmapdata/area_data/edgemarking/make_edgemark.rb
+++ resource/fldmapdata/area_data/edgemarking/make_edgemark.rb
@@ -53,7 +53,8 @@ def getRGB(line)
   if r >= 32 then raise InputDataError, "G‚Ì’l‚ª§ŒÀ‚ğ‰z‚¦‚Ä‚¢‚Ü‚·" end
   b = Integer(column[2])
   if r >= 32 then raise InputDataError, "B‚Ì’l‚ª§ŒÀ‚ğ‰z‚¦‚Ä‚¢‚Ü‚·" end
-  value = Integer( r * 1024 + g * 32 + b )
+  value = Integer( b * 1024 + g * 32 + r )
+  //value = Integer( r * 1024 + g * 32 + b )
   puts "R=#{r} G=#{g} B=#{b} rgb = #{sprintf("%04x",value)}"
   return value
 end
