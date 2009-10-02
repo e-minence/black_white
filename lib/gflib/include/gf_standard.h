867a8a08c8f91f44e65bf7ff12361b7811fac0a7
Masao Taya <taya@gamefreak.co.jp>
2009-10-02 04:11:32 +0000

    C標準のstrcmpに相当する関数を追加

--- lib/gflib/include/gf_standard.h
+++ lib/gflib/include/gf_standard.h
@@ -3,11 +3,11 @@ extern "C" {
 #endif
 //=============================================================================================
 /**
- * @file	gf_standard.h
- * @brief	Ｃ標準関数定義用ヘッダ
+ * @file  gf_standard.h
+ * @brief Ｃ標準関数定義用ヘッダ
  *          inlineにできないものは gf_standard.cに記述します
- * @author	GAME FREAK inc.
- * @date	2006.11.29
+ * @author  GAME FREAK inc.
+ * @date  2006.11.29
  */
 //=============================================================================================
 
@@ -41,27 +41,27 @@ typedef struct {
 } GFL_RECT;
 
 
-#define GFL_STD_RAND_MAX	(0)  ///< MtRand等で渡す最大乱数を引き出す引数
+#define GFL_STD_RAND_MAX  (0)  ///< MtRand等で渡す最大乱数を引き出す引数
 
 //----------------------------------------------------------------------------
 /**
- *	@brief	標準ライブラリの初期化関数
- *	@retval	なし
+ *  @brief  標準ライブラリの初期化関数
+ *  @retval なし
  */
 //-----------------------------------------------------------------------------
 extern void GFL_STD_Init(HEAPID heapID);
 
 //----------------------------------------------------------------------------
 /**
- *	@brief	標準ライブラリの終了関数
- *	@retval	なし
+ *  @brief  標準ライブラリの終了関数
+ *  @retval なし
  */
 //-----------------------------------------------------------------------------
 extern void GFL_STD_Exit(void);
 
 //--------------------------------------------------------------------
 /**
- * @brief	メモリーコピー関数
+ * @brief メモリーコピー関数
  * @param   src   コピー元
  * @param   dest  コピー先
  * @param   size  サイズ
@@ -72,7 +72,7 @@ inline void GFL_STD_MemCopy( const void* src, void* dest, u32 size ) { MI_CpuCop
 
 //--------------------------------------------------------------------
 /**
- * @brief	メモリーコピー関数 16BIT転送版
+ * @brief メモリーコピー関数 16BIT転送版
  * @param   src   コピー元
  * @param   dest  コピー先
  * @param   size  サイズ
@@ -83,7 +83,7 @@ inline void GFL_STD_MemCopy16( const void* src, void* dest, u32 size ) { MI_CpuC
 
 //--------------------------------------------------------------------
 /**
- * @brief	メモリーコピー関数 32BIT転送版
+ * @brief メモリーコピー関数 32BIT転送版
  * @param   src   コピー元
  * @param   dest  コピー先
  * @param   size  サイズ
@@ -94,7 +94,7 @@ inline void GFL_STD_MemCopy32( const void* src, void* dest, u32 size ) { MI_CpuC
 
 //--------------------------------------------------------------------
 /**
- * @brief	メモリを指定のデータで埋めます
+ * @brief メモリを指定のデータで埋めます
  * @param   dest   転送先アドレス
  * @param   data  転送データ
  * @param   size  サイズ
@@ -105,7 +105,7 @@ inline void GFL_STD_MemFill( void* dest, u8 data, u32 size ) { MI_CpuFill8(dest,
 
 //--------------------------------------------------------------------
 /**
- * @brief	メモリを指定のデータで埋めます(16BIT版)
+ * @brief メモリを指定のデータで埋めます(16BIT版)
  * @param   dest   転送先アドレス
  * @param   data  転送データ
  * @param   size  サイズ
@@ -116,7 +116,7 @@ inline void GFL_STD_MemFill16( void* dest, u16 data, u32 size ) { MI_CpuFill16(d
 
 //--------------------------------------------------------------------
 /**
- * @brief	メモリを指定のデータで埋めます(32BIT版)
+ * @brief メモリを指定のデータで埋めます(32BIT版)
  * @param   dest   転送先アドレス
  * @param   data  転送データ
  * @param   size  サイズ
@@ -127,7 +127,7 @@ inline void GFL_STD_MemFill32( void* dest, u32 data, u32 size ) { MI_CpuFill32(d
 
 //--------------------------------------------------------------------
 /**
- * @brief	メモリを０でクリアします
+ * @brief メモリを０でクリアします
  * @param   dest   転送先アドレス
  * @param   size  サイズ
  * @return  none
@@ -137,7 +137,7 @@ inline void GFL_STD_MemClear( void* dest, u32 size ) { MI_CpuClear8(dest,size); 
 
 //--------------------------------------------------------------------
 /**
- * @brief	メモリを０でクリアします(16BIT版)
+ * @brief メモリを０でクリアします(16BIT版)
  * @param   dest   転送先アドレス
  * @param   size  サイズ
  * @return  none
@@ -147,7 +147,7 @@ inline void GFL_STD_MemClear16( void* dest, u32 size ) { MI_CpuClear16(dest,size
 
 //--------------------------------------------------------------------
 /**
- * @brief	メモリを０でクリアします(32BIT版)
+ * @brief メモリを０でクリアします(32BIT版)
  * @param   dest   転送先アドレス
  * @param   size  サイズ
  * @return  none
@@ -157,62 +157,75 @@ inline void GFL_STD_MemClear32( void* dest, u32 size ) { MI_CpuClear32(dest,size
 
 //----------------------------------------------------------------------------
 /**
- *	@brief	メモリ領域を比較する
- *	@param	s1	    比較メモリー1
- *	@param	s2	    比較メモリー2
- *	@param	size	比較サイズ
- *	@retval	０      同じ
- *	@retval	！０    異なる
+ *  @brief  メモリ領域を比較する
+ *  @param  s1      比較メモリー1
+ *  @param  s2      比較メモリー2
+ *  @param  size  比較サイズ
+ *  @retval ０      同じ
+ *  @retval ！０    異なる
  */
 //-----------------------------------------------------------------------------
 extern int GFL_STD_MemComp( const void *s1, const void *s2, const u32 size );
 
 //----------------------------------------------------------------------------
 /**
- *	@brief	絶対値を得る 注意  式を書くと二回呼ばれることがあります
- *	@param	a       絶対値がほしい値
- *	@retval	        絶対値
+ *  @brief  絶対値を得る 注意  式を書くと二回呼ばれることがあります
+ *  @param  a       絶対値がほしい値
+ *  @retval         絶対値
  */
 //-----------------------------------------------------------------------------
 inline int GFL_STD_Abs(int a){ return MATH_ABS(a); }
 
 //----------------------------------------------------------------------------
 /**
- *	@brief	０が現れるまでのバイト数を計る
+ *  @brief  ０が現れるまでのバイト数を計る
             文字の扱いにはSTRBUFを使うこと  これは通信のSSIDなど ASCII文字列に使用する用途にもちいる事
- *	@param	s	    データ列
- *	@return	サイズ＝長さ
+ *  @param  s     データ列
+ *  @return サイズ＝長さ
  */
 //-----------------------------------------------------------------------------
 extern int GFL_STD_StrLen( const void *s );
 //----------------------------------------------------------------------------
 /**
- *	@brief	文字列連結
+ *  @brief  文字列連結
             文字の扱いにはSTRBUFを使うこと  これは通信のSSIDなど ASCII文字列に使用する用途にもちいる事
- *	@param	src	  連結される方の文字列
- *	@param	dist	連結する方の文字列
- *	@param	s	    サイズ
- *	@return	none
+ *  @param  src   連結される方の文字列
+ *  @param  dist  連結する方の文字列
+ *  @param  s     サイズ
+ *  @return none
  */
 //-----------------------------------------------------------------------------
 extern void GFL_STD_StrCat( void *src, const void *dist, int size );
 
 //----------------------------------------------------------------------------
 /**
- *	@brief	文字列比較 \0'以降の比較は行いません。
-            文字の扱いにはSTRBUFを使うこと  これは通信のSSIDなど ASCII文字列に使用する用途にもちいる事
- *	@param	cmp1	  連結される方の文字列
- *	@param	cmp2	連結する方の文字列
- *	@param	size	    サイズ
- *	@return	0なら同じ 0!=なら異なる
+ *  @brief  文字列比較  (C標準 strncmp と等価です）
+ *          文字の扱いにはSTRBUFを使うこと  これは通信のSSIDなど ASCII文字列に使用する用途にもちいる事
+ *  @param  str1  比較する文字列１
+ *  @param  str2  比較する文字列２
+ *  @return 一致していれば0、その他の場合には0以外が返る
  */
 //-----------------------------------------------------------------------------
-extern int GFL_STD_StrnCmp( const void *cmp1, const void *cmp2, int size );
+extern int GFL_STD_StrCmp( const char* str1, const char* str2 );
 
 //----------------------------------------------------------------------------
 /**
- *	@brief   メルセンヌツイスター初期化関数 RTCとvsyncnumで初期化してください
- *	@param   s  シード値(この値から初期化値が線形合同法で生成されます)
+ *  @brief  文字列比較  (C標準 strncmp と等価です）
+ *          文字の扱いにはSTRBUFを使うこと  これは通信のSSIDなど ASCII文字列に使用する用途にもちいる事
+ *  @param  str1  比較する文字列１
+ *  @param  str2  比較する文字列２
+ *  @param  len   比較する文字列長
+ *  @return len まで一致していれば0、その他の場合には0以外が返る
+ */
+//-----------------------------------------------------------------------------
+extern int GFL_STD_StrnCmp( const char *str1, const char *str2, int len );
+
+
+
+//----------------------------------------------------------------------------
+/**
+ *  @brief   メルセンヌツイスター初期化関数 RTCとvsyncnumで初期化してください
+ *  @param   s  シード値(この値から初期化値が線形合同法で生成されます)
  *  @return  none
  */
 //----------------------------------------------------------------------------
@@ -225,7 +238,7 @@ extern void GFL_STD_MtRandInit(u32 s);
  *                  GFL_STD_RAND_MAX=0を指定した場合にはすべての範囲の32bit値となります。
  *                  % や / を使用せずにここのrange値を変更してください
  *                  GFL_STD_RAND_MAXを使わない場合は下の関数
- *	@return  生成された乱数
+ *  @return  生成された乱数
  */
 //----------------------------------------------------------------------------
 extern u32 __GFL_STD_MtRand(void); ///< 使用禁止  下のinline関数を使ってください
@@ -247,7 +260,7 @@ static inline u32 GFL_STD_MtRand(u32 range)
  *  @param   range    取得数値の範囲を指定 0 ～ range-1 の範囲の値が取得できます。
  *                    % や / を使用せずにここのrange値を変更してください
  *                  全部とる場合上の関数
- *	@return  生成された乱数
+ *  @return  生成された乱数
  */
 //----------------------------------------------------------------------------
 static inline u32 GFL_STD_MtRand0(u32 range)
@@ -325,10 +338,10 @@ static inline u32 GFL_STD_Rand0(GFL_STD_RandContext *context, u32 range)
 
 //----------------------------------------------------------------------------
 /**
- *	@brief	CRCを計算する
- *	@param	pData	    計算する領域
- *	@param	dataLength  データの長さ
- *	@return	CRC値
+ *  @brief  CRCを計算する
+ *  @param  pData     計算する領域
+ *  @param  dataLength  データの長さ
+ *  @return CRC値
  */
 //-----------------------------------------------------------------------------
 
@@ -345,22 +358,22 @@ extern u16 GFL_STD_CrcCalc( const void* pData, u32 dataLength );
 extern void GFL_STD_RandGeneralInit(GFL_STD_RandContext *context);
 
 //----------------------------------------------------------------------------------------------
-//	暗号化系移植
+//  暗号化系移植
 //----------------------------------------------------------------------------------------------
 //----------------------------------------------------------------------------------------------
 /**
  * チェックサムの算出ルーチン
  *
- * @param	adrs	開始アドレス
- * @param	size	データサイズ
+ * @param adrs  開始アドレス
+ * @param size  データサイズ
  *
- * @retval	"算出した値"
+ * @retval  "算出した値"
  */
 //----------------------------------------------------------------------------------------------
 extern u32 GFL_STD_CODED_CheckSum(const void * adrs, u32 size);
 
-extern void	GFL_STD_CODED_Coded(void *data,u32 size,u32 code);
-extern void	GFL_STD_CODED_Decoded(void *data,u32 size,u32 code);
+extern void GFL_STD_CODED_Coded(void *data,u32 size,u32 code);
+extern void GFL_STD_CODED_Decoded(void *data,u32 size,u32 code);
 
 
 #ifdef __cplusplus
