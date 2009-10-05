e8a35b11057e7fd47596a2fac8476234038ae6af
Naoto Murakami <murakami_naoto@gamefreak.co.jp>
2009-10-05 12:26:50 +0000

    スクリプト作業_イベントデータ（R02）を修正しました

--- resource/fldmapdata/script/r02_def.h
+++ resource/fldmapdata/script/r02_def.h
@@ -6,8 +6,10 @@
 
 //スクリプトデータID定義
 #define SCRID_POS_R02_RIVAL_01 (1)
-#define SCRID_R02_TALKOBJ_MAMA (2)
-#define SCRID_R02_TALKOBJ_BOY2 (3)
-#define SCRID_R02_TALKOBJ_GIRL2 (4)
+#define SCRID_R02_MAMA_01 (2)
+#define SCRID_R02_BOY2_01 (3)
+#define SCRID_R02_GIRL2_01 (4)
+#define SCRID_BG_R02_BOARD_01 (5)
+#define SCRID_BG_R02_BOARD_02 (6)
 
 #endif //_R02_DEF_H_
