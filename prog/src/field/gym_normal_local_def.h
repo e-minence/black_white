ae30bc1f5fb09bfb78a244a2c405bcf6e4136e13
Nozomu Saito <saito_nozomu@gamefreak.co.jp>
2009-10-06 04:31:03 +0000

    //ノーマルジム本棚配置座標定義追加

---
+++ prog/src/field/gym_normal_local_def.h
@@ -0,0 +1,29 @@
+//======================================================================
+/**
+ * @file  gym_normal_local_def.h
+ * @brief  ノーマルジムフレーム制御定義
+ * @author  Saito
+ */
+//======================================================================
+#ifndef __GYM_NOEMAL_LOCAL_DEF_H__
+
+#define __GYM_NORMAL_LOCAL_DEF_H__
+
+#define WALL0_GX  (16)  //一部屋目の正解本棚X座標
+#define WALL0_GZ  (16)  //一部屋目の正解本棚Z座標
+#define WALL1_GX  (16)  //二部屋目の正解本棚X座標
+#define WALL1_GZ  (16)  //二部屋目の正解本棚Z座標
+#define WALL2_GX  (1)
+#define WALL2_GZ  (1)
+#define WALL3_GX  (2)
+#define WALL3_GZ  (2)
+#define WALL4_GX  (3)
+#define WALL4_GZ  (3)
+#define WALL5_GX  (4)
+#define WALL5_GZ  (4)
+#define WALL6_GX  (5)
+#define WALL6_GZ  (5)
+
+#endif  //__GYM_NORMAL_LOCAL_DEF_H__
+
+
