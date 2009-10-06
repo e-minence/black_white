7c2b61504d16d6a1e46b73d347a415c322d5b00f
Naoto Murakami <murakami_naoto@gamefreak.co.jp>
2009-10-06 06:38:16 +0000

    スクリプト作業_R02を修正しました

--- resource/fldmapdata/script/r02_def.h
+++ resource/fldmapdata/script/r02_def.h
@@ -9,7 +9,8 @@
 #define SCRID_R02_MAMA_01 (2)
 #define SCRID_R02_BOY2_01 (3)
 #define SCRID_R02_GIRL2_01 (4)
-#define SCRID_BG_R02_BOARD_01 (5)
-#define SCRID_BG_R02_BOARD_02 (6)
+#define SCRID_BG_R02_BOARD1_01 (5)
+#define SCRID_BG_R02_BOARD2_01 (6)
+#define SCRID_BG_R02_BOARD3_01 (7)
 
 #endif //_R02_DEF_H_
