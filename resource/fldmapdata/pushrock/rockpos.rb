596a1d8f0c50717ba59b16ded02ebb64dc3f3bf1
Keita Kagaya <kagaya@gamefreak.co.jp>
2009-10-07 08:09:51 +0000

    出力するデータ形式を変更

--- resource/fldmapdata/pushrock/rockpos.rb
+++ resource/fldmapdata/pushrock/rockpos.rb
@@ -22,22 +22,27 @@ file_cdat = File.open( fname_cdat, "w" )
 file_cdat.printf( "//%s\n", fname_cdat )
 file_cdat.printf( "//このファイルはコンバータから出力されました\n\n" )
 file_cdat.printf( "#define %s (0xffff)\n\n", STR_PUSHROCK_DATA_END )
-file_cdat.printf( "unsigned short DATA_MMDL_PushRockPosNum[][2] = {\n" )
+file_cdat.printf( "//[Zone ID, OBJ ID, Use Number, Padding 4byte]\n" )
+file_cdat.printf( "unsigned short DATA_MMDL_PushRockPosNum[][4] = {\n" )
 
 line = file_csv.gets #データ開始位置まで進める
 line = file_csv.gets
 line = file_csv.gets
+
+i = 0
 
 while line = file_csv.gets
   line = line.strip
   str = line.split( "," )
   
   if( str[0] != nil && str[1] != nil && str[2] != nil )
-    file_cdat.printf( "  {%s,%s},\n", str[1], str[2] )
+    file_cdat.printf( "  {%s,%s,%d,0},\n", str[1], str[2], i )
   end
+  
+  i = i + 1
 end
 
-file_cdat.printf( "  {%s,%s},\n", STR_PUSHROCK_DATA_END, STR_PUSHROCK_DATA_END )
+file_cdat.printf( "  {%s,%s,%d,0},\n", STR_PUSHROCK_DATA_END, STR_PUSHROCK_DATA_END, i )
 file_cdat.printf( "};" )
 file_cdat.close
 file_csv.close
