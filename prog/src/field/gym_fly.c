0cef69452205596c114a28419aafc3d2abce0d17
Nozomu Saito <saito_nozomu@gamefreak.co.jp>
2009-10-08 07:56:48 +0000

    //飛行ジム更新

--- prog/src/field/gym_fly.c
+++ prog/src/field/gym_fly.c
@@ -11,10 +11,11 @@
 #include "field_gimmick_def.h"
 #include "savedata/gimmickwork.h"
 #include "gmk_tmp_wk.h"
+#include "system/ica_anime.h"
 
 #include "field/field_const.h"  //for FIELD_CONST_GRID_FX32_SIZE
 
-//#include "arc/fieldmap/gym_fly.naix"
+#include "arc/fieldmap/gym_fly.naix"
 
 #include "system/main.h"    //for HEAPID_FIELDMAP
 #include "script.h"     //for SCRIPT_CallScript
@@ -25,37 +26,49 @@
 #define GYM_FLY_UNIT_IDX (0)
 #define GYM_FLY_TMP_ASSIGN_ID  (1)
 
-#define FRAME_POS_SIZE  (4*3*360)   //座標ＸＹＺ各4バイトｘ360フレーム
+#define FRAME_POS_SIZE  (4*3*150)   //座標ＸＹＺ各4バイトｘ150フレーム
 #define HEADER_SIZE  (8)   //フレームサイズ4バイト+格納情報3バイトのヘッダー情報があるためアライメントを加味して8バイト確保する
 
 #define CANNON_NUM_MAX  (10)
 
+#define GRID_HALF_SIZE ((FIELD_CONST_GRID_SIZE/2)*FX32_ONE)
+
 //砲台グリッド座標
-#define CAN1_GX (22)
-#define CAN1_GZ (13)
+#define CAN1_GX (15)
+#define CAN1_GY (0)
+#define CAN1_GZ (28)
 #define CAN2_GX (15)
+#define CAN2_GY (0)
 #define CAN2_GZ (9)
 #define CAN3_GX (23)
+#define CAN3_GY (0)
 #define CAN3_GZ (17)
 #define CAN4_GX (20)
+#define CAN4_GY (0)
 #define CAN4_GZ (9)
 #define CAN5_GX (11)
+#define CAN5_GY (0)
 #define CAN5_GZ (13)
 #define CAN6_GX (8)
+#define CAN6_GY (0)
 #define CAN6_GZ (4)
 #define CAN7_GX (28)
+#define CAN7_GY (0)
 #define CAN7_GZ (4)
 #define CAN8_GX (26)
+#define CAN8_GY (0)
 #define CAN8_GZ (22)
 #define CAN9_GX (6)
+#define CAN9_GY (0)
 #define CAN9_GZ (20)
 #define CAN10_GX (2)
+#define CAN10_GY (0)
 #define CAN10_GZ (13)
 
 
 //砲台座標
 #define CAN1_X  (CAN1_GX*FIELD_CONST_GRID_FX32_SIZE + GRID_HALF_SIZE)
-#define CAN1_Y  (1*FIELD_CONST_GRID_FX32_SIZE + GRID_HALF_SIZE)
+#define CAN1_Y  (CAN1_GY*FIELD_CONST_GRID_FX32_SIZE)
 #define CAN1_Z  (CAN1_GZ*FIELD_CONST_GRID_FX32_SIZE + GRID_HALF_SIZE)
 
 #define CAN2_X  (CAN2_GX*FIELD_CONST_GRID_FX32_SIZE + GRID_HALF_SIZE)
@@ -94,6 +107,11 @@
 #define CAN10_Y  (1*FIELD_CONST_GRID_FX32_SIZE + GRID_HALF_SIZE)
 #define CAN10_Z  (CAN10_GZ*FIELD_CONST_GRID_FX32_SIZE + GRID_HALF_SIZE)
 
+//着地座標
+#define STAND1_GX (15)
+#define STAND1_GY (2)
+#define STAND1_GZ (19)
+
 //ジム内部中の一時ワーク
 typedef struct GYM_FLY_TMP_tag
 {
@@ -103,20 +121,24 @@ typedef struct GYM_FLY_TMP_tag
   u8 dummy;
 
   u8 FramePosDat[FRAME_POS_SIZE+HEADER_SIZE];
-
+  ICA_ANIME* IcaAnmPtr;
+  u16 NowIcaAnmFrmIdx;
+  u16 MaxIcaAnmFrm;
 }GYM_FLY_TMP;
 
 //リソースの並び順番
 enum {
   RES_ID_CAN_MDL = 0,
   RES_ID_CAN_ANM1_ICA,
-  RES_ID_CAN_ANM1_ITP,
+  RES_ID_CAN_ANM1_IMA,
+/**
   RES_ID_CAN_ANM2_ICA,
   RES_ID_CAN_ANM2_ITP,
   RES_ID_CAN_ANM3_ICA,
   RES_ID_CAN_ANM3_ITP,
   RES_ID_CAN_ANM4_ICA,
   RES_ID_CAN_ANM4_ITP,
+*/  
 };
 
 //ＯＢＪインデックス
@@ -156,28 +178,28 @@ static u8 CannoneRot[CANNON_NUM_MAX] = {
 };
 
 //大砲位置
-static const u16 CanPos[CANNON_NUM_MAX][2] = {
-  {CAN1_GX, CAN1_GZ},
-  {CAN2_GX, CAN2_GZ},
-  {CAN3_GX, CAN3_GZ},
-  {CAN4_GX, CAN4_GZ},
-  {CAN5_GX, CAN5_GZ},
-  {CAN6_GX, CAN6_GZ},
-  {CAN7_GX, CAN7_GZ},
-  {CAN8_GX, CAN8_GZ},
-  {CAN9_GX, CAN9_GZ},
-  {CAN10_GX, CAN10_GZ},
+static const u16 CanPos[CANNON_NUM_MAX][3] = {
+  {CAN1_GX, CAN1_GY, CAN1_GZ},
+  {CAN2_GX, CAN2_GY, CAN2_GZ},
+  {CAN3_GX, CAN3_GY, CAN3_GZ},
+  {CAN4_GX, CAN4_GY, CAN4_GZ},
+  {CAN5_GX, CAN5_GY, CAN5_GZ},
+  {CAN6_GX, CAN6_GY, CAN6_GZ},
+  {CAN7_GX, CAN7_GY, CAN7_GZ},
+  {CAN8_GX, CAN8_GY, CAN8_GZ},
+  {CAN9_GX, CAN9_GY, CAN9_GZ},
+  {CAN10_GX, CAN10_GY, CAN10_GZ},
 
 };
 
 //--リソース関連--
 //読み込む3Dリソース
-static const GFL_G3D_UTIL_RES g3Dutil_resTbl[] = {
-#if 0  
-	{ ARCID_GYM_FLY, NARC_gym_elec_gym04_cart_nsbmd, GFL_G3D_UTIL_RESARC }, //IMD　大砲本体
+static const GFL_G3D_UTIL_RES g3Dutil_resTbl[] = {  
+	{ ARCID_GYM_FLY, NARC_gym_fly_cannon_01_00_nsbmd, GFL_G3D_UTIL_RESARC }, //IMD　大砲本体
   
-  { ARCID_GYM_ELEC, NARC_gym_elec_gym04_rail_02a_nsbca, GFL_G3D_UTIL_RESARC }, //ICA　大砲アニメ1
-  { ARCID_GYM_ELEC, NARC_gym_elec_gym04_rail_02b_nsbca, GFL_G3D_UTIL_RESARC }, //ITP　大砲アニメ1
+  { ARCID_GYM_FLY, NARC_gym_fly_cannon_01_00_nsbca, GFL_G3D_UTIL_RESARC }, //ICA　大砲アニメ1
+  { ARCID_GYM_FLY, NARC_gym_fly_cannon_01_00_nsbma, GFL_G3D_UTIL_RESARC }, //IMA　大砲アニメ1
+#if 0  
   { ARCID_GYM_ELEC, NARC_gym_elec_gym04_rail_02a_nsbca, GFL_G3D_UTIL_RESARC }, //ICA　大砲アニメ2
   { ARCID_GYM_ELEC, NARC_gym_elec_gym04_rail_02b_nsbca, GFL_G3D_UTIL_RESARC }, //ITP　大砲アニメ2
   { ARCID_GYM_ELEC, NARC_gym_elec_gym04_rail_02a_nsbca, GFL_G3D_UTIL_RESARC }, //ICA　大砲アニメ3
@@ -191,14 +213,15 @@ static const GFL_G3D_UTIL_RES g3Dutil_resTbl[] = {
 //3Dアニメ　大砲
 static const GFL_G3D_UTIL_ANM g3Dutil_anmTbl[] = {
 	{ RES_ID_CAN_ANM1_ICA,0 }, //アニメリソースID, アニメデータID(リソース内部INDEX)
-  { RES_ID_CAN_ANM1_ITP,0 }, //アニメリソースID, アニメデータID(リソース内部INDEX)
+  { RES_ID_CAN_ANM1_IMA,0 }, //アニメリソースID, アニメデータID(リソース内部INDEX)
+#if 0
   { RES_ID_CAN_ANM2_ICA,0 }, //アニメリソースID, アニメデータID(リソース内部INDEX)
   { RES_ID_CAN_ANM2_ITP,0 }, //アニメリソースID, アニメデータID(リソース内部INDEX)
   { RES_ID_CAN_ANM3_ICA,0 }, //アニメリソースID, アニメデータID(リソース内部INDEX)
   { RES_ID_CAN_ANM3_ITP,0 }, //アニメリソースID, アニメデータID(リソース内部INDEX)
   { RES_ID_CAN_ANM4_ICA,0 }, //アニメリソースID, アニメデータID(リソース内部INDEX)
   { RES_ID_CAN_ANM4_ITP,0 }, //アニメリソースID, アニメデータID(リソース内部INDEX)
-
+#endif
 };
 
 //3Dオブジェクト設定テーブル
@@ -244,11 +267,11 @@ static const GFL_G3D_UTIL_SETUP Setup = {
 	NELEMS(g3Dutil_objTbl),		//オブジェクト数
 };
 
-
-
-
 static GMEVENT_RESULT ShotEvt( GMEVENT* event, int* seq, void* work );
 static u8 GetCanIdx(const int inX, const int inZ);
+
+//@todo
+BOOL test_GYM_FLY_Shot(GAMESYS_WORK *gsys, const u8 inShotIdx, const u8 inShotDir);
 
 //--------------------------------------------------------------
 /**
@@ -259,9 +282,32 @@ static u8 GetCanIdx(const int inX, const int inZ);
 //--------------------------------------------------------------
 void GYM_FLY_Setup(FIELDMAP_WORK *fieldWork)
 {
+  FLD_EXP_OBJ_CNT_PTR ptr = FIELDMAP_GetExpObjCntPtr( fieldWork );
+
   //汎用ワーク確保
   GMK_TMP_WK_AllocWork
       (fieldWork, GYM_FLY_TMP_ASSIGN_ID, FIELDMAP_GetHeapID(fieldWork), sizeof(GYM_FLY_TMP));
+
+  //必要なリソースの用意
+  FLD_EXP_OBJ_AddUnit(ptr, &Setup, GYM_FLY_UNIT_IDX );
+
+  //座標セット　大砲
+  {
+    VecFx32 pos = {CAN1_X, CAN1_Y, CAN1_Z};
+    GFL_G3D_OBJSTATUS *status = FLD_EXP_OBJ_GetUnitObjStatus(ptr, GYM_FLY_UNIT_IDX, OBJ_CAN_1);
+    status->trans = pos;
+  }
+
+  {
+    //1回再生設定
+    EXP_OBJ_ANM_CNT_PTR anm;
+    anm = FLD_EXP_OBJ_GetAnmCnt( ptr, GYM_FLY_UNIT_IDX, OBJ_CAN_1, 0);
+    FLD_EXP_OBJ_ChgAnmLoopFlg(anm, 0);
+    anm = FLD_EXP_OBJ_GetAnmCnt( ptr, GYM_FLY_UNIT_IDX, OBJ_CAN_1, 1);
+    FLD_EXP_OBJ_ChgAnmLoopFlg(anm, 0);
+  }
+
+  
 }
 
 //--------------------------------------------------------------
@@ -291,6 +337,14 @@ void GYM_FLY_Move(FIELDMAP_WORK *fieldWork)
 {
   int i;
   FLD_EXP_OBJ_CNT_PTR ptr = FIELDMAP_GetExpObjCntPtr( fieldWork );
+
+  //テスト
+  {
+    GAMESYS_WORK *gsys  = FIELDMAP_GetGameSysWork( fieldWork );
+    if ( GFL_UI_KEY_GetTrg() & PAD_BUTTON_L ){
+      test_GYM_FLY_Shot(gsys, 0, 0);
+    }
+  } 
 
   //アニメーション再生
   FLD_EXP_OBJ_PlayAnime( ptr );
@@ -330,7 +384,38 @@ GMEVENT *GYM_FLY_Shot(GAMESYS_WORK *gsys, const u8 inShotIdx, const u8 inShotDir
 
 //--------------------------------------------------------------
 /**
- * スイッチイベント
+ * 実験イベント起動
+ * @param	
+ * @return
+ */
+//--------------------------------------------------------------
+BOOL test_GYM_FLY_Shot(GAMESYS_WORK *gsys, const u8 inShotIdx, const u8 inShotDir)
+{
+  GYM_FLY_SV_WORK *gmk_sv_work;
+  FIELDMAP_WORK *fieldWork = GAMESYSTEM_GetFieldMapWork(gsys);
+  FLD_EXP_OBJ_CNT_PTR ptr = FIELDMAP_GetExpObjCntPtr( fieldWork );
+  GYM_FLY_TMP *tmp = GMK_TMP_WK_GetWork(fieldWork, GYM_FLY_TMP_ASSIGN_ID);
+  GAMEDATA *gamedata = GAMESYSTEM_GetGameData( FIELDMAP_GetGameSysWork( fieldWork ) );
+  GIMMICKWORK *gmkwork = SaveData_GetGimmickWork( GAMEDATA_GetSaveControlWork( gamedata ) );
+
+  gmk_sv_work = GIMMICKWORK_Get( gmkwork, FLD_GIMMICK_GYM_FLY );
+
+  {
+    //イベントセット
+    GMEVENT * event = GMEVENT_Create( gsys, NULL, ShotEvt, 0/*sizeof(ENCEFF_WORK)*/ );
+    GAMESYSTEM_SetEvent(gsys, event);
+    //打ち出しインデックスをセット
+    tmp->ShotIdx = inShotIdx;
+    //打ち出し方向をセット
+    tmp->ShotDir = inShotDir;
+  }
+  return TRUE;
+  
+}
+
+//--------------------------------------------------------------
+/**
+ * 発射イベント
  * @param	
  * @return
  */
@@ -351,54 +436,154 @@ static GMEVENT_RESULT ShotEvt( GMEVENT* event, int* seq, void* work )
 
   switch(*seq){
   case 0:
+    {
+      u16 arc_idx;
+      arc_idx = 3;
+      tmp->IcaAnmPtr = ICA_ANIME_Create( 
+          GFL_HEAP_LOWID(HEAPID_FIELDMAP), ARCID_GYM_FLY, arc_idx, tmp->FramePosDat, FRAME_POS_SIZE
+          );
+      GF_ASSERT(tmp->IcaAnmPtr != NULL);
+      tmp->MaxIcaAnmFrm = ICA_ANIME_GetMaxFrame(tmp->IcaAnmPtr);
+    }
+
     //移動データロード
     ;
     //自機の向きにより分岐
     //大砲ドアオープンフレームまで再生開始
     ;
+    {
+      ///EXP_OBJ_ANM_CNT_PTR anm;
+      u8 obj_idx;
+      obj_idx = tmp->ShotIdx;
+      //アニメを開始
+      FLD_EXP_OBJ_ValidCntAnm(ptr, GYM_FLY_UNIT_IDX, obj_idx, 0, TRUE);
+      FLD_EXP_OBJ_ValidCntAnm(ptr, GYM_FLY_UNIT_IDX, obj_idx, 1, TRUE);
+      //頭出し
+      FLD_EXP_OBJ_SetObjAnmFrm( ptr, GYM_FLY_UNIT_IDX, obj_idx, 0, 0 );
+      FLD_EXP_OBJ_SetObjAnmFrm( ptr, GYM_FLY_UNIT_IDX, obj_idx, 1, 0 );  
+    }
     (*seq)++;
     break;
   case 1:
-    //オープンフレーム到達監視
-    if (1){
-      //自機、大砲に入る
-      (*seq)++;
+    {
+      u8 obj_idx;
+      fx32 frm;
+      obj_idx = tmp->ShotIdx;
+      frm = FLD_EXP_OBJ_GetAnimeFrame(ptr, GYM_FLY_UNIT_IDX, obj_idx, 0);
+      //オープンフレーム到達監視
+      if (frm >= 15*FX32_ONE){
+        //大砲アニメ停止
+        {
+          EXP_OBJ_ANM_CNT_PTR anm;
+          anm = FLD_EXP_OBJ_GetAnmCnt( ptr, GYM_FLY_UNIT_IDX, obj_idx, 0);
+          FLD_EXP_OBJ_ChgAnmStopFlg(anm, 1);
+          anm = FLD_EXP_OBJ_GetAnmCnt( ptr, GYM_FLY_UNIT_IDX, obj_idx, 1);
+          FLD_EXP_OBJ_ChgAnmStopFlg(anm, 1);
+        }
+        //自機、大砲に入る
+        (*seq)++;
+      }
     }
     break;
   case 2:
     //自機アニメ終了待ち
     if(1){
-      //大砲アニメ際開始
+      //自機を非表示
+      {
+        MMDL * mmdl;
+        FIELD_PLAYER *fld_player;
+        fld_player = FIELDMAP_GetFieldPlayer( fieldWork );
+        mmdl = FIELD_PLAYER_GetMMdl( fld_player );
+        MMDL_SetStatusBitVanish(mmdl, TRUE);
+      }
+      //大砲アニメ再開
+      {
+        u8 obj_idx;
+        EXP_OBJ_ANM_CNT_PTR anm;
+        obj_idx = tmp->ShotIdx;
+        anm = FLD_EXP_OBJ_GetAnmCnt( ptr, GYM_FLY_UNIT_IDX, obj_idx, 0);
+        FLD_EXP_OBJ_ChgAnmStopFlg(anm, 0);
+        anm = FLD_EXP_OBJ_GetAnmCnt( ptr, GYM_FLY_UNIT_IDX, obj_idx, 1);
+        FLD_EXP_OBJ_ChgAnmStopFlg(anm, 0);
+      }
       (*seq)++;
     }
     break;
   case 3:
-    //発射フレーム到達チェック
-    if (1){
-      //フレーム読み取り開始
-      tmp->FrameSetStart = 1;
-      (*seq)++;
+    {
+      u8 obj_idx;
+      fx32 frm;
+      obj_idx = tmp->ShotIdx;
+      frm = FLD_EXP_OBJ_GetAnimeFrame(ptr, GYM_FLY_UNIT_IDX, obj_idx, 0);
+      //発射フレーム到達チェック
+      if (frm >= 30*FX32_ONE){
+        //フレーム読み取り開始
+        tmp->FrameSetStart = 1;
+        tmp->NowIcaAnmFrmIdx = 30;    //@todo
+        (*seq)++;
+      }
     }
     break;
   case 4:
     //大砲アニメ終了チェック＆フレーム読み取り終了チェック
-    if (1){
-      //フレーム読み取り終了
-      tmp->FrameSetStart = 0;
+    if (!tmp->FrameSetStart){
       //主人公最終位置セット
-      ;
+      {
+        FIELD_PLAYER *fld_player;
+        fld_player = FIELDMAP_GetFieldPlayer( fieldWork );
+        //主人公のカプセル出る座標セット
+        {
+          VecFx32 pos = {0,0,0};
+          pos.x = STAND1_GX * FIELD_CONST_GRID_FX32_SIZE; //@todo
+          pos.x += GRID_HALF_SIZE;
+          pos.y = STAND1_GY * FIELD_CONST_GRID_FX32_SIZE; //@todo
+          pos.z = STAND1_GZ * FIELD_CONST_GRID_FX32_SIZE; //@todo
+          pos.z += GRID_HALF_SIZE;
+          FIELD_PLAYER_SetPos( fld_player, &pos );
+        }        
+      }      
       (*seq)++;
     }
     break;
   case 5:
     //フレーム読み取り解放
-    ;
+    ICA_ANIME_Delete( tmp->IcaAnmPtr );
     return GMEVENT_RES_FINISH;
   }
 
   //フレーム読み取り処理部分
   if (tmp->FrameSetStart){      //読み取りフラグが立っていれば、実行
-    ;
+    u8 obj_idx;
+    u8 anm_idx;
+    VecFx32 dst_vec = {0,0,0};
+
+    obj_idx = tmp->ShotIdx;
+
+    //@todo
+    if (tmp->NowIcaAnmFrmIdx == 70){
+      //自機表示
+      MMDL * mmdl;
+      FIELD_PLAYER *fld_player;
+      fld_player = FIELDMAP_GetFieldPlayer( fieldWork );
+      mmdl = FIELD_PLAYER_GetMMdl( fld_player );
+      MMDL_SetStatusBitVanish(mmdl, FALSE);
+    }
+
+    //座標取得
+    if ( ICA_ANIME_GetTranslateAt( tmp->IcaAnmPtr, &dst_vec, tmp->NowIcaAnmFrmIdx ) ){
+      dst_vec.x += (CanPos[obj_idx][0]*FIELD_CONST_GRID_FX32_SIZE + GRID_HALF_SIZE);
+      dst_vec.y += (CanPos[obj_idx][1]*FIELD_CONST_GRID_FX32_SIZE);
+      dst_vec.z += (CanPos[obj_idx][2]*FIELD_CONST_GRID_FX32_SIZE + GRID_HALF_SIZE);
+      //自機座標セット
+      FIELD_PLAYER_SetPos( FIELDMAP_GetFieldPlayer( fieldWork ), &dst_vec );
+    }
+
+    //アニメフレーム更新
+    if ( tmp->NowIcaAnmFrmIdx < tmp->MaxIcaAnmFrm ){
+      tmp->NowIcaAnmFrmIdx++;
+    }else{
+      tmp->FrameSetStart = 0;
+    }
   }
 
   return GMEVENT_RES_CONTINUE;
@@ -414,6 +599,12 @@ static u8 GetCanIdx(const int inX, const int inZ)
     }
   }
   return i;
+}
+
+//大砲アニメ中の自機の座標更新
+static void PlayerMove()
+{
+  ;
 }
 
 
