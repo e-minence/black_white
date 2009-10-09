40d8a9a75aefb0122adff8c6de79e2bdb92bb22d
Keita Kagaya <kagaya@gamefreak.co.jp>
2009-10-09 09:00:46 +0000

    trainer.bat trainer.ev生成バッチファイル

---
+++ resource/fldmapdata/script/trainer.bat
@@ -0,0 +1,3 @@
+echo create trainer.ev
+@ruby scr_trainer/trainer.rb ../../../prog/include/tr_tool/trno_def.h scr_trainer/trainer_base.ev
+@ruby scr_tools/ev_def.rb trainer.ev scrid_offset/scr_offset_id.h
