6813738424f06fe0320724a38e3e4fa6ac82bad8
Naoto Murakami <murakami_naoto@gamefreak.co.jp>
2009-10-12 10:12:20 +0000

    スクリプト作業_シッポウシティ博物館（c02gym0101）を作成しました

--- resource/fldmapdata/eventdata/total_header.h
+++ resource/fldmapdata/eventdata/total_header.h
@@ -257,6 +257,12 @@
 #ifndef ZONE_C02GYM0101_EVC_H
 #define ZONE_C02GYM0101_EVC_H
 
+#define C02GYM0101_COUNTER1_01             0
+#define C02GYM0101_COUNTER1_02             1
+#define C02GYM0101_AD_01                   2
+#define C02GYM0101_OLDMAN_01               3
+#define C02GYM0101_MAN1_01                 4
+#define C02GYM0101_GIRL3_01                5
 
 #endif //ZONE_C02GYM0101_EVC_H
 //Ç±ÇÃÉtÉ@ÉCÉãÇÕdata/c02pc0101.mevÇ©ÇÁé©ìÆê∂ê¨Ç≥ÇÍÇ‹ÇµÇΩ
