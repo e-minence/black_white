6fe31cbbf5d26138cdcdb7fa7891abd0747d2d9c
Genya Hosaka <hosaka@gamefreak.co.jp>
2009-10-13 03:38:05 +0000

    //	コンバータから吐き出された定数を受け渡すようにした 2009.10.10 by genya hosaka

--- prog/include/savedata/myitem_savedata_def.h
+++ prog/include/savedata/myitem_savedata_def.h
@@ -12,6 +12,8 @@
 //============================================================================
 #pragma once
 
+#include "item\itempocket_def.h"
+
 //=============================================================================
 /**
  *								定数定義
@@ -19,13 +21,14 @@
 //=============================================================================
 //--------------------------------------------------------------
 //	この定義はitemconv_dp の datamake.cppと同じでなければいけない 2009.07.21
+//	コンバータから吐き出された定数を受け渡すようにした 2009.10.10 by genya hosaka
 //--------------------------------------------------------------
 // ポケットID
-#define	BAG_POKE_NORMAL		( 0 )		// 道具
-#define	BAG_POKE_DRUG		( 1 )		// 薬
-#define	BAG_POKE_WAZA		( 2 )		// 技マシン
-#define	BAG_POKE_NUTS		( 3 )		// 木の実
-#define	BAG_POKE_EVENT		( 4 )		// 大切な物
+#define	BAG_POKE_NORMAL		( ITEMPOCKET_NORMAL )		// 道具
+#define	BAG_POKE_DRUG		( ITEMPOCKET_DRUG )		// 薬
+#define	BAG_POKE_WAZA		( ITEMPOCKET_WAZA )		// 技マシン
+#define	BAG_POKE_NUTS		( ITEMPOCKET_NUTS )		// 木の実
+#define	BAG_POKE_EVENT		( ITEMPOCKET_EVENT )		// 大切な物
 
 #define	BAG_POKE_MAX		( 5 )		// ポケット最大数
 
