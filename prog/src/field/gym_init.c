5b15a55d8a55184ceb9b4f105cf9194bed318c0b
Nozomu Saito <saito_nozomu@gamefreak.co.jp>
2009-10-13 04:40:04 +0000

    //ノーマルジム初期化処理追加

--- prog/src/field/gym_init.c
+++ prog/src/field/gym_init.c
@@ -12,6 +12,7 @@
 #include "gym_elec_sv.h"
 #include "gym_normal_sv.h"
 #include "gym_anti_sv.h"
+#include "field/field_const.h"  //for FIELD_CONST_GRID_FX32_SIZE
 
 #include "../../../resource/fldmapdata/gimmick/gym_normal/gym_normal_exit_def.cdat"
 
@@ -52,23 +53,36 @@ void GYM_INIT_Normal(GAMESYS_WORK *gsys)
 {
   u8 i;
   GYM_NORMAL_SV_WORK *gmk_sv_work;
+  PLAYER_WORK *player_work;
 
   {
     GAMEDATA *gamedata = GAMESYSTEM_GetGameData( gsys );
     GIMMICKWORK *gmkwork = SaveData_GetGimmickWork( GAMEDATA_GetSaveControlWork( gamedata ) );
     gmk_sv_work = GIMMICKWORK_Get( gmkwork, FLD_GIMMICK_GYM_NORM );
+    player_work = GAMEDATA_GetMyPlayerWork(gamedata);
   }
 
-  //PLAYERWORKから、初回位置座標を取得し、ギミック稼動後かどうかを判定する
+  //PLAYER_WORKから、初回位置座標を取得し、ギミック稼動後かどうかを判定する
+  {
+    int gx,gz;
+    gx = player_work->position.x / FIELD_CONST_GRID_FX32_SIZE;
+    gz = player_work->position.z / FIELD_CONST_GRID_FX32_SIZE;
 
-  if (0){
-    gmk_sv_work->GmkUnrock = TRUE;
-  }else{
-    gmk_sv_work->GmkUnrock = FALSE;
+    OS_Printf("NormalGymInit:: pos gx gz  = %d,%d\n",gx,gz);
+  
+    if ((gx == HIDEN_EXIT_GX)&&(gz == HIDEN_EXIT_GZ)){
+      gmk_sv_work->GmkUnrock = TRUE;
+    }else{
+      gmk_sv_work->GmkUnrock = FALSE;
+    }
   }
 
   for(i=0;i<NRM_WALL_NUM_MAX;i++){
-    gmk_sv_work->Wall[i] = 0;
+    if (gmk_sv_work->GmkUnrock){
+      gmk_sv_work->Wall[i] = 1;
+    }else{
+      gmk_sv_work->Wall[i] = 0;
+    }
   }
 }
 
