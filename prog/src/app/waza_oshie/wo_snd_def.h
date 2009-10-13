6865870ddca50d28f24b650866ae4e2992d57a94
Akito Mori <mori@gamefreak.co.jp>
2009-10-13 08:27:35 +0000

    技教え画面（技思い出し画面）追加

---
+++ prog/src/app/waza_oshie/wo_snd_def.h
@@ -0,0 +1,14 @@
+//============================================================================================
+/**
+ * @file	wo_snd_def.h
+ * @brief	技教え画面　SE定義
+ * @author	Hiroyuki Nakamura
+ * @date	2006.05.12
+ */
+//============================================================================================
+#include "sound/pm_sndsys.h"
+
+#define	WO_SE_PAGE_MOVE	( SEQ_SE_DECIDE1 )	// ページ切り替え
+#define	WO_SE_LIST_MOVE	( SEQ_SE_DECIDE1 )	// 技リスト選択
+#define	WO_SE_DECIDE	  ( SEQ_SE_DECIDE1 )	// 決定
+#define	WO_SE_CANCEL	  ( SEQ_SE_DECIDE1 )	// キャンセル
