b373d0e4e0613bb75c89458911c15a46e3abc4e8
Miyuki Iwasawa <iwasawa@gamefreak.co.jp>
2009-10-14 02:45:23 +0000

    岩澤用プロンプト設定変更

--- tools/personal/iwasawa_prompt.bat
+++ tools/personal/iwasawa_prompt.bat
@@ -12,6 +12,9 @@ REM ツールパス系
 doskey vi=c:\tools\vi\gvim.exe $*
 doskey golib=cd c:\home\gflib\gflib\src\$*
 
+doskey chp=cd %PROJECT_ROOT:/=\%prog\$*
 doskey chs=cd %PROJECT_ROOT:/=\%prog\src\$*
+doskey chi=cd %PROJECT_ROOT:/=\%prog\include\$*
 doskey che=cd %PROJECT_ROOT:/=\%prog\src\fielddata\$*
-doskey chr=cd %PROJECT_ROOT:/=\%resource\fldmapdata\$*
+doskey chr=cd %PROJECT_ROOT:/=\%resource\$*
+doskey chf=cd %PROJECT_ROOT:/=\%resource\fldmapdata\$*
