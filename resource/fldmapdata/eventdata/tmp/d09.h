8a64da06b72d26e6936bb328798b127fbef61785
Toshihiro Obata <obata_toshihiro@gamefreak.co.jp>
2009-10-14 11:15:10 +0000

    ゾーンＩＤ追加時のコミット忘れを追加

---
+++ resource/fldmapdata/eventdata/tmp/d09.h
@@ -0,0 +1,12 @@
+//このファイルは../data/d09.mevから自動生成されました
+#ifndef ZONE_D09_EVD_H
+#define ZONE_D09_EVD_H
+
+
+#endif //ZONE_D09_EVD_H
+//このファイルは../data/d09.mevから自動生成されました
+#ifndef ZONE_D09_EVC_H
+#define ZONE_D09_EVC_H
+
+
+#endif //ZONE_D09_EVC_H
