571665b97cfd86bd606b43e28832b402871258ab
Nozomu Saito <saito_nozomu@gamefreak.co.jp>
2009-10-15 03:10:38 +0000

    //公開関数変更

--- prog/src/field/gym_fly.h
+++ prog/src/field/gym_fly.h
@@ -12,6 +12,6 @@ extern void GYM_FLY_Setup(FIELDMAP_WORK *fieldWork);
 extern void GYM_FLY_End(FIELDMAP_WORK *fieldWork);
 extern void GYM_FLY_Move(FIELDMAP_WORK *fieldWork);
 
-extern GMEVENT *GYM_FLY_Shot(GAMESYS_WORK *gsys, const u8 inShotIdx, const u8 inShotDir);
+extern GMEVENT *GYM_FLY_CreateShotEvt(GAMESYS_WORK *gsys);
 
 #endif //__GYM_FLY_H__
