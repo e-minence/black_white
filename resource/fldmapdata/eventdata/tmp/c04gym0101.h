20fafd27ad80fd01b720fe791514fda53099b5b9
Masafumi Saito <saito_masafumi@gamefreak.co.jp>
2009-10-15 10:20:17 +0000

    イベント作成：C04ジムの基本動作完了

--- resource/fldmapdata/eventdata/tmp/c04gym0101.h
+++ resource/fldmapdata/eventdata/tmp/c04gym0101.h
@@ -8,7 +8,11 @@
 #ifndef ZONE_C04GYM0101_EVC_H
 #define ZONE_C04GYM0101_EVC_H
 
-#define C04GYM0101_BOY1_01                 0
-#define C04GYM0101_BOY1_02                 1
+#define C04GYM0101_TRAINER_02              0
+#define C04GYM0101_GUIDE_01                1
+#define TR_PRINCESS_01                     2
+#define TR_PRINCE_01                       3
+#define C04GYM0101_TRAINER_01              4
+#define C04GYM0101_LEADER4_01              5
 
 #endif //ZONE_C04GYM0101_EVC_H
