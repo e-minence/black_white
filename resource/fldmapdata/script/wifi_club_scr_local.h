b11c4c8a230975cdaca83f7e32e78179ea2d2cd4
Miyuki Iwasawa <iwasawa@gamefreak.co.jp>
2009-10-20 10:43:51 +0000

    スクリプト　Wi-Fiクラブ受付更新

--- resource/fldmapdata/script/wifi_club_scr_local.h
+++ resource/fldmapdata/script/wifi_club_scr_local.h
@@ -4,11 +4,10 @@
 //    スクリプトファイル：wifi_club_scr用ローカル定数定義
 //
 //======================================================================
-#define SEQ_CGEAR_CHECK       (0)
-#define SEQ_DAMETAMA_CHECK    (1)
-#define SEQ_MENU              (2)
-#define SEQ_CLUB_EXP          (3)
-#define SEQ_CONNECT           (4)
-#define SEQ_CANCEL            (10)
+#define SEQ_MENU              (0)
+#define SEQ_CLUB_EXP          (1)
+#define SEQ_CHECK_CONNECT     (2)
+#define SEQ_CONNECT           (3)
+#define SEQ_GOODBYE           (10)
 #define SEQ_END               (0xFF)
 
