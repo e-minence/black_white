793420c15147fbc197d9fd3cd3599a9b73c4e615
Shinya Takahashi <takahashi@gamefreak.co.jp>
2009-10-20 02:07:52 +0000

    アイテム管理表更新

--- resource/fldmapdata/script/fld_item_def.h
+++ resource/fldmapdata/script/fld_item_def.h
@@ -6,6 +6,9 @@
 
 //スクリプトデータID定義
 #define SCRID_FLD_ITEM_0 (7000)
-#define SCRID_FLD_ITEM2 (7001)
+#define SCRID_FLD_ITEM_1 (7001)
+#define SCRID_FLD_ITEM_2 (7002)
+#define SCRID_FLD_ITEM_3 (7003)
+#define SCRID_FLD_ITEM2 (7004)
 
 #endif //_FLD_ITEM_DEF_H_
