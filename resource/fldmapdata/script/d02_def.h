08d6a59b01c8a30ed8a8b9445dc1d7424227d31d
Katsunori Suginaka <suginaka_katsunori@gamefreak.co.jp>
2009-10-21 02:08:04 +0000

    d02スクリプト追加致しました。

--- resource/fldmapdata/script/d02_def.h
+++ resource/fldmapdata/script/d02_def.h
@@ -5,6 +5,11 @@
 #define _D02_DEF_H_
 
 //スクリプトデータID定義
-#define SCRID_D02_TALKOBJ_GIRL1 (1)
+#define SCRID_D02_GIRL2_01 (1)
+#define SCRID_D02_HAKAIM1_01 (2)
+#define SCRID_D02_HAKAIM2_01 (3)
+#define SCRID_D02_HAKAIW_01 (4)
+#define SCRID_D02_LEADER3_01 (5)
+#define SCRID_POS_D02_STOP (6)
 
 #endif //_D02_DEF_H_
