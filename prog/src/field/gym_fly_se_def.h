20c8740cd7138a1932c2504facad9a7bb16c5a6e
Nozomu Saito <saito_nozomu@gamefreak.co.jp>
2009-10-22 07:14:35 +0000

    //飛行ジムＳＥ定義追加

---
+++ prog/src/field/gym_fly_se_def.h
@@ -0,0 +1,15 @@
+//======================================================================
+/**
+ * @file  gym_fly_se_def.h
+ * @author  GAMEFREAK inc.
+ *
+ * SE定義
+ */
+//======================================================================
+#pragma once
+
+#define GYM_FLY_SE_OPEN  (SEQ_SE_FLD_39)  //大砲ドア開く
+#define GYM_FLY_SE_CLOSE  (SEQ_SE_FLD_40)    //大砲ドア閉まる
+#define GYM_FLY_SE_FIRE  (SEQ_SE_FLD_37)     //大砲撃つ
+#define GYM_FLY_SE_STAND  (SEQ_SE_FLD_38)     //自機着地
+
