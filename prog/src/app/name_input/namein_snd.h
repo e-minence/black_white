631a5876399c2aac387743fa46c1f2df27f7156e
Toru Nagihashi <nagihashi@gamefreak.co.jp>
2009-10-22 06:01:03 +0000

    削除音と決定音が逆だったのを修正

--- prog/src/app/name_input/namein_snd.h
+++ prog/src/app/name_input/namein_snd.h
@@ -19,6 +19,6 @@
 //=============================================================================
 #define NAMEIN_SE_MOVE_CURSOR	(SEQ_SE_SELECT1)
 #define NAMEIN_SE_DECIDE_STR	(SEQ_SE_DECIDE1)
-#define NAMEIN_SE_DELETE_STR	(SEQ_SE_DECIDE2)
+#define NAMEIN_SE_DELETE_STR	(SEQ_SE_CANCEL3)
 #define NAMEIN_SE_CHANGE_MODE	(SEQ_SE_SYS_12)
-#define NAMEIN_SE_DECIDE			(SEQ_SE_CANCEL3)
+#define NAMEIN_SE_DECIDE			(SEQ_SE_DECIDE2)
