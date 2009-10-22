decf03a8d957937edc7edd46cf54f17dbcb11c30
Katsumi Ohno <ohno@gamefreak.co.jp>
2009-10-22 06:49:23 +0000

    古いSEを外した 仮のSEに変更

--- prog/src/app/codein/codein_snd.h
+++ prog/src/app/codein/codein_snd.h
@@ -3,13 +3,13 @@
 #define __CODEIN_SND_H__
 
 #include "sound/pm_sndsys.h"
-
-#define CI_SE_BLOCK		( SEQ_SE_BUTTON3 )
-#define CI_SE_TOUCH		( SEQ_SE_BUTTON3 )
-#define CI_SE_BACK		( SEQ_SE_BUTTON3 )
-#define CI_SE_END		( SEQ_SE_PIRORIRO )
+//âºÇÃSEÇ…ïœçX
+#define CI_SE_BLOCK		( SEQ_SE_SELECT1 ) 
+#define CI_SE_TOUCH		( SEQ_SE_SELECT1 )
+#define CI_SE_BACK		( SEQ_SE_SELECT1 )
+#define CI_SE_END		( SEQ_SE_SELECT1 )
 //#define CI_SE_MOVE		( SEQ_SE_DP_SELECT )
-#define CI_SE_MOVE		( SEQ_SE_SELECT78 )
+#define CI_SE_MOVE		( SEQ_SE_SELECT1 )
 
 
 #endif
