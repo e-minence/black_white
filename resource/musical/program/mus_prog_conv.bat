5a9dc081ab28d0e891dfa6771a849108b82691b6
Kazumasa Iwao <iwao_kazumasa@gamefreak.co.jp>
2009-10-23 10:28:15 +0000

    環境変数の設定を修正(有泉)

--- resource/musical/program/mus_prog_conv.bat
+++ resource/musical/program/mus_prog_conv.bat
@@ -1,9 +1,12 @@
 SET PROJECT_ROOT=../../..
 SET WBLIBDIR=%PROJECT_ROOT%/lib
-SET NITROSYSTEM_ROOT=%WBLIBDIR%/NitroSystem
+SET NITROSDK_ROOT=%WBLIBDIR%\TwlSDK
+SET NITROSYSTEM_ROOT=%WBLIBDIR%\TwlSystem
+SET NITROWIFI_ROOT=%WBLIBDIR%\TwlDWC
+SET NITRODWC_ROOT=%WBLIBDIR%\TwlDWC
 SET CWFOLDER=C:/Program Files/Freescale/CW for NINTENDO DSi V1.1
 
-PATH=%NITROSYSTEM_ROOT%\tools\win\bin;%PATH%
+PATH=c:\tools;%PROJECT_ROOT%;%PROJECT_ROOT%\tools;c:\tools\subversion\bin;c:\cygwin\bin;c:\tools\vim;C:\tools;%NITROSDK_ROOT%\tools\bin;%NITROSYSTEM_ROOT%\tools\bin;%PATH%;
 
 ruby program_make.rb
 
