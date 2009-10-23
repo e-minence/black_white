abeb2d77e29cc36285cf3012f65be6d5f3ae788e
Masafumi Saito <saito_masafumi@gamefreak.co.jp>
2009-10-23 09:04:23 +0000

    イベント接続：ミュージカル受付フロアを接続

--- resource/fldmapdata/eventdata/tmp/c04r0201.h
+++ resource/fldmapdata/eventdata/tmp/c04r0201.h
@@ -2,11 +2,15 @@
 #ifndef ZONE_C04R0201_EVD_H
 #define ZONE_C04R0201_EVD_H
 
+#define DOOR_ID_C04R0201_C04_EXIT01       0
+#define DOOR_ID_C04R0201_C04_EXIT03       1
+#define DOOR_ID_C04R0201_C04_EXIT02       2
 
 #endif //ZONE_C04R0201_EVD_H
 //Ç±ÇÃÉtÉ@ÉCÉãÇÕ../data/c04r0201.mevÇ©ÇÁé©ìÆê∂ê¨Ç≥ÇÍÇ‹ÇµÇΩ
 #ifndef ZONE_C04R0201_EVC_H
 #define ZONE_C04R0201_EVC_H
 
+#define MUSICAL_RECEPT_SINGLE              0
 
 #endif //ZONE_C04R0201_EVC_H
