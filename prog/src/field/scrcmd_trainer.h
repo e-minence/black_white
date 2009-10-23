0544680f47c6d85ce66d8bce5a8109e2b70e3d07
Sosuke Tamada <tamada@gamefreak.co.jp>
2009-10-23 13:03:51 +0000

    勝ち負け処理コマンドを常駐領域に移動した

--- prog/src/field/scrcmd_trainer.h
+++ prog/src/field/scrcmd_trainer.h
@@ -29,15 +29,18 @@ extern VMCMD_RESULT EvCmdTrainerTalkTypeGet(VMHANDLE *core,void *wk);
 extern VMCMD_RESULT EvCmdRevengeTrainerTalkTypeGet(VMHANDLE *core,void *wk);
 extern VMCMD_RESULT EvCmdTrainerTypeGet(VMHANDLE *core,void *wk);
 extern VMCMD_RESULT EvCmdTrainerBgmSet(VMHANDLE *core,void *wk);
+extern VMCMD_RESULT EvCmdTrainerWin(VMHANDLE *core,void *wk);
 extern VMCMD_RESULT EvCmdTrainerLose(VMHANDLE *core,void *wk);
 extern VMCMD_RESULT EvCmdTrainerLoseCheck(VMHANDLE *core,void *wk);
-extern VMCMD_RESULT EvCmdSeacretPokeRetryCheck(VMHANDLE *core,void *wk);
-extern VMCMD_RESULT EvCmdHaifuPokeRetryCheck(VMHANDLE *core,void *wk);
 extern VMCMD_RESULT EvCmd2vs2BattleCheck(VMHANDLE *core,void *wk);
 extern VMCMD_RESULT EvCmdDebugBattleSet(VMHANDLE *core,void *wk);
 extern VMCMD_RESULT EvCmdDebugTrainerFlagSet(VMHANDLE *core,void *wk);
 extern VMCMD_RESULT EvCmdDebugTrainerFlagOnJump(VMHANDLE *core,void *wk);
 extern VMCMD_RESULT EvCmdBattleResultGet(VMHANDLE *core,void *wk);
+
+extern VMCMD_RESULT EvCmdSeacretPokeRetryCheck(VMHANDLE *core,void *wk);
+extern VMCMD_RESULT EvCmdHaifuPokeRetryCheck(VMHANDLE *core,void *wk);
+extern VMCMD_RESULT EvCmdWildLoseCheck( VMHANDLE *core, void *wk );
 #endif	/* __ASM_NO_DEF_ */
 
 #endif	/* SCR_TRAINER_H */
