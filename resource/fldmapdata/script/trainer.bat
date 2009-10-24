416146ccf906e6860128ccf9422dd23a3c592445
Sosuke Tamada <tamada@gamefreak.co.jp>
2009-10-24 06:35:43 +0000

    トレーナースクリプト生成でウィンドウズ行末コードになるように修正

--- resource/fldmapdata/script/trainer.bat
+++ resource/fldmapdata/script/trainer.bat
@@ -1,3 +1,5 @@
 echo create trainer.ev
 @ruby scr_trainer/trainer.rb ../../../prog/include/tr_tool/trno_def.h scr_trainer/trainer_base.ev
 @ruby scr_tools/ev_def.rb trainer.ev scrid_offset/scr_offset_id.h
+@REM 行末コードをUNIXからWINDOWSに変更
+@unix2dos -D trainer.ev
