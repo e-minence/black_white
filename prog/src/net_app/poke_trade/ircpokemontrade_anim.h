c75656585e9d92459650edbed5924cf0390f2825
Mana Ibe <ibe_mana@gamefreak.co.jp>
2009-10-24 05:04:06 +0000

    数値変更

--- prog/src/net_app/poke_trade/ircpokemontrade_anim.h
+++ prog/src/net_app/poke_trade/ircpokemontrade_anim.h
@@ -31,7 +31,7 @@
 #define _POKESPLASH_WHITEOUT_TIMER (29)       //絵コンテ２０番ボールおちシーンのホワイトアウト時間
 
 #define _POKESPLASH_WHITEIN_START  (1390)  //絵コンテ２１番もう一回ボール落ちのフェードイン
-#define _POKESPLASH_WHITEIN_TIMER (60)        //絵コンテ２１番もう一回ボール落ちのフェードイン時間
+#define _POKESPLASH_WHITEIN_TIMER (30)        //絵コンテ２１番もう一回ボール落ちのフェードイン時間
 
 #define _DEMO_END (1740)   //デモが終わる最後のフレーム
 
@@ -40,14 +40,14 @@
 #define _POKEMON_DELETE_TIME   (5)     // 絵コンテ３番  ポケモンを消す
 
 #define _POKE_APPEAR_START   (470)   //絵コンテ８番  ポケモン登場 自分のは裏
-#define _POKE_APPEAR_TIME    (23)       //絵コンテ８番  ポケモン登場移動時間
+#define _POKE_APPEAR_TIME    (20)       //絵コンテ８番  ポケモン登場移動時間
 #define _POKE_APPEAR_START2   (485)   //絵コンテ９番     さらにゆっくり移動
 #define _POKE_APPEAR_TIME2   (40)       //絵コンテ９番  さらにゆっくり移動
-#define _POKE_SIDEOUT_START   (530)  //絵コンテ１０番  ポケモン左右に移動していく
-#define _POKE_SIDEOUT_TIME    (90)      //絵コンテ１０番  ポケモン登場移動時間
+#define _POKE_SIDEOUT_START   (525)  //絵コンテ１０番  ポケモン左右に移動していく
+#define _POKE_SIDEOUT_TIME    (65)      //絵コンテ１０番  ポケモン登場移動時間
 #define _POKE_SIDEIN_START   (620)   //絵コンテ１１番  ポケモン左右に移動して戻ってくる
-#define _POKE_SIDEIN_TIME    (240)     //絵コンテ１１番 ポケモン登場移動時間
-#define _POKE_LEAVE_START   (915)  //絵コンテ１３番  ポケモン退場
+#define _POKE_SIDEIN_TIME    (200)     //絵コンテ１１番 ポケモン登場移動時間
+#define _POKE_LEAVE_START   (910)  //絵コンテ１３番  ポケモン退場
 #define _POKE_LEAVE_TIME    (10)      //絵コンテ１３番  ポケモン退場移動時間
 
 #define _POKEMON_CREATE_TIME   (1665)  ///絵コンテ２４番 交換終了してポケモン登場 
@@ -57,9 +57,9 @@
 #define _BALL_PARTICLE_START   (1220)  ///モンスターボール激突 パーティクル開始
 #define _PARTICLE_DEMO2_START   (145)  ///demo_tex002パーティクル開始
 #define _PARTICLE_DEMO3_START   (220)  ///demo_tex003パーティクル開始
-#define _PARTICLE_DEMO3_START2  (1240)  ///パーティクル開始
+#define _PARTICLE_DEMO3_START2  (1220)  ///パーティクル開始
 #define _PARTICLE_DEMO4_START   (20)     ///ポケモン＞ボールエフェクト
-#define _PARTICLE_DEMO4_START2   (1660)  ///ボール＞ポケモンエフェクト
+#define _PARTICLE_DEMO4_START2   (1650)  ///ボール＞ポケモンエフェクト
 #define _PARTICLE_DEMO5_START   (220)  ///モンスターボールが打ちあがるパーティクル開始
 #define _PARTICLE_DEMO6_START   (490)  ///パーティクル開始
 #define _PARTICLE_DEMO7_START   (1000)  ///パーティクル開始
@@ -78,19 +78,19 @@
 #define _POKEMON_PLAYER_CENTER_POSZ _MCSS_POS_Z( 10)   // 自分のポケモンが真ん中に移動する際のZ
 
 //絵コンテ８番の最初の位置
-#define _POKEMON_PLAYER_APPEAR_POSX _MCSS_POS_X( -60)  // 自分のポケモンのX
-#define _POKEMON_PLAYER_APPEAR_POSY _MCSS_POS_Y(-100)  // 自分のポケモンのY
+#define _POKEMON_PLAYER_APPEAR_POSX _MCSS_POS_X( -50)  // 自分のポケモンのX
+#define _POKEMON_PLAYER_APPEAR_POSY _MCSS_POS_Y(-150)  // 自分のポケモンのY
 #define _POKEMON_PLAYER_APPEAR_POSZ _MCSS_POS_Z(  10)  // 自分のポケモンのZ
 #define _POKEMON_FRIEND_APPEAR_POSX _MCSS_POS_X( 60)  // 相手のポケモンのX
-#define _POKEMON_FRIEND_APPEAR_POSY _MCSS_POS_Y(100)  // 相手のポケモンのY
+#define _POKEMON_FRIEND_APPEAR_POSY _MCSS_POS_Y(80)  // 相手のポケモンのY
 #define _POKEMON_FRIEND_APPEAR_POSZ _MCSS_POS_Z(-10)  // 相手のポケモンのZ
 
 //絵コンテ９番の移動後の位置
 #define _POKEMON_PLAYER_UP_POSX   _POKEMON_PLAYER_APPEAR_POSX  // 自分のポケモンのX
-#define _POKEMON_PLAYER_UP_POSY   _MCSS_POS_Y(-45)             // 自分のポケモンのY
+#define _POKEMON_PLAYER_UP_POSY   _MCSS_POS_Y(-55)             // 自分のポケモンのY
 #define _POKEMON_PLAYER_UP_POSZ   _POKEMON_PLAYER_APPEAR_POSZ  // 自分のポケモンのZ
 #define _POKEMON_FRIEND_DOWN_POSX _POKEMON_FRIEND_APPEAR_POSX  // 相手のポケモンのX
-#define _POKEMON_FRIEND_DOWN_POSY _MCSS_POS_Y( 0)              // 相手のポケモンのY
+#define _POKEMON_FRIEND_DOWN_POSY _MCSS_POS_Y( 5)              // 相手のポケモンのY
 #define _POKEMON_FRIEND_DOWN_POSZ _POKEMON_FRIEND_APPEAR_POSZ  // 相手のポケモンのZ
 
 
@@ -104,17 +104,17 @@
 
 //絵コンテ１１番
 #define _POKEMON_PLAYER_SIDEST_POSX _POKEMON_PLAYER_SIDEOUT_POSX  // 自分のポケモンが左から登場する場合のX
-#define _POKEMON_PLAYER_SIDEST_POSY _MCSS_POS_Y(0)  // 自分のポケモンが左から登場する場合のY
+#define _POKEMON_PLAYER_SIDEST_POSY _MCSS_POS_Y(5)  // 自分のポケモンが左から登場する場合のY
 #define _POKEMON_PLAYER_SIDEST_POSZ _MCSS_POS_Z(-10)  // 自分のポケモンが左から登場する場合のY
 #define _POKEMON_FRIEND_SIDEST_POSX _POKEMON_FRIEND_SIDEOUT_POSX  // 相手のポケモンが右から登場する場合のX
-#define _POKEMON_FRIEND_SIDEST_POSY _MCSS_POS_Y(-45)  // 相手のポケモンが右から登場する場合のY
+#define _POKEMON_FRIEND_SIDEST_POSY _MCSS_POS_Y(-55)  // 相手のポケモンが右から登場する場合のY
 #define _POKEMON_FRIEND_SIDEST_POSZ _MCSS_POS_Z(10)  // 相手のポケモンが右から登場する場合のY
 
 //絵コンテ１２番
 #define _POKEMON_PLAYER_SIDEIN_POSX _MCSS_POS_X( 60)  // 自分のポケモンが左から登場する場合のX
 #define _POKEMON_PLAYER_SIDEIN_POSY _POKEMON_PLAYER_SIDEST_POSY  // 自分のポケモンが左から登場する場合のY
 #define _POKEMON_PLAYER_SIDEIN_POSZ _POKEMON_PLAYER_SIDEST_POSZ  // 自分のポケモンが左から登場する場合のY
-#define _POKEMON_FRIEND_SIDEIN_POSX _MCSS_POS_X(-55)  // 相手のポケモンが右から登場する場合のX
+#define _POKEMON_FRIEND_SIDEIN_POSX _MCSS_POS_X(-45)  // 相手のポケモンが右から登場する場合のX
 #define _POKEMON_FRIEND_SIDEIN_POSY _POKEMON_FRIEND_SIDEST_POSY  // 相手のポケモンが右から登場する場合のY
 #define _POKEMON_FRIEND_SIDEIN_POSZ _POKEMON_FRIEND_SIDEST_POSZ  // 相手のポケモンが右から登場する場合のY
 
