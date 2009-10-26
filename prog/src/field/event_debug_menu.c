2b752a03b945c6cf0c0d94f59b181d19ff17de1e
Kōji Nishino <nishino@gamefreak.co.jp>
2009-10-26 03:16:27 +0000

    ポケモン名が変更になったので修正 by soga

--- prog/src/field/event_debug_menu.c
+++ prog/src/field/event_debug_menu.c
@@ -2910,7 +2910,7 @@ static BOOL debugMenuCallProc_BoxMax( DEBUG_MENU_EVENT_WORK *wk )
   myStatus = GAMEDATA_GetMyStatus(GAMESYSTEM_GetGameData(gameSys));
   name = MyStatus_GetMyName( myStatus );
   
-	pp = PP_Create(MONSNO_REIBAAN, 100, 123456, HEAPID_FIELDMAP);
+	pp = PP_Create(MONSNO_WARUBIARU, 100, 123456, HEAPID_FIELDMAP);
 
 	{
 		int i,j;
