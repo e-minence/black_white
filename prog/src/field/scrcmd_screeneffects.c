6a74edddb0820780a649c5d94d1a59b9f339dda8
Sosuke Tamada <tamada@gamefreak.co.jp>
2009-10-26 04:04:43 +0000

    ドアアニメを機能するように修正した

--- prog/src/field/scrcmd_screeneffects.c
+++ prog/src/field/scrcmd_screeneffects.c
@@ -27,6 +27,12 @@
 #include "field_player.h"   //FIELD_PLAYER
 #include "fieldmap.h"       //FIELDMAP_GetFieldPlayer
 
+#include "field_g3d_mapper.h" //FLDMAPPPER
+#include "field_buildmodel.h"
+#include "field_bmanime_tool.h"
+
+#include "field/field_const.h"
+
 static BOOL EvWaitDispFade( VMHANDLE *core, void *wk );
 
 //======================================================================
@@ -122,8 +128,58 @@ VMCMD_RESULT EvCmdPokecenRecoverAnime( VMHANDLE * core, void *wk )
 //
 //======================================================================
 //--------------------------------------------------------------
+//--------------------------------------------------------------
+typedef u16 MEMKEY;
+
+//--------------------------------------------------------------
+//--------------------------------------------------------------
+typedef struct {
+  MEMKEY key;
+  void * memory;
+}MEM_RES;
+
+//--------------------------------------------------------------
 /**
- * @brief
+ * @brief ドアアニメを確保するためのキー
+ * @todo  キーで管理するメモリ機構を用意し、そちらに移行する
+ */
+//--------------------------------------------------------------
+static MEM_RES memRes = { 0, NULL };
+
+//--------------------------------------------------------------
+//--------------------------------------------------------------
+static void reserveKey(void * mem, MEMKEY key)
+{
+  GF_ASSERT_MSG(memRes.key == 0, "アニメ管理領域が確保できません!!\n");
+  memRes.key = key;
+  memRes.memory = mem;
+}
+
+//--------------------------------------------------------------
+//--------------------------------------------------------------
+static void releaseKey(MEMKEY key)
+{
+  GF_ASSERT_MSG( memRes.key == key, "不正な管理番号です\n");
+  memRes.key = 0;
+  memRes.memory = NULL;
+}
+
+//--------------------------------------------------------------
+//--------------------------------------------------------------
+static void * getMemory(MEMKEY key)
+{
+  GF_ASSERT_MSG( memRes.key == key, "不正な管理番号です\n");
+  return memRes.memory;
+}
+
+//--------------------------------------------------------------
+//--------------------------------------------------------------
+static BOOL EvWaitDoorAnime( VMHANDLE *core, void *wk );
+enum { DOOR_ANIME_KEY_01 = 0x03fa };
+
+//--------------------------------------------------------------
+/**
+ * @brief ドアアニメ制御：ハンドル生成処理
  * @param  core    仮想マシン制御構造体へのポインタ
  * @param wk      SCRCMD_WORKへのポインタ
  * @retval VMCMD_RESULT
@@ -135,17 +191,32 @@ VMCMD_RESULT EvCmdDoorAnimeCreate( VMHANDLE * core, void *wk )
   u16 * ret_wk = SCRCMD_GetVMWork( core, wk );
   u16 gx = SCRCMD_GetVMWorkValue( core, wk );
   u16 gz = SCRCMD_GetVMWorkValue( core, wk );
-  /*
-   * 生成：
-   */
-  *ret_wk = 0;
+  MEMKEY key;
+  VecFx32 pos;
+  SCRCMD_WORK *work = wk;
+  GAMESYS_WORK *gsys = SCRCMD_WORK_GetGameSysWork( work );
+  FIELDMAP_WORK * fieldmap = GAMESYSTEM_GetFieldMapWork( gsys );
+  FLDMAPPER * mapper = FIELDMAP_GetFieldG3Dmapper( fieldmap );
+  FIELD_BMODEL_MAN * bmodel_man = FLDMAPPER_GetBuildModelManager( mapper );
+
+  BMANIME_CONTROL_WORK * ctrl;
+
+  pos.x = GRID_TO_FX32( gx );
+  pos.y = 0;
+  pos.z = GRID_TO_FX32( gz );
+
+  ctrl = BMANIME_CTRL_Create( bmodel_man, &pos );
+
+  reserveKey( ctrl, DOOR_ANIME_KEY_01 );
+
+  *ret_wk = DOOR_ANIME_KEY_01;
 
   return VMCMD_RESULT_CONTINUE;
 }
 
 //--------------------------------------------------------------
 /**
- * @brief
+ * @brief   ドアアニメ制御：ハンドル削除処理
  * @param  core    仮想マシン制御構造体へのポインタ
  * @param wk      SCRCMD_WORKへのポインタ
  * @retval VMCMD_RESULT
@@ -155,15 +226,19 @@ VMCMD_RESULT EvCmdDoorAnimeCreate( VMHANDLE * core, void *wk )
 VMCMD_RESULT EvCmdDoorAnimeDelete( VMHANDLE * core, void *wk )
 {
   u16 anime_id = SCRCMD_GetVMWorkValue( core, wk );
-  /*
-   * 削除：
-   */
+
+  BMANIME_CONTROL_WORK * ctrl;
+
+  ctrl = getMemory( anime_id );
+  BMANIME_CTRL_Delete( ctrl );
+  releaseKey( anime_id );
+
   return VMCMD_RESULT_CONTINUE;
 }
 
 //--------------------------------------------------------------
 /**
- * @brief
+ * @brief ドアアニメ制御：アニメセット
  * @param  core    仮想マシン制御構造体へのポインタ
  * @param wk      SCRCMD_WORKへのポインタ
  * @retval VMCMD_RESULT
@@ -174,15 +249,17 @@ VMCMD_RESULT EvCmdDoorAnimeSet( VMHANDLE * core, void *wk )
 {
   u16 anime_id = SCRCMD_GetVMWorkValue( core, wk );
   u16 anime_type = SCRCMD_GetVMWorkValue( core, wk );
-  /*
-   * アニメセット：
-   */
+  BMANIME_CONTROL_WORK * ctrl;
+
+  ctrl = getMemory( anime_id );
+  BMANIME_CTRL_SetAnime( ctrl, anime_type );
+
   return VMCMD_RESULT_CONTINUE;
 }
 
 //--------------------------------------------------------------
 /**
- * @brief
+ * @brief ドアアニメ制御：アニメウェイト
  * @param  core    仮想マシン制御構造体へのポインタ
  * @param wk      SCRCMD_WORKへのポインタ
  * @retval VMCMD_RESULT
@@ -195,9 +272,24 @@ VMCMD_RESULT EvCmdDoorAnimeSet( VMHANDLE * core, void *wk )
 VMCMD_RESULT EvCmdDoorAnimeWait( VMHANDLE * core, void *wk )
 {
   u16 anime_id = SCRCMD_GetVMWorkValue( core, wk );
-  /*
-   * アニメウェイト呼び出し
-   */
-  return VMCMD_RESULT_CONTINUE;
+  VMCMD_SetWait( core, EvWaitDoorAnime );
+
+  return VMCMD_RESULT_SUSPEND;
 }
+
+//--------------------------------------------------------------
+/**
+ * @brief   ドアアニメ終了待ち関数
+ * @param  core    仮想マシン制御構造体へのポインタ
+ * @param   wk      SCRCMD_WORKへのポインタ
+ * @return  BOOL  TRUEのとき終了
+ */
+//--------------------------------------------------------------
+static BOOL EvWaitDoorAnime( VMHANDLE *core, void *wk )
+{
+  BMANIME_CONTROL_WORK * ctrl;
+  ctrl = getMemory( DOOR_ANIME_KEY_01 );  //手抜き
+  return BMANIME_CTRL_WaitAnime( ctrl );
+}
+
 
