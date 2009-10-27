5b160bbe58c7fe0f96d4d49abd99a75988f818cf
Miyuki Iwasawa <iwasawa@gamefreak.co.jp>
2009-10-27 08:29:27 +0000

    スクリプト 　各種フィールドイベント用コモンスクリプトファイル追加 　　虫除けスプレー、甘い香り用ID定義

---
+++ resource/fldmapdata/script/field_ev_scr_def.h
@@ -0,0 +1,12 @@
+//field_ev_scr_def.h
+//このファイルはコンバータによって作成されました
+
+#ifndef _FIELD_EV_SCR_DEF_H_
+#define _FIELD_EV_SCR_DEF_H_
+
+//スクリプトデータID定義
+#define SCRID_FLD_EV_SPRAY_EFFECT_END (10130)
+#define SCRID_FLD_EV_AMAIKAORI_ENCOUNT_FAILED (10131)
+#define SCRID_FLD_EV_AMAIKAORI_WEATHER (10132)
+
+#endif //_FIELD_EV_SCR_DEF_H_
