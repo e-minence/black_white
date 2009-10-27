1314f0935a6a156a05c985e7040d771445118407
Nozomu Saito <saito_nozomu@gamefreak.co.jp>
2009-10-27 12:53:24 +0000

    //SE変更

--- prog/src/field/gym_fly_se_def.h
+++ prog/src/field/gym_fly_se_def.h
@@ -12,6 +12,6 @@
 #define GYM_FLY_SE_CLOSE  (SEQ_SE_FLD_40)    //‘å–CƒhƒA•Â‚Ü‚é
 #define GYM_FLY_SE_FIRE  (SEQ_SE_FLD_37)     //‘å–CŒ‚‚Â
 #define GYM_FLY_SE_STAND  (SEQ_SE_FLD_38)     //Ž©‹@’…’n
-#define GYM_FLY_SE_WALL_HIT  (SEQ_SE_WALL_HIT)     //•Çƒqƒbƒg
+#define GYM_FLY_SE_WALL_HIT  (SEQ_SE_FLD_48)     //•Çƒqƒbƒg
 #define GYM_FLY_SE_CAN_MOVE  (SEQ_SE_FLD_47)     //‘å–CŒX‚­
 
