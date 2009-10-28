42b1e3fc2292bb9891fdaba8012c93428493d315
Masafumi Saito <saito_masafumi@gamefreak.co.jp>
2009-10-28 14:14:41 +0000

    イベント修正：シーンチェンジラベルの名称を修正

--- resource/fldmapdata/script/t01r0401_def.h
+++ resource/fldmapdata/script/t01r0401_def.h
@@ -5,7 +5,7 @@
 #define _T01R0401_DEF_H_
 
 //スクリプトデータID定義
-#define SCRID_T01R0401_SCENE_CHANGE_01 (1)
+#define SCRID_SCENE_T01R0401_ZUKANGET_01 (1)
 #define SCRID_T01R0401_HAKASE2_01 (2)
 
 #endif //_T01R0401_DEF_H_
