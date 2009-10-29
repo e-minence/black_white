d5486eacfb766e0737108b1dd2bb1c2c39b73bf1
Katsunori Suginaka <suginaka_katsunori@gamefreak.co.jp>
2009-10-29 10:36:11 +0000

    接続データ：C03P04～C03R06～R04を追加致しました スクリプト：C01GYMにD01R01のワークを付加しました

--- resource/fldmapdata/eventdata/tmp/c03r0601.h
+++ resource/fldmapdata/eventdata/tmp/c03r0601.h
@@ -2,6 +2,12 @@
 #ifndef ZONE_C03R0601_EVD_H
 #define ZONE_C03R0601_EVD_H
 
+#define DOOR_ID_C03R0601_C03P04_EXIT01    0
+#define DOOR_ID_C03R0601_C03P04_EXIT03    1
+#define DOOR_ID_C03R0601_C03P04_EXIT02    2
+#define DOOR_ID_C03R0601_R04_EXIT01       3
+#define DOOR_ID_C03R0601_R04_EXIT03       4
+#define DOOR_ID_C03R0601_R04_EXIT02       5
 
 #endif //ZONE_C03R0601_EVD_H
 //Ç±ÇÃÉtÉ@ÉCÉãÇÕ../data/c03r0601.mevÇ©ÇÁé©ìÆê∂ê¨Ç≥ÇÍÇ‹ÇµÇΩ
--- resource/fldmapdata/eventdata/tmp/r04.h
+++ resource/fldmapdata/eventdata/tmp/r04.h
@@ -6,12 +6,12 @@
 #define DOOR_ID_R04_C04R0601_EXIT02       1
 #define DOOR_ID_R04_C04R0601_EXIT03       2
 #define DOOR_ID_R04_C04R0601_EXIT04       3
-#define DOOR_ID_R04_C03P04_EXIT01         4
-#define DOOR_ID_R04_C03P04_EXIT02         5
+#define DOOR_ID_R04_C03R0601_EXIT01       4
+#define DOOR_ID_R04_C03R0601_EXIT04       5
 #define DOOR_ID_R04_R04R0101_EXIT01       6
 #define DOOR_ID_R04_R04R0201_EXIT01       7
-#define DOOR_ID_R04_C03P04_EXIT03         8
-#define DOOR_ID_R04_C03P04_EXIT04         9
+#define DOOR_ID_R04_C03R0601_EXIT03       8
+#define DOOR_ID_R04_C03R0601_EXIT02       9
 
 #endif //ZONE_R04_EVD_H
 //Ç±ÇÃÉtÉ@ÉCÉãÇÕ../data/r04.mevÇ©ÇÁé©ìÆê∂ê¨Ç≥ÇÍÇ‹ÇµÇΩ
