d388a0a7a669c2c95d8731609ec167099562fd54
Genya Hosaka <hosaka@gamefreak.co.jp>
2009-11-13 11:03:34 +0000

    簡易会話 50音GMM生成コンバータ完成

--- resource/pms/pms_gmm_conv.rb
+++ resource/pms/pms_gmm_conv.rb
@@ -22,45 +22,61 @@ require File.dirname(__FILE__) + '/../../tools/gmm_make/gmm_make'
 # 定数宣言
 #====================================================
 GMM_SOURCE = "../message/template.gmm"
-
 SRC_RESOURCE = "pms_input.res"
 DST_GMM_PATH = "../message/src/pms/abc/"
 
 #====================================================
 # GMM 行書き込み
 #====================================================
-def make_row( gmm )
-  row_name = sprintf("search_abc_%02d",1)
-  row_data = "あいう"
-  gmm.make_row_kanji( row_name, row_data, row_data )
+def make_row( gmm, data )
+  data.each{|i|
+    #空を見つけたら抜ける
+    if(i == nil )
+      break
+    end
+    
+    row_name = sprintf("search_abc_%02d",1)
+    row_data = i
+    gmm.make_row_kanji( row_name, row_data, row_data )
+  }
 end
 
 #====================================================
 # GMM 吐き出し
 #====================================================
-def make_gmm( gmm_name )
+def make_gmm( gmm_name, data )
   gmm = GMM::new
   gmm.open_gmm( GMM_SOURCE, DST_GMM_PATH + gmm_name )
-  make_row( gmm )
+  make_row( gmm, data )
   gmm.close_gmm
 end
-
-
-#====================================================
-# データ変換
-#====================================================
 
 #====================================================
 # リソースから文字列を抽出
 #====================================================
-def line_trim
+def str_trim( str )
+  start = str.index("//") + 2
+  finish = str.index("\n") - 1
 
-  
+  str = str[start..finish]
+
+  # カタカナをひらがなに
+  # 濁点を外す
+  origin = "ａ-ｚＡ-Ｚァ-ン０-９ぁぃぅぇぉがぎぐげござじずぜぞだぢづでどばびぶべぼぱぴぷぺぽゃゅょっゎァィゥェォガギグゲゴザジズゼゾダヂヅデドバビブベボパピプペポャュョッヮ"
+  normalize = "a-zA-Zぁ-ん0-9あいうえおかきくけこさしすせそたちつてとはひふへほはひふへほやゆよつわあいうえおかきくけこさしすせそたちつてとはひふへほはひふへほやゆよつわ"
+  str = str.tr(origin,normalize);
+
+  return str
 end
 
 
+#多次元配列
+def make_nm_array(n,m)
+  (0...n).map { Array.new(m) }
+end
+
 $filenum = 0
-$data = Array.new
+$data = make_nm_array( 50, 200 )
 
 #====================================================
 # リソース 読み込み
@@ -77,8 +93,11 @@ def load_resource
           output_flag = 0
           $filenum += 1
         else
+          #余分な文字列を除去
+          line = str_trim( line )
           #項目出力
-          $data[$filenum,row_idx] = line
+          $data[$filenum][row_idx] = line
+          #要素を進める
           row_idx += 1;
           
         end
@@ -92,12 +111,6 @@ def load_resource
   }
 end
 
-
-
-# カタカナをひらがなに
-# 濁点を外す
-
-
 #====================================================
 # 主処理 開始
 #====================================================
@@ -105,22 +118,19 @@ end
 # データ解析
 load_resource
 
-#$data = $data.sort_by{ |id,elem|
-#  id.downcase
-#}
-
-
-
-
 # 出力
+cnt = 0
+$data.each{|data|
+  #空を見つけたら抜ける
+  if(data[0] == nil )
+    break
+  end
 
-#TODO eachにかえる 
-for i in 1..50
-  name = "pms_abc" + sprintf("%02d",i) + ".gmm"
-  make_gmm( name )
-end
-
-
+  name = "pms_abc" + sprintf("%02d",cnt) + ".gmm"
+  make_gmm( name, data )
+  
+  cnt += 1
+}
 
 
 
