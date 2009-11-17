36fdf65bcc8fde8b660fd0965b25058ea94fc9b2
Masao Taya <taya@gamefreak.co.jp>
2009-11-17 02:32:40 +0000

    追加

---
+++ tools/personal/taya_prompt.bat
@@ -0,0 +1,13 @@
+REM ##################################################
+REM
+REM		田谷の個人用設定（prompt.batから呼び出される）
+REM
+REM ##################################################
+
+
+doskey gowa = cd %PROJECT_RSCDIR:/=\%waza_tbl
+doskey gome = cd %PROJECT_RSCDIR:/=\%message
+doskey gofo = cd %PROJECT_RSCDIR:/=\%font
+
+cd %PROJECT_PROGDIR:/=\%
+
