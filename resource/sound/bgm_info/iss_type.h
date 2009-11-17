82be4c0ce3bee2bd10520cc663976ac2b9994382
Toshihiro Obata <obata_toshihiro@gamefreak.co.jp>
2009-11-17 08:14:41 +0000

    ISSタイプの指定方法を変更

--- resource/sound/bgm_info/iss_type.h
+++ resource/sound/bgm_info/iss_type.h
@@ -1,9 +1,11 @@
 #pragma once 
-#define	ISS_TYPE_NORMAL		(0)		// 通常
-#define	ISS_TYPE_SEASON		(1)		// 季節
-#define	ISS_TYPE_LOAD		(2)		// 道路
-#define	ISS_TYPE_CITY		(3)		// 街(町)
-#define	ISS_TYPE_DUNGEON	(4)		// ダンジョン
-#define	ISS_TYPE_BATTLE		(5)		// 戦闘
-#define ISS_TYPE_ZONE (6)  // ゾーン or 場所スイッチ
-#define	ISS_TYPE_MAX		(7)
+#define	ISS_TYPE_NORMAL		(0)	 // 通常ISS
+#define	ISS_TYPE_SEASON		(1)	 // 四季ISS
+#define	ISS_TYPE_LOAD		  (2)	 // 道路ISS
+#define	ISS_TYPE_CITY		  (3)	 // 街ISS
+#define	ISS_TYPE_BRIDGE		(3)	 // 橋ISS
+#define	ISS_TYPE_DUNGEON	(4)	 // ダンジョンISS
+#define	ISS_TYPE_BATTLE		(5)	 // バトルISS
+#define ISS_TYPE_SWITCH   (6)  // 人スイッチISS
+#define ISS_TYPE_ZONE     (6)  // 場所スイッチISS
+#define	ISS_TYPE_MAX		  (7)
