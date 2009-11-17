26af0cf58927c3e9fd5dc0e21a27b2788d4a2cf2
Nozomu Saito <saito_nozomu@gamefreak.co.jp>
2009-11-17 04:35:20 +0000

    //コンバート環境更新

---
+++ resource/fld3d_ci/make_arc_target.rb
@@ -0,0 +1,67 @@
+$KCODE = "SJIS"
+
+def CollectStr(vec, str)
+  if str == "NONE" then
+  elsif str == nil then
+    #エラー
+    p "ERROR::指定文字列がありません"
+    exit -1
+  else
+    ret_idx = vec.index(str)
+    if ret_idx == nil then
+      #エントリー
+      fix_str = str
+      fix_str = fix_str.sub(/.imd/,".nsbmd")
+      fix_str = fix_str.sub(/.ica/,".nsbca")
+      fix_str = fix_str.sub(/.ima/,".nsbma")
+      fix_str = fix_str.sub(/.itp/,".nsbtp")
+      fix_str = fix_str.sub(/.ita/,".nsbta")
+      fix_str = fix_str.sub(/.iva/,".nsbva")
+      fix_str = fix_str + " \\"
+      vec << fix_str
+    else
+      printf("ERROR::指定文字列は既にある %s\n",str)
+      exit -1
+    end
+  end
+end
+
+csv_file = open(ARGV.shift,"r")
+list_file = open(ARGV.shift,"w")
+
+ary = Array.new
+ary.clear
+#1行読み飛ばし
+csv_file.gets
+while line = csv_file.gets
+  #END検出
+  if line =~/^#END/ then
+		break
+	end
+  column = line.split ","
+
+  #データ収集
+  CollectStr(ary,column[1]) #SPA
+  CollectStr(ary,column[4])  #MDL_1
+  CollectStr(ary,column[9])  #MDL_2
+
+  CollectStr(ary,column[5])  #MDL_1ANM_1
+  CollectStr(ary,column[6])  #MDL_1ANM_2
+  CollectStr(ary,column[7])  #MDL_1ANM_3
+  
+  CollectStr(ary,column[10])   #MDL_2ANM_1
+  CollectStr(ary,column[11])  #MDL_2ANM_2
+  CollectStr(ary,column[12])  #MDL_2ANM_3
+
+end
+
+#最後の文字列だけ「￥」をとる
+last = ary.pop
+last.delete!(" \\")
+ary<<last
+
+list_file.printf("ARC_TARGET_DATA = \\\n")
+ary.each{ |i|
+  list_file.printf("\t%s\n",i)
+}
+
