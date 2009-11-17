0531daffa839c98d285aa7360f5771e1e6680a32
Nozomu Saito <saito_nozomu@gamefreak.co.jp>
2009-11-17 09:09:01 +0000

    //カットインナンバー定義ファイル追加

---
+++ resource/fld3d_ci/fldci_id_def.h
@@ -0,0 +1,21 @@
+//======================================================================
+/**
+ * @file	fldci_id_def.h
+ * @brief	フィールド3Dカットインナンバー定義
+ * @author	Saito
+ *
+ * @note    このファイルはコンバータにより出力されます
+ *
+ */
+//======================================================================
+
+#pragma once
+
+#define FLDCIID_GYM01 (0)
+#define FLDCIID_GYM02 (1)
+#define FLDCIID_GYM03 (2)
+#define FLDCIID_GYM04 (3)
+#define FLDCIID_GYM05 (4)
+#define FLDCIID_GYM06 (5)
+#define FLDCIID_GYM07 (6)
+#define FLDCIID_GYM08 (7)
