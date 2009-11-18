3f25a14cfc354a724aeb63f74ac659f7d9c50006
Sosuke Tamada <tamada@gamefreak.co.jp>
2009-11-18 14:35:56 +0000

    取り急ぎ、R10R0101で必要そうなスクリプトラベルのみ作成

--- resource/fldmapdata/script/r10r0101_def.h
+++ resource/fldmapdata/script/r10r0101_def.h
@@ -5,6 +5,8 @@
 #define _R10R0101_DEF_H_
 
 //スクリプトデータID定義
-#define SCRID_R10R0101 (1)
+#define SCRID_R10R0101_VETERANM_01 (1)
+#define SCRID_POS_R10R0101_LIMITTER_01 (2)
+#define SCRID_ZONE_R10R0101 (3)
 
 #endif //_R10R0101_DEF_H_
