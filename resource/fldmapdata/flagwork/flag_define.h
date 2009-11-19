c91b5733732c8775960e99eea1151ef5d7313de7
Emi Ando <ando@gamefreak.co.jp>
2009-11-19 10:25:42 +0000

    07：アララギ父のバニッシュフラグ追加

--- resource/fldmapdata/flagwork/flag_define.h
+++ resource/fldmapdata/flagwork/flag_define.h
@@ -600,7 +600,7 @@
 #define FV_C07_RiIVAL_01                   576 /* 0x0240 C07セッカシティ：ライバル・サポート */
 #define FV_C07_HAKAIM_01                   577 /* 0x0241 C07セッカシティ：プラズマ団たち */
 #define FV_C07_READER7                     578 /* 0x0242 C07セッカシティ：ハチク */
-#define FV_C07GYM0101_READER7              579 /* 0x0243 C07GYM0101セッカシティジム：ハチク */
+#define FV_C07_HAKASE1                     579 /* 0x0243 C07セッカシティジム：アララギ（父） */
 		/* not used   580(0x0244) */
 		/* not used   581(0x0245) */
 		/* not used   582(0x0246) */
