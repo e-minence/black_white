41e5ef7eb07a3c5e34f9e36480cd91e8ddc5d037
Toshihiro Obata <obata_toshihiro@gamefreak.co.jp>
2009-11-20 05:56:08 +0000

    不要になったソースファイルを削除

--- prog/src/field/fieldmap_tcb.h
+++ prog/src/field/fieldmap_tcb.h
@@ -10,23 +10,6 @@
 ////////////////////////////////////////////////////////////////////////
 #pragma once
 
-//============================
-// fieldmap_tcb_rot_player.c
-//============================ 
-// プレイヤーの回転
-void FIELDMAP_TCB_AddTask_RotatePlayer(                // 等速
-    FIELDMAP_WORK* fieldmap, int frame, int rot_num );    
-void FIELDMAP_TCB_AddTask_RotatePlayer_SpeedUp(        // 加速
-    FIELDMAP_WORK* fieldmap, int frame, int rot_num );    
-void FIELDMAP_TCB_AddTask_RotatePlayer_SpeedDown(      // 減速
-    FIELDMAP_WORK* fieldmap, int frame, int rot_num );   
-
-//============================
-// fieldmap_tcb_move_player.c
-//============================
-// プレイヤーの移動
-void FIELDMAP_TCB_AddTask_MovePlayer(
-    FIELDMAP_WORK* fieldmap, int frame, VecFx32* pos );
 
 //============================
 // fieldmap_tcb_camera_zoom.c
