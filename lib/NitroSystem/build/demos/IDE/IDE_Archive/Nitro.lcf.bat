97ca442de6075676a6c188be2d226dc6b4d20dfb
Katsumi Ohno <ohno@gamefreak.co.jp>
2009-11-21 06:29:15 +0000

    NITROSYSTEMを削除

--- lib/NitroSystem/build/demos/IDE/IDE_Archive/Nitro.lcf.bat
+++
@@ -1,1 +0,0 @@
-"%NITROSDK_ROOT%\tools\bin\makelcf.exe" "C:\home\NitroProject\IDESample\IDE_Archive\Nitro.lcf.lsf" "C:\home\NitroProject\NitroSDK\include\nitro\specfiles\ARM9-TS.lcf.template" "C:\home\NitroProject\IDESample\IDE_Archive\Nitro.lcf"
