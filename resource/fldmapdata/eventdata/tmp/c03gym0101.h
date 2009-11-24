4874518f4f94db4bbb624c54285785e552f7e2d6
Masafumi Saito <saito_masafumi@gamefreak.co.jp>
2009-11-24 12:36:30 +0000

    イベント修正：虫ジムの透明人間を減らしました

--- resource/fldmapdata/eventdata/tmp/c03gym0101.h
+++ resource/fldmapdata/eventdata/tmp/c03gym0101.h
@@ -11,34 +11,20 @@
 #ifndef ZONE_C03GYM0101_EVC_H
 #define ZONE_C03GYM0101_EVC_H
 
-#define C03GYM0101_WALL01_L                0
-#define C03GYM0101_WALL01_R                1
-#define C03GYM0101_WALL01_C                2
-#define C03GYM0101_WALL02_L                3
-#define C03GYM0101_WALL02_R                4
-#define C03GYM0101_WALL02_C                5
-#define C03GYM0101_WALL03_L                6
-#define C03GYM0101_WALL03_R                7
-#define C03GYM0101_WALL03_C                8
-#define C03GYM0101_WALL04_L                9
-#define C03GYM0101_WALL04_R               10
-#define C03GYM0101_WALL04_C               11
-#define C03GYM0101_WALL05_L               12
-#define C03GYM0101_WALL05_R               13
-#define C03GYM0101_WALL05_C               14
-#define C03GYM0101_WALL06_L               15
-#define C03GYM0101_WALL06_R               16
-#define C03GYM0101_WALL06_C               17
-#define C03GYM0101_WALL07_L               18
-#define C03GYM0101_WALL07_R               19
-#define C03GYM0101_WALL07_C               20
-#define C03GYM0101_TRAINER_01             21
-#define C03GYM0101_TRAINER_02             22
-#define C03GYM0101_LEADER3_01             23
-#define C03GYM0101_SUNGLASSES_01          24
-#define TR_DUMMY02                        25
-#define TR_DUMMY01                        26
-#define TR_DUMMY03                        27
-#define TR_DUMMY04                        28
+#define C03GYM0101_WALL01_C                0
+#define C03GYM0101_WALL02_C                1
+#define C03GYM0101_WALL03_C                2
+#define C03GYM0101_WALL04_C                3
+#define C03GYM0101_WALL05_C                4
+#define C03GYM0101_WALL06_C                5
+#define C03GYM0101_WALL07_C                6
+#define C03GYM0101_TRAINER_01              7
+#define C03GYM0101_TRAINER_02              8
+#define C03GYM0101_LEADER3_01              9
+#define C03GYM0101_SUNGLASSES_01          10
+#define TR_DUMMY02                        11
+#define TR_DUMMY01                        12
+#define TR_DUMMY03                        13
+#define TR_DUMMY04                        14
 
 #endif //ZONE_C03GYM0101_EVC_H
