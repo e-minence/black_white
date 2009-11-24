17538d099b7dd60e41b1d6e667b34e59e504b9ef
Ariizumi Nobuhiko <ariizumi_nobuhiko@gamefreak.co.jp>
2009-11-24 08:41:23 +0000

    マルチブート：素材周り環境修正

---
+++ prog/src/multiboot/capture/mb_capture_sys.c
@@ -0,0 +1,492 @@
+//======================================================================
+/**
+ * @file	mb_capture_sys.h
+ * @brief	マルチブート・捕獲ゲーム
+ * @author	ariizumi
+ * @data	09/11/24
+ *
+ * モジュール名：MB_CAPTURE
+ */
+//======================================================================
+#include <gflib.h>
+#include "system/main.h"
+#include "system/gfl_use.h"
+#include "print/printsys.h"
+#include "print/wordset.h"
+#include "sound/pm_sndsys.h"
+#include "system/wipe.h"
+
+#include "arc_def.h"
+#include "mb_child_gra.naix"
+
+#include "multiboot/mb_util_msg.h"
+#include "multiboot/mb_poke_icon.h"
+#include "multiboot/mb_local_def.h"
+#include "multiboot/mb_capture_sys.h"
+#include "./mb_cap_obj.h"
+
+
+//======================================================================
+//	define
+//======================================================================
+#pragma mark [> define
+#define MB_CAPTURE_FRAME_MSG (GFL_BG_FRAME1_M)
+#define MB_CAPTURE_FRAME_BG  (GFL_BG_FRAME3_M)
+
+#define MB_CAPTURE_FRAME_SUB_BG  (GFL_BG_FRAME3_S)
+
+//======================================================================
+//	enum
+//======================================================================
+#pragma mark [> enum
+typedef enum
+{
+  MSS_FADEIN,
+  MSS_WAIT_FADEIN,
+  MSS_FADEOUT,
+  MSS_WAIT_FADEOUT,
+  
+}MB_CAPTURE_STATE;
+
+//======================================================================
+//	typedef struct
+//======================================================================
+#pragma mark [> struct
+typedef struct
+{
+  HEAPID heapId;
+  GFL_TCB *vBlankTcb;
+  MB_CAPTURE_INIT_WORK *initWork;
+  
+  MB_CAPTURE_STATE  state;
+  
+  GFL_G3D_CAMERA    *camera;
+  GFL_BBD_SYS     *bbdSys;
+
+  //メッセージ用
+  MB_MSG_WORK *msgWork;
+
+  //仮出し
+  GFL_CLUNIT  *cellUnit;
+  GFL_CLWK    *pokeIcon[MB_CAP_POKE_NUM];
+  u32         iconPalRes;
+  u32         iconAnmRes;
+  u32         iconCharRes[MB_CAP_POKE_NUM];
+  
+}MB_CAPTURE_WORK;
+
+
+//======================================================================
+//	proto
+//======================================================================
+#pragma mark [> proto
+
+static void MB_CAPTURE_Init( MB_CAPTURE_WORK *work );
+static void MB_CAPTURE_Term( MB_CAPTURE_WORK *work );
+static const BOOL MB_CAPTURE_Main( MB_CAPTURE_WORK *work );
+static void MB_CAPTURE_VBlankFunc(GFL_TCB *tcb, void *wk );
+
+static void MB_CAPTURE_InitGraphic( MB_CAPTURE_WORK *work );
+static void MB_CAPTURE_TermGraphic( MB_CAPTURE_WORK *work );
+static void MB_CAPTURE_SetupBgFunc( const GFL_BG_BGCNT_HEADER *bgCont , u8 bgPlane , u8 mode );
+static void MB_CAPTURE_LoadResource( MB_CAPTURE_WORK *work );
+
+static const GFL_DISP_VRAM vramBank = {
+  GX_VRAM_BG_128_A,             // メイン2DエンジンのBG
+  GX_VRAM_BGEXTPLTT_NONE,       // メイン2DエンジンのBG拡張パレット
+  GX_VRAM_SUB_BG_128_C,         // サブ2DエンジンのBG
+  GX_VRAM_SUB_BGEXTPLTT_NONE,   // サブ2DエンジンのBG拡張パレット
+  GX_VRAM_OBJ_128_B ,           // メイン2DエンジンのOBJ
+  GX_VRAM_OBJEXTPLTT_NONE,      // メイン2DエンジンのOBJ拡張パレット
+  GX_VRAM_SUB_OBJ_128_D,        // サブ2DエンジンのOBJ
+  GX_VRAM_SUB_OBJEXTPLTT_NONE,  // サブ2DエンジンのOBJ拡張パレット
+  GX_VRAM_TEX_NONE,             // テクスチャイメージスロット
+  GX_VRAM_TEXPLTT_NONE,         // テクスチャパレットスロット
+  GX_OBJVRAMMODE_CHAR_1D_128K,
+  GX_OBJVRAMMODE_CHAR_1D_128K
+};
+
+
+//--------------------------------------------------------------
+//  初期化
+//--------------------------------------------------------------
+static void MB_CAPTURE_Init( MB_CAPTURE_WORK *work )
+{
+  work->state = MSS_FADEIN;
+  
+  MB_CAPTURE_InitGraphic( work );
+  MB_CAPTURE_LoadResource( work );
+  work->msgWork = MB_MSG_MessageInit( work->heapId , MB_CAPTURE_FRAME_MSG );
+  work->vBlankTcb = GFUser_VIntr_CreateTCB( MB_CAPTURE_VBlankFunc , work , 8 );
+
+}
+
+//--------------------------------------------------------------
+//  開放
+//--------------------------------------------------------------
+static void MB_CAPTURE_Term( MB_CAPTURE_WORK *work )
+{
+  {
+    //仮
+    u8 i;
+    for( i=0;i<MB_CAP_POKE_NUM;i++ )
+    {
+      GFL_CLACT_WK_Remove( work->pokeIcon[i] );
+      GFL_CLGRP_CGR_Release( work->iconCharRes[i] );
+    }
+    GFL_CLGRP_PLTT_Release( work->iconPalRes );
+    GFL_CLGRP_CELLANIM_Release( work->iconAnmRes );
+    GFL_CLACT_UNIT_Delete( work->cellUnit );
+  }
+
+  GFL_TCB_DeleteTask( work->vBlankTcb );
+
+  MB_MSG_MessageTerm( work->msgWork );
+  MB_CAPTURE_TermGraphic( work );
+}
+
+//--------------------------------------------------------------
+//  更新
+//--------------------------------------------------------------
+static const BOOL MB_CAPTURE_Main( MB_CAPTURE_WORK *work )
+{
+  switch( work->state )
+  {
+  case MSS_FADEIN:
+    WIPE_SYS_Start( WIPE_PATTERN_WMS , WIPE_TYPE_FADEIN , WIPE_TYPE_FADEIN , 
+                WIPE_FADE_BLACK , WIPE_DEF_DIV , WIPE_DEF_SYNC , work->heapId );
+
+    work->state = MSS_WAIT_FADEIN;
+    break;
+    
+  case MSS_WAIT_FADEIN:
+    if( WIPE_SYS_EndCheck() == TRUE )
+    {
+      if( GFL_UI_KEY_GetTrg() & PAD_BUTTON_A )
+      {
+        
+        work->state = MSS_FADEOUT;
+      }
+    }
+    break;
+
+  case MSS_FADEOUT:
+    WIPE_SYS_Start( WIPE_PATTERN_WMS , WIPE_TYPE_FADEOUT , WIPE_TYPE_FADEOUT , 
+                WIPE_FADE_BLACK , WIPE_DEF_DIV , WIPE_DEF_SYNC , work->heapId );
+    work->state = MSS_WAIT_FADEOUT;
+    break;
+    
+  case MSS_WAIT_FADEOUT:
+    if( WIPE_SYS_EndCheck() == TRUE )
+    {
+      return TRUE;
+    }
+    break;
+  }
+
+  //OBJの更新
+  GFL_CLACT_SYS_Main();
+
+  
+  if( GFL_UI_KEY_GetTrg() & PAD_BUTTON_START &&
+      GFL_UI_KEY_GetTrg() & PAD_BUTTON_SELECT )
+  {
+    return TRUE;
+  }
+  return FALSE;
+}
+
+
+//--------------------------------------------------------------
+//	VBlankTcb
+//--------------------------------------------------------------
+static void MB_CAPTURE_VBlankFunc(GFL_TCB *tcb, void *wk )
+{
+  //OBJの更新
+  GFL_CLACT_SYS_VBlankFunc();
+}
+
+//--------------------------------------------------------------
+//  グラフィック系初期化
+//--------------------------------------------------------------
+static void MB_CAPTURE_InitGraphic( MB_CAPTURE_WORK *work )
+{
+  GFL_DISP_SetDispOn();
+  GFL_DISP_GX_InitVisibleControl();
+  GFL_DISP_GXS_InitVisibleControl();
+  GX_SetVisiblePlane( 0 );
+  GXS_SetVisiblePlane( 0 );
+  WIPE_SetBrightness(WIPE_DISP_MAIN,WIPE_FADE_BLACK);
+  WIPE_SetBrightness(WIPE_DISP_SUB,WIPE_FADE_BLACK);
+  WIPE_ResetWndMask(WIPE_DISP_MAIN);
+  WIPE_ResetWndMask(WIPE_DISP_SUB);
+  
+  GX_SetDispSelect(GX_DISP_SELECT_MAIN_SUB);
+  GFL_DISP_SetBank( &vramBank );
+
+  //BG系の初期化
+  GFL_BG_Init( work->heapId );
+  GFL_BMPWIN_Init( work->heapId );
+
+  //Vram割り当ての設定
+  {
+    static const GFL_BG_SYS_HEADER sys_data = {
+        GX_DISPMODE_GRAPHICS, GX_BGMODE_0, GX_BGMODE_0, GX_BG0_AS_3D,
+    };
+    // BG1 MAIN (メッセージ
+    static const GFL_BG_BGCNT_HEADER header_main1 = {
+      0, 0, 0x800, 0, // scrX, scrY, scrbufSize, scrbufofs,
+      GFL_BG_SCRSIZ_256x256, GX_BG_COLORMODE_16,
+      GX_BG_SCRBASE_0x6000, GX_BG_CHARBASE_0x00000,0x6000,
+      GX_BG_EXTPLTT_01, 1, 0, 0, FALSE  // pal, pri, areaover, dmy, mosaic
+    };
+    // BG2 MAIN (キャラ
+    //static const GFL_BG_BGCNT_HEADER header_main2 = {
+    //  0, 0, 0x800, 0,  // scrX, scrY, scrbufSize, scrbufofs,
+    //  GFL_BG_SCRSIZ_256x256, GX_BG_COLORMODE_16,
+    //  GX_BG_SCRBASE_0x6800, GX_BG_CHARBASE_0x10000,0x0C000,
+    //  GX_BG_EXTPLTT_23, 2, 1, 0, FALSE  // pal, pri, areaover, dmy, mosaic
+    //};
+    // BG3 MAIN (背景
+    static const GFL_BG_BGCNT_HEADER header_main3 = {
+      0, 0, 0x800, 0,  // scrX, scrY, scrbufSize, scrbufofs,
+      GFL_BG_SCRSIZ_256x256, GX_BG_COLORMODE_16,
+      GX_BG_SCRBASE_0x7000, GX_BG_CHARBASE_0x18000,0x08000,
+      GX_BG_EXTPLTT_23, 3, 0, 0, FALSE  // pal, pri, areaover, dmy, mosaic
+    };
+
+    // BG3 SUB (背景
+    static const GFL_BG_BGCNT_HEADER header_sub3 = {
+      0, 0, 0x800, 0,  // scrX, scrY, scrbufSize, scrbufofs,
+      GFL_BG_SCRSIZ_256x256, GX_BG_COLORMODE_16,
+      GX_BG_SCRBASE_0x7000, GX_BG_CHARBASE_0x18000,0x08000,
+      GX_BG_EXTPLTT_23, 3, 0, 0, FALSE  // pal, pri, areaover, dmy, mosaic
+    };
+    GFL_BG_SetBGMode( &sys_data );
+
+    MB_CAPTURE_SetupBgFunc( &header_main1 , MB_CAPTURE_FRAME_MSG , GFL_BG_MODE_TEXT );
+    //MB_CAPTURE_SetupBgFunc( &header_main2 , LTVT_FRAME_CHARA , GFL_BG_MODE_TEXT );
+    MB_CAPTURE_SetupBgFunc( &header_main3 , MB_CAPTURE_FRAME_BG , GFL_BG_MODE_TEXT );
+    MB_CAPTURE_SetupBgFunc( &header_sub3  , MB_CAPTURE_FRAME_SUB_BG , GFL_BG_MODE_TEXT );
+    
+  }
+
+  //OBJ系の初期化
+  {
+    GFL_CLSYS_INIT cellSysInitData = GFL_CLSYSINIT_DEF_DIVSCREEN;
+    GFL_CLACT_SYS_Create( &cellSysInitData , &vramBank ,work->heapId );
+    
+    GFL_DISP_GX_SetVisibleControl( GX_PLANEMASK_OBJ , TRUE );
+  }
+  
+  //3Dの初期化
+  
+  { //3D系の設定
+    static const VecFx32 cam_pos = {0,0,FX32_CONST(300.0f)};
+    static const VecFx32 cam_target = {0,0,0};
+    static const VecFx32 cam_up = {0,FX32_ONE,0};
+    //エッジマーキングカラー
+    static  const GXRgb stage_edge_color_table[8]=
+      { GX_RGB( 0, 0, 0 ), GX_RGB( 0, 0, 0 ), 0, 0, 0, 0, 0, 0 };
+    GFL_G3D_Init( GFL_G3D_VMANLNK, GFL_G3D_TEX256K, GFL_G3D_VMANLNK, GFL_G3D_PLT80K,
+            0, work->heapId, NULL );
+    GFL_BG_SetBGControl3D( 2 ); //NNS_g3dInitの中で表示優先順位変わる・・・
+    GFL_G3D_SetSystemSwapBufferMode( GX_SORTMODE_AUTO, GX_BUFFERMODE_W );
+    //正射影カメラ
+    work->camera =  GFL_G3D_CAMERA_Create( GFL_G3D_PRJORTH, 
+                       0,
+                       FX32_ONE*192.0f,
+                       0,
+                       FX32_ONE*256.0f,
+                       FX32_ONE,
+                       FX32_ONE*350,
+                       0,
+                       &cam_pos,
+                       &cam_up,
+                       &cam_target,
+                       work->heapId );
+    
+    GFL_G3D_CAMERA_Switching( work->camera );
+    //エッジマーキングカラーセット
+    G3X_SetEdgeColorTable( &stage_edge_color_table[0] );
+    G3X_EdgeMarking( TRUE );
+    G3X_AntiAlias( TRUE );
+    G3X_AlphaBlend( TRUE );
+    
+    GFL_G3D_SetSystemSwapBufferMode( GX_SORTMODE_AUTO , GX_BUFFERMODE_Z );
+  }
+  {
+    GFL_BBD_SETUP bbdSetup = {
+      128,128,
+      {FX32_ONE,FX32_ONE,FX32_ONE},
+      GX_RGB(0,0,0),
+      GX_RGB(0,0,0),
+      GX_RGB(0,0,0),
+      GX_RGB(0,0,0),
+      0,
+      GFL_BBD_ORIGIN_CENTER,
+    };
+    //ビルボードシステム構築
+    work->bbdSys = GFL_BBD_CreateSys( &bbdSetup , work->heapId );
+  }
+}
+
+static void MB_CAPTURE_TermGraphic( MB_CAPTURE_WORK *work )
+{
+  GFL_BBD_DeleteSys( work->bbdSys );
+  GFL_G3D_CAMERA_Delete( work->camera );
+  GFL_G3D_Exit();
+
+  GFL_CLACT_SYS_Delete();
+  GFL_BG_FreeBGControl( MB_CAPTURE_FRAME_MSG );
+  GFL_BG_FreeBGControl( MB_CAPTURE_FRAME_BG );
+  GFL_BG_FreeBGControl( MB_CAPTURE_FRAME_SUB_BG );
+  GFL_BMPWIN_Exit();
+  GFL_BG_Exit();
+}
+
+
+//--------------------------------------------------------------------------
+//  Bg初期化 機能部
+//--------------------------------------------------------------------------
+static void MB_CAPTURE_SetupBgFunc( const GFL_BG_BGCNT_HEADER *bgCont , u8 bgPlane , u8 mode )
+{
+  GFL_BG_SetBGControl( bgPlane, bgCont, mode );
+  GFL_BG_SetVisible( bgPlane, VISIBLE_ON );
+  GFL_BG_ClearFrame( bgPlane );
+  GFL_BG_LoadScreenReq( bgPlane );
+}
+
+
+//--------------------------------------------------------------
+//  リソース読み込み
+//--------------------------------------------------------------
+static void MB_CAPTURE_LoadResource( MB_CAPTURE_WORK *work )
+{
+  ARCHANDLE *arcHandle = GFL_ARC_OpenDataHandle( ARCID_MB_CHILD , work->heapId );
+
+  //下画面
+  GFL_ARCHDL_UTIL_TransVramPalette( arcHandle , NARC_mb_child_gra_bg_sub_NCLR , 
+                    PALTYPE_SUB_BG , 0 , 0 , work->heapId );
+  GFL_ARCHDL_UTIL_TransVramBgCharacter( arcHandle , NARC_mb_child_gra_bg_sub_NCGR ,
+                    MB_CAPTURE_FRAME_SUB_BG , 0 , 0, FALSE , work->heapId );
+  GFL_ARCHDL_UTIL_TransVramScreen( arcHandle , NARC_mb_child_gra_bg_sub_NSCR , 
+                    MB_CAPTURE_FRAME_SUB_BG ,  0 , 0, FALSE , work->heapId );
+  
+  //上画面
+  GFL_ARCHDL_UTIL_TransVramPalette( arcHandle , NARC_mb_child_gra_bg_main_NCLR , 
+                    PALTYPE_MAIN_BG , 0 , 0 , work->heapId );
+  GFL_ARCHDL_UTIL_TransVramBgCharacter( arcHandle , NARC_mb_child_gra_bg_main_NCGR ,
+                    MB_CAPTURE_FRAME_BG , 0 , 0, FALSE , work->heapId );
+  GFL_ARCHDL_UTIL_TransVramScreen( arcHandle , NARC_mb_child_gra_bg_main_NSCR , 
+                    MB_CAPTURE_FRAME_BG ,  0 , 0, FALSE , work->heapId );
+  
+  GFL_ARC_CloseDataHandle( arcHandle );
+}
+
+
+
+#pragma mark [>proc func
+static GFL_PROC_RESULT MB_CAPTURE_ProcInit( GFL_PROC * proc, int * seq , void *pwk, void *mywk );
+static GFL_PROC_RESULT MB_CAPTURE_ProcMain( GFL_PROC * proc, int * seq , void *pwk, void *mywk );
+static GFL_PROC_RESULT MB_CAPTURE_ProcTerm( GFL_PROC * proc, int * seq , void *pwk, void *mywk );
+
+GFL_PROC_DATA MultiBootCapture_ProcData =
+{
+  MB_CAPTURE_ProcInit,
+  MB_CAPTURE_ProcMain,
+  MB_CAPTURE_ProcTerm
+};
+
+//--------------------------------------------------------------
+//  初期化
+//--------------------------------------------------------------
+static GFL_PROC_RESULT MB_CAPTURE_ProcInit( GFL_PROC * proc, int * seq , void *pwk, void *mywk )
+{
+  MB_CAPTURE_WORK *work;
+  MB_CAPTURE_INIT_WORK *initWork;
+  
+  if( pwk == NULL )
+  {
+//マルチブート用きり分け
+#ifndef MULTI_BOOT_MAKE  //通常時処理
+    const HEAPID parentHeap = GFL_HEAPID_APP;
+#else                    //DL子機時処理
+    const HEAPID parentHeap = HEAPID_MULTIBOOT;
+#endif //MULTI_BOOT_MAKE
+    u8 i;
+    GFL_HEAP_CreateHeap( parentHeap , HEAPID_MB_BOX, 0x40000 );
+    
+    work = GFL_PROC_AllocWork( proc, sizeof(MB_CAPTURE_WORK), parentHeap );
+    initWork = GFL_HEAP_AllocClearMemory( HEAPID_MB_BOX , sizeof(MB_CAPTURE_INIT_WORK) );
+
+    initWork->parentHeap = parentHeap;
+    initWork->cardType = CARD_TYPE_DUMMY;
+    for( i=0;i<MB_CAP_POKE_NUM;i++ )
+    {
+      initWork->ppp[i] = GFL_HEAP_AllocClearMemory( parentHeap , POKETOOL_GetPPPWorkSize() );
+      PPP_Setup( initWork->ppp[i] , 
+                 GFUser_GetPublicRand0(493)+1 ,
+                 GFUser_GetPublicRand0(100)+1 ,
+                 PTL_SETUP_ID_AUTO );
+    }
+    work->initWork = initWork;
+  }
+  else
+  {
+    initWork = pwk;
+    GFL_HEAP_CreateHeap( initWork->parentHeap , HEAPID_MB_BOX, 0x40000 );
+    work = GFL_PROC_AllocWork( proc, sizeof(MB_CAPTURE_WORK), HEAPID_MULTIBOOT );
+    work->initWork = pwk;
+  }
+  
+
+  work->heapId = HEAPID_MB_BOX;
+  
+  MB_CAPTURE_Init( work );
+  
+  return GFL_PROC_RES_FINISH;
+}
+
+//--------------------------------------------------------------
+//  開放
+//--------------------------------------------------------------
+static GFL_PROC_RESULT MB_CAPTURE_ProcTerm( GFL_PROC * proc, int * seq , void *pwk, void *mywk )
+{
+  MB_CAPTURE_WORK *work = mywk;
+  
+  MB_CAPTURE_Term( work );
+
+  if( pwk == NULL )
+  {
+    u8 i;
+    for( i=0;i<MB_CAP_POKE_NUM;i++ )
+    {
+      GFL_HEAP_FreeMemory( work->initWork->ppp[i] );
+    }
+    GFL_HEAP_FreeMemory( work->initWork );
+  }
+
+  GFL_PROC_FreeWork( proc );
+  GFL_HEAP_DeleteHeap( HEAPID_MB_BOX );
+
+  return GFL_PROC_RES_FINISH;
+}
+
+//--------------------------------------------------------------
+//  ループ
+//--------------------------------------------------------------
+static GFL_PROC_RESULT MB_CAPTURE_ProcMain( GFL_PROC * proc, int * seq , void *pwk, void *mywk )
+{
+  MB_CAPTURE_WORK *work = mywk;
+  const BOOL ret = MB_CAPTURE_Main( work );
+  
+  if( ret == TRUE )
+  {
+    return GFL_PROC_RES_FINISH;
+  }
+  return GFL_PROC_RES_CONTINUE;
+}
+
+
+
---
+++ resource/multiboot/capture/ncg2imd.rb
@@ -0,0 +1,282 @@
+require '../../../tools/ncg2imd/ncg_parser'
+require '../../../tools/ncg2imd/ncl_parser'
+require '../../../tools/ncg2imd/rewrite_imd'
+
+def GetFileName(pathfile)
+	last = pathfile.rindex("/")
+	
+	file = pathfile.dup
+  if( last != nil )
+  	file[0,last+1] = ""
+  end
+	return file
+end
+
+def GetAddPath(path,word)
+	size = path.size
+	last = path.rindex(word)
+  if( last != nil )
+  	del_size = size-last
+  	add_path = path.dup
+  	add_path[last,del_size] = ""
+  else
+    add_path = "."
+  end
+	return add_path
+end
+
+def FixPalText(inPaletteData, ary)
+	i = 0
+	while i < 2
+		n = 0
+		str = ""
+		while n < 16
+			if (n == 0)||(n==8) then
+				str += "\n\t\t"
+			end
+			col = sprintf("%04x ",inPaletteData[i*16+n])
+			str += col
+			n+=1
+		end
+		str += "\n\t"
+		ary << str
+		i+=1
+	end
+end
+
+class FIX_TEX_DATA_MAKER
+	def initialize(w,h,flg = TRUE)
+		line_num = h*8
+		if flg == TRUE then
+			@LineData = (0...line_num).map{ Array.new }
+		else
+			@LineData = []
+		end
+		@TexW = w		#キャラクタサイズ指定
+		@TexH = h		#キャラクタサイズ指定
+	end
+
+	def PushData(inTex)
+		char_no = 0
+		char_floor = 0
+		
+		inTex.each { | char |
+			space_flg = 0
+			char_line = 0
+			dot_count = 0
+			line_ofs = char_floor*8
+			char.each { | data |
+				#2ドット分処理（16色のとき限定）
+				dot_count+=2
+	
+				#現在処理中のラインを計算
+				line = line_ofs + char_line
+	
+				#下位4ビットを取得
+				low = (data & 0x0f)
+				#上位4ビットを取得
+				hi = ((data>>4) & 0x0f)
+				#文字列として、データを格納
+				new_str = sprintf("%x%x",hi,low)
+				#格納スペースのあるデータがあるなら、それを取得してそこにセット
+				#ない場合は、新規に作る
+				if space_flg == 1 then		#空きアリ
+					array = @LineData[line]
+					str = array.pop
+					str = new_str + str
+					array << str
+					#スペースが埋まった
+					space_flg = 0
+				else						#空きナシ
+					array = @LineData[line]
+					array << new_str
+					#スペースができた
+					space_flg = 1
+				end
+	
+				if dot_count%8 == 0 then
+					#ライン更新
+					char_line+=1
+				end
+			} #end data
+			char_no+=1
+			#処理キャラが、指定テクスチャのキャラ横サイズをオーバーしていれば処理キャラ階層を更新する
+			if (char_no % @TexW == 0) then
+				char_floor+=1
+			end
+		} #end char
+	end
+
+	def PushLineData(inLineData)
+		@LineData << inLineData
+	end
+
+	#反転テクスチャを作成
+	def MakeReverseTex
+		fix_data_maker = FIX_TEX_DATA_MAKER.new(@TexW, @TexH, FALSE)
+
+		@LineData.each { | line |
+			#ラインデータを逆転させて作る
+			rev = line.reverse
+			new_line = Array.new(line.length,"")
+			count = 0
+			rev.each { | s |
+				s_split = s.scan(/.{1,1}/m)
+				s_rev = s_split.reverse
+				new_line[count] = s_rev.join
+				count+=1
+			}
+			fix_data_maker.PushLineData(new_line)
+		}
+		return fix_data_maker
+	end
+
+	def MakeText
+		str = ""
+		count = 0
+		@LineData.each { | line |
+			str += "\n\t\t\t"
+			line.each { | l |
+				str += l
+				str += " "
+			}
+		}
+		str += "\n\t\t"
+		return str
+	end
+	
+	def Dump
+		@LineData.each { | line |
+			p line
+		}
+	end
+end
+
+#コマンド引数
+#NCGファイル、出力先
+ncg_file_name = ARGV.shift
+output_path = ARGV.shift
+
+ncg_parser = NCG_STRUCT.new
+ncg_parser.Pars(ncg_file_name)
+#相対パスの部分を抽出
+add_path = GetAddPath(ncg_file_name,"/")
+
+char_data = ncg_parser.GetCharData
+ncg_width = ncg_parser.GetWidth
+ncg_height = ncg_parser.GetHeight
+
+tex_data_array = []
+
+#floor:		NCGでマッピングされている1コマのキャラデータの羅列の階層
+#			NCGの横サイズを使い切っても、コマが足らない場合は、
+#			折り返して、1コマ目の下（1コマ縦64ドットだった場合は縦に64ドット下がった所）からマッピングされる
+#			この折り返し回数をfloorとする
+#pat:		横並びのコマに対して、何コマ目を処理しているかを示す
+#			コマ折り返し（floorの値が増える）が発生した場合、patは0に戻る
+#height:	1コマ内の横キャラ列において、何列目を処理しているかを示す
+#width:		1コマ内の横キャラ列において、何キャラ目を処理しているかを示す
+#byte_par_char:
+#			1キャラ何バイトかを示す
+
+byte_par_char = 0
+floor = 0
+pat = 0
+
+tex_num = 0
+
+if (1) then
+	byte_par_char = 32
+else
+	byte_par_char = 64
+end
+
+tex_num_max = 1
+
+while tex_num < tex_num_max
+	tex_data = []
+	floor_ofs = floor*ncg_height*ncg_width*byte_par_char
+	pat_ofs = pat*ncg_width*byte_par_char
+	
+	height = 0
+	#1コマ処理
+	while height < ncg_height
+		height_ofs = height*ncg_width*byte_par_char
+		width = 0
+		while width < ncg_width
+			char_dat = []
+			#キャラ取得
+			char_ofs = width*byte_par_char
+			shift = 0
+			while shift < byte_par_char
+				ofs = shift + floor_ofs + height_ofs + pat_ofs + char_ofs
+				char_dat << char_data[ofs]
+				shift+=1
+			end
+			width+=1
+			tex_data << char_dat
+		end
+		height+=1
+	end
+	#floor・patを更新
+	pat+=1
+	if ((pat+1)*ncg_width > ncg_width) then
+		pat = 0
+		floor+=1
+	end
+	
+	tex_num += 1
+	tex_data_array << tex_data
+end
+
+#テクスチャアレイを解釈してIMDに適用できる形に加工する
+maker = []
+
+tex_data_array.each { | tex |
+	fix_data_maker = FIX_TEX_DATA_MAKER.new(ncg_width, ncg_height)
+	fix_data_maker.PushData(tex)
+	maker << fix_data_maker
+}
+
+#maker.each { | i |  i.Dump}
+ncl_parser = NCL_STRUCT.new(1)
+
+ncl_parser.Pars(add_path + "/" + ncg_parser.GetPalFileName)
+
+palette_data = ncl_parser.GetPaletteData
+#パレットテキストを整形する
+fix_pal_text_ary = Array.new(0)
+FixPalText(palette_data,fix_pal_text_ary)
+#テクスチャイメージとパレットデータを書き換える
+tex_text_array = []
+maker.each { | i |
+	tex_text_array << i.MakeText
+}
+
+output_file_name = GetFileName( ncg_file_name.sub(/.ncg/,".imd") )
+
+rewrite = REWRITE_TEX.new(tex_text_array,fix_pal_text_ary)
+
+#テンプレートを選択
+template = ""
+if (ncg_width == 4 && ncg_height == 4) then
+	template = "dummy_32x32.imd"
+elsif (ncg_width == 8 && ncg_height == 4) then
+	template = "dummy_64x32.imd"
+elsif (ncg_width == 16 && ncg_height == 4) then
+	template = "dummy_128x32.imd"
+elsif (ncg_width == 4 && ncg_height == 8) then
+	template = "dummy_32x64.imd"
+elsif (ncg_width == 4 && ncg_height == 16) then
+	template = "dummy_32x128.imd"
+else
+  printf( "w[%d] h[%d]\n",ncg_width,ncg_height )
+	p "OutputFailed!!"
+	return
+end
+
+#org_imd_path = GetAddPath(add_path,/tsure_data/)
+org_imd_path = "./"
+#rewrite.OutPut(org_imd_path + template, output_path + "/" + output_file_name)
+rewrite.OutPut(org_imd_path + template, output_path + output_file_name)
+
