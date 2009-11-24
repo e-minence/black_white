49880c0706b1931b07706c74702d6abb558be48b
Genya Hosaka <hosaka@gamefreak.co.jp>
2009-11-24 03:28:11 +0000

    hudson用

---
+++ hudson_make.bat
@@ -0,0 +1,9 @@
+REM hudson用のmakeバッチ
+REM 用途    : prompt.bat cmd hudson_make.bat でhudsonから呼び出し
+REM 制作者  : hosaka_genya
+REM 日付    : 2009/11/24
+
+SET USERNAME=hudson
+@echo USERNAME = %USERNAME%
+make
+exit %errorlevel%
