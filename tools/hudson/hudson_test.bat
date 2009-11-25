df0523efe8082d6adb966c324b2ec9a8ea5b7c78
Genya Hosaka <hosaka@gamefreak.co.jp>
2009-11-25 04:12:37 +0000

    bugfix

--- tools/hudson/hudson_test.bat
+++ tools/hudson/hudson_test.bat
@@ -10,10 +10,10 @@ REM main.srl
 SET PATH_MAIN_SRL=%PROJECT_PROGDIR%/bin/ARM9-TS/Release/main.srl
 
 REM 実行タイムアウト(秒) >> hudsonで指定
-REM SET TIMEOUT_EXEC=60 
+SET TIMEOUT_EXEC=60 
 
 REM 対象DSのシリアルナンバー >> hudsonで指定
-REM SET SERIAL_NO=05093474
+SET SERIAL_NO=05093474
 
 REM 終了検出文字列(改行が来た時に先頭から判定)
 SET ABORT_STRING="  **** ASSERTION FAILED ! ****"
@@ -30,24 +30,22 @@ REM ================================
 loadrun -T %TIMEOUT_EXEC% -a %ABORT_STRING% -s %SERIAL_NO% %PATH_MAIN_SRL%
 echo ErrorLevel = %ERRORLEVEL%
 
+REM デバイスが見つからなかった
+if %ERRORLEVEL% == -1 goto _ERROR_END:
+
 REM タイムアウト判定
-if %ERRORLEVEL% == 205 goto _TIMEOUT:
+if %ERRORLEVEL% == 205 goto _END:
 
 REM 指定文字列検知で終了
-if %ERRORLEVEL% == 200 goto _ABORT_STRING:
-
-REM ================================
-REM タイムアウト
-REM ================================
-:_TIMEOUT
-  @echo timeout
-  goto _END:
+if %ERRORLEVEL% == 200 goto _ERROR_END:
   
 REM ================================
 REM 指定文字列検知終了
 REM ================================
-:_ABORT_STRING
+:_ERROR_END
   REM エラーコードを丸々返す
+  @echo hudson test is FAILED .
+  pause
   exit %ERRORLEVEL%
 
 REM ================================
@@ -55,5 +53,6 @@ REM 正常終了
 REM ================================
 :_END
   @echo hudson test is SUCCESS .
+  pause
   exit 0
 
