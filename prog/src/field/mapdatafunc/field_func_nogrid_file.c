7f4cd6b56b3610e3175e6b67e32645f9fc77f754
Sosuke Tamada <tamada@gamefreak.co.jp>
2009-11-25 03:24:01 +0000

    新規ノーグリッドフォーマット用関数追加

---
+++ prog/src/field/mapdatafunc/field_func_nogrid_file.c
@@ -0,0 +1,173 @@
+//============================================================================================
+/**
+ * @file	field_func_nogrid_file.c
+ * @brief	
+ * @author	
+ * @date	
+ */
+//============================================================================================
+#include <gflib.h>
+#include "system\main.h"
+
+#include "field_func_nogrid_file.h"
+
+
+#include "../field_g3dmap_exwork.h"	// GFL_G3D_MAP拡張ワーク
+
+#include "height.h"
+
+/// 全部をパックしたファイルのヘッダー
+typedef struct {
+    u16 DataID;         ////< NOGRIDPACK_HEADER
+    u16 dummy1;
+    u32 nsbmdOffset;    ///< ファイルの先頭からnsbmdの場所までのOFFSET
+    u32 positionOffset; ///< ファイルの先頭からポジションの場所までのOFFSET
+    u32 endPos;         ///< ファイルの先頭からポジションの最後までのOFFSET
+} NoGridPackHeaderSt;
+
+
+//============================================================================================
+/**
+ *
+ *
+ *
+ * @brief	データを処理する関数群
+ *
+ *
+ *
+ */
+//============================================================================================
+//============================================================================================
+/**
+ *
+ *
+ *
+ * @brief	マップデータ読み込み
+ *				※シーケンス0番はIDLING 1番はスタートＩＤＸで固定
+ *
+ *
+ */
+//============================================================================================
+enum {
+	FILE_LOAD_START = GFL_G3D_MAP_LOAD_START,
+	FILE_LOAD,
+	RND_CREATE,
+	TEX_TRANS,
+};
+
+BOOL FieldLoadMapData_NoGridFile( GFL_G3D_MAP* g3Dmap, void * exWork )
+{
+	GFL_G3D_MAP_LOAD_STATUS* ldst;
+	FLD_G3D_MAP_EXWORK* p_exwork;	// GFL_G3D_MAP拡張ワーク
+
+	GFL_G3D_MAP_GetLoadStatusPointer( g3Dmap, &ldst );
+
+	// 拡張ワーク取得
+	p_exwork = exWork;
+
+	switch( ldst->seq ){
+
+	case FILE_LOAD_START:
+		GFL_G3D_MAP_ResetLoadStatus(g3Dmap);
+
+		//モデルデータロード開始
+		{
+			u32		datID;
+			GFL_G3D_MAP_GetLoadDatID( g3Dmap, &datID );
+			GFL_G3D_MAP_StartFileLoad( g3Dmap, datID );
+		}
+		ldst->seq = FILE_LOAD;
+		break;
+
+	case FILE_LOAD:
+		if( GFL_G3D_MAP_ContinueFileLoad(g3Dmap) == FALSE ){
+			ldst->mdlLoaded = TRUE;
+			ldst->texLoaded = TRUE;
+			ldst->attrLoaded = TRUE;
+
+			ldst->seq = RND_CREATE;
+		}
+		break;
+
+	case RND_CREATE:
+		//レンダー作成
+		{
+			void*				mem;
+			NoGridPackHeaderSt*	fileHeader;
+			//ヘッダー設定
+			GFL_G3D_MAP_GetLoadMemoryPointer( g3Dmap, &mem );
+			fileHeader = (NoGridPackHeaderSt*)mem;
+
+			//モデルリソース設定
+			GFL_G3D_MAP_CreateResourceMdl(g3Dmap, (void*)((u32)mem + fileHeader->nsbmdOffset));
+			//配置オブジェクト設定
+			if( fileHeader->positionOffset != fileHeader->endPos ){
+				LayoutFormat* layout = (LayoutFormat*)((u32)mem + fileHeader->positionOffset);
+				PositionSt* objStatus = (PositionSt*)&layout->posData;
+        FIELD_BMODEL_MAN * bm = FLD_G3D_MAP_EXWORK_GetBModelMan(p_exwork);
+				GFL_G3D_MAP_GLOBALOBJ_ST status;
+				int i, count = layout->count;
+
+				for( i=0; i<count; i++ ){
+					FIELD_BMODEL_MAN_ResistMapObject( bm, g3Dmap, &objStatus[i], i );
+
+				}
+			}
+    }
+
+      
+#if 0
+    OS_Printf("DataID=%08x\n",fileHeader->DataID);         ////< DP3PACK_HEADER
+    OS_Printf("dummy1=%08x\n",fileHeader->dummy1);
+    OS_Printf("nsbmdOffset=%08x\n",fileHeader->nsbmdOffset);    ///< ファイルの先頭からnsbmdの場所までのOFFSET
+    OS_Printf("nsbtxOffset=%08x\n",fileHeader->nsbtxOffset);    ///< ファイルの先頭からnsbtxの場所までのOFFSET
+    OS_Printf("bhcOffset=%08x\n",fileHeader->bhcOffset);	    ///< ファイルの先頭からbhcの場所までのOFFSET
+    OS_Printf("vertexOffset=%08x\n",fileHeader->vertexOffset);   ///< ファイルの先頭から法線＋アトリビュートの場所までのOFFSET
+    OS_Printf("positionOffset=%08x\n",fileHeader->positionOffset); ///< ファイルの先頭からポジションの場所までのOFFSET
+    OS_Printf("endPos=%08x\n",fileHeader->endPos);         ///< ファイルの先頭からポジションの最後までのOFFSET
+#endif
+		GFL_G3D_MAP_MakeRenderObj( g3Dmap );
+
+
+
+		// 地面アニメーションの設定
+		if( FLD_G3D_MAP_EXWORK_IsGranm( p_exwork ) ){
+			FIELD_GRANM_WORK* p_granm;
+
+			p_granm = FLD_G3D_MAP_EXWORK_GetGranmWork( p_exwork );
+			FIELD_GRANM_WORK_Bind( p_granm, 
+					GFL_G3D_MAP_GetResourceMdl(g3Dmap), GFL_G3D_MAP_GetResourceTex(g3Dmap), 
+					GFL_G3D_MAP_GetRenderObj(g3Dmap) );
+		}
+
+		ldst->seq = TEX_TRANS;
+		break;
+
+	case TEX_TRANS:
+		{
+			ldst->seq = GFL_G3D_MAP_LOAD_IDLING;
+			return FALSE;
+		}
+		break;
+	}
+	return TRUE;
+}
+
+//============================================================================================
+/**
+ *
+ *
+ *
+ * @brief	３Ｄマップ情報取得
+ *
+ *
+ *
+ */
+//============================================================================================
+void FieldGetAttr_NoGridFile( GFL_G3D_MAP_ATTRINFO* attrInfo, const void* mapdata, 
+					const VecFx32* posInBlock, const fx32 map_width, const fx32 map_height )
+{
+  // アトリビュートも高さもない！
+  attrInfo->mapAttrCount = 0;
+}
+
