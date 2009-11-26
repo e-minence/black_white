910207959a1042be19e5a75b52fac2f49d6dd656
Nozomu Saito <saito_nozomu@gamefreak.co.jp>
2009-11-26 10:29:28 +0000

    //環境更新

---
+++ resource/fldmapdata/gimmick/gym_ice/gym_ice_local_def.h
@@ -0,0 +1,26 @@
+//======================================================================
+/**
+ * @file	gym_ice_local_def.h
+ * @brief	数値定義ファイル
+ * @note	このファイルはコンバーターから出力されます
+ *
+ */
+//======================================================================
+
+//スイッチのグリッド位置座標
+#define SW1_GX (26)
+#define SW1_GZ (17)
+#define SW2_GX (38)
+#define SW2_GZ (17)
+#define SW3_GX (20)
+#define SW3_GZ (21)
+//回転壁の位置座標
+#define WALL1_GX (10)
+#define WALL1_GY (0)
+#define WALL1_GZ (10)
+#define WALL2_GX (10)
+#define WALL2_GY (0)
+#define WALL2_GZ (10)
+#define WALL3_GX (10)
+#define WALL3_GY (0)
+#define WALL3_GZ (10)
