4c512038cfdcd2951bd83bed68727e254481badf
Nozomu Saito <saito_nozomu@gamefreak.co.jp>
2009-11-26 09:49:06 +0000

    //SE追加

---
+++ prog/src/field/gym_insect_se_def.h
@@ -0,0 +1,16 @@
+//======================================================================
+/**
+ * @file  gym_insect_se_def.h
+ * @author  GAMEFREAK inc.
+ *
+ * SEíËã`
+ */
+//======================================================================
+#pragma once
+
+#define GYM_INSECT_SE_WALL_PUSH  (SEQ_SE_FLD_51)  //ï«ÇâüÇµë±ÇØÇÈ
+#define GYM_INSECT_SE_WALL_THROUGH  (SEQ_SE_FLD_52)    //ï«ÇÇ∑ÇËî≤ÇØÇÈèuä‘
+#define GYM_INSECT_SE_WALL_RETURN  (SEQ_SE_FLD_53)     //ï«Ç…íeÇ©ÇÍÇΩ
+#define GYM_INSECT_SE_WALL_TOUCH  (SEQ_SE_FLD_54)     //ï«Ç…êGÇÍÇΩ
+#define GYM_INSECT_SE_WALL_OPEN  (SEQ_SE_FLD_57)    //çÚÇ™ìÆçÏÇ∑ÇÈ
+
