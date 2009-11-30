1370f333220b09f5fc61fd83bd624528a95e2dda
Nozomu Saito <saito_nozomu@gamefreak.co.jp>
2009-11-30 06:35:37 +0000

    //定義ファイル追加

---
+++ prog/src/field/pl_boat_def.h
@@ -0,0 +1,13 @@
+//======================================================================
+/**
+ * @file  pl_boat_def.h
+ * @brief  —V——‘DŠÖ˜A’è‹`
+ * @author  Saito
+ */
+//======================================================================
+
+#pragma once
+
+#define PL_TR_SEARCH_TYPE_LEFT  (0)
+#define PL_TR_SEARCH_TYPE_RIGHT  (1)
+
