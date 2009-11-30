7dcfa83d32923caad862725a111b7ed38f6cc245
Toshihiro Obata <obata_toshihiro@gamefreak.co.jp>
2009-11-30 02:22:07 +0000

    移動床イベントのソースを整理

--- prog/src/field/event_autoway.c
+++ prog/src/field/event_autoway.c
@@ -1,4 +1,4 @@
-//////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////
 /**
  * @brief  移動床による強制移動イベント
  * @file   event_autoway.c
@@ -7,7 +7,7 @@
  *
  * @todo SEをあてる
  */
-//////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////
 #include <gflib.h>
 #include "gamesystem/gamesystem.h"
 #include "gamesystem/game_event.h"
@@ -20,12 +20,9 @@
 #include "map_attr_def.h"  // for MATTR_xxxx
 
 
-//====================================================================================================
+//=============================================================================================
 // ■定数
-//====================================================================================================
-#define FRAME_PER_GRID (8)                             // 1グリッドの移動にかかる時間
-#define FRAME_PER_DIRCHENGE (FRAME_PER_GRID/DIR_MAX4)  // 向きの変更間隔
-
+//=============================================================================================
 // 処理シーケンス
 enum
 {
@@ -35,36 +32,102 @@ enum
 };
 
 
-//====================================================================================================
+//=============================================================================================
+// ■移動床
+//=============================================================================================
+typedef struct 
+{
+  MAPATTR_VALUE attrval;  // 起動アトリビュート
+  u16              acmd;  // 自機に適用するアニメーションコマンド( AC_xxxx )
+  u16           moveDir;  // 移動方向( DIR_xxxx )
+  u16           rotWait;  // 回転時の向き変更間隔
+
+} AUTOWAY;
+
+//---------------------------------------------------------------------------------------------
+/**
+ * @brief 移動床データ
+ */
+//---------------------------------------------------------------------------------------------
+static const AUTOWAY autoway[] = 
+{
+  {MATTR_MOVEF_LEFT,  AC_WALK_L_8F, DIR_LEFT,  2},
+  {MATTR_MOVEF_RIGHT, AC_WALK_R_8F, DIR_RIGHT, 2},
+  {MATTR_MOVEF_UP,    AC_WALK_U_8F, DIR_UP,    2},
+  {MATTR_MOVEF_DOWN,  AC_WALK_D_8F, DIR_DOWN,  2},
+};
+
+//---------------------------------------------------------------------------------------------
+/**
+ * @brief 移動データを取得する
+ *
+ * @param attrval アトリビュート
+ *
+ * @return 指定したアトリビュートに対応する移動データ
+ *         対応するデータが存在しない場合, NULL
+ */
+//---------------------------------------------------------------------------------------------
+static const AUTOWAY* GetAutoWay( MAPATTR_VALUE attrval )
+{
+  int i;
+  for(i=0; i<NELEMS(autoway); i++ )
+  {
+    if( autoway[i].attrval == attrval )
+    {
+      return &autoway[i];
+    }
+  }
+  return NULL;
+}
+
+//---------------------------------------------------------------------------------------------
+/**
+ * @brief 移動床アトリビュートかどうかを判定する
+ *
+ * @param attrval アトリビュート
+ *
+ * @return 指定アトリビュートが移動床なら TRUE, そうでないなら FALSE
+ */
+//---------------------------------------------------------------------------------------------
+static BOOL IsAutoWay( MAPATTR_VALUE attrval )
+{
+  return (GetAutoWay(attrval) != NULL);
+}
+
+
+//=============================================================================================
 // ■イベントワーク
-//====================================================================================================
+//=============================================================================================
 typedef struct
 {
   GAMESYS_WORK*      gsys;  // ゲームシステム
   FIELDMAP_WORK* fieldmap;  // フィールドマップ
   FIELD_PLAYER*    player;  // 自機
   MMDL*              mmdl;  // 自機の動作モデル
-  u16             moveDir;  // 移動方向
+  const AUTOWAY*  autoWay;  // 適用中の移動床データ
   u16              rotDir;  // 自機の向き
   u32            rotCount;  // 回転カウンタ
 
 } EVENT_WORK;
 
 
-//====================================================================================================
+//=============================================================================================
 // ■非公開関数のプロトタイプ宣言
-//====================================================================================================
+//=============================================================================================
+static u16 GetAcmd( const EVENT_WORK* work );
+static u16 GetMoveDir( const EVENT_WORK* work );
+static u16 GetRotWait( const EVENT_WORK* work );
+static MAPATTR_VALUE GetAttribute( const EVENT_WORK* work );
 static void UpdateRotDir( EVENT_WORK* work );
-static void UpdateMoveDir( EVENT_WORK* work, MAPATTR_VALUE attrval );
 static void MoveStart( EVENT_WORK* work );
-static MAPATTR_VALUE GetAttribute( EVENT_WORK* work );
 static void AttributeCheck( EVENT_WORK* work );
 static BOOL HitCheck( EVENT_WORK* work );
+static void ExitEvent( EVENT_WORK* work );
 
 
-//====================================================================================================
+//=============================================================================================
 // ■イベント処理関数
-//====================================================================================================
+//=============================================================================================
 static GMEVENT_RESULT AutoMove( GMEVENT* event, int* seq, void* wk )
 {
   EVENT_WORK* work = (EVENT_WORK*)wk;
@@ -74,14 +137,10 @@ static GMEVENT_RESULT AutoMove( GMEVENT* event, int* seq, void* wk )
   // イベント開始
   case SEQ_START:
     // 足元が移動床でなければ終了
+    if( !IsAutoWay( GetAttribute(work) ) )
     {
-      MAPATTR_VALUE attrval = GetAttribute( work );
-      if( (attrval != MATTR_MOVEF_LEFT) && (attrval != MATTR_MOVEF_RIGHT) &&
-          (attrval != MATTR_MOVEF_UP)   && (attrval != MATTR_MOVEF_DOWN) )
-      { 
-        *seq = SEQ_EXIT;
-        break;
-      }
+      *seq = SEQ_EXIT;
+      break;
     }
     *seq = SEQ_MAIN;
     break;
@@ -106,21 +165,18 @@ static GMEVENT_RESULT AutoMove( GMEVENT* event, int* seq, void* wk )
     break;
   // イベント終了
   case SEQ_EXIT:
-    // 変更したステータスビットを元に戻す
-    MMDL_OffStatusBit( work->mmdl, MMDL_STABIT_PAUSE_DIR );
-    // 進行方向を向く
-    MMDL_SetDirDisp( work->mmdl, work->moveDir );
+    ExitEvent( work );
     return GMEVENT_RES_FINISH;
   }
   return GMEVENT_RES_CONTINUE;
 }
 
 
-//====================================================================================================
+//=============================================================================================
 // ■外部公開関数
-//====================================================================================================
+//=============================================================================================
 
-//----------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------
 /**
  * @brief 移動床イベントを作成する
  *
@@ -130,8 +186,9 @@ static GMEVENT_RESULT AutoMove( GMEVENT* event, int* seq, void* wk )
  *
  * @return 移動床イベント
  */
-//----------------------------------------------------------------------------------------------------
-GMEVENT* EVENT_PlayerMoveOnAutoWay( GMEVENT* parent, GAMESYS_WORK* gsys, FIELDMAP_WORK* fieldmap )
+//---------------------------------------------------------------------------------------------
+GMEVENT* EVENT_PlayerMoveOnAutoWay( GMEVENT* parent, 
+                                    GAMESYS_WORK* gsys, FIELDMAP_WORK* fieldmap )
 {
   GMEVENT* event;
   EVENT_WORK* work;
@@ -144,79 +201,63 @@ GMEVENT* EVENT_PlayerMoveOnAutoWay( GMEVENT* parent, GAMESYS_WORK* gsys, FIELDMA
   work->fieldmap = fieldmap;
   work->player   = FIELDMAP_GetFieldPlayer( fieldmap );
   work->mmdl     = FIELD_PLAYER_GetMMdl( work->player );
-  work->moveDir  = DIR_LEFT;
+  work->autoWay  = 0;
   work->rotDir   = MMDL_GetDirDisp( work->mmdl );
   work->rotCount = 0;
   return event;
 } 
 
 
-//====================================================================================================
+//=============================================================================================
 // ■非公開関数のプロトタイプ宣言
-//====================================================================================================
+//=============================================================================================
 
-//----------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------
 /**
- * @brief 自機の向きを更新する
- *
- * @param work    イベントワーク
- */
-//----------------------------------------------------------------------------------------------------
-static void UpdateRotDir( EVENT_WORK* work )
-{ 
-  if( ++work->rotCount % FRAME_PER_DIRCHENGE == 0 )
-  {
-    // 更新
-    work->rotDir = (work->rotDir + 1) % DIR_MAX4;
-    // 動作モデルに反映
-    MMDL_OffStatusBit( work->mmdl, MMDL_STABIT_PAUSE_DIR );
-    MMDL_SetDirDisp( work->mmdl, work->rotDir );
-    MMDL_OnStatusBit( work->mmdl, MMDL_STABIT_PAUSE_DIR );
-  }
-}
-
-//----------------------------------------------------------------------------------------------------
-/**
- * @brief 移動方向を更新する
- *
- * @param work    イベントワーク
- * @param attrval 足元のアトリビュート
- */
-//----------------------------------------------------------------------------------------------------
-static void UpdateMoveDir( EVENT_WORK* work, MAPATTR_VALUE attrval )
-{
-  switch( attrval ) 
-  {
-  case MATTR_MOVEF_RIGHT: work->moveDir = DIR_RIGHT; break;
-  case MATTR_MOVEF_LEFT:  work->moveDir = DIR_LEFT;  break;
-  case MATTR_MOVEF_UP:    work->moveDir = DIR_UP;    break;
-  case MATTR_MOVEF_DOWN:  work->moveDir = DIR_DOWN;  break;
-  }
-}
-
-//----------------------------------------------------------------------------------------------------
-/**
- * @brief 移動を開始する
+ * @brief 現在参照中の移動データを取得する
  *
  * @param work イベントワーク
+ *
+ * @return 現在参照中の移動データ(アニメーションコマンド AC_xxxx)
  */
-//----------------------------------------------------------------------------------------------------
-static void MoveStart( EVENT_WORK* work )
-{ 
-  switch( work->moveDir )
-  {
-  case DIR_LEFT:  MMDL_SetAcmd( work->mmdl, AC_WALK_L_8F ); break;
-  case DIR_RIGHT: MMDL_SetAcmd( work->mmdl, AC_WALK_R_8F ); break;
-  case DIR_UP:    MMDL_SetAcmd( work->mmdl, AC_WALK_U_8F ); break;
-  case DIR_DOWN:  MMDL_SetAcmd( work->mmdl, AC_WALK_D_8F ); break;
-  default:        return;
-  }
-
-  // 移動アニメーションによる向きの変更を一時停止
-  MMDL_OnStatusBit( work->mmdl, MMDL_STABIT_PAUSE_DIR );
+//---------------------------------------------------------------------------------------------
+static u16 GetAcmd( const EVENT_WORK* work )
+{
+  if( !work->autoWay ){ return 0; }
+  return work->autoWay->acmd;
 }
 
-//----------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------
+/**
+ * @brief 現在参照中の移動データを取得する
+ *
+ * @param work イベントワーク
+ *
+ * @return 現在参照中の移動データ(移動方向 DIR_xxxx)
+ */
+//---------------------------------------------------------------------------------------------
+static u16 GetMoveDir( const EVENT_WORK* work )
+{
+  if( !work->autoWay ){ return 0; }
+  return work->autoWay->moveDir;
+}
+
+//---------------------------------------------------------------------------------------------
+/**
+ * @brief 現在参照中の移動データを取得する
+ *
+ * @param work イベントワーク
+ *
+ * @return 現在参照中の移動データ(回転待ち時間)
+ */
+//---------------------------------------------------------------------------------------------
+static u16 GetRotWait( const EVENT_WORK* work )
+{
+  if( !work->autoWay ){ return 0; }
+  return work->autoWay->rotWait;
+}
+
+//---------------------------------------------------------------------------------------------
 /**
  * @brief 足元のアトリビュートを取得する
  *
@@ -224,8 +265,8 @@ static void MoveStart( EVENT_WORK* work )
  *
  * @return 主人公の足元のアトリビュート
  */
-//----------------------------------------------------------------------------------------------------
-static MAPATTR_VALUE GetAttribute( EVENT_WORK* work )
+//---------------------------------------------------------------------------------------------
+static MAPATTR_VALUE GetAttribute( const EVENT_WORK* work )
 {
   VecFx32 pos;
   MAPATTR attr;
@@ -240,25 +281,63 @@ static MAPATTR_VALUE GetAttribute( EVENT_WORK* work )
   return attrval;
 }
 
-//----------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------
+/**
+ * @brief 自機の向きを更新する
+ *
+ * @param work    イベントワーク
+ */
+//---------------------------------------------------------------------------------------------
+static void UpdateRotDir( EVENT_WORK* work )
+{ 
+  if( ++work->rotCount % GetRotWait(work) == 0 )
+  {
+    // 向きを変更
+    work->rotDir = (work->rotDir + 1) % DIR_MAX4;
+    // 動作モデルに反映
+    MMDL_OffStatusBit( work->mmdl, MMDL_STABIT_PAUSE_DIR );
+    MMDL_SetDirDisp( work->mmdl, work->rotDir );
+    MMDL_OnStatusBit( work->mmdl, MMDL_STABIT_PAUSE_DIR );
+  }
+}
+
+//---------------------------------------------------------------------------------------------
+/**
+ * @brief 移動を開始する
+ *
+ * @param work イベントワーク
+ */
+//---------------------------------------------------------------------------------------------
+static void MoveStart( EVENT_WORK* work )
+{ 
+  // アニメーションコマンドをセット
+  MMDL_SetAcmd( work->mmdl, GetAcmd(work) ); 
+
+  // 移動アニメーションによる向きの変更を一時停止
+  MMDL_OnStatusBit( work->mmdl, MMDL_STABIT_PAUSE_DIR );
+}
+
+//---------------------------------------------------------------------------------------------
 /**
  * @brief アトリビュートチェック
  *
  * @param work イベントワーク
  */
-//----------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------
 static void AttributeCheck( EVENT_WORK* work )
 {
+  const AUTOWAY* new_way;
   MAPATTR_VALUE attrval;
 
   // 足元のアトリビュートを取得
   attrval = GetAttribute( work );
 
-  // 移動方向を更新
-  UpdateMoveDir( work, attrval );
+  // 参照する移動データを更新
+  new_way = GetAutoWay( attrval );
+  if( new_way ){ work->autoWay = new_way; }
 }
 
-//----------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------
 /**
  * @brief 衝突チェック
  *
@@ -266,10 +345,29 @@ static void AttributeCheck( EVENT_WORK* work )
  *
  * @return 衝突したら TRUE, そうでなければ FALSE
  */
-//----------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------
 static BOOL HitCheck( EVENT_WORK* work )
 {
   // 進行方向のグリッドをチェック
-  u32 hitbit = MMDL_HitCheckMoveDir( work->mmdl, work->moveDir );
+  u32 hitbit = MMDL_HitCheckMoveDir( work->mmdl, GetMoveDir(work) );
   return (hitbit != MMDL_MOVEHITBIT_NON);
+}
+
+//---------------------------------------------------------------------------------------------
+/**
+ * @brief イベント終了処理
+ *
+ * @param work イベントワーク
+ */
+//---------------------------------------------------------------------------------------------
+static void ExitEvent( EVENT_WORK* work )
+{
+  // 変更したステータスビットを元に戻す
+  MMDL_OffStatusBit( work->mmdl, MMDL_STABIT_PAUSE_DIR );
+
+  // 進行方向を向く
+  if( work->autoWay )
+  {
+    MMDL_SetDirDisp( work->mmdl, GetMoveDir(work) );
+  }
 }
