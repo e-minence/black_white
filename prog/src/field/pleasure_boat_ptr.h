c66502cd7e3e2c7491f46ace0fc90ea4132b31ed
Nozomu Saito <saito_nozomu@gamefreak.co.jp>
2009-11-30 09:35:09 +0000

    //型定義ファイル追加

---
+++ prog/src/field/pleasure_boat_ptr.h
@@ -0,0 +1,12 @@
+//======================================================================
+/**
+ * @file  pleasure_boat_ptr.h
+ * @brief —V——‘D
+ * @author  Saito
+ */
+//======================================================================
+
+#pragma once
+
+typedef struct PL_BOAT_WORK_tag * PL_BOAT_WORK_PTR;
+
