31cf927473806409e393c134e7f41734e28081d1
Katsunori Suginaka <suginaka_katsunori@gamefreak.co.jp>
2009-11-30 03:36:44 +0000

    遊覧船イベント用、仮マップを追加致しました。

--- resource/fldmapdata/eventdata/tmp/c03r0801.h
+++ resource/fldmapdata/eventdata/tmp/c03r0801.h
@@ -8,5 +8,15 @@
 #ifndef ZONE_C03R0801_EVC_H
 #define ZONE_C03R0801_EVC_H
 
+#define C03R0801_CREW_01                   0
+#define C03R0801_CREW_02                   1
+#define C03R0801_CREW_03                   2
+#define C03R0801_CREW_04                   3
+#define C03R0801_CREW_05                   4
+#define C03R0801_VISITOR_01                5
+#define C03R0801_VISITOR_02                6
+#define C03R0801_VISITOR_03                7
+#define C03R0801_VISITOR_04                8
+#define C03R0801_VISITOR_05                9
 
 #endif //ZONE_C03R0801_EVC_H
