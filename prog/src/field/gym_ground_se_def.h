6780f552d31c7bcd9aeddff0524c4c838acc7c6b
Nozomu Saito <saito_nozomu@gamefreak.co.jp>
2009-11-30 02:41:50 +0000

    //SE定義ファイル追加

---
+++ prog/src/field/gym_ground_se_def.h
@@ -0,0 +1,15 @@
+//======================================================================
+/**
+ * @file  gym_ground_se_def.h
+ * @author  GAMEFREAK inc.
+ *
+ * SE定義
+ */
+//======================================================================
+#pragma once
+
+#define GYM_GROUND_SE_LIFT_MOVE  (SEQ_SE_FLD_59)  //リフト移動
+#define GYM_GROUND_SE_LIFT_STOP  (SEQ_SE_FLD_60)    //リフト停止
+#define GYM_GROUND_SE_SWITCH  (SEQ_SE_FLD_61)     //スイッチ
+#define GYM_GROUND_SE_WALL_OPEN  (SEQ_SE_FLD_62)     //隔壁開く
+
