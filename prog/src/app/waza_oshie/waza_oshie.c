aa87bbe079fadc6103193ca02ea2b545796d816d
Hisashi Sogabe <sogabe@gamefreak.co.jp>
2009-11-30 04:19:33 +0000

    技教えテーブルの型を変更したことに伴う修正

--- prog/src/app/waza_oshie/waza_oshie.c
+++ prog/src/app/waza_oshie/waza_oshie.c
@@ -15,24 +15,7 @@
 #define WAZA_OSHIE_H_GLOBAL
 #include "app/waza_oshie.h"
 
-// --------poke_tool.hになかったので一旦こっちに持ってきた----------------
-// 技覚えテーブルに関する定義
-#define LEVELUPWAZA_OBOE_NUM  ( 20 )              ///<技20個
-#define LEVELUPWAZA_OBOE_MAX  ( LEVELUPWAZA_OBOE_NUM+2 )    ///<技20個＋終端コード:1
-#define LEVELUPWAZA_OBOE_BUFLEN ( (LEVELUPWAZA_OBOE_MAX+1)*2) ///<（技20個＋終端コード:1+4バイト境界補正:1）* 2バイト
-#define LEVELUPWAZA_OBOE_END  ( 0xffff )
-#define LEVELUPWAZA_LEVEL_MASK  ( 0xfe00 )
-#define LEVELUPWAZA_WAZA_MASK   ( 0x01ff )
-#define LEVELUPWAZA_LEVEL_SHIFT (      9 )
-// ------------------------------------------------------------------------
-
-
-#define LV_WAZA_OBOE_MAX  ( LEVELUPWAZA_OBOE_MAX )  // レベルアップで覚える技数 + 終端コード
-#define LV_WAZA_OBOE_END  ( LEVELUPWAZA_OBOE_END )  // 終端コード
-#define LV_WAZA_LV_MASK   ( LEVELUPWAZA_LEVEL_MASK )  // 覚えるレベル部分
-#define LV_WAZA_WAZA_MASK   ( LEVELUPWAZA_WAZA_MASK ) // 覚える技部分
-#define LV_WAZA_LV_SHIFT  ( LEVELUPWAZA_LEVEL_SHIFT ) // レベル取得シフト
-
+#define LV_WAZA_OBOE_MAX  ( POKEPER_WAZAOBOE_TABLE_ELEMS )  // レベルアップで覚える技数 + 終端コード
 
 //--------------------------------------------------------------------------------------------
 /**
@@ -66,9 +49,6 @@ void WAZAOSHIE_DataFree( WAZAOSHIE_DATA * dat )
   GFL_HEAP_FreeMemory( dat );
 }
 
-extern void POKE_PERSONAL_LoadWazaOboeTable( u16 mons_no, u16 form_no, u16* dst );
-
-
 //--------------------------------------------------------------------------------------------
 /**
  * 思い出し可能な技を取得
@@ -83,7 +63,7 @@ extern void POKE_PERSONAL_LoadWazaOboeTable( u16 mons_no, u16 form_no, u16* dst 
 //--------------------------------------------------------------------------------------------
 u16 * WAZAOSHIE_GetRemaindWaza( POKEMON_PARAM * pp, HEAPID heap )
 {
-  u16 * get;
+  POKEPER_WAZAOBOE_CODE * get;
   u16 * ret;
   u16 waza[4];
   u16 mons;
@@ -98,7 +78,7 @@ u16 * WAZAOSHIE_GetRemaindWaza( POKEMON_PARAM * pp, HEAPID heap )
     waza[i] = (u16)PP_Get( pp, ID_PARA_waza1+i, NULL );
   }
 
-  get = GFL_HEAP_AllocMemory( heap, LV_WAZA_OBOE_MAX * 2 );
+  get = GFL_HEAP_AllocMemory( heap, LV_WAZA_OBOE_MAX * sizeof( POKEPER_WAZAOBOE_CODE ) );
   ret = GFL_HEAP_AllocMemory( heap, LV_WAZA_OBOE_MAX * 2 );
 
 //  PokeFormNoWazaOboeDataGet( mons, form, get );
@@ -107,25 +87,24 @@ u16 * WAZAOSHIE_GetRemaindWaza( POKEMON_PARAM * pp, HEAPID heap )
   j = 0;
   for( i=0; i<LV_WAZA_OBOE_MAX; i++ ){
     // 終端コードチェック
-    if( get[i] == LV_WAZA_OBOE_END ){
+    if( POKEPER_WAZAOBOE_IsEndCode( get[i] ) ){
       ret[j] = WAZAOSHIE_TBL_MAX;
       break;
     // 現在のレベルで覚えられる技か
-    }else if( ((get[i]&LV_WAZA_LV_MASK)>>LV_WAZA_LV_SHIFT) > lv ){
+    }else if( POKEPER_WAZAOBOE_GetLevel( get[i] ) > lv ){
       continue;
     }else{
-      get[i] &= LV_WAZA_WAZA_MASK;
       // すでに覚えているか
       for( h=0; h<4; h++ ){
-        if( get[i] == waza[h] ){ break; }
+        if( POKEPER_WAZAOBOE_GetWazaID( get[i] ) == waza[h] ){ break; }
       }
       if( h == 4 ){
         // テーブルに同じ技がないか
         for( h=0; h<j; h++ ){
-          if( ret[h] == get[i] ){ break; }
+          if( ret[h] == POKEPER_WAZAOBOE_GetWazaID( get[i] ) ){ break; }
         }
         if( h == j ){
-          ret[j] = get[i];
+          ret[j] = POKEPER_WAZAOBOE_GetWazaID( get[i] );
           j++;
         }
       }
