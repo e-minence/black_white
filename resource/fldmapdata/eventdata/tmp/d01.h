bcb5f53c360de37bc6ed7e7beacde5f0f7705bf1
Masafumi Saito <saito_masafumi@gamefreak.co.jp>
2009-12-01 12:07:22 +0000

    イベント修正：削除予定OBJを排除

--- resource/fldmapdata/eventdata/tmp/d01.h
+++ resource/fldmapdata/eventdata/tmp/d01.h
@@ -8,8 +8,5 @@
 #ifndef ZONE_D01_EVC_H
 #define ZONE_D01_EVC_H
 
-#define D01_BOY2                           0
-#define D01_SUPPORT                        1
-#define D01_GYMLD                          2
 
 #endif //ZONE_D01_EVC_H
