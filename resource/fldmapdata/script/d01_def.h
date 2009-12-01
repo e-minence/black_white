e150f4b7a69bb88b620b8e346178a459649a018f
Masafumi Saito <saito_masafumi@gamefreak.co.jp>
2009-12-01 12:47:31 +0000

    スクリプト修正：D01のイベント削除に伴い、スクリプトラベルも削除

--- resource/fldmapdata/script/d01_def.h
+++ resource/fldmapdata/script/d01_def.h
@@ -5,8 +5,6 @@
 #define _D01_DEF_H_
 
 //スクリプトデータID定義
-#define SCRID_D01_TALKOBJ_BOY2 (1)
-#define SCRID_POS_D01_SUPPORT_01 (2)
-#define SCRID_D01_SUPPORT_02 (3)
+#define SCRID_D01_DUMMY (1)
 
 #endif //_D01_DEF_H_
