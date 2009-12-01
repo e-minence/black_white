11afc71a98bcde62816d5bb7ddc8bb5df86cf78c
Katsunori Suginaka <suginaka_katsunori@gamefreak.co.jp>
2009-12-01 08:42:11 +0000

    ギアステーションの接続を追加致しました。

--- resource/fldmapdata/eventdata/tmp/c04r0102.h
+++ resource/fldmapdata/eventdata/tmp/c04r0102.h
@@ -2,6 +2,7 @@
 #ifndef ZONE_C04R0102_EVD_H
 #define ZONE_C04R0102_EVD_H
 
+#define DOOR_ID_C04R0102_C04R0101_EXIT01  0
 
 #endif //ZONE_C04R0102_EVD_H
 //‚±‚Ìƒtƒ@ƒCƒ‹‚Í../data/c04r0102.mev‚©‚ç©“®¶¬‚³‚ê‚Ü‚µ‚½
