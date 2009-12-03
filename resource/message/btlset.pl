64c7dadef5ece4107ed28bc8d668d709e604a731
Masao Taya <taya@gamefreak.co.jp>
2009-12-03 06:32:56 +0000

    ポケモン２体パターンもコンバートできるようにした

--- resource/message/btlset.pl
+++ resource/message/btlset.pl
@@ -4,7 +4,7 @@
 # btl_set メッセージコンバータ
 #
 # バトル用メッセージデータ btl_set_src. gmm を読み込み、
-# 必要に応じて「あいての」「やせいの」などの文字列を負荷して btl_set.gmm を生成するための
+# 必要に応じて「あいての」「やせいの」などの文字列を付加して btl_set.gmm を生成するための
 # コンバータです。
 #
 # GameFreak taya
@@ -76,12 +76,16 @@ sub main {
 					else
 					{
 						if( $line =~/attribute name=\"コンバートタイプ\">/ ){
-							$blockMode = 2;
+							if( $line =~/そのまま/ ){
+								$blockMode = 2;
+							}else{
+								$blockMode = 3;
+							}
 							next;
 						}
 						push (@srcBlock, $line);
 						if( $line =~ /<\/row>/ ){
-							if( $blockMode == 1 )
+							if( $blockMode == 1 )	# デフォルト変換
 							{
 								my $id = &get_id_str( $srcBlock[0] );
 								my $bl_line;
@@ -118,13 +122,101 @@ sub main {
 									print OUTPUT $output_line;
 								}
 							}
-							elsif( $blockMode == 2 )
+							elsif( $blockMode == 2 )	# そのまま出力
 							{
 								my $bl_line;
 								foreach $bl_line (@srcBlock) {
 									print OUTPUT $bl_line;
 								}
 								@srcBlock = ();
+							}
+							elsif( $blockMode == 3 )	# ポケ名２体 -> 7パターン出力
+							{
+								my $id = &get_id_str( $srcBlock[0] );
+								my $bl_line;
+								my $output_line;
+								my $convMode = 0;
+								my $prefixCnt = 0;
+								my @subBlock = ();	
+								my $commentMode = 0;
+								foreach $bl_line (@srcBlock) {
+									print OUTPUT $bl_line;
+									if( $bl_line =~ /<comment>/ ){
+										$commentMode = 1;
+									}
+									if( $commentMode == 0 ){
+										push (@subBlock, $bl_line );
+									}
+									elsif( $bl_line =~ /<\/comment>/ ){
+										$commentMode = 0;
+									}
+								}
+
+								my $id_line = "\t<row id=\"" . $id . "_2Y\">\n";
+								$subBlock[0] = $id_line;
+								$convMode = $prefixCnt = 0;
+								foreach $bl_line (@subBlock) {
+									$output_line = &conv_str_ex( $bl_line, \$convMode, \$prefixCnt, 1, "やせいの　", "野生の　" );
+									print OUTPUT $output_line;
+								}
+								$convMode = @subBlock;
+								print "---$convMode \n";
+
+								$id_line = "\t<row id=\"" . $id . "_2E\">\n";
+								$subBlock[0] = $id_line;
+								$convMode = $prefixCnt = 0;
+								foreach $bl_line (@subBlock) {
+									$output_line = &conv_str_ex( $bl_line, \$convMode, \$prefixCnt, 1, "あいての　", "相手の　" );
+									print OUTPUT $output_line;
+								}
+								$convMode = @subBlock;
+								print "---$convMode \n";
+
+
+								my $id_line = "\t<row id=\"" . $id . "_Y\">\n";
+								$subBlock[0] = $id_line;
+								$convMode = $prefixCnt = 0;
+								foreach $bl_line (@subBlock) {
+									$output_line = &conv_str_ex( $bl_line, \$convMode, \$prefixCnt, 0, "やせいの　", "野生の　" );
+									print OUTPUT $output_line;
+								}
+								$convMode = @subBlock;
+								print "---$convMode \n";
+
+
+								my $id_line = "\t<row id=\"" . $id . "_Y2Y\">\n";
+								$subBlock[0] = $id_line;
+								$convMode = $prefixCnt = 0;
+								foreach $bl_line (@subBlock) {
+									$output_line = &conv_str_ex( $bl_line, \$convMode, \$prefixCnt, -1, "やせいの　", "野生の　" );
+									print OUTPUT $output_line;
+								}
+								$convMode = @subBlock;
+								print "---$convMode \n";
+
+
+								$id_line = "\t<row id=\"" . $id . "_E\">\n";
+								$subBlock[0] = $id_line;
+								$convMode = $prefixCnt = 0;
+								foreach $bl_line (@subBlock) {
+									$output_line = &conv_str_ex( $bl_line, \$convMode, \$prefixCnt, 0, "あいての　", "相手の　" );
+									print OUTPUT $output_line;
+								}
+								$convMode = @subBlock;
+								print "---$convMode \n";
+
+
+								$id_line = "\t<row id=\"" . $id . "_E2E\">\n";
+								$subBlock[0] = $id_line;
+								$convMode = $prefixCnt = 0;
+								foreach $bl_line (@subBlock) {
+									$output_line = &conv_str_ex( $bl_line, \$convMode, \$prefixCnt, -1, "あいての　", "相手の　" );
+									print OUTPUT $output_line;
+								}
+								$convMode = @subBlock;
+								print "---$convMode \n";
+
+
 							}
 							$blockMode = 0;
 						}
@@ -195,6 +287,77 @@ sub conv_str {
 		}
 	}
 
+
+	return $result;
+}
+
+
+sub conv_str_ex {
+	my $line = shift;
+	my $refMode = shift;
+	my $refPrefixCnt = shift;
+	my $tagIdx = shift;
+	my $add1 = shift;
+	my $add2 = shift;
+	my $result = $line;
+	my $i;
+
+	print "tagIdx=$tagIdx, mode=$$refMode, nidx=$$refPrefixCnt";
+
+	if( $$refMode == 0 )	# かな・未返還
+	{
+		my $p = index( $line, "[1:02:" );
+		if( $p >= 0 )
+		{
+			print " hit .. ";
+			if( ($tagIdx < 0)
+			||	($$refPrefixCnt == $tagIdx)
+			){
+				print "  match!";
+				substr( $result, $p, 0 ) = $add1;
+				if( $tagIdx >= 0 ){
+					$$refMode = 1;
+				}
+			}
+			$$refPrefixCnt++;
+		}
+		if( $tagIdx < 0 ){
+			if( $result =~ /<\/language>/ ){
+				print "\n";
+				$$refMode = 1;
+				$$refPrefixCnt = 0;
+			}
+		}
+	}
+	if( $$refMode == 1 )	# かな・変換済み
+	{
+		if( $result =~ /<\/language>/ ){
+			print "\n";
+			$$refMode = 2;
+			$$refPrefixCnt = 0;
+			return $result;
+		}
+	}
+	elsif( $$refMode == 2 )	# かな文終了 -> 漢字
+	{
+		my $p = index( $line, "[1:02:" );
+		if( $p >= 0 )
+		{
+			print " hit .. ";
+			if( ($tagIdx < 0)
+			||	($$refPrefixCnt == $tagIdx)
+			){
+				print "  match!";
+				substr( $result, $p, 0 ) = $add2;
+				if( $tagIdx > 0){
+					$$refMode = 3;
+				}
+			}
+			$$refPrefixCnt++;
+		}
+	}
+
+	print "\n";
 
 	return $result;
 }
