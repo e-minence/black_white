a49f63718fd38867aa7c94904ef3822939eb174f
Genya Hosaka <hosaka@gamefreak.co.jp>
2009-12-04 07:03:15 +0000

    hudsonによるmake all

---
+++ tools/hudson/hudson_make_all.bat
@@ -0,0 +1,45 @@
+@echo off
+REM =================================================================
+REM hudson > make clean > make 用バッチ
+REM 用途    : prompt.bat cmd hudson_make.bat でhudsonから呼び出し
+REM 制作者  : hosaka_genya
+REM 日付    : 2009/11/24
+REM =================================================================
+
+SET PATH_TOOLS=C:\.hudson\jobs\wb_make\workspace\pokemon_wb\tools\hudson
+
+REM ================================
+REM ユーザーネーム設定
+REM ================================
+SET USERNAME=hudson
+@echo USERNAME = %USERNAME%
+
+REM ================================
+REM  make clean
+REM ================================
+@echo ***** make clean *****
+make clean
+
+REM ================================
+REM  make
+REM ================================
+@echo ***** make *****
+make
+
+REM 正常終了判定
+if %ERRORLEVEL% == 0 goto END:
+
+REM ================================
+REM エラー処理
+REM ================================
+@echo errorlevel = %ERRORLEVEL%
+exit %ERRORLEVEL%
+
+REM ================================
+REM 正常終了
+REM ================================
+:END
+  @echo hudson make end
+
+
+
