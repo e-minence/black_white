68a31d1ee2272ec1d324cc21a4ac3a5fd31b2637
Miyuki Iwasawa <iwasawa@gamefreak.co.jp>
2009-12-07 09:35:54 +0000

    スクリプト 　Wi-FIカウンター用リソース追加

---
+++ resource/fldmapdata/script/wifi_counter_scr_def.h
@@ -0,0 +1,10 @@
+//wifi_counter_scr_def.h
+//このファイルはコンバータによって作成されました
+
+#ifndef _WIFI_COUNTER_SCR_DEF_H_
+#define _WIFI_COUNTER_SCR_DEF_H_
+
+//スクリプトデータID定義
+#define SCRID_WIFI_COUNTER (1)
+
+#endif //_WIFI_COUNTER_SCR_DEF_H_
---
+++ resource/fldmapdata/script/wifi_counter_scr_local.h
@@ -0,0 +1,27 @@
+//======================================================================
+//                wifi_counter_scr_local.h
+//
+//    スクリプトファイル：wifi_counter_scr用ローカル定数定義
+//
+//======================================================================
+
+///メインシーケンス
+#define SEQ_MAIN_MENU         (0)
+#define SEQ_COUNTER_EXP       (1)
+#define SEQ_RANDOM_MATCH      (2)
+#define SEQ_GLOBAL_TERMINAL   (3)
+#define SEQ_GEO_NET           (10)
+#define SEQ_GOODBYE           (10)
+#define SEQ_RETRY             (11)
+#define SEQ_EXIT              (12)
+#define SEQ_END               (0xFF)
+#define SEQ_CONNECT           (0)
+
+///グローバルターミナルシーケンス
+#define SEQ_SUB_GTERMINAL_MENU        (0)
+#define SEQ_SUB_GTERMINAL_GTS         (1)
+#define SEQ_SUB_GTERMINAL_BTL_VIDEO   (2)
+#define SEQ_SUB_GTERMINAL_MUSICAL     (3)
+#define SEQ_SUB_GTERMINAL_EXP         (4)
+#define SEQ_SUB_GTERMINAL_RETURN      (0xFF)
+
