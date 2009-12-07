8d019f11a24ff0b7e28fc1423f4caf293be5717f
Masafumi Saito <saito_masafumi@gamefreak.co.jp>
2009-12-07 11:39:27 +0000

    イベント修正：C01ポケセンにWiFi受付を追加。T01看板を簡易コマンドに置き換え

--- resource/fldmapdata/eventdata/tmp/c01pc0101.h
+++ resource/fldmapdata/eventdata/tmp/c01pc0101.h
@@ -15,8 +15,9 @@
 #define C01PC0101_SHOPM1_01                1
 #define C01PC0101_MAN2_01                  2
 #define C01PC0101_MIDDLEMAN2_01            3
-#define CONNECT_COUNTER_02                 4
+#define C01PC0101_CONNECT_02               4
 #define C01PC0101_SHOPM1_02                5
-#define CONNECT_COUNTER_01                 6
+#define C01PC0101_CONNECT_01               6
+#define C01PC0101_CONNECT_00               7
 
 #endif //ZONE_C01PC0101_EVC_H
