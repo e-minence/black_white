812d99d5033cb80e6fa4ffbab6688abd88fbd5f4
Nozomu Saito <saito_nozomu@gamefreak.co.jp>
2009-12-08 14:01:33 +0000

    //カメラパラメータ作成コンバート環境追加

---
+++ resource/fldmapdata/scr_cam_prm/cam_prm.rb
@@ -0,0 +1,65 @@
+$KCODE = "SJIS"
+
+csv_file = open(ARGV.shift,"r")
+
+h_file_name = ARGV.shift
+h_file = open(h_file_name,"w")
+
+list_file_name = "arc_list"
+list_file = open(list_file_name,"w")
+
+h_file.printf("//======================================================================\n")
+h_file.printf("/**\n")
+h_file.printf(" * @file	%s\n",h_file_name)
+h_file.printf(" * @brief	数値定義ファイル\n")
+h_file.printf(" * @note	このファイルはコンバーターから出力されます\n")
+h_file.printf(" * @note	スクリプトが参照します\n")
+h_file.printf(" *\n")
+h_file.printf(" */\n")
+h_file.printf("//======================================================================\n")
+h_file.printf("\n")
+
+h_file.printf("#pragma once\n")
+h_file.printf("\n")
+
+ary = Array.new
+
+#1行飛ばし
+csv_file.gets
+index = 0
+while line = csv_file.gets
+  #END検出
+  if line =~/^#END/ then
+		break
+	end
+  
+  column = line.split ","
+
+  h_file.printf("#define %s (%d)\n",column[0].upcase, index)
+
+  #バイナリデータ作成
+  bin_name = "bin/" + column[0].downcase + ".bin"
+  bin_file = open(bin_name,"wb")
+
+  list_file.printf("\"%s\"\n",bin_name)
+
+  ary.clear
+  ary << column[1].to_i   #pitch
+  ary << column[2].to_i   #yaw
+  ary << column[3].to_i   #distance
+  ary << 0   #pos x dummy
+  ary << 0   #pos y dummy
+  ary << 0   #pos z dummy
+
+  ary << 0   #shift check
+  ary << 1   #pitch check
+  ary << 1   #yaw check
+  ary << 1   #distance check
+  ary << 0   #Fovy check
+  ary << 0   #pos check
+  pack_str = ary.pack("S2L4L6")	#44byte
+  bin_file.write(pack_str)
+  
+  index += 1
+end
+
