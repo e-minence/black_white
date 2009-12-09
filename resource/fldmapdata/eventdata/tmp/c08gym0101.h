95b460473b41ae908712847b456ed7857023e30c
Emi Ando <ando@gamefreak.co.jp>
2009-12-09 08:49:09 +0000

    C08GYM0101：トレーナー配置

--- resource/fldmapdata/eventdata/tmp/c08gym0101.h
+++ resource/fldmapdata/eventdata/tmp/c08gym0101.h
@@ -11,5 +11,16 @@
 #ifndef ZONE_C08GYM0101_EVC_H
 #define ZONE_C08GYM0101_EVC_H
 
+#define C08GYM0101_DUMMY_01                0
+#define C08GYM0101_DUMMY_02                1
+#define C08GYM0101_DUMMY_03                2
+#define C08GYM0101_DUMMY_04                3
+#define C08GYM0101_DUMMY_05                4
+#define C08GYM0101_DUMMY_06                5
+#define C08GYM0101_DUMMY_07                6
+#define C08GYM0101_DUMMY_08                7
+#define C08GYM0101_DUMMY_09                8
+#define C08GYM0101_DUMMY_10                9
+#define C08GYM0101_DUMMY_11               10
 
 #endif //ZONE_C08GYM0101_EVC_H
