83740f05e5d1d6e16edc99cb2e8b02aa9a348787
Shinya Takahashi <takahashi@gamefreak.co.jp>
2009-12-10 12:23:48 +0000

    接続修正

--- resource/fldmapdata/eventdata/tmp/d08r0101.h
+++ resource/fldmapdata/eventdata/tmp/d08r0101.h
@@ -3,7 +3,6 @@
 #define ZONE_D08R0101_EVD_H
 
 #define DOOR_ID_D08R0101_D08_EXIT01       0
-#define DOOR_ID_D08R0101_D08R0201_EXIT01  1
 
 #endif //ZONE_D08R0101_EVD_H
 //‚±‚Ìƒtƒ@ƒCƒ‹‚Í../data/d08r0101.mev‚©‚ç©“®¶¬‚³‚ê‚Ü‚µ‚½
