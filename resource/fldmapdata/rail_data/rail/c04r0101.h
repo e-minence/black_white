a8229341fabb490a65c385b2986d1866b95a1ad1
Yukiko Hozumi <hozumi_yukiko@gamefreak.co.jp>
2009-12-10 11:53:39 +0000

    ギアステーションのレールデータ（仮）を設定しました。

--- resource/fldmapdata/rail_data/rail/c04r0101.h
+++ resource/fldmapdata/rail_data/rail/c04r0101.h
@@ -1,7 +1,15 @@
 #pragma once
 // out put rail_conv.pl
 // rail line name
+<<<<<<< .mine
+#define RE_LINE_2 (0)
+#define div_2_div_1_RE_LINE_0 (1)
+#define div_3_RE_LINE_2 (2)
+#define div_4_div_1_RE_LINE_0 (3)
+#define RE_LINE_4 (4)
+=======
 #define div_1_RE_LINE_0 (0)
 #define RE_LINE_2 (1)
 #define div_2_div_1_RE_LINE_0 (2)
 #define div_3_RE_LINE_2 (3)
+>>>>>>> .r14109
