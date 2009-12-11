8cbfa3030d16e70c919cb914c5076c609a6e020f
Shinya Takahashi <takahashi@gamefreak.co.jp>
2009-12-11 03:08:11 +0000

    人物配置

--- resource/fldmapdata/eventdata/tmp/c07gym0101.h
+++ resource/fldmapdata/eventdata/tmp/c07gym0101.h
@@ -9,7 +9,7 @@
 #ifndef ZONE_C07GYM0101_EVC_H
 #define ZONE_C07GYM0101_EVC_H
 
-#define C07GYM0101_LEADER7_01              0
+#define TR_KARATE_04                       0
 #define C07GYM0101_SUNGLASSES_01           1
 #define C07GYM0101_TOUMEI0_01              2
 #define C07GYM0101_TOUMEI0_02              3
@@ -20,5 +20,11 @@
 #define C07GYM0101_TOUMEI2_01              8
 #define C07GYM0101_TOUMEI2_02              9
 #define C07GYM0101_TOUMEI2_03             10
+#define C07GYM0101_LEADER7_01             11
+#define TR_BATTLEG_04                     12
+#define TR_KARATE_05                      13
+#define TR_BATTLEG_05                     14
+#define TR_BATTLEG_06                     15
+#define TR_KARATE_06                      16
 
 #endif //ZONE_C07GYM0101_EVC_H
