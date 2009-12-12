371e1f07da1e0836891fa9a56b8e821fe704df76
Katsunori Suginaka <suginaka_katsunori@gamefreak.co.jp>
2009-12-12 03:48:59 +0000

    C03P10・D04スクリプト修正を行いました。

--- resource/fldmapdata/eventdata/tmp/c03p10.h
+++ resource/fldmapdata/eventdata/tmp/c03p10.h
@@ -12,5 +12,20 @@
 #ifndef ZONE_C03P10_EVC_H
 #define ZONE_C03P10_EVC_H
 
+#define C03P10_BUSINESSMAN_01              0
+#define C03P10_BUSINESSMAN_02              1
+#define C03P10_BUSINESSMAN_03              2
+#define C03P10_BUSINESSMAN_04              3
+#define C03P10_BUSINESSMAN_05              4
+#define C03P10_BUSINESSMAN_06              5
+#define C03P10_BUSINESSMAN_07              6
+#define C03P10_BUSINESSMAN_08              7
+#define C03P10_BUSINESSMAN_09              8
+#define C03P10_BUSINESSMAN_10              9
+#define C03P10_OL_01                      10
+#define C03P10_OL_02                      11
+#define C03P10_OL_03                      12
+#define C03P10_OL_04                      13
+#define C03P10_OL_05                      14
 
 #endif //ZONE_C03P10_EVC_H
