868d689da9d6a4c25bebf434d5890001c36f63a6
Tomoya Takahashi <tomoya_takahashi@gamefreak.co.jp>
2009-12-12 13:23:50 +0000

    天気サウンドコミット

---
+++ prog/src/field/weather_snd.h
@@ -0,0 +1,53 @@
+//[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[
+/**
+ *	GAME FREAK inc.
+ *
+ *	@file		weather_snd.h
+ *	@brief  天気サウンド　
+ *	@author	tomoya takahashi
+ *	@date		2009.12.12
+ *
+ *	モジュール名：
+ */
+//]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]
+#pragma once
+
+#ifdef _cplusplus
+extern "C"{
+#endif
+
+
+#include "sound/pm_sndsys.h"
+#include "sound/wb_sound_data.sadl"
+
+//-----------------------------------------------------------------------------
+/**
+ *					定数宣言
+*/
+//-----------------------------------------------------------------------------
+
+// 雨SE
+#define WEATHER_SND_SE_RAIN   ( SEQ_SE_FLD_32 )
+
+// 大雨SE
+#define WEATHER_SND_SE_HIGHRAIN   ( SEQ_SE_FLD_31 )
+
+// 雷SE
+#define WEATHER_SND_SE_SPARK  ( SEQ_SE_FLD_33 )
+
+// 砂嵐SE
+#define WEATHER_SND_SE_STORM  ( SEQ_SE_FLD_84 )
+
+// 吹雪SE
+#define WEATHER_SND_SE_SNOWSTORM  ( SEQ_SE_FLD_84 )
+
+// あられSE
+#define WEATHER_SND_SE_ARARE  ( SEQ_SE_FLD_85 )
+
+
+#ifdef _cplusplus
+}	// extern "C"{
+#endif
+
+
+
