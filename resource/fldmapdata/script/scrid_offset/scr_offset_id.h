d9a9a4f655dbc33791f1fcc30f418f6c56be2352
Nozomu Saito <saito_nozomu@gamefreak.co.jp>
2009-12-14 09:12:58 +0000

    //姓名判断スクリプト追加

--- resource/fldmapdata/script/scrid_offset/scr_offset_id.h
+++ resource/fldmapdata/script/scrid_offset/scr_offset_id.h
@@ -3,7 +3,7 @@
  * @file  scr_offset_id.h
  * @bfief  スクリプトIDのオフセット定義
  * @author  tamada GAMEFREAK inc.
- * @date  2009 12/09 (Wed) 19:11
+ * @date  2009 12/14 (Mon) 17:39
  *
  * このファイルはコンバータにより自動生成されています
  */
@@ -23,7 +23,10 @@
 #define ID_POKECEN_SCR_OFFSET_END        (2199)
 
 #define ID_SODATEYA_SCR_OFFSET           (2200) //育て屋用スクリプト
-#define ID_SODATEYA_SCR_OFFSET_END       (2249)
+#define ID_SODATEYA_SCR_OFFSET_END       (2224)
+
+#define ID_NAMECHANGE_SCR_OFFSET         (2225) //姓名判断
+#define ID_NAMECHANGE_SCR_OFFSET_END     (2249)
 
 #define ID_EGGEVENT_SCR_OFFSET           (2250) //タマゴ孵化スクリプト
 #define ID_EGGEVENT_SCR_OFFSET_END       (2259)
