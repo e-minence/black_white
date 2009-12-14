25280ab8c58f374f702fd462c53b72991738238b
Nozomu Saito <saito_nozomu@gamefreak.co.jp>
2009-12-14 13:47:03 +0000

    //カットイン更新

--- resource/fld3d_ci/fldci_id_def.h
+++ resource/fld3d_ci/fldci_id_def.h
@@ -1,22 +1,25 @@
 //======================================================================
 /**
  * @file	fldci_id_def.h
- * @brief	フィールド3Dカットインナンバー定義
- * @author	Saito
- *
- * @note    このファイルはコンバータにより出力されます
+ * @brief	数値定義ファイル
+ * @note	このファイルはコンバーターから出力されます
+ * @note	スクリプトが参照します
  *
  */
 //======================================================================
 
 #pragma once
 
-#define FLDCIID_GYM01 (0)
-#define FLDCIID_GYM02 (1)
-#define FLDCIID_GYM03 (2)
-#define FLDCIID_GYM04 (3)
-#define FLDCIID_GYM05 (4)
-#define FLDCIID_GYM06 (5)
-#define FLDCIID_GYM07 (6)
-#define FLDCIID_GYM08 (7)
+#define FLDCIID_GET1 (0)
+#define FLDCIID_GET2 (1)
+#define FLDCIID_GYM01 (2)
 #define FLDCIID_POKE (3)
+#define FLDCIID_GYM02 (4)
+#define FLDCIID_GYM03 (5)
+#define FLDCIID_GYM04 (6)
+#define FLDCIID_GYM05 (7)
+#define FLDCIID_GYM06 (8)
+#define FLDCIID_GYM07 (9)
+#define FLDCIID_GYM08 (10)
+#define FLDCIID_FLY_OUT (11)
+#define FLDCIID_FLY_IN (12)
