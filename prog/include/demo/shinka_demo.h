9fff154ac90c4951c990c88e1282e18f5a23fc90
Hisashi Sogabe <sogabe@gamefreak.co.jp>
2009-12-15 06:13:21 +0000

    進化デモ（仮）作成

--- prog/include/demo/shinka_demo.h
+++ prog/include/demo/shinka_demo.h
@@ -16,4 +16,4 @@ typedef struct _SHINKA_DEMO_PARAM SHINKA_DEMO_PARAM;
 FS_EXTERN_OVERLAY(shinka_demo);
 extern  const GFL_PROC_DATA   ShinkaDemoProcData;
 extern  SHINKA_DEMO_PARAM*  SHINKADEMO_AllocParam( HEAPID heapID, const POKEPARTY* ppt, u16 after_mons_no, u8 pos, u8 cond );
-extern  BOOL                SHINKADEMO_CheckExecute( SHINKA_DEMO_PARAM* param );
+extern  void                SHINKADEMO_FreeParam( SHINKA_DEMO_PARAM* sdp );
