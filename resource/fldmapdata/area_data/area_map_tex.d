a6594b63a106ba86449e0ffef489e205e6ecf868
Hiroki Fuchino <fuchino@gamefreak.co.jp>
2009-12-16 06:41:48 +0000

    新規エリア　out48を追加（ホワイトフォレスト用）

--- resource/fldmapdata/area_data/area_map_tex.d
+++ resource/fldmapdata/area_data/area_map_tex.d
@@ -1134,6 +1134,30 @@ nsbtx_files/tex_set_out47_winter.nsbtx: src_imd_files/out00_tex_winter.imd src_i
 	@-$(TEX_MAG) -q -o marged_imd_files/tex_set_out47_winter.imd src_imd_files/out00_tex_winter.imd src_imd_files/out47_local_texset.imd src_imd_files/tex_griddmy.imd
 	@$(G3DCVTR) marged_imd_files/tex_set_out47_winter.imd -o nsbtx_files/tex_set_out47_winter.nsbtx -etex
 
+nsbtx_files/tex_set_out48_spring.nsbtx: src_imd_files/out00_tex_spring.imd src_imd_files/out48_local_texset.imd src_imd_files/tex_griddmy.imd
+	@echo src_imd_files/out00_tex_spring.imd + src_imd_files/out48_local_texset.imd Å® nsbtx_files/tex_set_out48_spring.nsbtx
+	@-rm marged_imd_files/tex_set_out48_spring.imd
+	@-$(TEX_MAG) -q -o marged_imd_files/tex_set_out48_spring.imd src_imd_files/out00_tex_spring.imd src_imd_files/out48_local_texset.imd src_imd_files/tex_griddmy.imd
+	@$(G3DCVTR) marged_imd_files/tex_set_out48_spring.imd -o nsbtx_files/tex_set_out48_spring.nsbtx -etex
+
+nsbtx_files/tex_set_out48_summer.nsbtx: src_imd_files/out00_tex_summer.imd src_imd_files/out48_local_texset.imd src_imd_files/tex_griddmy.imd
+	@echo src_imd_files/out00_tex_summer.imd + src_imd_files/out48_local_texset.imd Å® nsbtx_files/tex_set_out48_summer.nsbtx
+	@-rm marged_imd_files/tex_set_out48_summer.imd
+	@-$(TEX_MAG) -q -o marged_imd_files/tex_set_out48_summer.imd src_imd_files/out00_tex_summer.imd src_imd_files/out48_local_texset.imd src_imd_files/tex_griddmy.imd
+	@$(G3DCVTR) marged_imd_files/tex_set_out48_summer.imd -o nsbtx_files/tex_set_out48_summer.nsbtx -etex
+
+nsbtx_files/tex_set_out48_autumn.nsbtx: src_imd_files/out00_tex_autumn.imd src_imd_files/out48_local_texset.imd src_imd_files/tex_griddmy.imd
+	@echo src_imd_files/out00_tex_autumn.imd + src_imd_files/out48_local_texset.imd Å® nsbtx_files/tex_set_out48_autumn.nsbtx
+	@-rm marged_imd_files/tex_set_out48_autumn.imd
+	@-$(TEX_MAG) -q -o marged_imd_files/tex_set_out48_autumn.imd src_imd_files/out00_tex_autumn.imd src_imd_files/out48_local_texset.imd src_imd_files/tex_griddmy.imd
+	@$(G3DCVTR) marged_imd_files/tex_set_out48_autumn.imd -o nsbtx_files/tex_set_out48_autumn.nsbtx -etex
+
+nsbtx_files/tex_set_out48_winter.nsbtx: src_imd_files/out00_tex_winter.imd src_imd_files/out48_local_texset.imd src_imd_files/tex_griddmy.imd
+	@echo src_imd_files/out00_tex_winter.imd + src_imd_files/out48_local_texset.imd Å® nsbtx_files/tex_set_out48_winter.nsbtx
+	@-rm marged_imd_files/tex_set_out48_winter.imd
+	@-$(TEX_MAG) -q -o marged_imd_files/tex_set_out48_winter.imd src_imd_files/out00_tex_winter.imd src_imd_files/out48_local_texset.imd src_imd_files/tex_griddmy.imd
+	@$(G3DCVTR) marged_imd_files/tex_set_out48_winter.imd -o nsbtx_files/tex_set_out48_winter.nsbtx -etex
+
 nsbtx_files/tex_set_in01.nsbtx: src_imd_files/in00_texset.imd src_imd_files/in01_local_texset.imd src_imd_files/tex_griddmy.imd
 	@echo src_imd_files/in00_texset.imd + src_imd_files/in01_local_texset.imd Å® nsbtx_files/tex_set_in01.nsbtx
 	@-rm marged_imd_files/tex_set_in01.imd
