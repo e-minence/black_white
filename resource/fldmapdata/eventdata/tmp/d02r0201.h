4b853f51f9c273523a9aa17fdfa344d022698eda
Toshihiro Obata <obata_toshihiro@gamefreak.co.jp>
2009-12-16 05:53:34 +0000

    コミット忘れを追加

---
+++ resource/fldmapdata/eventdata/tmp/d02r0201.h
@@ -0,0 +1,12 @@
+//このファイルは../data/d02r0201.mevから自動生成されました
+#ifndef ZONE_D02R0201_EVD_H
+#define ZONE_D02R0201_EVD_H
+
+
+#endif //ZONE_D02R0201_EVD_H
+//このファイルは../data/d02r0201.mevから自動生成されました
+#ifndef ZONE_D02R0201_EVC_H
+#define ZONE_D02R0201_EVC_H
+
+
+#endif //ZONE_D02R0201_EVC_H
