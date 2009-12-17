8c0f40be580383bbe4d11a395525ad5693b3695d
Nozomu Saito <saito_nozomu@gamefreak.co.jp>
2009-12-17 12:18:51 +0000

    //パルパーク受付スクリプト追加

---
+++ resource/fldmapdata/script/palpark_scr_def.h
@@ -0,0 +1,10 @@
+//palpark_scr_def.h
+//このファイルはコンバータによって作成されました
+
+#ifndef _PALPARK_SCR_DEF_H_
+#define _PALPARK_SCR_DEF_H_
+
+//スクリプトデータID定義
+#define SCRID_PALPARK_RECEPTION (10290)
+
+#endif //_PALPARK_SCR_DEF_H_
