0faa56d8295d601687c833f6a4911b2088782d01
Kazumasa Iwao <iwao_kazumasa@gamefreak.co.jp>
2009-12-17 03:14:47 +0000

    パッチにパスが通ってなかったのを修正(有泉)

--- resource/musical/program/mus_prog_copy_arc.bat
+++ resource/musical/program/mus_prog_copy_arc.bat
@@ -1,3 +1,14 @@
+SET PROJECT_ROOT=../../..
+SET WBLIBDIR=%PROJECT_ROOT%/lib
+SET NITROSDK_ROOT=%WBLIBDIR%\TwlSDK
+SET NITROSYSTEM_ROOT=%WBLIBDIR%\TwlSystem
+SET NITROWIFI_ROOT=%WBLIBDIR%\TwlDWC
+SET NITRODWC_ROOT=%WBLIBDIR%\TwlDWC
+SET CWFOLDER=C:/Program Files/Freescale/CW for NINTENDO DSi V1.1
+
+PATH=c:\tools;%PROJECT_ROOT%;%PROJECT_ROOT%\tools;c:\tools\subversion\bin;c:\cygwin\bin;c:\tools\vim;C:\tools;%NITROSDK_ROOT%\tools\bin;%NITROSYSTEM_ROOT%\tools\bin;%PATH%;
+
+
 cp p01/mus_prog_data.narc ./mus_prog_data_01.narc
 cp p02/mus_prog_data.narc ./mus_prog_data_02.narc
 cp p03/mus_prog_data.narc ./mus_prog_data_03.narc
