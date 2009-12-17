fd916703f4e078522009a8a5bf13d549f1d93480
Tomoya Takahashi <tomoya_takahashi@gamefreak.co.jp>
2009-12-17 14:13:37 +0000

    コミット忘れ

---
+++ resource/fldmapdata/eventdata/tmp/bcblock.h
@@ -0,0 +1,16 @@
+//このファイルは../data/bcblock.mevから自動生成されました
+#ifndef ZONE_BCBLOCK_EVD_H
+#define ZONE_BCBLOCK_EVD_H
+
+#define DOOR_ID_BCBLOCK_BUILD00           0
+#define DOOR_ID_BCBLOCK_BUILD01           1
+#define DOOR_ID_BCBLOCK_BUILD02           2
+#define DOOR_ID_BCBLOCK_BUILD03           3
+
+#endif //ZONE_BCBLOCK_EVD_H
+//このファイルは../data/bcblock.mevから自動生成されました
+#ifndef ZONE_BCBLOCK_EVC_H
+#define ZONE_BCBLOCK_EVC_H
+
+
+#endif //ZONE_BCBLOCK_EVC_H
---
+++ resource/fldmapdata/eventdata/tmp/wfblock.h
@@ -0,0 +1,12 @@
+//このファイルは../data/wfblock.mevから自動生成されました
+#ifndef ZONE_WFBLOCK_EVD_H
+#define ZONE_WFBLOCK_EVD_H
+
+
+#endif //ZONE_WFBLOCK_EVD_H
+//このファイルは../data/wfblock.mevから自動生成されました
+#ifndef ZONE_WFBLOCK_EVC_H
+#define ZONE_WFBLOCK_EVC_H
+
+
+#endif //ZONE_WFBLOCK_EVC_H
