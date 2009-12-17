af43c5573193a4137a8c6cf27dc7637b0c682bc1
Masafumi Saito <saito_masafumi@gamefreak.co.jp>
2009-12-17 11:52:43 +0000

    イベントフラグ追加：パルパーク用初回会話フラグ

--- resource/fldmapdata/flagwork/flag_define.h
+++ resource/fldmapdata/flagwork/flag_define.h
@@ -162,7 +162,7 @@
 #define FE_D04R0201_KASEKITALK             146 /* 0x0092 D04遺跡でカセキくれる人に話しかけた */
 #define FE_C05PC0101_WAZA                  147 /* 0x0093 C05PC0101：ホモエドシティPC、なみのり入手フラグ */
 #define FE_C02R0301_POKETRADE              148 /* 0x0094 C02R0301のゲーム内交換フラグ */
-		/* not used   149(0x0095) */
+#define FE_PALPARK_FIRSTTALK               149 /* 0x0095 パルパーク受付の初回会話フラグ（説明有り） */
 		/* not used   150(0x0096) */
 		/* not used   151(0x0097) */
 		/* not used   152(0x0098) */
