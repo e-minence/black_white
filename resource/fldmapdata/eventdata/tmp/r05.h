5e05492e69b407fc851fd370ff62a509cdc7bd38
Shinya Takahashi <takahashi@gamefreak.co.jp>
2009-12-18 19:21:31 +0000

    トレーナー配置

--- resource/fldmapdata/eventdata/tmp/r05.h
+++ resource/fldmapdata/eventdata/tmp/r05.h
@@ -10,24 +10,21 @@
 #ifndef ZONE_R05_EVC_H
 #define ZONE_R05_EVC_H
 
-#define R05_TREE_01                        0
-#define R05_TREE_02                        1
-#define R05_TREE_04                        2
-#define TR_JUGGLING_01                     3
-#define R05_FLD_ITEM_03                    4
-#define R05_TREE_03                        5
-#define R05_FLD_ITEM_01                    6
-#define R05_FLD_ITEM_02                    7
-#define TR_BAKER_01                        8
-#define TR_BACKPACKERM_02                  9
-#define TR_ARTIST_01                      10
-#define TR_MUSICIAN_01                    11
-#define TR_BACKPACKERW_02                 12
-#define R05_RIVAL_01                      13
-#define R05_CHAMPION_01                   14
-#define R05_BOY3_01                       15
-#define R05_GIRL2_01                      16
-#define R05_LEADER4_01                    17
-#define R05_CAMERAMAN_01                  18
+#define TR_DANCER_01                       0
+#define TR_JUGGLING_01                     1
+#define R05_FLD_ITEM_03                    2
+#define R05_FLD_ITEM_01                    3
+#define R05_FLD_ITEM_02                    4
+#define TR_BAKER_01                        5
+#define TR_BACKPACKERM_02                  6
+#define TR_ARTIST_01                       7
+#define TR_MUSICIAN_01                     8
+#define TR_BACKPACKERW_02                  9
+#define R05_RIVAL_01                      10
+#define R05_CHAMPION_01                   11
+#define R05_BOY3_01                       12
+#define R05_GIRL2_01                      13
+#define R05_LEADER4_01                    14
+#define R05_CAMERAMAN_01                  15
 
 #endif //ZONE_R05_EVC_H
