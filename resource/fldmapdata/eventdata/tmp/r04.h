b29d29e540d1fcbf81a39ba3aa0cb6d0e4805ee8
Shinya Takahashi <takahashi@gamefreak.co.jp>
2009-12-18 07:52:02 +0000

    トレーナー配置

--- resource/fldmapdata/eventdata/tmp/r04.h
+++ resource/fldmapdata/eventdata/tmp/r04.h
@@ -14,18 +14,17 @@
 
 #define TR_FISHING_02                      0
 #define TR_FISHING_01                      1
-#define TR_BASKET_02                       2
-#define TR_BASKET_01                       3
+#define TR_BACKPACKERW_04                  2
+#define TR_BACKPACKERM_01                  3
 #define TR_WORKER2_01                      4
 #define TR_WORKER2_02                      5
 #define TR_WORKER2_03                      6
-#define TR_WORKER2_04                      7
-#define TR_BACKPACKERM_01                  8
-#define TR_BACKPACKERW_01                  9
-#define R04_FLD_ITEM_02                   10
-#define R04_FLD_ITEM_01                   11
-#define R04_STOP_01                       12
-#define R04_WOMAN2_01                     13
-#define R04_MAN2_01                       14
+#define TR_BACKPACKERW_03                  7
+#define TR_BACKPACKERW_01                  8
+#define R04_FLD_ITEM_02                    9
+#define R04_FLD_ITEM_01                   10
+#define R04_STOP_01                       11
+#define R04_WOMAN2_01                     12
+#define R04_MAN2_01                       13
 
 #endif //ZONE_R04_EVC_H
