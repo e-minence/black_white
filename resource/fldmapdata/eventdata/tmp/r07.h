701d541290b35509851e24d1fe01e3de1f2219f1
Shinya Takahashi <takahashi@gamefreak.co.jp>
2009-12-18 08:56:49 +0000

    トレーナー配置

--- resource/fldmapdata/eventdata/tmp/r07.h
+++ resource/fldmapdata/eventdata/tmp/r07.h
@@ -10,5 +10,12 @@
 #define ZONE_R07_EVC_H
 
 #define R07_FIGHTREW_01                    0
+#define TR_TANPAN_07                       1
+#define TR_BACKPACKERM_03                  2
+#define TR_BACKPACKERM_04                  3
+#define TR_RANGERM_04                      4
+#define TR_RANGERW_04                      5
+#define TR_TANPAN_08                       6
+#define R07_TREE_01                        7
 
 #endif //ZONE_R07_EVC_H
