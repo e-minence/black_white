b4601255bba8d3fbc5216cc7ba31590f6d94e3f0
Yukiko Hozumi <hozumi_yukiko@gamefreak.co.jp>
2009-12-19 08:12:00 +0000

    タワーオブヘブン４Fの階段れーるデータを修正

--- resource/fldmapdata/rail_data/rail/r07r0104.h
+++ resource/fldmapdata/rail_data/rail/r07r0104.h
@@ -1,7 +1,6 @@
 #pragma once
 // out put rail_conv.pl
 // rail line name
-#define RE_LINE_0 (0)
-#define div_1_RE_LINE_0 (1)
-#define RE_LINE_2 (2)
-#define div_3_RE_LINE_0 (3)
+#define div_1_RE_LINE_0 (0)
+#define RE_LINE_2 (1)
+#define div_3_RE_LINE_0 (2)
