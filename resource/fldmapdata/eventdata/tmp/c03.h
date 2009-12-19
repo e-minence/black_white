1e769622d31c8e48282a525e80f2d06e1c64fbca
Katsunori Suginaka <suginaka_katsunori@gamefreak.co.jp>
2009-12-19 04:43:58 +0000

    接続修正・スクリプト修正

--- resource/fldmapdata/eventdata/tmp/c03.h
+++ resource/fldmapdata/eventdata/tmp/c03.h
@@ -26,23 +26,13 @@
 
 #define C03_BUSINESSMAN4_01                0
 #define C03_BUSINESSMAN5_01                1
-#define C03_BUSINESSMAN6_01                2
-#define C03_BUSINESSMAN7_01                3
-#define C03_BUSINESSMAN3_01                4
-#define C03_BUSINESSMAN8_01                5
-#define C03_BUSINESSMAN1_01                6
-#define C03_BUSINESSMAN9_01                7
-#define C03_BUSINESSMAN10_01               8
-#define C03_BUSINESSMAN11_01               9
-#define C03_BUSINESSMAN2_01               10
-#define C03_BUSINESSMAN12_01              11
-#define C03_BUSINESSMAN13_01              12
-#define C03_BUSINESSMAN14_01              13
-#define C03_BUSINESSMAN15_01              14
-#define C03_OL1_01                        15
-#define C03_OL2_01                        16
-#define C03_OL4_01                        17
-#define C03_OL3_01                        18
-#define C03_OL5_01                        19
+#define C03_BUSINESSMAN3_01                2
+#define C03_BUSINESSMAN1_01                3
+#define C03_BUSINESSMAN2_01                4
+#define C03_OL1_01                         5
+#define C03_OL2_01                         6
+#define C03_OL4_01                         7
+#define C03_OL3_01                         8
+#define C03_OL5_01                         9
 
 #endif //ZONE_C03_EVC_H
