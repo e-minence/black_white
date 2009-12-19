02d9f154451df699c9e12d4d97d65bd111a62b6f
Masafumi Saito <saito_masafumi@gamefreak.co.jp>
2009-12-19 12:46:32 +0000

    接続修正：C04汎用ビルの２Fと接続

--- resource/fldmapdata/eventdata/tmp/c04r0501.h
+++ resource/fldmapdata/eventdata/tmp/c04r0501.h
@@ -3,6 +3,7 @@
 #define ZONE_C04R0501_EVD_H
 
 #define DOOR_ID_C04R0501_C04_EXIT01       0
+#define DOOR_ID_C04R0501_C04R0502_EXIT01  1
 
 #endif //ZONE_C04R0501_EVD_H
 //‚±‚Ìƒtƒ@ƒCƒ‹‚Í../data/c04r0501.mev‚©‚ç©“®¶¬‚³‚ê‚Ü‚µ‚½
--- resource/fldmapdata/eventdata/tmp/c04r0502.h
+++ resource/fldmapdata/eventdata/tmp/c04r0502.h
@@ -2,6 +2,7 @@
 #ifndef ZONE_C04R0502_EVD_H
 #define ZONE_C04R0502_EVD_H
 
+#define DOOR_ID_C04R0502_C04R0501_EXIT01  0
 
 #endif //ZONE_C04R0502_EVD_H
 //‚±‚Ìƒtƒ@ƒCƒ‹‚Í../data/c04r0502.mev‚©‚ç©“®¶¬‚³‚ê‚Ü‚µ‚½
