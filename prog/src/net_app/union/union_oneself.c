e2055a2f4d525a9da42bd39847e73bcd5759fbe4
Junichi Masuda <matsuda@gamefreak.co.jp>
2009-12-21 12:09:22 +0000

    マルチバトルのPOKEPARTY代入方法修正

--- prog/src/net_app/union/union_oneself.c
+++ prog/src/net_app/union/union_oneself.c
@@ -3131,7 +3131,7 @@ static BOOL OneselfSeq_ColosseumPokelist(UNION_SYSTEM_PTR unisys, UNION_MY_SITUA
       
       for(net_id = 0; net_id < GFL_NET_GetConnectNum(); net_id++){
         if(net_id != my_net_id){
-          if((my_net_id & 1) == (clsys->recvbuf.stand_position[net_id] & 1)){
+          if((clsys->recvbuf.stand_position[my_net_id] & 1) == (clsys->recvbuf.stand_position[net_id] & 1)){
             comm_player = PL_COMM_PLAYER_TYPE_ALLY;
           }
           else{
