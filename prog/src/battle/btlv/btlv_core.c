64a48f0074fe2fa371858cfb08b424a5a1d712c9
Hiroyuki Nakamura <nakamura@gamefreak.co.jp>
2009-12-22 10:38:08 +0000

    とりあえず、multiPosに０をいれた

--- prog/src/battle/btlv/btlv_core.c
+++ prog/src/battle/btlv/btlv_core.c
@@ -616,6 +616,7 @@ void BTLV_StartPokeSelect( BTLV_CORE* wk, const BTL_POKESELECT_PARAM* param, BOO
   wk->plistData.pp = BTL_MAIN_GetPlayerPokeParty( wk->mainModule );
   wk->plistData.multi_pp = BTL_MAIN_GetMultiPlayerPokeParty( wk->mainModule );
   wk->plistData.multiMode = ( wk->plistData.multi_pp != NULL );
+  wk->plistData.multiPos = 0;
   wk->plistData.font = wk->largeFontHandle;
   wk->plistData.heap = wk->heapID;
   wk->plistData.mode = param->bplMode;
@@ -729,6 +730,7 @@ void BTLV_ITEMSELECT_Start( BTLV_CORE* wk, u8 bagMode, u8 energy, u8 reserved_en
     wk->plistData.pp = BTL_MAIN_GetPlayerPokeParty( wk->mainModule );
     wk->plistData.multi_pp = BTL_MAIN_GetMultiPlayerPokeParty( wk->mainModule );
     wk->plistData.multiMode = ( wk->plistData.multi_pp != NULL );
+	  wk->plistData.multiPos = 0;
     wk->plistData.font = wk->largeFontHandle;
     wk->plistData.heap = wk->heapID;
     wk->plistData.mode = BPL_MODE_ITEMUSE;
@@ -1779,6 +1781,7 @@ void BTLV_WAZAWASURE_Start( BTLV_CORE* wk, u8 pos, WazaID waza )
   wk->plistData.pp = BTL_MAIN_GetPlayerPokeParty( wk->mainModule );
   wk->plistData.multi_pp = BTL_MAIN_GetMultiPlayerPokeParty( wk->mainModule );
   wk->plistData.multiMode = ( wk->plistData.multi_pp != NULL );
+  wk->plistData.multiPos = 0;
   wk->plistData.font = wk->largeFontHandle;
   wk->plistData.heap = wk->heapID;
   wk->plistData.mode = BPL_MODE_WAZASET;
