fea97095e7713979dfe1770841122fd8924774de
Toshihiro Obata <obata_toshihiro@gamefreak.co.jp>
2009-12-24 08:06:06 +0000

    コンバート環境を更新

--- resource/iss/zone/zone.rb
+++ resource/iss/zone/zone.rb
@@ -132,15 +132,4 @@ end
 # 出力したバイナリファイル名を表示
 bin_file_list.each do |filename|
   puts "-output: #{filename}"
-end
-
-# 出力したバイナリファイルリストを作成
-str = "ISS_Z_DATA = "
-bin_file_list.each do |filename|
-  str += "\\"
-  str += "\n"
-  str += "#{filename} "
-end
-file = File.open( ARGV[1] + "/iss_zone_data.list", "w" )
-file.write( str )
-file.close
+end 
