b443d0edf7f0ba4d87cfabbb34b58ce2bcb287cb
Toshihiro Obata <obata_toshihiro@gamefreak.co.jp>
2009-12-24 08:03:13 +0000

    コンバート環境を更新

--- resource/iss/switch/switch.rb
+++ resource/iss/switch/switch.rb
@@ -129,15 +129,4 @@ end
 # 出力したバイナリファイル名を表示
 bin_file_list.each do |filename|
   puts "-output: #{filename}"
-end
-
-# 出力したバイナリファイルリストを作成
-str = "ISS_S_DATA = "
-bin_file_list.each do |filename|
-  str += "\\"
-  str += "\n"
-  str += "#{filename} "
-end
-file = File.open( ARGV[1] + "/iss_switch_data.list", "w" )
-file.write( str )
-file.close
+end 
