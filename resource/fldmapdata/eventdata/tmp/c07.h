0d670cb7b26fbce58bc83f63c880ac23a5218892
Emi Ando <ando@gamefreak.co.jp>
2009-12-28 08:11:10 +0000

    C07：演出修整、冬のみ行ける部屋をロケット団の部屋に差し替え C08：演説イベント後にヤーコンを消す処理を追加

--- resource/fldmapdata/eventdata/tmp/c07.h
+++ resource/fldmapdata/eventdata/tmp/c07.h
@@ -3,12 +3,12 @@
 #define ZONE_C07_EVD_H
 
 #define DOOR_ID_C07_C07R0201_EXIT01       0
-#define DOOR_ID_C07_C07R0101_EXIT01       1
-#define DOOR_ID_C07_C07PC0101_EXIT01      2
-#define DOOR_ID_C07_C07R0301_EXIT01       3
-#define DOOR_ID_C07_C07GYM0101_EXIT01     4
-#define DOOR_ID_C07_D07R0401_EXIT01       5
-#define DOOR_ID_C07_C07R0401_EXIT01       6
+#define DOOR_ID_C07_C07PC0101_EXIT01      1
+#define DOOR_ID_C07_C07R0101_EXIT01       2
+#define DOOR_ID_C07_C07GYM0101_EXIT01     3
+#define DOOR_ID_C07_D07R0401_EXIT01       4
+#define DOOR_ID_C07_C07R0401_EXIT01       5
+#define DOOR_ID_C07_C07R0301_EXIT01       6
 
 #endif //ZONE_C07_EVD_H
 //Ç±ÇÃÉtÉ@ÉCÉãÇÕ../data/c07.mevÇ©ÇÁé©ìÆê∂ê¨Ç≥ÇÍÇ‹ÇµÇΩ
