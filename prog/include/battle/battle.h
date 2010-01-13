9e36e84e254ab6314d037ceb3ae2684e5a7f0081
Masao Taya <taya@gamefreak.co.jp>
2010-01-13 08:26:30 +0000

    デバッグフラグ追加

--- prog/include/battle/battle.h
+++ prog/include/battle/battle.h
@@ -166,6 +166,7 @@ typedef enum {
   BTL_DEBUGFLAG_MUST_CRITICAL,  ///< 打撃ワザのクリティカル発生率を100％にする
   BTL_DEBUGFLAG_HP_CONST,       ///< HP減らない
   BTL_DEBUGFLAG_PP_CONST,       ///< PP減らない
+  BTL_DEBUGFLAG_HIT100PER,      ///< 命中率が常に100％
   BTL_DEBUGFLAG_DMG_RAND_OFF,   ///< ダメージのランダム補正をオフ
 
   BTL_DEBUGFLAG_MAX,
