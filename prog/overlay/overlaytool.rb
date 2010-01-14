56609e572e15489e5505dd37dd4de857f7afe998
Sosuke Tamada <tamada@gamefreak.co.jp>
2010-01-14 13:00:07 +0000

    TWLでの拡張メモリ対応のため、ソース整理

--- prog/overlay/overlaytool.rb
+++ prog/overlay/overlaytool.rb
@@ -1,9 +1,16 @@
 #----------------------------------------------------------------------------
 #  overlaytool.rb
-#  afetrを複数指定できるようにパッチ修正  2009.01.16 k.ohno
+#  afterを複数指定できるようにパッチ修正  2009.01.16 k.ohno
 #  TWL専用に改造                          2009.04.15 k.ohno
+#  拡張メモリ対応の為にソース整理         2010.01.13 tamada
 #----------------------------------------------------------------------------
 
+
+#----------------------------------------------------------------------------
+#
+#       定数定義
+#
+#----------------------------------------------------------------------------
 
 #DEFAULT_LSFFILE		= "default.lsf"
 OVERLAY_DIR			= "overlay"
@@ -18,6 +25,8 @@ MATCH_LIB        = /^#LIB=>/            # ライブラリを含める場合
 MATCH_OVERLAYSRC = /^SRCS_OVERLAY_.*/	# 行頭から「SRCS_OVERLAY_...」
 MATCH_ENDSRC	 = /[\s\t].*\\\z/				# 行末が「\」
 MATCH_ERROR      = /\\\s/
+
+DEFAULT_SECTION_NAME		= ["main", "ITCM", "EX_MEM_START"]
 
 #デフォルトで読み込むファイル
 #default_lsf = File.read(DEFAULT_LSFFILE)
@@ -55,7 +64,7 @@ Autoload DTCM
 
 Autoload EX_MEM_START
 {
-  Address		0x02400000
+  Address		$EX_MEM_START_ADRS
 }
 
 #----------------------------------------------------------------------------
@@ -86,24 +95,80 @@ bottom_lsf2 = <<BOTTOM_LSFFILE2
 BOTTOM_LSFFILE2
 
 
+#----------------------------------------------------------------------------
+#
+#
+#
+#----------------------------------------------------------------------------
+class OverlayInfo
+
+  attr_reader :name, :put_name, :afterList, :source_name, :lib_name
+
+  def initialize( name, afterList, source_name, lib_name )
+    @name = name
+    @put_name = @name.downcase.sub(/srcs_overlay_/,"")
+    @afterList = afterList
+    @source_name = source_name
+    @lib_name = lib_name
+  end
+
+  def putOverlaySection()
+    
+    output = sprintf("Overlay %s\n{\n", @put_name )
+
+    # 「SRCS_OVERLAY_???」からSRCS_OVERLAY_を削って小文字化した名前をオーバーレイ領域の
+    # ターゲットにする。ただし、予約語として「main,ITCM,DTCM」という文字列が定義されているので
+    # これらは避けるようにする
+    @afterList.each { |afline|
+      if DEFAULT_SECTION_NAME.include?( afline ) == false then
+        afline = afline.downcase.sub(/srcs_overlay_/,"")
+      end
+      output += sprintf( "\tAfter\t%s\n", afline )
+    }
+    output += sprintf("\tObject")
+    @source_name.each_with_index{ | name, index |
+      filename = name.slice(/[a-zA-Z_0-9]+\./)
+      if index != 0 then
+        output += sprintf("\t")
+      end
+      output += sprintf("\t$(OBJDIR)/%so",filename)
+      if name.match(/\\\z/) then
+        output += sprintf(" \\")
+      end
+      output += sprintf("\n")
+    }
+
+    if @lib_name.length > 0 then
+      output += sprintf("\tLibrary")
+      @lib_name.each_with_index{ |name, index|
+        filename = name
+        output += sprintf("\t%s",filename)
+        if index + 1 != @lib_name.length  then
+          output += sprintf(" \\")
+        end
+        output += sprintf("\n")
+      }
+    end
+    output += sprintf("}\n\n")
+    return output
+  end
+
+end
 
 
-overlay_count        =  0
-overlay_table_num    =  0
-overlay_name		= ["main", "ITCM", "EX_MEM_START"]
-overlay_target		= ["main", "ITCM", "EX_MEM_START"]
-source_name          = Array.new
+
+
+#----------------------------------------------------------------------------
+#
+#ターゲットになるソースコードの記述ファイルを１行ずつ処理
+#
+#----------------------------------------------------------------------------
+sections = Array.new
+
+section_name = DEFAULT_SECTION_NAME.dup
+source_name          = nil
 lib_name          = Array.new
-overlay_hash		= { "main" => 1, "ITCM" => 1, "EX_MEM_START" => 1 }
-overlay_flag        = 0
-
-line_get_flag     = 0
-
-line_count        = 1
-
 afterList = []
-overlayList = []
-afteroverlaycnt = 0
 
 USERNAME_SKIP = 1     #解釈しない
 USERNAME_INSERT = 0   #解釈する
@@ -112,22 +177,15 @@ USERNAME_NONE = -1     #ユーザーネーム定義はまだない
 usernamestate = USERNAME_NONE
 
 
-File.open(OUTPUT_LSFFILE,"w"){|file|
-  file.puts(default_lsf)
-  file.puts("#ここから下はoverlaytool.rbで自動生成されています\n\n")
-}
 
-
-#ターゲットになるソースコードの記述ファイルを１行ずつ処理
-File.readlines(MAKE_PROG_FILE).each{ |line|
+File.readlines(MAKE_PROG_FILE).each_with_index{ |line, line_count|
   line.chomp!
 
   #行末の「\」の後ろにスペースが入っていると誤動作するのでチェックする
   if line.match(MATCH_ERROR) then
-    printf "%d行目： \\マークの後ろにスペースがはいっています。\n",line_count
+    printf "%d行目： \\マークの後ろにスペースがはいっています。\n",line_count + 1
     exit 1
   end
-  line_count = line_count + 1
   
   #プリプロセッサ解釈部分
   if usernamestate == USERNAME_NONE
@@ -154,196 +212,112 @@ File.readlines(MAKE_PROG_FILE).each{ |line|
   
   #オーバーレイ定義の取得
   if line.match(MATCH_KEYWORD) then
-    if afteroverlaycnt != 0 then
-      m = MATCH_KEYWORD.match(line)
-      afterList[afteroverlaycnt-1] = m.post_match.split(nil)[0].sub(/[\s\t]/,"")
-      afteroverlaycnt = afteroverlaycnt + 1
+    m = MATCH_KEYWORD.match(line)
+    _str = m.post_match.split.first.sub(/[\s\t]/,"")
+    if section_name.include?(_str) == true then
+      afterList << m.post_match.split.first.sub(/[\s\t]/,"")
     else
-      afteroverlaycnt = 1   ##ohno
-      overlay_flag = 1
-      m = MATCH_KEYWORD.match(line)
-      _str = m.post_match.split(nil)[0].sub(/[\s\t]/,"")
-
-      if overlay_hash.key?(_str) then
-        overlay_target << _str
-        overlay_count = overlay_count+1
-      else
-        printf "「%s」 というオーバーレイソ\ースは定義されていません\n",_str
-        exit 1
-      end
+      printf "「%s」 というオーバーレイソ\ースは定義されていません\n",_str
+      p section_name
+      exit 1
     end
+    next
   end
 
-  #「SRCS_OVERLAY」の後はソースコード行なので「\」がなくなるまで
-  # 保存し続ける
-  if line_get_flag==1 then
-    if line.match(/[a-zA-Z_0-9]+\.[cs]/) then
-      source_name << line
-    end
-
-    # 「\」が無い行がでてきたら終わりなので、取得終了
-    # lsfファイルに書き込みを行う
-    if line.match(MATCH_ENDSRC)==nil || line=="" then
-      #			p source_name
-      line_get_flag = 0
-      _count = 0
-      File.open(OUTPUT_LSFFILE,"a"){|file|
-
-
-        # 「SRCS_OVERLAY_???」からSRCS_OVERLAY_を削って小文字化した名前をオーバーレイ領域の
-        # ターゲットにする。ただし、予約語として「main,ITCM,DTCM」という文字列が定義されているので
-        # これらは避けるようにする
-        _targetname = overlay_target[overlay_table_num+2]
-        if _targetname != "main" && _targetname!="EX_MEM_START" && _targetname != "ITCM" then
-          _targetname = _targetname.downcase.sub(/srcs_overlay_/,"")
-        end
-        ##オーバーレイのリストを全部取っておく
-        overlayList.push(overlay_name[overlay_table_num+2].downcase.sub(/srcs_overlay_/,""))
-        
-        file.printf("Overlay %s\n{\n\tAfter\t%s\n",
-        overlay_name[overlay_table_num+2].downcase.sub(/srcs_overlay_/,""),
-        _targetname
-        )
-        if afteroverlaycnt != 0 then
-          afterList.each { |afline|
-            file.printf("\tAfter\t%s\n",afline.downcase.sub(/srcs_overlay_/,""))
-          }
-        end
-        file.printf("\tObject")
-        source_name.each{ |name|
-          filename = name.slice(/[a-zA-Z_0-9]+\./)
-          if _count != 0 then
-            file.printf("\t")
-          end
-          file.printf("\t$(OBJDIR)/%so",filename)
-          if name.match(/\\\z/) then
-            file.printf(" \\")
-          end
-          file.puts("\n")
-          _count = _count+1
-        }
-
-        if lib_name.length > 0 then
-          file.printf("\tLibrary")
-          local_c = 0
-          lib_name.each{ |name|
-            local_c = local_c + 1
-            filename = name
-            file.printf("\t%s",filename)
-            if local_c != lib_name.length  then
-              file.printf(" \\")
-            end
-            file.puts("\n")
-          }
-          lib_name.clear
-        end
-        file.puts("}\n\n")
-        afteroverlaycnt = 0   ##ohno
-        afterList = []
-      }
-    end
+  #オーバーレイ配置ライブラリ名の取得
+  if line.match(MATCH_LIB) then
+    line =~ /(\S+)\s+(\S+)/
+    lib_name << $2
+    next
   end
 
-  #オーバーレイソース名の取得
+  #上記意外の＃から始まる行はコメントとみなす
+  if line =~/^#/ then
+    next
+  end
+
+  #オーバーレイセクション名の取得
   if line.match(MATCH_OVERLAYSRC) then
-    line_get_flag = 1
 
-    if overlay_flag==0 then
+    if afterList.length == 0 then
       printf "Error!!\n"
       printf "#===>でオーバーレイアドレスを指定していないのに、\n"
       printf "オーバーレイ用のソ\ースコードが記述されています\n"
       exit 1
     end
 
-    overlay_name << line.split(nil)[0]
-    overlay_hash[line.split(nil)[0]] = 1
-
-    source_name = Array.new(0)
-    source_name << line
-
-    overlay_table_num = overlay_table_num + 1
+    section_name << line.split.first
+    source_name = [ line ]
+    next
   end
 
-  if line.match(MATCH_LIB) then
-    line =~ /(\S+)\s+(\S+)/
-    lib_name << $2
+  #「SRCS_OVERLAY」の後はソースコード行なので「\」がなくなるまで
+  # 保存し続ける
+  if source_name != nil then
+    if line.match(/[a-zA-Z_0-9]+\.[cs]/) then
+      source_name << line
+    end
+
+    # 「\」が無い行がでてきたら終わりなので、取得終了
+    if line.match(MATCH_ENDSRC)==nil || line=="" then
+      sections << OverlayInfo.new( section_name.last, afterList, source_name, lib_name )
+      source_name = nil
+      lib_name = []
+      afterList = []
+    end
   end
 
 }
 
 
-#オーバーレイに該当するファイルがコンパルターゲットになるようにする
+#----------------------------------------------------------------------------
+#
+#  読み込みデータを解釈して出力
+#
+#----------------------------------------------------------------------------
+total_output = ""
+total_output += default_lsf
+total_output += "#ここから下はoverlaytool.rbで自動生成されています\n\n"
+sections.each{|overlay_info|
+  total_output += overlay_info.putOverlaySection()
+}
+total_output.gsub!("$EX_MEM_START_ADRS","0x02400000")
+
+#NITRO用ファイルを作成する部分
+File.open(OUTPUT_LSFFILE, "w" ){|file|
+  #NITROのLSFのファイルの$LIBを読み替える
+  file.puts total_output.gsub(".$HYB","")
+}
+
+##TWL用ファイルを作成する部分
+File.open(OUTPUT_TWL_LSFFILE, "w" ) {|file|
+  #TWLのLSFファイルの$LIBを読み替える
+  file.puts total_output.gsub("$HYB","TWL.HYB")
+  file.puts(bottom_lsf1)
+  sections.each{|section|
+    file.printf("\tAfter\t%s\n",section.put_name )
+  }
+  file.puts(bottom_lsf2)
+}
+
+#オーバーレイに該当するファイルがコンパイルターゲットになるようにする
 File.open(OUTPUT_OVERLAYFILES,"w"){|file|
-  for i in 3..overlay_name.size-1
-    if i==3 then
-      file.printf("SRCS_OVERLAY\t=\t")
-    end
-    file.printf("\t\t\t$(%s)",overlay_name[i])
-    if i!=overlay_name.size-1 then
-      file.printf("\t\\\n",overlay_name[i])
+  file.printf("SRCS_OVERLAY\t=\t")
+  sections.each{|section|
+    file.printf( "\t\t\t$(%s)",section.name )
+    if section != sections.last then
+      file.printf("\t\\\n")
     else
       file.printf("\n")
     end
-  end
 
+  }
 }
-
-
-##TWL用ファイルを作成する部分
-
-copycmd = "cp " + OUTPUT_LSFFILE+" "+OUTPUT_TWL_LSFFILE
-system(copycmd)
-
-fh = File.open(OUTPUT_TWL_LSFFILE,"a")
-fh.puts(bottom_lsf1)
-overlayList.each{ |line|
-  fh.printf("\tAfter\t%s\n",line )
-}
-fh.puts(bottom_lsf2)
-fh.close
-
-#TWLのLSFファイルの$LIBを読み替える
-fh = File.open(OUTPUT_TWL_LSFFILE,"r")
-buf = fh.read();
-fh.close
-buf	= buf.gsub("$HYB","TWL.HYB")
-fh = File.open(OUTPUT_TWL_LSFFILE,"w")
-fh.write(buf)
-fh.close
-
-
-#NITROのLSFのファイルの$LIBを読み替える
-fh = File.open(OUTPUT_LSFFILE,"r")
-buf = fh.read();
-fh.close
-buf	= buf.gsub(".$HYB","")
-fh = File.open(OUTPUT_LSFFILE,"w")
-fh.write(buf)
-fh.close
-
 
 #オーバーレイのターゲットネームとその番号をテキストに吐き出す
 File.open(OUTPUT_OVERLAYTEXT,"w"){|file|
-  for i in 0..overlay_count-1
-    file.printf( " ID 0x%x = %s\n",i,overlay_name[i+3].downcase.sub(/srcs_overlay_/,""));
-  end
+  sections.each_with_index{|section, index|
+    file.printf( " ID 0x%x = %s\n",index + 3, section.put_name )
+  }
 }
-
-#p overlay_count
-#p overlay_table_num
-
-if overlay_count != overlay_table_num then
-  printf "'Error!!!\n"
-  printf "===>            =  %d 回\n",overlay_count
-  printf "SRCS_OVERLAY_?  =  %d 回\n",overlay_table_num
-  printf "'===>'で指定するオーバーレイターゲットの回数と\nオーバーレイ指定のソ\ースコード群の数があっていません\n"
-  exit 1
-else
-  printf "オーバーレイツール正常終了\n"
-end
-
-
-
-#p overlay_name
-#p overlay_target
 
