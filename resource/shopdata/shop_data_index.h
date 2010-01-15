3ed011a6c1cf7347e5efd00dda73acaa76143a76
Akito Mori <mori@gamefreak.co.jp>
2010-01-15 07:05:21 +0000

    コンバーターから新しく追加されるデータを登録してなかったので追加

---
+++ resource/shopdata/shop_data_index.h
@@ -0,0 +1,23 @@
+//====================================================================
+// @file  shop_data_index.h
+// @brief ショップデータ指定用ヘッダ
+//
+// ●fs_item.xlsをresource/shopdata/でコンバートして出力しています
+//
+//====================================================================
+
+#ifndef __SHOP_DATA_INDEX_H__
+#define __SHOP_DATA_INDEX_H__
+
+#define T02_FS    ( 0 )  // タウン１
+#define C01_FS    ( 1 )  // シティ１
+#define C02_FS    ( 2 )  // シティ２
+#define C03_FS    ( 3 )  // シティ３
+#define C04_FS    ( 4 )  // シティ４
+#define C05_FS    ( 5 )  // シティ５
+#define C06_FS    ( 6 )  // シティ６
+#define C07_FS    ( 7 )  // シティ７
+#define C08_FS    ( 8 )  // シティ８
+
+
+#endif
