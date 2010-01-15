ad90da651d194a84abe4ec10f2f301b9bec2f5d1
Yukiko Hozumi <hozumi_yukiko@gamefreak.co.jp>
2010-01-15 09:46:46 +0000

    ギアステーションのカメラを変更

--- resource/fldmapdata/rail_data/rail/c04r0101.h
+++ resource/fldmapdata/rail_data/rail/c04r0101.h
@@ -2,8 +2,8 @@
 // out put rail_conv.pl
 // rail line name
 #define RE_LINE_2 (0)
-#define div_2_div_1_RE_LINE_0 (1)
-#define div_3_RE_LINE_2 (2)
-#define div_4_div_1_RE_LINE_0 (3)
-#define RE_LINE_4 (4)
-#define RE_LINE_6 (5)
+#define RE_LINE_3 (1)
+#define RE_LINE_1 (2)
+#define RE_LINE_4 (3)
+#define RE_LINE_JUNCTION (4)
+#define RE_LINE_STAIRS (5)
