4ad3184fbb33cac1dfd6a10cdba5c4cde46bab62
Toru Nagihashi <nagihashi@gamefreak.co.jp>
2010-01-19 09:36:27 +0000

    デバッグプリントをOFFに

--- resource/wifi_union_obj/wifi_unionobj_xls_conv.pl
+++ resource/wifi_union_obj/wifi_unionobj_xls_conv.pl
@@ -93,7 +93,7 @@ foreach $line ( @XLS_DATA )
 #-------------------------------------
 #	デバッグプリント
 #=====================================
-if(1)
+if(0)
 {
 	print( "DEBUG_PRINT_START\n" );
 	for( my $i = 0; $i < $DATA_LENGTH; $i++ )
