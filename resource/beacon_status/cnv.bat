9674fa887b6036f1d9bb90189fb255ce5c557217
Miyuki Iwasawa <iwasawa@gamefreak.co.jp>
2010-01-20 10:33:04 +0000

    すれ違い通信画面仮素材追加

--- resource/beacon_status/cnv.bat
+++ resource/beacon_status/cnv.bat
@@ -3,30 +3,27 @@ del *.NCGR
 del *.NSCR
 del *.NCER
 del *.NANR
-del *.nsbtx
-del *.nsbmd
-del *.nsbca
-del *.ntfp
-del *.ntft
-
 
 g2dcvtr beacon_status.nsc -bg
+g2dcvtr bstatus_bg01.nsc -bg -pcm
+g2dcvtr bstatus_bg02.nsc -bg -pcm
+g2dcvtr bstatus_bg03.nsc -bg -pcm
 ntrcomp -lex -A4 -o beacon_status_lz.NSCR beacon_status.NSCR
 ntrcomp -lex -A4 -o beacon_status_lz.NCGR beacon_status.NCGR
+ntrcomp -lex -A4 -o bstatus_bg_lz.NCGR bstatus_bg.NCGR
+ntrcomp -lex -A4 -o bstatus_bg01_lz.NSCR bstatus_bg01.NSCR
+ntrcomp -lex -A4 -o bstatus_bg02_lz.NSCR bstatus_bg02.NSCR
+ntrcomp -lex -A4 -o bstatus_bg03_lz.NSCR bstatus_bg03.NSCR
+
+g2dcvtr bstatus_obj.nce -br -pcm
 
 g2dcvtr beacon_status.ncl
 
 echo narcçÏê¨
 nnsarc -c -l -n -i beacon_status -S arc_list.txt
 
-
 del *.NCLR
 del *.NCGR
 del *.NSCR
 del *.NCER
 del *.NANR
-del *.nsbtx
-del *.nsbmd
-del *.nsbca
-del *.ntfp
-del *.ntft
