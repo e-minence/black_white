9441a3b461270d04445a7499c013924e91f86efe
Sosuke Tamada <tamada@gamefreak.co.jp>
2010-01-20 02:43:31 +0000

    ファイルパスの位置を修正

--- resource/fldmapdata/script/scr_debug/make_filepath.rb
+++ resource/fldmapdata/script/scr_debug/make_filepath.rb
@@ -8,14 +8,15 @@
 #
 #==========================================================================
 output = ""
-pwd = Dir.pwd.sub(/^\/cygdrive\/c\//, "c:\/")
+#base_dir = Dir.pwd.sub(/^\/cygdrive\/c\//, "c:\/")
+base_dir = File.expand_path("..").sub(/^\/cygdrive\/c\//, "c:\/")
 lines = STDIN.readlines
 
 =begin
 MAX_PATH_SIZE = 128
 
 lines[0].chomp.split.each{|name|
-	path = pwd + "\/" + name
+	path = base_dir + "\/" + name
 	if path.length > MAX_PATH_SIZE 
 		raise Exception, "#{path} size(#{path.length}) > MAX_PATH_SIZE"
 	end
@@ -25,7 +26,7 @@ File.open(ARGV[0],"wb"){|file| file.write output }
 =end
 
 MAX_PATH_SIZE = 32
-output += sprintf("static const char scrDirPath[] = \"%s/\";\n\n", pwd )
+output += sprintf("static const char scrDirPath[] = \"%s/\";\n\n", base_dir )
 output += sprintf("static const char * scrNamePath[] = {\n")
 
 lines[0].chomp.split.each{|name|
