20cd7aac80fc0ccd2a1bbea65f96c8e847ff6480
Emi Ando <ando@gamefreak.co.jp>
2010-01-20 03:34:12 +0000

    c08gym0101：トレーナー配置修整

--- resource/fldmapdata/eventdata/tmp/c08gym0101.h
+++ resource/fldmapdata/eventdata/tmp/c08gym0101.h
@@ -11,17 +11,16 @@
 #ifndef ZONE_C08GYM0101_EVC_H
 #define ZONE_C08GYM0101_EVC_H
 
-#define TR_C08GYM0101_F1_01                0
-#define TR_C08GYM0101_F1_02                1
-#define TR_C08GYM0101_F1_03                2
-#define TR_C08GYM0101_F1_04                3
-#define TR_C08GYM0101_F1_06                4
-#define C08GYM0101_WKOBJCODE00_01          5
-#define TR_C08GYM0101_F2_01                6
-#define TR_C08GYM0101_F2_02                7
-#define TR_C08GYM0101_F2_04                8
-#define TR_C08GYM0101_F1_05                9
-#define C08GYM0101_SUNGLASSES_01          10
-#define TR_C08GYM0101_F2_03               11
+#define TR_C08GYM0101_F1_02                0
+#define TR_C08GYM0101_F1_03                1
+#define TR_C08GYM0101_F1_04                2
+#define TR_C08GYM0101_F1_06                3
+#define C08GYM0101_WKOBJCODE00_01          4
+#define TR_C08GYM0101_F2_01                5
+#define TR_C08GYM0101_F2_02                6
+#define TR_C08GYM0101_F2_04                7
+#define TR_C08GYM0101_F1_05                8
+#define C08GYM0101_SUNGLASSES_01           9
+#define TR_C08GYM0101_F2_03               10
 
 #endif //ZONE_C08GYM0101_EVC_H
