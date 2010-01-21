e8a87a846bea6fc54cc2d329dc5f30cb3127fc65
Miyuki Iwasawa <iwasawa@gamefreak.co.jp>
2010-01-21 08:23:02 +0000

    ビーコンステータス用素材更新

--- resource/beacon_status/cnv.bat
+++ resource/beacon_status/cnv.bat
@@ -15,22 +15,22 @@ ntrcomp -lex -A4 -o bstatus_bg01_lz.NSCR bstatus_bg01.NSCR
 ntrcomp -lex -A4 -o bstatus_bg02_lz.NSCR bstatus_bg02.NSCR
 ntrcomp -lex -A4 -o bstatus_bg03_lz.NSCR bstatus_bg03.NSCR
 
-g2dcvtr bstatus_obj.nce -br -pcm
-g2dcvtr bstatus_icon01.nce -br -pcm
-g2dcvtr bstatus_icon02.nce -br -pcm
-g2dcvtr bstatus_icon03.nce -br -pcm
-g2dcvtr bstatus_icon04.nce -br -pcm
-g2dcvtr bstatus_icon01.nce -br -pcm
-g2dcvtr bstatus_icon01.nce -br -pcm
-g2dcvtr bstatus_icon05.nce -br -pcm
-g2dcvtr bstatus_icon06.nce -br -pcm
-g2dcvtr bstatus_icon07.nce -br -pcm
-g2dcvtr bstatus_icon08.nce -br -pcm
-g2dcvtr bstatus_icon09.nce -br -pcm
-g2dcvtr bstatus_icon10.nce -br -pcm
-g2dcvtr bstatus_icon11.nce -br -pcm
-g2dcvtr bstatus_icon12.nce -br -pcm
-g2dcvtr bstatus_icon13.nce -br -pcm
+g2dcvtr bstatus_obj.nce -br
+g2dcvtr bstatus_icon01.nce -br
+g2dcvtr bstatus_icon02.nce -br
+g2dcvtr bstatus_icon03.nce -br
+g2dcvtr bstatus_icon04.nce -br
+g2dcvtr bstatus_icon01.nce -br
+g2dcvtr bstatus_icon01.nce -br
+g2dcvtr bstatus_icon05.nce -br
+g2dcvtr bstatus_icon06.nce -br
+g2dcvtr bstatus_icon07.nce -br
+g2dcvtr bstatus_icon08.nce -br
+g2dcvtr bstatus_icon09.nce -br
+g2dcvtr bstatus_icon10.nce -br
+g2dcvtr bstatus_icon11.nce -br
+g2dcvtr bstatus_icon12.nce -br
+g2dcvtr bstatus_icon13.nce -br
 
 g2dcvtr beacon_status.ncl
 
