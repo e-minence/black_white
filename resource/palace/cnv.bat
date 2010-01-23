875564dddcb780322fc8265677fb59dcce617734
Junichi Masuda <matsuda@gamefreak.co.jp>
2010-01-23 08:34:25 +0000

    パレス下画面修正

--- resource/palace/cnv.bat
+++ resource/palace/cnv.bat
@@ -16,8 +16,12 @@ g2dcvtr palace_obj.nce -br -ncn
 ntrcomp -lex -A4 -o palace_obj_lz.NCGR palace_obj.NCGR
 
 g2dcvtr palace_bg.nsc -bg
+g2dcvtr palace_bar.nsc -bg
+g2dcvtr palace_bar2.nsc -bg
 ntrcomp -lex -A4 -o palace_bg_lz.NCGR palace_bg.NCGR
 ntrcomp -lex -A4 -o palace_bg_lz.NSCR palace_bg.NSCR
+ntrcomp -lex -A4 -o palace_bar_lz.NSCR palace_bar.NSCR
+ntrcomp -lex -A4 -o palace_bar2_lz.NSCR palace_bar2.NSCR
 
 
 echo g3dコンバート
