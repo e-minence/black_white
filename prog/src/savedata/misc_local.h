9aa0ed6e1698205016e42bdf1b504ca5b0943c20
Sosuke Tamada <tamada@gamefreak.co.jp>
2010-01-23 11:04:47 +0000

    MISCにジムリーダー戦勝利の情報セットワークを追加した

--- prog/src/savedata/misc_local.h
+++ prog/src/savedata/misc_local.h
@@ -1,8 +1,12 @@
 
+#include "pm_define.h"
+
 ///すれ違い：自己紹介メッセージ長
 #define SAVE_SURETIGAI_SELFINTRODUCTION_LEN   (10 + 1)  //EOM込み
 ///すれ違い：お礼メッセージ長
 #define SAVE_SURETIGAI_THANKYOU_LEN           (10 + 1)  //EOM込み
+
+#define GYM_MAX     (8)
 
 //=============================================================================
 /**
@@ -33,5 +37,8 @@ struct _MISC
   u32 suretigai_count;           ///<すれ違い人数
   STRCODE self_introduction[SAVE_SURETIGAI_SELFINTRODUCTION_LEN]; ///<自己紹介
   STRCODE thankyou_message[SAVE_SURETIGAI_THANKYOU_LEN];  ///<お礼メッセージ
+
+  //ジムリーダー戦勝利時の手持ち記録
+  u16 gym_win_monsno[GYM_MAX][TEMOTI_POKEMAX];
 };
 
