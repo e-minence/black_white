8e8ba5fe64876dcb8f5ff2e67fc7f302f87dd78f
Sosuke Tamada <tamada@gamefreak.co.jp>
2010-01-23 04:41:59 +0000

    helpを追加。エラー出力を見れるようにバッチを修正

--- tools/script_debugger/ev_make.bat
+++ tools/script_debugger/ev_make.bat
@@ -1,2 +1,4 @@
-touch c:\home\pokemon_wb\test.txt
-make -C %PROJECT_RSCDIR%fldmapdata
+REM make -C %PROJECT_RSCDIR%fldmapdata > %PROJECT_ROOT%tools\script_debugger\result.txt 2>&1
+make -C %PROJECT_RSCDIR%fldmapdata 2>&1 |tee %PROJECT_ROOT%tools\script_debugger\result.txt
+unix2dos -D %PROJECT_ROOT%tools\script_debugger\result.txt
+pause
