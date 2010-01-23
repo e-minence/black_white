39303618c783a46fe6cf64fe0c2566135c1c71dd
Sosuke Tamada <tamada@gamefreak.co.jp>
2010-01-23 12:11:33 +0000

    努力値を見るお嬢様のスクリプトを作成

--- resource/fldmapdata/script/c08r0401_def.h
+++ resource/fldmapdata/script/c08r0401_def.h
@@ -5,6 +5,8 @@
 #define _C08R0401_DEF_H_
 
 //スクリプトデータID定義
-#define SCRID_C08R0401 (1)
+#define SCRID_C08R0401_GENTLEMAN_01 (1)
+#define SCRID_C08R0401_LADY_01 (2)
+#define SCRID_C08R0401_GIRL4_01 (3)
 
 #endif //_C08R0401_DEF_H_
