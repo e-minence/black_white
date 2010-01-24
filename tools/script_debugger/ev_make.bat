5b91f788497fc811f4ebc18ad928cd019fc49ed7
Sosuke Tamada <tamada@gamefreak.co.jp>
2010-01-24 11:28:53 +0000

    スクリプトデバッガ更新。コンパイルエラー時にダイアログ表示を行うように修正

--- tools/script_debugger/ev_make.bat
+++ tools/script_debugger/ev_make.bat
@@ -1,4 +1,12 @@
-REM make -C %PROJECT_RSCDIR%fldmapdata > %PROJECT_ROOT%tools\script_debugger\result.txt 2>&1
-make -C %PROJECT_RSCDIR%fldmapdata 2>&1 |tee %PROJECT_ROOT%tools\script_debugger\result.txt
+SET SCRIPT_DEBUGGER_MODE=yes
+make -C %PROJECT_RSCDIR%fldmapdata > %PROJECT_ROOT%tools\script_debugger\result.txt 2>&1
+@REM make -C %PROJECT_RSCDIR%fldmapdata 2>&1 |tee %PROJECT_ROOT%tools\script_debugger\result.txt
+@REM echo %ERRORLEVEL%
+if ERRORLEVEL 1 goto error
+goto end
+
+:error
 unix2dos -D %PROJECT_ROOT%tools\script_debugger\result.txt
-pause
+exit 1
+
+:end
