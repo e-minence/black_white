6f18099dbc1f9156ae98d1e58aa16f07ade62b76
Katsunori Suginaka <suginaka_katsunori@gamefreak.co.jp>
2010-01-27 10:55:12 +0000

    C03負荷実験用の人物OBJ削除

--- resource/fldmapdata/eventdata/tmp/c03p10.h
+++ resource/fldmapdata/eventdata/tmp/c03p10.h
@@ -12,35 +12,5 @@
 #ifndef ZONE_C03P10_EVC_H
 #define ZONE_C03P10_EVC_H
 
-#define C03P10_BUSINESSMAN_01              0
-#define C03P10_BUSINESSMAN_02              1
-#define C03P10_BUSINESSMAN_03              2
-#define C03P10_BUSINESSMAN_04              3
-#define C03P10_BUSINESSMAN_05              4
-#define C03P10_BUSINESSMAN_06              5
-#define C03P10_BUSINESSMAN_07              6
-#define C03P10_BUSINESSMAN_08              7
-#define C03P10_BUSINESSMAN_09              8
-#define C03P10_BUSINESSMAN_10              9
-#define C03P10_OL_01                      10
-#define C03P10_OL_02                      11
-#define C03P10_OL_03                      12
-#define C03P10_OL_04                      13
-#define C03P10_OL_05                      14
-#define C03P10_OL_06                      15
-#define C03P10_OL_07                      16
-#define C03P10_OL_08                      17
-#define C03P10_OL_09                      18
-#define C03P10_OL_10                      19
-#define C03P10_OL_11                      20
-#define C03P10_OL_12                      21
-#define C03P10_OL_13                      22
-#define C03P10_OL_14                      23
-#define C03P10_OL_15                      24
-#define C03P10_BUSINESSMAN_11             25
-#define C03P10_BUSINESSMAN_12             26
-#define C03P10_BUSINESSMAN_13             27
-#define C03P10_BUSINESSMAN_14             28
-#define C03P10_BUSINESSMAN_15             29
 
 #endif //ZONE_C03P10_EVC_H
--- resource/fldmapdata/script/c03p10_def.h
+++ resource/fldmapdata/script/c03p10_def.h
@@ -5,8 +5,5 @@
 #define _C03P10_DEF_H_
 
 //スクリプトデータID定義
-#define SCRID_C03P10_BUSINESSMAN_01 (1)
-#define SCRID_C03P10_BUSINESSMAN_02 (2)
-#define SCRID_C03P10_OL_01 (3)
 
 #endif //_C03P10_DEF_H_
