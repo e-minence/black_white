3e4a5dae84ec1c0f57669156e0d1419f92ebb913
Ariizumi Nobuhiko <ariizumi_nobuhiko@gamefreak.co.jp>
2010-01-27 06:36:07 +0000

    通信TVT：ADPCM対応

--- prog/src/net_app/comm_tvt/enc_adpcm.c
+++ prog/src/net_app/comm_tvt/enc_adpcm.c
@@ -30,22 +30,25 @@
 //======================================================================
 #pragma mark [> struct
 typedef struct {
-    s16 valprev;  /* Previous output value */
-    s8  index;    /* Index into stepsize table */
-}adpcm_state ;
+    s16 prevSample;  /* Previous output value */
+    s8  prevIndex;    /* Index into stepsize table */
+}ADPCMstate ;
 //======================================================================
 //  proto
 //======================================================================
 #pragma mark [> proto
-void adpcm_coder( s16 *indata, s8 *outdata, int len, adpcm_state *state);
-void adpcm_decoder( s8 *indata, s16 *outdata, int len,adpcm_state *state);
+static void adpcm_coder( s16 *indata, s8 *outdata, int len, ADPCMstate *state);
+static void adpcm_decoder( s8 *indata, s16 *outdata, int len,ADPCMstate *state);
 
-static adpcm_state adpcmState;
+static char ADPCMEncorder( short sample, ADPCMstate* state );
+static s16 ADPCMDecoder( int code, ADPCMstate* state );
+
+static ADPCMstate adpcmState;
 
 void ENC_ADPCM_ResetParam( void )
 {
-  adpcmState.valprev = 0;
-  adpcmState.index = 0;
+  adpcmState.prevSample = 0;
+  adpcmState.prevIndex = 0;
 }
 
 //--------------------------------------------------------------
@@ -103,249 +106,169 @@ const u32 ENC_ADPCM_DecodeData( void* src , const u32 srcSize , void* dst )
   return srcSize*4;
 }
 
-
-//--------------------------------------------------------------
-//  ˆÈ‰º“]Ú
-//--------------------------------------------------------------
-/* Intel ADPCM step variation table */
-static int indexTable[16] = 
+static void adpcm_coder( s16 *indata, s8 *outdata, int len, ADPCMstate *state)
 {
-    -1, -1, -1, -1, 2, 4, 6, 8,
-    -1, -1, -1, -1, 2, 4, 6, 8,
-};
-
-static int stepsizeTable[89] = 
-{
-    7, 8, 9, 10, 11, 12, 13, 14, 16, 17,
-    19, 21, 23, 25, 28, 31, 34, 37, 41, 45,
-    50, 55, 60, 66, 73, 80, 88, 97, 107, 118,
-    130, 143, 157, 173, 190, 209, 230, 253, 279, 307,
-    337, 371, 408, 449, 494, 544, 598, 658, 724, 796,
-    876, 963, 1060, 1166, 1282, 1411, 1552, 1707, 1878, 2066,
-    2272, 2499, 2749, 3024, 3327, 3660, 4026, 4428, 4871, 5358,
-    5894, 6484, 7132, 7845, 8630, 9493, 10442, 11487, 12635, 13899,
-    15289, 16818, 18500, 20350, 22385, 24623, 27086, 29794, 32767
-};
-    
-void adpcm_coder( s16 *indata, s8 *outdata, int len, adpcm_state *state)
-{
-  s16 *inp;     /* Input buffer pointer */
-  s8 *outp;   /* output buffer pointer */
-  int val;      /* Current input sample value */
-  int sign;     /* Current adpcm sign bit */
-  int delta;      /* Current adpcm output value */
-  int diff;     /* Difference between val and valprev */
-  int step;     /* Stepsize */
-  int valpred;    /* Predicted output value */
-  int vpdiff;     /* Current change to valpred */
-  int index;      /* Current step change index */
-  int outputbuffer;   /* place to keep previous 4-bit value */
-  int bufferstep;   /* toggle between outputbuffer/output */
-
-  outp = (s8 *)outdata;
-  inp = indata;
-
-  valpred = state->valprev;
-  index = state->index;
-  step = stepsizeTable[index];
-  
-  bufferstep = 1;
-
-  for ( ; len > 0 ; len-- )
+  int i;
+  int outIdx = 0;
+  u8 tempData = 0;
+  u8 isUpper = FALSE;
+  for( i=0;i<len;i++ )
   {
-    val = *inp++;
+    const u8 retCode = ADPCMEncorder( indata[i] , state );
 
-    /* Step 1 - compute difference with previous value */
-    diff = val - valpred;
-    sign = (diff < 0) ? 8 : 0;
-    if ( sign )
+    if( isUpper == FALSE )
     {
-      diff = (-diff);
-    }
-
-    /* Step 2 - Divide and clamp */
-    /* Note:
-    ** This code *approximately* computes:
-    **    delta = diff*4/step;
-    **    vpdiff = (delta+0.5)*step/4;
-    ** but in shift step bits are dropped. The net result of this is
-    ** that even if you have fast mul/div hardware you cannot put it to
-    ** good use since the fixup would be too expensive.
-    */
-    delta = 0;
-    vpdiff = (step >> 3);
-    
-    if ( diff >= step )
-    {
-        delta = 4;
-        diff -= step;
-        vpdiff += step;
-    }
-    step >>= 1;
-    if ( diff >= step  )
-    {
-        delta |= 2;
-        diff -= step;
-        vpdiff += step;
-    }
-    step >>= 1;
-    if ( diff >= step )
-    {
-        delta |= 1;
-        vpdiff += step;
-    }
-
-    /* Step 3 - Update previous value */
-    if ( sign )
-    {
-      valpred -= vpdiff;
+      tempData = retCode;
+      isUpper = TRUE;
     }
     else
     {
-      valpred += vpdiff;
+      tempData += retCode<<4;
+      outdata[outIdx] = tempData;
+      
+      isUpper = FALSE;
+      tempData = 0;
+      outIdx++;
     }
-
-    /* Step 4 - Clamp previous value to 16 bits */
-    if ( valpred > 32767 )
-    {
-      valpred = 32767;
-    }
-    else if ( valpred < -32768 )
-    {
-      valpred = -32768;
-    }
-
-    /* Step 5 - Assemble value, update index and step values */
-    delta |= sign;
-    
-    index += indexTable[delta];
-    if ( index < 0 )
-    {
-      index = 0;
-    }
-    if ( index > 88 )
-    {
-      index = 88;
-    }
-    step = stepsizeTable[index];
-
-    /* Step 6 - Output value */
-    if ( bufferstep ) 
-    {
-        outputbuffer = (delta << 4) & 0xf0;
-    }
-    else 
-    {
-        *outp++ = (delta & 0x0f) | outputbuffer;
-    }
-    bufferstep = !bufferstep;
   }
 
-    /* Output last step, if needed */
-    if ( !bufferstep )
-      *outp++ = outputbuffer;
-    
-    state->valprev = valpred;
-    state->index = index;
+  if( isUpper == TRUE )
+  {
+    outdata[outIdx] = tempData;
+  }
 }
 
-void adpcm_decoder( s8 *indata, s16 *outdata, int len,adpcm_state *state)
+static void adpcm_decoder( s8 *indata, s16 *outdata, int len,ADPCMstate *state)
 {
-  s8 *inp;   /* Input buffer pointer */
-  s16 *outp;    /* output buffer pointer */
-  int sign;     /* Current adpcm sign bit */
-  int delta;      /* Current adpcm output value */
-  int step;     /* Stepsize */
-  int valpred;    /* Predicted value */
-  int vpdiff;     /* Current change to valpred */
-  int index;      /* Current step change index */
-  int inputbuffer;    /* place to keep next 4-bit value */
-  int bufferstep;   /* toggle between inputbuffer/input */
-
-  outp = outdata;
-  inp = (s8*)indata;
-
-  valpred = state->valprev;
-  index = state->index;
-  step = stepsizeTable[index];
-
-  bufferstep = 0;
-  
-  for ( ; len > 0 ; len-- )
+  int i;
+  for( i=0;i<len/2;i++ )
   {
-    
-    /* Step 1 - get the delta value */
-    if ( bufferstep )
-    {
-      delta = inputbuffer & 0xf;
-    }
-    else
-    {
-      inputbuffer = *inp++;
-      delta = (inputbuffer >> 4) & 0xf;
-    }
-    bufferstep = !bufferstep;
+    u8 lowerData = indata[i]&0x0f;
+    u8 upperData = (indata[i]&0xf0)>>4;
 
-    /* Step 2 - Find new index value (for later) */
-    index += indexTable[delta];
-    if ( index < 0 )
-    {
-      index = 0;
-    }
-    if ( index > 88 )
-    {
-      index = 88;
-    }
+    outdata[i*2] = ADPCMDecoder( lowerData , state );
+    outdata[i*2+1] = ADPCMDecoder( upperData , state );
+  }
+}
 
-    /* Step 3 - Separate sign and magnitude */
-    sign = delta & 8;
-    delta = delta & 7;
 
-    /* Step 4 - Compute difference and new predicted value */
-    /*
-    ** Computes 'vpdiff = (delta+0.5)*step/4', but see comment
-    ** in adpcm_coder.
-    */
-    vpdiff = step >> 3;
-    if ( delta & 4 )
-    {
-      vpdiff += step;
-    }
-    if ( delta & 2 )
-    {
-      vpdiff += step>>1;
-    }
-    if ( delta & 1 )
-    {
-      vpdiff += step>>2;
-    }
 
-    if ( sign )
-    {
-      valpred -= vpdiff;
-    }
-    else
-    {
-      valpred += vpdiff;
-    }
+#define INDEX_NUM 89
 
-    /* Step 5 - clamp output value */
-    if ( valpred > 32767 )
-    {
-      valpred = 32767;
-    }
-    else if ( valpred < -32768 )
-    {
-      valpred = -32768;
-    }
+static const signed char sIndexTable[16] = {
+  -1, -1, -1, -1, 2, 4, 6, 8,
+  -1, -1, -1, -1, 2, 4, 6, 8,
+};
 
-    /* Step 6 - Update step value */
-    step = stepsizeTable[index];
+static const int sStepSizeTable[ INDEX_NUM ] = {
+  7, 8, 9, 10, 11, 12, 13, 14, 16, 17,
+  19, 21, 23, 25, 28, 31, 34, 37, 41, 45,
+  50, 55, 60, 66, 73, 80, 88, 97, 107, 118,
+  130, 143, 157, 173, 190, 209, 230, 253, 279, 307,
+  337, 371, 408, 449, 494, 544, 598, 658, 724, 796,
+  876, 963, 1060, 1166, 1282, 1411, 1552, 1707, 1878, 2066,
+  2272, 2499, 2749, 3024, 3327, 3660, 4026, 4428, 4871, 5358,
+  5894, 6484, 7132, 7845, 8630, 9493, 10442, 11487, 12635, 13899,
+  15289, 16818, 18500, 20350, 22385, 24623, 27086, 29794, 32767
+};
 
-    /* Step 7 - Output value */
-    *outp++ = valpred;
+
+static char ADPCMEncorder( short sample, ADPCMstate* state )
+{
+  int code;
+  int diff;
+  int diff2;
+  int step;
+  int pred;
+  int index;
+
+  pred = (int)(state->prevSample);
+  index = state->prevIndex;
+  step = sStepSizeTable[index];
+
+  diff = sample - pred;
+  if ( diff >= 0 ) {
+    code = 0;
+  }
+  else {
+    code = 8;
+    diff = -diff;
   }
 
-  state->valprev = valpred;
-  state->index = index;
+  diff2 = step >> 3;
+  if ( diff >= step ) {
+    code |= 4;
+    diff -= step;
+    diff2 += step;
+  }
+  step >>= 1;
+  if ( diff >= step ) {
+    code |= 2;
+    diff -= step;
+    diff2 += step;
+  }
+  step >>= 1;
+  if ( diff >= step ) {
+    code |= 1;
+    diff2 += step;
+  }
+  
+  if ( code & 8 ) 
+    pred -= diff2;
+  else
+    pred += diff2;
+  
+  if ( pred > 32767 )
+    pred = 32767;
+  else if ( pred < -32767 )
+    pred = -32767;
+
+  index += sIndexTable[code];
+  
+  if ( index < 0 )
+    index = 0;
+  if ( index > 88 )
+    index = 88;
+
+  state->prevSample = (short)pred;
+  state->prevIndex = index;
+
+  return ( (char)(code & 0x0f) );
+}
+
+static s16 ADPCMDecoder( int code, ADPCMstate* state )
+{
+  int step;
+  int sample;
+  int index;
+  int d;
+    
+  sample = state->prevSample;
+  index  = state->prevIndex;
+    
+  step = sStepSizeTable[ index ];
+    
+  d = step >> 3;
+  if ( code & 4 ) d += step;
+    if ( code & 2 ) d += step >> 1;
+  if ( code & 1 ) d += step >> 2;
+    
+  if ( code & 8 ) {
+        sample -= d;
+        if ( sample < -32768 ) sample = -32768;
+    }
+  else {
+        sample += d;
+        if ( sample > 32767 ) sample = 32767;
+    }
+    
+  index += sIndexTable[ code ];
+    
+  if ( index < 0 ) index = 0;
+  else if ( index > INDEX_NUM-1 ) index = INDEX_NUM-1;
+    
+  state->prevSample = (s16)sample;
+  state->prevIndex = (u8)index;
+    
+  return (s16)sample;
 }
