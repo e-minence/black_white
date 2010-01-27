e2ac2058b54e381072be10acf5148b3d49a9252b
Katsumi Ohno <ohno@gamefreak.co.jp>
2010-01-27 10:39:05 +0000

    不思議データからポケモンを作る関数を作成 性別とレアは曽我部さん待ち

--- prog/src/net_app/mystery/mystery_pokemoncreate.c
+++ prog/src/net_app/mystery/mystery_pokemoncreate.c
@@ -9,15 +9,26 @@
 
 
 #include <gflib.h>
+#include "gamesystem/game_data.h"
+
 #include "poke_tool/poke_tool.h"
+#include "item/itemsym.h"
+#include "waza_tool/wazano_def.h"
 #include "savedata/mystery_data.h"
 
+#include "net_app/mystery.h"
+#include "system/gfl_use.h"
 
 
-POKEMON_PARAM* MYSTERY_PokemonCreate(const GIFT_PRESENT_POKEMON* pGift, HEAPID heapID)
+
+POKEMON_PARAM* MYSTERY_PokemonCreate(const GIFT_PRESENT_POKEMON* pGift, HEAPID heapID, GAMEDATA* pGameData)
 {
   POKEMON_PARAM* pp;
   u16 level=pGift->level;
+  u32 id = pGift->id_no;
+  PtlSetupPow pow=0;
+  u64 rand;
+  u32 buff;
 
   
   if(pGift->mons_no > MONSNO_END){  //モンスター番号が存在しない物は作らない
@@ -32,20 +43,117 @@ POKEMON_PARAM* MYSTERY_PokemonCreate(const GIFT_PRESENT_POKEMON* pGift, HEAPID h
   if(pGift->level > 100){  //レベル100より上は作らない
     return NULL;
   }
+  if(pGift->item > ITEM_DATA_MAX){  //アイテム番号間違い
+    return NULL;
+  }
+
+  
   
   if(pGift->level == 0){  //レベル０指定は乱数
     level = GFUser_GetPublicRand(99)+1;
   }
+  if(pGift->id_no == 0){  //ID０指定は自分が親
+    id = MyStatus_GetID(GAMEDATA_GetMyStatus(pGameData));
+  }
 
+
+
+  pp = PP_Create( pGift->mons_no, level, id, heapID );
+
+  pow = PTL_SETUP_POW_PACK(pGift->hp_rnd,pGift->pow_rnd,
+                           pGift->def_rnd,
+                           pGift->spepow_rnd,pGift->spedef_rnd,pGift->agi_rnd);
+  if(pow==0){
+    pow = PTL_SETUP_POW_AUTO;
+  }
+  if(pGift->rnd != 0){
+    rand = pGift->rnd;
+  }
+  else if(pGift->rare == 0){
+    rand = PTL_SETUP_ID_NOT_RARE;
+  }
+  else if(pGift->rare == 1){
+    rand = PTL_SETUP_ID_AUTO;
+  }
+  else if(pGift->rare == 2){
+    rand = PTL_SETUP_RND_RARE;
+  }
+
+  //POKETOOL_CalcPersonalRand( data->monsno, data->formno, data->sex );
+  // @todo  性別設定がわからない
+//  u8 sex;              //性別
+
+  PP_SetupEx( pp, pGift->mons_no, level, id, pow, rand );
+
+  PP_Put(pp, ID_PARA_item, pGift->item);
+
+  if(pGift->waza1!=0 && pGift->waza1<WAZANO_MAX){
+    PP_SetWazaPush(pp, pGift->waza1);
+  }
+  if(pGift->waza2!=0 && pGift->waza2<WAZANO_MAX){
+    PP_SetWazaPush(pp, pGift->waza2);
+  }
+  if(pGift->waza3!=0 && pGift->waza3<WAZANO_MAX){
+    PP_SetWazaPush(pp, pGift->waza3);
+  }
+  if(pGift->waza4!=0 && pGift->waza4<WAZANO_MAX){
+    PP_SetWazaPush(pp, pGift->waza4);
+  }
+  PP_Put(pp, ID_PARA_form_no, pGift->form_no);  //フォルム番号はすべて許す
   
-  pp = PP_Create( pGift->mons_no, level, u64 id, HEAPID heapID );
+  if(pGift->get_ball!=0){
+    PP_Put(pp, ID_PARA_get_ball, pGift->get_ball);
+  }
+  if(pGift->get_level!=0){
+    PP_Put(pp, ID_PARA_get_ball, pGift->get_level);  
+  }
 
+  PP_Put(pp, ID_PARA_style, pGift->style);  
+  PP_Put(pp, ID_PARA_beautiful, pGift->beautiful);  
+  PP_Put(pp, ID_PARA_cute, pGift->cute);  
+  PP_Put(pp, ID_PARA_clever, pGift->clever);  
+  PP_Put(pp, ID_PARA_strong, pGift->strong);  
+  PP_Put(pp, ID_PARA_fur, pGift->strong);  
+
+//  u16 ribbon_no;        //リボンビット 16本  @todo 下山田さん待ち
+
+  if(pGift->version!=0){
+    PP_Put(pp, ID_PARA_get_cassette, pGift->version);
+  }
+    
+  PP_Put(pp, ID_PARA_country_code, pGift->country_code);  
+
+  if(pGift->nickname[0]!= GFL_STR_GetEOMCode()){
+    PP_Put(pp, ID_PARA_nickname_raw, (u32)pGift->nickname);
+  }
   
 
-  PP_SetupEx
+  PP_Put(pp, ID_PARA_seikaku, pGift->seikaku);  
+  PP_Put(pp, ID_PARA_speabino, pGift->speabino);  
+  PP_Put(pp, ID_PARA_get_place, pGift->get_place);  
+  PP_Put(pp, ID_PARA_birth_place, pGift->birth_place);  
 
-  
+  if(pGift->nickname[0]!= GFL_STR_GetEOMCode()){
+    PP_Put(pp, ID_PARA_oyaname_raw, (u32)pGift->oyaname);
+  }
+  if(pGift->oyasex <= PM_NEUTRAL){
+    PP_Put(pp, ID_PARA_oyasex, pGift->oyasex);
+  }
+//  else{
+//    PP_Put(pp, ID_PARA_oyasex, MyStatus_GetMySex(GAMEDATA_GetMyStatus(pGameData)));
+//  }
 
+  if(pGift->egg){       //タマゴかどうか TRUE＝たまご
+    PP_Put(pp, ID_PARA_tamago_flag, TRUE);
+  }
+
+	PP_Renew( pp );
+
+  if(PP_Get(pp,ID_PARA_fusei_tamago_flag, NULL)){  //不正ポケモンが出来てしまった場合
+    GFL_HEAP_FreeMemory(pp);
+    return NULL;
+  }
+  return pp;
 }
 
 
