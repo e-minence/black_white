e42fa32ce1f8a1c59cfa2c4d87bcbd5d3c626522
Emi Ando <ando@gamefreak.co.jp>
2010-01-27 05:48:47 +0000

    WC10、BC10：自動配置OBJスクリプト実装 C08R0301：グッズおじさん部屋に他のOBJも配置

--- resource/fldmapdata/eventdata/tmp/c08r0301.h
+++ resource/fldmapdata/eventdata/tmp/c08r0301.h
@@ -10,5 +10,7 @@
 #define ZONE_C08R0301_EVC_H
 
 #define C08R0301_GENTLEMAN_01              0
+#define C08R0301_MIDDLEWOMAN1_01           1
+#define C08R0301_GIRL4_01                  2
 
 #endif //ZONE_C08R0301_EVC_H
--- resource/fldmapdata/script/c08r0301_def.h
+++ resource/fldmapdata/script/c08r0301_def.h
@@ -6,5 +6,7 @@
 
 //スクリプトデータID定義
 #define SCRID_C08R0301_GENTLEMAN_01 (1)
+#define SCRID_C08R0301_MIDDLEWOMAN1_01 (2)
+#define SCRID_C08R0301_GIRL4_01 (3)
 
 #endif //_C08R0301_DEF_H_
