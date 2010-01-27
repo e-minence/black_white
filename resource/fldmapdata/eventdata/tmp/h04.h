647540907c80cfb88324d7a63278edf4d7d8225e
Emi Ando <ando@gamefreak.co.jp>
2010-01-27 10:11:47 +0000

    H04：ISSスクリプト作成

--- resource/fldmapdata/eventdata/tmp/h04.h
+++ resource/fldmapdata/eventdata/tmp/h04.h
@@ -8,5 +8,9 @@
 #ifndef ZONE_H04_EVC_H
 #define ZONE_H04_EVC_H
 
+#define H04_WHISLE                         0
+#define H04_GUITER                         1
+#define H04_VOICE                          2
+#define H04_LEADERSINGER                   3
 
 #endif //ZONE_H04_EVC_H
--- resource/fldmapdata/script/h04_def.h
+++ resource/fldmapdata/script/h04_def.h
@@ -5,6 +5,9 @@
 #define _H04_DEF_H_
 
 //スクリプトデータID定義
-#define SCRID_H04 (1)
+#define SCRID_H04_WHISLE (1)
+#define SCRID_H04_GUITER (2)
+#define SCRID_H04_VOICE (3)
+#define SCRID_H04_LEADERSINGER (4)
 
 #endif //_H04_DEF_H_
