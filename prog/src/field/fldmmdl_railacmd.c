58b3cceaecdc07dfd1f5b986deab9b6d73826747
Tomoya Takahashi <tomoya_takahashi@gamefreak.co.jp>
2010-01-29 09:47:28 +0000

    レール　デバックプリントが大量に出ていたの修正

--- prog/src/field/fldmmdl_railacmd.c
+++ prog/src/field/fldmmdl_railacmd.c
@@ -309,7 +309,7 @@ static int AC_RailWalk_1( MMDL * mmdl )
   p_rail = MMDL_GetRailWork( mmdl );
   MMDL_UpdateRail( mmdl );
 
-  FIELD_RAIL_WORK_DEBUG_PrintRailGrid( p_rail );
+  //FIELD_RAIL_WORK_DEBUG_PrintRailGrid( p_rail );
 
   // 新ポジションの設定
   FIELD_RAIL_WORK_GetPos( p_rail, &pos );
