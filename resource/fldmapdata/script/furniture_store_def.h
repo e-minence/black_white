193698dc169782cc03b74fa31499cf5177917d12
Sosuke Tamada <tamada@gamefreak.co.jp>
2010-01-29 03:57:44 +0000

    家具屋用スクリプトファイルを追加した

---
+++ resource/fldmapdata/script/furniture_store_def.h
@@ -0,0 +1,10 @@
+//furniture_store_scr_def.h
+//このファイルはコンバータによって作成されました
+
+#ifndef _FURNITURE_STORE_SCR_DEF_H_
+#define _FURNITURE_STORE_SCR_DEF_H_
+
+//スクリプトデータID定義
+#define SCRID_FURNITURE_STORE (10380)
+
+#endif //_FURNITURE_STORE_SCR_DEF_H_
--- resource/fldmapdata/script/scrid_offset/scr_offset_id.h
+++ resource/fldmapdata/script/scrid_offset/scr_offset_id.h
@@ -3,7 +3,7 @@
  * @file  scr_offset_id.h
  * @bfief  スクリプトIDのオフセット定義
  * @author  tamada GAMEFREAK inc.
- * @date  2010 01/28 (Thu) 23:49
+ * @date  2010 01/29 (Fri) 12:57
  *
  * このファイルはコンバータにより自動生成されています
  */
@@ -104,13 +104,16 @@
 #define ID_ID_PRESENT_SCR_OFFSET_END     (10329)
 
 #define ID_BSUBWAY_SCR_OFFSET            (10330) //バトルサブウェイ用スクリプト
-#define ID_BSUBWAY_SCR_OFFSET_END        (10339)
+#define ID_BSUBWAY_SCR_OFFSET_END        (10349)
 
-#define ID_CROWD_PEOPLE_SCR_OFFSET       (10340) //群集用スクリプト
-#define ID_CROWD_PEOPLE_SCR_OFFSET_END   (10369)
+#define ID_CROWD_PEOPLE_SCR_OFFSET       (10350) //群集用スクリプト
+#define ID_CROWD_PEOPLE_SCR_OFFSET_END   (10379)
 
-#define ID_HYOUKA_SCR_OFFSET             (10370) //ずかん評価スクリプト
-#define ID_HYOUKA_SCR_OFFSET_END         (10379)
+#define ID_HYOUKA_SCR_OFFSET             (10380) //ずかん評価スクリプト
+#define ID_HYOUKA_SCR_OFFSET_END         (10389)
+
+#define ID_FURNITURE_STORE_SCR_OFFSET    (10390) //家具屋（PDW)用スクリプト
+#define ID_FURNITURE_STORE_SCR_OFFSET_END (10394)
 
 #define ID_DEBUG_SCR_OFFSET              (12000) //デバッグ用共通スクリプト（この下にはファイルを追加しない！！）
 #define ID_DEBUG_SCR_OFFSET_END          (12199)
