abccba592d57e57f1a3615ecefca46446c956934
Sosuke Tamada <tamada@gamefreak.co.jp>
2010-02-01 07:46:28 +0000

    commit忘れを追加

--- resource/fldmapdata/script/furniture_store_def.h
+++ resource/fldmapdata/script/furniture_store_def.h
@@ -1,10 +1,10 @@
-//furniture_store_scr_def.h
+//furniture_store_def.h
 //このファイルはコンバータによって作成されました
 
-#ifndef _FURNITURE_STORE_SCR_DEF_H_
-#define _FURNITURE_STORE_SCR_DEF_H_
+#ifndef _FURNITURE_STORE_DEF_H_
+#define _FURNITURE_STORE_DEF_H_
 
 //スクリプトデータID定義
-#define SCRID_FURNITURE_STORE (10380)
+#define SCRID_FURNITURE_STORE (10390)
 
-#endif //_FURNITURE_STORE_SCR_DEF_H_
+#endif //_FURNITURE_STORE_DEF_H_
