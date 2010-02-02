4a90da8d558bbbf5d9b1d9362fada6d1bd73b8af
Hisashi Sogabe <sogabe@gamefreak.co.jp>
2010-02-02 11:45:43 +0000

    無駄なOS_TPrintfを削除

--- prog/src/system/mcss.c
+++ prog/src/system/mcss.c
@@ -353,10 +353,6 @@ void	MCSS_Draw( MCSS_SYS_WORK *mcss_sys )
 
 				anim_pos.x = FX32_CONST( anim_SRT_mc.px );
 				anim_pos.y = FX32_CONST( -anim_SRT_mc.py );
-				OS_TPrintf("x:%08x\n", mcss->scale.x / 16 );
-				OS_TPrintf("y:%08x\n", mcss->scale.y / 16 );
-				//anim_pos.x = FX_Mul( anim_pos.x, FX_Div( mcss->scale.x, 16 ) );
-				//anim_pos.y = FX_Mul( anim_pos.y, FX_Div( mcss->scale.y, 16 ) );
 				anim_pos.x = FX_Mul( anim_pos.x, mcss->scale.x / 16 );
 				anim_pos.y = FX_Mul( anim_pos.y, mcss->scale.y / 16 );
 			}
