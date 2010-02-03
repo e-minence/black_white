4fb1b35c25d1e6ea908121bed54978c3b535543e
Yukiko Hozumi <hozumi_yukiko@gamefreak.co.jp>
2010-02-03 06:15:09 +0000

    国連共通フロアにNPCを配置

--- resource/fldmapdata/eventdata/tmp/c11r0101.h
+++ resource/fldmapdata/eventdata/tmp/c11r0101.h
@@ -9,5 +9,12 @@
 #ifndef ZONE_C11R0101_EVC_H
 #define ZONE_C11R0101_EVC_H
 
+#define UN_ENTRANCE_RECEPTION_01           0
+#define UN_ENTRANCE_RECEPTION_02           1
+#define UN_ENTRANCE_RECEPTION_03           2
+#define UN_ENTRANCE_BACKPACKERM_01         3
+#define UN_ENTRANCE_POLICEMAN_01           4
+#define UN_ENTRANCE_BABYBOY1_01            5
+#define UN_ENTRANCE_DANCER_01              6
 
 #endif //ZONE_C11R0101_EVC_H
--- resource/fldmapdata/eventdata/tmp/c11r0201.h
+++ resource/fldmapdata/eventdata/tmp/c11r0201.h
@@ -8,5 +8,11 @@
 #ifndef ZONE_C11R0201_EVC_H
 #define ZONE_C11R0201_EVC_H
 
+#define UN_ROOM_TALKER                     0
+#define UN_WKOBJCODE00                     1
+#define UN_WKOBJCODE01                     2
+#define UN_WKOBJCODE02                     3
+#define UN_WKOBJCODE03                     4
+#define UN_WKOBJCODE04                     5
 
 #endif //ZONE_C11R0201_EVC_H
