9bc83757b8410cba8fc4962e9c36f02b35b40469
Nozomu Saito <saito_nozomu@gamefreak.co.jp>
2010-02-03 03:37:39 +0000

    //国連内部屋スクリプト更新

--- resource/fldmapdata/script/c11r0201_def.h
+++ resource/fldmapdata/script/c11r0201_def.h
@@ -5,6 +5,12 @@
 #define _C11R0201_DEF_H_
 
 //スクリプトデータID定義
-#define SCRID_C11R0201 (1)
+#define SCRID_ZONE_C11R0201 (1)
+#define SCRID_UN_ROOM_TALKER (2)
+#define SCRID_UN_WKOBJCODE00 (3)
+#define SCRID_UN_WKOBJCODE01 (4)
+#define SCRID_UN_WKOBJCODE02 (5)
+#define SCRID_UN_WKOBJCODE03 (6)
+#define SCRID_UN_WKOBJCODE04 (7)
 
 #endif //_C11R0201_DEF_H_
