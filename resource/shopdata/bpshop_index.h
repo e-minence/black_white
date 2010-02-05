c3d4d8bfc856c50540800c35890a642fb876cb9f
Akito Mori <mori@gamefreak.co.jp>
2010-02-05 09:22:32 +0000

    バトルポイントショップ用のコンバーターを追加

---
+++ resource/shopdata/bpshop_index.h
@@ -0,0 +1,16 @@
+//====================================================================
+// @file  bpshop_index.h
+// @brief BPショップデータ指定用ヘッダ
+//
+// ●bp_item.xlsをresource/shopdata/でコンバートして出力しています
+//
+//====================================================================
+
+#ifndef __BPSHOP_INDEX_H__
+#define __BPSHOP_INDEX_H__
+
+#define BP_SHOP1    ( 0 )  // BPショップ１号
+#define BP_WAZA1    ( 1 )  // BP技ショップ１号
+
+
+#endif
--- resource/shopdata/bpshopdata.rb
+++ resource/shopdata/bpshopdata.rb
@@ -1,13 +1,13 @@
 #===================================================================
 #
-# @brief  ショップデータコンバーター
+# @brief  BPショップデータコンバーター
 #
-# itemym.hとfs_item.csvを読み込んでショップデータを作成します。
+# itemym.hとbp_item.csvを読み込んでBPショップデータを作成します。
 #
 # ex)
-# ruby shopdata.rb fs_item.csv itemsym.h > fs_item.cdat fs_item_index.h
+# ruby bpshopdata.rb bp_item.csv itemsym.h > bp_item.cdat bp_item_inde.h
 #
-# @data   09.10.19
+# @data   10.02.05
 # @author mori
 #
 #===================================================================
@@ -28,11 +28,11 @@ namelist   = Array.new	# 配列名
 maxlist    = Array.new	# 配列MAX名
 outbuf	   = ""			# 出力ショップデータ群
 headerbuf  = ""			# スクリプトで指定するためのヘッダ
+lastnamebuf = ""		# ショップ定義名を一回分覚えておく
 line_count = 0
 shop_count = 0
+closeflag  = 0			# 初回だけは配列定義を括る必要が無いのでフラグ
 
-#STDERR.print("エラー\n")
-#exit(1)
 
 
 #---------------------------------------
@@ -58,26 +58,24 @@ file.close
 #---------------------------------------
 reader = CSV.open( ARGV[0], 'r')
 
-# 3行読み飛ばし
-reader.shift
-reader.shift
+# 1行読み飛ばし
 reader.shift
 
 # ショップデータヘッダ部分出力
 outbuf += sprintf( "//====================================================================\n")
 outbuf += sprintf( "// @file  %s\n", ARGV[2])
-outbuf += sprintf( "// @brief ショップデータ\n")
+outbuf += sprintf( "// @brief BPショップデータ\n")
 outbuf += sprintf( "//\n")
-outbuf += sprintf( "// ●fs_item.xlsをresource/shopdata/でコンバートして出力しています\n")
+outbuf += sprintf( "// ●bp_item.xlsをresource/shopdata/でコンバートして出力しています\n")
 outbuf += sprintf( "//\n")
 outbuf += sprintf( "//====================================================================\n\n")
 
 # ヘッダファイルヘッダ部分出力
 headerbuf += sprintf( "//====================================================================\n")
 headerbuf += sprintf( "// @file  %s\n",ARGV[3])
-headerbuf += sprintf( "// @brief ショップデータ指定用ヘッダ\n")
+headerbuf += sprintf( "// @brief BPショップデータ指定用ヘッダ\n")
 headerbuf += sprintf( "//\n")
-headerbuf += sprintf( "// ●fs_item.xlsをresource/shopdata/でコンバートして出力しています\n")
+headerbuf += sprintf( "// ●bp_item.xlsをresource/shopdata/でコンバートして出力しています\n")
 headerbuf += sprintf( "//\n")
 headerbuf += sprintf( "//====================================================================\n\n")
 headerbuf += sprintf( "#ifndef __%s__\n",   ARGV[3].to_s.upcase.tr(".","_"))
@@ -86,41 +84,57 @@ headerbuf += sprintf( "#define __%s__\n\n", ARGV[3].to_s.upcase.tr(".","_"))
 
 # CSVデータから出力
 reader.each { |row|
-	if row[0].to_s=="" then
-		break
-	end
-	outbuf += sprintf( "// %s\n", row[0] )
-	outbuf += sprintf( "static const u16 %s[] = {\n", row[1].to_s )
-	namelist << row[1].to_s
-
-	# どうぐデータ出力
-	(row.length-2).times{|i|
-		if row[i+2]==nil then
-			outbuf += sprintf("\t\t0xffff,\n")
-		else
-			if hash[row[i+2].to_s]!=nil then
-				outbuf += sprintf("\t\t%s,\n", hash[row[i+2].to_s])
-			else
-				STDERR.print( ""+row[0]+"の行、「"+row[i+2].to_s+"」と言うどうぐは存在しません\n")
-				exit(1)
-			end
+	# 1列目に名前が入っている場合はBPショップデータ定義開始
+	if row[0].to_s!="" then
+	
+		#前の行までのデータ定義を終了する
+		if closeflag==1 then
+#			outbuf += sprintf("\t\t{0xffff, 0xffff, },\n")
+			outbuf += sprintf("};\n")
+			outbuf += sprintf("#define %s_MAX	(NELEMS(%s))\n\n", lastnamebuf.upcase,lastnamebuf)
+			maxlist << sprintf( "%s_MAX",lastnamebuf.upcase,lastnamebuf )
 		end
-	}
-	outbuf += sprintf("};\n")
-	outbuf += sprintf("#define %s_MAX	(NELEMS(%s))\n\n", row[1].to_s.upcase,row[1].to_s)
-	maxlist << sprintf( "%s_MAX",row[1].to_s.upcase,row[1].to_s )
-	shop_count=shop_count+1
 
-	if shop_count>=FIXSHOP_START_LINE then
-		headerbuf += sprintf("#define %s    ( %d )  // %s\n", row[1].to_s.upcase,
-		shop_count-FIXSHOP_START_LINE,row[0].to_s)
+		#新しいデータ定義の宣言
+		outbuf += sprintf( "// %s\n", row[0] )
+		outbuf += sprintf( "static const SHOP_ITEM %s[][2] = {\n", row[1].to_s )
+		namelist << row[1].to_s
+		closeflag = 1
+		lastnamebuf = row[1].to_s
+		headerbuf += sprintf("#define %s    ( %d )  // %s\n", 
+							 row[1].to_s.upcase,shop_count,row[0].to_s)
+		shop_count=shop_count+1
+					     		
+
+	end
+
+	if row[3].to_s!="" then
+		if row[4].to_s=="" then
+			STDERR.print( ""+row[3]+"の横に値段が書いてありません\n")
+			exit(1)
+		end
+		if hash[row[3].to_s]!=nil then
+			outbuf += sprintf("\t\t{ %s,\t%s, },\n", hash[row[3].to_s],row[4].to_s)
+		else
+			STDERR.print( "「"+row[3].to_s+"」と言うどうぐは存在しません\n")
+			exit(1)
+		end
+
+		
 	end
 }
 reader.close
 
+#最後の定義は終端が無いのでココで括る
+#outbuf += sprintf("\t\t{0xffff, 0xffff, },\n")
+outbuf += sprintf("};\n")
+outbuf += sprintf("#define %s_MAX	(NELEMS(%s))\n\n", lastnamebuf.upcase,lastnamebuf)
+maxlist << sprintf( "%s_MAX",lastnamebuf.upcase,lastnamebuf )
+
+
 # 一括アクセス用に配列ポインタの配列を定義
 outbuf += sprintf("\n\n// 一括アクセス用配列定義\n")
-outbuf += sprintf("static const u16 *shop_data_table[]={\n")
+outbuf += sprintf("static const SHOP_ITEM *bp_shop_data_table[]={\n")
 namelist.each{ |name|
 	outbuf += sprintf("\t%s,\n", name)
 }
@@ -132,7 +146,7 @@ headerbuf += sprintf( "\n\n#endif\n" )
 
 # 各ショップのアイテム数の配列化
 outbuf += sprintf("\n\n// 各ショップ販売最大数取得用\n")
-outbuf += sprintf("static const u8 shop_itemnum_table[]={\n")
+outbuf += sprintf("static const u8 bp_shop_itemnum_table[]={\n")
 maxlist.each{ |maxdef|
 	outbuf += sprintf("\t%s,\n", maxdef)
 }
