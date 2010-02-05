4f2c2407d828f97339bc81df715cadfb7ab583c1
Katsunori Suginaka <suginaka_katsunori@gamefreak.co.jp>
2010-02-05 10:30:06 +0000

    R06の接続を追加致しました。

--- resource/fldmapdata/eventdata/tmp/r06r0301.h
+++ resource/fldmapdata/eventdata/tmp/r06r0301.h
@@ -2,6 +2,7 @@
 #ifndef ZONE_R06R0301_EVD_H
 #define ZONE_R06R0301_EVD_H
 
+#define DOOR_ID_R06R0301_R06_EXIT01       0
 
 #endif //ZONE_R06R0301_EVD_H
 //‚±‚Ìƒtƒ@ƒCƒ‹‚Í../data/r06r0301.mev‚©‚ç©“®¶¬‚³‚ê‚Ü‚µ‚½
