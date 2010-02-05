62b00fd21694456b8050be2833690f3b08d48a9e
Sosuke Tamada <tamada@gamefreak.co.jp>
2010-02-05 13:54:37 +0000

    わざ教え用スクリプト追加

---
+++ resource/fldmapdata/script/skill_teach_scr_def.h
@@ -0,0 +1,11 @@
+//skill_teach_scr_def.h
+//このファイルはコンバータによって作成されました
+
+#ifndef _SKILL_TEACH_SCR_DEF_H_
+#define _SKILL_TEACH_SCR_DEF_H_
+
+//スクリプトデータID定義
+#define SCRID_WAZA_TEACHER_DRAGON (1)
+#define SCRID_WAZA_TEACHER_POWERFUL (2)
+
+#endif //_SKILL_TEACH_SCR_DEF_H_
