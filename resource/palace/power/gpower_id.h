62f9285eba6e1c4dd480f3bb305ebcf99388f9fe
Junichi Masuda <matsuda@gamefreak.co.jp>
2010-02-07 09:16:03 +0000

    パワーデータコンバータ修正 gpower_id.hをコンバータから出力するようにしたのでSVN管理から外した

---
+++ resource/palace/power/gpower_id.h
@@ -0,0 +1,32 @@
+//============================================================
+//       power_conv.rb で出力されたファイルです
+//============================================================
+
+#pragma once
+
+typedef enum{
+	GPOWER_ID_MAX = 83,		//Gパワー最大数
+	GPOWER_ID_NULL = GPOWER_ID_MAX,		//Gパワーが発動していない
+}GPOWER_ID;
+
+
+
+//効果系統
+typedef enum{
+	GPOWER_TYPE_NULL,
+	GPOWER_TYPE_1,
+	GPOWER_TYPE_2,
+	GPOWER_TYPE_3,
+	GPOWER_TYPE_4,
+	GPOWER_TYPE_5,
+	GPOWER_TYPE_6,
+	GPOWER_TYPE_7,
+	GPOWER_TYPE_8,
+	GPOWER_TYPE_9,
+	GPOWER_TYPE_10,
+	GPOWER_TYPE_11,
+	GPOWER_TYPE_12,
+	GPOWER_TYPE_13,
+	GPOWER_TYPE_MAX,
+}GPOWER_TYPE;
+
--- resource/palace/power/power_conv.rb
+++ resource/palace/power/power_conv.rb
@@ -43,6 +43,8 @@ end
 
 PowerData = [];
 
+POWER_TYPE_ARRAY_MAX = 100;
+PowerType = [];
 
 =begin
 //==================================================================
@@ -85,8 +87,6 @@ def CsvConvFileCheck()
     cell += 1;
     PowerData[s].point = line[cell];
     cell += 1;
-    PowerData[s].time = line[cell];
-    cell += 1;
     PowerData[s].msg_id_title = line[cell];
     cell += 1;
     PowerData[s].msg_id_explain = line[cell];
@@ -94,6 +94,8 @@ def CsvConvFileCheck()
     PowerData[s].type = line[cell];
     cell += 1;
     PowerData[s].data = line[cell];
+    cell += 3;
+    PowerData[s].time = line[cell];
     cell += 1;
 
     s += 1;
@@ -111,9 +113,32 @@ end
 =end
 def DataConv()
   for i in 0..PowerData.size-1
-    
+    if(PowerData[i].type == "1" || PowerData[i].type == "2" || PowerData[i].type == "3" \
+        || PowerData[i].type == "9" || PowerData[i].type == "10" || PowerData[i].type == "11")
+      num = PowerData[i].data.to_f;
+      num *= 100; #小数を整数に変換する(100倍の固定小数)
+      PowerData[i].data = (0x100 * num / 100).to_i;  #下位8bit固定小数にさらに変換
+    end
   end
-
+  
+  #効果系統を抽出
+  power_type_count = 0;
+  for i in 0..POWER_TYPE_ARRAY_MAX 
+    PowerType[i] = "NULL";
+  end
+  for i in 0..PowerData.size-1
+    type = PowerData[i].type;
+    for s in 0..POWER_TYPE_ARRAY_MAX 
+      if(type == PowerType[s])
+        break;
+      end
+      if(PowerType[s] == "NULL")
+        PowerType[s] = type;
+        power_type_count += 1;
+        break;
+      end
+    end
+  end
 end
 
 
@@ -134,21 +159,44 @@ def DataFileOutput()
 
     for i in 0..PowerData.size-1
       file.printf("\t{\t//%d\n", i);
-      file.printf("\t\t%s,\t\t//no\n", PowerData[i].no);
       file.printf("\t\t%s,\t\t//level_w\n", PowerData[i].level_w);
       file.printf("\t\t%s,\t\t//level_b\n", PowerData[i].level_b);
-      file.printf("\t\t%s,\t\t//time\n", PowerData[i].time);
       file.printf("\t\t%s,\t\t//point\n", PowerData[i].point);
       file.printf("\t\t%s,\t\t//msg_id_title\n", PowerData[i].msg_id_title);
       file.printf("\t\t%s,\t\t//msg_id_explain\n", PowerData[i].msg_id_explain);
-      file.printf("\t\t%s,\t\t//type\n", PowerData[i].type);
       file.printf("\t\t%s,\t\t//data\n", PowerData[i].data);
+      file.printf("\t\t%s,\t\t//type\n", PowerData[i].type);
+      file.printf("\t\t%s,\t\t//time\n", PowerData[i].time);
       file.printf("\t},\n");
     end
     
     file.printf("};\n");
   }
 
+  File.open("gpower_id.h", "w"){|file|
+    file.printf("//============================================================\n");
+    file.printf("//       power_conv.rb で出力されたファイルです\n");
+    file.printf("//============================================================\n\n");
+    file.printf("#pragma once\n\n");
+    
+    file.printf("typedef enum{\n");
+    file.printf("\tGPOWER_ID_MAX = %d,\t\t//Gパワー最大数\n", PowerData.size);
+    file.printf("\tGPOWER_ID_NULL = GPOWER_ID_MAX,\t\t//Gパワーが発動していない\n");
+    file.printf("}GPOWER_ID;\n\n");
+    
+    file.printf("\n\n//効果系統\n");
+    file.printf("typedef enum{\n");
+    file.printf("\tGPOWER_TYPE_NULL,\n");
+    for i in 0..POWER_TYPE_ARRAY_MAX
+      if(PowerType[i] == "NULL")
+        break;
+      end
+      file.printf("\tGPOWER_TYPE_%s,\n", PowerType[i]);
+    end
+    file.printf("\tGPOWER_TYPE_MAX,\n");
+    file.printf("}GPOWER_TYPE;\n\n");
+  }
+  
 end
 
 
