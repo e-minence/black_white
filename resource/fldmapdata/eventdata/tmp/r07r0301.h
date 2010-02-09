21d3f767046444dbf9e51c2d229c68fff27dd57a
Toshihiro Obata <obata_toshihiro@gamefreak.co.jp>
2010-02-09 10:13:02 +0000

    ゾーン追加時の追加忘れをコミット

---
+++ resource/fldmapdata/eventdata/tmp/r07r0301.h
@@ -0,0 +1,18 @@
+//このファイルは../data/r07r0301.mevから自動生成されました
+#ifndef ZONE_R07R0301_EVD_H
+#define ZONE_R07R0301_EVD_H
+
+
+#endif //ZONE_R07R0301_EVD_H
+//このファイルは../data/r07r0301.mevから自動生成されました
+#ifndef ZONE_R07R0301_EVC_H
+#define ZONE_R07R0301_EVC_H
+
+
+#endif //ZONE_R07R0301_EVC_H
+//このファイルは../data/r07r0301.mevから自動生成されました
+#ifndef ZONE_R07R0301_EVB_H
+#define ZONE_R07R0301_EVB_H
+
+
+#endif //ZONE_R07R0301_EVB_H
---
+++ resource/fldmapdata/script/r07r0301_def.h
@@ -0,0 +1,10 @@
+//r07r0301_def.h
+//このファイルはコンバータによって作成されました
+
+#ifndef _R07R0301_DEF_H_
+#define _R07R0301_DEF_H_
+
+//スクリプトデータID定義
+#define SCRID_R07R0301 (1)
+
+#endif //_R07R0301_DEF_H_
