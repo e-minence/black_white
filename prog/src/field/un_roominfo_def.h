19f0c6574757661921ed4a52a9293aa63702a9c2
Nozomu Saito <saito_nozomu@gamefreak.co.jp>
2010-02-12 04:18:58 +0000

    //国連部屋情報タイプ定義ファイル追加

---
+++ prog/src/field/un_roominfo_def.h
@@ -0,0 +1,13 @@
+//======================================================================
+/**
+ * @file  un_roominfo_def.h
+ * @brief  国連関連 部屋情報タイプ定義
+ * @author  Saito
+ */
+//======================================================================
+#pragma once
+
+#define UN_ROOMINFO_TYPE_OBJNUM   (0)     //客の数
+#define UN_ROOMINFO_TYPE_COUNTRY  (1)     //今いるフロアの国コード
+#define UN_ROOMINFO_TYPE_FLOOR  (2)       //今いるフロア数
+
