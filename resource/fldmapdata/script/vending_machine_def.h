1ba679d627666ca8ccd2abcb8717a10894918c24
Nozomu Saito <saito_nozomu@gamefreak.co.jp>
2010-02-15 09:49:12 +0000

    //自動販売機スクリプト追加

---
+++ resource/fldmapdata/script/vending_machine_def.h
@@ -0,0 +1,10 @@
+//vending_machine_def.h
+//このファイルはコンバータによって作成されました
+
+#ifndef _VENDING_MACHINE_DEF_H_
+#define _VENDING_MACHINE_DEF_H_
+
+//スクリプトデータID定義
+#define SCRID_VENDING_MACHINE01 (10400)
+
+#endif //_VENDING_MACHINE_DEF_H_
