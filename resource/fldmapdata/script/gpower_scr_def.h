e19812fe0d61e0412d1f56c93a705b5f1c66385b
Miyuki Iwasawa <iwasawa@gamefreak.co.jp>
2010-02-17 08:03:11 +0000

    Gパワーイベントスクリプト専用evファイルを追加

---
+++ resource/fldmapdata/script/gpower_scr_def.h
@@ -0,0 +1,11 @@
+//gpower_scr_def.h
+//このファイルはコンバータによって作成されました
+
+#ifndef _GPOWER_SCR_DEF_H_
+#define _GPOWER_SCR_DEF_H_
+
+//スクリプトデータID定義
+#define SCRID_GPOWER_EVENT_START (10425)
+#define SCRID_GPOWER_EVENT_END (10426)
+
+#endif //_GPOWER_SCR_DEF_H_
--- resource/fldmapdata/script/scrid_offset/scr_offset_id.h
+++ resource/fldmapdata/script/scrid_offset/scr_offset_id.h
@@ -3,7 +3,7 @@
  * @file  scr_offset_id.h
  * @bfief  スクリプトIDのオフセット定義
  * @author  tamada GAMEFREAK inc.
- * @date  2010 02/15 (Mon) 19:12
+ * @date  2010 02/17 (Wed) 15:29
  *
  * このファイルはコンバータにより自動生成されています
  */
@@ -126,6 +126,9 @@
 
 #define ID_ELEVATOR_OFFSET               (10405) //エレベーター共通スクリプト
 #define ID_ELEVATOR_OFFSET_END           (10424)
+
+#define ID_GPOWER_SCR_OFFSET             (10425) //Gパワーイベントスクリプト
+#define ID_GPOWER_SCR_OFFSET_END         (10429)
 
 #define ID_DEBUG_SCR_OFFSET              (12000) //デバッグ用共通スクリプト（この下にはファイルを追加しない！！）
 #define ID_DEBUG_SCR_OFFSET_END          (12199)
