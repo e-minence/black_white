e9ca429bd85d07a57e039f34e6fea26e4b656849
Akito Mori <mori@gamefreak.co.jp>
2010-02-17 08:38:23 +0000

    主人公の立ち絵４方向のみで構成されたセルアニメを新しく追加（男女）

--- resource/wifi_leading_obj/conv.bat
+++ resource/wifi_leading_obj/conv.bat
@@ -9,6 +9,12 @@ g2dcvtr heroine_gts.nce -br
 rename hero.NCGR hero_gts.NCGR
 rename heroine.NCGR heroine_gts.NCGR
 
+g2dcvtr hero_simple.nce -br
+g2dcvtr heroine_simple.nce -br
+
+rename hero.NCGR hero_simple.NCGR
+rename heroine.NCGR heroine_simple.NCGR
+
 g2dcvtr hero.nce -br
 g2dcvtr heroine.nce -br
 g2dcvtr hero_ine_kage.nce -br
