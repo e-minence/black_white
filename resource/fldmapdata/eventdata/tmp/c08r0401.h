dd19aec6422a954c3bf15b8866af970f8444a78f
Mai Mizuguchi <mizuguchi_mai@gamefreak.co.jp>
2010-02-18 11:00:35 +0000

    c08r0401：OBJ配置

--- resource/fldmapdata/eventdata/tmp/c08r0401.h
+++ resource/fldmapdata/eventdata/tmp/c08r0401.h
@@ -10,6 +10,9 @@
 #ifndef ZONE_C08R0401_EVC_H
 #define ZONE_C08R0401_EVC_H
 
+#define C08R0401_BOY2_01                   0
+#define C08R0401_OLDMAN1_01                1
+#define C08R0401_GIRL4_01                  2
 
 #endif //ZONE_C08R0401_EVC_H
 //‚±‚Ìƒtƒ@ƒCƒ‹‚Í../data/c08r0401.mev‚©‚ç©“®¶¬‚³‚ê‚Ü‚µ‚½
