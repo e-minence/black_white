44157603b7451cbd56865a8072eef203f99f8b43
Nozomu Saito <saito_nozomu@gamefreak.co.jp>
2010-02-19 04:32:32 +0000

    //トライアルハウス関連ファイル追加

---
+++ prog/src/field/trial_house_ptr.h
@@ -0,0 +1,12 @@
+//======================================================================
+/**
+ * @file  trial_house_ptr.h
+ * @brief トライアルハウスワークポインタ
+ * @author  Saito
+ */
+//======================================================================
+
+#pragma once
+
+typedef struct TRIAL_HOUSE_WORK_tag * TRIAL_HOUSE_WORK_PTR;
+
