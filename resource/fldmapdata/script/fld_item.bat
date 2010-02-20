f3f453b40e3c3b3fc9d29e1d37909a8a2b1ee963
Sosuke Tamada <tamada@gamefreak.co.jp>
2010-02-20 12:13:56 +0000

    fld_item.evをWindows行末コードに変換するようにバッチファイルを修正。 _SYSWIN_MSG_DOWN( msg_id ) --> _SYSWIN_MSG( msg_id, WIN_DOWN )への置換え。 動作確認は指定ないがバイナリレベルでの差分がないことを確認

--- resource/fldmapdata/script/fld_item.bat
+++ resource/fldmapdata/script/fld_item.bat
@@ -2,3 +2,4 @@
 @..\..\..\tools\exceltool\excelseetconv.exe -o scr_flditem/fld_item.csv -p 1 -s csv scr_flditem/fld_item.xls
 @ruby scr_flditem/fld_item.rb scr_flditem/fld_item.csv ../../../prog/include/item/itemsym.h scr_flditem/fld_item_base.ev
 @ruby scr_tools/ev_def.rb fld_item.ev scrid_offset/scr_offset_id.h
+@unix2dos -D fld_item.ev
