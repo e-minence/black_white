1e20e708c1f0bdb17bb4b57b85a059445b766588
Nozomu Saito <saito_nozomu@gamefreak.co.jp>
2010-02-21 08:05:12 +0000

    //トライアルハウスランキング定義追加

---
+++ prog/include/savedata/th_rank_def.h
@@ -0,0 +1,18 @@
+//==============================================================================================
+/**
+ * @file	th_rank_def.h
+ * @brief	トライアルハウスランキング定義
+ * @author	Nozomu Saito
+ */
+//==============================================================================================
+
+#pragma once
+
+#define TH_RANK_BEGINNER  (0)   //ビギナーランク
+#define TH_RANK_NOVICE    (1)   //ノービスランク
+#define TH_RANK_NORMAL    (2)   //ノーマルランク
+#define TH_RANK_SUPER     (3)   //スーパーランク
+#define TH_RANK_HYPER     (4)   //ハイパーランク
+#define TH_RANK_ELITE     (5)   //エリートランク
+#define TH_RANK_MASTER    (6)   //マスターランク
+
