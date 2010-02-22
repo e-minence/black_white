81a9aa653638f3cddf50c06635f4127db5988568
Shinya Takahashi <takahashi@gamefreak.co.jp>
2010-02-22 23:03:15 +0000

    トレーナー配置

--- resource/fldmapdata/eventdata/tmp/r13.h
+++ resource/fldmapdata/eventdata/tmp/r13.h
@@ -13,6 +13,21 @@
 
 #define R13_ROCK_01                        0
 #define R13_ROCK_02                        1
+#define R13_TREE_01                        2
+#define R13_TREE_02                        3
+#define R13_GIRL2_01                       4
+#define R13_BOY2_01                        5
+#define R13_AMBRELLA_01                    6
+#define R13_FIGHTERM_01                    7
+#define R13_FUTAGO_01                      8
+#define R13_FUTAGO_01_2                    9
+#define R13_GENTLEMAN_01                  10
+#define R13_LADY_01                       11
+#define R13_MIDDLEMAN_01                  12
+#define R13_FISHING_01                    13
+#define R13_FISHING_02                    14
+#define R13_FISHING_03                    15
+#define R13_FISHING_04                    16
 
 #endif //ZONE_R13_EVC_H
 //Ç±ÇÃÉtÉ@ÉCÉãÇÕ../data/r13.mevÇ©ÇÁé©ìÆê∂ê¨Ç≥ÇÍÇ‹ÇµÇΩ
