2bc342d085656bc31b4a9689f9d93ae6d8e6ef2d
Hisashi Sogabe <sogabe@gamefreak.co.jp>
2010-02-22 02:09:03 +0000

    技エフェクト更新

--- resource/wazaeffect/graphic/mkscr.rb
+++ resource/wazaeffect/graphic/mkscr.rb
@@ -17,6 +17,7 @@
     fp_w.print( "\"" + name + ".NCLR\"\n" )
   }
 
+=begin
   #OBJファイルリスト生成
   Dir::glob("./*.nce").each {|f|
     name = File::basename( f, '.*' )
@@ -25,5 +26,40 @@
     fp_w.print( "\"" + name + ".NCER\"\n" )
     fp_w.print( "\"" + name + ".NANR\"\n" )
   }
+=end
+  fp_w.print( "\"ball_anim.NCER\"\n" )
+  fp_w.print( "\"ball_anim.NANR\"\n" )
+  fp_w.print( "\"ball_03_anim.NCGR\"\n" )
+  fp_w.print( "\"ball_03_anim.NCLR\"\n" )
+  fp_w.print( "\"ball_02_anim.NCGR\"\n" )
+  fp_w.print( "\"ball_02_anim.NCLR\"\n" )
+  fp_w.print( "\"ball_01_anim.NCGR\"\n" )
+  fp_w.print( "\"ball_01_anim.NCLR\"\n" )
+  fp_w.print( "\"ball_anim.NCGR\"\n" )
+  fp_w.print( "\"ball_anim.NCLR\"\n" )
+  fp_w.print( "\"ball_04_anim.NCGR\"\n" )
+  fp_w.print( "\"ball_04_anim.NCLR\"\n" )
+  fp_w.print( "\"ball_05_anim.NCGR\"\n" )
+  fp_w.print( "\"ball_05_anim.NCLR\"\n" )
+  fp_w.print( "\"ball_06_anim.NCGR\"\n" )
+  fp_w.print( "\"ball_06_anim.NCLR\"\n" )
+  fp_w.print( "\"ball_07_anim.NCGR\"\n" )
+  fp_w.print( "\"ball_07_anim.NCLR\"\n" )
+  fp_w.print( "\"ball_08_anim.NCGR\"\n" )
+  fp_w.print( "\"ball_08_anim.NCLR\"\n" )
+  fp_w.print( "\"ball_09_anim.NCGR\"\n" )
+  fp_w.print( "\"ball_09_anim.NCLR\"\n" )
+  fp_w.print( "\"ball_10_anim.NCGR\"\n" )
+  fp_w.print( "\"ball_10_anim.NCLR\"\n" )
+  fp_w.print( "\"ball_11_anim.NCGR\"\n" )
+  fp_w.print( "\"ball_11_anim.NCLR\"\n" )
+  fp_w.print( "\"ball_13_anim.NCGR\"\n" )
+  fp_w.print( "\"ball_13_anim.NCLR\"\n" )
+  fp_w.print( "\"ball_14_anim.NCGR\"\n" )
+  fp_w.print( "\"ball_14_anim.NCLR\"\n" )
+  fp_w.print( "\"ball_12_anim.NCGR\"\n" )
+  fp_w.print( "\"ball_12_anim.NCLR\"\n" )
+  fp_w.print( "\"ball_15_anim.NCGR\"\n" )
+  fp_w.print( "\"ball_15_anim.NCLR\"\n" )
 
   fp_w.close
