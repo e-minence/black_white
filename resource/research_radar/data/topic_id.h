8ceec5d7a073ba8181201b865117fb6b69ef17d5
Shigeru Ohmori <ohmori@gamefreak.co.jp>
2010-02-23 09:47:15 +0000

    レーダーのメッセージとエクセルデータの更新

--- resource/research_radar/data/topic_id.h
+++ resource/research_radar/data/topic_id.h
@@ -1,14 +1,14 @@
 // コンバータにより生成
-#define TOPIC_ID_STYLE (0) //「ちょうさこうもく1」
-#define TOPIC_ID_PERSONAL (1) //「ちょうさこうもく2」
-#define TOPIC_ID_POKEMON (2) //「ちょうさこうもく3」
-#define TOPIC_ID_TREND (3) //「ちょうさこうもく4」
-#define TOPIC_ID_PARTNER (4) //「ちょうさこうもく5」
-#define TOPIC_ID_FAVARITE (5) //「ちょうさこうもく6」
-#define TOPIC_ID_HOBBY (6) //「ちょうさこうもく7」
-#define TOPIC_ID_SCOOL (7) //「ちょうさこうもく8」
-#define TOPIC_ID_PLAY (8) //「ちょうさこうもく9」
-#define TOPIC_ID_DETAIL (9) //「ちょうさこうもく10」
+#define TOPIC_ID_STYLE (0) //「どうあそんでるか　ちょうさ」
+#define TOPIC_ID_PERSONAL (1) //「どんなひとが　いるかの　ちょうさ」
+#define TOPIC_ID_POKEMON (2) //「すきなポケモンの　ちょうさ」
+#define TOPIC_ID_TREND (3) //「みんなの　りそうを　ちょうさ」
+#define TOPIC_ID_PARTNER (4) //「すきなひとの　ちょうさ」
+#define TOPIC_ID_FAVARITE (5) //「みんなの　このみを　ちょうさ」
+#define TOPIC_ID_HOBBY (6) //「みんなの　しゅみを　ちょうさ」
+#define TOPIC_ID_SCOOL (7) //「がっこうせいかつを　ちょうさ」
+#define TOPIC_ID_PLAY (8) //「どんな　あそびがすきか　ちょうさ」
+#define TOPIC_ID_DETAIL (9) //「ポケモンのことをくわしくちょうさ」
 #define TOPIC_ID_NUM   (10)    // 調査項目IDの数
 #define TOPIC_ID_MAX   (TOPIC_ID_NUM - 1)  // 調査項目IDの最大値
 #define TOPIC_ID_DUMMY (0xff)              // 調査項目IDのダミー値
