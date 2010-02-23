035f7456ce517f066acc866a301d14eddf750618
Kazumasa Iwao <iwao_kazumasa@gamefreak.co.jp>
2010-02-23 14:56:57 +0000

    ミュージカル：マップ修正及びNPC配置

--- resource/fldmapdata/eventdata/tmp/c04r0203.h
+++ resource/fldmapdata/eventdata/tmp/c04r0203.h
@@ -9,6 +9,13 @@
 #ifndef ZONE_C04R0203_EVC_H
 #define ZONE_C04R0203_EVC_H
 
+#define C04R0203_POLICEMAN_01              0
+#define C04R0203_POLICEMAN_02              1
+#define C04R0203_BABYBOY1_01               2
+#define C04R0203_GIRL3_01                  3
+#define C04R0203_OLDWOMAN1_01              4
+#define C04R0203_BREEDERW_01               5
+#define C04R0203_GIRL2_01                  6
 
 #endif //ZONE_C04R0203_EVC_H
 //Ç±ÇÃÉtÉ@ÉCÉãÇÕ../data/c04r0203.mevÇ©ÇÁé©ìÆê∂ê¨Ç≥ÇÍÇ‹ÇµÇΩ
