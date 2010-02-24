3978654ad4bfdcdbcbd55e8bf42408f646e0d48d
Naoto Murakami <murakami_naoto@gamefreak.co.jp>
2010-02-24 07:38:01 +0000

    スクリプトC02　C02GYM

--- resource/fldmapdata/eventdata/tmp/c02.h
+++ resource/fldmapdata/eventdata/tmp/c02.h
@@ -41,5 +41,7 @@
 #define ZONE_C02_EVB_H
 
 #define C02_HIDE_ITEM_00                  0
+#define BG_C02_SIGN1_01                   1
+#define BG_C02_SIGN2_01                   2
 
 #endif //ZONE_C02_EVB_H
