9b63b0b6f79a947190d94e20562b384094414619
Shinya Takahashi <takahashi@gamefreak.co.jp>
2010-02-24 14:49:31 +0000

    配置修正

--- resource/fldmapdata/eventdata/tmp/d08r0401.h
+++ resource/fldmapdata/eventdata/tmp/d08r0401.h
@@ -10,6 +10,7 @@
 #ifndef ZONE_D08R0401_EVC_H
 #define ZONE_D08R0401_EVC_H
 
+#define D08R0401_ROCK_01                   0
 
 #endif //ZONE_D08R0401_EVC_H
 //‚±‚Ìƒtƒ@ƒCƒ‹‚Í../data/d08r0401.mev‚©‚ç©“®¶¬‚³‚ê‚Ü‚µ‚½
