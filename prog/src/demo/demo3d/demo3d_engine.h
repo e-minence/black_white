dc3e594682460346eead64f6bce13787ef643591
Miyuki Iwasawa <iwasawa@gamefreak.co.jp>
2010-02-24 05:53:46 +0000

    demo3d　Nの城デモ　2画面ぶち抜き設定適用

--- prog/src/demo/demo3d/demo3d_engine.h
+++ prog/src/demo/demo3d/demo3d_engine.h
@@ -18,18 +18,19 @@ typedef struct _DEMO3D_ENGINE_WORK DEMO3D_ENGINE_WORK;
 #if DEBUG_ONLY_FOR_james_turner
 
 #define DEBUG_USE_KEY // デバッグ用のキー操作を使うフラグ。基本的に↓の機能を使う場合は必須
-#define DEBUG_CAMERA_DISP_OFFSET_CONTROL // 2画面のオフセットを操作
+//#define DEBUG_CAMERA_DISP_OFFSET_CONTROL // 2画面のオフセットを操作
 //#define DEBUG_CAMERA_CONTROL // 2画面時のカメラ操作デバッグ
 
 #endif // DEBUG_ONLY_FOR_james_turner
 
 #if DEBUG_ONLY_FOR_iwasawa
 
-#define DEBUG_USE_KEY // デバッグ用のキー操作を使うフラグ。基本的に↓の機能を使う場合は必須
-#define DEBUG_CAMERA_DISP_OFFSET_CONTROL // 2画面のオフセットを操作
+//#define DEBUG_USE_KEY // デバッグ用のキー操作を使うフラグ。基本的に↓の機能を使う場合は必須
 //#define DEBUG_CAMERA_CONTROL // 2画面時のカメラ操作デバッグ
 
 #endif // DEBUG_ONLY_FOR_iwasawa
+
+#define DEBUG_CAMERA_DISP_OFFSET_CONTROL // 2画面のオフセットを操作
 
 #endif //PM_DEBUG
 
