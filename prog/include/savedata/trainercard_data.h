4d9ec69725781ce982707ae0e8adbd23bae18725
Akito Mori <mori@gamefreak.co.jp>
2010-02-24 07:44:59 +0000

    トレーナーカードセーブデータに 「ユニオン見た目を変更した事がフラグ」と 「性格」領域を追加した

--- prog/include/savedata/trainercard_data.h
+++ prog/include/savedata/trainercard_data.h
@@ -1,8 +1,8 @@
 //=============================================================================
 /**
- * @file	trainerbadge_data.h
- * @bfief	トレーナーカード用セーブデータアクセス関連
- * @author	Nozomu Saito
+ * @file  trainerbadge_data.h
+ * @bfief トレーナーカード用セーブデータアクセス関連
+ * @author  Nozomu Saito
  *
  *
  */
@@ -10,14 +10,29 @@
 #ifndef __TRAINERCARD_DATA_H__
 #define __TRAINERCARD_DATA_H__
 
+// トレーナーカードで設定できる「性格」の最大値
+#define TRCARD_PERSONARITY_MAX    ( 8 )
+
 #include "savedata/save_control.h"
 typedef struct TR_CARD_SV_DATA_tag * TR_CARD_SV_PTR;
 
 extern int TRCSave_GetSaveDataSize(void);
 extern void TRCSave_InitSaveData(TR_CARD_SV_PTR outTrCard);
+
+// トレーナカードデータ取得
 extern TR_CARD_SV_PTR TRCSave_GetSaveDataPtr(SAVE_CONTROL_WORK * sv);
+
+// サインデータ周り
 extern u8 *TRCSave_GetSignDataPtr(TR_CARD_SV_PTR inTrCard);
 extern int TRCSave_GetSigned( TR_CARD_SV_PTR inTrCard );
+// ユニオン見た目を変更しているか？
+extern void TRCSave_SetTrainerViewChange( TR_CARD_SV_PTR inTrCard );
+extern  int TRCSave_GetTrainerViewChange( TR_CARD_SV_PTR outTrCard );
+// 性格（トレーナーカードで表示される）
+extern void TRCSave_SetPersonarity( TR_CARD_SV_PTR inTrCard, int personarity );
+extern  int TRCSave_GetPersonarity( TR_CARD_SV_PTR outTrCard );
+
+
 
 #endif //__TRAINERCARD_DATA_H__
 
--- prog/src/savedata/trainercard_data.c
+++ prog/src/savedata/trainercard_data.c
@@ -1,8 +1,8 @@
 //=============================================================================
 /**
- * @file	trainercard_data.c
- * @bfief	トレーナーカード用セーブデータアクセス関連
- * @author	Nozomu Saito
+ * @file  trainercard_data.c
+ * @bfief トレーナーカード用セーブデータアクセス関連
+ * @author  Nozomu Saito
  *
  *
  */
@@ -11,46 +11,48 @@
 #include "savedata/save_tbl.h"
 #include "savedata/trainercard_data.h"
 
-#define BADGE_MAX	(8)
-#define SIGHN_W	(24)
-#define SIGHN_H	(8)
+#define BADGE_MAX (8)
+#define SIGHN_W (24)
+#define SIGHN_H (8)
 
-#define DEFAULT_BADGE_SCRUCH	(140)
+#define DEFAULT_BADGE_SCRUCH  (140)
 
 typedef struct TR_CARD_SV_DATA_tag
 {
-	u8 SignData[SIGHN_W*SIGHN_H*64/8];	//サイン面データ
+  u8 SignData[SIGHN_W*SIGHN_H*64/8]; ///<サイン面データ
+  u16 personarity;                   ///<性格（トレーナーカードで参照）
+  u16 trainer_view_change;           ///<トレーナーカードで見た目を変更した(0:していない 1:した）
 }TR_CARD_SV_DATA;
 
 //==============================================================================
 /**
  * セーブデータサイズ取得
  *
- * @param	none
+ * @param none
  *
- * @return	int		サイズ
+ * @return  int   サイズ
  */
 //==============================================================================
 int TRCSave_GetSaveDataSize(void)
 {
-	return sizeof(TR_CARD_SV_DATA);
+  return sizeof(TR_CARD_SV_DATA);
 }
 
 //==============================================================================
 /**
  * セーブデータ初期化
  *
- * @param	outTrCard	トレーナーカードデータポインタ
+ * @param outTrCard トレーナーカードデータポインタ
  *
- * @return	none
+ * @return  none
  */
 //==============================================================================
 void TRCSave_InitSaveData(TR_CARD_SV_PTR outTrCard)
 {
-	GFL_STD_MemFill( outTrCard, 0, sizeof(TR_CARD_SV_DATA) );
+  GFL_STD_MemFill( outTrCard, 0, sizeof(TR_CARD_SV_DATA) );
 
 #if (CRC_LOADCHECK && CRCLOADCHECK_GMDATA_ID_TRCARD)
-	SVLD_SetCrc(GMDATA_ID_TRCARD);
+  SVLD_SetCrc(GMDATA_ID_TRCARD);
 #endif //CRC_LOADCHECK
 }
 
@@ -58,18 +60,18 @@ void TRCSave_InitSaveData(TR_CARD_SV_PTR outTrCard)
 /**
  * セーブデータ先頭ポインタ取得
  *
- * @param	sv		セーブポインタ
+ * @param sv    セーブポインタ
  *
- * @return	TR_CARD_SV_PTR		トレーナーカードデータポインタ
+ * @return  TR_CARD_SV_PTR    トレーナーカードデータポインタ
  */
 //==============================================================================
 TR_CARD_SV_PTR TRCSave_GetSaveDataPtr(SAVE_CONTROL_WORK * sv)
 {
 #if (CRC_LOADCHECK && CRCLOADCHECK_GMDATA_ID_TRCARD)
-	SVLD_CheckCrc(GMDATA_ID_TRCARD);
+  SVLD_CheckCrc(GMDATA_ID_TRCARD);
 #endif //CRC_LOADCHECK
 
-	return SaveControl_DataPtrGet(sv, GMDATA_ID_TRCARD);
+  return SaveControl_DataPtrGet(sv, GMDATA_ID_TRCARD);
 }
 
 
@@ -78,39 +80,91 @@ TR_CARD_SV_PTR TRCSave_GetSaveDataPtr(SAVE_CONTROL_WORK * sv)
 /**
  * サインデータ先頭ポインタ取得
  *
- * @param	inTrCard		トレーナーカードデータポインタ
+ * @param inTrCard    トレーナーカードデータポインタ
  *
- * @return	u8*				サインデータポインタ
+ * @return  u8*       サインデータポインタ
  */
 //==============================================================================
 u8 *TRCSave_GetSignDataPtr(TR_CARD_SV_PTR inTrCard)
 {
-	return inTrCard->SignData;
+  return inTrCard->SignData;
 }
 
 //==============================================================================
 /**
  * @brief   サインが書き込まれているか確認する
  *
- * @param   inTrCard		
+ * @param   inTrCard    
  *
- * @retval  int		TRUEなら書き込まれている。FALSEなら全て空白
+ * @retval  int   TRUEなら書き込まれている。FALSEなら全て空白
  */
 //==============================================================================
 int TRCSave_GetSigned( TR_CARD_SV_PTR inTrCard )
 {
-	int search=0;
-	int i;
-	u32 *ptr = 	(u32*)inTrCard->SignData;	//サイン面データ
+  int search=0;
+  int i;
+  u32 *ptr =  (u32*)inTrCard->SignData; //サイン面データ
 
-	
-	for(i=0;i<SIGHN_W*SIGHN_H*64/(8*4);i++){
-		search += ptr[i];
-	}
+  
+  for(i=0;i<SIGHN_W*SIGHN_H*64/(8*4);i++){
+    search += ptr[i];
+  }
 
-	if(search){
-		return TRUE;
-	}
-	
-	return FALSE;
+  if(search){
+    return TRUE;
+  }
+  
+  return FALSE;
+}
+
+//=============================================================================================
+/**
+ * @brief トレーナーカードでユニオン見た目を変更したか？
+ *
+ * @param   misc    
+ *
+ * @retval  int   トレーナーカードでユニオン見た目を変更したか？（0:してない 1:した）
+ */
+//=============================================================================================
+int TRCSave_GetTrainerViewChange( TR_CARD_SV_PTR outTrCard )
+{
+  return outTrCard->trainer_view_change;
+}
+
+//=============================================================================================
+/**
+ * @brief トレーナーカードでユニオン見た目を変更したフラグを立てる
+ */
+//=============================================================================================
+void TRCSave_SetTrainerViewChange( TR_CARD_SV_PTR inTrCard )
+{
+  inTrCard->trainer_view_change = 1;
+}
+
+
+//=============================================================================================
+/**
+ * @brief 性格の取得
+ *
+ * @param   misc    
+ *
+ * @retval  int   性格値
+ */
+//=============================================================================================
+int TRCSave_GetPersonarity( TR_CARD_SV_PTR outTrCard )
+{
+  return outTrCard->personarity;
+
+}
+
+//=============================================================================================
+/**
+ * @brief トレーナーカードで変更した性格を格納
+ */
+//=============================================================================================
+void TRCSave_SetPersonarity( TR_CARD_SV_PTR inTrCard, int personarity )
+{
+  GF_ASSERT_MSG( (personarity < TRCARD_PERSONARITY_MAX),"性格の最大値を超えている\n");
+
+  inTrCard->personarity = personarity;
 }
