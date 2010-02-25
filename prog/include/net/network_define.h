3df4109de6b745428e0655a05b993af8a6f2d24d
Toru Nagihashi <nagihashi@gamefreak.co.jp>
2010-02-25 09:37:56 +0000

    WIFIMATCH用ネットワークにヒープを取りすぎていて、バトルのヒープが取得できなかったので、サイズを減らした。

--- prog/include/net/network_define.h
+++ prog/include/net/network_define.h
@@ -271,6 +271,6 @@ typedef enum{
 #define GFL_NET_DWC_HEAPSIZE        (0x45400)                           //WIFIクラブのHEAP量
 #define GFL_NET_DWCLOBBY_HEAPSIZE   (0x60000)                           //WIFI広場のHEAP量
 #define GFL_NET_DWC_BSUBWAY_HEAPSIZE  (0x50000)                        //バトルサブウェイのHEAP量
-#define GFL_NET_DWC_RNDMATCH_HEAPSIZE (0x58000)   //ランダムマッチングのHEAP量
+#define GFL_NET_DWC_RNDMATCH_HEAPSIZE (0x54000)   //ランダムマッチングのHEAP量
 
 
