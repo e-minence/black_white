7a6b14ee26ff02f2f819f661de0f6846578c15c0
Hisashi Sogabe <sogabe@gamefreak.co.jp>
2010-03-02 08:09:12 +0000

    トレーナーデータ更新

--- resource/trdata/trtype_sex.h
+++ resource/trdata/trtype_sex.h
@@ -15,92 +15,92 @@ static	const	u8	TrTypeSexTable[]={
 	PTL_SEX_FEMALE,	//女主人公
 	PTL_SEX_MALE,		//たんぱんこぞう
 	PTL_SEX_FEMALE,		//ミニスカート
-	PTL_SEX_MALE,		//じゅくがえり
-	PTL_SEX_FEMALE,		//じゅくがえり
+	PTL_SEX_MALE,		//じゅくがえり♂
+	PTL_SEX_FEMALE,		//じゅくがえり♀
 	PTL_SEX_FEMALE,		//テニスプレイヤー
 	PTL_SEX_MALE,		//フットボーラー
 	PTL_SEX_MALE,		//ウエーター
 	PTL_SEX_FEMALE,		//ウエートレス
-	PTL_SEX_MALE,		//ジムリーダー
-	PTL_SEX_MALE,		//ジムリーダー
-	PTL_SEX_MALE,		//ジムリーダー
+	PTL_SEX_MALE,		//ジムリーダー1A
+	PTL_SEX_MALE,		//ジムリーダー1C
+	PTL_SEX_MALE,		//ジムリーダー1B
 	PTL_SEX_FEMALE,		//ほいくし
-	PTL_SEX_FEMALE,		//ようちえん
-	PTL_SEX_MALE,		//ようちえん
+	PTL_SEX_FEMALE,		//ようちえん♀
+	PTL_SEX_MALE,		//ようちえん♂
 	PTL_SEX_FEMALE,		//ふたごちゃん
-	PTL_SEX_MALE,		//ポケモンブリーダー
-	PTL_SEX_FEMALE,		//ポケモンブリーダー
-	PTL_SEX_FEMALE,		//ジムリーダー
-	PTL_SEX_MALE,		//ジムリーダー
-	PTL_SEX_FEMALE,		//ジムリーダー
-	PTL_SEX_MALE,		//ジムリーダー
-	PTL_SEX_FEMALE,		//ジムリーダー
-	PTL_SEX_MALE,		//ポケモンレンジャー
-	PTL_SEX_FEMALE,		//ポケモンレンジャー
-	PTL_SEX_MALE,		//さぎょういん
-	PTL_SEX_MALE,		//バックパッカー
-	PTL_SEX_FEMALE,		//バックパッカー
+	PTL_SEX_MALE,		//ポケモンブリーダー♂
+	PTL_SEX_FEMALE,		//ポケモンブリーダー♀
+	PTL_SEX_FEMALE,		//ジムリーダー2
+	PTL_SEX_MALE,		//ジムリーダー3
+	PTL_SEX_FEMALE,		//ジムリーダー4
+	PTL_SEX_MALE,		//ジムリーダー5
+	PTL_SEX_FEMALE,		//ジムリーダー6
+	PTL_SEX_MALE,		//ポケモンレンジャー♂
+	PTL_SEX_FEMALE,		//ポケモンレンジャー♀
+	PTL_SEX_MALE,		//さぎょういん2
+	PTL_SEX_MALE,		//バックパッカー♂
+	PTL_SEX_FEMALE,		//バックパッカー♀
 	PTL_SEX_MALE,		//つりびと
 	PTL_SEX_MALE,		//ミュージシャン
 	PTL_SEX_MALE,		//ダンサー
 	PTL_SEX_MALE,		//クラウン
 	PTL_SEX_MALE,		//げいじゅつか
 	PTL_SEX_FEMALE,		//ベーカリー
-	PTL_SEX_MALE,		//サイキッカー
-	PTL_SEX_FEMALE,		//サイキッカー
-	PTL_SEX_MALE,		//ポケモントレーナー
-	PTL_SEX_FEMALE,		//ポケモントレーナー
-	PTL_SEX_MALE,		//プラズマだん
-	PTL_SEX_MALE,		//ポケモントレーナー
+	PTL_SEX_MALE,		//サイキッカー♂
+	PTL_SEX_FEMALE,		//サイキッカー♀
+	PTL_SEX_MALE,		//ポケモントレーナー（RIVAL）
+	PTL_SEX_FEMALE,		//ポケモントレーナー（SUPPORT）
+	PTL_SEX_MALE,		//プラズマだん♂
+	PTL_SEX_MALE,		//ポケモントレーナー（BOSS）
 	PTL_SEX_MALE,		//おぼっちゃま
 	PTL_SEX_FEMALE,		//おじょうさま
 	PTL_SEX_MALE,		//パイロット
-	PTL_SEX_MALE,		//さぎょういん
+	PTL_SEX_MALE,		//さぎょういん1
 	PTL_SEX_MALE,		//バスケせんしゅ
-	PTL_SEX_FEMALE,		//けんきゅういん
+	PTL_SEX_FEMALE,		//けんきゅういん♀
 	PTL_SEX_FEMALE,		//ＯＬ
-	PTL_SEX_FEMALE,		//エリートトレーナー
-	PTL_SEX_MALE,		//エリートトレーナー
+	PTL_SEX_FEMALE,		//エリートトレーナー♀
+	PTL_SEX_MALE,		//エリートトレーナー♂
 	PTL_SEX_MALE,		//カラテおう
-	PTL_SEX_MALE,		//けんきゅういん
+	PTL_SEX_MALE,		//けんきゅういん♂
 	PTL_SEX_MALE,		//サッカーせんしゅ
-	PTL_SEX_MALE,		//ジムリーダー
-	PTL_SEX_FEMALE,		//ジムリーダー
-	PTL_SEX_MALE,		//ジムリーダー
+	PTL_SEX_MALE,		//ジムリーダー7
+	PTL_SEX_FEMALE,		//ジムリーダー8A
+	PTL_SEX_MALE,		//ジムリーダー8B
 	PTL_SEX_MALE,		//スキンヘッズ
 	PTL_SEX_MALE,		//せいそういん
-	PTL_SEX_MALE,		//だいすきクラブ
-	PTL_SEX_FEMALE,		//だいすきクラブ
+	PTL_SEX_MALE,		//だいすきクラブ♂
+	PTL_SEX_FEMALE,		//だいすきクラブ♀
 	PTL_SEX_MALE,		//ドクター
 	PTL_SEX_FEMALE,		//ナース
 	PTL_SEX_MALE,		//バッドチーム
 	PTL_SEX_FEMALE,		//バトルガール
 	PTL_SEX_FEMALE,		//パラソルおねえさん
-	PTL_SEX_MALE,		//ビジネスマン
-	PTL_SEX_MALE,		//ビジネスマン
-	PTL_SEX_MALE,		//ファンクラブ
-	PTL_SEX_FEMALE,		//ファンクラブ
-	PTL_SEX_MALE,		//ベテラントレーナー
-	PTL_SEX_FEMALE,		//ベテラントレーナー
+	PTL_SEX_MALE,		//ビジネスマン1
+	PTL_SEX_MALE,		//ビジネスマン2
+	PTL_SEX_MALE,		//ファンクラブ♂
+	PTL_SEX_FEMALE,		//ファンクラブ♀
+	PTL_SEX_MALE,		//ベテラントレーナー♂
+	PTL_SEX_FEMALE,		//ベテラントレーナー♀
 	PTL_SEX_MALE,		//ぼうそうぞく
 	PTL_SEX_MALE,		//やきゅうせんしゅ
 	PTL_SEX_MALE,		//やまおとこ
 	PTL_SEX_FEMALE,		//マダム
 	PTL_SEX_MALE,		//ジェントルマン
-	PTL_SEX_FEMALE,		//プラズマだん
-	PTL_SEX_MALE,		//してんのう
-	PTL_SEX_MALE,		//してんのう
-	PTL_SEX_MALE,		//してんのう
-	PTL_SEX_MALE,		//してんのう
-	PTL_SEX_MALE,		//プラズマだん
+	PTL_SEX_FEMALE,		//プラズマだん♀
+	PTL_SEX_MALE,		//してんのう1
+	PTL_SEX_MALE,		//してんのう3
+	PTL_SEX_MALE,		//してんのう2
+	PTL_SEX_MALE,		//してんのう4
+	PTL_SEX_MALE,		//プラズマだん1
 	PTL_SEX_MALE,		//てつどういん
 	PTL_SEX_MALE,		//かいパンやろう
 	PTL_SEX_FEMALE,		//ビキニのおねえさん
 	PTL_SEX_MALE,		//おまわりさん
 	PTL_SEX_FEMALE,		//メイド
-	PTL_SEX_MALE,		//ポケモントレーナー
+	PTL_SEX_MALE,		//ポケモントレーナー（BCHAMP）
 	PTL_SEX_MALE,		//チャンピオン
-	PTL_SEX_MALE,		//サイクリング
-	PTL_SEX_FEMALE,		//サイクリング
+	PTL_SEX_MALE,		//サイクリング♂
+	PTL_SEX_FEMALE,		//サイクリング♀
 };
 
