7563b9a6d4468663bdf182241df3a45984fabe3e
Hisashi Sogabe <sogabe@gamefreak.co.jp>
2010-03-02 10:33:57 +0000

    景山君に技エフェクト起動時のメインボリューム調整を任すために定義を外部化

---
+++ prog/src/battle/btlv/btlv_effvm_dat.h
@@ -0,0 +1,13 @@
+//======================================================================
+/**
+ * @file	  btlv_effvm_dat.h
+ * @brief	  戦闘エフェクト
+ * @author	HisashiSogabe
+ * @date	2010.03.02
+ */
+//======================================================================
+
+#pragma once
+
+#define EFFVM_VOLUME_DOWN_RATIO ( 80 )  //マスターボリュームを何％ダウンするか（80で80%ダウン）
+#define EFFVM_VOLUME_DOWN_FRAME ( 20 )  //EFFVM_VOLUME_DOWN_RATIOの％に何フレームでなるかを指定（60で1秒）
