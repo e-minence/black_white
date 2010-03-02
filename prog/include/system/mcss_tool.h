5ecfbbd60db9b036dd75c8f2c1ddb9fae7fc674a
Hisashi Sogabe <sogabe@gamefreak.co.jp>
2010-03-02 09:46:01 +0000

    MCSS_Addのラッパー関数を作成

--- prog/include/system/mcss_tool.h
+++ prog/include/system/mcss_tool.h
@@ -8,20 +8,23 @@
  */
 //============================================================================================
 
+#pragma once
+
 #include "system/mcss.h"
 #include "poke_tool/poke_tool.h"
 #include "system/pokegra.h"
 
-#ifndef __MCSS_TOOL_H_
-#define __MCSS_TOOL_H_
-
-#define	MCSS_DIR_FRONT	( POKEGRA_DIR_FRONT )	//ê≥ñ 
-#define	MCSS_DIR_BACK	( POKEGRA_DIR_BACK )	//îwñ 
+typedef enum
+{ 
+  MCSS_DIR_FRONT = 0,
+  MCSS_DIR_BACK,
+}MCSS_DIR;
 
 extern	void	MCSS_TOOL_MakeMAWPP( const POKEMON_PARAM *pp, MCSS_ADD_WORK *maw, int dir );
 extern	void	MCSS_TOOL_MakeMAWPPP( const POKEMON_PASO_PARAM *ppp, MCSS_ADD_WORK *maw, int dir );
 extern	void	MCSS_TOOL_MakeMAWParam( int mons_no, int form_no, int sex, int rare, BOOL egg, MCSS_ADD_WORK *maw, int dir );
 extern  void	MCSS_TOOL_MakeMAWTrainer( int tr_type, MCSS_ADD_WORK *maw, int dir );
+extern  MCSS_WORK*  MCSS_TOOL_AddPokeMcss( MCSS_SYS_WORK* mcss_sys, const POKEMON_PARAM* pp, MCSS_DIR dir,
+                                           fx32	pos_x, fx32	pos_y, fx32	pos_z );
+extern  void  MCSS_TOOL_SetMakeBuchiCallback( MCSS_SYS_WORK* mcss_sys, u32 personal_rnd );
 
-
-#endif //__MCSS_TOOL_H_
