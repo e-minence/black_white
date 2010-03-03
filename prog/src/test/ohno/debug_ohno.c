049cdfac9994afcdb0db9a8b7163ab664e3ee9c8
Katsumi Ohno <ohno@gamefreak.co.jp>
2010-03-03 10:44:08 +0000

    GGID修正

--- prog/src/test/ohno/debug_ohno.c
+++ prog/src/test/ohno/debug_ohno.c
@@ -129,7 +129,7 @@ static GFLNetInitializeStruct aGFLNetInit = {
   0,   ///< DWCへのHEAPサイズ
   TRUE,        ///< デバック用サーバにつなぐかどうか
 #endif  //GFL_NET_WIFI
-  0x444,  //ggid  DP=0x333,RANGER=0x178,WII=0x346
+  SYASHI_NETWORK_GGID,  //ggid  DP=0x333,RANGER=0x178,WII=0x346
   GFL_HEAPID_APP,  //元になるheapid
   HEAPID_NETWORK,  //通信用にcreateされるHEAPID
   HEAPID_WIFI,  //wifi用にcreateされるHEAPID
