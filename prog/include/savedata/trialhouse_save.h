53795f13e20f48162e430acaa4b3324bc6aa3279
Nozomu Saito <saito_nozomu@gamefreak.co.jp>
2010-03-03 04:16:26 +0000

    //トライアルハウスランキング閲覧用データ追加

---
+++ prog/include/savedata/trialhouse_save.h
@@ -0,0 +1,24 @@
+//============================================================================================
+/**
+ * @file	trialhouse_save.h
+ * @brief	トライアルハウスセーブデータ
+ * @author	saito
+ *
+ */
+//============================================================================================
+
+#pragma once
+
+#include "savedata/save_control.h"
+#include "gamesystem/gamedata_def.h"  //GAMEDATA
+
+//---------------------------------------------------------------------------
+/**
+ * @brief	ワークへの不完全型定義
+ */
+//---------------------------------------------------------------------------
+typedef struct _THSV_WORK THSV_WORK;
+
+
+extern int THSV_GetWorkSize(void);
+extern void THSV_Init(THSV_WORK * work);
