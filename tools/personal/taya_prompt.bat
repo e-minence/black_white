3d9e8257802554f679c0817caae124b3459e40d3
Masao Taya <taya@gamefreak.co.jp>
2010-03-04 10:17:23 +0000

    ローカルなバッチファイル呼び出しDOSKEY追加

--- tools/personal/taya_prompt.bat
+++ tools/personal/taya_prompt.bat
@@ -9,6 +9,7 @@ doskey gowa = cd %PROJECT_RSCDIR:/=\%waza_tbl
 doskey gome = cd %PROJECT_RSCDIR:/=\%message
 doskey gofo = cd %PROJECT_RSCDIR:/=\%font
 doskey goan = cd %PROJECT_ROOT:/=\%tools\analyze\overlaymap
+doskey mk = call %PROJECT_PROGDIR:/=\%mksnd.bat
 
 cd %PROJECT_PROGDIR:/=\%
 
