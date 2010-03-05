f785ae4c174fb1684ff7950423388f896e955589
Toru Nagihashi <nagihashi@gamefreak.co.jp>
2010-03-05 09:46:07 +0000

    コメント修正。

--- prog/include/app/name_input.h
+++ prog/include/app/name_input.h
@@ -28,15 +28,12 @@ typedef enum
 	NAMEIN_MYNAME = 0,	// 自分の名前	param1自分の性別
 	NAMEIN_POKEMON,			// ポケモンの名前 param1モンスター番号 param2フォルム番号
 	NAMEIN_BOX,					// ボックスの名前
-	NAMEIN_RIVALNAME,		// ライバルネーム
-	NAMEIN_FRIENDNAME,  // WIFIともだち手帳に書き込むともだちの名前 param1 フィールド３DOBJ
+	NAMEIN_RIVALNAME,		// ライバルネーム WBでは使わなくなりました
+	NAMEIN_FRIENDNAME,  // WIFIともだち手帳に書き込むともだちの名前 param1 ユニオンの見た目
   NAMEIN_GREETING_WORD, // すれちがい挨拶言葉 param1自分の性別
   NAMEIN_THANKS_WORD,   // すれちがいお礼言葉 param1自分の性別
   NAMEIN_FREE_WORD,     // すれちがいフリーワードparam1自分の性別
 
-//	NAMEIN_FRIENDCODE,	// ともだちコード
-//	NAMEIN_RANDOMGROUP, // 乱数の種グループの名前
-//	NAMEIN_STONE,			// 石碑(配布）
 	NAMEIN_MAX,
 }NAMEIN_MODE;
 
