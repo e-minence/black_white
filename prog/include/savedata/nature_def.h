7b88715f02beea8474d4abb1b18e380bbae97810
Nozomu Saito <saito_nozomu@gamefreak.co.jp>
2010-03-05 04:21:36 +0000

    //自分の性格定義追加

---
+++ prog/include/savedata/nature_def.h
@@ -0,0 +1,16 @@
+//==============================================================================
+/**
+ * @file	nature_def.h
+ * @brief	自分の性格定義（ポケモンの性格とは異なります）
+ * @note  スクリプトで参照します
+ * @author	Saito
+ */
+//==============================================================================
+#pragma once
+
+#define NATURE_DEF      (0)   //デフォルト
+#define NATUEW_LEADER   (1)   //リーダー風
+#define NATUEW_STRANGER (2)   //変人
+#define NATUEW_SCHOLAR  (3)   //研究者
+#define NATUEW_QUIET    (4)   //おっとり
+
