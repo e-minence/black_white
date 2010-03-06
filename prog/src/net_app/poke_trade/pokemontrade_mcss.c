36f1394572115b5c3d0edcd2d35e7463ef98700b
Katsumi Ohno <ohno@gamefreak.co.jp>
2010-03-06 06:08:09 +0000

    分離

---
+++ prog/src/net_app/poke_trade/pokemontrade_mcss.c
@@ -0,0 +1,154 @@
+
+
+//-------------------------------------------------
+/**
+ *	@brief      MCSS移動命令
+ *	@param[in,out]	_POKEMCSS_MOVE_WORK   移動ポインタ
+ *	@param[in]	time   移動時間
+ *	@param[in]	pPos    最終移動先
+ */
+//-------------------------------------------------
+
+static void _pokeMoveRenew(_POKEMCSS_MOVE_WORK* pPoke,int time, const VecFx32* pPos)
+{
+  {
+    VecFx32 apos;
+    MCSS_GetPosition(pPoke->pMcss, &apos);
+    pPoke->time = time;
+    pPoke->nowcount=0;
+    GFL_STD_MemCopy(pPos, &pPoke->end, sizeof(VecFx32));
+    GFL_STD_MemCopy(&apos, &pPoke->start, sizeof(VecFx32));
+  }
+}
+
+//-------------------------------------------------
+/**
+ *	@brief      MCSS移動命令作成
+ *	@param[in]	pokeMcss   対象MCSS
+ *	@param[in]	time   移動時間
+ *	@param[in]	pPos    最終移動先
+ *	@param[in]	HEAPID  HEAPID
+ */
+//-------------------------------------------------
+
+static _POKEMCSS_MOVE_WORK* _pokeMoveCreate(MCSS_WORK* pokeMcss, int time, const VecFx32* pPos, HEAPID heapID)
+{
+  _POKEMCSS_MOVE_WORK* pPoke = GFL_HEAP_AllocClearMemory(heapID, sizeof(_POKEMCSS_MOVE_WORK));
+
+  pPoke->pMcss = pokeMcss;
+  _pokeMoveRenew(pPoke,time,pPos);
+  pPoke->percent=0.0f;
+  return pPoke;
+
+}
+
+//-------------------------------------------------
+/**
+ *	@brief      MCSSテーブル移動命令作成
+ *	@param[in]	pokeMcss   対象MCSS
+ *	@param[in]	time   移動時間
+ *	@param[in]	pPos    最終移動先
+ *	@param[in]	HEAPID  HEAPID
+ */
+//-------------------------------------------------
+
+static _POKEMCSS_MOVE_WORK* _pokeTblMoveCreate(MCSS_WORK* pokeMcss, int time, const VecFx32* pPos, VecFx32* pTbl, HEAPID heapID)
+{
+  _POKEMCSS_MOVE_WORK* pPoke = GFL_HEAP_AllocClearMemory(heapID, sizeof(_POKEMCSS_MOVE_WORK));
+
+  pPoke->pMcss = pokeMcss;
+  _pokeMoveRenew(pPoke,time,pPos);
+  pPoke->percent=0.0f;
+  pPoke->MoveTbl = pTbl;
+  return pPoke;
+
+}
+
+
+//-------------------------------------------------
+/**
+ *	@brief      MCSS移動命令実行
+ *	@param[in]	pokeMcss   対象MCSS
+ *	@param[in]	time   移動時間
+ *	@param[in]	xend    Xの移動先
+ *	@param[in]	zend    Zの移動先
+ *	@param[in]	HEAPID  HEAPID
+ */
+//-------------------------------------------------
+
+static fx32 _movemath(fx32 st,fx32 en,_POKEMCSS_MOVE_WORK* pMove,BOOL bWave)
+{
+  fx32 re;
+  re = en - st;
+  re = re / pMove->time;
+  
+
+  re = st + re * pMove->nowcount;
+  if(pMove->wave && bWave){
+    re = en + FX_SinIdx(pMove->sins) * _WAVE_NUM;
+  }
+  return re;
+}
+
+//-------------------------------------------------
+/**
+ *	@brief      テーブルによる移動関数
+ *	@param[in]	pMove
+ */
+//-------------------------------------------------
+
+static void _pokeMoveTblFunc(_POKEMCSS_MOVE_WORK* pMove)
+{
+  VecFx32 apos,aposold;
+  VecFx32* pPMT = &pMove->MoveTbl[pMove->nowcount];
+  
+  apos.x = pPMT->x + pMove->start.x;
+  apos.y = pPMT->y + pMove->start.y;
+  apos.z = pPMT->z + pMove->start.z;
+
+  MCSS_SetPosition( pMove->pMcss ,&apos );
+
+
+}
+
+//-------------------------------------------------
+/**
+ *	@brief      MCSS移動関数
+ *	@param[in]	pMove   移動ワーク
+ */
+//-------------------------------------------------
+
+static void _pokeMoveFunc(_POKEMCSS_MOVE_WORK* pMove)
+{
+  if(!pMove){
+    return;
+  }
+  pMove->nowcount++;
+  if(pMove->time < pMove->nowcount){
+    return;
+  }
+  if(pMove->time != pMove->nowcount)
+  {
+    if(pMove->MoveTbl){
+      _pokeMoveTblFunc(pMove);
+      return;
+    }
+    else{
+      VecFx32 apos,aposold;
+      MCSS_GetPosition( pMove->pMcss ,&aposold );
+      apos.x = _movemath(pMove->start.x, pMove->end.x ,pMove, FALSE);
+      if(pMove->wave){
+        float an = FX_FX32_TO_F32(aposold.x - apos.x);
+        an = an * 374;
+        pMove->sins -= (s32)an;
+      }
+      apos.y = _movemath(pMove->start.y, pMove->end.y ,pMove, TRUE);
+      apos.z = _movemath(pMove->start.z, pMove->end.z ,pMove, FALSE);
+      MCSS_SetPosition( pMove->pMcss ,&apos );
+    }
+  }
+  else{
+    MCSS_SetPosition( pMove->pMcss ,&pMove->end );
+  }
+}
+
