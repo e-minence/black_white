57a7f4e9cbc4d438f455856bc0ad31af6c25346e
Naoto Murakami <murakami_naoto@gamefreak.co.jp>
2010-03-06 06:32:56 +0000

    スクリプト：C02ブラッシュアップ

--- resource/fldmapdata/eventdata/tmp/c02r0301.h
+++ resource/fldmapdata/eventdata/tmp/c02r0301.h
@@ -13,6 +13,7 @@
 #define C02R0301_POKE2_01                  0
 #define C02R0301_WOMAN2_01                 1
 #define C02R0301_GIRL1_01                  2
+#define C02R0301_BOY1_01                   3
 
 #endif //ZONE_C02R0301_EVC_H
 //‚±‚Ìƒtƒ@ƒCƒ‹‚Í../data/c02r0301.mev‚©‚ç©“®¶¬‚³‚ê‚Ü‚µ‚½
