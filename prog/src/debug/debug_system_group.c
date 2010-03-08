0b6ea572028ae1ec4182aa394d4e4c0bc57c7cd9
Ariizumi Nobuhiko <ariizumi_nobuhiko@gamefreak.co.jp>
2010-03-08 14:01:24 +0000

    PDWデバッグ追加

--- prog/src/debug/debug_system_group.c
+++ prog/src/debug/debug_system_group.c
@@ -12,6 +12,7 @@
 #include "debug/debug_system_group.h"
 #include "debug/debug_flg.h"
 #include "savedata/config.h"
+#include "savedata/dreamworld_data.h"
 #include "test/performance.h"
 #include "test/debug_pause.h"
 
@@ -67,7 +68,13 @@ static void DEBWIN_Draw_RTC_hour( void* userWork , DEBUGWIN_ITEM* item );
 static void DEBWIN_Draw_RTC_minute( void* userWork , DEBUGWIN_ITEM* item );
 static void DEBWIN_Draw_RTC_sec( void* userWork , DEBUGWIN_ITEM* item );
 
-static void DEBWIN_UTIL_UpdateU32( u32 *value , const u32 min, const u32 max );
+static void DEBWIN_U_PDW_Account( void* userWork , DEBUGWIN_ITEM* item );
+static void DEBWIN_D_PDW_Account( void* userWork , DEBUGWIN_ITEM* item );
+static void DEBWIN_U_PDW_SetFurniture( void* userWork , DEBUGWIN_ITEM* item );
+static void DEBWIN_D_PDW_SetFurniture( void* userWork , DEBUGWIN_ITEM* item );
+
+static const BOOL DEBWIN_UTIL_UpdateU32( u32 *value , const u32 min, const u32 max );
+static const BOOL DEBWIN_UTIL_UpdateBOOL( BOOL *value );
 
 DEBUG_SYS_GROUP_WORK *sysGroupWork = NULL;
 //--------------------------------------------------------------
@@ -83,6 +90,7 @@ void DEBUGWIN_AddSystemGroup( const HEAPID heapId )
   GFL_RTC_GetDateTime( &sysGroupWork->rtcDate , &sysGroupWork->rtcTime );
   
   DEBUGWIN_AddGroupToTop( DEBUGWIN_GROUPID_SYSTEM , "System" , heapId );
+  DEBUGWIN_AddGroupToTop( DEBUGWIN_GROUPID_PDW , "PDW" , heapId );
   
   //System下
   DEBUGWIN_AddGroupToGroup( DEBUGWIN_GROUPID_RTC , "RTC" , DEBUGWIN_GROUPID_SYSTEM , heapId );
@@ -100,6 +108,11 @@ void DEBUGWIN_AddSystemGroup( const HEAPID heapId )
   DEBUGWIN_AddItemToGroupEx( DEBWIN_Update_RTC_sec    ,DEBWIN_Draw_RTC_sec    , (void*)sysGroupWork , DEBUGWIN_GROUPID_RTC , heapId );
   DEBUGWIN_AddItemToGroup( "てきよう",DEBWIN_Update_RTC_apply , (void*)sysGroupWork , DEBUGWIN_GROUPID_RTC , heapId );
   DEBUGWIN_AddItemToGroup( "しゅとく",DEBWIN_Update_RTC_get , (void*)sysGroupWork , DEBUGWIN_GROUPID_RTC , heapId );
+
+  //PDW下
+  DEBUGWIN_AddItemToGroupEx( DEBWIN_U_PDW_Account   ,DEBWIN_D_PDW_Account   , (void*)sysGroupWork , DEBUGWIN_GROUPID_PDW , heapId );
+  DEBUGWIN_AddItemToGroupEx( DEBWIN_U_PDW_SetFurniture   ,DEBWIN_D_PDW_SetFurniture   , (void*)sysGroupWork , DEBUGWIN_GROUPID_PDW , heapId );
+  
   
   DEBUG_FLG_CreateDebugGoupe( heapId );
 }
@@ -269,8 +282,86 @@ static void DEBWIN_Draw_RTC_sec( void* userWork , DEBUGWIN_ITEM* item )
   DEBUGWIN_ITEM_SetNameV( item , "びょう[%2d]",work->rtcTime.second );
 }
 
+#pragma mark [> PWD
+
+static void DEBWIN_U_PDW_Account( void* userWork , DEBUGWIN_ITEM* item )
+{
+  DREAMWORLD_SAVEDATA *pdwSave = DREAMWORLD_SV_GetDreamWorldSaveData(SaveControl_GetPointer());
+  BOOL isAccount = DREAMWORLD_SV_GetAccount( pdwSave );
+  if( DEBWIN_UTIL_UpdateBOOL( &isAccount ) == TRUE )
+  {
+    DREAMWORLD_SV_SetAccount( pdwSave , isAccount );
+    DEBUGWIN_RefreshScreen();
+  }
+}
+
+static void DEBWIN_D_PDW_Account( void* userWork , DEBUGWIN_ITEM* item )
+{
+  DREAMWORLD_SAVEDATA *pdwSave = DREAMWORLD_SV_GetDreamWorldSaveData(SaveControl_GetPointer());
+  BOOL isAccount = DREAMWORLD_SV_GetAccount( pdwSave );
+  
+  if( isAccount )
+  {
+    DEBUGWIN_ITEM_SetNameV( item , "アカウントO");
+  }
+  else
+  {
+    DEBUGWIN_ITEM_SetNameV( item , "アカウントX");
+  }
+}
+
+static void DEBWIN_U_PDW_SetFurniture( void* userWork , DEBUGWIN_ITEM* item )
+{
+  DREAMWORLD_SAVEDATA *pdwSave = DREAMWORLD_SV_GetDreamWorldSaveData(SaveControl_GetPointer());
+  DREAM_WORLD_FURNITUREDATA *topFurData = DREAMWORLD_SV_GetFurnitureData( pdwSave , 0 );
+  BOOL setEnable = FALSE;
+  if( topFurData->id == DREAM_WORLD_INVALID_FURNITURE )
+  {
+    setEnable = TRUE;
+  }
+  if( GFL_UI_KEY_GetTrg() & PAD_BUTTON_A )
+  {
+    u8 i;
+    for( i=0;i<DREAM_WORLD_DATA_MAX_FURNITURE;i++ )
+    {
+      DREAM_WORLD_FURNITUREDATA *furData = DREAMWORLD_SV_GetFurnitureData( pdwSave , i );
+      if( setEnable == TRUE )
+      {
+        furData->id = i+1;
+        furData->furnitureName[0] = L'か';
+        furData->furnitureName[1] = L'ぐ';
+        furData->furnitureName[2] = L'の';
+        furData->furnitureName[3] = L'な';
+        furData->furnitureName[4] = L'ま';
+        furData->furnitureName[5] = L'え';
+        furData->furnitureName[6] = L'1'+i;
+        furData->furnitureName[7] = 0xFFFF;
+      }
+      else
+      {
+        furData->id = 0;
+      }
+    }
+    DEBUGWIN_RefreshScreen();
+  }
+}
+
+static void DEBWIN_D_PDW_SetFurniture( void* userWork , DEBUGWIN_ITEM* item )
+{
+  DREAMWORLD_SAVEDATA *pdwSave = DREAMWORLD_SV_GetDreamWorldSaveData(SaveControl_GetPointer());
+  DREAM_WORLD_FURNITUREDATA *topFurData = DREAMWORLD_SV_GetFurnitureData( pdwSave , 0 );
+  if( topFurData->id == DREAM_WORLD_INVALID_FURNITURE )
+  {
+    DEBUGWIN_ITEM_SetNameV( item , "かぐカタログX");
+  }
+  else
+  {
+    DEBUGWIN_ITEM_SetNameV( item , "かぐカタログO");
+  }
+}
+
 #pragma mark [> util
-static void DEBWIN_UTIL_UpdateU32( u32 *value , const u32 min, const u32 max )
+static const BOOL DEBWIN_UTIL_UpdateU32( u32 *value , const u32 min, const u32 max )
 {
   u32 addValue = 1;
   if( GFL_UI_KEY_GetCont() & PAD_BUTTON_R )
@@ -288,6 +379,7 @@ static void DEBWIN_UTIL_UpdateU32( u32 *value , const u32 min, const u32 max )
       *value = min;
     }
     DEBUGWIN_RefreshScreen();
+    return TRUE;
   }
   if( GFL_UI_KEY_GetTrg() & PAD_KEY_LEFT )
   {
@@ -300,6 +392,20 @@ static void DEBWIN_UTIL_UpdateU32( u32 *value , const u32 min, const u32 max )
       *value -= addValue;
     }
     DEBUGWIN_RefreshScreen();
+    return TRUE;
   }
+  return FALSE;
 }
 
+static const BOOL DEBWIN_UTIL_UpdateBOOL( BOOL *value )
+{
+  if( (GFL_UI_KEY_GetTrg() & PAD_KEY_RIGHT) ||
+      (GFL_UI_KEY_GetTrg() & PAD_KEY_LEFT)  ||
+      (GFL_UI_KEY_GetTrg() & PAD_BUTTON_A)  )
+  {
+    *value = !*value;
+    DEBUGWIN_RefreshScreen();
+    return TRUE;
+  }
+  return FALSE;
+}
