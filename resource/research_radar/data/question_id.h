699d97eb89365d6fcd05bc70822d10cc3f6cc982
Shigeru Ohmori <ohmori@gamefreak.co.jp>
2010-03-08 03:20:28 +0000

    調査レーダーのgmm更新

--- resource/research_radar/data/question_id.h
+++ resource/research_radar/data/question_id.h
@@ -1,31 +1,31 @@
 // コンバータにより生成
 #define QUESTION_ID_WB (0) //「・ホワイトとブラック　どっち？」
-#define QUESTION_ID_SEX (1) //「・おとこのこ　おんなのこ　どっち？」
+#define QUESTION_ID_SEX (1) //「・おとこと　おんな　どっち？」
 #define QUESTION_ID_FUN (2) //「・たいせんと　こうかん　どっち？」
 #define QUESTION_ID_PLACE (3) //「・とかいと　いなか　すむなら？」
 #define QUESTION_ID_PARTNER (4) //「・あいてを　えらぶなら　どっち？」
-#define QUESTION_ID_WANT (5) //「・ほしいのは　どっち？」
+#define QUESTION_ID_WANT (5) //「・ほしいものは　どっち？」
 #define QUESTION_REQUIREMENT (6) //「・あいてに　もとめるものは？」
 #define QUESTION_ID_FAVORITE_POKEMON (7) //「・どんな　ポケモンが　すき？」
 #define QUESTION_ID_FIRST_POKEMON (8) //「・さいしょにえらんだ　ポケモンは？」
 #define QUESTION_ID_PLAY_SPOT (9) //「・あそびにいくなら？」
 #define QUESTION_ID_FAVORITE_CLASS (10) //「・すきな　がっこうの　じかんは？」
-#define QUESTION_ID_PRECIOUS (11) //「・だいじなのは？」
+#define QUESTION_ID_PRECIOUS (11) //「・じぶんにとって　だいじなのは？」
 #define QUESTION_ID_SEASON (12) //「・どの　きせつが　すき？」
 #define QUESTION_ID_FOOD (13) //「・どんな　たべものが　すき？」
 #define QUESTION_ID_CHARACTER (14) //「・すきな　にんげんの　タイプは？」
 #define QUESTION_ID_ (15) //「・ポケモンの　おもしろいところは？」
 #define QUESTION_ID_STRONG_CLASS (16) //「・すきな　べんきょうは？」
 #define QUESTION_ID_WEAK_CLASS (17) //「・にがてな　べんきょうは？」
-#define QUESTION_ID_DREAM (18) //「・もし　なれるなら？」
-#define QUESTION_ID_DAY_OFF (19) //「・きゅうじつは　なにしてる？」
+#define QUESTION_ID_DREAM (18) //「・もし　なれるとしたら？」
+#define QUESTION_ID_DAY_OFF (19) //「・きゅうじつは　なにをしてる？」
 #define QUESTION_ID_MUSIC (20) //「・どんな　おんがくが　すき？」
 #define QUESTION_ID_TV (21) //「・すきな　TVばんぐみは？」
 #define QUESTION_ID_MOVIE (22) //「・どんな　えいがが　すき？」
 #define QUESTION_ID_SPORTS (23) //「・すきな　スポーツは？」
 #define QUESTION_ID_COLOR (24) //「・すきないろは　どれ？」
-#define QUESTION_ID_HOBBY (25) //「・なにが　しゅみ？」
-#define QUESTION_ID_JOB (26) //「・どんな　しごと？」
+#define QUESTION_ID_HOBBY (25) //「・どんな　しゅみを　もってる？」
+#define QUESTION_ID_JOB (26) //「・どんな　しごとを　してる？」
 #define QUESTION_ID_GYM_LEADER (27) //「・どのジムリーダーが　すき？」
 #define QUESTION_ID_POKEMON_TYPE (28) //「・すきな　ポケモンの　タイプは？」
 #define QUESTION_ID_PLAY_TIME (29) //「・ゲームプレイじかんは？」
