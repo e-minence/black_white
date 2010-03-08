dfcd3aa6bb4d177052d8a16483dc40f9a779c30e
Katsunori Suginaka <suginaka_katsunori@gamefreak.co.jp>
2010-03-08 09:31:18 +0000

    N01R0402:BGメッセージ追加 T02PC0101：自宅戻しコマンドを削除 C01R0102：ゆめのあとちWK追加

--- resource/fldmapdata/eventdata/tmp/n01r0402.h
+++ resource/fldmapdata/eventdata/tmp/n01r0402.h
@@ -15,7 +15,48 @@
 #ifndef ZONE_N01R0402_EVB_H
 #define ZONE_N01R0402_EVB_H
 
-#define BG_N01R0402_GOODS_01l             0
-#define BG_N01R0402_GOODS_01r             1
+#define BG_N01R0402_GOODS_02              0
+#define BG_N01R0402_GOODS_06              1
+#define BG_N01R0402_GOODS_01              2
+#define BG_N01R0402_GOODS_03              3
+#define BG_N01R0402_GOODS_03A             4
+#define BG_N01R0402_GOODS_04              5
+#define BG_N01R0402_GOODS_04A             6
+#define BG_N01R0402_GOODS_04B             7
+#define BG_N01R0402_GOODS_04C             8
+#define BG_N01R0402_GOODS_05              9
+#define BG_N01R0402_GOODS_05A            10
+#define BG_N01R0402_GOODS_05B            11
+#define BG_N01R0402_GOODS_05C            12
+#define BG_N01R0402_GOODS_05D            13
+#define BG_N01R0402_GOODS_05E            14
+#define BG_N01R0402_GOODS_05F            15
+#define BG_N01R0402_GOODS_05G            16
+#define BG_N01R0402_GOODS_06A            17
+#define BG_N01R0402_GOODS_06B            18
+#define BG_N01R0402_GOODS_07             19
+#define BG_N01R0402_GOODS_08F            20
+#define BG_N01R0402_GOODS_08E            21
+#define BG_N01R0402_GOODS_08D            22
+#define BG_N01R0402_GOODS_08C            23
+#define BG_N01R0402_GOODS_08B            24
+#define BG_N01R0402_GOODS_08A            25
+#define BG_N01R0402_GOODS_08R            26
+#define BG_N01R0402_GOODS_08S            27
+#define BG_N01R0402_GOODS_08Q            28
+#define BG_N01R0402_GOODS_08P            29
+#define BG_N01R0402_GOODS_08O            30
+#define BG_N01R0402_GOODS_08N            31
+#define BG_N01R0402_GOODS_08M            32
+#define BG_N01R0402_GOODS_08L            33
+#define BG_N01R0402_GOODS_08K            34
+#define BG_N01R0402_GOODS_08J            35
+#define BG_N01R0402_GOODS_08I            36
+#define BG_N01R0402_GOODS_08H            37
+#define BG_N01R0402_GOODS_08G            38
+#define BG_N01R0402_GOODS_09             39
+#define BG_N01R0402_GOODS_09A            40
+#define BG_N01R0402_GOODS_09B            41
+#define BG_N01R0402_GOODS_09C            42
 
 #endif //ZONE_N01R0402_EVB_H
