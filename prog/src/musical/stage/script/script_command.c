aab598d9560c514ebfd8979ad843c0a9df510b63
Ariizumi Nobuhiko <ariizumi_nobuhiko@gamefreak.co.jp>
2010-03-08 02:25:01 +0000

    デバッグプリント修正

--- prog/src/musical/stage/script/script_command.c
+++ prog/src/musical/stage/script/script_command.c
@@ -27,8 +27,8 @@
 #pragma mark [> define
 
 #if USE_MUS_SCRIPT_PRINT
-#define SCRIPT_TPrintf(...) ARI_TPrintf(...)
-#define SCRIPT_Printf(...) ARI_Printf(...)
+#define SCRIPT_TPrintf(...) ARI_TPrintf(__VA_ARGS__)
+#define SCRIPT_Printf(...) ARI_Printf(__VA_ARGS__)
 #else
 //#define SCRIPT_TPrintf ARI_TPrintf
 //#define SCRIPT_Printf ARI_Printf
