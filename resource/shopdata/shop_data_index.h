84c032f85d12c339e9c7aadc8c7aa31844b7f672
Tetsuji Ohta <ohta_tetsuji@gamefreak.co.jp>
2010-03-09 11:03:40 +0000

    ショップデータを更新

--- resource/shopdata/shop_data_index.h
+++ resource/shopdata/shop_data_index.h
@@ -9,19 +9,19 @@
 #ifndef __SHOP_DATA_INDEX_H__
 #define __SHOP_DATA_INDEX_H__
 
-#define T02_FS    ( 0 )  // タウン１
-#define C01_FS    ( 1 )  // シティ１
-#define C02_FS    ( 2 )  // シティ２
-#define C03_FS    ( 3 )  // シティ３
-#define C04_FS    ( 4 )  // シティ４
-#define C05_FS    ( 5 )  // シティ５
-#define C06_FS    ( 6 )  // シティ６
-#define C07_FS    ( 7 )  // シティ７
-#define C08_FS    ( 8 )  // シティ８
+#define T02_FS    ( 0 )  // カラクサラウン
+#define C01_FS    ( 1 )  // サンヨウシティ
+#define C02_FS    ( 2 )  // シッポウシティ
+#define C03_FS    ( 3 )  // ヒウンシティ
+#define C04_FS    ( 4 )  // ライモンシティ
+#define C05_FS    ( 5 )  // ホドモシティ
+#define C06_FS    ( 6 )  // フキヨシティ
+#define C07_FS    ( 7 )  // セッカシティ
+#define C08_FS    ( 8 )  // ソウリュウシティ
 #define C09_FS    ( 9 )  // ポケモンリーグ
 #define T03_FS    ( 10 )  // カゴメタウン
 #define T04_FS    ( 11 )  // サザナミタウン
-#define C10_FS    ( 12 )  // Ｃ１０ＷＦＢＣ
+#define C10_FS    ( 12 )  // ブラック／ホワイトシティ
 
 
 #endif
