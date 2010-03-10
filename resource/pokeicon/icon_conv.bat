177995d53c02d2fe8d5b2181a1d58ce3e8108f08
Hisashi Sogabe <sogabe@gamefreak.co.jp>
2010-03-10 04:03:33 +0000

    雌雄書き分けに対応

--- resource/pokeicon/icon_conv.bat
+++ resource/pokeicon/icon_conv.bat
@@ -6,24 +6,40 @@ copy poke_icon_128k.nce graphic\poke_icon_128k.nce
 cd graphic
 
 rem poke_icon.nclを各アイコン用にコピーして作成する
-call ncl_copy.bat
+rem call ncl_copy.bat←たぶんやらんでもいいはず　poke_icon.nclとリンクしとけばこれでコンバートするはず
 
 rem 共通のnceをコピーして全てのポケモン分のnceファイルを作成
-call link.bat
+rem call link.bat
 
 rem コンバート実行
-call cnv.bat
+rem call cnv.bat
 
 rem アイコンのパレット番号テーブル生成
-picon_attr_cnv attr_list.txt
+rem picon_attr_cnv attr_list.txt
+
+rem 上三つのバッチファイルを１つのRubyスクリプトで実行する
+ruby poke_icon_conv.rb attr_list.txt
+
+del *.naix
+del *.narc
+
+echo g2dコンバート
+g2dcvtr poke_icon_32k.nce -br
+g2dcvtr poke_icon_64k.nce -br
+g2dcvtr poke_icon_128k.nce -br
+
+g2dcvtr poke_icon.ncl -pcm
+
+echo narc作成
+nnsarc -c -l -n -i poke_icon -S pokeicon_all.scr
 
 echo g2dファイル削除
 del *_????.NCGR
 del *_????.NCER
 del *_????.NANR
 del poke_icon.NCLR
-del icon_???.NCLR
-del icon_*.ncl
+rem del icon_???.NCLR
+rem del icon_*.ncl
 del *.nce
 
 move *.narc ../
