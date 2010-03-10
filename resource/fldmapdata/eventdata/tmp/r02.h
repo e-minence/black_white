15cb3596798b04c42edf2e580e9456f4646a1d7b
Naoto Murakami <murakami_naoto@gamefreak.co.jp>
2010-03-10 03:28:14 +0000

    スクリプト/R02：サポート戦を追加

--- resource/fldmapdata/eventdata/tmp/r02.h
+++ resource/fldmapdata/eventdata/tmp/r02.h
@@ -12,17 +12,16 @@
 #define R02_BOY2_01                        0
 #define R02_GIRL2_01                       1
 #define R02_ROCK_01                        2
-#define R02_MAMA_01                        3
-#define TR_TANPAN_01                       4
-#define TR_MINI_01                         5
-#define TR_TANPAN_04                       6
-#define R02_FLD_ITEM_04                    7
-#define R02_FLD_ITEM_01                    8
-#define R02_FLD_ITEM_03                    9
-#define R02_FLD_ITEM_02                   10
-#define R02_BOY1_01                       11
-#define R02_TREE_01                       12
-#define R02_FLD_ITEM_05                   13
+#define TR_TANPAN_01                       3
+#define TR_MINI_01                         4
+#define TR_TANPAN_04                       5
+#define R02_FLD_ITEM_04                    6
+#define R02_FLD_ITEM_01                    7
+#define R02_FLD_ITEM_03                    8
+#define R02_FLD_ITEM_02                    9
+#define R02_BOY1_01                       10
+#define R02_TREE_01                       11
+#define R02_FLD_ITEM_05                   12
 
 #endif //ZONE_R02_EVC_H
 //Ç±ÇÃÉtÉ@ÉCÉãÇÕ../data/r02.mevÇ©ÇÁé©ìÆê∂ê¨Ç≥ÇÍÇ‹ÇµÇΩ
--- resource/fldmapdata/script/r02_def.h
+++ resource/fldmapdata/script/r02_def.h
@@ -6,11 +6,12 @@
 
 //スクリプトデータID定義
 #define SCRID_POS_R02_MAMA_01 (1)
-#define SCRID_R02_BOY2_01 (2)
-#define SCRID_R02_GIRL2_01 (3)
-#define SCRID_BG_R02_BOARD1_01 (4)
-#define SCRID_BG_R02_BOARD2_01 (5)
-#define SCRID_BG_R02_BOARD3_01 (6)
-#define SCRID_R02_BOY1_01 (7)
+#define SCRID_POS_R02_SUPPORT_01 (2)
+#define SCRID_R02_BOY2_01 (3)
+#define SCRID_R02_GIRL2_01 (4)
+#define SCRID_BG_R02_BOARD1_01 (5)
+#define SCRID_BG_R02_BOARD2_01 (6)
+#define SCRID_BG_R02_BOARD3_01 (7)
+#define SCRID_R02_BOY1_01 (8)
 
 #endif //_R02_DEF_H_
