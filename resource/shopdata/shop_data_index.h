f47cb52f5bf2e812173d4ebc14988db1e358a1b6
Kazumasa Iwao <iwao_kazumasa@gamefreak.co.jp>
2010-03-10 06:23:32 +0000

    ショップデータ修正＋C２道具屋・C5市場用のデータを追加

--- resource/shopdata/shop_data_index.h
+++ resource/shopdata/shop_data_index.h
@@ -9,19 +9,21 @@
 #ifndef __SHOP_DATA_INDEX_H__
 #define __SHOP_DATA_INDEX_H__
 
-#define T02_FS    ( 0 )  // カラクサラウン
+#define T02_FS    ( 0 )  // カラクサタウン
 #define C01_FS    ( 1 )  // サンヨウシティ
 #define C02_FS    ( 2 )  // シッポウシティ
 #define C03_FS    ( 3 )  // ヒウンシティ
 #define C04_FS    ( 4 )  // ライモンシティ
-#define C05_FS    ( 5 )  // ホドモシティ
-#define C06_FS    ( 6 )  // フキヨシティ
+#define C05_FS    ( 5 )  // ホドモエシティ
+#define C06_FS    ( 6 )  // フキヨセシティ
 #define C07_FS    ( 7 )  // セッカシティ
 #define C08_FS    ( 8 )  // ソウリュウシティ
 #define C09_FS    ( 9 )  // ポケモンリーグ
 #define T03_FS    ( 10 )  // カゴメタウン
 #define T04_FS    ( 11 )  // サザナミタウン
-#define C10_FS    ( 12 )  // ブラック／ホワイトシティ
+#define C10_FS    ( 12 )  // ブラックシティ／ホワイトフォレスト
+#define C02_PS    ( 13 )  // シッポウシティその２
+#define C05_PS    ( 14 )  // ホドモエシティその２
 
 
 #endif
