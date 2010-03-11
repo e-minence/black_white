2d69c7e5fa3f6a00ac5f1af5361091fc925f1738
Masafumi Saito <saito_masafumi@gamefreak.co.jp>
2010-03-11 09:20:48 +0000

    イベント追加：C04民家アイテム入手イベント

--- resource/fldmapdata/eventdata/tmp/c04r1002.h
+++ resource/fldmapdata/eventdata/tmp/c04r1002.h
@@ -10,6 +10,8 @@
 #define ZONE_C04R1002_EVC_H
 
 #define C04R1002_TRAINERM_01               0
+#define C04R1002_OLDMAN1_01                1
+#define C04R1002_WOMAN1_01                 2
 
 #endif //ZONE_C04R1002_EVC_H
 //‚±‚Ìƒtƒ@ƒCƒ‹‚Í../data/c04r1002.mev‚©‚ç©“®¶¬‚³‚ê‚Ü‚µ‚½
--- resource/fldmapdata/script/c04r1002_def.h
+++ resource/fldmapdata/script/c04r1002_def.h
@@ -6,5 +6,7 @@
 
 //スクリプトデータID定義
 #define SCRID_C04R1002_TRAINERM_01 (1)
+#define SCRID_C04R1002_WOMAN1_01 (2)
+#define SCRID_C04R1002_OLDMAN1_01 (3)
 
 #endif //_C04R1002_DEF_H_
