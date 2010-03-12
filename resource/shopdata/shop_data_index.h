43f3c7ae640b8e7a47927afbe801a1d58f127f1a
Kazumasa Iwao <iwao_kazumasa@gamefreak.co.jp>
2010-03-12 13:05:08 +0000

    ショップデータ追加修正（未実装のラベル名修正とお香屋追加）

--- resource/shopdata/shop_data_index.h
+++ resource/shopdata/shop_data_index.h
@@ -22,8 +22,13 @@
 #define T03_FS    ( 10 )  // カゴメタウン
 #define T04_FS    ( 11 )  // サザナミタウン
 #define C10_FS    ( 12 )  // ブラックシティ／ホワイトフォレスト
-#define C02_RL    ( 13 )  // シッポウシティその２
-#define C05_RL    ( 14 )  // ホドモエシティその２
+#define SHOP_UP    ( 13 )  // シッポウシティ個人商店
+#define SHOP_KANPO    ( 14 )  // ホドモエシティ市場１
+#define SHOP_OKOU    ( 15 )  // ホドモエシティ市場２
+#define SHOP_MALL1    ( 16 )  // ショッピングモール１Ｆ
+#define SHOP_MALL2    ( 17 )  // ショッピングモール２Ｆ右
+#define SHOP_MALL3    ( 18 )  // ショッピングモール２Ｆ左
+#define SHOP_MALL4    ( 19 )  // ショッピングモール３Ｆ左
 
 
 #endif
