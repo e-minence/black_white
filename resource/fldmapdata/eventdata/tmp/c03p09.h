34ea35ec336e34d7b3677c8487180fd5ac498877
Mai Mizuguchi <mizuguchi_mai@gamefreak.co.jp>
2010-03-12 04:03:40 +0000

    c03p09：はやりのスイーツイベント実装 C09R0201：ウィンドウ修正 c05r0601：ウィンドウ修正 D07R0101：冬だけメッセージ実装

--- resource/fldmapdata/eventdata/tmp/c03p09.h
+++ resource/fldmapdata/eventdata/tmp/c03p09.h
@@ -11,11 +11,22 @@
 #ifndef ZONE_C03P09_EVC_H
 #define ZONE_C03P09_EVC_H
 
+#define C03P09_SHOPW_01                    0
+#define C03P09_ICELINE_01                  1
+#define C03P09_ICELINE_02                  2
+#define C03P09_ICELINE_03                  3
+#define C03P09_ICELINE_04                  4
+#define C03P09_ICELINE_05                  5
+#define C03P09_ICELINE_06                  6
+#define C03P09_ICELINE_07                  7
+#define C03P09_ICELINE_08                  8
 
 #endif //ZONE_C03P09_EVC_H
 //Ç±ÇÃÉtÉ@ÉCÉãÇÕ../data/c03p09.mevÇ©ÇÁé©ìÆê∂ê¨Ç≥ÇÍÇ‹ÇµÇΩ
 #ifndef ZONE_C03P09_EVB_H
 #define ZONE_C03P09_EVB_H
 
+#define BG_C03P09_BOARD_01                0
+#define BG_C03P09_BOARD_02                1
 
 #endif //ZONE_C03P09_EVB_H
