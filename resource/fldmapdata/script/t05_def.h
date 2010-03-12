f580a79e28541e0454cb75d5748a964bffb49d12
Sosuke Tamada <tamada@gamefreak.co.jp>
2010-03-12 15:24:36 +0000

    トレインタウンの電車変更の仕組みを実装（タイミング、抽選方法は仮当）

--- resource/fldmapdata/script/t05_def.h
+++ resource/fldmapdata/script/t05_def.h
@@ -7,5 +7,8 @@
 //スクリプトデータID定義
 #define SCRID_T05_RAILFAN_01 (1)
 #define SCRID_T05_PIPE_01 (2)
+#define SCRID_ZONE_T05 (3)
+#define SCRID_INIT_T05 (4)
+#define SCRID_RECOVER_T05 (5)
 
 #endif //_T05_DEF_H_
