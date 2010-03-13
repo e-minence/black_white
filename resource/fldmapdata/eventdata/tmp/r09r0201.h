8af4ea68c284afde29daf83387ac1fbcbb8946ca
Naoto Murakami <murakami_naoto@gamefreak.co.jp>
2010-03-13 14:25:23 +0000

    スクリプト/d06r0101　ｒ０９ｒ０２０１

--- resource/fldmapdata/eventdata/tmp/r09r0201.h
+++ resource/fldmapdata/eventdata/tmp/r09r0201.h
@@ -10,16 +10,23 @@
 #ifndef ZONE_R09R0201_EVC_H
 #define ZONE_R09R0201_EVC_H
 
-#define R09R0201_WAITRESS_01               0
-#define R09R0201_BOY3_01                   1
-#define R09R0201_MIDDLEWOMAN1_01           2
-#define R09R0201_BREEDERM_01               3
-#define R09R0201_WOMAN3_01                 4
+#define R09R0201_BOY3_01                   0
+#define R09R0201_MIDDLEWOMAN1_01           1
+#define R09R0201_BREEDERM_01               2
+#define R09R0201_WOMAN3_01                 3
+#define R09R0201_BABYBOY_01                4
+#define R09R0201_INFOMATIONW_01            5
+#define R09R0201_BAKER_01                  6
+#define R09R0201_WORKMAN_01                7
 
 #endif //ZONE_R09R0201_EVC_H
 //Ç±ÇÃÉtÉ@ÉCÉãÇÕ../data/r09r0201.mevÇ©ÇÁé©ìÆê∂ê¨Ç≥ÇÍÇ‹ÇµÇΩ
 #ifndef ZONE_R09R0201_EVB_H
 #define ZONE_R09R0201_EVB_H
 
+#define BG_R09R0201_STATUE_01_01          0
+#define BG_R09R0201_STATUE_01_02          1
+#define BG_R09R0201_STATUE_01_03          2
+#define BG_R09R0201_STATUE_01_04          3
 
 #endif //ZONE_R09R0201_EVB_H
