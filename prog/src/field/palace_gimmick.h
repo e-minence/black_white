048675c3d730f3a26e32e094168b2717b8d41248
Nozomu Saito <saito_nozomu@gamefreak.co.jp>
2010-03-15 02:15:37 +0000

    //パレスギミックファイル追加

---
+++ prog/src/field/palace_gimmick.h
@@ -0,0 +1,12 @@
+//======================================================================
+/**
+ * @file  palace_gimmick.h
+ * @brief  パレスギミック
+ * @author  Saito
+ */
+//======================================================================
+
+extern void PALACE_GMK_Setup(FIELDMAP_WORK *fieldWork);
+extern void PALACE_GMK_End(FIELDMAP_WORK *fieldWork);
+extern void PALACE_GMK_Move(FIELDMAP_WORK *fieldWork);
+
