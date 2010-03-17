ec5080e6ac20d7d500576e999ce8dd94876a9b05
Shinya Takahashi <takahashi@gamefreak.co.jp>
2010-03-17 11:01:09 +0000

    接続・配置修正

--- resource/fldmapdata/eventdata/tmp/d10r0101.h
+++ resource/fldmapdata/eventdata/tmp/d10r0101.h
@@ -8,6 +8,10 @@
 #ifndef ZONE_D10R0101_EVC_H
 #define ZONE_D10R0101_EVC_H
 
+#define D10R0101_ROCK_01                   0
+#define D10R0101_ROCK_02                   1
+#define D10R0101_ROCK_04                   2
+#define D10R0101_ROCK_03                   3
 
 #endif //ZONE_D10R0101_EVC_H
 //‚±‚Ìƒtƒ@ƒCƒ‹‚Í../data/d10r0101.mev‚©‚ç©“®¶¬‚³‚ê‚Ü‚µ‚½
