7170b02d71dea1029e2f5a92bc2528212fecfb9a
Hisashi Sogabe <sogabe@gamefreak.co.jp>
2010-03-17 13:16:50 +0000

    ポケモンドリームキャッチ作成

--- prog/include/app/pdc.h
+++ prog/include/app/pdc.h
@@ -24,12 +24,14 @@ typedef enum
 
 typedef struct  _PDC_SETUP_PARAM  PDC_SETUP_PARAM;
 
-extern  PDC_SETUP_PARAM*      PDC_MakeSetUpParam( POKEMON_PARAM* pp, BTL_FIELD_SITUATION* bfs,
-                                                  MYSTATUS* my_status, MYITEM* my_item, HEAPID heapID );
+extern  PDC_SETUP_PARAM*      PDC_MakeSetUpParam( POKEMON_PARAM* pp, BTL_FIELD_SITUATION* bfs, MYSTATUS* my_status,
+                                                  MYITEM* my_item, CONFIG* config, ZUKAN_SAVEDATA* zs, HEAPID heapID );
 extern  POKEMON_PARAM*        PDC_GetPP( PDC_SETUP_PARAM* psp );
 extern  BTL_FIELD_SITUATION*  PDC_GetBFS( PDC_SETUP_PARAM* psp );
 extern  MYSTATUS*             PDC_GetMyStatus( PDC_SETUP_PARAM* psp );
 extern  MYITEM*               PDC_GetMyItem( PDC_SETUP_PARAM* psp );
+extern  CONFIG*               PDC_GetConfig( PDC_SETUP_PARAM* psp );
+extern  ZUKAN_SAVEDATA*       PDC_GetZukanWork( PDC_SETUP_PARAM* psp );
 extern  PDC_RESULT            PDC_GetResult( PDC_SETUP_PARAM* psp );
 
 extern  const GFL_PROC_DATA   PDC_ProcData;
