7da16401213bbf2e4ba52d670289fac4f7131055
Sosuke Tamada <tamada@gamefreak.co.jp>
2010-03-17 09:08:17 +0000

    svnup命令にmultiboot2も追加

--- tools/svnup.bat
+++ tools/svnup.bat
@@ -1,4 +1,4 @@
 @REM %ENV:A=B%‚ÍAŠÂ‹«•Ï”ENV‚ÌA‚ğB‚É’u‚«Š·‚¦‚éA‚Æ‚¢‚¤ˆÓ–¡
 pushd %PROJECT_ROOT:/=\\%
-svn up lib\gflib prog resource tools multiboot
+svn up lib\gflib prog resource tools multiboot multiboot2
 popd
