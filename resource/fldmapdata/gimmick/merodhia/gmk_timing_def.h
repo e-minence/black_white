34c37a2688b4752b2474168ef3fbb344269e60a2
Nozomu Saito <saito_nozomu@gamefreak.co.jp>
2010-03-17 11:18:50 +0000

    //メロディアイベントタイミング定義追加

---
+++ resource/fldmapdata/gimmick/merodhia/gmk_timing_def.h
@@ -0,0 +1,27 @@
+//======================================================================
+/**
+ * @file  gmk_timing_def.h
+ * @brief  メロディアイベントタイミング定義群
+ * @author  Saito
+ * @note    松島さんが編集します
+ */
+//======================================================================
+#pragma once
+
+//↓プログラムで使用します
+#define MERODHIA_ROTATE_RANGE     (4)     //旋回幅
+#define MERODHIA_ROUND_OUT_SYNC     (90)     //最大旋回幅になるまでのシンク数
+#define MERODHIA_ROUND_OUT_NUM     (3)     //最大旋回幅になるまでの旋回回数
+
+#define MERODHIA_ROUND_SYNC     (120)     //最大旋回するシンク数
+#define MERODHIA_ROUND_NUM     (4)     //最大旋回幅で旋回する回数
+
+#define MERODHIA_ROUND_IN_SYNC     (90)     //収縮旋回するシンク数
+#define MERODHIA_ROUND_IN_NUM     (3)     //収縮旋回する回数
+
+
+
+
+
+
+
---
+++ resource/fldmapdata/gimmick/merodhia/scr_gmk_timing_def.h
@@ -0,0 +1,21 @@
+//======================================================================
+/**
+ * @file  scr_gmk_timing_def.h
+ * @brief  メロディアイベントタイミング定義群
+ * @author  Saito
+ * @note    松島さんが編集します
+ */
+//======================================================================
+#pragma once
+
+//↓スクリプトが使用します
+#define MERODHIA_BALL_OUT_WAIT    (330)   //ボールから出す
+#define MERODHIA_DANCE_WAIT       (120)   //ダンス開始
+#define MERODHIA_RISE_WAIT        (0)     //上昇開始
+#define MERODHIA_ROTATE_WAIT      (200)   //旋回開始
+
+
+
+
+
+
