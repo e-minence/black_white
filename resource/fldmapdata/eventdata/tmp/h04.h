05c6a2a4c07fdfae431f95449a3581c8accfeb9a
Naoto Murakami <murakami_naoto@gamefreak.co.jp>
2010-03-18 08:59:18 +0000

    スクリプト/H04

--- resource/fldmapdata/eventdata/tmp/h04.h
+++ resource/fldmapdata/eventdata/tmp/h04.h
@@ -2,29 +2,41 @@
 #ifndef ZONE_H04_EVD_H
 #define ZONE_H04_EVD_H
 
-#define DOOR_ID_H04_H04R0101_EXIT01       0
-#define DOOR_ID_H04_H04R0201_EXIT01       1
-#define DOOR_ID_H04_H04R0301_EXIT01       2
-#define DOOR_ID_H04_H04R0401_EXIT01       3
-#define DOOR_ID_H04_H04R0501_EXIT01       4
+#define DOOR_ID_H04_R11R0101_EXIT01       0
+#define DOOR_ID_H04_R12R0101_EXIT01       1
+#define DOOR_ID_H04_H04R0101_EXIT01       2
+#define DOOR_ID_H04_H04R0201_EXIT01       3
+#define DOOR_ID_H04_H04R0301_EXIT01       4
 #define DOOR_ID_H04_H04R0601_EXIT01       5
-#define DOOR_ID_H04_R11R0101_EXIT01       6
-#define DOOR_ID_H04_R12R0101_EXIT01       7
+#define DOOR_ID_H04_H04R0501_EXIT01       6
+#define DOOR_ID_H04_H04R0401_EXIT01       7
 
 #endif //ZONE_H04_EVD_H
 //このファイルは../data/h04.mevから自動生成されました
 #ifndef ZONE_H04_EVC_H
 #define ZONE_H04_EVC_H
 
-#define H04_WHISLE                         0
-#define H04_GUITER                         1
-#define H04_VOICE                          2
-#define H04_LEADERSINGER                   3
+#define H04_WAITRESS_01                    0
+#define H04_MAN1_01                        1
+#define H04_POKE17_01                      2
+#define H04_POKE20_01                      3
+#define H04_POKE19_01                      4
+#define H04_PIKACHU_01                     5
+#define H04_GIRL2_01                       6
+#define H04_WOMAN1_01                      7
+#define H04_MAN3_01                        8
+#define H04_GUITAR_01                      9
+#define H04_LEADSINGER_01                 10
+#define H04_VOICE_01                      11
+#define H04_GRASSSOUND_01                 12
 
 #endif //ZONE_H04_EVC_H
 //このファイルは../data/h04.mevから自動生成されました
 #ifndef ZONE_H04_EVB_H
 #define ZONE_H04_EVB_H
 
+#define BG_H04_SIGN_03                    0
+#define BG_H04_SIGN_01                    1
+#define BG_H04_SIGN_02                    2
 
 #endif //ZONE_H04_EVB_H
--- resource/fldmapdata/script/h04_def.h
+++ resource/fldmapdata/script/h04_def.h
@@ -5,9 +5,22 @@
 #define _H04_DEF_H_
 
 //スクリプトデータID定義
-#define SCRID_H04_WHISLE (1)
-#define SCRID_H04_GUITER (2)
-#define SCRID_H04_VOICE (3)
-#define SCRID_H04_LEADERSINGER (4)
+#define SCRID_H04_GRASSSOUND_01 (1)
+#define SCRID_H04_GUITAR_01 (2)
+#define SCRID_H04_VOICE_01 (3)
+#define SCRID_H04_LEADSINGER_01 (4)
+#define SCRID_BG_H04_SIGN_01 (5)
+#define SCRID_BG_H04_SIGN_02 (6)
+#define SCRID_BG_H04_SIGN_03 (7)
+#define SCRID_POS_H04_WAITRESS (8)
+#define SCRID_H04_WAITRESS_01 (9)
+#define SCRID_H04_MAN1_01 (10)
+#define SCRID_H04_WOMAN1_01 (11)
+#define SCRID_H04_GIRL2_01 (12)
+#define SCRID_H04_MAN3_01 (13)
+#define SCRID_H04_PIKACHU_01 (14)
+#define SCRID_H04_POKE17_01 (15)
+#define SCRID_H04_POKE19_01 (16)
+#define SCRID_H04_POKE20_01 (17)
 
 #endif //_H04_DEF_H_
