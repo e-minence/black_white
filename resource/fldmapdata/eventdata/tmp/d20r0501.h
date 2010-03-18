3bf2985ce08c8109ae684320f6af6214c73f2366
Katsunori Suginaka <suginaka_katsunori@gamefreak.co.jp>
2010-03-18 12:11:49 +0000

    海底遺跡スクリプト追加

--- resource/fldmapdata/eventdata/tmp/d20r0501.h
+++ resource/fldmapdata/eventdata/tmp/d20r0501.h
@@ -4,6 +4,9 @@
 
 #define DOOR_ID_D20R0501_D20R0601_EXIT01  0
 #define DOOR_ID_D20R0501_D20R0301_EXIT01  1
+#define DOOR_ID_D20R0501_D20R0101_EXIT01  2
+#define DOOR_ID_D20R0501_D20R0201_EXIT01  3
+#define DOOR_ID_D20R0501_D20R0401_EXIT01  4
 
 #endif //ZONE_D20R0501_EVD_H
 //Ç±ÇÃÉtÉ@ÉCÉãÇÕ../data/d20r0501.mevÇ©ÇÁé©ìÆê∂ê¨Ç≥ÇÍÇ‹ÇµÇΩ
@@ -17,5 +20,56 @@
 #ifndef ZONE_D20R0501_EVB_H
 #define ZONE_D20R0501_EVB_H
 
+#define BGID_D20R0501_BG1_01              0
+#define BGID_D20R0501_BG1_02              1
+#define BGID_D20R0501_BG1_03              2
+#define BGID_D20R0501_BG1_04              3
+#define BGID_D20R0501_BG1_05              4
+#define BGID_D20R0501_BG1_06              5
+#define BGID_D20R0501_BG1_07              6
+#define BGID_D20R0501_BG1_08              7
+#define BGID_D20R0501_BG1_09              8
+#define BGID_D20R0501_BG1_10              9
+#define BGID_D20R0501_BG1_11             10
+#define BGID_D20R0501_BG1_12             11
+#define BGID_D20R0501_BG2_01             12
+#define BGID_D20R0501_BG2_02             13
+#define BGID_D20R0501_BG2_03             14
+#define BGID_D20R0501_BG2_04             15
+#define BGID_D20R0501_BG2_05             16
+#define BGID_D20R0501_BG2_06             17
+#define BGID_D20R0501_BG2_07             18
+#define BGID_D20R0501_BG2_08             19
+#define BGID_D20R0501_BG2_09             20
+#define BGID_D20R0501_BG2_10             21
+#define BGID_D20R0501_BG2_11             22
+#define BGID_D20R0501_BG2_12             23
+#define BGID_D20R0501_BG3_01             24
+#define BGID_D20R0501_BG3_02             25
+#define BGID_D20R0501_BG3_03             26
+#define BGID_D20R0501_BG3_04             27
+#define BGID_D20R0501_BG3_05             28
+#define BGID_D20R0501_BG3_06             29
+#define BGID_D20R0501_BG3_07             30
+#define BGID_D20R0501_BG3_08             31
+#define BGID_D20R0501_BG3_09             32
+#define BGID_D20R0501_BG3_10             33
+#define BGID_D20R0501_BG3_11             34
+#define BGID_D20R0501_BG3_12             35
+#define BGID_D20R0501_BG4_01             36
+#define BGID_D20R0501_BG4_02             37
+#define BGID_D20R0501_BG4_03             38
+#define BGID_D20R0501_BG4_04             39
+#define BGID_D20R0501_BG4_05             40
+#define BGID_D20R0501_BG4_06             41
+#define BGID_D20R0501_BG4_07             42
+#define BGID_D20R0501_BG4_08             43
+#define BGID_D20R0501_BG4_09             44
+#define BGID_D20R0501_BG4_10             45
+#define BGID_D20R0501_BG4_11             46
+#define BGID_D20R0501_BG4_12             47
+#define BGID_D20R0501_BG8_01             48
+#define BGID_D20R0501_BG8_02             49
+#define BGID_D20R0501_BG8_03             50
 
 #endif //ZONE_D20R0501_EVB_H
