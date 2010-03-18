288ddf774bf588d9f4311c4b51d50e3b9d332f7d
Yukiko Hozumi <hozumi_yukiko@gamefreak.co.jp>
2010-03-18 11:56:07 +0000

    H04のマップをハイブリッド用に修正しました

--- resource/fldmapdata/rail_data/rail/h04.h
+++ resource/fldmapdata/rail_data/rail/h04.h
@@ -1,5 +1,17 @@
 #pragma once
 // out put rail_conv.pl
 // rail line name
-#define RE_LINE_0 (0)
-#define RE_LINE_1 (1)
+#define LINE_GROUND2 (0)
+#define LINE_SLOPE1 (1)
+#define LINE_GROUND1 (2)
+#define div_3_RE_LINE_0 (3)
+#define LINE_SLOPE2 (4)
+#define div_5_RE_LINE_0 (5)
+#define LINE_SLOPE3 (6)
+#define div_7_RE_LINE_0 (7)
+#define div_8_RE_LINE_0 (8)
+#define LINE_SLOPE4 (9)
+#define div_10_RE_LINE_0 (10)
+#define LINE_SLOPE5 (11)
+#define div_12_RE_LINE_0 (12)
+#define LINE_SLOPE6 (13)
