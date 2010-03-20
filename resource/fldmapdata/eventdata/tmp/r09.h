1b5cbe3b128f4c7dd3547cbd473c1e66a5c2edf6
Naoto Murakami <murakami_naoto@gamefreak.co.jp>
2010-03-20 13:20:49 +0000

    スクリプト/R09　R02

--- resource/fldmapdata/eventdata/tmp/r09.h
+++ resource/fldmapdata/eventdata/tmp/r09.h
@@ -21,7 +21,7 @@
 #define R09_FLD_ITEM_01                    6
 #define R09_FLD_ITEM_02                    7
 #define R09_FLD_ITEM_03                    8
-#define R09_BADMAN_01                      9
+#define R09_BASEBALLM_01                   9
 
 #endif //ZONE_R09_EVC_H
 //Ç±ÇÃÉtÉ@ÉCÉãÇÕ../data/r09.mevÇ©ÇÁé©ìÆê∂ê¨Ç≥ÇÍÇ‹ÇµÇΩ
