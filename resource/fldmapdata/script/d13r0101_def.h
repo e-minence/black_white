ca983638b2f94679f3ba43209855d9400255fb7b
Nozomu Saito <saito_nozomu@gamefreak.co.jp>
2010-03-23 03:44:16 +0000

    //インセクタイベント追加

--- resource/fldmapdata/script/d13r0101_def.h
+++ resource/fldmapdata/script/d13r0101_def.h
@@ -7,5 +7,8 @@
 //スクリプトデータID定義
 #define SCRID_D13R0101_ASSISTANTM_01 (1)
 #define SCRID_BG_D13R0101_PC_01 (2)
+#define SCRID_ZONE_D13R0101 (3)
+#define SCRID_SCENE_D13R0101 (4)
+#define SCRID_INIT_D13R0101 (5)
 
 #endif //_D13R0101_DEF_H_
