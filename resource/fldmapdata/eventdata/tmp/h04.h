da6b8b78b2dea36b2a2257006686405574a86f2a
Naoto Murakami <murakami_naoto@gamefreak.co.jp>
2010-03-27 13:37:27 +0000

    マップ修正/h04

--- resource/fldmapdata/eventdata/tmp/h04.h
+++ resource/fldmapdata/eventdata/tmp/h04.h
@@ -5,26 +5,26 @@
 #define DOOR_ID_H04_R11R0101_EXIT01       0
 #define DOOR_ID_H04_R12R0101_EXIT01       1
 #define DOOR_ID_H04_H04R0101_EXIT01       2
-#define DOOR_ID_H04_H04R0201_EXIT01       3
-#define DOOR_ID_H04_H04R0301_EXIT01       4
+#define DOOR_ID_H04_H04R0301_EXIT01       3
+#define DOOR_ID_H04_H04R0401_EXIT01       4
 #define DOOR_ID_H04_H04R0601_EXIT01       5
 #define DOOR_ID_H04_H04R0501_EXIT01       6
-#define DOOR_ID_H04_H04R0401_EXIT01       7
+#define DOOR_ID_H04_H04R0201_EXIT01       7
 
 #endif //ZONE_H04_EVD_H
 //Ç±ÇÃÉtÉ@ÉCÉãÇÕ../data/h04.mevÇ©ÇÁé©ìÆê∂ê¨Ç≥ÇÍÇ‹ÇµÇΩ
 #ifndef ZONE_H04_EVC_H
 #define ZONE_H04_EVC_H
 
-#define H04_WAITRESS_01                    0
-#define H04_MAN1_01                        1
-#define H04_POKE17_01                      2
-#define H04_POKE20_01                      3
-#define H04_POKE19_01                      4
-#define H04_PIKACHU_01                     5
-#define H04_GIRL2_01                       6
-#define H04_WOMAN1_01                      7
-#define H04_MAN3_01                        8
+#define H04_MAN1_01                        0
+#define H04_POKE17_01                      1
+#define H04_POKE20_01                      2
+#define H04_POKE19_01                      3
+#define H04_PIKACHU_01                     4
+#define H04_GIRL2_01                       5
+#define H04_WOMAN1_01                      6
+#define H04_MAN3_01                        7
+#define H04_WAITRESS_01                    8
 #define H04_GUITAR_01                      9
 #define H04_LEADSINGER_01                 10
 #define H04_VOICE_01                      11
