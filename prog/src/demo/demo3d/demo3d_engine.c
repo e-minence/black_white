ab7f745398c305ed0852efb46f107e0e0d1b956e
Miyuki Iwasawa <iwasawa@gamefreak.co.jp>
2010-03-28 06:49:16 +0000

    demo3d ユニット生成最大数を引き上げ

--- prog/src/demo/demo3d/demo3d_engine.c
+++ prog/src/demo/demo3d/demo3d_engine.c
@@ -186,7 +186,7 @@ DEMO3D_ENGINE_WORK* Demo3D_ENGINE_Init( DEMO3D_GRAPHIC_WORK* graphic, DEMO3D_PAR
   ICA_ANIME_SetAnimeFrame( wk->ica_anime, FX32_CONST(wk->env.start_frame) );
 
   // 3D管理ユーティリティーの生成
-  wk->g3d_util = GFL_G3D_UTIL_Create( 10, 16, heapID );
+  wk->g3d_util = GFL_G3D_UTIL_Create( unit_max, unit_max, heapID );
 
   // ユニット追加
   {
