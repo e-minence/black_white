5d4069b0c77c5e1a3ee4fef283d1cc09d8836b43
Masafumi Saito <saito_masafumi@gamefreak.co.jp>
2010-03-28 10:58:34 +0000

    イベント修正：ビッグスタジアム受付

--- resource/fldmapdata/eventdata/tmp/c04r0301.h
+++ resource/fldmapdata/eventdata/tmp/c04r0301.h
@@ -13,12 +13,12 @@
 
 #define C04R0301_INFOMATIONW_01            0
 #define C04R0301_TRAINERM_01               1
-#define C04R0301_BASEBALLM_01              2
+#define C04R0301_SPORTS_01                 2
 #define C04R0301_OL_01                     3
 #define C04R0301_BOY2_01                   4
-#define C04R0301_GUARDMAN_01               5
-#define C04R0301_GUARDMAN_02               6
-#define C04R0301_GUARDMAN_03               7
+#define C04R0301_POLICEMAN_01              5
+#define C04R0301_POLICEMAN_02              6
+#define C04R0301_POLICEMAN_03              7
 
 #endif //ZONE_C04R0301_EVC_H
 //Ç±ÇÃÉtÉ@ÉCÉãÇÕ../data/c04r0301.mevÇ©ÇÁé©ìÆê∂ê¨Ç≥ÇÍÇ‹ÇµÇΩ
