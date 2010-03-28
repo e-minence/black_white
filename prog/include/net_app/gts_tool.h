3bc6643ed078b146702a4e7b31b95c15722744f7
Toru Nagihashi <nagihashi@gamefreak.co.jp>
2010-03-28 09:19:13 +0000

    外部配布リボン設定追加

--- prog/include/net_app/gts_tool.h
+++ prog/include/net_app/gts_tool.h
@@ -1,8 +1,8 @@
 //[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[
 /**
  *
- *	@file		gts_tool.h
- *	@brief  GTSNEGOとの共有ツール
+ *	@file	  gts_tool.h
+ *	@brief  外部リボン
  *	@author	Toru=Nagihashi
  *	@date		2010.03.18
  *
@@ -14,9 +14,27 @@
 
 //=============================================================================
 /**
- *					定数宣言
+ *					外部配布リボンデータ
 */
 //=============================================================================
+static const int sc_distribution_ribbon_tbl[] =
+{ 
+  ID_PARA_country_ribbon,           //ポケモンリーグ優勝記念(全国大会地区優勝者
+  ID_PARA_national_ribbon,          //ダークポケモン リライブ完了の証
+  ID_PARA_earth_ribbon,             //バトル山 100人抜き達成
+  ID_PARA_world_ribbon,             //ポケモンリーグ優勝記念(全国大会で日本一になった者のみ)
+  ID_PARA_sinou_classic_ribbon,     //外部配布用
+  ID_PARA_sinou_premiere_ribbon,    //外部配布用
+  ID_PARA_sinou_history_ribbon,     //外部配布用：
+  ID_PARA_sinou_green_ribbon,       //外部配布用：
+  ID_PARA_sinou_blue_ribbon,        //外部配布用：
+  ID_PARA_sinou_festival_ribbon,    //外部配布用：
+  ID_PARA_sinou_carnival_ribbon,    //外部配布用：
+  ID_PARA_marine_ribbon,            //汎用バトル大会
+  ID_PARA_land_ribbon,              //WCS地方大会優勝者配布用
+  ID_PARA_sky_ribbon,               //WCS国大会優勝者配布用
+  ID_PARA_sinou_red_ribbon,         //WCS世界大会優勝者配布用                                  
+};
 
 //=============================================================================
 /**
@@ -34,24 +52,7 @@
 //-----------------------------------------------------------------------------
 static inline BOOL GTS_TOOL_IsForbidRibbonPPP( POKEMON_PASO_PARAM *p_ppp )
 {
-  static const int sc_forbid_ribbon_tbl[] =
-  { 
-    ID_PARA_country_ribbon,           //ポケモンリーグ優勝記念(全国大会地区優勝者
-    ID_PARA_national_ribbon,          //ダークポケモン リライブ完了の証
-    ID_PARA_earth_ribbon,             //バトル山 100人抜き達成
-    ID_PARA_world_ribbon,             //ポケモンリーグ優勝記念(全国大会で日本一になった者のみ)
-    ID_PARA_sinou_classic_ribbon,     //外部配布用
-    ID_PARA_sinou_premiere_ribbon,    //外部配布用
-    ID_PARA_sinou_history_ribbon,     //外部配布用：
-    ID_PARA_sinou_green_ribbon,       //外部配布用：
-    ID_PARA_sinou_blue_ribbon,        //外部配布用：
-    ID_PARA_sinou_festival_ribbon,    //外部配布用：
-    ID_PARA_sinou_carnival_ribbon,    //外部配布用：
-    ID_PARA_marine_ribbon,            //汎用バトル大会
-    ID_PARA_land_ribbon,              //WCS地方大会優勝者配布用
-    ID_PARA_sky_ribbon,               //WCS国大会優勝者配布用
-    ID_PARA_sinou_red_ribbon,         //WCS世界大会優勝者配布用                                  
-  };
+
 
   BOOL ret;
 
@@ -60,9 +61,9 @@ static inline BOOL GTS_TOOL_IsForbidRibbonPPP( POKEMON_PASO_PARAM *p_ppp )
     BOOL temp;
 
     temp = PPP_FastModeOn( p_ppp );
-    for( i = 0; i < NELEMS(sc_forbid_ribbon_tbl ); i++ )
+    for( i = 0; i < NELEMS(sc_distribution_ribbon_tbl ); i++ )
     { 
-      ret = PPP_Get( p_ppp, sc_forbid_ribbon_tbl[i], NULL );
+      ret = PPP_Get( p_ppp, sc_distribution_ribbon_tbl[i], NULL );
       if( ret == TRUE )
       { 
         break;
@@ -77,4 +78,15 @@ static inline BOOL GTS_TOOL_IsForbidRibbonPPP( POKEMON_PASO_PARAM *p_ppp )
 static inline BOOL GTS_TOOL_IsForbidRibbonPP( POKEMON_PARAM *p_pp )
 { 
   return GTS_TOOL_IsForbidRibbonPPP( PP_GetPPPPointer( p_pp ) );
+}
+
+static inline u32 GTS_TOOL_GetDistributeRibbonMax( void )
+{ 
+  return NELEMS(sc_distribution_ribbon_tbl);
+}
+
+static inline u32 GTS_TOOL_GetDistributeRibbon( u32 idx )
+{ 
+  GF_ASSERT( idx < GTS_TOOL_GetDistributeRibbonMax() );
+  return sc_distribution_ribbon_tbl[ idx ];
 }
