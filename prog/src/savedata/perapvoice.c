33ca08a4a20795ca95b3f9288ba139d8fe73cd6c
Akito Mori <mori@gamefreak.co.jp>
2010-03-29 14:10:11 +0000

    ペラップボイスのエンコード・デコード処理を書くまでは録音状態を常にFALSEで返すようにした

--- prog/src/savedata/perapvoice.c
+++ prog/src/savedata/perapvoice.c
@@ -1,8 +1,8 @@
 //============================================================================================
 /**
- * @file	perapvoice.c
+ * @file  perapvoice.c
  * @brief
- * @date	2006.04.06
+ * @date  2006.04.06
  */
 //============================================================================================
 
@@ -12,125 +12,129 @@
 #include "perapvoice_local.h"
 
 
-
-
+// @todo ペラップボイス格納ルーチンを書く
+#define PERAPVOICE_NO_FUNC
 
 //============================================================================================
 //============================================================================================
 //----------------------------------------------------------
 /**
- * @brief	オウムポケモンの声データセーブワークの定義
+ * @brief オウムポケモンの声データセーブワークの定義
  */
 //----------------------------------------------------------
 struct PERAPVOICE {
-	BOOL exist_flag;
-	s8 voicedata[PERAPVOICE_LENGTH];
+  BOOL exist_flag;
+  s8 voicedata[PERAPVOICE_LENGTH];
 };
 
 //============================================================================================
 //
-//	セーブデータシステムが依存する関数
+//  セーブデータシステムが依存する関数
 //
 //============================================================================================
 //----------------------------------------------------------
 /**
- * @brief	ぺラップの声ワークのサイズ取得
- * @return	int		サイズ（バイト単位）
+ * @brief ぺラップの声ワークのサイズ取得
+ * @return  int   サイズ（バイト単位）
  */
 //----------------------------------------------------------
 int PERAPVOICE_GetWorkSize(void)
 {
-	return sizeof(PERAPVOICE);
+  return sizeof(PERAPVOICE);
 }
 
 //----------------------------------------------------------
 /**
- * @brief	ぺラップの声ワークの初期化
- * @param	pv		PERAPVOICEデータへのポインタ
+ * @brief ぺラップの声ワークの初期化
+ * @param pv    PERAPVOICEデータへのポインタ
  */
 //----------------------------------------------------------
 void PERAPVOICE_Init(PERAPVOICE * pv)
 {
-	MI_CpuClear32(pv, sizeof(PERAPVOICE));
-	pv->exist_flag = FALSE;
+  MI_CpuClear32(pv, sizeof(PERAPVOICE));
+  pv->exist_flag = FALSE;
 }
 
 //----------------------------------------------------------
 /**
- * @brief	ぺラップの声ワークの初期化
- * @param	heapID		メモリ確保をおこなうヒープ指定
- * @return	PERAPVOICE	取得したワークへのポインタ
+ * @brief ぺラップの声ワークの初期化
+ * @param heapID    メモリ確保をおこなうヒープ指定
+ * @return  PERAPVOICE  取得したワークへのポインタ
  */
 //----------------------------------------------------------
 PERAPVOICE * PERAPVOICE_AllocWork(HEAPID heapID)
 {
-	PERAPVOICE *pv = GFL_HEAP_AllocClearMemory(heapID, sizeof(PERAPVOICE));
-	PERAPVOICE_Init(pv);
-	return pv;
+  PERAPVOICE *pv = GFL_HEAP_AllocClearMemory(heapID, sizeof(PERAPVOICE));
+  PERAPVOICE_Init(pv);
+  return pv;
 }
 
 //============================================================================================
 //============================================================================================
 //----------------------------------------------------------
 /**
- * @brief	ぺラップの声セーブデータの取得
- * @param	sv		セーブデータへのポインタ
- * @return	PERAPVOICEデータへのポインタ
+ * @brief ぺラップの声セーブデータの取得
+ * @param sv    セーブデータへのポインタ
+ * @return  PERAPVOICEデータへのポインタ
  */
 //----------------------------------------------------------
 PERAPVOICE * SaveData_GetPerapVoice(SAVE_CONTROL_WORK * sv)
 {
-	return (PERAPVOICE*)SaveControl_DataPtrGet(sv, GMDATA_ID_PERAPVOICE);
+  return (PERAPVOICE*)SaveControl_DataPtrGet(sv, GMDATA_ID_PERAPVOICE);
 }
 
 //============================================================================================
 //============================================================================================
 //----------------------------------------------------------
 /**
- * @brief	声データが存在するかどうかのチェック
- * @param	pv		声データへのポインタ
- * @return	BOOL	TRUE：存在する、FALSE：存在しない
+ * @brief 声データが存在するかどうかのチェック
+ * @param pv    声データへのポインタ
+ * @return  BOOL  TRUE：存在する、FALSE：存在しない
  */
 //----------------------------------------------------------
 BOOL PERAPVOICE_GetExistFlag(const PERAPVOICE * pv)
 {
-	return pv->exist_flag;
+#ifdef PERAPVOICE_NO_FUNC
+  return 0;
+#else
+  return pv->exist_flag;
+#endif
 }
 
 //==============================================================================
 /**
  * @brief   声データの存在フラグを落とす
  *
- * @param   pv		声データへのポインタ
+ * @param   pv    声データへのポインタ
  *
- * @retval  none		
+ * @retval  none    
  */
 //==============================================================================
 void PERAPVOICE_ClearExistFlag( PERAPVOICE * pv )
 {
-	pv->exist_flag = 0;
+  pv->exist_flag = 0;
 }
 
 //----------------------------------------------------------
 /**
- * @brief	声データの取得
- * @param	pv		声データへのポインタ
- * @return	声データへのポインタ
+ * @brief 声データの取得
+ * @param pv    声データへのポインタ
+ * @return  声データへのポインタ
  */
 //----------------------------------------------------------
 const void * PERAPVOICE_GetVoiceData(const PERAPVOICE * pv)
 {
-	return pv->voicedata;
+  return pv->voicedata;
 }
 
 //==============================================================================
 /**
  * $brief   声データの展開
  *
- * @param   des		展開先
- * @param   src		ペラップボイスのポインタ
+ * @param   des   展開先
+ * @param   src   ペラップボイスのポインタ
  *
- * @retval  none		
+ * @retval  none    
  *
  * 06/04/15時点ではペラップボイスは4bit,2khz,1秒サンプリング=1k
  * このデータを8bitに伸ばして2kにして格納する
@@ -138,33 +142,33 @@ const void * PERAPVOICE_GetVoiceData(const PERAPVOICE * pv)
 //==============================================================================
 void PERAPVOICE_ExpandVoiceData( s8 *des, const s8 *src )
 {
-	int i,count=0;
-	s8 tmp;
-	u8 dat;
+  int i,count=0;
+  s8 tmp;
+  u8 dat;
 
-	// 4bit1kbyteを8bit2kbyteに伸ばす
-	for(i=0;i<PERAPVOICE_LENGTH;i++){
-		dat = src[i]&0x0f;
-		tmp = dat-8;
-		
-		des[count]   = tmp*16;
+  // 4bit1kbyteを8bit2kbyteに伸ばす
+  for(i=0;i<PERAPVOICE_LENGTH;i++){
+    dat = src[i]&0x0f;
+    tmp = dat-8;
+    
+    des[count]   = tmp*16;
 
-		dat = src[i]>>4;
-		tmp = dat-8;
-		des[count+1]   = tmp*16;
+    dat = src[i]>>4;
+    tmp = dat-8;
+    des[count+1]   = tmp*16;
 
 
-//		des[count  ] = src[i];
-//		des[count+1] = src[i]>>4;
-		count += 2;
-	}
+//    des[count  ] = src[i];
+//    des[count+1] = src[i]>>4;
+    count += 2;
+  }
 }
 
 //----------------------------------------------------------
 /**
- * @brief	声データのセット
- * @param	pv		声データへのポインタ
- * @param	src		セットするデータへのポインタ
+ * @brief 声データのセット
+ * @param pv    声データへのポインタ
+ * @param src   セットするデータへのポインタ
  *
  * 06/04/15時点ではペラップボイスは8bit,2khz,1秒サンプリング=2k
  * このデータを4bitに縮めて1kにして格納する
@@ -173,28 +177,28 @@ void PERAPVOICE_ExpandVoiceData( s8 *des, const s8 *src )
 //----------------------------------------------------------
 void PERAPVOICE_SetVoiceData(PERAPVOICE * pv, const s8 * src)
 {
-	int i,count;
-	s8 tmp;
-	u8 dat;
+  int i,count;
+  s8 tmp;
+  u8 dat;
 
-	// 格納フラグを立てる
-	pv->exist_flag = TRUE;
+  // 格納フラグを立てる
+  pv->exist_flag = TRUE;
 
-	// 2khz8bitを4bitにして格納
-	count = 0;
-	for(i=0;i<PERAPVOICE_LENGTH*2;i+=2){
-		tmp = (src[i]/16);
-		dat = tmp+8;
-		pv->voicedata[count]   = dat;
-//		pv->voicedata[count]  =  src[i];
+  // 2khz8bitを4bitにして格納
+  count = 0;
+  for(i=0;i<PERAPVOICE_LENGTH*2;i+=2){
+    tmp = (src[i]/16);
+    dat = tmp+8;
+    pv->voicedata[count]   = dat;
+//    pv->voicedata[count]  =  src[i];
 
-		tmp = (src[i+1]/16);
-		dat = tmp+8;
+    tmp = (src[i+1]/16);
+    dat = tmp+8;
 
-		pv->voicedata[count] |= (dat<<4);
-//		pv->voicedata[count] |= (src[i+1]<<4);
-		count++;
-	}
+    pv->voicedata[count] |= (dat<<4);
+//    pv->voicedata[count] |= (src[i+1]<<4);
+    count++;
+  }
 
 }
 
@@ -202,13 +206,13 @@ void PERAPVOICE_SetVoiceData(PERAPVOICE * pv, const s8 * src)
 /**
  * $brief   声データのコピー
  *
- * @param   des		コピー先声データのポインタ
- * @param   src		コピー元声データのポインタ
+ * @param   des   コピー先声データのポインタ
+ * @param   src   コピー元声データのポインタ
  *
- * @retval  none	
+ * @retval  none  
  */
 //==============================================================================
 void PERAPVOICE_CopyData(PERAPVOICE * des, const PERAPVOICE * src)
 {
-	GFL_STD_MemCopy(src, des, sizeof(PERAPVOICE));
+  GFL_STD_MemCopy(src, des, sizeof(PERAPVOICE));
 }
