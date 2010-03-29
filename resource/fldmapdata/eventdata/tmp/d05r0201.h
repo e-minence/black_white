3ee2746d5b1437c1fa516f425fdbecfffeb0a1ef
Katsunori Suginaka <suginaka_katsunori@gamefreak.co.jp>
2010-03-29 10:47:30 +0000

    D04:接続修正 C03:スクリプト修正

--- resource/fldmapdata/eventdata/tmp/d05r0201.h
+++ resource/fldmapdata/eventdata/tmp/d05r0201.h
@@ -14,17 +14,16 @@
 #define D05R0201_SAGE_01                   2
 #define D05R0201_RIVAL_01                  3
 #define D05R0201_WORKMAN_01                4
-#define D05R0201_SAGE_02                   5
-#define D05R0201_HAKAIM_03                 6
-#define D05R0201_HAKAIW_04                 7
-#define D05R0201_HAKAIW_05                 8
-#define D05R0201_HAKAIM_06                 9
-#define D05R0201_HAKAIM_07                10
-#define D05R0201_HAKAIM_08                11
-#define D05R0201_WORKMAN_02               12
-#define D05R0201_WORKMAN_03               13
-#define D05R0201_WORKMAN_04               14
-#define D05R0201_LEADER5_01               15
+#define D05R0201_HAKAIM_03                 5
+#define D05R0201_HAKAIW_04                 6
+#define D05R0201_HAKAIW_05                 7
+#define D05R0201_HAKAIM_06                 8
+#define D05R0201_HAKAIM_07                 9
+#define D05R0201_HAKAIM_08                10
+#define D05R0201_WORKMAN_02               11
+#define D05R0201_WORKMAN_03               12
+#define D05R0201_WORKMAN_04               13
+#define D05R0201_LEADER5_01               14
 
 #endif //ZONE_D05R0201_EVC_H
 //Ç±ÇÃÉtÉ@ÉCÉãÇÕ../data/d05r0201.mevÇ©ÇÁé©ìÆê∂ê¨Ç≥ÇÍÇ‹ÇµÇΩ
