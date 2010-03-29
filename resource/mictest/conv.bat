6ea4208a392fe7a4114c7c152b6433b3b8c3fe7a
Akito Mori <mori@gamefreak.co.jp>
2010-03-29 11:36:13 +0000

    マイクテスト素材更新。task_menu.ncg task_menu.nclをapp_menu_commonフォルダから コピーしてコンバートするようにした。

--- resource/mictest/conv.bat
+++ resource/mictest/conv.bat
@@ -1,8 +1,10 @@
 PATH=%PATH%;c:\NitroSDK\tools\bin;c:\NitroSystem\tools\win\bin;
 
+copy ..\app_menu_common\task_menu.ncl .\
+copy ..\app_menu_common\task_menu.ncg .\
 
 g2dcvtr back_bg_up.ncl
-g2dcvtr back_bg_dwon.ncl 
+g2dcvtr back_bg_down.ncl 
 g2dcvtr back_bg_up.nsc
 g2dcvtr back_bg_down.nsc
 g2dcvtr obj_gage.ncl
@@ -12,6 +14,13 @@ g2dcvtr obj_pokemon.nce
 g2dcvtr obj_note.ncl
 g2dcvtr obj_note.nce
 g2dcvtr obj_button.ncl
+
+@echo off
+REM ボタンOBJは\resource\app_menu_common\task_menu.ncg task_menu.nclをコピーしてきて作成しています。
+REM ncgが更新された時には反映させる必要があります。
+REM そのためコンバートしてもこのフォルダのobj_button.ncgは反映していません。
+@echo on
+
 g2dcvtr obj_button.nce
 
 @if errorlevel 1 goto err
