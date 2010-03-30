356b6ca201d92824385a1df6193d3f1acabcbb5d
Naoto Murakami <murakami_naoto@gamefreak.co.jp>
2010-03-30 04:29:04 +0000

    マップ：フィールドアイテム配置H04 R16 R14R0201/R16修正

--- resource/fldmapdata/eventdata/tmp/r14r0201.h
+++ resource/fldmapdata/eventdata/tmp/r14r0201.h
@@ -13,6 +13,16 @@
 #define R14R0201_BOY1_01                   1
 #define R14R0201_GIRL1_01                  2
 #define R14R0201_POKE9_01                  3
+#define R014_FLD_ITEM_01                   4
+#define R014_FLD_ITEM_02                   5
+#define R014_FLD_ITEM_03                   6
+#define R014_FLD_ITEM_04                   7
+#define R014_FLD_ITEM_05                   8
+#define R014_FLD_ITEM_06                   9
+#define R014_FLD_ITEM_07                  10
+#define R014_FLD_ITEM_08                  11
+#define R014_FLD_ITEM_09                  12
+#define R014_FLD_ITEM_10                  13
 
 #endif //ZONE_R14R0201_EVC_H
 //Ç±ÇÃÉtÉ@ÉCÉãÇÕ../data/r14r0201.mevÇ©ÇÁé©ìÆê∂ê¨Ç≥ÇÍÇ‹ÇµÇΩ
