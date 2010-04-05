4d1203e330798532b1c456821b6e7fa7e3d3cb52
Toshihiro Obata <obata_toshihiro@gamefreak.co.jp>
2010-04-05 15:07:59 +0000

    デバッグ出力部分を削除

--- resource/research_radar/data/topic.rb
+++ resource/research_radar/data/topic.rb
@@ -87,7 +87,6 @@ class Topic
     outData << "captionJPN            = #{@captionJPN}"
     outData << "captionJPN_KANJI      = #{@captionJPN_KANJI}"
     @questions.each do |question|
-      puts question.titleStringJPN
       outData << "Q.#{question.titleStringJPN} (#{question.titleStringJPN_KANJI})"
     end
     # o—Í
