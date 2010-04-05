ddabbaecf5089fcc0e836a11951721124bf0a8e2
Akira Nakamura <nakamura_akira@gamefreak.co.jp>
2010-04-05 08:22:23 +0000

    イベント作成：一言メッセージを追加

--- resource/fldmapdata/script/c04r0403_def.h
+++ resource/fldmapdata/script/c04r0403_def.h
@@ -6,5 +6,9 @@
 
 //スクリプトデータID定義
 #define SCRID_ZONE_C04R0403 (1)
+#define SCRID_C04R0403_TENNISW_01 (2)
+#define SCRID_C04R0403_TENNISW_02 (3)
+#define SCRID_C04R0403_BOY2_01 (4)
+#define SCRID_C04R0403_BOY2_02 (5)
 
 #endif //_C04R0403_DEF_H_
