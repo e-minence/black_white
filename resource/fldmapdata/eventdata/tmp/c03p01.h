4544f6afe65bddf984410cff7659080e32a814a8
Katsunori Suginaka <suginaka_katsunori@gamefreak.co.jp>
2010-04-06 11:04:09 +0000

    C03:中央部接続修正

--- resource/fldmapdata/eventdata/tmp/c03p01.h
+++ resource/fldmapdata/eventdata/tmp/c03p01.h
@@ -5,8 +5,8 @@
 #define DOOR_ID_C03P01_C03P08_EXIT01      0
 #define DOOR_ID_C03P01_C03P07_EXIT01      1
 #define DOOR_ID_C03P01_C03P12_EXIT01      2
-#define DOOR_ID_C03P01_C03P10_EXIT01      3
-#define DOOR_ID_C03P01_C03P09_EXIT01      4
+#define DOOR_ID_C03P01_C03P09_EXIT01      3
+#define DOOR_ID_C03P01_C03P10_EXIT01      4
 
 #endif //ZONE_C03P01_EVD_H
 //Ç±ÇÃÉtÉ@ÉCÉãÇÕ../data/c03p01.mevÇ©ÇÁé©ìÆê∂ê¨Ç≥ÇÍÇ‹ÇµÇΩ
