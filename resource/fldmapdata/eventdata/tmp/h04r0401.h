7c2a5613be5a2954f569ff862a7e6b628dd55a5e
Akira Nakamura <nakamura_akira@gamefreak.co.jp>
2010-04-06 09:03:08 +0000

    イベント作成：一言メッセージを修正

--- resource/fldmapdata/eventdata/tmp/h04r0401.h
+++ resource/fldmapdata/eventdata/tmp/h04r0401.h
@@ -9,6 +9,8 @@
 #ifndef ZONE_H04R0401_EVC_H
 #define ZONE_H04R0401_EVC_H
 
+#define H04R0401_BREEDERM1_01              0
+#define H04R0401_POKE19_01                 1
 
 #endif //ZONE_H04R0401_EVC_H
 //‚±‚Ìƒtƒ@ƒCƒ‹‚Í../data/h04r0401.mev‚©‚ç©“®¶¬‚³‚ê‚Ü‚µ‚½
--- resource/fldmapdata/script/h04r0401_def.h
+++ resource/fldmapdata/script/h04r0401_def.h
@@ -5,6 +5,7 @@
 #define _H04R0401_DEF_H_
 
 //スクリプトデータID定義
-#define SCRID_H04R0401 (1)
+#define SCRID_H04R0401_BREEDERM1_01 (1)
+#define SCRID_H04R0401_POKE19_01 (2)
 
 #endif //_H04R0401_DEF_H_
