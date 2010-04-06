6817ca4a58d50f481a693860d4fdbf52c6df3977
Naoto Murakami <murakami_naoto@gamefreak.co.jp>
2010-04-06 10:11:57 +0000

    スクリプト/H03 エラー回避の為、仮コミット

--- resource/fldmapdata/script/h03_def.h
+++ resource/fldmapdata/script/h03_def.h
@@ -6,8 +6,9 @@
 
 //スクリプトデータID定義
 #define SCRID_H03_FIGHTERW_01 (1)
-#define SCRID_INIT_H03 (2)
-#define SCRID_RECOVER_H03 (3)
-#define SCRID_ZONE_H03 (4)
+#define SCRID_H03_MIDDLEWOMAN1_01 (2)
+#define SCRID_INIT_H03 (3)
+#define SCRID_RECOVER_H03 (4)
+#define SCRID_ZONE_H03 (5)
 
 #endif //_H03_DEF_H_
