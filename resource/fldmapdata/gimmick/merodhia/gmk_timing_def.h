98ca49206748edbbf2cfc7d9cc2edcd5dbe16b6c
Nozomu Saito <saito_nozomu@gamefreak.co.jp>
2010-04-07 03:21:08 +0000

    //メロディア演出調整

--- resource/fldmapdata/gimmick/merodhia/gmk_timing_def.h
+++ resource/fldmapdata/gimmick/merodhia/gmk_timing_def.h
@@ -14,7 +14,7 @@
 #define MERODHIA_ROUND_OUT_NUM     (3)     //最大旋回幅になるまでの旋回回数
 
 #define MERODHIA_ROUND_SYNC     (120)     //最大旋回するシンク数
-#define MERODHIA_ROUND_NUM     (4)     //最大旋回幅で旋回する回数
+#define MERODHIA_ROUND_NUM     (3)     //最大旋回幅で旋回する回数
 
 #define MERODHIA_ROUND_IN_SYNC     (90)     //収縮旋回するシンク数
 #define MERODHIA_ROUND_IN_NUM     (3)     //収縮旋回する回数
