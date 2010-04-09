168e5ea91eabceee99a637103e0e94a51c356be6
Nozomu Saito <saito_nozomu@gamefreak.co.jp>
2010-04-09 06:45:50 +0000

    //エリア別動作モデルリスト更新

--- resource/fldmapdata/mm_list/mmlist_def.h
+++ resource/fldmapdata/mm_list/mmlist_def.h
@@ -3,6 +3,21 @@
 #ifndef	__MMLIST_DEF_H__
 #define __MMLIST_DEF_H__
 
-#define	MMLID_AREA01         0
-#define	MMLID_NOENTRY        1
+#define	MMLID_AREA_OUT01     0
+#define	MMLID_AREA_OUT02     1
+#define	MMLID_AREA_OUT03     2
+#define	MMLID_AREA_OUT05     3
+#define	MMLID_AREA_OUT06     4
+#define	MMLID_AREA_OUT07     5
+#define	MMLID_AREA_OUT09     6
+#define	MMLID_AREA_OUT10     7
+#define	MMLID_AREA_OUT11     8
+#define	MMLID_AREA_OUT13     9
+#define	MMLID_AREA_OUT14     10
+#define	MMLID_AREA_OUT17     11
+#define	MMLID_AREA_OUT18     12
+#define	MMLID_AREA_OUT20     13
+#define	MMLID_AREA_OUT22     14
+#define	MMLID_AREA_OUT30     15
+#define	MMLID_NOENTRY        16
 #endif
