1176514240c02bc132a4f10c8d0cca72392aedd4
Shinya Takahashi <takahashi@gamefreak.co.jp>
2010-04-13 09:02:54 +0000

    接続修正

--- resource/fldmapdata/eventdata/tmp/d09.h
+++ resource/fldmapdata/eventdata/tmp/d09.h
@@ -2,40 +2,40 @@
 #ifndef ZONE_D09_EVD_H
 #define ZONE_D09_EVD_H
 
-#define DOOR_ID_D09_D09R0201_EXIT01       0
-#define DOOR_ID_D09_D09R1101_EXIT01       1
-#define DOOR_ID_D09_D09R0401_EXIT01       2
-#define DOOR_ID_D09_D09R0301_EXIT01       3
-#define DOOR_ID_D09_D09R1001_EXIT01       4
-#define DOOR_ID_D09_D09R0601_EXIT01       5
-#define DOOR_ID_D09_D09R1201_EXIT01       6
-#define DOOR_ID_D09_D09R1201_EXIT04       7
-#define DOOR_ID_D09_D09R0101_EXIT01       8
-#define DOOR_ID_D09_D09R0701_EXIT01       9
-#define DOOR_ID_D09_D09R0901_EXIT01      10
-#define DOOR_ID_D09_D09R0501_EXIT01      11
-#define DOOR_ID_D09_D09R1401_EXIT01      12
-#define DOOR_ID_D09_C09_EXIT01           13
-#define DOOR_ID_D09_D09R1201_EXIT03      14
-#define DOOR_ID_D09_D09R1201_EXIT02      15
-#define DOOR_ID_D09_D09R0801_EXIT01      16
-#define DOOR_ID_D09_D09R1301_EXIT01      17
-#define DOOR_ID_D09_R10R0901_EXIT01      18
+#define DOOR_ID_D09_D09R0501_EXIT01       0
+#define DOOR_ID_D09_D09R1201_EXIT01       1
+#define DOOR_ID_D09_D09R1001_EXIT01       2
+#define DOOR_ID_D09_D09R0901_EXIT01       3
+#define DOOR_ID_D09_D09R1101_EXIT01       4
+#define DOOR_ID_D09_D09R1201_EXIT02       5
+#define DOOR_ID_D09_D09R0601_EXIT01       6
+#define DOOR_ID_D09_D09R0301_EXIT01       7
+#define DOOR_ID_D09_D09R0401_EXIT01       8
+#define DOOR_ID_D09_D09R1301_EXIT01       9
+#define DOOR_ID_D09_D09R1401_EXIT01      10
+#define DOOR_ID_D09_C09_EXIT01           11
+#define DOOR_ID_D09_D09R1201_EXIT04      12
+#define DOOR_ID_D09_D09R0801_EXIT01      13
+#define DOOR_ID_D09_D09R0701_EXIT01      14
+#define DOOR_ID_D09_R10R0901_EXIT01      15
+#define DOOR_ID_D09_D09R0101_EXIT01      16
+#define DOOR_ID_D09_D09R0201_EXIT01      17
+#define DOOR_ID_D09_D09R1201_EXIT03      18
 
 #endif //ZONE_D09_EVD_H
 //このファイルは../data/d09.mevから自動生成されました
 #ifndef ZONE_D09_EVC_H
 #define ZONE_D09_EVC_H
 
-#define D09_FLD_ITEM_01                    0
+#define D09_VETERANM_02                    0
 #define D09_FLD_ITEM_02                    1
 #define D09_FLD_ITEM_03                    2
 #define D09_FLD_ITEM_04                    3
 #define D09_FLD_ITEM_05                    4
 #define D09_VETERANM_01                    5
 #define D09_TRAINERM_01                    6
-#define D09_VETERANM_02                    7
-#define D09_RANGERM_01                     8
+#define D09_RANGERM_01                     7
+#define D09_FLD_ITEM_01                    8
 
 #endif //ZONE_D09_EVC_H
 //このファイルは../data/d09.mevから自動生成されました
