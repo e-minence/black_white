24fe5b3dcc720ae23139a3f93d1e35251e3b646a
Masafumi Saito <saito_masafumi@gamefreak.co.jp>
2010-04-14 12:11:03 +0000

    フラグ追加：ゾロアークと出会った

--- resource/fldmapdata/flagwork/flag_define.h
+++ resource/fldmapdata/flagwork/flag_define.h
@@ -357,7 +357,7 @@
 #define FE_R18R0101_APPRAISAL_TALK         341 /* 0x0155 18番道路：鉱物鑑定士2初回話しかけフラグ */
 #define FE_JUDGE_TALK                      342 /* 0x0156 ジャッジ初回会話フラグ */
 #define FE_TEACH_COALESCENCE_TALK          343 /* 0x0157 合体技教え初回会話フラグ */
-		/* not used   344(0x0158) */
+#define FE_R16R0201_POKE23_ENC             344 /* 0x0158 ゾロアークと出会ったフラグ（C04ゲートのメッセージ分岐に使用） */
 		/* not used   345(0x0159) */
 		/* not used   346(0x015a) */
 		/* not used   347(0x015b) */
