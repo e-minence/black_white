3a12afdf546c7e7cb341fd7ec8618dfd56faeefa
Miyuki Iwasawa <iwasawa@gamefreak.co.jp>
2010-04-15 04:27:09 +0000

    エフェクトエンカウント　発生率調整

--- prog/src/field/effect_encount_def.h
+++ prog/src/field/effect_encount_def.h
@@ -7,12 +7,12 @@
 
 #pragma once
 
-#define EFFENC_NORMAL_INTERVAL (50)  //エフェクト抽選デフォルトインターバル
-#define EFFENC_NORMAL_PROB (20)  //エフェクト抽選確率デフォルト
-#define EFFENC_CAVE_INTERVAL (50)  //エフェクト抽選洞窟インターバル
-#define EFFENC_CAVE_PROB (20)  //エフェクト抽選確率洞窟
-#define EFFENC_BRIDGE_INTERVAL (10)  //エフェクト抽選橋インターバル
-#define EFFENC_BRIDGE_PROB (80)  //エフェクト抽選確率橋
+#define EFFENC_NORMAL_INTERVAL (20)  //エフェクト抽選デフォルトインターバル
+#define EFFENC_NORMAL_PROB (10)  //エフェクト抽選確率デフォルト
+#define EFFENC_CAVE_INTERVAL (20)  //エフェクト抽選洞窟インターバル
+#define EFFENC_CAVE_PROB (10)  //エフェクト抽選確率洞窟
+#define EFFENC_BRIDGE_INTERVAL (5)  //エフェクト抽選橋インターバル
+#define EFFENC_BRIDGE_PROB (15)  //エフェクト抽選確率橋
 
 //======================================================================
 //  define
