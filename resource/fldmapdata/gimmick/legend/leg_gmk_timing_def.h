38f944cbc75b0199aa917fa8672f686dd9834c26
Nozomu Saito <saito_nozomu@gamefreak.co.jp>
2010-04-20 10:30:12 +0000

    //定義追加

--- resource/fldmapdata/gimmick/legend/leg_gmk_timing_def.h
+++ resource/fldmapdata/gimmick/legend/leg_gmk_timing_def.h
@@ -11,4 +11,7 @@
 #define FLASH_FRAME  ( 80 )           //輝度変更開始フレーム
 #define LOOP_START_FRAME ( 450 )      //ループ先頭フレーム
 #define FADE_WAIT    (0)              //輝度ウェイト
+
+#define LEG_SE1_FRM  (0)              //ＳＥ１つ目
+#define LEG_SE2_FRM  (450)            //ＳＥ２つ目
   
