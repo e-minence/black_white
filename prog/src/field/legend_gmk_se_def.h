224b3f9bdcba7aed1918b3b592f7c958a620c41b
Nozomu Saito <saito_nozomu@gamefreak.co.jp>
2010-04-20 11:26:27 +0000

    //ＳＥ定義追加

---
+++ prog/src/field/legend_gmk_se_def.h
@@ -0,0 +1,12 @@
+//======================================================================
+/**
+ * @file  legend_gmk_se_def.h
+ * @author  GAMEFREAK inc.
+ *
+ * SE定義
+ */
+//======================================================================
+#pragma once
+
+#define LEG_GMK_ENERGY  (SEQ_SE_FLD_162)  //エネルギーが集まる（loop）
+#define LEG_GMK_BALL  (SEQ_SE_FLD_163)  //玉から光、もしくは浮き上がる
