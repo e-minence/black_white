e5b3114682a1fe8e191c488ff3083a9527e0eda7
Masao Taya <taya@gamefreak.co.jp>
2010-04-28 10:14:40 +0000

    デバッグフラグ追加

--- prog/include/battle/battle.h
+++ prog/include/battle/battle.h
@@ -221,6 +221,7 @@ typedef enum {
   BTL_DEBUGFLAG_HIT100PER,      ///< 命中率が常に100％
   BTL_DEBUGFLAG_DMG_RAND_OFF,   ///< ダメージのランダム補正をオフ
   BTL_DEBUGFLAG_SKIP_BTLIN,     ///< 入場エフェクトスキップ（非通信時のみ）
+  BTL_DEBUGFLAG_AI_CTRL,        ///< AIを起動せずにプレイヤーが操作する
 
   BTL_DEBUGFLAG_MAX,
 
