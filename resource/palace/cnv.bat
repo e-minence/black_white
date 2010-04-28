75b172386ddff37c3f6f0c3939874937f2245b88
Junichi Masuda <matsuda@gamefreak.co.jp>
2010-04-28 05:41:08 +0000

    不要ファイル削除

--- resource/palace/cnv.bat
+++ resource/palace/cnv.bat
@@ -9,9 +9,6 @@ del *.nsbca
 del *.ntfp
 del *.ntft
 
-echo g2dコンバート
-g2dcvtr debug_areano.nce -br -ncn
-
 g2dcvtr palace_obj.nce -br -ncn
 ntrcomp -lex -A4 -o palace_obj_lz.NCGR palace_obj.NCGR
 
@@ -25,12 +22,6 @@ ntrcomp -lex -A4 -o palace_bar_lz.NSCR palace_bar.NSCR
 ntrcomp -lex -A4 -o palace_bar2_lz.NSCR palace_bar2.NSCR
 ntrcomp -lex -A4 -o palace_time_lz.NSCR palace_time.NSCR
 
-
-echo g3dコンバート
-g3dcvtr "cube.imd"
-
-echo ntf
-ntexconv block_tex16.bmp -b -f palette16
 
 echo narc作成
 nnsarc -c -l -n -i palace -S palace_arc.txt
