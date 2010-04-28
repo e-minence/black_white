8ee877ee215f99fb7bb950e5f7ba4fc6b483679a
Miyuki Iwasawa <iwasawa@gamefreak.co.jp>
2010-04-28 02:58:14 +0000

    demo3d リソースコンバート環境 　2Dリソースコンバート環境を修正 　新規BG一枚絵表示コマンドに対応

--- resource/demo3d/conv/c_demo3d_cmd.rb
+++ resource/demo3d/conv/c_demo3d_cmd.rb
@@ -11,12 +11,16 @@ $h_se_default = {"---"=>"DEMO3D_SE_PARAM_DEFAULT"}
 $h_brightness_disp = {"main"=>"MASK_MAIN_DISPLAY","sub"=>"MASK_SUB_DISPLAY","double"=>"MASK_DOUBLE_DISPLAY"}
 $h_bgm_chg_type = {"play"=>"DEMO3D_BGM_PLAY","stop"=>"DEMO3D_BGM_STOP","change"=>"DEMO3D_BGM_CHANGE","push"=>"DEMO3D_BGM_PUSH","pop"=>"DEMO3D_BGM_POP"}
 $h_bgm_fade_type = {"fadein"=>"0","fadeout"=>"1"}
+$h_pm_version = {"ver_all"=>"0xFF","ver_black"=>"VERSION_BLACK","ver_white"=>"VERSION_WHITE","ver_gray"=>"VERSION_WHITE+1"}
 
 #==============================================
 #プレフィックス・サフィックス定義
 #==============================================
 $psfix_fx16 = $_psfix_prm.new( "FX16_CONST( ", " )")
 $psfix_msg_arc = $_psfix_prm.new( "NARC_message_", "_dat")
+$psfix_2dgra_ncl = $_psfix_prm.new( "NARC_demo3d_2dgra_", "_NCLR")
+$psfix_2dgra_ncg = $_psfix_prm.new( "NARC_demo3d_2dgra_", "_lz_NCGR")
+$psfix_2dgra_nsc = $_psfix_prm.new( "NARC_demo3d_2dgra_", "_lz_NSCR")
 
 #==============================================
 #コマンドパラメータ定義
@@ -99,6 +103,13 @@ $prm_talkwin_req = [
   $_cmd_prm.new( 2, 28, nil, nil ), #ウィンドウサイズX
   $_cmd_prm.new( 2, 22, nil, nil ), #ウィンドウサイズY
 ] 
+$prm_scr_draw_req = [
+  $_cmd_prm.new( P_LIST, P_LIST, $h_pm_version, nil ), #有効バージョン指定
+  $_cmd_prm.new( P_FREE, P_FREE, nil, $psfix_2dgra_ncl ), #nclr
+  $_cmd_prm.new( P_FREE, P_FREE, nil, $psfix_2dgra_ncg ), #ncg
+  $_cmd_prm.new( P_FREE, P_FREE, nil, $psfix_2dgra_nsc ), #nscr
+  $_cmd_prm.new( 1, P_FREE, nil, nil ), #表示するフレーム数
+] 
 $prm_light_vector_set = [
   $_cmd_prm.new( 0, 3, nil, nil ), #ライトNo
   $_cmd_prm.new( -7.9, 7.9, nil, $psfix_fx16 ), #vector X
@@ -125,6 +136,7 @@ $cmd_tbl = [
   CDemo3DCmd::new( "LIGHT_COLOR_SET", $prm_light_color_set),
   CDemo3DCmd::new( "LIGHT_VECTOR_SET", $prm_light_vector_set),
   CDemo3DCmd::new( "TALKWIN_REQ", $prm_talkwin_req),
+  CDemo3DCmd::new( "SCR_DRAW_REQ", $prm_scr_draw_req),
   CDemo3DCmd::new( "MOTIONBL_START", $prm_motionbl_start),
   CDemo3DCmd::new( "MOTIONBL_END", nil ),
   CDemo3DCmd::new( "END", nil),
