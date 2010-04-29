7b8952f0f5aa040f26ca8049cf1d04ed31848382
Sosuke Tamada <tamada@gamefreak.co.jp>
2010-04-29 12:29:15 +0000

    未使用コマンドを削除した

--- prog/src/field/scrcmd_trainer.h
+++ prog/src/field/scrcmd_trainer.h
@@ -26,12 +26,9 @@ extern VMCMD_RESULT EvCmdRevengeTrainerTalkTypeGet(VMHANDLE *core,void *wk);
 extern VMCMD_RESULT EvCmdTrainerTypeGet(VMHANDLE *core,void *wk);
 extern VMCMD_RESULT EvCmdTrainerBgmSet(VMHANDLE *core,void *wk);
 extern VMCMD_RESULT EvCmdTrainerWin(VMHANDLE *core,void *wk);
-extern VMCMD_RESULT EvCmd2vs2BattleCheck(VMHANDLE *core,void *wk);
 extern VMCMD_RESULT EvCmdTrainerSpecialTypeGet( VMHANDLE * core, void *wk );
 extern VMCMD_RESULT EvCmdTrainerItemGet( VMHANDLE *core, void *wk );
 
-extern VMCMD_RESULT EvCmdDebugBattleSet(VMHANDLE *core,void *wk);
-extern VMCMD_RESULT EvCmdBattleResultGet(VMHANDLE *core,void *wk);
 
 extern VMCMD_RESULT EvCmdGetWheelTrainerObjID( VMHANDLE *core, void *wk );
 extern VMCMD_RESULT EvCmdGetWheelTrainerTrID( VMHANDLE *core, void *wk );
