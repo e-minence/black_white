d19eb37b203e93aefbac8a648c5c52a0999f70ba
Shota Kageyama <kageyama_shota@gamefreak.co.jp>
2010-04-29 12:05:03 +0000

    ●バトル
    ポケモンの鳴き声のときのボリュームを落とす値を変更
    
    ●SE更新
    
    ・SEQ_SE_FLD_171  橋通過音　追加
    ・SEQ_SE_VDEMO_02　長さを伸ばす修正
    ・パキャーン！音の音量調整済み
    
    汽笛の音、ムービー上のツールで音量上げ
    
    ビクティボートPORT、上記の調整を反映したため、
    若干水を切る音の聞こえ方がかわっています。

--- prog/src/battle/btlv/btlv_effvm_dat.h
+++ prog/src/battle/btlv/btlv_effvm_dat.h
@@ -10,11 +10,11 @@
 #pragma once
 
 //技エフェクト発動
-#define EFFVM_VOLUME_DOWN_RATIO ( 80 )  //マスターボリュームを何％ダウンするか（80で80%ダウン）
+#define EFFVM_VOLUME_DOWN_RATIO ( 85 )  //マスターボリュームを何％ダウンするか（80で80%ダウン）
 #define EFFVM_VOLUME_DOWN_FRAME ( 20 )  //EFFVM_VOLUME_DOWN_RATIOの％に何フレームでなるかを指定（60で1秒）
 #define EFFVM_VOLUME_UP_FRAME ( 20 )    //100％に何フレームでなるかを指定（60で1秒）
 
 //ポケモンの鳴き声
-#define EFFVM_VOLUME_DOWN_RATIO_PV ( 50 )  //マスターボリュームを何％ダウンするか（80で80%ダウン）
+#define EFFVM_VOLUME_DOWN_RATIO_PV ( 75 )  //マスターボリュームを何％ダウンするか（80で80%ダウン）
 #define EFFVM_VOLUME_DOWN_FRAME_PV ( 10 )  //EFFVM_VOLUME_DOWN_RATIOの％に何フレームでなるかを指定（60で1秒）
 #define EFFVM_VOLUME_UP_FRAME_PV ( 10 )    //100％に何フレームでなるかを指定（60で1秒）
