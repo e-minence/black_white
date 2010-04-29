c6404129d303392b82977dc51cee390c36c329fb
Miyuki Iwasawa <iwasawa@gamefreak.co.jp>
2010-04-29 07:28:04 +0000

    エンカウントエフェクト　ハイレベルエンカウント、エフェクトエンカウント時のエフェクトとBGM指定を変更

--- prog/src/field/enceffno.h
+++ prog/src/field/enceffno.h
@@ -9,6 +9,7 @@
 #include "field/fieldmap_proc.h"
 #include "encount_data.h"
 
-extern void ENCEFFNO_GetWildEncEffNoBgmNo( const int inMonsNo, ENCOUNT_TYPE inEncType, FIELDMAP_WORK *fieldmap, int *outEffNo, u16 *outBgmNo );
+extern void ENCEFFNO_GetWildEncEffNoBgmNo( const int inMonsNo, ENCOUNT_TYPE inEncType, BOOL inDoubleBattle, 
+    FIELDMAP_WORK *fieldmap,int *outEffNo, u16 *outBgmNo );
 extern void ENCEFFNO_GetTrEncEffNoBgmNo( const int inTrID, FIELDMAP_WORK *fieldmap, int *outEffNo, u16 *outBgmNo );
 
