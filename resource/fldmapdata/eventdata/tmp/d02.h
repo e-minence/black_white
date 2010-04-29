4517cbbfcf600bbb2f751c89652279cba95b1133
Shinya Takahashi <takahashi@gamefreak.co.jp>
2010-04-29 10:59:30 +0000

    配置修正

--- resource/fldmapdata/eventdata/tmp/d02.h
+++ resource/fldmapdata/eventdata/tmp/d02.h
@@ -13,17 +13,17 @@
 #define D02_LEADER3_01                     1
 #define TR_TANPAN_09                       2
 #define TR_TANPAN_10                       3
-#define TR_KARATE_03                       4
-#define D02_FLD_ITEM_01                    5
-#define D02_FLD_ITEM_02                    6
-#define D02_FLD_ITEM_03                    7
-#define D02_FLD_ITEM_05                    8
-#define TR_KINDERGARTENM_05                9
-#define TR_KINDERGARTENW_04               10
-#define TR_BATTLEG_03                     11
-#define D02_FIGHTERW_01                   12
-#define TR_NURSE_03                       13
-#define D02_FLD_ITEM_04                   14
+#define D02_FLD_ITEM_01                    4
+#define D02_FLD_ITEM_02                    5
+#define D02_FLD_ITEM_03                    6
+#define D02_FLD_ITEM_05                    7
+#define TR_KINDERGARTENM_05                8
+#define TR_KINDERGARTENW_04                9
+#define TR_BATTLEG_03                     10
+#define D02_FIGHTERW_01                   11
+#define TR_NURSE_03                       12
+#define D02_FLD_ITEM_04                   13
+#define TR_KARATE_03                      14
 
 #endif //ZONE_D02_EVC_H
 //Ç±ÇÃÉtÉ@ÉCÉãÇÕ../data/d02.mevÇ©ÇÁé©ìÆê∂ê¨Ç≥ÇÍÇ‹ÇµÇΩ
