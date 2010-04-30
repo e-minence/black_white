8a4dad7db01acac1e6d84d09e5deb182fe95fc9a
Mai Mizuguchi <mizuguchi_mai@gamefreak.co.jp>
2010-04-30 07:16:00 +0000

    d07r0501：メッセージ配置

--- resource/fldmapdata/eventdata/tmp/d07r0501.h
+++ resource/fldmapdata/eventdata/tmp/d07r0501.h
@@ -16,5 +16,13 @@
 #ifndef ZONE_D07R0501_EVB_H
 #define ZONE_D07R0501_EVB_H
 
+#define BG_D07R0501_STONE_01              0
+#define BG_D07R0501_STONE_02              1
+#define BG_D07R0501_STONE_03              2
+#define BG_D07R0501_STONE_04              3
+#define BG_D07R0501_STONE_05              4
+#define BG_D07R0501_STONE_06              5
+#define BG_D07R0501_STONE_07              6
+#define BG_D07R0501_STONE_08              7
 
 #endif //ZONE_D07R0501_EVB_H
--- resource/fldmapdata/script/d07r0501_def.h
+++ resource/fldmapdata/script/d07r0501_def.h
@@ -5,6 +5,6 @@
 #define _D07R0501_DEF_H_
 
 //スクリプトデータID定義
-#define SCRID_D07R0501 (1)
+#define SCRID_BG_D07R0501_STONE_01 (1)
 
 #endif //_D07R0501_DEF_H_
