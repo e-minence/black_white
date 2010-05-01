a7a79365a89dc260de14871054ddd7a08345d48f
Toru Nagihashi <nagihashi@gamefreak.co.jp>
2010-05-01 08:15:15 +0000

    メニューへ戻ってきたときに接続・切断処理を改善

--- prog/include/net_app/compatible_irc_sys.h
+++ prog/include/net_app/compatible_irc_sys.h
@@ -111,7 +111,8 @@ extern void COMPATIBLE_IRC_DeleteSystem( COMPATIBLE_IRC_SYS *p_sys );
 extern BOOL COMPATIBLE_IRC_InitWait( COMPATIBLE_IRC_SYS *p_sys );
 extern BOOL COMPATIBLE_IRC_ExitWait( COMPATIBLE_IRC_SYS *p_sys );
 extern BOOL COMPATIBLE_IRC_ConnextWait( COMPATIBLE_IRC_SYS *p_sys );
-extern BOOL COMPATIBLE_IRC_DisConnextWait( COMPATIBLE_IRC_SYS *p_sys );
+//extern BOOL COMPATIBLE_IRC_DisConnextWait( COMPATIBLE_IRC_SYS *p_sys );
+extern BOOL COMPATIBLE_IRC_IsCancelConnext( const COMPATIBLE_IRC_SYS *cp_sys );
 
 extern BOOL COMPATIBLE_IRC_IsConnext( const COMPATIBLE_IRC_SYS *cp_sys );
 extern BOOL COMPATIBLE_IRC_IsInit( const COMPATIBLE_IRC_SYS *cp_sys );
