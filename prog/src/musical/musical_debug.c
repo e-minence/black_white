2a24895d5f0e7eb87ba3440291f19431709c2c72
Junichi Masuda <matsuda@gamefreak.co.jp>
2010-05-03 10:46:28 +0000

    デバッグデータ変更

--- prog/src/musical/musical_debug.c
+++ prog/src/musical/musical_debug.c
@@ -75,9 +75,9 @@ const BOOL MUSICAL_DEBUG_CreateDummyData( MUSICAL_SHOT_DATA* shotData , const u1
     //‘•”õ‰ÓŠ‚Ì‰Šú‰»
     for( j=0;j<MUSICAL_ITEM_EQUIP_MAX;j++ )
     {
-      shotData->shotPoke[i].equip[j].itemNo = 65535;
+      shotData->shotPoke[i].equip[j].itemNo = 0;  //65535;
       shotData->shotPoke[i].equip[j].angle = 0;
-      shotData->shotPoke[i].equip[j].equipPos = MUS_POKE_EQU_INVALID;
+      shotData->shotPoke[i].equip[j].equipPos = MUS_POKE_EQU_EAR_R; //MUS_POKE_EQU_INVALID;
     }
     shotData->shotPoke[i].equip[0].itemNo = 19;
     shotData->shotPoke[i].equip[0].equipPos = MUS_POKE_EQU_HAND_L;
