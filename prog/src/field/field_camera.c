285bd639d941ebc90b7037d1a160fc3bd5d781e9
Tomoya Takahashi <tomoya_takahashi@gamefreak.co.jp>
2010-05-07 06:24:45 +0000

    TRACE 処理を元に戻す。
    イベントでのカメラ操作復帰時におかしくなるため。

--- prog/src/field/field_camera.c
+++ prog/src/field/field_camera.c
@@ -25,8 +25,6 @@
 //============================================================================================
 //============================================================================================
 
-// x = CAMERA_TRACE*
-#define TRACEDATA_GET_FIRST_POINT(x) ((x)->Delay)
 
 
 static void createTraceData(const int inHistNum, const int inDelay,
@@ -1576,11 +1574,6 @@ void FIELD_CAMERA_SetFovy(FIELD_CAMERA * camera, u16 fovy )
 //---------------------------------------------------------------------------
 /**
  * @brief
- *
- *10.05.06 tomoya 
- * 動きをSMOOTHにするため、
- * バッファにカメラ、ターゲット座標がたまりきるまでは、
- * 最初に格納された座標を返し続けるように変更
  */
 //---------------------------------------------------------------------------
 static void updateTraceData(CAMERA_TRACE * trace,
@@ -1605,16 +1598,6 @@ static void updateTraceData(CAMERA_TRACE * trace,
   cam_ofs = trace->CamPoint;    // 参照位置
   tgt_ofs = trace->TargetPoint; // 格納位置
 
-  // 格納処理
-  if( !trace->StopReq ){
-    //履歴に積む
-    trace->targetBuffer[tgt_ofs] = *inTarget;
-    trace->camPosBuffer[tgt_ofs] = *inCamPos;
-    //書き換え位置更新
-    tgt_ofs = (tgt_ofs+1) % trace->bufsize;
-  }
-
-  // 設定情報を取得
   if (trace->UpdateFlg){
     //履歴データから座標取得
     *outTarget = trace->targetBuffer[cam_ofs];
@@ -1622,14 +1605,12 @@ static void updateTraceData(CAMERA_TRACE * trace,
   }
   else
   {
-    // たまっている間は、TRACEDATA_GET_FIRST_POINTの位置のカメラを渡す。
-    *outTarget = trace->targetBuffer[ TRACEDATA_GET_FIRST_POINT(trace) ];
-    *outCamPos = trace->camPosBuffer[ TRACEDATA_GET_FIRST_POINT(trace) ];
-    if ( cam_ofs==TRACEDATA_GET_FIRST_POINT(trace) ){
+		*outTarget = *inTarget;
+		*outCamPos = *inCamPos;
+    if (cam_ofs==trace->Delay){
       trace->UpdateFlg = TRUE;
     }
   }
-
 
   //ストップリクエストが出ている場合
   if (trace->StopReq)
@@ -1652,6 +1633,11 @@ static void updateTraceData(CAMERA_TRACE * trace,
   {
     //参照位置更新
     cam_ofs = (cam_ofs+1) % trace->bufsize;
+    //履歴に積む
+    trace->targetBuffer[tgt_ofs] = *inTarget;
+    trace->camPosBuffer[tgt_ofs] = *inCamPos;
+    //書き換え位置更新
+    tgt_ofs = (tgt_ofs+1) % trace->bufsize;
   }
 
   trace->CamPoint = cam_ofs;    // 参照位置
@@ -1710,14 +1696,13 @@ void createTraceData(const int inHistNum, const int inDelay,
 	}
 	
 	trace->bufsize = inHistNum;
-	
-	trace->Delay = inDelay;
-	trace->UpdateFlg = FALSE;
-
 	//０番目にカメラ参照位置をセット
 	trace->CamPoint = 0;
 	//対象物参照位置セット
-	trace->TargetPoint = TRACEDATA_GET_FIRST_POINT(trace);
+	trace->TargetPoint = 0+inDelay;
+	
+	trace->Delay = inDelay;
+	trace->UpdateFlg = FALSE;
 	
 	trace->ValidX = FALSE;
 	trace->ValidY = FALSE;
@@ -1806,7 +1791,7 @@ void FIELD_CAMERA_RestartTrace(FIELD_CAMERA * camera_ptr)
     //０番目にカメラ参照位置をセット
     trace->CamPoint = 0;
     //対象物参照位置セット
-    trace->TargetPoint = TRACEDATA_GET_FIRST_POINT(trace);
+    trace->TargetPoint = 0+trace->Delay;
   }
 
   trace->UpdateFlg = FALSE;
