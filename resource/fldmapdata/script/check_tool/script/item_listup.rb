6b006d03ff0ae5226ef097b09fe2852b41857f77
Toshihiro Obata <obata_toshihiro@gamefreak.co.jp>
2010-05-11 02:23:32 +0000

    コンバータを整理

--- resource/fldmapdata/script/check_tool/script/item_listup.rb
+++ resource/fldmapdata/script/check_tool/script/item_listup.rb
@@ -7,28 +7,27 @@
 require "item_info.rb"
 require "place_name_hash.rb"
 
-# チェック対象のファイル名
-CHECK_TARGET_FILE_NAME = "*.ev"
+PATH_TO_CHECK_DIR      = "../../" # チェック対象のディレクトリ
+FILE_NAME_CHECK_TARGET = "*.ev"   # チェック対象のファイル名
+PATH_TO_CHECK_TARGET_FILE = PATH_TO_CHECK_DIR + FILE_NAME_CHECK_TARGET # チェック対象ファイルへのパス
 
-# チェック対象のディレクトリ
-PATH_TO_CHECK_DIR = "../../"
-
-# チェック対象ファイルへのパス
-PATH_TO_CHECK_TARGET_FILE = PATH_TO_CHECK_DIR + CHECK_TARGET_FILE_NAME
+PATH_TO_RESULT_OUTPUT = "../result/" # 結果の出力先ディレクトリへの相対パス
+FILE_NAME_RESULT      = "result.txt" # 解析結果の出力ファイル名
+FILE_NAME_FAILED      = "failed.txt" # 解析失敗リストの出力ファイル名
 
 
 # チェック対象外のファイル一覧
-item_get_skip_file_list = Array.new
-item_get_skip_file_list << PATH_TO_CHECK_DIR + "item_get_scr.ev"
-item_get_skip_file_list << PATH_TO_CHECK_DIR + "debug_scr.ev"
-item_get_skip_file_list << PATH_TO_CHECK_DIR + "hide_item.ev"
-item_get_skip_file_list << PATH_TO_CHECK_DIR + "hyouka_scr.ev"
-item_get_skip_file_list << PATH_TO_CHECK_DIR + "poke_waza.ev"
-item_get_skip_file_list << PATH_TO_CHECK_DIR + "fld_item.ev"
-item_get_skip_file_list << PATH_TO_CHECK_DIR + "c03r0101.ev" # 調査隊スクリプト
+skip_file_list = Array.new
+skip_file_list << PATH_TO_CHECK_DIR + "item_get_scr.ev"
+skip_file_list << PATH_TO_CHECK_DIR + "debug_scr.ev"
+skip_file_list << PATH_TO_CHECK_DIR + "hide_item.ev"
+skip_file_list << PATH_TO_CHECK_DIR + "hyouka_scr.ev"
+skip_file_list << PATH_TO_CHECK_DIR + "poke_waza.ev"
+skip_file_list << PATH_TO_CHECK_DIR + "fld_item.ev"
+skip_file_list << PATH_TO_CHECK_DIR + "c03r0101.ev" # 調査隊スクリプト
 
 # チェックに失敗したファイル一覧
-item_get_failed_file_list = Array.new
+failed_file_list = Array.new
 
 # チェックするコマンド一覧
 item_get_command_list = Array.new
@@ -43,17 +42,16 @@ item_get_command_list << "_ITEM_EVENT_SUCCESS_NOWAIT"
 
 # チェック対象コマンド ==> アイテム定義ラベルを取得するための正規表現
 item_label_pattern = Hash.new
-item_label_pattern[ "_ADD_ITEM" ] = /_ADD_ITEM.*\(\s*(ITEM_[^,]*),/
-item_label_pattern[ "_EASY_TALK_ITEM_EVENT" ] = /_EASY_TALK_ITEM_EVENT*\(\s*(ITEM_[^,]*),/
-item_label_pattern[ "_ITEM_EVENT_KEYWAIT" ] = /_ITEM_EVENT_KEYWAIT.*\(\s*(ITEM_[^,]*),/
-item_label_pattern[ "_ITEM_EVENT_NOWAIT" ] = /_ITEM_EVENT_NOWAIT.*\(\s*(ITEM_[^,]*),/
-item_label_pattern[ "_ITEM_EVENT_FIELD" ] = /_ITEM_EVENT_FIELD.*\(\s*(ITEM_[^,]*),/
+item_label_pattern[ "_ADD_ITEM" ]                   = /_ADD_ITEM.*\(\s*(ITEM_[^,]*),/
+item_label_pattern[ "_EASY_TALK_ITEM_EVENT" ]       = /_EASY_TALK_ITEM_EVENT*\(\s*(ITEM_[^,]*),/
+item_label_pattern[ "_ITEM_EVENT_KEYWAIT" ]         = /_ITEM_EVENT_KEYWAIT.*\(\s*(ITEM_[^,]*),/
+item_label_pattern[ "_ITEM_EVENT_NOWAIT" ]          = /_ITEM_EVENT_NOWAIT.*\(\s*(ITEM_[^,]*),/
+item_label_pattern[ "_ITEM_EVENT_FIELD" ]           = /_ITEM_EVENT_FIELD.*\(\s*(ITEM_[^,]*),/
 item_label_pattern[ "_ITEM_EVENT_SUCCESS_KEYWAIT" ] = /_ITEM_EVENT_SUCCESS_KEYWAIT.*\(\s*(ITEM_[^,]*),/
-item_label_pattern[ "_ITEM_EVENT_SUCCESS_NOWAIT" ] = /_ITEM_EVENT_SUCCESS_NOWAIT.*\(\s*(ITEM_[^,]*),/
-
+item_label_pattern[ "_ITEM_EVENT_SUCCESS_NOWAIT" ]  = /_ITEM_EVENT_SUCCESS_NOWAIT.*\(\s*(ITEM_[^,]*),/
 
 # 取得アイテム情報
-item_info = Array.new
+item_info_list = Array.new
 
 
 #----------------------------------------------------------------------------------
@@ -61,9 +59,8 @@ item_info = Array.new
 #----------------------------------------------------------------------------------
 create_place_name_hash
 
-
 check_file_list = Dir[ PATH_TO_CHECK_TARGET_FILE ]
-check_file_list -= item_get_skip_file_list
+check_file_list -= skip_file_list
 
 
 check_file_list.each do |file_path|
@@ -87,10 +84,10 @@ check_file_list.each do |file_path|
           info.zone_id    = GetZoneID_byEvFileName( filename )
           info.item_name  = GetItemName( item_label )
           info.zone_name  = @@place_name_hash[ info.zone_id ]
-          item_info << info
+          item_info_list << info
         # アイテムラベル名がない
         else
-          item_get_failed_file_list << file_path
+          failed_file_list << file_path
         end
       end
     end
@@ -101,15 +98,15 @@ end
 
 
 # 解析結果を出力
-file = File::open( "../result/result.txt", "w" )
-item_info.each do |info|
+file = File::open( PATH_TO_RESULT_OUTPUT + FILE_NAME_RESULT, "w" )
+item_info_list.each do |info|
   data = info.zone_id + "\t" + info.zone_name + "\t" + info.item_name
   file.puts( data )
 end
 file.close
 
 # 解析失敗リストを出力
-file = File::open( "../result/failed.txt", "w" )
-file.puts( item_get_skip_file_list )
-file.puts( item_get_failed_file_list )
+file = File::open( PATH_TO_RESULT_OUTPUT + FILE_NAME_FAILED, "w" )
+file.puts( skip_file_list )
+file.puts( failed_file_list )
 file.close
