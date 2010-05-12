1313cf3b6b2594b9e5b2d8d7e7352115bc177c4f
Shinya Takahashi <takahashi@gamefreak.co.jp>
2010-05-12 04:11:49 +0000

    BTS1727 トレーナー配置修正

--- resource/fldmapdata/eventdata/tmp/d06r0201.h
+++ resource/fldmapdata/eventdata/tmp/d06r0201.h
@@ -15,15 +15,15 @@
 #define D06R0201_MIDDLEMAN1_02             0
 #define D06R0201_MIDDLEMAN1_01             1
 #define D06R0201_FLD_ITEM_01               2
-#define TR_HAKAIW1_08                      3
-#define TR_HAKAIW1_07                      4
-#define TR_HAKAIM1_17                      5
-#define TR_HAKAIM1_18                      6
-#define TR_HAKAIM1_19                      7
-#define TR_HAKAIM1_20                      8
-#define TR_DOCTOR_01                       9
-#define D06R0201_FLD_ITEM_02              10
-#define TR_HAKAIW1_13                     11
+#define TR_HAKAIW1_07                      3
+#define TR_HAKAIM1_17                      4
+#define TR_HAKAIM1_18                      5
+#define TR_HAKAIM1_19                      6
+#define TR_HAKAIM1_20                      7
+#define TR_DOCTOR_01                       8
+#define D06R0201_FLD_ITEM_02               9
+#define TR_HAKAIW1_13                     10
+#define TR_HAKAIW1_08                     11
 
 #endif //ZONE_D06R0201_EVC_H
 //Ç±ÇÃÉtÉ@ÉCÉãÇÕ../data/d06r0201.mevÇ©ÇÁé©ìÆê∂ê¨Ç≥ÇÍÇ‹ÇµÇΩ
