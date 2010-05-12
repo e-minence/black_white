aeeea785345d2d5bec866d1a2ad90acd7db80614
Katsunori Suginaka <suginaka_katsunori@gamefreak.co.jp>
2010-05-12 09:16:21 +0000

    R04:ストッパーの位置を調整（ジムクリア後だと移動している）

--- resource/fldmapdata/script/r04_def.h
+++ resource/fldmapdata/script/r04_def.h
@@ -15,5 +15,7 @@
 #define SCRID_BG_R04_SIGN4_01 (8)
 #define SCRID_BG_R04_SIGN1_01 (9)
 #define SCRID_R04_MOUNTAIN_01 (10)
+#define SCRID_INIT_R04 (11)
+#define SCRID_RECOVER_R04 (12)
 
 #endif //_R04_DEF_H_
