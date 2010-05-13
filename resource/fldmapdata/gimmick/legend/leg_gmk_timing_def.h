10fbe212ec811b507598a41211f8c43786d2ef33
Minako Adachi <adachi_minako@gamefreak.co.jp>
2010-05-13 10:30:58 +0000

    シン・ム　エネルギー収縮イベント　タイミング調整完了

--- resource/fldmapdata/gimmick/legend/leg_gmk_timing_def.h
+++ resource/fldmapdata/gimmick/legend/leg_gmk_timing_def.h
@@ -15,5 +15,5 @@
 #define LEG_SE0_FRM  (0)              //ＳＥ1つ目上がる音（じょわ）
 #define LEG_SE1_FRM  (131)            //ＳＥ2つ目光る音（ぴかあああ)
 #define LEG_SE2_FRM  (108)            //ＳＥ3つ目エネルギーループ音
-#define LEG_SE3_FRM  (305)            //ＳＥ4つ目エネルギー収縮音
+#define LEG_SE3_FRM  (250)            //ＳＥ4つ目エネルギー収縮音
   
