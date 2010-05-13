a7d2eec0d5ea412e44c43177772e20b86ae023e3
Toshihiro Obata <obata_toshihiro@gamefreak.co.jp>
2010-05-13 11:52:21 +0000

    フォルダ構成を整理

--- resource/fldmapdata/script/check_tool/item_check.bat
+++
@@ -1,8 +0,0 @@
-Path=c:\cygwin\bin; 
-cp ../../../../prog/include/item/itemsym.h ./item/copy/itemsym.h
-cp ../../../message/src/place_name.gmm ./item/copy/place_name.gmm
-cp ../../zonetable/zonetable.xls ./item/copy/zonetable.xls
-cd ./item/script
-ruby ./excel_tab_out.rb ../copy/zonetable.xls 1 ../copy/zonetable.txt
-ruby ./item_listup.rb
-pause
