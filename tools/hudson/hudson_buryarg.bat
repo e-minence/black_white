c0b7b31c6bc8dc3e7486a7d71a6d7c2278b01046
Genya Hosaka <hosaka@gamefreak.co.jp>
2010-05-13 02:45:26 +0000

    HUDSON用バッチ　整理

--- tools/hudson/hudson_buryarg.bat
+++ tools/hudson/hudson_buryarg.bat
@@ -6,19 +6,19 @@ REM 制作者  : hosaka_genya
 REM 日付    : 2010/05/12
 REM =================================================================
 
-SET PATH_TOOLS=%PROJECT_ROOT%\tools\hudson
+REM main.srl
+SET PATH_MAIN_SRL=%PROJECT_PROGDIR%bin/ARM9-TS.HYB/Release/main.srl
+SET PATH_MAIN_TLF=%PROJECT_PROGDIR%bin/ARM9-TS.HYB/Release/main.tlf
 
 REM テストナンバー >> hudsonで指定
 SET TEST_NO=%1
+@echo TEST_NO = %TEST_NO%
 
 REM ================================
 REM ユーザーネーム設定
 REM ================================
 SET USERNAME=hudson
 @echo USERNAME = %USERNAME%
-
-REM まずmakeする
-make
 
 REM ================================
 REM TLFに書きこんでからSRLを再生成
--- tools/hudson/hudson_test.bat
+++ tools/hudson/hudson_test.bat
@@ -6,17 +6,7 @@ REM 制作者  : hosaka_genya
 REM 日付    : 2009/11/24
 REM =================================================================
 
-REM hudsonで定義される環境変数を利用
-SET PROJECT_ROOT=%WORKSPACE%
-SET PROJECT_PROGDIR=%PROJECT_ROOT%/prog/
-
-REM ##################################################
-REM		パスの追加
-REM ##################################################
-PATH=c:\tools;%PROJECT_ROOT%;%PROJECT_ROOT%\tools;c:\tools\subversion\bin;c:\cygwin\bin;c:\tools\vim;C:\tools;%NITROSDK_ROOT%\tools\bin;%NITROSYSTEM_ROOT%\tools\bin;%PATH%;
-
 REM main.srl
-SET PATH_MAIN_TLF=%PROJECT_PROGDIR%bin/ARM9-TS.HYB/Release/main.tlf
 SET PATH_MAIN_SRL=%PROJECT_PROGDIR%bin/ARM9-TS.HYB/Release/main.srl
 
 REM 最後のPrintfからのタイムアウト(秒) >> hudsonで指定
@@ -24,9 +14,6 @@ SET TIMEOUT_DISP=%1
 
 REM 対象DSのシリアルナンバー >> hudsonで指定
 SET SERIAL_NO=%2
-
-REM テストナンバー >> hudsonで指定
-SET TEST_NO=%3
 
 REM wbrom:05093474
 REM hosaka:05113644
@@ -43,22 +30,6 @@ SET USERNAME=hudson
 @echo loadpath = %PATH_MAIN_SRL%
 @echo TIMEOUT_DISP = %TIMEOUT_DISP%
 @echo serial_no = %SERIAL_NO%
-
-REM ================================
-REM makeする
-REM ================================
-make
-if %ERRORLEVEL% == 0 goto _MAKE_SUCCESS:
-@echo make error.
-exit 0
-:_MAKE_SUCCESS
-
-REM ================================
-REM まずTLFに書きこんでからSRLを再生成
-REM ================================
-buryarg.TWL %PATH_MAIN_TLF% %TEST_NO%
-touch %PATH_MAIN_SRL%
-make
 
 REM ================================
 REM ブート
