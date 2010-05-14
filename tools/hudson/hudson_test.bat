a5690ba736d4aadbe13b82450e649c5495cb438e
Genya Hosaka <hosaka@gamefreak.co.jp>
2010-05-14 02:56:26 +0000

    HUDSONテスト：引数埋込も同一マクロで

--- tools/hudson/hudson_test.bat
+++ tools/hudson/hudson_test.bat
@@ -8,12 +8,17 @@ REM =================================================================
 
 REM main.srl
 SET PATH_MAIN_SRL=%PROJECT_PROGDIR%bin/ARM9-TS.HYB/Release/main.srl
+SET PATH_MAIN_TEF=%PROJECT_PROGDIR%bin/ARM9-TS.HYB/Release/main.tef
+SET PATH_MAIN_TLF=%PROJECT_PROGDIR%bin/ARM9-TS.HYB/Release/main.tlf
 
 REM 最後のPrintfからのタイムアウト(秒) >> hudsonで指定
 SET TIMEOUT_DISP=%1 
 
 REM 対象DSのシリアルナンバー >> hudsonで指定
 SET SERIAL_NO=%2
+
+REM テストナンバー >> hudsonで指定
+SET TEST_NO=%3
 
 REM wbrom:05093474
 REM hosaka:05113644
@@ -30,6 +35,18 @@ SET USERNAME=hudson
 @echo loadpath = %PATH_MAIN_SRL%
 @echo TIMEOUT_DISP = %TIMEOUT_DISP%
 @echo serial_no = %SERIAL_NO%
+@echo test_no = %TEST_NO%
+
+REM ================================
+REM 引数をTLFに書きこんでからSRLを再生成
+REM ================================
+cd prog
+
+buryarg.TWL %PATH_MAIN_TLF% %TEST_NO%
+touch %PATH_MAIN_TEF%
+make
+
+cd ..
 
 REM ================================
 REM ブート
