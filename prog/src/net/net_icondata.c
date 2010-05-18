5ac150b52df87d7840338f34f8d4e8bda2dbc6e3
Katsumi Ohno <ohno@gamefreak.co.jp>
2010-05-18 12:32:02 +0000

    通信アイコン変更は元に戻した

--- prog/src/net/net_icondata.c
+++ prog/src/net/net_icondata.c
@@ -38,15 +38,8 @@ const u32 NET_ICONDATA_GetTableID(void)
 
 void NET_ICONDATA_GetNoBuff(int* pNoBuff)
 {
-  if(GFL_NET_GetGameServiceID() == WB_NET_FIELDMOVE_SERVICEID){
-    pNoBuff[GFL_NET_ICON_WIFINCGR] = NARC_wmi_wiwi_NCGR;
-    pNoBuff[GFL_NET_ICON_WMNCGR] = NARC_wmi_wiwi_NCGR;
-    pNoBuff[GFL_NET_ICON_WMNCLR] = NARC_wmi_wiwi_NCLR;
-  }
-  else{
-    pNoBuff[GFL_NET_ICON_WIFINCGR] = NARC_wmi_wifi_NCGR;
-    pNoBuff[GFL_NET_ICON_WMNCGR] = NARC_wmi_wm_NCGR;
-    pNoBuff[GFL_NET_ICON_WMNCLR] = NARC_wmi_wm_NCLR;
-  }
+  pNoBuff[GFL_NET_ICON_WIFINCGR] = NARC_wmi_wifi_NCGR;
+  pNoBuff[GFL_NET_ICON_WMNCGR] = NARC_wmi_wm_NCGR;
+  pNoBuff[GFL_NET_ICON_WMNCLR] = NARC_wmi_wm_NCLR;
 }
 
