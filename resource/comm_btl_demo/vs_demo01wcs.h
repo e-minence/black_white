8dae8ffc72aba40fdaf6558b6cf0754f029ab752
Akito Mori <mori@gamefreak.co.jp>
2010-05-20 11:53:46 +0000

    社内調整：WCS大会用のパーティクル追加

---
+++ resource/comm_btl_demo/vs_demo01wcs.h
@@ -0,0 +1,13 @@
+#ifndef __VS_DEMO01WCS_H__
+#define __VS_DEMO01WCS_H__
+
+#define VS_DEMO01WCS_VS_DEMO01A		0
+#define VS_DEMO01WCS_VS_DEMO01C		1
+#define VS_DEMO01WCS_VS_DEMO02A		2
+#define VS_DEMO01WCS_VS_WCS01B		3
+
+//vs_demo01wcs.spaì‡ÇÃÉäÉ\Å[ÉXå¬êî
+#define VS_DEMO01WCS_SPAMAX	(4)
+
+#endif
+
