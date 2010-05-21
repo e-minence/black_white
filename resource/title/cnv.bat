b4ae6f6db03b2686a8f285f806a48924f12e968e
Akito Mori <mori@gamefreak.co.jp>
2010-05-21 08:25:07 +0000

    社内要望：タイトル画面２D素材追加。背景・OBJ

--- resource/title/cnv.bat
+++ resource/title/cnv.bat
@@ -10,11 +10,16 @@ g2dcvtr wb_logo_bk.ncl -pcm
 g2dcvtr wb_logo_bk2.nsc -bg
 g2dcvtr wb_logo_bk2.ncl -pcm
 g2dcvtr wht_logo.nsc -bg
+g2dcvtr wht_base.nsc -bg
 g2dcvtr wht_logo.ncl
 g2dcvtr blk_logo.nsc -bg
+g2dcvtr blk_base.nsc -bg
 g2dcvtr blk_logo.ncl
 g2dcvtr mist.nsc -bg
 g2dcvtr mist.ncl -pcm
+g2dcvtr wht_obj.nce
+g2dcvtr blk_obj.nce
+
 
 echo narcçÏê¨
 nnsarc -c -l -n -i title -S title_arc.txt
