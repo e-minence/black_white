d86c0a7b6accdfe5b48a39f8e13eed9d23e18151
Nozomu Saito <saito_nozomu@gamefreak.co.jp>
2010-05-27 11:26:04 +0000

    //BG話しかけ開始コマンドチェック追加

---
+++ resource/fldmapdata/script/scr_check/bg_talk_start_chk.bat
@@ -0,0 +1,3 @@
+@echo check_start..
+ruby bg_talk_start_chk.rb > bg_result.txt
+@echo check_end
---
+++ resource/fldmapdata/script/scr_check/bg_talk_start_chk.rb
@@ -0,0 +1,17 @@
+#EVƒtƒ@ƒCƒ‹—ñ‹“
+all_ary = Dir.glob("../*.ev")
+ary = Array.new
+all_ary.each{|i|
+  i.sub!(/..\//,"")
+  idx = i.index("sp_")
+  if (idx == nil) || (idx != 0) then
+    ary << i
+  end
+}
+
+ary.each{|i|
+  src = "../" + i
+  dst = i
+#  system("ruby com_del.rb #{src} #{dst}")
+  system("ruby scr_check_bg.rb #{dst}")
+}
