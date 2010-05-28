9a167a8fd50b2cf7df39b2ae101090ffc8d28e70
Naoto Murakami <murakami_naoto@gamefreak.co.jp>
2010-05-28 08:47:24 +0000

    GFBTS877 OBJキャラワークの操作を、イニットからゾーンに修正した。フラグの条件にも不具合があったので合わせて修正した。

--- resource/fldmapdata/script/d08r0901_def.h
+++ resource/fldmapdata/script/d08r0901_def.h
@@ -7,6 +7,6 @@
 //スクリプトデータID定義
 #define SCRID_POS_D08R0901_BOSS_01 (1)
 #define SCRID_D08R0901_POKE_01 (2)
-#define SCRID_INIT_D08R0901 (3)
+#define SCRID_ZONE_D08R0901 (3)
 
 #endif //_D08R0901_DEF_H_
