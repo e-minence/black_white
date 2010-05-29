24cfe44e709a234377cfb519149eae352830a60e
Nozomu Saito <saito_nozomu@gamefreak.co.jp>
2010-05-29 05:14:41 +0000

    //不要データをテーブルから削除 セーブデータに影響してますが、許可をいただいております。

--- prog/src/field/field_gimmick.c
+++ prog/src/field/field_gimmick.c
@@ -64,7 +64,6 @@ FS_EXTERN_OVERLAY(field_gimmick_t01);
 
 const static FSOverlayID FldGimmickOverlay[FLD_GIMMICK_MAX] = {
 	NULL,					//0:–³‚µ
-  NULL,
   FS_OVERLAY_ID(field_gimmick_h01),
   FS_OVERLAY_ID(field_gym_elec),
   FS_OVERLAY_ID(field_gym_normal),
@@ -123,7 +122,6 @@ const static FSOverlayID FldGimmickOverlay[FLD_GIMMICK_MAX] = {
 
 const static FLD_GMK_SETUP_FUNC FldGimmickSetupFunc[FLD_GIMMICK_MAX] = {
 	NULL,					//0:–³‚µ
-  NULL,
   H01_GIMMICK_Setup,
   GYM_ELEC_Setup,
   GYM_NORMAL_Setup,
@@ -182,7 +180,6 @@ const static FLD_GMK_SETUP_FUNC FldGimmickSetupFunc[FLD_GIMMICK_MAX] = {
 
 const static FLD_GMK_END_FUNC FldGimmickEndFunc[FLD_GIMMICK_MAX] = {
 	NULL,					//0:–³‚µ
-  NULL,
   H01_GIMMICK_End,
   GYM_ELEC_End,
   GYM_NORMAL_End,
@@ -241,7 +238,6 @@ const static FLD_GMK_END_FUNC FldGimmickEndFunc[FLD_GIMMICK_MAX] = {
 
 const static FLD_GMK_MOVE_FUNC FldGimmickMoveFunc[FLD_GIMMICK_MAX] = {
 	NULL,					//0:–³‚µ
-  NULL,
   H01_GIMMICK_Move,
   GYM_ELEC_Move,
   GYM_NORMAL_Move,
