897491ca020ba1b1a4dc1e41e4b4343f0597bdb7
Shota Kageyama <kageyama_shota@gamefreak.co.jp>
2010-05-30 08:39:44 +0000

    【社内要望】
    ・バトル
    鳴き声、わざが出るときのボリューム変化を修正
    
    【MC_BTS】No.3368の対処
    
    SEQ_SE_SYS_21（検索中の音）ですが、
    下記の通りに変更しました。
    
    ●SEQ_SE_SYS_21（PLAYER_SE_SYSに戻した。）
    
    ⇒フィーリングチェックで使用。（今までと同じ）
    
    ●SEQ_SE_SYS_102を新たに追加。（SEQ_SE_SYS_21と中身は同じ。PLAYER_SE_2に変更）
    
    ⇒WCSで使用。検索中の音。
    
    ●BGM修正
    
    C04
    C_10_W
    D04
    Subway_trainer
    Legend_Born,Come

--- prog/src/battle/btlv/btlv_effvm_dat.h
+++ prog/src/battle/btlv/btlv_effvm_dat.h
@@ -15,6 +15,6 @@
 #define EFFVM_VOLUME_UP_FRAME ( 20 )    //100％に何フレームでなるかを指定（60で1秒）
 
 //ポケモンの鳴き声
-#define EFFVM_VOLUME_DOWN_RATIO_PV ( 75 )  //マスターボリュームを何％ダウンするか（80で80%ダウン）
+#define EFFVM_VOLUME_DOWN_RATIO_PV ( 85 )  //マスターボリュームを何％ダウンするか（80で80%ダウン）
 #define EFFVM_VOLUME_DOWN_FRAME_PV ( 10 )  //EFFVM_VOLUME_DOWN_RATIOの％に何フレームでなるかを指定（60で1秒）
 #define EFFVM_VOLUME_UP_FRAME_PV ( 10 )    //100％に何フレームでなるかを指定（60で1秒）
