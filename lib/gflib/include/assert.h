3c1835d537dfd207b4d551b9407bde4e3f953418
Toru Nagihashi <nagihashi@gamefreak.co.jp>
2010-06-02 12:34:46 +0000

    assert.hのGF_ASSERT_ENABLEが定義されていないとmakeが通らなかったバグを修正

--- lib/gflib/include/assert.h
+++ lib/gflib/include/assert.h
@@ -13,7 +13,7 @@
 extern "C" {
 #endif
 
-//デバッグ定義がなければ
+//デバッグ定義
 #ifdef PM_DEBUG
 #define	GF_ASSERT_ENABLE
 #endif//PM_DEBUG
@@ -22,9 +22,6 @@ extern "C" {
 #ifdef	GF_ASSERT_ENABLE
 
 // ASSERT有効の場合 --------------------------------------------------------------------------
-extern void GFL_Assert(const char * filename, unsigned int line_no, const char * exp);
-extern void GFL_AssertMsg(const char * filename, unsigned int line_no, const char * fmt, ... );
-
 #define GF_ASSERT(exp)		\
 	(exp)? ((void)0): GFL_Assert(__FILE__, __LINE__, #exp);
 #define GF_ASSERT_MSG(exp, ... )		\
@@ -34,8 +31,11 @@ extern void GFL_AssertMsg(const char * filename, unsigned int line_no, const cha
 #define GF_PANIC(...)         GF_ASSERT_MSG(0,__VA_ARGS__)
 
 
-/* ASSERT情報出力をユーザ定義するための関数型定義 */
+//下記関数を直接使用を禁止  GF_ASSERTマクロを使用すること
+extern void GFL_Assert(const char * filename, unsigned int line_no, const char * exp);
+extern void GFL_AssertMsg(const char * filename, unsigned int line_no, const char * fmt, ... );
 
+/* ASSERT情報出力をユーザ定義するための関数型定義 */
 typedef void (*GFL_ASSERT_DispInitFunc)(void);
 typedef void (*GFL_ASSERT_DispMainFunc)( const char* );
 typedef void (*GFL_ASSERT_DispFinishFunc)(void);
@@ -45,11 +45,13 @@ extern void GFL_ASSERT_SetDisplayFunc( GFL_ASSERT_DispInitFunc initFunc, GFL_ASS
 
 #else
 // ASSERT無効の場合 --------------------------------------------------------------------------
-
 #define GF_ASSERT(exp)				((void)0);
 #define GF_ASSERT_MSG(exp, ...)		((void)0);
 
 #define GF_PANIC(...)         OS_Panic(__VA_ARGS__)
+
+
+#define GFL_ASSERT_SetDisplayFunc( ... )  ((void)0);
 
 #endif
 
