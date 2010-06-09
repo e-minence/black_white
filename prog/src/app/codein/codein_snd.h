72813de8ae7a2ed23c50db1ce34df00d080e8002
Akito Mori <mori@gamefreak.co.jp>
2010-06-09 03:29:10 +0000

    GFBTS1270:指定と違うSEが鳴っている→対処

--- prog/src/app/codein/codein_snd.h
+++ prog/src/app/codein/codein_snd.h
@@ -13,9 +13,9 @@
 #include "sound/pm_sndsys.h"
 
 #define CI_SE_BLOCK   ( SEQ_SE_SELECT1 ) 
-#define CI_SE_TOUCH   ( SEQ_SE_SELECT1 )
-#define CI_SE_BACK    ( SEQ_SE_SELECT1 )
-#define CI_SE_END     ( SEQ_SE_SELECT1 )
+#define CI_SE_TOUCH   ( SEQ_SE_DECIDE1 )
+#define CI_SE_BACK    ( SEQ_SE_CANCEL3 )
+#define CI_SE_END     ( SEQ_SE_DECIDE2 )
 #define CI_SE_MOVE    ( SEQ_SE_SELECT1 )
 
 
