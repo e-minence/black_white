bfb57f4937921081a369cdbb9f58bf7724919f18
Hisashi Sogabe <sogabe@gamefreak.co.jp>
2010-06-10 06:34:33 +0000

    下画面切り替え時にちらついてしまう不具合の対処

--- prog/src/battle/btlv/btlv_input.c
+++ prog/src/battle/btlv/btlv_input.c
@@ -2439,13 +2439,16 @@ static  void  TCB_TransformWaza2Standby( GFL_TCB* tcb, void* work )
     if( ttw->biw->tcb_execute_count == 0 )
     {
       SetupScaleChange( ttw->biw, TTC2S_START_SCALE, TTC2S_END_SCALE, TTC2S_SCALE_SPEED, COMMAND_POS_Y );
-      GFL_BG_SetVisible( GFL_BG_FRAME0_S, VISIBLE_OFF );
-      GFL_BG_SetVisible( GFL_BG_FRAME1_S, VISIBLE_OFF );
-      GFL_BG_SetVisible( GFL_BG_FRAME3_S, VISIBLE_ON );
       ttw->seq_no++;
     }
     break;
   case 4:
+    GFL_BG_SetVisible( GFL_BG_FRAME0_S, VISIBLE_OFF );
+    GFL_BG_SetVisible( GFL_BG_FRAME1_S, VISIBLE_OFF );
+    GFL_BG_SetVisible( GFL_BG_FRAME3_S, VISIBLE_ON );
+    ttw->seq_no++;
+    break;
+  case 5:
   default:
     if( ttw->biw->tcb_execute_count == 0 )
     {
