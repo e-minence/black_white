cd014e4d9291cb34d102c6edd17edf87934f75a1
Koji Kawada <kawada_koji@gamefreak.co.jp>
2010-06-12 07:11:10 +0000

    フォント修正に伴い、録画選択画面の数字の位置を修正。

--- prog/src/net_app/btl_rec_sel/btl_rec_sel.c
+++ prog/src/net_app/btl_rec_sel/btl_rec_sel.c
@@ -1967,7 +1967,7 @@ static void Btl_Rec_Sel_FixUpdateTime( BTL_REC_SEL_PARAM* param, BTL_REC_SEL_WOR
     WORDSET_ExpandStr( wordset, strbuf, src_strbuf );
    
     bmp_data = GFL_BMPWIN_GetBmp( work->fix_bmpwin[FIX_TIME] );
-    PRINTSYS_PrintQueColor( work->print_que_fix_time, bmp_data, 2, 0, strbuf, work->font, color );
+    PRINTSYS_PrintQueColor( work->print_que_fix_time, bmp_data, 2, 2, strbuf, work->font, color );
     work->fix_finish[FIX_TIME] = FALSE;
 
     GFL_STR_DeleteBuffer( strbuf );
