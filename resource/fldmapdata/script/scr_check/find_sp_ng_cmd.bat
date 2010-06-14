123a2c579c438ac4e1810bbb25e98b9f81ff64bf
Nozomu Saito <saito_nozomu@gamefreak.co.jp>
2010-06-14 04:10:24 +0000

    //デバッグ環境追加 バッチファイル追加

---
+++ resource/fldmapdata/script/scr_check/find_sp_ng_cmd.bat
@@ -0,0 +1,3 @@
+ruby find_splabel_ng_cmd.rb init_ng_cmd.txt ev_init > init_out.txt
+ruby find_splabel_ng_cmd.rb recover_ng_cmd.txt ev_recover > recov_out.txt
+ruby find_splabel_ng_cmd.rb zone_ng_cmd.txt ev_zone > zone_out.txt
