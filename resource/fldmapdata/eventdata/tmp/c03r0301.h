2fbeef527aea818ebb24b926ef050d9335a005c0
Naoto Murakami <murakami_naoto@gamefreak.co.jp>
2010-06-18 15:03:28 +0000

    GFBTS1515 不要なBGイベントを削除

--- resource/fldmapdata/eventdata/tmp/c03r0301.h
+++ resource/fldmapdata/eventdata/tmp/c03r0301.h
@@ -20,7 +20,6 @@
 
 #define BGID_C03R0301_ELEVATOR            0
 #define BG_C03R0301_STATUE_01             1
-#define BG_C03R0301_PAINTING_01           2
-#define BGID_C03R0301_ELEVATOR_FRONT      3
+#define BGID_C03R0301_ELEVATOR_FRONT      2
 
 #endif //ZONE_C03R0301_EVB_H
