3a759ed660f99817e2db371817e3606060777b05
Toru Nagihashi <nagihashi@gamefreak.co.jp>
2010-06-19 06:14:18 +0000

    アニメ待ち中にアクティブ設定されると、タッチしても反応がなくなるバグ修正

--- prog/src/ui/touchbar.c
+++ prog/src/ui/touchbar.c
@@ -143,7 +143,10 @@ static void Icon_PushFuncNormal( ICON_WORK *p_wk );
 static void Icon_PushFuncFlip( ICON_WORK *p_wk );
 static void Icon_ActiveFuncNormal( ICON_WORK *p_wk, BOOL is_active );
 static void Icon_ActiveFuncFlip( ICON_WORK *p_wk, BOOL is_active );
-
+//-------------------------------------
+///	再スタート
+//=====================================
+static void TOUCHBAR_ReStart( TOUCHBAR_WORK *p_wk );
 //=============================================================================
 /**
  *					データ
@@ -529,6 +532,10 @@ void TOUCHBAR_SetActive( TOUCHBAR_WORK *p_wk,  TOUCHBAR_ICON icon, BOOL is_activ
 
 	//設定
 	ICON_SetActive( p_icon, is_active );
+
+  //内部状態を修復
+  //（タッチアニメ中にアクティブにするとアニメ待ちで無限ループになるため）
+  TOUCHBAR_ReStart( p_wk );
 }
 //----------------------------------------------------------------------------
 /**	
@@ -787,6 +794,21 @@ BOOL TOUCHBAR_GetFlip( const TOUCHBAR_WORK *cp_wk, TOUCHBAR_ICON icon )
 	//取得
 	return ICON_GetFlip( cp_icon );
 }
+
+
+
+//----------------------------------------------------------------------------
+/**
+ *	@brief  タッチバーリセット：ｗ
+ *
+ *	@param	TOUCHBAR_WORK *p_wk ワーク
+ */
+//-----------------------------------------------------------------------------
+static void TOUCHBAR_ReStart( TOUCHBAR_WORK *p_wk )
+{
+  p_wk->seq = TOUCHBAR_SEQ_MAIN;
+}
+
 //=============================================================================
 /**
  *	TOUCHBAR private
