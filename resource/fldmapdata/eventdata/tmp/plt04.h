287a5486334424496469c68aef4b04ec18236088
Katsunori Suginaka <suginaka_katsunori@gamefreak.co.jp>
2010-06-24 09:19:11 +0000

    【GFBTS：1739】PLT04：ハイリンクで本来行けないはずのサザナミ湾に行けてしまうバグを修正（ストッパー対処）

--- resource/fldmapdata/eventdata/tmp/plt04.h
+++ resource/fldmapdata/eventdata/tmp/plt04.h
@@ -13,6 +13,7 @@
 #define PLT04_MAN02_01                     1
 #define PLT04_STOPPER_01                   2
 #define PLT04_STOPPER_PC                   3
+#define PLT04_STOPPER_02                   4
 
 #endif //ZONE_PLT04_EVC_H
 //‚±‚Ìƒtƒ@ƒCƒ‹‚Í../data/plt04.mev‚©‚ç©“®¶¬‚³‚ê‚Ü‚µ‚½
