c6a569c41b056ca51ac61112631428970e209375
Sosuke Tamada <tamada@gamefreak.co.jp>
2010-06-27 09:59:32 +0000

    C07/D08のメッセージも削除できるように修正。ダミー箇所にはD08R0101を当てるようにした

--- resource/fldmapdata/zonetable/scripts/zonetable.rb
+++ resource/fldmapdata/zonetable/scripts/zonetable.rb
@@ -252,11 +252,12 @@ class ZoneDataFile < OutputFile
     matrixId = "MATRIX_ID_#{column[@cl.cMATRIXID]}".upcase
 
     mapid = id.downcase
-    if mapid != "c07" && mapid != "d08" && mapid != "d08r0101" then mapid = "d08" end
+    if mapid != "c07" && mapid != "d08" && mapid != "d08r0101" then mapid = "d08r0101" end
     event_id = "NARC_eventdata_#{mapid}_bin"
 		script =  "NARC_script_seq_#{mapid}_bin"
 		sp_script =  "NARC_script_seq_sp_#{mapid}_bin"
-		msg = "NARC_script_message_#{mapid}_dat"
+		#msg = "NARC_script_message_#{mapid}_dat"
+		msg = "NARC_script_message_d08r0101_dat"
 
 		bgm_spring = column[@cl.cBGM_SPRING]
 		bgm_summer = column[@cl.cBGM_SUMMER]
