42fc6667926f517640da46c1fba16261b7af31e9
Junichi Masuda <matsuda@gamefreak.co.jp>
2010-07-13 08:40:17 +0000

    ASSERT_HEAVYを作成

--- lib/gflib/include/assert.h
+++ lib/gflib/include/assert.h
@@ -14,22 +14,43 @@ extern "C" {
 #endif
 
 //デバッグ定義
-#ifdef PM_DEBUG
+//#ifdef PM_DEBUG
 #define	GF_ASSERT_ENABLE
-#endif//PM_DEBUG
+//#endif//PM_DEBUG
 
 
 #ifdef	GF_ASSERT_ENABLE
 
-// ASSERT有効の場合 --------------------------------------------------------------------------
-#define GF_ASSERT(exp)		\
-	(exp)? ((void)0): GFL_Assert(__FILE__, __LINE__, #exp);
-#define GF_ASSERT_MSG(exp, ... )		\
-	(exp)? ((void)0): GFL_AssertMsg(__FILE__, __LINE__, __VA_ARGS__ );
-//#define GF_ASSERT_Printf( ... )		((exp)? ((void)0);
-//
-#define GF_PANIC(...)         GF_ASSERT_MSG(0,__VA_ARGS__)
+  //#define GF_ASSERT_Printf( ... )		((exp)? ((void)0);
+  //
 
+  #ifdef PM_DEBUG 
+  // ASSERT有効でデバッグROMの場合 ------------------------------------------------------
+    #define GF_ASSERT(exp)		\
+    	(exp)? ((void)0): GFL_Assert(__FILE__, __LINE__, #exp);
+    #define GF_ASSERT_MSG(exp, ... )		\
+    	(exp)? ((void)0): GFL_AssertMsg(__FILE__, __LINE__, __VA_ARGS__ );
+    #define GF_ASSERT_LIGHT(exp)		\
+    	(exp)? ((void)0): GFL_Assert(__FILE__, __LINE__, #exp);
+    #define GF_ASSERT_MSG_LIGHT(exp, ... )		\
+    	(exp)? ((void)0): GFL_AssertMsg(__FILE__, __LINE__, __VA_ARGS__ );
+    #define GF_ASSERT_HEAVY(exp)		\
+    	(exp)? ((void)0): GFL_Assert(__FILE__, __LINE__, #exp);
+    #define GF_ASSERT_MSG_HEAVY(exp, ... )		\
+    	(exp)? ((void)0): GFL_AssertMsg(__FILE__, __LINE__, __VA_ARGS__ );
+    #define GF_PANIC(...)         GF_ASSERT_MSG(0,__VA_ARGS__)
+  #else
+  // ASSERT有効で製品版ROMの場合 ------------------------------------------------------
+    #define GF_ASSERT(exp)				((void)0);
+    #define GF_ASSERT_MSG(exp, ...)		((void)0);
+    #define GF_ASSERT_LIGHT(exp)				((void)0);
+    #define GF_ASSERT_MSG_LIGHT(exp, ...)		((void)0);
+    #define GF_ASSERT_HEAVY(exp)		\
+    	(exp)? ((void)0): GFL_Assert(__FILE__, __LINE__, #exp);
+    #define GF_ASSERT_MSG_HEAVY(exp, ... )		\
+    	(exp)? ((void)0): GFL_AssertMsg(__FILE__, __LINE__, __VA_ARGS__ );
+    #define GF_PANIC(...)         OS_Panic(__VA_ARGS__)
+  #endif
 
 //下記関数を直接使用を禁止  GF_ASSERTマクロを使用すること
 extern void GFL_Assert(const char * filename, unsigned int line_no, const char * exp);
@@ -54,6 +75,11 @@ extern void GFL_ASSERT_SetDisplayFunc( GFL_ASSERT_DispInitFunc initFunc, GFL_ASS
 #define GFL_ASSERT_SetDisplayFunc( ... )  ((void)0);
 
 #endif
+
+///製品版ROMでASSERTが呼び出されたときに、コールバックをする関数へのポインタ
+typedef void (*GFL_ASSERT_RELEASEROM_CALLBACK_FUNC)( void );
+
+extern void GFL_ASSERT_SetReleaseRomWarningFunc( GFL_ASSERT_RELEASEROM_CALLBACK_FUNC func );
 
 #ifdef __cplusplus
 }/* extern "C" */
