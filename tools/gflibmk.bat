997205cd04def64af0d3509c6355c9fb008e5e5e
Katsumi Ohno <ohno@gamefreak.co.jp>
2010-10-04 09:04:50 +0000

    GPOWER配信プログラム このROMは現状店舗で稼動している配信プログラムです
    １．配信用プログラムが src test d_haishin_comm.cや src/test/ohno/d_gpower_list.c にかかれています
    ２．ROMの容量を削減してほしい、メモリも削減してほしいとの要望から
         関連付けしているファイルが少なくなっております。
         ライブラリをROM版にしてあります
    ３．起動すると、GPOWER用セーブデータがあるかどうか判別し
          ある場合には配信が開始、無い場合には設定画面に行きます

--- tools/gflibmk.bat
+++ tools/gflibmk.bat
@@ -11,6 +11,10 @@ SET TMP_PM_DEBUG=%PM_DEBUG%
 SET TMP_TWLSDK_PLATFORM=%TWLSDK_PLATFORM%
 SET TMP_TARGET_PLATFORM=%TARGET_PLATFORM%
 
+REM cp -r /cygdrive/e/home/gflib/gflib/src/network/*.* /cygdrive/c/home/gflib/gflib/src/network/
+REM cp -r /cygdrive/e/home/gflib/gflib/src/network/tool/*.* /cygdrive/c/home/gflib/gflib/src/network/tool/
+REM cp -r /cygdrive/e/home/gflib/gflib/src/ui/*.* /cygdrive/c/home/gflib/gflib/src/ui/
+REM cp -r /cygdrive/e/home/gflib/gflib/include/*.* /cygdrive/c/home/gflib/gflib/include/
 
 
 REM ============================================
@@ -24,8 +28,8 @@ SET TWL_FINALROM=
 REM ============================================
 REM     í èÌî≈ÇçÏê¨
 REM ============================================
-call %PROJECT_ROOT%prompt.bat cmd mkgl.bat %1
-call %PROJECT_ROOT%nitro_prompt.bat cmd mkgl.bat %1
+call e:\home\pokemon_wb\nitro_prompt.bat cmd mkgl.bat %1
+call e:\home\pokemon_wb\prompt.bat cmd mkgl.bat %1
 
 
 REM ============================================
@@ -39,8 +43,8 @@ SET TWL_FINALROM=yes
 REM ============================================
 REM     ROMî≈çÏê¨
 REM ============================================
-call %PROJECT_ROOT%prompt.bat cmd mkgl.bat %1
-call %PROJECT_ROOT%nitro_prompt.bat cmd mkgl.bat %1
+call e:\home\pokemon_wb\nitro_prompt.bat cmd mkgl.bat %1
+call e:\home\pokemon_wb\prompt.bat cmd mkgl.bat %1
 
 
 REM ============================================
